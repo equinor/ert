74315baf50e08ec2496fd3b95fc3a930b0f2cf20
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-02-26 22:14:48 +0000

    Added libecl_queue
    git-svn-id: http://ert.nr.no/repos/ert@639 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/libenkf/src/enkf_site_config.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <enkf_site_config.h>
+#include <hash.h>
+
+struct enkf_stite_config_struct {
+  hash_type * config;
+};
+
---
+++ devel/libenkf/src/enkf_site_config.h
@@ -0,0 +1,6 @@
+#ifndef __ENKF_SITE_CONFIG_H__
+#define __ENKF_SITE_CONFIG_H__
+
+typedef struct enkf_site_config_struct enkf_site_config_type;
+
+#endif
