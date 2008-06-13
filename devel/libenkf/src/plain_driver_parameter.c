c25a313d8b51464a35784db9f0ef8696ade99354
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-06-13 09:10:22 +0000

    Makefile update III
    git-svn-id: http://ert.nr.no/repos/ert@908 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/plain_driver_parameter.c
+++ devel/libenkf/src/plain_driver_parameter.c
@@ -37,8 +37,10 @@ static int __get_report_step(int report_step , state_enum state) {
     if (report_step == 0) 
       util_abort("%s: sorry at report_step:0 there is no forecast - aborting \n",__func__);
     return report_step - 1;
-  } else 
+  } else {
     util_abort("%s state:%d - internal error - aborting \n",__func__ , state);
+    return -1;  /* Shut up the compiler */
+  }
 }
 
 
