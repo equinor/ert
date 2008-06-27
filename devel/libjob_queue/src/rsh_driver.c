7ac07cd6cf407a923c991f431590ba93e3e5171b
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-06-27 21:26:41 +0000

    Updated havana_fault_config + added run_path + util_vfork_exec
    git-svn-id: http://ert.nr.no/repos/ert@950 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libjob_queue/src/rsh_driver.c
+++ devel/libjob_queue/src/rsh_driver.c
@@ -103,7 +103,7 @@ static void rsh_host_submit_job(rsh_host_type * rsh_host , rsh_job_type * job, c
      in the rsh_host_available function.
   */
 
-  util_vfork_exec(rsh_cmd , 3 , (const char *[3]) {rsh_host->host_name , submit_cmd , run_path} , true , NULL , NULL , NULL , NULL);
+  util_vfork_exec(rsh_cmd , 3 , (const char *[3]) {rsh_host->host_name , submit_cmd , run_path} , true , NULL , NULL , NULL , NULL , NULL);
   job->status = job_queue_done;
 
   pthread_mutex_lock( &rsh_host->host_mutex );
