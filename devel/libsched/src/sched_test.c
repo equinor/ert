0b985df2e88861bf6bb28529f09aae6b3b5527b4
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-07-04 05:42:09 +0000

    Updated sched_test.c
    git-svn-id: http://ert.nr.no/repos/ert@983 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libsched/src/sched_test.c
+++ devel/libsched/src/sched_test.c
@@ -15,30 +15,14 @@
 
 
 int main(int argc, char ** argv) {
-  int files;
-  const char *base_dir     = "tmpdir_";
-  const char *base     	   = "ECLIPSE";
-  const char *history_file = "/d/proj/bg/frs/EnKF_3Dsynthetic/FourWellsEnKF/Observations/History";
-  /*
-    char ** file_list    = ecl_util_alloc_scandir_filelist(path , base , ecl_summary_file , false , &files);
-    char  * header_file  = ecl_util_alloc_exfilename(path , base , ecl_summary_header_file , false , -1);
-  */
+  history_type * hist;
+  sched_file_type * sched_file = sched_file_alloc( util_make_time1(1 , 1 , 2000) );
+  sched_file_parse(sched_file ,  "/h/a152128/EnKF/devel/EnKF/libenkf/src/Gurbat/target.SCH");
   
+  hist = history_alloc_from_schedule( sched_file );
+  sched_file_fprintf(sched_file , 25 , -1 , -1 , "/tmp/target.SCH");
   
-  {
-    history_type * hist;
-    {
-      FILE * stream = util_fopen(history_file , "r");
-      hist = history_fread_alloc(stream);
-      fclose(stream);
-      history_summarize(hist , stdout);
-    }
-    history_ens_diag_ens_interactive(base_dir , "ECLIPSE" , false , false , true , hist);
-  }
-
-  /*
-    util_free_string_list(file_list , files);
-    free(header_file);
-  */
+  history_free( hist );
+  sched_file_free(sched_file);
 }
 				  
