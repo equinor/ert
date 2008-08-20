344c4ca8e0cf6fba52638f17f7dee420a668f953
jgv <jgv@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-08-20 08:07:38 +0000

    git-svn-id: http://ert.nr.no/repos/ert@1072 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/main.c
+++ devel/libenkf/src/main.c
@@ -29,7 +29,7 @@ void install_SIGNALS(void) {
 void text_splash() {
   int i;
   {
-#include "helge_lund.inc"
+#include "enkf.inc"
     printf("\n\n");
     for (i = 0; i < SPLASH_LENGTH; i++)
       printf("%s\n" , splash_text[i]);
