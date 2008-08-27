91bd3ddfe07c0aba038facc9d052e5df3cf4ec39
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-08-27 18:07:32 +0000

    Used stringlist in ext_joblist_python_fprintf()
    git-svn-id: http://ert.nr.no/repos/ert@1092 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libjob_queue/src/ext_joblist.h
+++ devel/libjob_queue/src/ext_joblist.h
@@ -2,6 +2,7 @@
 #define __EXT_JOBLIST_H__
 #include <hash.h>
 #include <ext_job.h>
+#include <stringlist.h>
 
 typedef struct ext_joblist_struct ext_joblist_type;
 
@@ -9,7 +10,7 @@ ext_joblist_type * ext_joblist_alloc();
 void               ext_joblist_free(ext_joblist_type * );
 ext_job_type     * ext_joblist_add_job(ext_joblist_type * , const char * , const char *);
 ext_job_type 	 * ext_joblist_get_job(const ext_joblist_type * , const char * );
-void               ext_joblist_python_fprintf(const ext_joblist_type * , const char ** , int , const char * , const hash_type * );
+void               ext_joblist_python_fprintf(const ext_joblist_type * , const stringlist_type * , const char * , const hash_type * );
 ext_job_type     * ext_joblist_alloc_new(ext_joblist_type * , const char * );
 bool               ext_joblist_has_job(const ext_joblist_type *  , const char * );
 
