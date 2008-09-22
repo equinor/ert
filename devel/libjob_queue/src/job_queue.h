42885a860774cab20c91ff29269eb8166b9d058d
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-09-22 15:51:49 +0000

    New version without the path_fmt variables
    git-svn-id: http://ert.nr.no/repos/ert@1176 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libjob_queue/src/job_queue.h
+++ devel/libjob_queue/src/job_queue.h
@@ -6,9 +6,9 @@
 typedef struct job_queue_struct job_queue_type;
 void                job_queue_finalize(job_queue_type * queue);
 void                job_queue_set_runpath_fmt(job_queue_type *  , const path_fmt_type * );
-job_queue_type   *  job_queue_alloc(int  , int , int  , const char * , const path_fmt_type * , const path_fmt_type * , void * );
+job_queue_type   *  job_queue_alloc(int  , int , int  , const char * , void * );
 void                job_queue_free(job_queue_type *);
-void                job_queue_add_job(job_queue_type * , int , int);
+void                job_queue_add_job(job_queue_type * , const char * , const char * , int , int);
 void                job_queue_run_jobs(job_queue_type * , int );
 void *              job_queue_run_jobs__(void * );
 job_status_type     job_queue_export_job_status(job_queue_type * , int );
