9290b88da0e646882575d8fe339c1975d80d84c3
jaskje <jaskje@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-11-03 15:22:48 +0000

    gen_data_config
    git-svn-id: http://ert.nr.no/repos/ert@1387 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/gen_data_config.h
+++ devel/libenkf/src/gen_data_config.h
@@ -8,7 +8,6 @@ extern "C" {
 #include <gen_common.h>
 typedef struct gen_data_config_struct gen_data_config_type; 
 
-
 void    	        gen_data_config_get_ecl_file(const gen_data_config_type * , int , char ** , char ** );
 void                    gen_data_config_get_obs_file(const gen_data_config_type *, int , char ** , char **);
 void    	        gen_data_config_free(gen_data_config_type * );
@@ -21,6 +20,19 @@ const bool            * gen_data_config_get_iactive(const gen_data_config_type *
 int                     gen_data_config_get_data_size(const gen_data_config_type * );
 void                    gen_data_config_set_obs_size(gen_data_config_type * , int);
 void                    gen_data_config_fload_iactive(gen_data_config_type * ,  FILE * , const char * ,gen_data_file_type  , int);
+void                    gen_data_config_read_local_map(gen_data_config_type *, int);
+void                    gen_data_config_change_iactive(gen_data_config_type *, int);
+void                    gen_data_config_fload_local_data(FILE *, gen_data_config_type *);
+bool                    gen_data_config_get_local_active(gen_data_config_type *, int);
+void                    gen_data_config_realloc_local(gen_data_config_type *, int);
+bool                    gen_data_config_fload_local_step_active(int);
+int                     gen_data_config_get_num_local_updates(gen_data_config_type *);
+void                    gen_data_config_set_local_step(gen_data_config_type *,int);
+int                     gen_data_config_get_local_step(gen_data_config_type *);
+int                   * gen_data_config_get_param_index_i(const gen_data_config_type *,int);
+int                   * gen_data_config_get_param_index_j(const gen_data_config_type *,int);
+int                   * gen_data_config_get_param_index_k(const gen_data_config_type *,int);
+int                     gen_data_config_get_num_param(const gen_data_config_type *,int);
 
 VOID_FREE_HEADER(gen_data_config)
 GET_ACTIVE_SIZE_HEADER(gen_data);
