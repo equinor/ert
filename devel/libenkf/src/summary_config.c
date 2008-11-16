bdca9e45399033cb29f29eb52601d16fd857dcb0
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-11-16 16:05:43 +0000

    Gurbat check in
    git-svn-id: http://ert.nr.no/repos/ert@1447 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/summary_config.c
+++ devel/libenkf/src/summary_config.c
@@ -21,66 +21,6 @@ struct summary_config_struct {
 
 
 
-
-
-//int summary_config_get_var_index(const summary_config_type * config , const char * var) {
-//  int index , i;
-//  index = -1;
-//  i = 0;
-//  while (index < 0 && i < config->data_size) {
-//    if (strcmp(var , config->var_list[i]) == 0) index = i;
-//    i++;
-//  }
-//  return index;
-//}
-//
-//
-//
-//bool summary_config_has_var(const summary_config_type * config , const char * var) {
-//  int index;
-//  index = summary_config_get_var_index(config , var);
-//  if (index >= 0)
-//    return true;
-//  else
-//    return false;
-//}
-//
-//
-//const char ** summary_config_get_var_list_ref(const summary_config_type * config) { return (const char **) config->var_list; }
-//
-//
-//void summary_config_add_var(summary_config_type * config , const char * var) {
-//  if (summary_config_has_var(config , var)) {
-//    fprintf(stderr,"%s: summary variable:%s already added to summary object - nothing done \n",__func__ , var);
-//    return;
-//  }
-//  
-//  if (true /* variable valid */) {
-//    config->data_size++;
-//    active_list_grow(config->active_list , 1);
-//    config->var_list = realloc(config->var_list , config->data_size * sizeof * config->var_list);
-//    config->var_list[config->data_size - 1] = util_alloc_string_copy(var);
-//  } else {
-//    fprintf(stderr,"%s: summary variable: %s not recognized - aborting \n",__func__ , var);
-//    abort();
-//  }
-//}
-
-
-
-
-//static summary_config_type * __summary_config_alloc( void ) {
-//  summary_config_type * config = malloc(sizeof *config);
-//
-//  config->data_size   	  = 0;
-//  config->var_list    	  = NULL;
-//  config->active_list     = active_list_alloc(0);
-//  return config;
-//
-//}
-
-
-
 const char * summary_config_get_var(const summary_config_type * config) {
   return config->var;
 }
