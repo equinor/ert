0f7d7b35863118f9ce3b2e5c44d2421bf8e67896
jgv <jgv@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2008-12-02 11:14:57 +0000

    Added some doc to conf.h.
    git-svn-id: http://ert.nr.no/repos/ert@1489 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libconfig/src/conf.h
+++ devel/libconfig/src/conf.h
@@ -1,5 +1,97 @@
 #ifndef __CONF_H__
 #define __CONF_H__
+
+/* libconfig: lightweight configuration parser
+ *
+ *
+ *
+ * Introduction
+ *
+ * This library provides a lightweight configuration parser for the
+ * enkf application. The goal of the library is to provide the
+ * developer with a tool for rapid specification of configuration
+ * files, automatic checking of user provided configuration files
+ * and typed access to configuration items.
+ * 
+ * 
+ * 
+ * A Simple Example
+ *
+ * Let us consider a simple example of user provided configuration
+ * file that can be used with the parser:
+ *
+ *
+ * res_sim FrontSim2007
+ * {
+ *   executable = /bin/frontsim2007; 
+ *   version    = 2007;
+ *   
+ *   run_host bgo179lin
+ *   {
+ *     hostname = bgo179lin.nho.hydro.com;
+ *     num_jobs = 4;
+ *   };
+ * };
+ *
+ *
+ * Note that the newlines are not neccessary. In the example above,
+ * the user has provided an instance of the class "res_sim" with name
+ * FrontSim2007. Further, the user has set the items executable and version.
+ * He has also provided a instance of the sub class "run_host" with name
+ * bgo179lin and allocated 4 jobs to this machine.
+ * 
+ * 
+ * 
+ * Structure
+ *
+ * The system is built around four basic objects:
+ *
+ *  - Class definitions.
+ *  - Item specifications.
+ *  - Instances of classes.
+ *  - Instances of item specifications, i.e. items.
+ *
+ * The relationship between the objects is as follows :
+ *
+ *  - Class:
+ *    . Can have contain both classes and item specifications.
+ *    . Can not contain items or class instances.
+ *
+ *  - Item specifications:
+ *    . Can not contain any of the other objects.
+ *
+ *  - Instances of classes:
+ *    . Can contain class instances and items.
+ *
+ *  - Items:
+ *    . Can not contain any of the other objects.
+ *
+ *
+ *
+ * General Use
+ *
+ * The parser is designed to be used in the following way:
+ *
+ *  - The developer creates the classes and item specifications needed.
+ *  - Using the library and the classes, user provided configuration
+ *    files are read and validated.
+ *  - If the validation fails, the developer can choose to exit.
+ *  - Using the library, the devloper has typed access to all
+ *    information provided by the user.
+ *  
+ */
+
+
+
+
+
+
+
+
+
+
+
+
 #include <stdbool.h>
 #include <set.h>
 #include <conf_data.h>
