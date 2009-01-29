c5774694159371dac5456a5486fcb2fe4498ac73
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-01-29 09:54:55 +0000

    Updated build process in libsched
    git-svn-id: http://ert.nr.no/repos/ert@1656 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/libsched/src/all_include.h
@@ -0,0 +1,15 @@
+#include <sched_file.h> 
+#include <sched_kw.h> 
+#include <sched_kw_untyped.h> 
+#include <sched_kw_gruptree.h> 
+#include <sched_kw_tstep.h> 
+#include <sched_kw_dates.h> 
+#include <sched_kw_wconhist.h> 
+#include <sched_kw_wconinjh.h> 
+#include <sched_kw_welspecs.h> 
+#include <sched_util.h> 
+#include <gruptree.h> 
+#include <history.h> 
+#include <sched_kw_wconprod.h> 
+#include <sched_kw_wconinj.h> 
+#include <sched_kw_wconinje.h>
