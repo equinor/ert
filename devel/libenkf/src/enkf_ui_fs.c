3ed1bd66b9db4d388bd4643f8cb6bb8ef1790d58
jgv <jgv@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-03-24 13:41:01 +0000

    git-svn-id: http://ert.nr.no/repos/ert@1858 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/enkf_ui_fs.c
+++ devel/libenkf/src/enkf_ui_fs.c
@@ -289,7 +289,7 @@ void enkf_ui_fs_menu(void * arg) {
 
    menu_add_separator(menu);
    menu_add_item(menu, "Copy full ensemble to another case", "eE", enkf_ui_fs_copy_ensemble, enkf_main, NULL); 
-   menu_add_item(menu, "Copy ensemble of parmaters to another case", "oO", enkf_ui_fs_copy_ensemble, enkf_main, NULL); 
+   menu_add_item(menu, "Copy ensemble of parmaters to another case", "oO", enkf_ui_fs_copy_ensemble_of_parameters, enkf_main, NULL); 
 
    menu_run(menu);
    menu_free(menu);
