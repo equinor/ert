ce355730ba58f504a8b17d418b43ce038c34eb34
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-05-04 16:37:46 +0000

    Bug in enkf_defaults.h
    git-svn-id: http://ert.nr.no/repos/ert@1926 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/enkf_defaults.h
+++ devel/libenkf/src/enkf_defaults.h
@@ -141,7 +141,7 @@
 #define DEFAULT_PARAMETER_PATH 	      "%04d/mem%03d/Parameter"
 #define DEFAULT_STATIC_PATH    	      "%04d/mem%03d/Static"
 #define DEFAULT_DYNAMIC_FORECAST_PATH "%04d/mem%03d/Forecast"
-#define DEFAULT_DYNAMIC_ANALYZED_PATH "%04d/mem%03d/Forecast"
+#define DEFAULT_DYNAMIC_ANALYZED_PATH "%04d/mem%03d/Analyzed"
 #define DEFAULT_INDEX_PATH            "%04d/mem%03d/INDEX"
 
 
