f7a6e70a3fe26094aa96f8c922d93b9d838dfd6f
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2009-11-19 11:03:18 +0000

    Moved all applications from libsched + building apps with scons
    git-svn-id: http://ert.nr.no/repos/ert@2294 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libsched/src/sched_kw_compdat.h
+++ devel/libsched/src/sched_kw_compdat.h
@@ -1,14 +1,11 @@
 #ifndef __SCHED_KW_COMPDAT_H__
 #define __SCHED_KW_COMPDAT_H__
-#include <ecl_kw.h>
 #include <stdio.h>
 #include <set.h>
 #include <sched_macros.h>
 
 typedef struct sched_kw_compdat_struct sched_kw_compdat_type;
 
-void                    sched_kw_compdat_set_conn_factor(sched_kw_compdat_type *  , const float *, const float * , const int *  , const int * );
-void                    sched_kw_compdat_init_conn_factor(sched_kw_compdat_type * , const ecl_kw_type *, const ecl_kw_type * , const int * , const int * , bool *);
 sched_kw_compdat_type * sched_kw_compdat_alloc();
 void                    sched_kw_compdat_free(sched_kw_compdat_type * );
 void                    sched_kw_compdat_fprintf(const sched_kw_compdat_type * , FILE *);
