047a99bc490ced755f3e8a19ec081a5eb0515ea8
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-02-22 13:12:04 +0000

    Updated conf_test.c
    git-svn-id: http://ert.nr.no/repos/ert@2447 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libconfig/applications/conf_test.c
+++ devel/libconfig/applications/conf_test.c
@@ -6,25 +6,6 @@ int main()
   conf_class_type * enkf_conf_class = conf_class_alloc_empty("ENKF_conf", true, false, enkf_conf_help);
   conf_class_set_help(enkf_conf_class, enkf_conf_help);
   
-  /** Create and insert INTVEC item. */
-  {
-    conf_item_spec_type * intvec_spec = conf_item_spec_alloc("INTVEC", false, false, DT_INT, NULL);
-    conf_class_insert_owned_item_spec(enkf_conf_class, intvec_spec);
-  }
-
-  /** Create and insert DOUBLEVEC item. */
-  {
-    conf_item_spec_type * dobulevec_spec = conf_item_spec_alloc("DOUBLEVEC", false, false, DT_FLOAT, NULL);
-    conf_class_insert_owned_item_spec(enkf_conf_class, dobulevec_spec);
-  }
-
-  /** Create and insert SINGLETON class. */
-  {
-    conf_class_type * singleton_class = conf_class_alloc_empty("SINGLETON", false, true, NULL);
-    conf_class_insert_owned_sub_class(enkf_conf_class, singleton_class);
-  }
-
-
   /** Create and insert HISTORY_OBSERVATION class. */
   {
     const char * help_class_history_observation = "The class HISTORY_OBSERVATION is used to condition on a time series from the production history. The name of the an instance is used to define the item to condition on, and should be in summary.x syntax. E.g., creating a HISTORY_OBSERVATION instance with name GOPR:P4 conditions on GOPR for group P4.";
@@ -33,7 +14,7 @@ int main()
 
     
     const char * help_item_spec_error_mode = "The string ERROR_MODE gives the error mode for the observation.";
-    conf_item_spec_type * item_spec_error_mode = conf_item_spec_alloc("ERROR_MODE", true, true, DT_STR, help_item_spec_error_mode);
+    conf_item_spec_type * item_spec_error_mode = conf_item_spec_alloc("ERROR_MODE", true, DT_STR, help_item_spec_error_mode);
     conf_item_spec_set_help(item_spec_error_mode, help_item_spec_error_mode);
     
     conf_item_spec_add_restriction(item_spec_error_mode, "rel");
@@ -44,12 +25,12 @@ int main()
     
 
     const char * help_item_spec_error = "The positive floating number ERROR gives the standard deviation (abs) or the relative uncertainty (rel/relmin) of the observations.";
-    conf_item_spec_type * item_spec_error     = conf_item_spec_alloc("ERROR", true, true, DT_POSFLOAT, help_item_spec_error);
+    conf_item_spec_type * item_spec_error     = conf_item_spec_alloc("ERROR", true, DT_POSFLOAT, help_item_spec_error);
     conf_item_spec_set_default_value(item_spec_error, "0.10");
     conf_item_spec_set_help(item_spec_error, help_item_spec_error);
 
     const char * help_item_spec_error_min = "The positive floating point number ERROR_MIN gives the minimum value for the standard deviation of the observation when relmin is used.";
-    conf_item_spec_type * item_spec_error_min = conf_item_spec_alloc("ERROR_MIN", true, true, DT_POSFLOAT, help_item_spec_error_min);
+    conf_item_spec_type * item_spec_error_min = conf_item_spec_alloc("ERROR_MIN", true, DT_POSFLOAT, help_item_spec_error_min);
     conf_item_spec_set_default_value(item_spec_error_min, "0.10");
     conf_item_spec_set_help(item_spec_error_min, help_item_spec_error_min);
     
@@ -70,28 +51,28 @@ int main()
     conf_class_set_help(summary_observation_class, help_class_summary_observation);
 
     const char * help_item_spec_value = "The floating point number VALUE gives the observed value.";
-    conf_item_spec_type * item_spec_value = conf_item_spec_alloc("VALUE", true, true, DT_FLOAT, help_item_spec_value);
+    conf_item_spec_type * item_spec_value = conf_item_spec_alloc("VALUE", true, DT_FLOAT, help_item_spec_value);
     conf_item_spec_set_help(item_spec_value, help_item_spec_value);
   
 
     const char * help_item_spec_error = "The positive floating point number ERROR is the standard deviation of the observed value.";
-    conf_item_spec_type * item_spec_error = conf_item_spec_alloc("ERROR", true, true, DT_POSFLOAT, help_item_spec_error);
+    conf_item_spec_type * item_spec_error = conf_item_spec_alloc("ERROR", true, DT_POSFLOAT, help_item_spec_error);
     conf_item_spec_set_help(item_spec_error, help_item_spec_error);
 
     const char * help_item_spec_date = "The DATE item gives the date of the observation. Format is dd/mm/yyyy.";
-    conf_item_spec_type * item_spec_date = conf_item_spec_alloc("DATE", false, true, DT_DATE, help_item_spec_date);
+    conf_item_spec_type * item_spec_date = conf_item_spec_alloc("DATE", false, DT_DATE, help_item_spec_date);
     conf_item_spec_set_help(item_spec_date, help_item_spec_date);
 
     const char * help_item_spec_days = "The DAYS item gives the observation time as days after simulation start.";
-    conf_item_spec_type * item_spec_days = conf_item_spec_alloc("DAYS", false, true, DT_POSFLOAT, help_item_spec_days);
+    conf_item_spec_type * item_spec_days = conf_item_spec_alloc("DAYS", false, DT_POSFLOAT, help_item_spec_days);
     conf_item_spec_set_help(item_spec_days, help_item_spec_days);
 
     const char * help_item_spec_restart = "The RESTART item gives the observation time as the ECLIPSE restart nr.";
-    conf_item_spec_type * item_spec_restart = conf_item_spec_alloc("RESTART", false, true, DT_POSINT, help_item_spec_restart);
+    conf_item_spec_type * item_spec_restart = conf_item_spec_alloc("RESTART", false, DT_POSINT, help_item_spec_restart);
     conf_item_spec_set_help(item_spec_restart, help_item_spec_restart);
 
     const char * help_item_spec_sumkey = "The string SUMMARY_KEY is used to look up the simulated value in the summary file. It has the same format as the summary.x program, e.g. WOPR:P4";
-    conf_item_spec_type * item_spec_sumkey = conf_item_spec_alloc("KEY", true, true, DT_STR, help_item_spec_sumkey);
+    conf_item_spec_type * item_spec_sumkey = conf_item_spec_alloc("KEY", true, DT_STR, help_item_spec_sumkey);
     conf_item_spec_set_help(item_spec_sumkey, help_item_spec_sumkey);
 
     conf_class_insert_owned_item_spec(summary_observation_class, item_spec_value);
@@ -123,11 +104,11 @@ int main()
     conf_class_set_help(block_observation_class, help_class_block_observation);
 
     const char * help_item_spec_field = "The item FIELD gives the observed field. E.g., ECLIPSE fields such as PRESSURE, SGAS or any user defined fields such as PORO or PERMX.";
-    conf_item_spec_type * item_spec_field = conf_item_spec_alloc("FIELD", true, true, DT_STR, help_item_spec_field);
+    conf_item_spec_type * item_spec_field = conf_item_spec_alloc("FIELD", true, DT_STR, help_item_spec_field);
     conf_item_spec_set_help(item_spec_field, help_item_spec_field);
 
     const char * help_item_spec_date = "The DATE item gives the date of the observation. Format is dd/mm/yyyy.";
-    conf_item_spec_type * item_spec_date = conf_item_spec_alloc("DATE", true, true, DT_DATE, help_item_spec_date);
+    conf_item_spec_type * item_spec_date = conf_item_spec_alloc("DATE", true, DT_DATE, help_item_spec_date);
     conf_item_spec_set_help(item_spec_date, help_item_spec_date);
 
     conf_class_insert_owned_item_spec(block_observation_class, item_spec_field);
@@ -136,27 +117,27 @@ int main()
     /** Create and insert the sub class OBS. */
     {
       const char * help_class_obs = "The class OBS is used to specify a single observed point.";
-      conf_class_type * obs_class = conf_class_alloc_empty("OBS", true, false, help_class_obs);
+      conf_class_type * obs_class = conf_class_alloc_empty("OBS", true, true , help_class_obs);
       conf_class_set_help(obs_class, help_class_obs);
 
       const char * help_item_i = "The item I gives the I index of the block observation.";
-      conf_item_spec_type * item_spec_i = conf_item_spec_alloc("I", true, true, DT_POSINT, help_item_i);
+      conf_item_spec_type * item_spec_i = conf_item_spec_alloc("I", true, DT_POSINT, help_item_i);
       conf_item_spec_set_help(item_spec_i, help_item_i);
 
       const char * help_item_j = "The item J gives the J index of the block observation.";
-      conf_item_spec_type * item_spec_j = conf_item_spec_alloc("J", true, true, DT_POSINT, help_item_j);
+      conf_item_spec_type * item_spec_j = conf_item_spec_alloc("J", true, DT_POSINT, help_item_j);
       conf_item_spec_set_help(item_spec_j, help_item_j);
 
       const char * help_item_k = "The item K gives the K index of the block observation.";
-      conf_item_spec_type * item_spec_k = conf_item_spec_alloc("K", true, true, DT_POSINT, help_item_k);
+      conf_item_spec_type * item_spec_k = conf_item_spec_alloc("K", true, DT_POSINT, help_item_k);
       conf_item_spec_set_help(item_spec_k, help_item_k);
 
       const char * help_item_spec_value = "The floating point number VALUE gives the observed value.";
-      conf_item_spec_type * item_spec_value = conf_item_spec_alloc("VALUE", true, true, DT_FLOAT, help_item_spec_value);
+      conf_item_spec_type * item_spec_value = conf_item_spec_alloc("VALUE", true, DT_FLOAT, help_item_spec_value);
       conf_item_spec_set_help(item_spec_value, help_item_spec_value);
     
       const char * help_item_spec_error = "The positive floating point number ERROR is the standard deviation of the observed value.";
-      conf_item_spec_type * item_spec_error = conf_item_spec_alloc("ERROR", true, true, DT_POSFLOAT, help_item_spec_error);
+      conf_item_spec_type * item_spec_error = conf_item_spec_alloc("ERROR", true, DT_POSFLOAT, help_item_spec_error);
       conf_item_spec_set_help(item_spec_error, help_item_spec_error);
 
       conf_class_insert_owned_item_spec(obs_class, item_spec_i);
@@ -204,33 +185,7 @@ int main()
     stringlist_free(history_observations);
   }
 
-  /** Try to print INTVEC. */
-  if(conf_instance_has_item(enkf_conf, "INTVEC"))
-  {
-    int_vector_type * intvec = conf_instance_get_item_value_int_vector(enkf_conf, "INTVEC");
-    for(int i=0; i<int_vector_size(intvec); i++)
-      printf("INTVEC[%d] = %d\n", i, int_vector_iget(intvec, i));
-
-    int_vector_free(intvec);
-  }
-  /** Try to print DOUBLEVEC. */
-  if(conf_instance_has_item(enkf_conf, "DOUBLEVEC"))
-  {
-    double_vector_type * doublevec = conf_instance_get_item_value_double_vector(enkf_conf, "DOUBLEVEC");
-    for(int i=0; i<double_vector_size(doublevec); i++)
-      printf("DOUBLEVEC[%d] = %f\n", i, double_vector_iget(doublevec, i));
-    
-    double_vector_free(doublevec);
-  }
-
-  /** Try to print DOUBLEVEC using stringlist. */
-  if(conf_instance_has_item(enkf_conf, "DOUBLEVEC"))
-  {
-    stringlist_type * elements = conf_instance_get_item_value_elements(enkf_conf, "DOUBLEVEC");
-    for(int i=0; i<stringlist_get_size(elements); i++)
-      printf("DOUBLEVEC[%d] as string: %s\n", i, stringlist_iget(elements, i));
-  }
-
+  
   /** Clean up. */
 
   conf_instance_free(enkf_conf);
