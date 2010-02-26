167ec746e33ddde53cd73dabd9772e8befa13da4
jaskje <jaskje@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-02-26 12:31:53 +0000

    del all obs
    git-svn-id: http://ert.nr.no/repos/ert@2456 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/local_config.c
+++ devel/libenkf/src/local_config.c
@@ -76,7 +76,7 @@ alloc_ministep_copy.
 
 DEL_ALL_OBS [NAME_OF_MINISTEP]
 -----------------------------------
-This function will delete all the obs keys from the ministep
+this function will delete all the obs keys from the ministep
 'NAME_OF_MINISTEP'; typically used after a call to
 alloc_ministep_copy.
 
@@ -346,6 +346,8 @@ static local_config_instruction_type local_config_cmd_from_string( char * cmd_st
     cmd = DEL_OBS;
   else if (strcmp( cmd_string ,DEL_ALL_DATA_STRING ) == 0)
     cmd = DEL_ALL_DATA;
+  else if (strcmp( cmd_string ,DEL_ALL_OBS_STRING ) == 0)
+    cmd = DEL_ALL_OBS;
   else {
     util_abort("%s: Command:%s not recognized \n",__func__ , cmd_string);
     cmd = INVALID_CMD;
@@ -401,6 +403,9 @@ const char * local_config_get_cmd_string( local_config_instruction_type cmd ) {
     break;
   case(DEL_ALL_DATA):
     return DEL_ALL_DATA_STRING;
+    break;
+  case(DEL_ALL_OBS):
+    return DEL_ALL_OBS_STRING;
     break;
   default:
     util_abort("%s: command:%d not recognized \n",__func__ , cmd);
