1b1464a0c079ad2652743e1b342570d04b21e5fd
jpb <jpb@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-04-06 13:19:45 +0000

    ERT GUI Updated
    git-svn-id: http://ert.nr.no/repos/ert@2558 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/libenkf/applications/ert_gui/code/widgets/application.py
@@ -0,0 +1,66 @@
+from PyQt4 import QtGui, QtCore
+import os
+
+
+class Application(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+
+        self.resize(750, 350)
+        self.setWindowTitle('ERT GUI')
+
+        widgetLayout = QtGui.QVBoxLayout()
+
+        self.contentsWidget = QtGui.QListWidget()
+        self.contentsWidget.setViewMode(QtGui.QListView.IconMode)
+        self.contentsWidget.setIconSize(QtCore.QSize(96, 96))
+        self.contentsWidget.setMovement(QtGui.QListView.Static)
+        self.contentsWidget.setMaximumWidth(128)
+        self.contentsWidget.setSpacing(12)
+
+        self.pagesWidget = QtGui.QStackedWidget()
+
+
+        horizontalLayout = QtGui.QHBoxLayout()
+        horizontalLayout.addWidget(self.contentsWidget)
+        horizontalLayout.addWidget(self.pagesWidget, 1)
+        widgetLayout.addLayout(horizontalLayout)
+
+
+        quitButton = QtGui.QPushButton("Close", self)
+        self.connect(quitButton, QtCore.SIGNAL('clicked()'), QtGui.qApp, QtCore.SLOT('quit()'))
+
+        buttonWidget = QtGui.QWidget(self)
+        buttonLayout = QtGui.QHBoxLayout()
+        buttonLayout.addStretch(1)
+        buttonLayout.addWidget(quitButton)
+
+        buttonWidget.setLayout(buttonLayout)
+        widgetLayout.addWidget(buttonWidget)
+
+        self.setLayout(widgetLayout)
+        self.show()
+
+
+
+    def addPage(self, name, icon, page):
+        button = QtGui.QListWidgetItem(self.contentsWidget)
+        button.setIcon(icon)
+        button.setText(name)
+        button.setTextAlignment(QtCore.Qt.AlignHCenter)
+        button.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+        
+        self.pagesWidget.addWidget(page)
+        self.connect(self.contentsWidget, QtCore.SIGNAL('currentItemChanged(QListWidgetItem *, QListWidgetItem *)'), self.changePage)
+
+        self.contentsWidget.setCurrentRow(0)
+        
+
+    def changePage(self, current, previous):
+        if not current:
+            current = previous
+
+        self.pagesWidget.setCurrentIndex(self.contentsWidget.row(current))
+
+    def resourceIcon(self, name):
+        return QtGui.QIcon(os.path.dirname(__file__) + "/../../img/" + name)
