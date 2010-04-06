4272af224c6d7491a08eea590da961e9ab343b33
jpb <jpb@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-04-06 07:16:12 +0000

    ERT GUI Updated
    + Documentation
    
    git-svn-id: http://ert.nr.no/repos/ert@2551 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

---
+++ devel/libenkf/applications/ert_gui/code/ertgui.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export QT_EXPERIMENTAL=1 
+export PYTHON_EXPERIMENTAL=1 
+source /prog/sdpsoft/environment.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/res/x86_64_RH_4/lib
+
+python main.py
