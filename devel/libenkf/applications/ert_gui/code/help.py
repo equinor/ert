ad511866152f569318e0a8748247c9b974d4b335
jpb <jpb@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-04-16 07:37:37 +0000

    ERT GUI Updated
    + splashscreen
    
    
    git-svn-id: http://ert.nr.no/repos/ert@2616 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_gui/code/help.py
+++ devel/libenkf/applications/ert_gui/code/help.py
@@ -7,5 +7,5 @@ def resolveHelpLabel(label):
     return ""
 
 help = "Not defined."
-help_plot_path = "The plotting engine creates 'files' with plots, they are stored in a directory. You can tell what that directory should be.\nObserve that the current 'casename' will automatically be appended to the plot path."
-help_plot_width = "When the PLPLOT driver creates a plot file, it will have the width (in pixels) given by the PLOT_WIDTH keyword. The default value for PLOT_WIDTH is 1024 pixels."
+help_plot_path = "The plotting engine creates <i>files</i> with plots, they are stored in a directory. You can tell what that directory should be.\nObserve that the current 'casename' will automatically be appended to the plot path."
+help_plot_width = "When the <b>PLPLOT</b> driver creates a plot file, it will have the width (in pixels) given by the PLOT_WIDTH keyword. The default value for PLOT_WIDTH is 1024 pixels."
