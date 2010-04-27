33410c99baf853793fa04cc559d72da6d9a189c5
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-04-27 06:58:44 +0000

    Added set/get functions to ext_job
    git-svn-id: http://ert.nr.no/repos/ert@2677 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libjob_queue/src/ext_job.h
+++ devel/libjob_queue/src/ext_job.h
@@ -20,12 +20,7 @@ void 	                ext_job_add_environment(ext_job_type *, const char * , con
 void                    ext_job_save( const ext_job_type * ext_job );
 void                    ext_job_fprintf(const ext_job_type * , FILE * stream );
 void                    ext_job_set_private_arg(ext_job_type * , const char *  , const char * );
-void 	                ext_job_set_stdout_file(ext_job_type * , const char * );
-void                    ext_job_set_config_file(ext_job_type * ext_job, const char * config_file);
-void 	                ext_job_set_stdin_file(ext_job_type * , const char * );
-void 	                ext_job_set_target_file(ext_job_type * , const char * );
-void 	                ext_job_set_start_file(ext_job_type * , const char * );
-void 	                ext_job_set_stderr_file(ext_job_type * , const char * );
+
 void 	                ext_job_add_platform_exe(ext_job_type *, const char * , const char * ) ;
 void 	                ext_job_set_argc(ext_job_type *   , const char ** , int);
 void 	                ext_job_python_fprintf(const ext_job_type * , FILE * , const subst_list_type *);
@@ -33,6 +28,36 @@ ext_job_type          * ext_job_fscanf_alloc(const char * , const char * , const
 const stringlist_type * ext_job_get_arglist( const ext_job_type * ext_job );
 bool                    ext_job_is_shared( const ext_job_type * ext_job );
 bool                    ext_job_is_private( const ext_job_type * ext_job );
+
+
+void                    ext_job_set_config_file(ext_job_type * ext_job, const char * config_file);
+const char *            ext_job_get_config_file(const ext_job_type * ext_job);
+void                    ext_job_set_target_file(ext_job_type * ext_job, const char * target_file);
+const char *            ext_job_get_target_file(const ext_job_type * ext_job);
+void                    ext_job_set_start_file(ext_job_type * ext_job, const char * start_file);
+const char *            ext_job_get_start_file(const ext_job_type * ext_job);
+void                    ext_job_set_name(ext_job_type * ext_job, const char * name);
+const char *            ext_job_get_name(const ext_job_type * ext_job);
+void                    ext_job_set_lsf_request(ext_job_type * ext_job, const char * lsf_request);
+const char *            ext_job_get_lsf_request(const ext_job_type * ext_job);
+void                    ext_job_set_stdin_file(ext_job_type * ext_job, const char * stdin_file);
+const char *            ext_job_get_stdin_file(const ext_job_type * ext_job);
+void                    ext_job_set_stdout_file(ext_job_type * ext_job, const char * stdout_file);
+const char *            ext_job_get_stdout_file(const ext_job_type * ext_job);
+void                    ext_job_set_stderr_file(ext_job_type * ext_job, const char * stderr_file);
+const char *            ext_job_get_stderr_file(const ext_job_type * ext_job);
+void                    ext_job_set_max_running( ext_job_type * ext_job , int max_running);
+int                     ext_job_get_max_running( const ext_job_type * ext_job );
+void                    ext_job_set_max_running_minutes( ext_job_type * ext_job , int max_running_minutes);
+int                     ext_job_get_max_running_minutes( const ext_job_type * ext_job );
+void                    ext_job_add_platform_exe(ext_job_type *ext_job , const char * platform , const char * exe);
+void                    ext_job_add_environment(ext_job_type *ext_job , const char * key , const char * value);
+void                    ext_job_clear_platform_exe( ext_job_type * ext_job );
+void                    ext_job_clear_environment( ext_job_type * ext_job );
+hash_type             * ext_job_get_environment( ext_job_type * ext_job );
+hash_type             * ext_job_get_platform_exe( ext_job_type * ext_job );
+
+
 
 #ifdef __cplusplus
 }
