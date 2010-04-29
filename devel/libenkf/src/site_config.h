03907b45b14bde101facfc6a50f0cf87033cd15e
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-04-29 08:51:23 +0000

    Site_config.h ??
    git-svn-id: http://ert.nr.no/repos/ert@2692 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/site_config.h
+++ devel/libenkf/src/site_config.h
@@ -12,7 +12,8 @@ extern "C" {
 
 typedef struct site_config_struct site_config_type;
 
-const char            * site_config_get_license_root_path__( const site_config_type * site_config );
+const char            *  site_config_get_license_root_path__( const site_config_type * site_config );
+void                     site_config_set_license_root_path( site_config_type * site_config , const char * license_root_path);
 
 bool                     site_config_get_statoil_mode(const site_config_type * site_config );
 void                     site_config_update_lsf_request(site_config_type *  , const forward_model_type *);
