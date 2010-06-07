95ddaa58dff65b8a39efadce0401abdcc3f41984
jpb <jpb@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-06-07 14:18:38 +0000

    ERT GUI Updated: * setting the config file on th ecommand line * environment variable for ERT_HOME
    git-svn-id: http://ert.nr.no/repos/ert@2812 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_gui/bin/ertgui.sh
+++ devel/libenkf/applications/ert_gui/bin/ertgui.sh
@@ -5,6 +5,8 @@ export PYTHON_EXPERIMENTAL=1
 source /prog/sdpsoft/environment.sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/res/x86_64_RH_4/lib
 
+export ERT_HOME=/private/jpb/EnKF/
+
 cd ../code
 
 python ../bin/clean.py
@@ -16,7 +18,7 @@ elif [ "$1" = "strace" ]
 then
     strace -e trace=file python main.py
 else
-    python main.py
+    python main.py "$1"
 fi
 
 #strace python main.py
