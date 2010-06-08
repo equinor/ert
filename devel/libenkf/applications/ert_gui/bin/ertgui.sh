cbd6a5d96e7b3d40295c24c0450d53740924c5c0
jpb <jpb@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-06-08 09:41:13 +0000

    ERT GUI Updated: * current working directory independent
    git-svn-id: http://ert.nr.no/repos/ert@2829 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_gui/bin/ertgui.sh
+++ devel/libenkf/applications/ert_gui/bin/ertgui.sh
@@ -5,26 +5,25 @@ export PYTHON_EXPERIMENTAL=1
 source /prog/sdpsoft/environment.sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/res/x86_64_RH_4/lib
 
-export ERT_HOME=/private/jpb/EnKF/
+#export ERT_HOME=/private/jpb/EnKF/
+export ERT_HOME=/d/proj/bg/enkf/jaskje/ERT_GUI/lib/
 
 ORIGINAL_DIRECTORY=$PWD
 
-script_name=$(dirname $0)
+export script_dir=$(dirname $0)
+export ert_gui_dir=$script_dir/../code
 
-cd $script_name
-cd ../code
-
-python ../bin/clean.py
+python $script_dir/clean.py
 
 if [ "$1" = "debug" ]
 then
     export CONFIG_FILE="$2"
-    gdb python --command=../bin/gdbcommands
+    gdb python --command=$script_dir/gdbcommands
 elif [ "$1" = "strace" ]
 then
-    strace -e trace=file python main.py "$2"
+    strace -e trace=file python $ert_gui_dir/main.py "$2"
 else
-    python main.py "$1"
+    python $ert_gui_dir/main.py "$1"
 fi
 
 cd "$ORIGINAL_DIRECTORY"
