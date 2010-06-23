60d174a01a52f28e8c2cdf5f2a97b9f7fc4b3258
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-06-23 12:28:14 +0000

    Changes to install.sh script
    git-svn-id: http://ert.nr.no/repos/ert@2859 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_gui/bin/install.sh
+++ devel/libenkf/applications/ert_gui/bin/install.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
+
 destination=$1
 script_dir=$(dirname $0)
 
 if [ -z "$destination" ]
 then
-    echo
-    echo "A destination directory is required!"
-    echo "  install /destination/directory/of/your/choice"
-    echo
-    exit
+    destination=/d/proj/bg/enkf/ERT_GUI
+    echo "Will install to default installation directory:" $destination
 fi
 
 ert_gui_dir=$script_dir/..
@@ -20,10 +18,10 @@ rm -rf $destination/bin
 rm -rf $destination/img
 rm -rf $destination/help
 
-cp -rf $ert_gui_dir/code $destination/code
-cp -rf $ert_gui_dir/bin $destination/bin
-cp -rf $ert_gui_dir/img $destination/img
-cp -rf $ert_gui_dir/help $destination/help
+cp -rf $ert_gui_dir/code $destination/
+cp -rf $ert_gui_dir/bin $destination/
+cp -rf $ert_gui_dir/img $destination/
+cp -rf $ert_gui_dir/help $destination/
 
 find $destination -name ".svn" -type d -exec rm -rf {} +
 find $destination -name "*.pyc" -exec rm -rf {} +
@@ -34,18 +32,17 @@ chmod a+rwx -R $destination/img
 chmod a+rwx -R $destination/help
 
 
-#export ERT_HOME=/private/jpb/EnKF/
 if [ -n "$ERT_HOME" ]
 then
-    rm -rf $destination/lib
-    mkdir $destination/lib
-    cp -f $ERT_HOME/libenkf/slib/libenkf.so $destination/lib
-    cp -f $ERT_HOME/libconfig/slib/libconfig.so $destination/lib
-    cp -f $ERT_HOME/libecl/slib/libecl.so $destination/lib
-    cp -f $ERT_HOME/libsched/slib/libsched.so $destination/lib
-    cp -f $ERT_HOME/libutil/slib/libutil.so $destination/lib
-    cp -f $ERT_HOME/librms/slib/librms.so $destination/lib
-    cp -f $ERT_HOME/libjob_queue/slib/libjob_queue.so $destination/lib
+    # Can NOT unconditionally remove the ERT_HOME/lib directory because it
+    # will in addition contain links to other external libraries (like LSF).        
+    cp -fv $ERT_HOME/libenkf/slib/libenkf.so $destination/lib
+    cp -fv $ERT_HOME/libconfig/slib/libconfig.so $destination/lib
+    cp -fv $ERT_HOME/libecl/slib/libecl.so $destination/lib
+    cp -fv $ERT_HOME/libsched/slib/libsched.so $destination/lib
+    cp -fv $ERT_HOME/libutil/slib/libutil.so $destination/lib
+    cp -fv $ERT_HOME/librms/slib/librms.so $destination/lib
+    cp -fv $ERT_HOME/libjob_queue/slib/libjob_queue.so $destination/lib
 
     chmod a+rwx -R $destination/lib
 fi
@@ -54,5 +51,4 @@ fi
 
 echo
 echo "The GUI for ERT has been installed in: $destination."
-#echo "The ERT_HOME environment variable in ertgui.sh must be set before running."
 echo
