194a6cdba8b1eb26c9802216dc7e0d56f5b247fc
jpb <jpb@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-06-30 12:07:50 +0000

    ERT GUI Updated: * plot fixes
    git-svn-id: http://ert.nr.no/repos/ert@2897 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_gui/code/pages/plot/plotfigure.py
+++ devel/libenkf/applications/ert_gui/code/pages/plot/plotfigure.py
@@ -2,23 +2,26 @@ from matplotlib.figure import Figure
 import matplotlib.lines
 import matplotlib.text
 
-from plotter import Plotter
 import numpy
 import erttypes
 import pages.plot.plotsettings
-from pages.plot.plotrenderer import PlotRenderer
+from pages.plot.plotrenderer import DefaultPlotRenderer
 
 class PlotFigure:
+    """A simple wrapper for a matplotlib Figure. Associated with a specified renderer."""
     def __init__(self):
         self.fig = Figure(dpi=100)
         self.axes = self.fig.add_subplot(111)
+        self.fig.subplots_adjust(left=0.15)
         self.axes.set_xlim()
-        self.plot_renderer = PlotRenderer()
+        self.plot_renderer = DefaultPlotRenderer()
 
     def getFigure(self):
+        """Return the matplotlib figure."""
         return self.fig
 
     def drawPlot(self, plot_data, plot_settings):
+        """Draw a plot based on the specified data and settings."""
         self.axes.cla()
 
         self.plot_renderer.drawPlot(self.axes, plot_data, plot_settings)
