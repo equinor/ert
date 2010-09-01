b772c42d018b6ebe745ec0347321fa1831af1bae
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-09-01 10:56:17 +0000

    Reworked lsf driver - now supports either LSF_LINK or remote ssh based lsf_server
    git-svn-id: http://ert.nr.no/repos/ert@3009 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libjob_queue/src/lsf_driver.h
+++ devel/libjob_queue/src/lsf_driver.h
@@ -14,11 +14,12 @@ typedef struct lsf_driver_struct lsf_driver_type;
 typedef struct lsf_job_struct    lsf_job_type;
 
 
-void 	  * lsf_driver_alloc(const char * , const char * , int num_cpu);
+void 	  * lsf_driver_alloc(const char * , const char * , const char * remote_lsf_server , int num_cpu);
 int         lsf_driver_get_num_cpu( const void * __lsf_driver );
 void        lsf_driver_set_num_cpu( void * __lsf_driver , int num_cpu);
 void        lsf_driver_set_queue_name( lsf_driver_type * driver, const char * queue_name );
 void        lsf_driver_set_resource_request( lsf_driver_type * driver, const char * resource_request );
+void        lsf_driver_set_remote_server( lsf_driver_type * driver, const char * remote_lsf_server);
 
 UTIL_SAFE_CAST_HEADER( lsf_driver );
 
