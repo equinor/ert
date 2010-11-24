d513c948016e1b1df86e667c3c275cd9ed0892cf
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2010-11-24 05:39:34 +0000

    Added libc/python code for gravity calculations
    git-svn-id: http://ert.nr.no/repos/ert@3239 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libjob_queue/src/job_queue.c
+++ devel/libjob_queue/src/job_queue.c
@@ -1035,7 +1035,7 @@ void * job_queue_run_jobs__(void * __arg_pack) {
 
 void job_queue_insert_job(job_queue_type * queue , const char * run_path , const char * job_name , int job_index , int argc , const char ** argv) {
   if (!queue->user_exit) {/* We do not accept new jobs if a user-shutdown has been iniated. */
-
+    
     if (job_index >= 0 && job_index < queue->size) 
       job_queue_initialize_node(queue , run_path , job_name , job_index , argc , argv);
     else
