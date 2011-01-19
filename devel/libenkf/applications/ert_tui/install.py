35145d9132822844ba03b0e12ae31747d2e48888
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-01-19 16:17:14 +0000

    Some install updates + doc updates
    git-svn-id: http://ert.nr.no/repos/ert@3327 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_tui/install.py
+++ devel/libenkf/applications/ert_tui/install.py
@@ -6,17 +6,20 @@ sys.path += ["../../../python/ctypes/SDP"]
 import SDP
 
 local_ert   = "ert"
-svn_version = commands.getoutput( "svnversion" )
+svn_version = commands.getoutput( "svnversion" ) 
+
+try:
+    numeric = int( svn_version )
+except:
+    sys.exit("Will not install svn version:%s - must have a pure checkout")
+
 svn_ert     = "%s_%s" % (local_ert , svn_version)
 (SDP_ROOT , RH_version) = SDP.get_SDP_ROOT()
 target_file = "%s/bin/ert_release/%s" % (SDP_ROOT, svn_ert)
 ert_link    = "%s/bin/ert_latest_and_greatest" % SDP_ROOT
 
 SDP.install_file( local_ert , target_file )
-#print "Installed........: %s" % target_file
-
 SDP.install_link( target_file , ert_link )
-#print "Installed link...: %s -> %s" % ( ert_link , target_file )
 
 
 
