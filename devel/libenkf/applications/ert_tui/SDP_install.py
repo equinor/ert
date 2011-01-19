647939b5e92f4e19da2a8c4124714f9364f5b76a
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-01-19 14:10:58 +0000

    minor install fix
    git-svn-id: http://ert.nr.no/repos/ert@3326 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_tui/SDP_install.py
+++ devel/libenkf/applications/ert_tui/SDP_install.py
@@ -13,10 +13,10 @@ target_file = "%s/bin/ert_release/%s" % (SDP_ROOT, svn_ert)
 ert_link    = "%s/bin/ert_latest_and_greatest" % SDP_ROOT
 
 SDP.install_file( local_ert , target_file )
-print "Installed........: %s" % target_file
+#print "Installed........: %s" % target_file
 
 SDP.install_link( target_file , ert_link )
-print "Installed link...: %s -> %s" % ( ert_link , target_file )
+#print "Installed link...: %s -> %s" % ( ert_link , target_file )
 
 
 
