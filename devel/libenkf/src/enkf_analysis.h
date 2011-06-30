3b215e81be75b4b0acccd517772cce698bea36de
jonsat <jonsat@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-06-30 13:09:48 +0000

    Added new functions (cross-validation based on principal components)
    git-svn-id: http://ert.nr.no/repos/ert@3479 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/src/enkf_analysis.h
+++ devel/libenkf/src/enkf_analysis.h
@@ -30,7 +30,9 @@ matrix_type * enkf_analysis_allocX( const analysis_config_type * config , rng_ty
 matrix_type * enkf_analysis_allocX_force( const analysis_config_type * config , rng_type * rng , const meas_data_type * meas_data , obs_data_type * obs_data , const matrix_type * randrot);
 matrix_type * enkf_analysis_allocX_pre_cv( const analysis_config_type * config , rng_type * rng , meas_data_type * meas_data , obs_data_type * obs_data , const matrix_type * randrot , const matrix_type * A , const matrix_type * V0T , const matrix_type * Z , const double * eig , const matrix_type * U0 , meas_data_type * fasit , int unique_bootstrap_components);
 matrix_type * enkf_analysis_allocX_boot( const analysis_config_type * config , rng_type * rng , const meas_data_type * meas_data , obs_data_type * obs_data , const matrix_type * randrot , const meas_data_type * fasit);
+matrix_type * enkf_analysis_allocX_principal_components_cv( const analysis_config_type * config , rng_type * rng, const matrix_type * A , const matrix_type * Z , const matrix_type * Rp , const matrix_type * Dp );
 void          enkf_analysis_local_pre_cv( const analysis_config_type * config , rng_type * rng , meas_data_type * meas_data , obs_data_type * obs_data , matrix_type  * V0T , matrix_type * Z , double * eig , matrix_type * U0 , meas_data_type * fasit);
+void          enkf_analysis_get_principal_components( const analysis_config_type * config , rng_type * rng , meas_data_type * meas_data , obs_data_type * obs_data , matrix_type * Z , matrix_type * Rp, matrix_type * Dp);
 void          enkf_analysis_fprintf_obs_summary(const obs_data_type * obs_data , const meas_data_type * meas_data  , const int_vector_type * step_list , const char * ministep_name ,  FILE * stream );
 void          enkf_analysis_deactivate_outliers(obs_data_type * obs_data , meas_data_type * meas_data , double std_cutoff , double alpha);
 matrix_type * enkf_analysis_alloc_mp_randrot(int ens_size , rng_type * rng);
