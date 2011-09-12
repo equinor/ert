850d90f652ad20fae627b4e6db6265753b4e8d2f
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2011-09-12 12:53:12 +0000

    Fix in ecl_file handling of sim_time for files with LGR
    git-svn-id: http://ert.nr.no/repos/ert@3607 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libanalysis/src/sqrt_enkf.c
+++ devel/libanalysis/src/sqrt_enkf.c
@@ -1,3 +1,21 @@
+/*
+   Copyright (C) 2011  Statoil ASA, Norway. 
+    
+   The file 'sqrt_enkf.c' is part of ERT - Ensemble based Reservoir Tool. 
+    
+   ERT is free software: you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by 
+   the Free Software Foundation, either version 3 of the License, or 
+   (at your option) any later version. 
+    
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+   FITNESS FOR A PARTICULAR PURPOSE.   
+    
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+   for more details. 
+*/
+
 #include <stdlib.h>
 #include <string.h>
 #include <util.h>
@@ -8,10 +26,14 @@
 #include <enkf_linalg.h>
 #include <std_enkf.h>
 
+
+/*
+  The sqrt_enkf module performs a EnKF update based on the square root
+  scheme. Observe that this module shares quite a lot of
+  implementation with the std_enkf module.
+*/
+
 #define SQRT_ENKF_TYPE_ID 268823
-
-
-
 
 typedef struct {
   UTIL_TYPE_ID_DECLARATION;
@@ -20,6 +42,7 @@ typedef struct {
 
 
 static UTIL_SAFE_CAST_FUNCTION( sqrt_enkf_data , SQRT_ENKF_TYPE_ID )
+
 
 void * sqrt_enkf_data_alloc( ) {
   sqrt_enkf_data_type * data = util_malloc( sizeof * data , __func__ );
