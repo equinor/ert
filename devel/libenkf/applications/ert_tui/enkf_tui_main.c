67fd6f389bd4b5a1b1fa11d6b57b921a23251eb6
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2012-02-07 16:31:57 +0000

    Added QC option for plotting PC
    git-svn-id: http://ert.nr.no/repos/ert@3746 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libenkf/applications/ert_tui/enkf_tui_main.c
+++ devel/libenkf/applications/ert_tui/enkf_tui_main.c
@@ -29,6 +29,7 @@
 #include <enkf_tui_table.h>
 #include <enkf_tui_fs.h>
 #include <enkf_tui_ranking.h>
+#include <enkf_tui_QC.h>
 #include <enkf_tui_misc.h>
 #include <enkf_main.h>
 #include <enkf_sched.h>
@@ -53,6 +54,7 @@ void enkf_tui_main_menu(enkf_main_type * enkf_main) {
   menu_type * menu = menu_alloc("Main menu" , "Quit" , "qQ");
   menu_add_item(menu , "Manage cases" , "cC" , enkf_tui_fs_menu , enkf_main , NULL);
   menu_add_item(menu , "Run or restart experiment"    , "rR" , enkf_tui_run_menu    , enkf_main , NULL);
+  menu_add_item(menu , "Prior quality check"          , "uU" , enkf_tui_QC_menu     , enkf_main , NULL);
   menu_add_item(menu , "Plot results"                 , "pP" , enkf_tui_plot_menu   , enkf_main , NULL);
   menu_add_item(menu , "Rank results"                 , "aA" , enkf_tui_ranking_menu, enkf_main , NULL);
   menu_add_item(menu , "Export data to other formats" , "eE" , enkf_tui_export_menu , enkf_main , NULL);
