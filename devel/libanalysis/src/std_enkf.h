7e84d826de888e6d63839c431c31439e0426f8e7
joaho <joaho@ecb61f28-d21e-0410-a4b6-a828cfe0ca3a>
2012-08-13 13:57:02 +0000

    Format cleanup
    git-svn-id: http://ert.nr.no/repos/ert@3946 ecb61f28-d21e-0410-a4b6-a828cfe0ca3a

--- devel/libanalysis/src/std_enkf.h
+++ devel/libanalysis/src/std_enkf.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include <stdbool.h>
+
 #include <matrix.h>
 #include <rng.h>
 
@@ -13,33 +14,33 @@ extern "C" {
 #define  ENKF_TRUNCATION_KEY_      "ENKF_TRUNCATION"
 #define  ENKF_NCOMP_KEY_           "ENKF_NCOMP" 
 
-typedef struct std_enkf_data_struct std_enkf_data_type;
+  typedef struct std_enkf_data_struct std_enkf_data_type;
 
 
   void     std_enkf_initX(void * module_data , matrix_type * X , matrix_type * A , matrix_type * S , matrix_type * R , matrix_type * innov , matrix_type * E , matrix_type *D );
-bool     std_enkf_set_double( void * arg , const char * var_name , double value);
-
-bool     std_enkf_set_int( void * arg , const char * var_name , int value);
-int      std_enkf_get_subspace_dimension( std_enkf_data_type * data );
-void     std_enkf_set_truncation( std_enkf_data_type * data , double truncation );
-void     std_enkf_set_subspace_dimension( std_enkf_data_type * data , int subspace_dimension);
-
-
-double   std_enkf_get_truncation( std_enkf_data_type * data );
-void   * std_enkf_data_alloc( rng_type * rng);
-void     std_enkf_data_free( void * module_data );
-
-void     std_enkf_initX__( matrix_type * X , 
-                           matrix_type * S , 
-                           matrix_type * R , 
-                           matrix_type * E , 
-                           matrix_type * D ,
-                           double truncation,
-                           int    ncomp,
-                           bool   bootstrap );
-
-
-
+  bool     std_enkf_set_double( void * arg , const char * var_name , double value);
+  
+  bool     std_enkf_set_int( void * arg , const char * var_name , int value);
+  int      std_enkf_get_subspace_dimension( std_enkf_data_type * data );
+  void     std_enkf_set_truncation( std_enkf_data_type * data , double truncation );
+  void     std_enkf_set_subspace_dimension( std_enkf_data_type * data , int subspace_dimension);
+  
+  
+  double   std_enkf_get_truncation( std_enkf_data_type * data );
+  void   * std_enkf_data_alloc( rng_type * rng);
+  void     std_enkf_data_free( void * module_data );
+  
+  void     std_enkf_initX__( matrix_type * X , 
+                             matrix_type * S , 
+                             matrix_type * R , 
+                             matrix_type * E , 
+                             matrix_type * D ,
+                             double truncation,
+                             int    ncomp,
+                             bool   bootstrap );
+  
+  
+  
 #ifdef __cplusplus
 }
 #endif
