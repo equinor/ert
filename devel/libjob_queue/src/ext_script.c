51f97cefb668e150898eba7f3a5ac3aa8dad1d04
Joakim Hove <joakim.hove@gmail.com>
2012-11-27 07:39:00 +0100

    Removed append_arg flag to config_add_schema_item()

--- devel/libjob_queue/src/ext_script.c
+++ devel/libjob_queue/src/ext_script.c
@@ -36,7 +36,7 @@
 #define CMD_TYPE_ID           66153
 #define EXT_SCRIPT_TYPE_ID  6762081
 
-struct cmd_struct {
+typedef struct cmd_struct {
   UTIL_TYPE_ID_DECLARATION;
   const ext_cmd_type * ext_cmd;
   stringlist_type    * arglist;
@@ -59,7 +59,7 @@ struct ext_script_struct {
 
 static cmd_type * cmd_alloc( const ext_cmd_type * ext_cmd , const stringlist_type * arglist) {
   cmd_type * cmd = util_malloc( sizeof * cmd );
-  UTIL_TYTPE_ID_INIT(cmd , CMD_TYPE_ID );
+  UTIL_TYPE_ID_INIT(cmd , CMD_TYPE_ID );
   cmd->ext_cmd = ext_cmd;
   cmd->arglist = stringlist_alloc_deep_copy( arglist );
   return cmd;
@@ -80,6 +80,35 @@ static void cmd_free__( void * arg ) {
 /*****************************************************************/
 
 
-ext_script_type * ext_script_alloc( const char * src_file ) {
+static bool ext_script_try_compile( ext_script_type * script ) {
+  if (util_file_exists( script->src_file )) {
+    time_t src_mtime = util_file_mtime( script->src_file );
+    if (script->compiled && (util_difftime( src_mtime , script->compile_time , NULL , NULL , NULL, NULL) > 0 ))
+      // Script is already up to date.
+      return true;
+    else {
+      // Try to compile
+      script->compiled = false;
+      vector_clear( script->cmd_list );
+      
+      return true;
+    }
+  } else
+    return script->compiled;  // It is legal to remove the script after 
+                              // successfull compilation.
+}
+
+
+ext_script_type * ext_script_alloc( const char * src_file , config_type * config_compiler) {
+  ext_script_type * script = util_malloc( sizeof * script );
+  UTIL_TYPE_ID_INIT( script , EXT_SCRIPT_TYPE_ID );
+
+  script->src_file        = util_alloc_string_copy( src_file );
+  script->config_compiler = config_compiler;
+  script->cmd_list        = vector_alloc_new();
+  script->compiled        = false;
   
+  ext_script_try_compile( script );
+
+  return script;
 }
