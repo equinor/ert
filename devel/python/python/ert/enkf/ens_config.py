587abf0f4fcde81c85fc1732fe5098f36e15fc63
Inge Myrseth <inge.myrseth@nr.no>
2013-03-18 15:01:06 +0100

    ensembel_config functions are now up to date. unittest is set up

--- devel/python/python/ert/enkf/ens_config.py
+++ devel/python/python/ert/enkf/ens_config.py
@@ -21,6 +21,8 @@ from    ert.util.tvector      import *
 from    ert.enkf.enkf_enum             import *
 import  ert.enkf.libenkf
 from    ert.ecl.ecl_grid import EclGrid
+import  ert.enkf.enkf_config_node
+from    ert.util.stringlist import StringList
 
 class EnsConfig(CClass):
     
@@ -37,24 +39,46 @@ class EnsConfig(CClass):
     def has_key(self , key):
         return cfunc.has_key( self ,key )
 
+    def get_node(self, key):
+        node = ert.enkf.enkf_config_node.EnkfConfigNode( cfunc.get_node(self, key))
+        return node
+    
+    @property
+    def alloc_keylist(self):
+        key_list = cfunc.alloc_keylist(self)
+        return key_list
+       
+    def add_summary(self, key):
+        node = ert.enkf.enkf_config_node.EnkfConfigNode( cfunc.add_summary(self, key, 2))
+        return node
 
+    def add_gen_kw(self, key):
+        node = ert.enkf.enkf_config_node.EnkfConfigNode( cfunc.add_gen_kw(self, key))
+        return node
+
+    def add_gen_data(self, key):
+        node = ert.enkf.enkf_config_node.EnkfConfigNode( cfunc.add_gen_data(self, key))
+        return node
+    
+    def add_field(self, key, eclipse_grid):
+        node = ert.enkf.enkf_config_node.EnkfConfigNode( cfunc.add_field(self, key, eclipse_grid))
+        return node
+    
+    def alloc_keylist_from_var_type(self, var_mask):
+        return cfunc.alloc_keylist_from_var_type(self,var_mask)
 
 ##################################################################
 
 cwrapper = CWrapper( libenkf.lib )
 cwrapper.registerType( "ens_config" , EnsConfig )
 
-# 3. Installing the c-functions used to manipulate ecl_kw instances.
-#    These functions are used when implementing the EclKW class, not
-#    used outside this scope.
 cfunc = CWrapperNameSpace("ens_config")
-
 
 cfunc.free                        = cwrapper.prototype("void ensemble_config_free( ens_config )")
 cfunc.has_key                     = cwrapper.prototype("bool ensemble_config_has_key( ens_config , char* )")
 cfunc.get_node                    = cwrapper.prototype("c_void_p ensemble_config_get_node( ens_config , char*)")
 cfunc.alloc_keylist               = cwrapper.prototype("c_void_p ensemble_config_alloc_keylist( ens_config )")
-cfunc.add_summary                 = cwrapper.prototype("c_void_p ensemble_config_add_summary( ens_config, char*)")
+cfunc.add_summary                 = cwrapper.prototype("c_void_p ensemble_config_add_summary( ens_config, char*, int)")
 cfunc.add_gen_kw                  = cwrapper.prototype("c_void_p ensemble_config_add_gen_kw( ens_config, char*)")
 cfunc.add_gen_data                = cwrapper.prototype("c_void_p ensemble_config_add_gen_data( ens_config, char*)")
 cfunc.add_field                   = cwrapper.prototype("c_void_p ensemble_config_add_field( ens_config, char*, ecl_grid)")
