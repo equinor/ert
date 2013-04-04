3b4265fa057d0667a71b9186ecc78992452ed974
Inge Myrseth <inge.myrseth@nr.no>
2013-04-04 09:28:15 +0200

    More python wrapped functions

--- devel/python/python/ert_gui/gert_main.py
+++ devel/python/python/ert_gui/gert_main.py
@@ -179,7 +179,7 @@ if not os.path.exists(enkf_config):
         dbase_type       = new_configuration_dialog.getDBaseType()
         num_realizations = new_configuration_dialog.getNumberOfRealizations()
         storage_path     = new_configuration_dialog.getStoragePath()
-        ert.enkf.enkf_main_create_new_config(enkf_config, storage_path , firste_case_name, dbase_type, num_realizations)
+        ert.main.create_new_config( storage_path , firste_case_name, dbase_type, num_realizations)
         strict = False
 
 ert.bootstrap(enkf_config, site_config = site_config, strict = strict)
