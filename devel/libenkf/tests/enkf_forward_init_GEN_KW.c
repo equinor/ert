5ce44cbd8cf8824cce0f4e2d834c7f0080b9bf20
Joakim Hove <joakim.hove@gmail.com>
2013-04-12 11:44:12 +0200

    Test rename

--- devel/libenkf/tests/enkf_forward_init_GEN_KW.c
+++ devel/libenkf/tests/enkf_forward_init_GEN_KW.c
@@ -1,7 +1,7 @@
 /*
    Copyright (C) 2013  Statoil ASA, Norway. 
     
-   The file 'enkf_forward_init.c' is part of ERT - Ensemble based Reservoir Tool. 
+   The file 'enkf_forward_init_GEN_KW.c' is part of ERT - Ensemble based Reservoir Tool. 
     
    ERT is free software: you can redistribute it and/or modify 
    it under the terms of the GNU General Public License as published by 
