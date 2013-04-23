80bc775306bb2f25469611c1dbff00619529bb36
Kristian Flikka <kristian.flikka@gmail.com>
2013-04-23 09:43:25 +0200

    Uncommented test_exit statements, forgot in previous commit

--- devel/libjob_queue/tests/job_torque_submit_test.c
+++ devel/libjob_queue/tests/job_torque_submit_test.c
@@ -31,8 +31,7 @@ void test_submit(torque_driver_type * driver, const char * cmd) {
   if (job != NULL) {
     int torque_status = torque_driver_get_job_status(driver, job);
     if (!((torque_status == JOB_QUEUE_RUNNING) || (torque_status == JOB_QUEUE_PENDING))) {
-      exit(1);
-      //test_exit("After start of job, the status is %d, it should have been JOB_QUEUE_RUNNING(%d) or JOB_QUEUE_PENDING (%d). Other statuses are JOB_QUEUE_EXIT(%d) and JOB_QUEUE_FAILED(%d)\n", torque_status, JOB_QUEUE_RUNNING, JOB_QUEUE_PENDING, JOB_QUEUE_EXIT, JOB_QUEUE_FAILED);
+      test_exit("After start of job, the status is %d, it should have been JOB_QUEUE_RUNNING(%d) or JOB_QUEUE_PENDING (%d). Other statuses are JOB_QUEUE_EXIT(%d) and JOB_QUEUE_FAILED(%d)\n", torque_status, JOB_QUEUE_RUNNING, JOB_QUEUE_PENDING, JOB_QUEUE_EXIT, JOB_QUEUE_FAILED);
     }
 
     torque_driver_kill_job(driver, job);
@@ -47,11 +46,11 @@ void test_submit(torque_driver_type * driver, const char * cmd) {
     torque_status = torque_driver_get_job_status(driver, job);
     if (torque_status != JOB_QUEUE_EXIT && torque_status != JOB_QUEUE_DONE) {
       exit(1);
-      //test_exit("After kill of job, the status is %d, it should have been JOB_QUEUE_EXIT, which is %d\n", torque_status, JOB_QUEUE_EXIT);
+      test_exit("After kill of job, the status is %d, it should have been JOB_QUEUE_EXIT, which is %d\n", torque_status, JOB_QUEUE_EXIT);
     }
   } else {
     exit(1);
-    //test_exit("Function %s returned null-pointer to job, terminating test.", "torque_driver_submit_job");
+    test_exit("Function %s returned null-pointer to job, terminating test.", "torque_driver_submit_job");
   }
 
   free(run_path);
