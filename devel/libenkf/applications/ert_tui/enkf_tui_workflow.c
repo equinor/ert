bc71858770764c2b3bb6f0a521bf759f372b55ba
Joakim Hove <joakim.hove@gmail.com>
2013-04-23 10:03:35 +0200

    Removed many unused variables & functions to reduce the number of warnings

--- devel/libenkf/applications/ert_tui/enkf_tui_workflow.c
+++ devel/libenkf/applications/ert_tui/enkf_tui_workflow.c
@@ -68,10 +68,7 @@ void enkf_tui_workflow_run( void * arg ) {
 
 
 void enkf_tui_workflow_load( void * arg ) {
-  enkf_main_type * enkf_main = enkf_main_safe_cast( arg );
-  {
-    
-  }
+  // ...
 }
 
 
