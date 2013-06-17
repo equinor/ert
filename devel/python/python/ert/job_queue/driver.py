8b24db8944963f8bdb7cfa95d9c84e0f3414aca2
Joakim Hove <joakim.hove@gmail.com>
2013-06-17 19:55:04 +0200

    Added name propertry to the driver

--- devel/python/python/ert/job_queue/driver.py
+++ devel/python/python/ert/job_queue/driver.py
@@ -85,6 +85,10 @@ class Driver(CClass):
         
     max_running = property( get_max_running , set_max_running )
 
+    @property
+    def name(self):
+        return cfunc.get_name( self )
+
 
 class LSFDriver(Driver):
     
@@ -149,3 +153,4 @@ cfunc.kill_job        = cwrapper.prototype("void        queue_driver_kill_job( d
 cfunc.set_max_running = cwrapper.prototype("void        queue_driver_set_max_running( driver , int )")
 cfunc.get_max_running = cwrapper.prototype("int         queue_driver_get_max_running( driver )")
 cfunc.set_str_option  = cwrapper.prototype("bool        queue_driver_set_option( driver , char* , char*) ")
+cfunc.get_name        = cwrapper.prototype("char*       queue_driver_get_name(driver)")  
