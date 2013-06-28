d5a82f732d9f6480e8c85a5871193b9940e71a49
Kristian Flikka <kristian.flikka@gmail.com>
2013-06-28 08:40:49 +0200

    Reintroduced enkf_main_invalidate_cache as a commented out function, so that if it is actually used, and someone gets into trouble, it is easier for them to find it.

--- devel/libenkf/src/enkf_main.c
+++ devel/libenkf/src/enkf_main.c
@@ -2261,6 +2261,22 @@ void enkf_main_update_node( enkf_main_type * enkf_main , const char * key ) {
 }
 
 
+// NOTE KF 20130628: This is commented out, because I don't think it is used, but we'll give it some time
+//
+///**
+//   When the case has changed it is essential to invalidate the meta
+//   information in the enkf_nodes, otherwise the nodes might reuse old
+//   data (from a previous case).
+//*/
+//
+//static void enkf_main_invalidate_cache( enkf_main_type * enkf_main ) {
+//  int ens_size = enkf_main_get_ensemble_size( enkf_main );
+//  int iens;
+//  for (iens = 0; iens < ens_size; iens++)
+//    enkf_state_invalidate_cache( enkf_main->ensemble[iens] );
+//}
+
+
 void enkf_main_create_fs( enkf_main_type * enkf_main , const char * fs_path) {
   fs_driver_impl driver_id = model_config_get_dbase_type( enkf_main->model_config );
   void * arg = NULL;
