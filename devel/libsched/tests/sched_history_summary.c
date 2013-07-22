6f36050999873af3906ba022cdd061a4d652d51f
Joakim Hove <joakim.hove@gmail.com>
2013-07-22 14:29:42 +0200

    Completed history testing

--- devel/libsched/tests/sched_history_summary.c
+++ devel/libsched/tests/sched_history_summary.c
@@ -29,11 +29,44 @@
 int main(int argc, char **argv) {
   char * sum_case = argv[1];
   ecl_sum_type * refcase = ecl_sum_fread_alloc_case( sum_case , ":" );
-  history_type * hist = history_alloc_from_refcase( refcase , true );
+  history_type * hist_h = history_alloc_from_refcase( refcase , true );
+  history_type * hist_sim = history_alloc_from_refcase( refcase , false );
   
-  test_assert_true( history_is_instance( hist ) );
+  test_assert_true( history_is_instance( hist_h ) );
+  test_assert_true( history_is_instance( hist_sim ) );
+  test_assert_int_equal( history_get_last_restart( hist_sim ) , ecl_sum_get_last_report_step( refcase ) );
+  test_assert_int_equal( history_get_last_restart( hist_h ) , ecl_sum_get_last_report_step( refcase ) );
 
-  history_free( hist );
+  {
+    double_vector_type * value_sim = double_vector_alloc(0 , 0);
+    double_vector_type * value_h   = double_vector_alloc(0 , 0);
+    bool_vector_type * valid_sim = bool_vector_alloc( 0 , false );
+    bool_vector_type * valid_h = bool_vector_alloc( 0 , false );
+    
+    test_assert_true( history_init_ts( hist_sim , "FOPT" , value_sim , valid_sim ));
+    test_assert_true( history_init_ts( hist_h , "FOPT" , value_h , valid_h ));
+    {
+      int step;
+      for (step = 1; step < ecl_sum_get_last_report_step( refcase ); step++) {
+        test_assert_true( bool_vector_iget( valid_sim , step ));
+        test_assert_true( bool_vector_iget( valid_h , step ));
+        {
+          int time_index = ecl_sum_iget_report_end( refcase , step );
+          test_assert_double_equal( ecl_sum_get_general_var( refcase , time_index , "FOPT" )  , double_vector_iget( value_sim , step ));
+          test_assert_double_equal( ecl_sum_get_general_var( refcase , time_index , "FOPTH" ) , double_vector_iget( value_h , step ));
+        }
+      }
+    }
+    bool_vector_free( valid_sim );
+    bool_vector_free( valid_h );
+    
+    double_vector_free( value_sim );
+    double_vector_free( value_h );
+  }
+
+
+  history_free( hist_h );
+  history_free( hist_sim );
   ecl_sum_free( refcase );
   exit(0);
 }
