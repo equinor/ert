a1c02aba6c774ce334fa755cdc2bfa21e2c6dcde
Jean-Paul Balabanian <jpb@statoil.com>
2013-09-25 09:50:20 +0200

    Fixed job_status_type enum.

--- devel/libjob_queue/src/queue_driver.c
+++ devel/libjob_queue/src/queue_driver.c
@@ -379,7 +379,7 @@ const char * queue_driver_type_enum_iget(int index, int * value) {
   }, value);
 }
 
-const char * queue_driver_status_emun_iget(int index, int * value) {
+const char * queue_driver_status_enum_iget(int index, int * value) {
 
   return util_enum_iget(index, JOB_STATUS_ENUM_SIZE, (const util_enum_element_type []) {
     JOB_STATUS_ENUM_DEFS
