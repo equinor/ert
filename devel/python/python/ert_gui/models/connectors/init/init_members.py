9c447214d34f5a640872fa5f7034112b9554da7c
Jean-Paul Balabanian <jpb@statoil.com>
2013-09-27 11:45:43 +0200

    Sorted case list and fixed __init__ of init_members

--- devel/python/python/ert_gui/models/connectors/init/init_members.py
+++ devel/python/python/ert_gui/models/connectors/init/init_members.py
@@ -6,9 +6,10 @@ from ert.util import StringList
 
 class InitializationMembersModel(ErtConnector, SelectableListModelMixin):
 
-    def initialize(self):
+
+    def __init__(self):
         EnsembleSizeModel().observable().attach(EnsembleSizeModel.SPINNER_VALUE_CHANGED_EVENT, self.__ensembleSizeChanged)
-        super(InitializationMembersModel, self).initialize()
+        super(InitializationMembersModel, self).__init__()
 
     def __ensembleSizeChanged(self):
         self.observable().notify(SelectableListModelMixin.LIST_CHANGED_EVENT)
