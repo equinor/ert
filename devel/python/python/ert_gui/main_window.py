a1067bfdd7de345db75552358a979f030496ab3c
Jean-Paul Balabanian <jpb@statoil.com>
2013-11-05 14:11:42 +0100

    Added an empty export tool and icon labels.

--- devel/python/python/ert_gui/main_window.py
+++ devel/python/python/ert_gui/main_window.py
@@ -1,18 +1,15 @@
 from PyQt4.QtCore import QSettings, Qt
-from PyQt4.QtGui import QMainWindow, QTabWidget, qApp, QDockWidget, QWidget, QVBoxLayout, QToolBar, QStyle
-from ert_gui.widgets import util
-from ert_gui.widgets.help_dock import HelpDock
+from PyQt4.QtGui import QMainWindow, qApp, QWidget, QVBoxLayout
 
 
 class GertMainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        self.tools ={}
+        self.tools = {}
 
         self.resize(300, 700)
         self.setWindowTitle('gERT')
-
 
         self.central_widget = QWidget()
         self.central_layout = QVBoxLayout()
@@ -20,9 +17,9 @@ class GertMainWindow(QMainWindow):
 
         self.setCentralWidget(self.central_widget)
 
-
         self.toolbar = self.addToolBar("Tools")
         self.toolbar.setObjectName("Toolbar")
+        self.toolbar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
         # configure_action = toolbar.addAction(util.resourceIcon("ide/cog_edit"), "Configure")
         #
@@ -48,6 +45,8 @@ class GertMainWindow(QMainWindow):
         tool.setParent(self)
         self.tools[tool.getName()] = tool
         action = self.toolbar.addAction(tool.getIcon(), tool.getName())
+        action.setIconText(tool.getName())
+        action.setEnabled(tool.isEnabled())
         action.triggered.connect(tool.trigger)
 
 
@@ -56,15 +55,16 @@ class GertMainWindow(QMainWindow):
         file_menu.addAction("Close", self.__quit)
 
 
-
     def __quit(self):
         self.__saveSettings()
         qApp.quit()
+
 
     def __saveSettings(self):
         settings = QSettings("Statoil", "ErtGui")
         settings.setValue("geometry", self.saveGeometry())
         settings.setValue("windowState", self.saveState())
+
 
     def closeEvent(self, event):
         #Use QT settings saving mechanism
@@ -72,10 +72,12 @@ class GertMainWindow(QMainWindow):
         self.__saveSettings()
         QMainWindow.closeEvent(self, event)
 
+
     def __fetchSettings(self):
         settings = QSettings("Statoil", "ErtGui")
         self.restoreGeometry(settings.value("geometry").toByteArray())
         self.restoreState(settings.value("windowState").toByteArray())
+
 
     def setWidget(self, widget):
         self.central_layout.addWidget(widget)
--- devel/python/python/ert_gui/tools/tool.py
+++ devel/python/python/ert_gui/tools/tool.py
@@ -1,10 +1,11 @@
 
 class Tool(object):
-    def __init__(self, name, icon=None):
+    def __init__(self, name, icon=None, enabled=True):
         super(Tool, self).__init__()
         self.__icon = icon
         self.__name = name
         self.__parent = None
+        self.__enabled = enabled
 
 
     def getIcon(self):
@@ -23,4 +24,5 @@ class Tool(object):
         return self.__parent
 
 
-
+    def isEnabled(self):
+        return self.__enabled
