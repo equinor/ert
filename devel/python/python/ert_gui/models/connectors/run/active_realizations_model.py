3676808a5a586a4dca48bc16c38dc2e7c3823d42
Jean-Paul Balabanian <jpb@statoil.com>
2013-11-12 07:55:32 +0100

    ERT-387 - Add the special fields associated with the Ensemble Smoother Updated and new Ert models

---
+++ devel/python/python/ert_gui/models/connectors/run/active_realizations_model.py
@@ -0,0 +1,48 @@
+from ert_gui.models import ErtConnector
+from ert_gui.models.connectors import EnsembleSizeModel
+from ert_gui.models.mixins import BasicModelMixin
+
+
+class ActiveRealizationsModel(ErtConnector, BasicModelMixin):
+
+
+    def __init__(self):
+        self.__active_realizations = self.getDefaultValue()
+        self.__custom = False
+        EnsembleSizeModel().observable().attach(EnsembleSizeModel.SPINNER_VALUE_CHANGED_EVENT, self.__ensembleSizeChanged)
+        super(ActiveRealizationsModel, self).__init__()
+
+
+    def __ensembleSizeChanged(self):
+        if not self.__custom:
+            self.__active_realizations = self.getDefaultValue()
+            self.observable().notify(self.VALUE_CHANGED_EVENT)
+
+    def getValue(self):
+        """ @rtype: str """
+        return self.__active_realizations
+
+    def setValue(self, active_realizations):
+        if active_realizations is None or active_realizations.strip() == "" or active_realizations == self.getDefaultValue():
+            self.__custom = False
+            self.__active_realizations = self.getDefaultValue()
+        else:
+            self.__custom = True
+            self.__active_realizations = active_realizations
+
+        self.observable().notify(self.VALUE_CHANGED_EVENT)
+
+
+    def getDefaultValue(self):
+        size = EnsembleSizeModel().getValue()
+        return "0-%d" % (size - 1)
+
+
+
+
+
+
+
+
+
+
