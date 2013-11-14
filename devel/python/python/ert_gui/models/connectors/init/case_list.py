7424adde9fbafeca874014934235a05bd8b42c3c
Jean-Paul Balabanian <jpb@statoil.com>
2013-11-14 09:06:44 +0100

    ERT-389 - Separeted run and filesystem management from EnkfMain and added init mode enum.

--- devel/python/python/ert_gui/models/connectors/init/case_list.py
+++ devel/python/python/ert_gui/models/connectors/init/case_list.py
@@ -5,12 +5,12 @@ from ert_gui.models.mixins import ListModelMixin
 class CaseList(ErtConnector, ListModelMixin):
 
     def getList(self):
-        fs = self.ert().getFileSystem()
-        case_list = self.ert().getCaseList()
+        fs = self.ert().getEnkfFsManager().getFileSystem()
+        case_list = self.ert().getEnkfFsManager().getCaseList()
         return sorted(case_list)
 
     def addItem(self, value):
-        self.ert().selectFileSystem(value)
+        self.ert().getEnkfFsManager().selectFileSystem(value)
         self.observable().notify(ListModelMixin.LIST_CHANGED_EVENT)
 
     # def removeItem(self, value):
