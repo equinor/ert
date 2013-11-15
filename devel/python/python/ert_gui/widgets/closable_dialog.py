fc0293a0bbe0ee59519bf7747e3f048dced6db1d
Jean-Paul Balabanian <jpb@statoil.com>
2013-11-15 14:11:06 +0100

    ERT-383 - Created GUI for interacting with workflows

--- devel/python/python/ert_gui/widgets/closable_dialog.py
+++ devel/python/python/ert_gui/widgets/closable_dialog.py
@@ -10,6 +10,7 @@ class ClosableDialog(QDialog):
         self.setWindowTitle(title)
         self.setModal(True)
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowCloseButtonHint)
 
 
         layout = QVBoxLayout()
@@ -17,12 +18,26 @@ class ClosableDialog(QDialog):
         layout.addWidget(widget)
 
         button_layout = QHBoxLayout()
-        close_button = QPushButton("Close")
-        close_button.clicked.connect(self.accept)
+        self.close_button = QPushButton("Close")
+        self.close_button.clicked.connect(self.accept)
         button_layout.addStretch()
-        button_layout.addWidget(close_button)
+        button_layout.addWidget(self.close_button)
 
         layout.addStretch()
         layout.addLayout(button_layout)
 
         self.setLayout(layout)
+
+
+    def disableCloseButton(self):
+        self.close_button.setEnabled(False)
+
+    def enableCloseButton(self):
+        self.close_button.setEnabled(True)
+
+    def keyPressEvent(self, q_key_event):
+        if not self.close_button.isEnabled() and q_key_event.key() == Qt.Key_Escape:
+            pass
+        else:
+            QDialog.keyPressEvent(self, q_key_event)
+
