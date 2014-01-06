720ddb37e1c68674e6b0d40a669d6170d8108e25
Jean-Paul Balabanian <jpb@statoil.com>
2014-01-06 12:41:15 +0100

    ERT-429 - Workflow current case update

--- devel/python/python/ert_gui/tools/workflows/workflows_tool.py
+++ devel/python/python/ert_gui/tools/workflows/workflows_tool.py
@@ -1,3 +1,5 @@
+from ert_gui.models.connectors.init import CaseList
+from ert_gui.models.connectors.run.workflows_model import WorkflowsModel
 from ert_gui.tools import Tool
 from ert_gui.tools.workflows import RunWorkflowWidget
 from ert_gui.widgets import util
@@ -6,10 +8,12 @@ from ert_gui.widgets.closable_dialog import ClosableDialog
 
 class WorkflowsTool(Tool):
     def __init__(self):
-        super(WorkflowsTool, self).__init__("Run Workflow", util.resourceIcon("ide/to_do_list_checked_1"))
+        enabled = len(WorkflowsModel().getList()) > 0
+        super(WorkflowsTool, self).__init__("Run Workflow", util.resourceIcon("ide/to_do_list_checked_1"), enabled)
 
     def trigger(self):
         run_workflow_widget = RunWorkflowWidget()
         dialog = ClosableDialog("Run workflow", run_workflow_widget, self.parent())
         dialog.exec_()
+        CaseList().externalModificationNotification() # workflow may have added new cases.
 
