3fc67bc659ac5e17f676e219c0e9c352b0bb13d1
Christine Floysand <chflo@statoil.com>
2014-01-10 12:30:17 +0100

    merge fra master

--- devel/python/python/ert_gui/tools/plot/__init__.py
+++ devel/python/python/ert_gui/tools/plot/__init__.py
@@ -1,10 +1,12 @@
+from .scale_tracker import ScaleTracker
+
 from .data_type_keys_list_model import DataTypeKeysListModel
 from .data_type_keys_widget import DataTypeKeysWidget
 
 from .plot_case_model import PlotCaseModel
 from .plot_case_selection_widget import CaseSelectionWidget
 from .report_step_widget import ReportStepWidget
-from .plot_scale_widget import PlotScaleWidget
+from .plot_metrics_widget import PlotMetricsWidget
 
 
 from .plot_panel import PlotPanel
--- devel/python/python/ert_gui/tools/plot/data/__init__.py
+++ devel/python/python/ert_gui/tools/plot/data/__init__.py
@@ -1,2 +1,6 @@
+from .histogram_plot_data import CaseHistogramPlotData, HistogramPlotData
+from .observation_plot_data import ObservationPlotData
+from .refcase_plot_data import RefcasePlotData
+from .ensemble_plot_data import EnsemblePlotData
 from .plot_data import PlotData
 from .plot_data_fetcher import PlotDataFetcher
