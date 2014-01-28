fc94f253ea86af58a8fea4b7d6ad5b5628d00453
Fredrik Gundersen <fgun@statoil.com>
2014-01-28 14:24:56 +0100

    ERT-493 - Add filtering on data types to plot data types

--- devel/python/python/ert_gui/tools/plot/data_type_keys_widget.py
+++ devel/python/python/ert_gui/tools/plot/data_type_keys_widget.py
@@ -1,6 +1,7 @@
 from PyQt4.QtCore import pyqtSignal, Qt
-from PyQt4.QtGui import QWidget, QVBoxLayout, QListView, QColor, QSortFilterProxyModel
-from ert_gui.tools.plot import DataTypeKeysListModel
+from PyQt4.QtGui import QWidget, QVBoxLayout, QListView, QColor, QSortFilterProxyModel, QHBoxLayout, QToolButton, \
+    QComboBox, QStandardItemModel, QStandardItem
+from ert_gui.tools.plot import DataTypeKeysListModel, DataTypeProxyModel
 from ert_gui.widgets.legend import Legend
 from ert_gui.widgets.search_box import SearchBox
 
@@ -14,26 +15,54 @@ class DataTypeKeysWidget(QWidget):
         layout = QVBoxLayout()
 
         self.model = DataTypeKeysListModel()
-        self.filter_model = QSortFilterProxyModel()
-        self.filter_model.setSourceModel(self.model)
-        self.filter_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        self.filter_model = DataTypeProxyModel(self.model)
 
         self.search_box = SearchBox()
         self.search_box.filterChanged.connect(self.setSearchString)
+
         layout.addWidget(self.search_box)
 
+        data_type_model = QStandardItemModel(0, 1)
+        item = QStandardItem("Select data types...")
 
+        data_type_model.appendRow(item)
+
+        self.__summary_item = QStandardItem("Summary")
+        self.__summary_item.setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
+        self.__summary_item.setData(Qt.Checked, Qt.CheckStateRole)
+
+        data_type_model.appendRow(self.__summary_item)
+
+        self.__block_item = QStandardItem("Block")
+        self.__block_item.setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
+        self.__block_item.setData(Qt.Checked, Qt.CheckStateRole)
+        data_type_model.appendRow(self.__block_item)
+
+        data_type_model.itemChanged.connect(self.onItemChanged)
+
+        combo = QComboBox()
+        combo.setModel(data_type_model)
+        layout.addWidget(combo)
 
         self.data_type_keys_widget = QListView()
         self.data_type_keys_widget.setModel(self.filter_model)
         self.data_type_keys_widget.selectionModel().selectionChanged.connect(self.itemSelected)
 
+        layout.addSpacing(15)
         layout.addWidget(self.data_type_keys_widget, 2)
 
         layout.addWidget(Legend("Default types", DataTypeKeysListModel.DEFAULT_DATA_TYPE))
         layout.addWidget(Legend("Observations available", DataTypeKeysListModel.HAS_OBSERVATIONS))
 
         self.setLayout(layout)
+
+    def onItemChanged(self, item):
+        assert isinstance(item, QStandardItem)
+        checked = item.checkState()==Qt.Checked
+        if item == self.__block_item:
+            self.filter_model.setShowBlockKeys(checked)
+        elif item == self.__summary_item:
+            self.filter_model.setShowSummaryKeys(checked)
 
 
     def itemSelected(self):
