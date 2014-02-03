0b61a927309ee5bf91bd243daeaf4a6d267989e4
Fredrik Gundersen <fgun@statoil.com>
2014-02-03 13:44:34 +0100

    ERT-500 - Load GEN_DATA

--- devel/libenkf/src/enkf_plot_genvector.c
+++ devel/libenkf/src/enkf_plot_genvector.c
@@ -37,15 +37,15 @@ struct enkf_plot_genvector_struct {
   UTIL_TYPE_ID_DECLARATION;
   int iens;
   double_vector_type * data;
-  const enkf_config_node_type * enkf_config_node;
+  const enkf_config_node_type * config_node;
 };
 
 UTIL_IS_INSTANCE_FUNCTION( enkf_plot_genvector , ENKF_PLOT_GENVECTOR_TYPE_ID )
 
-enkf_plot_genvector_type * enkf_plot_genvector_alloc( const enkf_config_node_type * enkf_config_node , int iens){
+enkf_plot_genvector_type * enkf_plot_genvector_alloc( const enkf_config_node_type * config_node , int iens){
     enkf_plot_genvector_type * vector = util_malloc( sizeof * vector );
     UTIL_TYPE_ID_INIT( vector , ENKF_PLOT_GENVECTOR_TYPE_ID );
-    vector->enkf_config_node = enkf_config_node;
+    vector->config_node = config_node;
     vector->data = double_vector_alloc(0,0);
     vector->iens = iens;
     return vector;
@@ -70,4 +70,40 @@ void enkf_plot_genvector_reset( enkf_plot_genvector_type * vector ){
 
 }
 
-void * enkf_plot_genvector_load__( void * arg ){}
+void enkf_plot_genvector_load( enkf_plot_genvector_type * vector , enkf_fs_type * fs , int report_step , state_enum state) {
+  enkf_plot_genvector_reset( vector );
+  {
+    enkf_node_type * work_node  = enkf_node_alloc( vector->config_node )
+    node_id_type node_id = { .report_step = report_step ,
+                             .state       = state ,
+                             .iens        = vector->iens };
+
+    enkf_node_type * data_node;
+    if (enkf_config_node_get_impl_type( config_node ) == CONTAINER)
+      data_node = enkf_node_alloc_private_container( config_node );
+    else
+      data_node = enklf_node_alloc( config_node );
+
+    if (enkf_node_try_load( data_node , fs , node_id )) {
+      const enkf_node_type * enkf_node = enkf_config_node_container_iget( vector->enkf_config_node , report_step );
+      for (int i=0; i < enkf_node_get_size( enkf_node ); i++)
+        double_vector_append(vector->data , block_obs_iget_data( block_obs , enkf_node_value_ptr( data_node ) , i , node_id));
+
+      double_vector_permute( vector->data , sort_perm );
+    }
+    enkf_node_free( data_node );
+  }
+}
+
+
+void * enkf_plot_genvector_load__( void * arg ){
+    arg_pack_type * arg_pack = arg_pack_safe_cast( arg );
+    enkf_plot_genvector_type * vector  = arg_pack_iget_ptr( arg_pack , 0);
+    enkf_fs_type * fs = arg_pack_iget_ptr( arg_pack , 1 );
+    int report_step = arg_pack_iget_int( arg_pack , 2 );
+    state_enum state = arg_pack_iget_int( arg_pack , 3 );
+
+    enkf_plot_genvector_load( vector , fs , report_step , state );
+    return NULL;
+
+}
