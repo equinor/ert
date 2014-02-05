a8dd952d5b7d56bb7600a242d1fcad34c029397f
Christine Floysand <chflo@statoil.com>
2014-02-05 08:19:56 +0100

    Change after review of ERT-423: Rearranging method order

--- devel/libenkf/src/enkf_main_manage_fs.c
+++ devel/libenkf/src/enkf_main_manage_fs.c
@@ -17,50 +17,75 @@
 */
 
 
-static void enkf_main_copy_ensemble( const enkf_main_type * enkf_main,
-                                     enkf_fs_type * source_case_fs,
-                                     int source_report_step,
-                                     state_enum source_state,
-                                     enkf_fs_type * target_case_fs,
-                                     int target_report_step,
-                                     state_enum target_state,
-                                     const bool_vector_type * iens_mask,
-                                     const char * ranking_key , /* It is OK to supply NULL - but if != NULL it must exist */
-                                     const stringlist_type * node_list) {
+bool enkf_main_case_is_current(const enkf_main_type * enkf_main , const char * case_path) {
+  char * mount_point               = enkf_main_alloc_mount_point( enkf_main , case_path );
+  const char * current_mount_point = NULL;
+  bool is_current;
 
-  const int ens_size = enkf_main_get_ensemble_size( enkf_main );
+  if (enkf_main->dbase != NULL)
+    current_mount_point = enkf_fs_get_mount_point( enkf_main->dbase );
 
+  is_current = util_string_equal( mount_point , current_mount_point );
+  free( mount_point );
+  return is_current;
+}
+
+static bool enkf_main_current_case_file_exists( const enkf_main_type * enkf_main) {
+  const char * ens_path = model_config_get_enspath( enkf_main->model_config);
+  char * current_case_file = util_alloc_filename(ens_path, CURRENT_CASE_FILE, NULL);
+  bool exists = util_file_exists(current_case_file);
+  free(current_case_file);
+  return exists;
+}
+
+char* enkf_main_read_alloc_current_case_name(const enkf_main_type * enkf_main) {
+  char * current_case = NULL;
+  const char * ens_path = model_config_get_enspath( enkf_main->model_config);
+  char * current_case_file = util_alloc_filename(ens_path, CURRENT_CASE_FILE, NULL);
+  if (enkf_main_current_case_file_exists(enkf_main)) {
+    FILE * stream = util_fopen( current_case_file  , "r");
+    current_case = util_fscanf_alloc_token(stream);
+    util_fclose(stream);
+  } else {
+    util_abort("%s: File: storage/current_case not found, aborting! \n",__func__);
+  }
+  free(current_case_file);
+  return current_case;
+}
+
+
+stringlist_type * enkf_main_alloc_caselist( const enkf_main_type * enkf_main ) {
+  stringlist_type * case_list = stringlist_alloc_new( );
   {
-    int * ranking_permutation;
-    int inode , src_iens;
-
-    if (ranking_key != NULL) {
-      ranking_table_type * ranking_table = enkf_main_get_ranking_table( enkf_main );
-      ranking_permutation = (int *) ranking_table_get_permutation( ranking_table , ranking_key );
-    } else {
-      ranking_permutation = util_calloc( ens_size , sizeof * ranking_permutation );
-      for (src_iens = 0; src_iens < ens_size; src_iens++)
-        ranking_permutation[src_iens] = src_iens;
-    }
-
-    for (inode =0; inode < stringlist_get_size( node_list ); inode++) {
-      enkf_config_node_type * config_node = ensemble_config_get_node( enkf_main_get_ensemble_config(enkf_main) , stringlist_iget( node_list , inode ));
-      for (src_iens = 0; src_iens < enkf_main_get_ensemble_size( enkf_main ); src_iens++) {
-        if (bool_vector_safe_iget(iens_mask , src_iens)) {
-          int target_iens = ranking_permutation[src_iens];
-          node_id_type src_id    = {.report_step = source_report_step , .iens = src_iens    , .state = source_state };
-          node_id_type target_id = {.report_step = target_report_step , .iens = target_iens , .state = target_state };
-
-          enkf_node_copy( config_node ,
-                          source_case_fs , target_case_fs ,
-                          src_id , target_id );
-        }
+    const char * ens_path = model_config_get_enspath( enkf_main->model_config );
+    DIR * ens_dir = opendir( ens_path );
+    if (ens_dir != NULL) {
+      int ens_fd = dirfd( ens_dir );
+      if (ens_fd != -1) {
+        struct dirent * dp;
+        do {
+          dp = readdir( ens_dir );
+          if (dp != NULL) {
+            if (!(util_string_equal( dp->d_name , ".") || util_string_equal(dp->d_name , ".."))) {
+              if (!util_string_equal( dp->d_name , CURRENT_CASE_FILE)) {
+                char * full_path = util_alloc_filename( ens_path , dp->d_name , NULL);
+                if (util_is_directory( full_path ))
+                  stringlist_append_copy( case_list , dp->d_name );
+                free( full_path);
+              }
+            }
+          }
+        } while (dp != NULL);
       }
     }
-
-    if (ranking_permutation == NULL)
-      free( ranking_permutation );
+    closedir( ens_dir );
   }
+  return case_list;
+}
+
+
+void enkf_main_set_case_table( enkf_main_type * enkf_main , const char * case_table_file ) {
+  model_config_set_case_table( enkf_main->model_config , enkf_main->ens_size , case_table_file );
 }
 
 
@@ -119,6 +144,98 @@ void enkf_main_initialize_from_scratch(enkf_main_type * enkf_main , const string
 }
 
 
+
+static void enkf_main_copy_ensemble( const enkf_main_type * enkf_main,
+                                     enkf_fs_type * source_case_fs,
+                                     int source_report_step,
+                                     state_enum source_state,
+                                     enkf_fs_type * target_case_fs,
+                                     int target_report_step,
+                                     state_enum target_state,
+                                     const bool_vector_type * iens_mask,
+                                     const char * ranking_key , /* It is OK to supply NULL - but if != NULL it must exist */
+                                     const stringlist_type * node_list) {
+
+  const int ens_size = enkf_main_get_ensemble_size( enkf_main );
+
+  {
+    int * ranking_permutation;
+    int inode , src_iens;
+
+    if (ranking_key != NULL) {
+      ranking_table_type * ranking_table = enkf_main_get_ranking_table( enkf_main );
+      ranking_permutation = (int *) ranking_table_get_permutation( ranking_table , ranking_key );
+    } else {
+      ranking_permutation = util_calloc( ens_size , sizeof * ranking_permutation );
+      for (src_iens = 0; src_iens < ens_size; src_iens++)
+        ranking_permutation[src_iens] = src_iens;
+    }
+
+    for (inode =0; inode < stringlist_get_size( node_list ); inode++) {
+      enkf_config_node_type * config_node = ensemble_config_get_node( enkf_main_get_ensemble_config(enkf_main) , stringlist_iget( node_list , inode ));
+      for (src_iens = 0; src_iens < enkf_main_get_ensemble_size( enkf_main ); src_iens++) {
+        if (bool_vector_safe_iget(iens_mask , src_iens)) {
+          int target_iens = ranking_permutation[src_iens];
+          node_id_type src_id    = {.report_step = source_report_step , .iens = src_iens    , .state = source_state };
+          node_id_type target_id = {.report_step = target_report_step , .iens = target_iens , .state = target_state };
+
+          enkf_node_copy( config_node ,
+                          source_case_fs , target_case_fs ,
+                          src_id , target_id );
+        }
+      }
+    }
+
+    if (ranking_permutation == NULL)
+      free( ranking_permutation );
+  }
+}
+
+
+
+void enkf_main_init_current_case_from_existing(const enkf_main_type * enkf_main,
+                                               enkf_fs_type * source_case_fs,
+                                               int source_report_step,
+                                               state_enum source_state) {
+
+  enkf_fs_type * current_fs = enkf_main_get_fs(enkf_main);
+
+  enkf_main_init_case_from_existing(enkf_main,
+                                    source_case_fs,
+                                    source_report_step,
+                                    source_state,
+                                    current_fs);
+}
+
+
+void enkf_main_init_case_from_existing(const enkf_main_type * enkf_main,
+                                       enkf_fs_type * source_case_fs,
+                                       int source_report_step,
+                                       state_enum source_state,
+                                       enkf_fs_type * target_case_fs ) {
+
+  stringlist_type * param_list = ensemble_config_alloc_keylist_from_var_type( enkf_main_get_ensemble_config(enkf_main) , PARAMETER ); /* Select only paramters - will fail for GEN_DATA of type DYNAMIC_STATE. */
+  int target_report_step  = 0;
+  state_enum target_state = ANALYZED;
+  bool_vector_type * iactive = bool_vector_alloc( 0 , true );
+
+  enkf_main_copy_ensemble(enkf_main,
+                          source_case_fs,
+                          source_report_step,
+                          source_state,
+                          target_case_fs,
+                          target_report_step,
+                          target_state ,
+                          iactive,
+                          NULL,
+                          param_list);
+
+  bool_vector_free(iactive);
+  stringlist_free(param_list);
+}
+
+
+
 /**
    This function will go through the filesystem and check that we have
    initial data for all parameters and all realizations. If the second
@@ -154,6 +271,24 @@ static bool enkf_main_case_is_initialized__( const enkf_main_type * enkf_main , 
   if (__mask == NULL)
     bool_vector_free( mask );
   return initialized;
+}
+
+
+
+bool enkf_main_case_is_initialized( const enkf_main_type * enkf_main , const char * case_name ,  bool_vector_type * __mask) {
+  enkf_fs_type * fs = enkf_main_mount_alt_fs(enkf_main , case_name , true , false);
+  if (fs) {
+    bool initialized = enkf_main_case_is_initialized__(enkf_main , fs , __mask);
+    enkf_fs_umount( fs );
+    return initialized;
+  } else
+    return false;
+}
+
+
+
+bool enkf_main_is_initialized( const enkf_main_type * enkf_main , bool_vector_type * __mask) {
+  return enkf_main_case_is_initialized__(enkf_main , enkf_main->dbase , __mask);
 }
 
 
@@ -221,15 +356,6 @@ static void enkf_main_update_current_case( enkf_main_type * enkf_main , const ch
 }
 
 
-static bool enkf_main_current_case_file_exists( const enkf_main_type * enkf_main) {
-  const char * ens_path = model_config_get_enspath( enkf_main->model_config);
-  char * current_case_file = util_alloc_filename(ens_path, CURRENT_CASE_FILE, NULL);
-  bool exists = util_file_exists(current_case_file);
-  free(current_case_file);
-  return exists;
-}
-
-
 
 static void enkf_main_close_fs( enkf_main_type * enkf_main ) {
   enkf_fs_umount( enkf_main->dbase );
@@ -245,131 +371,6 @@ static void enkf_main_create_fs( const enkf_main_type * enkf_main , const char *
                      model_config_get_dbase_args( enkf_main->model_config ));
 
   free( new_mount_point );
-}
-
-
-
-bool enkf_main_case_is_current(const enkf_main_type * enkf_main , const char * case_path) {
-  char * mount_point               = enkf_main_alloc_mount_point( enkf_main , case_path );
-  const char * current_mount_point = NULL;
-  bool is_current;
-
-  if (enkf_main->dbase != NULL)
-    current_mount_point = enkf_fs_get_mount_point( enkf_main->dbase );
-
-  is_current = util_string_equal( mount_point , current_mount_point );
-  free( mount_point );
-  return is_current;
-}
-
-
-char* enkf_main_read_alloc_current_case_name(const enkf_main_type * enkf_main) {
-  char * current_case = NULL;
-  const char * ens_path = model_config_get_enspath( enkf_main->model_config);
-  char * current_case_file = util_alloc_filename(ens_path, CURRENT_CASE_FILE, NULL);
-  if (enkf_main_current_case_file_exists(enkf_main)) {
-    FILE * stream = util_fopen( current_case_file  , "r");
-    current_case = util_fscanf_alloc_token(stream);
-    util_fclose(stream);
-  } else {
-    util_abort("%s: File: storage/current_case not found, aborting! \n",__func__);
-  }
-  free(current_case_file);
-  return current_case;
-}
-
-
-stringlist_type * enkf_main_alloc_caselist( const enkf_main_type * enkf_main ) {
-  stringlist_type * case_list = stringlist_alloc_new( );
-  {
-    const char * ens_path = model_config_get_enspath( enkf_main->model_config );
-    DIR * ens_dir = opendir( ens_path );
-    if (ens_dir != NULL) {
-      int ens_fd = dirfd( ens_dir );
-      if (ens_fd != -1) {
-        struct dirent * dp;
-        do {
-          dp = readdir( ens_dir );
-          if (dp != NULL) {
-            if (!(util_string_equal( dp->d_name , ".") || util_string_equal(dp->d_name , ".."))) {
-              if (!util_string_equal( dp->d_name , CURRENT_CASE_FILE)) {
-                char * full_path = util_alloc_filename( ens_path , dp->d_name , NULL);
-                if (util_is_directory( full_path ))
-                  stringlist_append_copy( case_list , dp->d_name );
-                free( full_path);
-              }
-            }
-          }
-        } while (dp != NULL);
-      }
-    }
-    closedir( ens_dir );
-  }
-  return case_list;
-}
-
-
-void enkf_main_set_case_table( enkf_main_type * enkf_main , const char * case_table_file ) {
-  model_config_set_case_table( enkf_main->model_config , enkf_main->ens_size , case_table_file );
-}
-
-
-
-void enkf_main_init_current_case_from_existing(const enkf_main_type * enkf_main,
-                                               enkf_fs_type * source_case_fs,
-                                               int source_report_step,
-                                               state_enum source_state) {
-
-  enkf_fs_type * current_fs = enkf_main_get_fs(enkf_main);
-
-  enkf_main_init_case_from_existing(enkf_main,
-                                    source_case_fs,
-                                    source_report_step,
-                                    source_state,
-                                    current_fs);
-}
-
-
-void enkf_main_init_case_from_existing(const enkf_main_type * enkf_main,
-                                       enkf_fs_type * source_case_fs,
-                                       int source_report_step,
-                                       state_enum source_state,
-                                       enkf_fs_type * target_case_fs ) {
-
-  stringlist_type * param_list = ensemble_config_alloc_keylist_from_var_type( enkf_main_get_ensemble_config(enkf_main) , PARAMETER ); /* Select only paramters - will fail for GEN_DATA of type DYNAMIC_STATE. */
-  int target_report_step  = 0;
-  state_enum target_state = ANALYZED;
-  bool_vector_type * iactive = bool_vector_alloc( 0 , true );
-
-  enkf_main_copy_ensemble(enkf_main,
-                          source_case_fs,
-                          source_report_step,
-                          source_state,
-                          target_case_fs,
-                          target_report_step,
-                          target_state ,
-                          iactive,
-                          NULL,
-                          param_list);
-
-  bool_vector_free(iactive);
-  stringlist_free(param_list);
-}
-
-
-bool enkf_main_case_is_initialized( const enkf_main_type * enkf_main , const char * case_name ,  bool_vector_type * __mask) {
-  enkf_fs_type * fs = enkf_main_mount_alt_fs(enkf_main , case_name , true , false);
-  if (fs) {
-    bool initialized = enkf_main_case_is_initialized__(enkf_main , fs , __mask);
-    enkf_fs_umount( fs );
-    return initialized;
-  } else
-    return false;
-}
-
-
-bool enkf_main_is_initialized( const enkf_main_type * enkf_main , bool_vector_type * __mask) {
-  return enkf_main_case_is_initialized__(enkf_main , enkf_main->dbase , __mask);
 }
 
 
