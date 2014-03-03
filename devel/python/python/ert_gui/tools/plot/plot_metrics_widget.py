eaef65804ea215f63794649a5bfa18beb47e8bb2
iLoop2 <fredrik.gundersen@mac.com>
2014-03-03 15:14:48 +0100

    ERT-514 Export of plot to png, refactoring plot_metric_widget and plot_window

--- devel/python/python/ert_gui/tools/plot/plot_metrics_widget.py
+++ devel/python/python/ert_gui/tools/plot/plot_metrics_widget.py
@@ -1,9 +1,6 @@
-from PyQt4.QtCore import pyqtSignal, Qt
-from PyQt4.QtGui import QWidget, QVBoxLayout, QHBoxLayout, QCheckBox, QLabel, QDoubleSpinBox
-from ert_gui.models.connectors.plot import ReportStepsModel
-from ert_gui.tools.plot import ReportStepWidget, ScaleTracker
-from ert_gui.widgets.list_spin_box import ListSpinBox
-
+from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtGui import QWidget, QVBoxLayout
+from ert_gui.tools.plot import ReportStepWidget, ScaleTracker, PlotScalesWidget
 
 class PlotMetricsWidget(QWidget):
 
@@ -20,26 +17,18 @@ class PlotMetricsWidget(QWidget):
 
     def __init__(self):
         QWidget.__init__(self)
-
-        self.__scalers = {}
-
         self.__layout = QVBoxLayout()
-        self.__time_map = ReportStepsModel().getList()
-        self.__time_index_map = {}
-        for index in range(len(self.__time_map)):
-            time = self.__time_map[index]
-            self.__time_index_map[time] = index
 
         self.__data_type_key = None
+        self.__trackers = {}
+        self.__spinners = {}
 
-        self.addScaler(PlotMetricsWidget.VALUE_MIN, self.__createDoubleSpinner())
-        self.addScaler(PlotMetricsWidget.VALUE_MAX, self.__createDoubleSpinner())
-
-        self.addScaler(PlotMetricsWidget.TIME_MIN, self.__createTimeSpinner(True))
-        self.addScaler(PlotMetricsWidget.TIME_MAX, self.__createTimeSpinner(False))
-
-        self.addScaler(PlotMetricsWidget.DEPTH_MIN, self.__createDoubleSpinner())
-        self.addScaler(PlotMetricsWidget.DEPTH_MAX, self.__createDoubleSpinner())
+        self.__layout.addWidget(self.addScaler("value_min", self.VALUE_MIN))
+        self.__layout.addWidget(self.addScaler("value_max", self.VALUE_MAX))
+        self.__layout.addWidget(self.addScaler("time_min", self.TIME_MIN, True,True))
+        self.__layout.addWidget(self.addScaler("time_max", self.TIME_MAX, True))
+        self.__layout.addWidget(self.addScaler("depth_min", self.DEPTH_MIN))
+        self.__layout.addWidget(self.addScaler("depth_max", self.DEPTH_MAX))
 
         self.__layout.addSpacing(10)
 
@@ -51,199 +40,54 @@ class PlotMetricsWidget(QWidget):
 
         self.setLayout(self.__layout)
 
-        self.__value_scale_tracker = ScaleTracker("Value")
-        self.__time_scale_tracker = ScaleTracker("Time")
-        self.__depth_scale_tracker = ScaleTracker("Depth")
+    def updateTrackers(self, values):
+        type_key = values["type_key"]
+        enabled = values["enabled"]
+        value = values["value"]
+        tracker = self.__trackers[type_key]
+        tracker.setValues(self.__data_type_key, value, enabled)
+        self.plotSettingsChanged.emit()
+
+    def addScaler(self, type_key, title, time_spinner=False,min_value=False):
+        valueSpinner = PlotScalesWidget(type_key, title, time_spinner,min_value)
+        valueSpinner.plotScaleChanged.connect(self.updateTrackers)
+        self.__spinners[type_key] = valueSpinner
+        self.__trackers[type_key] = ScaleTracker(type_key)
+        return valueSpinner
 
     def getDataKeyType(self):
         return self.__data_type_key
 
     def setDataKeyType(self, data_key_type):
         self.__data_type_key = data_key_type
-        self.blockSignals(True)
-        self.updateScales()
-        self.blockSignals(False)
-
-    def updateScales(self):
-        value_min = self.__value_scale_tracker.getMinimumScaleValue(self.__data_type_key)
-        value_max = self.__value_scale_tracker.getMaximumScaleValue(self.__data_type_key)
-        time_min = self.__time_scale_tracker.getMinimumScaleValue(self.__data_type_key)
-        time_max = self.__time_scale_tracker.getMaximumScaleValue(self.__data_type_key)
-        depth_min = self.__depth_scale_tracker.getMinimumScaleValue(self.__data_type_key)
-        depth_max = self.__depth_scale_tracker.getMaximumScaleValue(self.__data_type_key)
-        self.setTimeScales(time_min, time_max)
-        self.setValueScales(value_min, value_max)
-        self.setDepthScales(depth_min, depth_max)
-
-    def getValueScaleTracker(self):
-        return self.__value_scale_tracker
-
-    def getTimeScaleTracker(self):
-        return self.__time_scale_tracker
-
-    def getDepthScaleTracker(self):
-        return self.__depth_scale_tracker
-
-    def __createDoubleSpinner(self):
-        spinner = QDoubleSpinBox()
-        spinner.setEnabled(False)
-        spinner.setMinimumWidth(75)
-        max = 999999999999
-        spinner.setRange(-max,max)
-        spinner.editingFinished.connect(self.updateScalers)
-        return spinner
-
-    def __createTimeSpinner(self, min_value):
-        def converter(item):
-            return "%s" % (str(item.date()))
-
-        spinner = ListSpinBox(self.__time_map)
-        spinner.setEnabled(False)
-        spinner.setMinimumWidth(75)
-        if min_value:
-            spinner.setValue(0)
-
-        spinner.valueChanged[int].connect(self.updateScalers)
-        spinner.setStringConverter(converter)
-
-        return spinner
-
-    def addScaler(self, name, spinner):
-        widget = QWidget()
-
-        layout = QHBoxLayout()
-        layout.setMargin(0)
-        widget.setLayout(layout)
-
-        checkbox = QCheckBox()
-        checkbox.setChecked(False)
-        checkbox.stateChanged.connect(self.updateScalers)
-        layout.addWidget(checkbox)
-
-        label = QLabel(name)
-        label.setEnabled(False)
-        layout.addWidget(label)
-
-        layout.addStretch()
-
-        layout.addWidget(spinner)
-
-        self.__layout.addWidget(widget)
-
-        self.__scalers[name] = {"checkbox": checkbox, "label": label, "spinner": spinner}
-
-    def updateScalers(self):
-        for scaler in self.__scalers.values():
-            checked = scaler["checkbox"].isChecked()
-            scaler["label"].setEnabled(checked)
-            scaler["spinner"].setEnabled(checked)
-
-        self.updateScaleTrackers()
+        for key in self.__spinners:
+            scaler = self.__spinners[key]
+            self.blockSignals(True)
+            values ={"type_key":key, "enabled": self.getIsEnabled(key), "value":self.getValue(key)}
+            scaler.setValues(values)
+            self.blockSignals(False)
         self.plotSettingsChanged.emit()
 
-    def updateScaleTrackers(self):
-        value_min = self.getValueMin()
-        value_max = self.getValueMax()
-        time_min = self.getTimeMin()
-        time_max = self.getTimeMax()
-        depth_min = self.getDepthMin()
-        depth_max = self.getDepthMax()
-
-        self.__value_scale_tracker.setScaleValues(self.__data_type_key, value_min, value_max)
-        self.__time_scale_tracker.setScaleValues(self.__data_type_key, time_min, time_max)
-        self.__depth_scale_tracker.setScaleValues(self.__data_type_key, depth_min, depth_max)
-
-    def getTimeMin(self):
-        scaler = self.__scalers[PlotMetricsWidget.TIME_MIN]
-
-        if scaler["checkbox"].isChecked():
-            index =scaler["spinner"].value()
-            return self.__time_map[index]
+    def getValue(self, type_key):
+        if type_key in self.__trackers:
+            return self.__trackers[type_key].getScaleValue(self.__data_type_key)
         else:
             return None
 
-    def getTimeMax(self):
-        scaler = self.__scalers[PlotMetricsWidget.TIME_MAX]
-
-        if scaler["checkbox"].isChecked():
-            index = scaler["spinner"].value()
-            return self.__time_map[index]
+    def getIsEnabled(self, type_key):
+        if type_key in self.__trackers:
+            return self.__trackers[type_key].isEnabled(self.__data_type_key)
         else:
             return None
-
-    def getValueMin(self):
-        scaler = self.__scalers[PlotMetricsWidget.VALUE_MIN]
-
-        if scaler["checkbox"].isChecked():
-            return scaler["spinner"].value()
-        else:
-            return None
-
-    def getValueMax(self):
-        scaler = self.__scalers[PlotMetricsWidget.VALUE_MAX]
-
-        if scaler["checkbox"].isChecked():
-            return scaler["spinner"].value()
-        else:
-            return None
-
-    def getDepthMin(self):
-        scaler = self.__scalers[PlotMetricsWidget.DEPTH_MIN]
-
-        if scaler["checkbox"].isChecked():
-            return scaler["spinner"].value()
-        else:
-            return None
-
-    def getDepthMax(self):
-        scaler = self.__scalers[PlotMetricsWidget.DEPTH_MAX]
-
-        if scaler["checkbox"].isChecked():
-            return scaler["spinner"].value()
-        else:
-            return None
-
-    def setTimeScales(self, time_min, time_max):
-        time_max = self.__time_index_map.get(time_max, None)
-        time_min = self.__time_index_map.get(time_min, None)
-        self.__updateScale(PlotMetricsWidget.TIME_MIN, time_min)
-        self.__updateScale(PlotMetricsWidget.TIME_MAX, time_max)
-
-    def setValueScales(self, value_min, value_max):
-        self.__updateScale(PlotMetricsWidget.VALUE_MIN, value_min)
-        self.__updateScale(PlotMetricsWidget.VALUE_MAX, value_max)
-
-    def setDepthScales(self, depth_min, depth_max):
-        self.__updateScale(PlotMetricsWidget.DEPTH_MIN, depth_min)
-        self.__updateScale(PlotMetricsWidget.DEPTH_MAX, depth_max)
-
-    def __updateScale(self, name, value):
-        scaler = self.__scalers[name]
-        if value is None:
-            scaler["checkbox"].blockSignals(True)
-            scaler["checkbox"].setCheckState(Qt.Unchecked)
-            scaler["checkbox"].blockSignals(False)
-        else:
-            scaler["checkbox"].blockSignals(True)
-            scaler["checkbox"].setCheckState(Qt.Checked)
-            scaler["checkbox"].blockSignals(False)
-
-            scaler["spinner"].blockSignals(True)
-            scaler["spinner"].setValue(value)
-            scaler["spinner"].blockSignals(False)
-
-        checked = scaler["checkbox"].isChecked()
-        scaler["label"].setEnabled(checked)
-        scaler["spinner"].setEnabled(checked)
 
     def getSettings(self):
         settings = {
-            "value_min" : self.__value_scale_tracker.getMinimumScaleValue(self.__data_type_key),
-            "value_max" : self.__value_scale_tracker.getMaximumScaleValue(self.__data_type_key),
-            "time_min" : self.__time_scale_tracker.getMinimumScaleValue(self.__data_type_key),
-            "time_max" : self.__time_scale_tracker.getMaximumScaleValue(self.__data_type_key),
-            "depth_min" : self.__depth_scale_tracker.getMinimumScaleValue(self.__data_type_key),
-            "depth_max" : self.__depth_scale_tracker.getMaximumScaleValue(self.__data_type_key),
+            "value_min" : self.getValue("value_min"),
+            "value_max" : self.getValue("value_max"),
+            "time_min" : self.getValue("time_min"),
+            "time_max" : self.getValue("time_max"),
+            "depth_min" : self.getValue("depth_min"),
+            "depth_max" : self.getValue("depth_max"),
             "report_step_time" : self.__report_step_widget.getSelectedValue().ctime()
         }
         return settings
---
+++ devel/python/python/ert_gui/tools/plot/plot_scale_widget.py
@@ -0,0 +1,102 @@
+from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtGui import QWidget, QHBoxLayout, QCheckBox, QDoubleSpinBox
+from ert_gui.models.connectors.plot import ReportStepsModel
+from ert_gui.widgets.list_spin_box import ListSpinBox
+
+class PlotScalesWidget(QWidget):
+
+    plotScaleChanged = pyqtSignal(dict)
+
+    def __init__(self, type_key, title, time_spinner, min_value, minimum=-999999999999, maximum=999999999999):
+        QWidget.__init__(self)
+        self.__time_spinner = time_spinner
+        if time_spinner:
+            self.__time_map = ReportStepsModel().getList()
+            self.__time_index_map = {}
+            for index in range(len(self.__time_map)):
+                time = self.__time_map[index]
+                self.__time_index_map[time] = index
+
+        self.__type_key = type_key
+        self.__spinner = None
+
+        layout = QHBoxLayout()
+        layout.setMargin(0)
+        self.setLayout(layout)
+
+        self.__checkbox = QCheckBox(title)
+        self.__checkbox.setChecked(False)
+        layout.addWidget(self.__checkbox)
+
+        layout.addStretch()
+
+        if time_spinner:
+            self.__spinner = self.createTimeSpinner(min_value)
+        else:
+            self.__spinner = self.createDoubleSpinner(minimum, maximum)
+
+        layout.addWidget(self.__spinner)
+
+        self.__checkbox.stateChanged.connect(self.toggleCheckbox)
+        self.setLayout(layout)
+
+    def createDoubleSpinner(self, minimum, maximum):
+        spinner = QDoubleSpinBox()
+        spinner.setEnabled(False)
+        spinner.setMinimumWidth(75)
+        spinner.setRange(minimum, maximum)
+        spinner.setKeyboardTracking(False)
+        spinner.editingFinished.connect(self.spinning)
+        spinner.valueChanged.connect(self.spinning)
+        return spinner
+
+    def getSpinnerValue(self):
+        if self.__time_spinner:
+            if self.__spinner is not None:
+                index = self.__spinner.value()
+                return self.__time_map[index]
+            else:
+                return None
+        else:
+            return self.__spinner.value()
+
+
+    def signalUpdatedValues(self):
+        scale_values = {"value": self.getSpinnerValue(), "type_key":self.__type_key, "enabled":self.__checkbox.isChecked()}
+        self.plotScaleChanged.emit(scale_values)
+
+    def spinning(self):
+        self.signalUpdatedValues()
+
+    def toggleCheckbox(self):
+        checked = self.__checkbox.isChecked()
+        self.__spinner.setEnabled(checked)
+        self.signalUpdatedValues()
+
+    def setValues(self, values):
+        self.blockSignals(True)
+        type_key = values["type_key"]
+        checked = values["enabled"]
+        value = values["value"]
+        if type_key == self.__type_key:
+            self.__checkbox.setChecked(checked)
+            if value is not None:
+                if self.__time_spinner:
+                    index = self.__time_index_map[value]
+                    self.__spinner.setValue(index)
+                else:
+                    self.__spinner.setValue(value)
+        self.blockSignals(False)
+
+    def createTimeSpinner(self, min_value):
+        def converter(item):
+            return "%s" % (str(item.date()))
+
+        spinner = ListSpinBox(self.__time_map)
+        spinner.setEnabled(False)
+        spinner.setMinimumWidth(75)
+        spinner.valueChanged[int].connect(self.spinning)
+        spinner.setStringConverter(converter)
+        if min_value:
+            spinner.setValue(0)
+        return spinner
