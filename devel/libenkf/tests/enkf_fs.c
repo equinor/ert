41e5ec217b5cc69ee25f65bc45dd3c4ef51f7ce4
Joakim Hove <joakim.hove@gmail.com>
2014-03-14 11:03:00 +0100

    ERT-530: Added fork() based test on interprocess file locking.

--- devel/libenkf/tests/enkf_fs.c
+++ devel/libenkf/tests/enkf_fs.c
@@ -20,6 +20,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <ert/util/test_util.h>
 #include <ert/util/test_work_area.h>
@@ -62,7 +64,69 @@ void test_refcount() {
   test_work_area_free( work_area );
 }
 
-void test_read_only() {
+
+void createFS() {
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    enkf_fs_type * fs_false = enkf_fs_mount( "mnt" , false );
+    test_assert_false(enkf_fs_is_read_only(fs_false));
+    test_assert_true( util_file_exists("mnt/mnt.lock"));
+    {
+      int total_sleep = 0;
+      while (true) {
+        if (util_file_exists( "stop")) {
+          unlink("stop");
+          break;
+        }
+        
+        usleep(1000);
+        total_sleep += 1000;
+        if (total_sleep > 1000000 * 5) {
+          fprintf(stderr,"Test failure - never receieved \"stop\" file from parent process \n");
+          break;
+        }
+      }
+    }
+    enkf_fs_decref( fs_false );
+    exit(0);
+  } 
+}
+
+
+/*
+  This test needs to fork off a seperate process to test the cross-process file locking. 
+*/
+void test_read_only2() {
+  test_work_area_type * work_area = test_work_area_alloc("enkf_fs/read_only2");
+  enkf_fs_create_fs("mnt" , BLOCK_FS_DRIVER_ID , NULL );
+  createFS();
+
+  while (true) {
+    if (util_file_exists("mnt/mnt.lock"))
+      break;
+  }
+  
+  {
+    enkf_fs_type * fs_false = enkf_fs_mount( "mnt" , false );
+    test_assert_true(enkf_fs_is_read_only(fs_false));
+    enkf_fs_decref( fs_false );
+  }
+  {
+    FILE * stream = util_fopen("stop" , "w");
+    fclose( stream );
+  }
+  
+  while (util_file_exists( "stop")) {
+    usleep( 1000 );
+  }      
+  test_work_area_free( work_area );
+}
+
+
+
+
+void test_read_only1() {
   test_work_area_type * work_area = test_work_area_alloc("enkf_fs/read_only");
 
   enkf_fs_create_fs("mnt" , BLOCK_FS_DRIVER_ID , NULL );
@@ -90,6 +154,7 @@ void test_read_only() {
 int main(int argc, char ** argv) {
   test_mount();
   test_refcount();
-  test_read_only();
+  test_read_only1();
+  test_read_only2();
   exit(0);
 }
