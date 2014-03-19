4e881a1cbe74366760b7182331522a50c1c04e21
jepebe <GitHubMongo80>
2014-03-19 08:55:39 +0100

    ERT-356 - Change some fetcher behaviors

--- devel/python/python/ert/enkf/plot/pca_fetcher.py
+++ devel/python/python/ert/enkf/plot/pca_fetcher.py
@@ -1,13 +1,27 @@
+from ert.enkf.plot import DataFetcher, ObservationGenDataFetcher, BlockObservationDataFetcher, EnsembleDataFetcher
 from ert.enkf.plot_data import PcaPlotData
 from ert.enkf.enums import EnkfStateType, RealizationStateEnum
 from ert.enkf import LocalObsdata, LocalObsdataNode, EnkfLinalg, MeasData, ObsData
-from ert.enkf.plot import DataFetcher
 from ert.util import Matrix, BoolVector
 
 
 class PcaDataFetcher(DataFetcher):
     def __init__(self, ert):
         super(PcaDataFetcher, self).__init__(ert)
+
+    def fetchSupportedKeys(self):
+        summary_keys = EnsembleDataFetcher(self.ert()).getSupportedKeys()
+
+        keys = []
+        for key in summary_keys:
+            obs_keys = self.ert().ensembleConfig().getNode(key).getObservationKeys()
+            if len(obs_keys) > 0:
+                keys.append(key)
+
+        keys += BlockObservationDataFetcher(self.ert()).getSupportedKeys()
+        keys += ObservationGenDataFetcher(self.ert()).getSupportedKeys()
+
+        return keys
 
 
     def truncationOrNumberOfComponents(self, truncation_or_ncomp):
@@ -38,6 +52,7 @@ class PcaDataFetcher(DataFetcher):
             ensemble = self.ert().getEnsembleConstant()
             meas_data = MeasData(active_list)
             obs_data = ObsData()
+
             self.ert().getObservations().getObservationAndMeasureData(fs, local_obsdata, state, active_list, ensemble, meas_data, obs_data)
 
             active_size = len(obs_data)
@@ -53,34 +68,52 @@ class PcaDataFetcher(DataFetcher):
 
         return None
 
+    def getObsKeys(self, key):
+        ensemble_data_fetcher = EnsembleDataFetcher(self.ert())
+        if ensemble_data_fetcher.supportsKey(key):
+            return self.ert().ensembleConfig().getNode(key).getObservationKeys()
+
+        block_observation_data_fetcher = BlockObservationDataFetcher(self.ert())
+        if block_observation_data_fetcher.supportsKey(key):
+            return [key]
+
+        return None
+
+
+
     def fetchData(self, key, case=None):
         data = {"x": None,
                 "y": None,
+                "obs_y": None,
                 "min_y": None,
                 "max_y": None,
                 "min_x": None,
                 "max_x": None}
 
+
+        obs_keys = self.getObsKeys(key)
+
+
         fs = self.ert().getEnkfFsManager().getFileSystem(case, read_only=True)
-        obs_keys = self.ert().ensembleConfig().getNode(key).getObservationKeys()
 
         step_1 = 0
         step_2 = self.ert().getHistoryLength()
 
-        obsdata = LocalObsdata("PCA Observations")
+        local_obsdata = LocalObsdata("PCA Observations %s" % case)
 
         for obs_key in obs_keys:
-            if not obs_key in obsdata:
+            if not obs_key in local_obsdata:
                 obs_node = LocalObsdataNode(obs_key)
                 obs_node.addRange(step_1, step_2)
-                obsdata.addNode(obs_node)
+                local_obsdata.addNode(obs_node)
 
-        if len(obsdata) > 0:
-            pca_data = self.calculatePrincipalComponent(fs, obsdata)
+        if len(local_obsdata) > 0:
+            pca_data = self.calculatePrincipalComponent(fs, local_obsdata)
 
             data["x"] = []
             data["y"] = []
             data["obs_y"] = []
+
             data["min_x"] = 1
             data["max_x"] = len(pca_data)
 
@@ -89,10 +122,20 @@ class PcaDataFetcher(DataFetcher):
                 component_number += 1
                 data["x"].append(component_number)
 
-                y = []
-                data["y"].append(y)
-                data["obs_y"].append(pca_vector.getObservation())
-                for value in pca_vector:
+                obs_y = pca_vector.getObservation()
+
+                if data["min_y"] is None or data["min_y"] > obs_y:
+                    data["min_y"] = obs_y
+
+                if data["max_y"] is None or data["max_y"] < obs_y:
+                    data["max_y"] = obs_y
+
+                data["obs_y"].append(obs_y)
+                for index, value in enumerate(pca_vector):
+                    if len(data["y"]) == index:
+                        data["y"].append([])
+
+                    y = data["y"][index]
                     y.append(value)
 
                     if data["min_y"] is None or data["min_y"] > value:
