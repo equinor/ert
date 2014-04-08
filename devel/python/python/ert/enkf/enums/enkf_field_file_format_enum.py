88b696dc1eeac7e72bc900fa942337eec5fca3ca
iLoop2 <fredrik.gundersen@mac.com>
2014-04-08 11:47:05 +0200

    ERT-592 Fix field file format enum

--- devel/python/python/ert/enkf/enums/enkf_field_file_format_enum.py
+++ devel/python/python/ert/enkf/enums/enkf_field_file_format_enum.py
@@ -18,19 +18,22 @@ from ert.enkf import ENKF_LIB
 
 
 class EnkfFieldFileFormatEnum(BaseCEnum):
-    UNDEFINED_FORMAT         = 0
-    RMS_ROFF_FILE            = 1
-    ECL_KW_FILE              = 2
-    ECL_KW_FILE_ACTIVE_CELLS = 3
-    ECL_KW_FILE_ALL_CELLS    = 4
-    ECL_GRDECL_FILE          = 5
-    ECL_FILE                 = 6
-    FILE_FORMAT_NULL         = 7
+    UNDEFINED_FORMAT         = None
+    RMS_ROFF_FILE            = None
+    ECL_KW_FILE              = None
+    ECL_KW_FILE_ACTIVE_CELLS = None
+    ECL_KW_FILE_ALL_CELLS    = None
+    ECL_GRDECL_FILE          = None
+    ECL_FILE                 = None
+    FILE_FORMAT_NULL         = None
 
-#EnkfFieldFileFormatEnum.populateEnum(ENKF_LIB, "enkf_field_file_format_iget")
+EnkfFieldFileFormatEnum.addEnum("UNDEFINED_FORMAT", 0)
+EnkfFieldFileFormatEnum.addEnum("RMS_ROFF_FILE", 1)
+EnkfFieldFileFormatEnum.addEnum("ECL_KW_FILE", 2)
+EnkfFieldFileFormatEnum.addEnum("ECL_KW_FILE_ACTIVE_CELLS", 3)
+EnkfFieldFileFormatEnum.addEnum("ECL_KW_FILE_ALL_CELLS", 4)
+EnkfFieldFileFormatEnum.addEnum("ECL_GRDECL_FILE", 5)
+EnkfFieldFileFormatEnum.addEnum("ECL_FILE", 6)
+EnkfFieldFileFormatEnum.addEnum("FILE_FORMAT_NULL", 7)
+
 EnkfFieldFileFormatEnum.registerEnum(ENKF_LIB, "enkf_field_file_format_enum")
-
-EnkfFieldFileFormatEnum.INITIALIZATION_TYPES = [EnkfFieldFileFormatEnum.ECL_KW_FILE]
-
-
-
---
+++ devel/python/python/ert_gui/models/connectors/export/export_model.py
@@ -0,0 +1,34 @@
+#  Copyright (C) 2011  Statoil ASA, Norway.
+#
+#  The file 'export_keyword_model.py' is part of ERT - Ensemble based Reservoir Tool.
+#
+#  ERT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+#  for more details.
+from ert.enkf import EnkfVarType
+from ert_gui.models import ErtConnector
+
+class ExportKeywordModel(ErtConnector):
+
+    def __init__(self):
+        super(ExportKeywordModel, self).__init__()
+
+    def getKeylistFromImplType(self, ert_impl_type):
+        return sorted(self.ert().ensembleConfig().getKeylistFromImplType(ert_impl_type))
+
+
+    def isDynamicFiled(self, key):
+        config_node = self.ert().ensembleConfig().getNode(key)
+        variable_type = config_node.getVariableType()
+        if variable_type == EnkfVarType.DYNAMIC_STATE:
+            return True
+        else:
+            return False
