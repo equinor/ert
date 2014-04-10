408a7d69dd2e08b4efe44fb16252a129add1a130
iLoop2 <fredrik.gundersen@mac.com>
2014-04-10 13:07:10 +0200

    ERT-592 Fix wrong file in copyright notice

--- devel/python/python/ert/enkf/enums/enkf_field_file_format_enum.py
+++ devel/python/python/ert/enkf/enums/enkf_field_file_format_enum.py
@@ -1,6 +1,6 @@
 #  Copyright (C) 2013  Statoil ASA, Norway.
 #
-#  The file 'content_type_enum.py' is part of ERT - Ensemble based Reservoir Tool.
+#  The file 'enkf_field_file_format_enum.py' is part of ERT - Ensemble based Reservoir Tool.
 #
 #  ERT is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
