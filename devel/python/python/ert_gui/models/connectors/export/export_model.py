366b11014edc031ec5c6849c932258f9985680cc
iLoop2 <fredrik.gundersen@mac.com>
2014-04-10 13:09:14 +0200

    ERT-592 Fix wrong file name in copyright notice

--- devel/python/python/ert_gui/models/connectors/export/export_model.py
+++ devel/python/python/ert_gui/models/connectors/export/export_model.py
@@ -1,6 +1,6 @@
 #  Copyright (C) 2011  Statoil ASA, Norway.
 #
-#  The file 'export_keyword_model.py' is part of ERT - Ensemble based Reservoir Tool.
+#  The file 'export_model.py' is part of ERT - Ensemble based Reservoir Tool.
 #
 #  ERT is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
