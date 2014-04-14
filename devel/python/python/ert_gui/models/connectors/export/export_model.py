00dcee2c12cdbde97a91bf8aa051c274a4db3fde
iLoop2 <fredrik.gundersen@mac.com>
2014-04-14 13:56:48 +0200

    ERT-593 Removed unused import

--- devel/python/python/ert_gui/models/connectors/export/export_model.py
+++ devel/python/python/ert_gui/models/connectors/export/export_model.py
@@ -14,7 +14,7 @@
 #  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
 #  for more details.
 
-from ert.enkf import EnsConfig, EnkfConfigNode, GenKw, EnkfNode, ErtImplType, NodeId
+from ert.enkf import EnkfConfigNode, GenKw, EnkfNode, ErtImplType, NodeId
 from ert_gui.models import ErtConnector
 
 class ExportModel(ErtConnector):
