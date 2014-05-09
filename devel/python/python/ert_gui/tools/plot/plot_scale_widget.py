5a3c3f1d8e0aff86003ef143c4333943a69a732b
jepebe <jepebe@users.noreply.github.co>
2014-05-09 07:11:12 +0200

    ERT-594 - Dynamic toolbar for plot scales

--- devel/python/python/ert_gui/tools/plot/plot_scale_widget.py
+++ devel/python/python/ert_gui/tools/plot/plot_scale_widget.py
@@ -1,49 +1,45 @@
 from PyQt4.QtCore import pyqtSignal
-from PyQt4.QtGui import QWidget, QHBoxLayout, QCheckBox, QDoubleSpinBox
+from PyQt4.QtGui import QWidget, QVBoxLayout, QCheckBox, QDoubleSpinBox, QSpinBox, QStackedWidget, QSizePolicy
+
+from ert.util import CTime
 from ert_gui.models.connectors.plot import ReportStepsModel
 from ert_gui.widgets.list_spin_box import ListSpinBox
 
+
 class PlotScalesWidget(QWidget):
+    plotScaleChanged = pyqtSignal()
 
-    plotScaleChanged = pyqtSignal(dict)
-
-    def __init__(self, type_key, title, time_spinner, min_value, minimum=None, maximum=None):
+    def __init__(self, type_key, title, select_min_time_value=False):
         QWidget.__init__(self)
 
-        if minimum is None:
-            minimum = -999999999999
-
-        if maximum is None:
-            maximum = 999999999999
-
-
-        self.__time_spinner = time_spinner
-        if time_spinner:
-            self.__time_map = ReportStepsModel().getList()
-            self.__time_index_map = {}
-            for index in range(len(self.__time_map)):
-                time = self.__time_map[index]
-                self.__time_index_map[time] = index
-
         self.__type_key = type_key
-        self.__spinner = None
+        self.__type = None
 
-        layout = QHBoxLayout()
-        layout.setMargin(0)
+        self.__double_spinner = self.createDoubleSpinner(minimum=-99999999.0, maximum=99999999.0)
+        self.__integer_spinner = self.createIntegerSpinner(minimum=0, maximum=999999999)
+
+        self.__time_map = ReportStepsModel().getList()
+        self.__time_index_map = {}
+        for index in range(len(self.__time_map)):
+            time = self.__time_map[index]
+            self.__time_index_map[time] = index
+
+        self.__time_spinner = self.createTimeSpinner(select_minimum_value=select_min_time_value)
+
+        layout = QVBoxLayout()
         self.setLayout(layout)
 
         self.__checkbox = QCheckBox(title)
         self.__checkbox.setChecked(False)
+
+        self.__stack = QStackedWidget()
+        self.__stack.setSizePolicy(QSizePolicy(QSizePolicy.Minimum))
+        self.__stack.addWidget(self.__integer_spinner)
+        self.__stack.addWidget(self.__double_spinner)
+        self.__stack.addWidget(self.__time_spinner)
+
+        layout.addWidget(self.__stack)
         layout.addWidget(self.__checkbox)
-
-        layout.addStretch()
-
-        if time_spinner:
-            self.__spinner = self.createTimeSpinner(min_value)
-        else:
-            self.__spinner = self.createDoubleSpinner(minimum, maximum)
-
-        layout.addWidget(self.__spinner)
 
         self.__checkbox.stateChanged.connect(self.toggleCheckbox)
         self.setLayout(layout)
@@ -54,58 +50,114 @@ class PlotScalesWidget(QWidget):
         spinner.setMinimumWidth(75)
         spinner.setRange(minimum, maximum)
         spinner.setKeyboardTracking(False)
-        spinner.editingFinished.connect(self.spinning)
-        spinner.valueChanged.connect(self.spinning)
+
+        spinner.editingFinished.connect(self.plotScaleChanged)
+        spinner.valueChanged.connect(self.plotScaleChanged)
+
         return spinner
 
-    def getSpinnerValue(self):
-        if self.__time_spinner:
-            if self.__spinner is not None:
-                index = self.__spinner.value()
-                return self.__time_map[index]
-            else:
-                return None
-        else:
-            return self.__spinner.value()
+    def createIntegerSpinner(self, minimum, maximum):
+        spinner = QSpinBox()
+        spinner.setEnabled(False)
+        spinner.setMinimumWidth(75)
+        spinner.setRange(minimum, maximum)
+        spinner.setKeyboardTracking(False)
 
+        spinner.editingFinished.connect(self.plotScaleChanged)
+        spinner.valueChanged.connect(self.plotScaleChanged)
 
-    def signalUpdatedValues(self):
-        scale_values = {"value": self.getSpinnerValue(), "type_key":self.__type_key, "enabled":self.__checkbox.isChecked()}
-        self.plotScaleChanged.emit(scale_values)
+        return spinner
 
-    def spinning(self):
-        self.signalUpdatedValues()
-
-    def toggleCheckbox(self):
-        checked = self.__checkbox.isChecked()
-        self.__spinner.setEnabled(checked)
-        self.signalUpdatedValues()
-
-    def setValues(self, values):
-        self.blockSignals(True)
-        type_key = values["type_key"]
-        checked = values["enabled"]
-        value = values["value"]
-        if type_key == self.__type_key:
-            self.__checkbox.setChecked(checked)
-            if value is not None:
-                if self.__time_spinner:
-                    index = self.__time_index_map[value]
-                    self.__spinner.setValue(index)
-                else:
-                    self.__spinner.setValue(value)
-        self.blockSignals(False)
-
-    def createTimeSpinner(self, min_value):
+    def createTimeSpinner(self, select_minimum_value):
         def converter(item):
             return "%s" % (str(item.date()))
 
         spinner = ListSpinBox(self.__time_map)
         spinner.setEnabled(False)
         spinner.setMinimumWidth(75)
-        spinner.valueChanged[int].connect(self.spinning)
-        spinner.editingFinished.connect(self.spinning)
-        spinner.setStringConverter(converter)
-        if min_value:
+
+        if select_minimum_value:
             spinner.setValue(0)
+
+        spinner.valueChanged[int].connect(self.plotScaleChanged)
+        spinner.editingFinished.connect(self.plotScaleChanged)
+        spinner.setStringConverter(converter)
+
         return spinner
+
+
+    def getValue(self):
+        if self.__type is int:
+            return self.__integer_spinner.value()
+        elif self.__type is float:
+            return self.__double_spinner.value()
+        elif self.__type is CTime:
+            index = self.__time_spinner.value()
+            return self.__time_map[index]
+        else:
+            raise TypeError("Unsupported spinner type: %s" % self.__type)
+
+
+    def toggleCheckbox(self):
+        checked = self.isChecked()
+        self.__double_spinner.setEnabled(checked)
+        self.__integer_spinner.setEnabled(checked)
+        self.__time_spinner.setEnabled(checked)
+        self.plotScaleChanged.emit()
+
+
+    def isChecked(self):
+        return self.__checkbox.isChecked()
+
+
+    def setChecked(self, checked):
+        self.__checkbox.setChecked(checked)
+
+
+    def setValue(self, value):
+        self.setChecked(value is not None)
+
+        if value is not None:
+            if self.__type is int:
+                self.__integer_spinner.setValue(int(value))
+            elif self.__type is float:
+                self.__double_spinner.setValue(value)
+            elif self.__type is CTime:
+                index = self.__time_index_map[value]
+                self.__time_spinner.setValue(index)
+            else:
+                raise TypeError("Unsupported spinner type: %s" % self.__type)
+
+
+    def setFontSize(self, size):
+        font = self.__double_spinner.font()
+        font.setPointSize(size)
+        self.__double_spinner.setFont(font)
+
+        font = self.__integer_spinner.font()
+        font.setPointSize(size)
+        self.__integer_spinner.setFont(font)
+
+        font = self.__time_spinner.font()
+        font.setPointSize(size)
+        self.__time_spinner.setFont(font)
+
+        font = self.__checkbox.font()
+        font.setPointSize(size)
+        self.__checkbox.setFont(font)
+
+
+    def setType(self, spinner_type):
+        self.__type = spinner_type
+        if spinner_type is int:
+            self.__stack.setCurrentWidget(self.__integer_spinner)
+        elif spinner_type is float:
+            self.__stack.setCurrentWidget(self.__double_spinner)
+        elif spinner_type is CTime:
+            self.__stack.setCurrentWidget(self.__time_spinner)
+        else:
+            raise TypeError("Unsupported spinner type: %s" % spinner_type)
+
+
+    def getType(self):
+        return self.__type
