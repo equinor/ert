bd77b86ffdf5588bfc0bbd5409e657ed92977403
Joakim Hove <joakim.hove@gmail.com>
2014-08-15 21:26:51 +0200

    ERT-711: Added copyright header and c++ guard.

--- devel/libenkf/include/ert/enkf/ert_log.h
+++ devel/libenkf/include/ert/enkf/ert_log.h
@@ -1,10 +1,32 @@
+/*
+   Copyright (C) 2014  Statoil ASA, Norway. 
+    
+   The file 'ert_log.c' is part of ERT - Ensemble based Reservoir Tool. 
+    
+   ERT is free software: you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by 
+   the Free Software Foundation, either version 3 of the License, or 
+   (at your option) any later version. 
+    
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+   FITNESS FOR A PARTICULAR PURPOSE.   
+    
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
+   for more details. 
+*/
+
 #ifndef ERTLOG_H
 #define ERTLOG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 #include <stdbool.h>
+
 #include <ert/util/log.h>
-#include <ert/util/util.h>
 
 void ert_log_init_log(int log_level,const char * log_file_name,const char * user_log_file_name,bool verbose);
 void ert_log_add_fmt_message(int message_level , FILE * dup_stream , const char * fmt , ...);
