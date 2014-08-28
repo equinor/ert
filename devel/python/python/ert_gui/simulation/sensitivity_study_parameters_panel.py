1bf44a82fb27f39542bbcb659c5df83a73dd5049
Per Røe <Per.Roe@nr.no>
2014-08-28 09:19:46 +0200

    ERT-735 ERT-716 Adding parameter types to sensitivity study, other than GEN_KW.

--- devel/python/python/ert_gui/simulation/sensitivity_study_parameters_panel.py
+++ devel/python/python/ert_gui/simulation/sensitivity_study_parameters_panel.py
@@ -17,14 +17,14 @@
 from collections import namedtuple
 
 from PyQt4.QtCore import Qt, QSize
-from PyQt4.QtGui import QWidget, QHBoxLayout, QTableWidget, QTableWidgetItem, \
-    QCheckBox, QHeaderView, QLabel
+from PyQt4.QtGui import QTableWidget, QHeaderView, QLabel
 
 from ert_gui.ide.keywords.definitions import FloatArgument
 from ert_gui.models.connectors.run import SensitivityStudyParametersModel, \
     SensivityStudyParametersConstantValueModel, SensivityStudyParametersIsIncludedModel
 from ert_gui.widgets.checkbox import CheckBox
 from ert_gui.widgets.string_box import StringBox
+from ert.enkf.enums.ert_impl_type_enum import ErtImplType
 
 
 class SensitivityStudyParametersPanel(QTableWidget):
@@ -34,34 +34,36 @@ class SensitivityStudyParametersPanel(QTableWidget):
     columns = {"name"        : Column(index = 0, header = "Parameter Name"),
                "is_active"   : Column(index = 1, header = "Include"),
                "const_value" : Column(index = 2, header = "Constant Value")}
-    
+
     column_list = ["name", "is_active", "const_value"]
 
     def __init__(self, parent=None):
         model = SensitivityStudyParametersModel()
         parameters = model.getParameters()
         n_parameters = len(parameters)
-        
+
         super(QTableWidget, self).__init__(n_parameters, len(self.columns), parent)
         self.verticalHeader().setResizeMode(QHeaderView.Fixed)
         self.verticalHeader().hide()
-        
+
         headers = [self.columns[col_id].header for col_id in self.column_list]
         self.setHorizontalHeaderLabels(headers)
 
         for row in range(n_parameters):
             param_name = parameters[row]
-            
+
             param_name_widget = QLabel(param_name)
             param_name_widget.setMargin(5)
             self.setCellWidget(row, self.columns["name"].index, param_name_widget)
-          
-            const_value_model = SensivityStudyParametersConstantValueModel(param_name, model)
-            const_value_widget = StringBox(const_value_model, "Constant value", 
-                                         "config/simulation/sensitivity_parameter_constant_value")
-            const_value_widget.setValidator(FloatArgument())
-            const_value_widget.setAlignment(Qt.AlignRight)
-            self.setCellWidget(row, self.columns["const_value"].index, const_value_widget)
+
+            if (model.getParameterType(param_name) == ErtImplType.GEN_KW):
+                const_value_model = SensivityStudyParametersConstantValueModel(param_name, model)
+                const_value_widget = StringBox(const_value_model, "Constant value", 
+                                               "config/simulation/sensitivity_parameter_constant_value")
+                const_value_widget.setValidator(FloatArgument())
+                const_value_widget.setAlignment(Qt.AlignRight)
+                self.setCellWidget(row, self.columns["const_value"].index, const_value_widget)
+
 
             is_active_model = SensivityStudyParametersIsIncludedModel(param_name, model)
             is_active_widget = CheckBox(is_active_model, "Is included", 
