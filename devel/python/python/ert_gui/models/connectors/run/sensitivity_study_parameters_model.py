da68afcc7c40edce2ec6d7a6a005518cce2aab9c
Per Røe <Per.Roe@nr.no>
2014-08-28 09:19:46 +0200

    ERT-735 ERT-716 Adding parameter types to sensitivity study, other than GEN_KW.

--- devel/python/python/ert_gui/models/connectors/run/sensitivity_study_parameters_model.py
+++ devel/python/python/ert_gui/models/connectors/run/sensitivity_study_parameters_model.py
@@ -5,13 +5,18 @@ Created on Aug 22, 2014
 '''
 
 from ert_gui.models import ErtConnector
+from ert.enkf.enums.ert_impl_type_enum import ErtImplType
 from ert.enkf.plot import EnsembleGenKWFetcher
+
 
 class SensitivityStudyParametersModel(ErtConnector):
 
     def __init__(self):
         super(SensitivityStudyParametersModel, self).__init__()
-        self.__gen_kw_params = None
+        self.__params = None
+        self.__is_included = {}
+        self.__constant_val = {}
+        self.__parameter_type = {}
 
 
     def setIsIncluded(self, parameter_name, is_included):
@@ -21,17 +26,35 @@ class SensitivityStudyParametersModel(ErtConnector):
         self.__constant_val[parameter_name] = constant_value
 
     def getParameters(self):
-        if self.__gen_kw_params is None:
-            keys = EnsembleGenKWFetcher(self.ert()).getSupportedKeys()
-            self.__gen_kw_params = sorted(keys, key=lambda k : k.lower())
-            
-            self.__is_included  = dict((p, True) for p in self.__gen_kw_params)
-            self.__constant_val = dict((p, 0.5) for p in self.__gen_kw_params)
 
-        return self.__gen_kw_params
+        if self.__params is None:
+            self.__params = []
+            for key in EnsembleGenKWFetcher(self.ert()).fetchSupportedKeys():
+                self.__params.append(key)
+                self.__is_included[key] = True
+                self.__constant_val[key] = 0.5
+                self.__parameter_type[key] = ErtImplType.GEN_KW
+
+            for key in self.ert().ensembleConfig().getKeylistFromImplType(ErtImplType.FIELD):
+                self.__params.append(key)
+                self.__is_included[key] = True
+                self.__constant_val[key] = None
+                self.__parameter_type[key] = ErtImplType.FIELD
+
+            for key in self.ert().ensembleConfig().getKeylistFromImplType(ErtImplType.GEN_DATA):
+                self.__params.append(key)
+                self.__is_included[key] = True
+                self.__constant_val[key] = None
+                self.__parameter_type[key] = ErtImplType.GEN_DATA
+                
+        return self.__params
 
     def getIsIncluded(self, parameter_name):
         return self.__is_included[parameter_name]
 
     def getConstantValue(self, parameter_name):
         return self.__constant_val[parameter_name]
+
+    def getParameterType(self, parameter_name):
+        return self.__parameter_type[parameter_name]
+
