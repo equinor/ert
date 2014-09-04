0127b61b6f6c34e206b98f3bd35bf4c640a4581b
jepebe <jepebe@users.noreply.github.co>
2014-09-04 14:23:57 +0200

    ERT-734 - Added support for new workflows in GUI

--- devel/python/python/ert_gui/models/connectors/run/workflows_model.py
+++ devel/python/python/ert_gui/models/connectors/run/workflows_model.py
@@ -1,7 +1,8 @@
-from ert.enkf.ert_workflow_list_handler import ErtWorkflowListHandler
 from ert_gui.models import ErtConnector
 from ert_gui.models.mixins import ChoiceModelMixin
 from ert_gui.models.mixins.list_model import ListModelMixin
+
+from ert.job_queue import WorkflowRunner, Workflow
 
 
 class WorkflowsModel(ErtConnector, ListModelMixin, ChoiceModelMixin):
@@ -27,18 +28,10 @@ class WorkflowsModel(ErtConnector, ListModelMixin, ChoiceModelMixin):
 
 
     def createWorkflowRunner(self):
+        """ @rtype: WorkflowRunner """
         workflow_name = self.getCurrentChoice()
         workflow_list = self.ert().getWorkflowList()
-        enkf_main_pointer = workflow_list.parent().from_param(workflow_list.parent())
-        return ErtWorkflowListHandler(workflow_list, workflow_name, enkf_main_pointer)
 
-
-    def getError(self):
-        error = self.ert().getWorkflowList().getLastError()
-
-        error_message = ""
-
-        for error_line in error:
-            error_message += error_line + "\n"
-
-        return error_message
+        workflow = workflow_list[workflow_name]
+        context = workflow_list.getContext()
+        return WorkflowRunner(workflow, self.ert(), context)
