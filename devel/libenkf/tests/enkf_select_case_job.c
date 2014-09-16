426457e202c509cae6328e57295071deee4519d9
Joakim Hove <joakim.hove@gmail.com>
2014-09-16 16:26:07 +0200

    ERT-734: Fixed test by explicitly installing job.

--- devel/libenkf/tests/enkf_select_case_job.c
+++ devel/libenkf/tests/enkf_select_case_job.c
@@ -36,17 +36,23 @@ int main(int argc , const char ** argv) {
   enkf_main_install_SIGNALS();
   
   const char * config_file  = argv[1];
+  const char * select_case_job = argv[2];
+
   ert_test_context_type * test_context = ert_test_context_alloc("SELECT_CASE" , config_file , NULL);
-  enkf_main_type * enkf_main = ert_test_context_get_main( test_context );
   
-  stringlist_type * args = stringlist_alloc_new();
-  stringlist_append_copy( args , "OtherCase");
-
-  test_assert_string_not_equal( "OtherCase" , enkf_main_get_current_fs( enkf_main ));
-  test_assert_true( ert_test_context_run_worklow_job( test_context , "SELECT_CASE" , args) );
-  test_assert_string_equal( "OtherCase" , enkf_main_get_current_fs( enkf_main ));
-
-  stringlist_free( args );
+  test_assert_true( ert_test_context_install_workflow_job( test_context , "SELECT_CASE" , select_case_job));
+  {
+    enkf_main_type * enkf_main = ert_test_context_get_main( test_context );
+    stringlist_type * args = stringlist_alloc_new();
+    stringlist_append_copy( args , "OtherCase");
+    
+    test_assert_string_not_equal( "OtherCase" , enkf_main_get_current_fs( enkf_main ));
+    ert_test_context_run_worklow_job( test_context , "SELECT_CASE" , args);
+    test_assert_true( ert_test_context_run_worklow_job( test_context , "SELECT_CASE" , args) );
+    test_assert_string_equal( "OtherCase" , enkf_main_get_current_fs( enkf_main ));
+    
+    stringlist_free( args );
+  }
   ert_test_context_free( test_context );
   exit(0);
 }
