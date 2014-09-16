ba6b7333677cb97e25ce14a3d4ab998e2bc6d783
chflo <chflo@statoil.com>
2014-09-16 10:44:10 +0200

    ERT-736: Fixed rng realloc bug

--- devel/libenkf/src/enkf_main.c
+++ devel/libenkf/src/enkf_main.c
@@ -2623,12 +2623,12 @@ rng_config_type * enkf_main_get_rng_config( const enkf_main_type * enkf_main ) {
   return enkf_main->rng_config;
 }
 
+
 void enkf_main_rng_init( enkf_main_type * enkf_main) {
-  if (enkf_main->rng != NULL) {
-    rng_free( enkf_main->rng );
-    enkf_main->rng = NULL;
-  }
-  enkf_main->rng = rng_config_alloc_rng( enkf_main->rng_config );
+  if (enkf_main->rng != NULL)
+    rng_config_init_rng(enkf_main->rng_config, enkf_main->rng);
+  else
+    enkf_main->rng = rng_config_alloc_rng( enkf_main->rng_config );
 }
 
 
@@ -2833,6 +2833,7 @@ enkf_main_type * enkf_main_bootstrap(const char * _site_config, const char * _mo
     */
     rng_config_init( enkf_main->rng_config , config );
     enkf_main_rng_init( enkf_main );  /* Must be called before the ensmeble is created. */
+
     enkf_main_init_subst_list( enkf_main );
     ert_workflow_list_init( enkf_main->workflow_list , config );
     
