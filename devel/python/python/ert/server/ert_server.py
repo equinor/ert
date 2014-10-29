e98a2311f4bc1df56e867a0586f02403accdaff2
Joakim Hove <joakim.hove@gmail.com>
2014-10-29 07:18:41 +0100

    ERT-782: Server can restart simulation set:Locking

--- devel/python/python/ert/server/ert_server.py
+++ devel/python/python/ert/server/ert_server.py
@@ -18,9 +18,10 @@ import sys
 import threading
 import json
 import os
+import threading 
 
-from ert.enkf import EnKFMain,RunArg
-from ert.enkf.enums import EnkfRunType , EnkfStateType
+from ert.enkf import EnKFMain,RunArg,EnkfFsManager
+from ert.enkf.enums import EnkfRunType, EnkfStateType, ErtImplType , EnkfVarType , RealizationStateEnum
 from ert.enkf import NodeId
 from ert.util import installAbortSignals
 
@@ -45,7 +46,9 @@ class ErtServer(object):
                 raise IOError("The config file:%s does not exist" % config_file)
         self.initCmdTable()
         self.run_context = None
-
+        self.init_fs = None
+        self.run_fs = None
+        self.run_count = 0
 
 
 
@@ -96,28 +99,64 @@ class ErtServer(object):
             if self.run_context is None:
                 return ["READY"]
             else:
-                return ["RUNNING" , self.run_context.getNumRunning() , self.run_context.getNumComplete()]
+                if self.run_context.isRunning():
+                    if len(args) == 0:
+                        return ["RUNNING" , self.run_context.getNumRunning() , self.run_context.getNumComplete()]
+                    else:
+                        iens = args[0]
+                        if self.run_context.realisationComplete(iens):
+                            return ["COMPLETE"]
+                        else:
+                            return ["RUNNING"]
+                else:
+                    return ["COMPLETE"]
         else:
             return ["CLOSED"]
 
+    
+    def initSimulations(self , args):
+        run_size = args[0]
+        init_case = str(args[1])
+        run_case = str(args[2])
+        
+        fs_manager = self.ert_handle.getEnkfFsManager()
+        self.run_fs = fs_manager.getFileSystem( run_case )
+        self.init_fs = fs_manager.getFileSystem( init_case )
+        fs_manager.switchFileSystem( self.run_fs )
+
+        self.run_context = RunContext(self.ert_handle , run_size , self.run_fs  , self.run_count)
+        self.run_count += 1
+        return self.handleSTATUS([])
+
+
+    def restartSimulations(self , args):
+        return self.initSimulations(args)
+
 
     def handleINIT_SIMULATIONS(self , args):
-        if len(args) == 2:
-            if self.run_context is None:
-                run_size = args[0]
-                init_case = args[1]
-                self.run_context = RunContext(self.ert_handle , run_size , init_case)
-                return self.handleSTATUS([])
-            else:
-                raise ErtCmdError("The ert server has already started simulations")
+        if len(args) == 3:
+            lock = threading.Lock()
+            result = []
+            with lock:
+                if self.run_context is None:
+                    self.initSimulations( args )
+                else:
+                    if not self.run_context.isRunning():
+                        self.restartSimulations( args )
+                
+                result = ["OK"]
+                
+            return result
         else:
-            raise ErtCmdError("The INIT_SIMULATIONS command expects two arguments: [ensemble_size , init_case]")
+            raise ErtCmdError("The INIT_SIMULATIONS command expects three arguments: [ensemble_size , init_case, run_case]")
+
 
     
     def handleGET_RESULT(self , args):
         iens = args[0]
         report_step = args[1]
         kw = str(args[2])
+
         ensembleConfig = self.ert_handle.ensembleConfig()
         if ensembleConfig.hasKey( kw ):
             state = self.ert_handle.getRealisation( iens )
@@ -128,7 +167,7 @@ class ErtServer(object):
             node_id = NodeId(report_step , iens , EnkfStateType.FORECAST )
             if node.tryLoad( fs , node_id ):
                 data = gen_data.getData()
-                return json.dumps( ["OK"] + data.asList() )
+                return ["OK"] + data.asList()
             else:
                 raise ErtCmdError("Loading iens:%d  report:%d   kw:%s   failed" % (iens , report_step , kw))
         else:
@@ -142,6 +181,7 @@ class ErtServer(object):
         pert_id = args[1]
         iens = args[2]
         kw = str(args[3])
+
         ensembleConfig = self.ert_handle.ensembleConfig()
         if ensembleConfig.hasKey(kw):
             state = self.ert_handle[iens]
@@ -157,6 +197,39 @@ class ErtServer(object):
             
 
 
+    # ["ADD_SIMULATION" , 0 , 1 , 1 [ ["KW1" , ...] , ["KW2" , ....]]]
     def handleADD_SIMULATION(self , args):
-        iens = args[0]
+        geo_id = args[0]
+        pert_id = args[1]
+        iens = args[2]
+        kw_list = args[3]
+        state = self.ert_handle.getRealisation( iens )
+        state.addSubstKeyword( "GEO_ID" , "%s" % geo_id )
+        
+        elco_kw = [ l[0] for l in kw_list ]
+        ens_config = self.ert_handle.ensembleConfig()
+
+        for kw in ens_config.getKeylistFromVarType( EnkfVarType.PARAMETER ):
+            if not kw in elco_kw:
+                node = state[kw]
+                init_id = NodeId(0 , geo_id , EnkfStateType.ANALYZED )
+                run_id = NodeId(0 , iens , EnkfStateType.ANALYZED )
+                node.load( self.init_fs , init_id )
+                node.save( self.run_fs , run_id )
+            
+        for kw_arg in kw_list:
+            kw = str(kw_arg[0])
+            data = kw_arg[1:]
+        
+            node = state[kw]
+            gen_kw = node.asGenKw()
+            gen_kw.setValues(data)
+        
+            run_id = NodeId(0 , iens , EnkfStateType.ANALYZED )
+            node.save( self.run_fs , run_id )
+
+        state_map = self.run_fs.getStateMap()
+        state_map[iens] = RealizationStateEnum.STATE_INITIALIZED
+
         self.run_context.startSimulation( iens )
+        return self.handleSTATUS([])
