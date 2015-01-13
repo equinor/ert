2498030e246e1bfb3d04c91ae6ab188c72831f0d
Joakim Hove <joakim.hove@gmail.com>
2015-01-13 16:14:48 +0100

    ERT-830: White space change

--- devel/libenkf/include/ert/enkf/meas_data.h
+++ devel/libenkf/include/ert/enkf/meas_data.h
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2011  Statoil ASA, Norway. 
-    
-   The file 'meas_data.h' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2011  Statoil ASA, Norway.
+
+   The file 'meas_data.h' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 
 #ifndef __MEAS_DATA_H__
@@ -31,14 +31,14 @@ extern "C" {
 #include <ert/util/type_macros.h>
 
 typedef struct meas_data_struct   meas_data_type;
-typedef struct meas_block_struct  meas_block_type;  
+typedef struct meas_block_struct  meas_block_type;
 
   UTIL_IS_INSTANCE_HEADER( meas_data );
 
 void               meas_block_iset( meas_block_type * meas_block , int iens , int iobs , double value);
 double             meas_block_iget_ens_mean( const meas_block_type * meas_block , int iobs );
 double             meas_block_iget_ens_std( const meas_block_type * meas_block , int iobs);
-void               meas_block_deactivate( meas_block_type * meas_block , int iobs );                               
+void               meas_block_deactivate( meas_block_type * meas_block , int iobs );
 
 void meas_data_fprintf( const meas_data_type * matrix , FILE * stream);
 
