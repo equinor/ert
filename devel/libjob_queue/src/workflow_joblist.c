3a5a2f4528354709cc9d36d0e76d91a6536b66b3
Joakim Hove <joakim.hove@gmail.com>
2015-01-15 08:47:23 +0100

    ERT-830: White space change

--- devel/libjob_queue/src/workflow_joblist.c
+++ devel/libjob_queue/src/workflow_joblist.c
@@ -1,20 +1,20 @@
 /*
-   Copyright (C) 2012  Statoil ASA, Norway. 
-    
+   Copyright (C) 2012  Statoil ASA, Norway.
+
    The file 'workflow_joblist.c' is part of ERT - Ensemble based
    Reservoir Tool.
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 
 #include <stdbool.h>
@@ -80,7 +80,7 @@ bool workflow_joblist_add_job_from_file( workflow_joblist_type * joblist , const
   if (job) {
     workflow_joblist_add_job( joblist , job );
     return true;
-  } else 
+  } else
     return false;
 }
 
