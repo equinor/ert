1a6b8076fc3510e964d9352368bda5a65f5ea1d9
Joakim Hove <joakim.hove@gmail.com>
2015-01-20 17:42:24 +0100

    ERT-830: White space change

--- devel/libenkf/include/ert/enkf/local_obsdata_node.h
+++ devel/libenkf/include/ert/enkf/local_obsdata_node.h
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2013  Statoil ASA, Norway. 
-    
+   Copyright (C) 2013  Statoil ASA, Norway.
+
    The file 'local_obsdata_node.h'
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 #ifndef __LOCAL_OBSDATA_NODE_H__
 #define __LOCAL_OBSDATA_NODE_H__
@@ -29,7 +29,7 @@ extern "C" {
 
 
   typedef struct local_obsdata_node_struct local_obsdata_node_type;
-  
+
   local_obsdata_node_type   * local_obsdata_node_alloc( const char * obs_key );
   const char                * local_obsdata_node_get_key( const local_obsdata_node_type * node );
   void                        local_obsdata_node_free( local_obsdata_node_type * node );
@@ -39,7 +39,7 @@ extern "C" {
   void                        local_obsdata_node_copy_active_list( local_obsdata_node_type * node , const active_list_type * active_list);
   void                        local_obsdata_node_add_tstep( local_obsdata_node_type * node, int tstep);
   void                        local_obsdata_node_add_range( local_obsdata_node_type * node, int step1, int step2);
-  
+
 UTIL_IS_INSTANCE_HEADER( local_obsdata_node );
 
 #ifdef __cplusplus
--- devel/libenkf/include/ert/enkf/obs_vector.h
+++ devel/libenkf/include/ert/enkf/obs_vector.h
@@ -1,25 +1,25 @@
 /*
-   Copyright (C) 2011  Statoil ASA, Norway. 
-    
-   The file 'obs_vector.h' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2011  Statoil ASA, Norway.
+
+   The file 'obs_vector.h' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 
 #ifndef __OBS_VECTOR_H__
 #define __OBS_VECTOR_H__
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 extern "C" {
 #endif
 
@@ -46,17 +46,17 @@ extern "C" {
   typedef void   (obs_free_ftype)                (void *);
   typedef void   (obs_get_ftype)                 (const void * , obs_data_type * , enkf_fs_type *, int , const active_list_type * );
   typedef void   (obs_meas_ftype)                (const void * , const void *, node_id_type , meas_data_type * , const active_list_type * );
-  typedef void   (obs_user_get_ftype)            (void * , const char * , double * , double * , bool *); 
-  typedef void   (obs_scale_std_ftype)           (void * , double ); 
+  typedef void   (obs_user_get_ftype)            (void * , const char * , double * , double * , bool *);
+  typedef void   (obs_scale_std_ftype)           (void * , double );
   typedef double (obs_chi2_ftype)                (const void * , const void *, node_id_type );
 
   typedef enum { GEN_OBS     = 1,
                  SUMMARY_OBS = 2,
                  BLOCK_OBS   = 3} obs_impl_type;
-  
+
   typedef struct obs_vector_struct obs_vector_type;
-  
-  
+
+
   void                 obs_vector_clear_nodes( obs_vector_type * obs_vector );
   void                 obs_vector_del_node(obs_vector_type * obs_vector , int index);
   void                 obs_vector_free(obs_vector_type * );
@@ -82,25 +82,25 @@ extern "C" {
   void                 obs_vector_install_node(obs_vector_type * obs_vector , int obs_index , void * node );
 
   double                  obs_vector_chi2(const obs_vector_type *  , enkf_fs_type *  , node_id_type node_id);
-  
-  void                    obs_vector_ensemble_chi2(const obs_vector_type * obs_vector , 
-                                                   enkf_fs_type * fs, 
-                                                   bool_vector_type * valid , 
-                                                   int step1 , int step2 , 
-                                                   int iens1 , int iens2 , 
-                                                   state_enum load_state , 
+
+  void                    obs_vector_ensemble_chi2(const obs_vector_type * obs_vector ,
+                                                   enkf_fs_type * fs,
+                                                   bool_vector_type * valid ,
+                                                   int step1 , int step2 ,
+                                                   int iens1 , int iens2 ,
+                                                   state_enum load_state ,
                                                    double ** chi2);
 
   double                  obs_vector_total_chi2(const obs_vector_type * , enkf_fs_type * , int , state_enum  );
   void                    obs_vector_ensemble_total_chi2(const obs_vector_type *  , enkf_fs_type *  , int  , state_enum , double * );
   enkf_config_node_type * obs_vector_get_config_node(const obs_vector_type * );
   const char            * obs_vector_get_obs_key( const obs_vector_type * obs_vector);
-  
+
   UTIL_SAFE_CAST_HEADER(obs_vector);
   VOID_FREE_HEADER(obs_vector);
-  
-  
-#ifdef __cplusplus 
+
+
+#ifdef __cplusplus
 }
 #endif
 #endif
