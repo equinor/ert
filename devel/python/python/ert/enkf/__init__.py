4720121829825a223c1e41a2d3d8b1421c61e201
jepebe <jepebe@users.noreply.github.co>
2015-01-29 15:25:00 +0100

    ERT-840 - Created SummaryKeySet data structure for storing Summary Keys that an EnkfFS instance has data for.

--- devel/python/python/ert/enkf/__init__.py
+++ devel/python/python/ert/enkf/__init__.py
@@ -26,6 +26,7 @@ from .node_id import NodeId
 from .enkf_linalg import EnkfLinalg
 from .util import TimeMap
 from .state_map import StateMap
+from .summary_key_set import SummaryKeySet
 from .enkf_fs import EnkfFs
 
 from .ert_workflow_list import ErtWorkflowList
