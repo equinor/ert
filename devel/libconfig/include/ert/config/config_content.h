330472330827024fef5c5bbe20cf26e5dcc3c7a3
Joakim Hove <joakim.hove@gmail.com>
2015-02-11 12:13:44 +0100

    ERT-747: Added empty config_content class

---
+++ devel/libconfig/include/ert/config/config_content.h
@@ -0,0 +1,40 @@
+/*
+   Copyright (C) 2015  Statoil ASA, Norway.
+
+   The file 'config_content.h' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
+*/
+
+
+#ifndef __CONFIG_CONTENT_H__
+#define __CONFIG_CONTENT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <ert/util/type_macros.h>
+
+typedef struct config_content_struct config_content_type;
+
+
+  config_content_type * config_content_alloc();
+  void config_content_free( config_content_type * content );
+
+  UTIL_IS_INSTANCE_HEADER( config_content );
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ devel/libconfig/src/config_content.c
@@ -0,0 +1,43 @@
+/*
+   Copyright (C) 2015  Statoil ASA, Norway.
+
+   The file 'config_content.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
+*/
+
+#include <ert/util/type_macros.h>
+
+#include <ert/config/config_content.h>
+
+
+#define CONFIG_CONTENT_TYPE_ID 6612520
+
+struct config_content_struct {
+  UTIL_TYPE_ID_DECLARATION;
+};
+
+
+UTIL_IS_INSTANCE_FUNCTION( config_content , CONFIG_CONTENT_TYPE_ID )
+
+config_content_type * config_content_alloc() {
+  config_content_type * content = util_malloc( sizeof * content );
+  UTIL_TYPE_ID_INIT( content , CONFIG_CONTENT_TYPE_ID );
+  return content;
+}
+
+
+
+void config_content_free( config_content_type * content ) {
+  free( content );
+}
