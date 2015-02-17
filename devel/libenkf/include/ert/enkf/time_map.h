780cbe4431c31cfe801bd3a0e1ae87da1feb88a6
Joakim Hove <joakim.hove@gmail.com>
2015-02-17 10:14:24 +0100

    ERT-837: White space change

--- devel/libenkf/include/ert/enkf/time_map.h
+++ devel/libenkf/include/ert/enkf/time_map.h
@@ -1,25 +1,25 @@
 /*
-   Copyright (C) 2011  Statoil ASA, Norway. 
-   The file 'time_map.c' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-   
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2011  Statoil ASA, Norway.
+   The file 'time_map.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 #ifndef __TIME_MAP_H__
 #define __TIME_MAP_H__
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 extern "C" {
-#endif 
+#endif
 
 #include <time.h>
 
@@ -61,7 +61,7 @@ typedef struct time_map_struct time_map_type;
   int              time_map_lookup_time( time_map_type * map , time_t time);
   int              time_map_lookup_days( time_map_type * map , double sim_days);
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 }
 #endif
 #endif
