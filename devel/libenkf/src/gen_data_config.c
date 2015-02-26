6cedeef28c4e80ef7d02c5101f8f71aa04fc698e
Joakim Hove <joakim.hove@gmail.com>
2015-02-26 13:05:18 +0100

    ERT-860: Added getKey and getDataSize() methods

--- devel/libenkf/src/gen_data_config.c
+++ devel/libenkf/src/gen_data_config.c
@@ -103,8 +103,13 @@ ecl_type_enum gen_data_config_get_internal_type(const gen_data_config_type * con
    (i.e. not set); we seek through
 */
 
-int gen_data_config_get_data_size( const gen_data_config_type * config , int report_step) {
+int gen_data_config_get_data_size__( const gen_data_config_type * config , int report_step) {
   int current_size = int_vector_safe_iget( config->data_size_vector , report_step );
+  return current_size;
+}
+
+int gen_data_config_get_data_size( const gen_data_config_type * config , int report_step) {
+  int current_size = gen_data_config_get_data_size__(config , report_step);
   if (current_size < 0)
     util_abort("%s: Size not set for object:%s report_step:%d - internal error: \n",__func__ , config->key , report_step);
   return current_size;
--- devel/python/python/ert/enkf/data/gen_data_config.py
+++ devel/python/python/ert/enkf/data/gen_data_config.py
@@ -27,6 +27,17 @@ class GenDataConfig(BaseCClass):
     def get_template_key(self):
         return GenDataConfig.cNamespace().get_template_key(self)
 
+    def getDataSize(self , report_step):
+        data_size = GenDataConfig.cNamespace().get_data_size(self , report_step)
+        if data_size < 0:
+            raise ValueError("No data has been loaded for %s at report step:%d " % (self.getName() , report_step))
+        else:
+            return data_size
+            
+    def getName(self):
+        return GenDataConfig.cNamespace().get_key(self)
+
+
     def get_initial_size(self):
         return GenDataConfig.cNamespace().get_initial_size(self)
 
@@ -60,4 +71,6 @@ GenDataConfig.cNamespace().get_template_file = cwrapper.prototype("char* gen_dat
 GenDataConfig.cNamespace().get_template_key = cwrapper.prototype("char* gen_data_config_get_template_key(gen_data_config)")
 GenDataConfig.cNamespace().get_initial_size = cwrapper.prototype("int gen_data_config_get_initial_size(gen_data_config)")
 GenDataConfig.cNamespace().has_report_step = cwrapper.prototype("bool gen_data_config_has_report_step(gen_data_config, int)")
+GenDataConfig.cNamespace().get_data_size    = cwrapper.prototype("int gen_data_config_get_data_size__(gen_data_config , int)")
+GenDataConfig.cNamespace().get_key          = cwrapper.prototype("char* gen_data_config_get_key(gen_data_config)")
 
