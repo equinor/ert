c84d32dee05d82397016cd5743ed3b43d193d139
jepebe <jepebe@users.noreply.github.co>
2015-02-27 15:05:13 +0100

    ERT-848 - Improved plotting and plotting of observations for summary

--- devel/python/python/ert_gui/shell/__init__.py
+++ devel/python/python/ert_gui/shell/__init__.py
@@ -1,3 +1,4 @@
-from .shell_tools import assertConfigLoaded, autoCompleteList, extractFullArgument, getPossibleFilenameCompletions, pathify, createParameterizedHelpFunction
+from .shell_plot import ShellPlot
+from .shell_tools import assertConfigLoaded, autoCompleteList, extractFullArgument, getPossibleFilenameCompletions, pathify, createParameterizedHelpFunction, autoCompleteListWithSeparator
 from .shell_function import ShellFunction
 from .ertshell import ErtShell
