4641ac7a8bebb05312166f95e4d16047fd550350
Joakim Hove <joakim.hove@gmail.com>
2015-02-27 12:12:25 +0100

    ERT-860: Added GEN_DATA collector

--- devel/python/python/ert/enkf/export/__init__.py
+++ devel/python/python/ert/enkf/export/__init__.py
@@ -2,7 +2,9 @@ from .design_matrix_reader import DesignMatrixReader
 from .summary_observation_collector import SummaryObservationCollector
 from .summary_collector import SummaryCollector
 from .gen_kw_collector import GenKwCollector
+from .gen_data_collector import GenDataCollector
 from .misfit_collector import MisfitCollector
 
-__all__ = ["DesignMatrixReader", "SummaryCollector", "SummaryObservationCollector", "GenKwCollector", "MisfitCollector"]
+__all__ = ["DesignMatrixReader", "SummaryCollector", "SummaryObservationCollector" , "GenKwCollector", "MisfitCollector", "GenDataCollector"]
+
 
