b50ce89376cff86bc26511329f2cd7dc1d77ab52
Jean-Paul <jepebe@users.noreply.github.com>
2015-06-02 13:59:06 +0200

    ERT-893 - Ertshell: Added support for smoother overlap_alpha and std_cutoff

--- devel/python/python/ert_gui/shell/smoother.py
+++ devel/python/python/ert_gui/shell/smoother.py
@@ -6,6 +6,8 @@ class Smoother(ShellFunction):
     def __init__(self, shell_context):
         super(Smoother, self).__init__("smoother", shell_context)
         self.addHelpFunction("update", "<target_case>", "Run smoother update on the current case, placing the results in the target case.")
+        self.addHelpFunction("overlap_alpha", "[alpha_value]", "Show or set the overlap alpha.")
+        self.addHelpFunction("std_cutoff", "[cutoff_value]", "Show or set the standard deviation cutoff value (>0).")
 
 
     @assertConfigLoaded
@@ -19,13 +21,42 @@ class Smoother(ShellFunction):
             success = simulation_runner.smootherUpdate(target_fs)
 
             if not success:
-                print("Error: Unable to perform update")
+                self.lastCommandFailed("Unable to perform update")
 
         else:
-            print("Error: Expected one argument: <target_fs> received: '%s'" % line)
+            self.lastCommandFailed("Expected one argument: <target_fs> received: '%s'" % line)
 
 
+    @assertConfigLoaded
+    def do_overlap_alpha(self, line):
+        value = line.strip()
+        if value == "":
+            analysis_config = self.shellContext().ert().analysisConfig()
+            print("Overlap Alpha = %f" % analysis_config.getEnkfAlpha())
+        else:
+            try:
+                value = float(value)
+                analysis_config = self.shellContext().ert().analysisConfig()
+                analysis_config.setEnkfAlpha(value)
+                print("Overlap Alpha set to: %f" % analysis_config.getEnkfAlpha())
+            except ValueError:
+                self.lastCommandFailed("Expected a number")
 
+
+    @assertConfigLoaded
+    def do_std_cutoff(self, line):
+        value = line.strip()
+        if value == "":
+            analysis_config = self.shellContext().ert().analysisConfig()
+            print("Standard Deviation Cutoff = %f" % analysis_config.getStdCutoff())
+        else:
+            try:
+                value = float(value)
+                analysis_config = self.shellContext().ert().analysisConfig()
+                analysis_config.setStdCutoff(value)
+                print("Standard Deviation Cutoff set to: %f" % analysis_config.getStdCutoff())
+            except ValueError:
+                self.lastCommandFailed("Expected a number")
 
 
 
