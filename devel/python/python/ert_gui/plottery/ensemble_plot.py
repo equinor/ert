6fa8df0bfb71609a55c43352ea8fec6cdd99e587
Jean-Paul <jepebe@users.noreply.github.com>
2015-08-25 14:41:00 +0200

    ERT-923 - Added refcase and observation plotting

--- devel/python/python/ert_gui/plottery/ensemble_plot.py
+++ devel/python/python/ert_gui/plottery/ensemble_plot.py
@@ -23,3 +23,21 @@ class EnsemblePlot(object):
         if len(lines) > 0:
             plot_config.addLegendItem(ensemble_label, lines[0])
 
+
+    @staticmethod
+    def plotRefcase(axes, plot_config, data):
+        """
+        @type axes: matplotlib.axes.Axes
+        @type plot_config: PlotConfig
+        @type data: DataFrame
+        """
+
+        line_color = plot_config.refcaseColor()
+        line_alpha = plot_config.refcaseAlpha()
+        line_marker = plot_config.refcaseMarker()
+        line_style = plot_config.refcaseStyle()
+        line_width = plot_config.refcaseWidth()
+        lines = axes.plot_date(x=data.index.values, y=data, color=line_color, alpha=line_alpha, marker=line_marker, linestyle=line_style, linewidth=line_width)
+
+        if len(lines) > 0:
+            plot_config.addLegendItem("Refcase", lines[0])
