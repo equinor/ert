676b1b3eec43765b82f5ab2536be8becb6fd5a85
Jean-Paul <jepebe@users.noreply.github.com>
2015-08-27 09:09:09 +0200

    ERT-924 - Added tracking of current plot and figure dirtyness

--- devel/python/python/ert_gui/tools/plot/plot_widget.py
+++ devel/python/python/ert_gui/tools/plot/plot_widget.py
@@ -6,11 +6,13 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 
 class PlotWidget(QWidget):
-    def __init__(self, name, short_name, parent=None):
+    def __init__(self, name, plotFunction, plotCondition, plotContextFunction, parent=None):
         QWidget.__init__(self, parent)
 
         self.__name = name
-        self.__short_name = short_name
+        self.__plotFunction = plotFunction
+        self.__plotContextFunction = plotContextFunction
+        self.__plotCondition = plotCondition
         self.__figure = Figure()
         self.__figure.set_tight_layout(True)
         self.__canvas = FigureCanvas(self.__figure)
@@ -24,8 +26,10 @@ class PlotWidget(QWidget):
         vbox.addWidget(self.__toolbar)
         self.setLayout(vbox)
 
-
+        self.__dirty = True
+        self.__active = False
         self.resetPlot()
+
 
 
     def getFigure(self):
@@ -38,4 +42,28 @@ class PlotWidget(QWidget):
 
 
     def updatePlot(self):
-        self.__canvas.draw()
+        if self.isDirty() and self.isActive():
+            print("Drawing: %s" % self.__name)
+            self.resetPlot()
+            plot_context = self.__plotContextFunction(self.getFigure())
+            self.__plotFunction(plot_context)
+            self.__canvas.draw()
+
+            self.setDirty(False)
+
+
+    def setDirty(self, dirty=True):
+        self.__dirty = dirty
+
+    def isDirty(self):
+        return self.__dirty
+
+    def setActive(self, active=True):
+        self.__active = active
+
+    def isActive(self):
+        return self.__active
+
+
+    def canPlotKey(self, key):
+        return self.__plotCondition(key)
