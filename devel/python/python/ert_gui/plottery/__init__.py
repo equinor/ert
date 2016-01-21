a2f5088520166885278269a50dab0cef2769df80
Jean-Paul Balabanian <jpb@statoil.com>
2016-01-21 12:34:01 +0100

    ERT-994 Added support for history tracking of PlotConfigs

--- devel/python/python/ert_gui/plottery/__init__.py
+++ devel/python/python/ert_gui/plottery/__init__.py
@@ -2,4 +2,5 @@ from .plot_data_gatherer import PlotDataGatherer
 from .plot_style import PlotStyle
 from .plot_config import PlotConfig
 from .plot_context import PlotContext
+from .plot_config_history import PlotConfigHistory
 
