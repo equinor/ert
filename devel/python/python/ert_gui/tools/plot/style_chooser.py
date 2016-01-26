3b68f4bc01f6d65b50cb03b9b08c4ac1a6329528
Jean-Paul Balabanian <jpb@statoil.com>
2016-01-26 15:21:54 +0100

    ERT-994 Changed how StyleChooser and ColorBox works

--- devel/python/python/ert_gui/tools/plot/style_chooser.py
+++ devel/python/python/ert_gui/tools/plot/style_chooser.py
@@ -1,40 +1,37 @@
-from PyQt4.QtCore import QSize, QRect, pyqtSignal, Qt
-from PyQt4.QtGui import QWidget, QPainter, QHBoxLayout, QLabel, QComboBox, QVBoxLayout
+from PyQt4.QtGui import QWidget, QHBoxLayout, QComboBox, QDoubleSpinBox, QLabel, QHBoxLayout
+
+from ert_gui.plottery import PlotStyle
+
+
+STYLE_OFF = ("Off", None)
+STYLE_AREA = ("Area", "#")
+STYLE_SOLID = ("Solid", "-")
+STYLE_DASHED = ("Dashed", "--")
+STYLE_DOTTED = ("Dotted", ":")
+STYLE_DASH_DOTTED = ("Dash Dotted", "-.")
+
+STYLES = [STYLE_OFF, STYLE_AREA, STYLE_SOLID, STYLE_DASHED, STYLE_DOTTED, STYLE_DASH_DOTTED]
+STYLES_LINE_ONLY = [STYLE_OFF, STYLE_SOLID, STYLE_DASHED, STYLE_DOTTED, STYLE_DASH_DOTTED]
+
+MARKER_OFF = ("Off", None)
+MARKER_X = ("X", "x")
+MARKER_CIRCLE = ("Circle", "o")
+MARKER_POINT = ("Point", ".")
+MARKER_STAR = ("Star", "*")
+MARKER_DIAMOND = ("Diamond", "D")
+
+MARKERS = [MARKER_OFF, MARKER_X, MARKER_CIRCLE, MARKER_POINT, MARKER_STAR, MARKER_DIAMOND]
 
 
 class StyleChooser(QWidget):
-    styleChanged = pyqtSignal(str, str)
 
-    STYLE_OFF = ("Off", None)
-    STYLE_AREA = ("Area", "#")
-    STYLE_SOLID = ("Solid", "-")
-    STYLE_DASHED = ("Dashed", "--")
-    STYLE_DOTTED = ("Dotted", ":")
-    STYLE_DASH_DOTTED = ("Dash Dotted", "-.")
-
-    STYLES = [STYLE_OFF, STYLE_AREA, STYLE_SOLID, STYLE_DASHED, STYLE_DOTTED, STYLE_DASH_DOTTED]
-    STYLES_LINE_ONLY = [STYLE_OFF, STYLE_SOLID, STYLE_DASHED, STYLE_DOTTED, STYLE_DASH_DOTTED]
-
-    MARKER_OFF = ("Off", None)
-    MARKER_X = ("X", "x")
-    MARKER_CIRCLE = ("Circle", "o")
-    MARKER_POINT = ("Point", ".")
-    MARKER_STAR = ("Star", "*")
-    MARKER_DIAMOND = ("Diamond", "D")
-
-    MARKERS = [MARKER_OFF, MARKER_X, MARKER_CIRCLE, MARKER_POINT, MARKER_STAR, MARKER_DIAMOND]
-
-    """Combines a StyleChooser with a label"""
-    def __init__(self, label, line_style=STYLE_OFF, marker_style=MARKER_OFF, area_supported=False, labeled=False):
+    def __init__(self, area_supported=False):
         QWidget.__init__(self)
-
-        self.__styles = StyleChooser.STYLES if area_supported else StyleChooser.STYLES_LINE_ONLY
+        self._style = PlotStyle("StyleChooser Internal Style")
+        self._styles = STYLES if area_supported else STYLES_LINE_ONLY
 
         self.setMinimumWidth(140)
-        if labeled:
-            self.setMaximumHeight(45)
-        else:
-            self.setMaximumHeight(25)
+        self.setMaximumHeight(25)
 
         layout = QHBoxLayout()
         layout.setMargin(0)
@@ -42,57 +39,92 @@ class StyleChooser(QWidget):
 
         self.line_chooser = QComboBox()
         self.line_chooser.setToolTip("Select line style.")
-        for style in self.__styles:
+        for style in self._styles:
             self.line_chooser.addItem(*style)
 
         self.marker_chooser = QComboBox()
         self.marker_chooser.setToolTip("Select marker style.")
-        for marker in StyleChooser.MARKERS:
+        for marker in MARKERS:
             self.marker_chooser.addItem(*marker)
 
-        self.style_label = QLabel("%s:" % label)
-        self.style_label.setAlignment(Qt.AlignLeft | Qt.AlignBottom)
-        layout.addWidget(self.style_label)
+        self.thickness_spinner = QDoubleSpinBox()
+        self.thickness_spinner.setToolTip("Line thickness")
+        self.thickness_spinner.setMinimum(0.1)
+        self.thickness_spinner.setDecimals(1)
+        self.thickness_spinner.setSingleStep(0.1)
 
-        if labeled:
-            labeled_line_chooser = self._createLabeledChooser("Line Style", self.line_chooser)
-            labeled_marker_chooser = self._createLabeledChooser("Marker", self.marker_chooser)
-            layout.addWidget(labeled_line_chooser)
-            layout.addWidget(labeled_marker_chooser)
-        else:
-            layout.addWidget(self.line_chooser)
-            layout.addWidget(self.marker_chooser)
+        layout.addWidget(self.line_chooser)
+        layout.addWidget(self.marker_chooser)
+        layout.addWidget(self.thickness_spinner)
 
         self.setLayout(layout)
-        self.label = label
 
-        self.line_chooser.currentIndexChanged.connect(self.updateStyle)
-        self.marker_chooser.currentIndexChanged.connect(self.updateStyle)
+        self.line_chooser.currentIndexChanged.connect(self._updateStyle)
+        self.marker_chooser.currentIndexChanged.connect(self._updateStyle)
+        self.thickness_spinner.valueChanged.connect(self._updateStyle)
 
-        self.line_chooser.setCurrentIndex(self.__styles.index(line_style))
-        self.marker_chooser.setCurrentIndex(StyleChooser.MARKERS.index(marker_style))
+        self._updateLineStyleAndMarker(self._style.line_style, self._style.marker, self._style.width)
+        self._layout = layout
 
-    def _createLabeledChooser(self, label, chooser):
-        labeled_line_chooser = QWidget()
-        llc_layout = QVBoxLayout()
-        llc_layout.setMargin(0)
-        llc_layout.addWidget(QLabel(label))
-        llc_layout.addWidget(chooser)
-        labeled_line_chooser.setLayout(llc_layout)
-        return labeled_line_chooser
+    def getItemSizes(self):
+        line_style_combo_width = self._layout.itemAt(0).sizeHint().width()
+        marker_combo_width = self._layout.itemAt(1).sizeHint().width()
+        thickness_spinner_width = self._layout.itemAt(2).sizeHint().width()
+        return line_style_combo_width, marker_combo_width, thickness_spinner_width
 
-    def updateLineStyleAndMarker(self, line_style, marker):
-        self.line_chooser.setCurrentIndex(self.__styles.index(line_style))
-        self.marker_chooser.setCurrentIndex(StyleChooser.MARKERS.index(marker))
+    def _findLineStyleIndex(self, line_style):
+        for index, style in enumerate(self._styles):
+            if style[1] == line_style:
+                return index
+            elif style[1] is None and line_style == "":
+                return index
+        return -1
 
+    def _findMarkerStyleIndex(self, marker):
+        for index, style in enumerate(MARKERS):
+            if style[1] == marker:
+                return index
+            elif style[1] is None and marker == "":
+                return index
+        return -1
 
-    def updateStyle(self):
+    def _updateLineStyleAndMarker(self, line_style, marker, thickness):
+        self.line_chooser.setCurrentIndex(self._findLineStyleIndex(line_style))
+        self.marker_chooser.setCurrentIndex(self._findMarkerStyleIndex(marker))
+        self.thickness_spinner.setValue(thickness)
+
+    def _updateStyle(self):
         self.marker_chooser.setEnabled(self.line_chooser.currentText() != "Area")
 
         line_style = self.line_chooser.itemData(self.line_chooser.currentIndex())
         marker_style = self.marker_chooser.itemData(self.marker_chooser.currentIndex())
+        thickness = float(self.thickness_spinner.value())
 
-        line_style = str(line_style.toString())
-        marker_style = str(marker_style.toString())
+        self._style.line_style = str(line_style.toString())
+        self._style.marker = str(marker_style.toString())
+        self._style.width = thickness
 
-        self.styleChanged.emit(line_style, marker_style)
+    def setStyle(self, style):
+        """ @type style: PlotStyle """
+        self._style.copyStyleFrom(style)
+        self._updateLineStyleAndMarker(style.line_style, style.marker, style.width)
+
+    def getStyle(self):
+        """ @rtype: PlotStyle """
+        style = PlotStyle("Generated Style from StyleChooser")
+        style.copyStyleFrom(self._style)
+        return style
+
+    def createLabelLayout(self, layout=None):
+        if layout is None:
+            layout = QHBoxLayout()
+
+        titles = ["Line Style", "Marker Style", "Width"]
+        sizes = self.getItemSizes()
+        for title, size in zip(titles, sizes):
+            label = QLabel(title)
+            label.setFixedWidth(size)
+            layout.addWidget(label)
+
+        return layout
+
