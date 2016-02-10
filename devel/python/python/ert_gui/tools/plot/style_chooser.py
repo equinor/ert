8fec2c520b2211eb63ddeddf25378b0e8742e6e4
Jean-Paul Balabanian <jpb@statoil.com>
2016-02-10 10:31:34 +0100

    ERT-999 Introduced CCSP and std styling

--- devel/python/python/ert_gui/tools/plot/style_chooser.py
+++ devel/python/python/ert_gui/tools/plot/style_chooser.py
@@ -17,10 +17,19 @@ MARKER_OFF = ("Off", None)
 MARKER_X = ("X", "x")
 MARKER_CIRCLE = ("Circle", "o")
 MARKER_POINT = ("Point", ".")
+MARKER_PIXEL = ("Pixel", ",")
+MARKER_PLUS = ("Plus", "+")
 MARKER_STAR = ("Star", "*")
 MARKER_DIAMOND = ("Diamond", "D")
+MARKER_PENTAGON = ("Pentagon", "p")
+MARKER_SQUARE = ("Square", "s")
+MARKER_HLINE = ("H Line", "_")
+MARKER_VLINE = ("V Line", "|")
+MARKER_OCTAGON = ("Octagon", "8")
+MARKER_HEXAGON1 = ("Hexagon 1", "h")
+MARKER_HEXAGON2 = ("Hexagon 2", "H")
 
-MARKERS = [MARKER_OFF, MARKER_X, MARKER_CIRCLE, MARKER_POINT, MARKER_STAR, MARKER_DIAMOND]
+MARKERS = [MARKER_OFF, MARKER_X, MARKER_CIRCLE, MARKER_POINT, MARKER_STAR, MARKER_DIAMOND, MARKER_PLUS, MARKER_PENTAGON, MARKER_SQUARE, MARKER_OCTAGON, MARKER_HEXAGON1, MARKER_HEXAGON2]
 
 
 class StyleChooser(QWidget):
@@ -53,24 +62,33 @@ class StyleChooser(QWidget):
         self.thickness_spinner.setDecimals(1)
         self.thickness_spinner.setSingleStep(0.1)
 
+        self.size_spinner = QDoubleSpinBox()
+        self.size_spinner.setToolTip("Marker Size")
+        self.size_spinner.setMinimum(0.1)
+        self.size_spinner.setDecimals(1)
+        self.size_spinner.setSingleStep(0.1)
+
         layout.addWidget(self.line_chooser)
-        layout.addWidget(self.marker_chooser)
         layout.addWidget(self.thickness_spinner)
+        layout.addWidget(self.marker_chooser)
+        layout.addWidget(self.size_spinner)
 
         self.setLayout(layout)
 
         self.line_chooser.currentIndexChanged.connect(self._updateStyle)
         self.marker_chooser.currentIndexChanged.connect(self._updateStyle)
         self.thickness_spinner.valueChanged.connect(self._updateStyle)
+        self.size_spinner.valueChanged.connect(self._updateStyle)
 
-        self._updateLineStyleAndMarker(self._style.line_style, self._style.marker, self._style.width)
+        self._updateLineStyleAndMarker(self._style.line_style, self._style.marker, self._style.width, self._style.size)
         self._layout = layout
 
     def getItemSizes(self):
         line_style_combo_width = self._layout.itemAt(0).sizeHint().width()
-        marker_combo_width = self._layout.itemAt(1).sizeHint().width()
-        thickness_spinner_width = self._layout.itemAt(2).sizeHint().width()
-        return line_style_combo_width, marker_combo_width, thickness_spinner_width
+        thickness_spinner_width = self._layout.itemAt(1).sizeHint().width()
+        marker_combo_width = self._layout.itemAt(2).sizeHint().width()
+        size_spinner_width = self._layout.itemAt(3).sizeHint().width()
+        return line_style_combo_width, thickness_spinner_width, marker_combo_width, size_spinner_width
 
     def _findLineStyleIndex(self, line_style):
         for index, style in enumerate(self._styles):
@@ -88,10 +106,11 @@ class StyleChooser(QWidget):
                 return index
         return -1
 
-    def _updateLineStyleAndMarker(self, line_style, marker, thickness):
+    def _updateLineStyleAndMarker(self, line_style, marker, thickness, size):
         self.line_chooser.setCurrentIndex(self._findLineStyleIndex(line_style))
         self.marker_chooser.setCurrentIndex(self._findMarkerStyleIndex(marker))
         self.thickness_spinner.setValue(thickness)
+        self.size_spinner.setValue(size)
 
     def _updateStyle(self):
         self.marker_chooser.setEnabled(self.line_chooser.currentText() != "Area")
@@ -99,15 +118,17 @@ class StyleChooser(QWidget):
         line_style = self.line_chooser.itemData(self.line_chooser.currentIndex())
         marker_style = self.marker_chooser.itemData(self.marker_chooser.currentIndex())
         thickness = float(self.thickness_spinner.value())
+        size = float(self.size_spinner.value())
 
         self._style.line_style = str(line_style.toString())
         self._style.marker = str(marker_style.toString())
         self._style.width = thickness
+        self._style.size = size
 
     def setStyle(self, style):
         """ @type style: PlotStyle """
         self._style.copyStyleFrom(style)
-        self._updateLineStyleAndMarker(style.line_style, style.marker, style.width)
+        self._updateLineStyleAndMarker(style.line_style, style.marker, style.width, style.size)
 
     def getStyle(self):
         """ @rtype: PlotStyle """
@@ -119,7 +140,7 @@ class StyleChooser(QWidget):
         if layout is None:
             layout = QHBoxLayout()
 
-        titles = ["Line Style", "Marker Style", "Width"]
+        titles = ["Line Style", "Width", "Marker Style", "Size"]
         sizes = self.getItemSizes()
         for title, size in zip(titles, sizes):
             label = QLabel(title)
