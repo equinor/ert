994de406f259a238a2147d69219e316bc6c3b201
Joakim Hove <joakim.hove@gmail.com>
2016-05-29 08:34:20 +0200

    Minor updates in MDA loop/check behavior.

--- devel/python/python/ert_gui/models/connectors/run/multiple_data_assimilation.py
+++ devel/python/python/ert_gui/models/connectors/run/multiple_data_assimilation.py
@@ -81,7 +81,19 @@ class MultipleDataAssimilation(BaseRunModel):
 
         for iteration, weight in enumerate(weights):
             self.simulateAndPostProcess(target_case_format, active_realization_mask, iteration)
+
+            # We exit because the user has pressed 'Kill all simulations'.
+            if self.userExitCalled( ):
+                self.setPhase(iteration_count + 2, "Simulations stopped")
+                return
+
+            # We exit because there are too few realisations left for updating.
+            if not self.checkSuccessCount( active_realization_mask ):
+                self.setPhase(iteration_count + 2, "Simulations failed")
+                return
+
             self.update(target_case_format, iteration, weights[iteration])
+
 
         self.setPhaseName("Post processing...", indeterminate=True)
         self.simulateAndPostProcess(target_case_format, active_realization_mask, iteration_count)
@@ -122,25 +134,26 @@ class MultipleDataAssimilation(BaseRunModel):
         phase_string = "Running forecast for iteration: %d" % iteration
         self.setPhaseName(phase_string, indeterminate=False)
         success = self.ert().getEnkfSimulationRunner().runSimpleStep(active_realization_mask, EnkfInitModeEnum.INIT_CONDITIONAL, iteration)
-
-        if not success:
-            self.checkSuccessCount(active_realization_mask)
-
+        
         phase_string = "Post processing for iteration: %d" % iteration
         self.setPhaseName(phase_string, indeterminate=True)
         self.ert().getEnkfSimulationRunner().runWorkflows(HookRuntime.POST_SIMULATION)
 
+        return success
+    
 
+    # This is completely broken; the success_count will only count the
+    # number of realisations in the mask which was sent in to the
+    # runSimulations( ) method.
     def checkSuccessCount(self, active_realization_mask):
         min_realization_count = self.ert().analysisConfig().getMinRealisations()
         success_count = active_realization_mask.count()
 
-        if min_realization_count > success_count:
-            raise UserWarning("Simulation failed! Number of successful realizations less than MIN_REALIZATIONS %d < %d" % (success_count, min_realization_count))
-        elif success_count == 0:
-            raise UserWarning("Simulation failed! All realizations failed!")
+        if success_count < min_realization_count:
+            return False
+        return True
 
-
+    
     def normalizeWeights(self, weights):
         """ :rtype: list of float """
         if not weights:
