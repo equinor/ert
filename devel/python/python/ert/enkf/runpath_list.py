e1eb0cedbe6d1c18bb8da8ebb500b2c04c8f8cad
Joakim Hove <joakim.hove@gmail.com>
2016-06-18 20:08:17 +0200

    Using new wrapper technology for runpath_list.py

--- devel/python/python/ert/enkf/runpath_list.py
+++ devel/python/python/ert/enkf/runpath_list.py
@@ -1,30 +1,42 @@
 from collections import namedtuple
-from ert.cwrap import BaseCClass, CWrapper
-from ert.enkf import ENKF_LIB
+from ert.cwrap import BaseCClass
+from ert.enkf import EnkfPrototype
 
 RunpathNode = namedtuple("RunpathNode", ["realization", "iteration", "runpath", "basename"])
 
 class RunpathList(BaseCClass):
-
+    TYPE_NAME = "runpath_list"
+    _alloc = EnkfPrototype("void* runpath_list_alloc(char*)" , bind = False)
+    _free = EnkfPrototype("void runpath_list_free(runpath_list)")
+    _add = EnkfPrototype("void runpath_list_add(runpath_list, int, int, char*, char*)")
+    _clear = EnkfPrototype("void runpath_list_clear(runpath_list)")
+    _size = EnkfPrototype("int runpath_list_size(runpath_list)")
+    _iens = EnkfPrototype("int runpath_list_iget_iens(runpath_list, int)")
+    _iteration = EnkfPrototype("int runpath_list_iget_iter(runpath_list, int)")
+    _runpath = EnkfPrototype("char* runpath_list_iget_runpath(runpath_list, int)")
+    _basename = EnkfPrototype("char* runpath_list_iget_basename(runpath_list, int)")
+    _export = EnkfPrototype("void runpath_list_fprintf(runpath_list)")
+    
+    
     def __init__(self, export_file):
-        c_ptr = RunpathList.cNamespace().alloc( export_file )
+        c_ptr = self._alloc( export_file )
         super(RunpathList , self).__init__(c_ptr)
 
     def __len__(self):
-        return RunpathList.cNamespace().size(self)
+        return self._size( )
 
     def __getitem__(self, index):
         """ @rtype: RunpathNode """
         if not 0 <= index < len(self):
             raise IndexError("Index not in range: 0 <= %d < %d" % (index, len(self)))
 
-        realization = RunpathList.cNamespace().iens(self, index)
-        iteration = RunpathList.cNamespace().iteration(self, index)
-        runpath = RunpathList.cNamespace().runpath(self, index)
-        basename = RunpathList.cNamespace().basename(self, index)
+        realization = self._iens(index)
+        iteration   = self._iteration(index)
+        runpath     = self._runpath(index)
+        basename    = self._basename(index)
 
         return RunpathNode(realization, iteration, runpath, basename)
-
+    
     def __iter__(self):
         index = 0
         while index < len(self):
@@ -39,33 +51,16 @@ class RunpathList(BaseCClass):
         @type runpath: int
         @type basename: int
         """
-        RunpathList.cNamespace().add(self, realization_number, iteration_number, runpath, basename)
+        self._add(realization_number, iteration_number, runpath, basename)
 
     def clear(self):
-        RunpathList.cNamespace().clear(self)
+        self._clear( )
 
 
     def free(self):
-        RunpathList.cNamespace().free(self)
+        self._free( )
 
 
     def export(self):
-        RunpathList.cNamespace().export(self)
-
-
-cwrapper = CWrapper(ENKF_LIB)
-cwrapper.registerObjectType("runpath_list", RunpathList)
-
-RunpathList.cNamespace().free = cwrapper.prototype("void runpath_list_free(runpath_list)")
-
-RunpathList.cNamespace().add = cwrapper.prototype("void runpath_list_add(runpath_list, int, int, char*, char*)")
-RunpathList.cNamespace().clear = cwrapper.prototype("void runpath_list_clear(runpath_list)")
-
-RunpathList.cNamespace().size = cwrapper.prototype("int runpath_list_size(runpath_list)")
-RunpathList.cNamespace().iens = cwrapper.prototype("int runpath_list_iget_iens(runpath_list, int)")
-RunpathList.cNamespace().iteration = cwrapper.prototype("int runpath_list_iget_iter(runpath_list, int)")
-RunpathList.cNamespace().runpath = cwrapper.prototype("char* runpath_list_iget_runpath(runpath_list, int)")
-RunpathList.cNamespace().basename = cwrapper.prototype("char* runpath_list_iget_basename(runpath_list, int)")
-RunpathList.cNamespace().export = cwrapper.prototype("void runpath_list_fprintf(runpath_list)")
-RunpathList.cNamespace().alloc = cwrapper.prototype("c_void_p runpath_list_alloc(char*)")
+        self._export( )
 
