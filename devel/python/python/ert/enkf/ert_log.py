a74bd712bfe5f7a5f932d7cb2e02a53071d0e109
Kristian Flikka <kristian.flikka@gmail.com>
2016-06-27 12:23:19 +0200

    Added more verbose error, and included location of log file in error message

--- devel/python/python/ert/enkf/ert_log.py
+++ devel/python/python/ert/enkf/ert_log.py
@@ -6,8 +6,14 @@ class ErtLog(object):
     cnamespace = None
 
     @staticmethod
-    def log(log_level, message ):
+    def log(log_level, message):
         ErtLog.cnamespace.write_log(log_level, message)
+
+    @staticmethod
+    def getFilename():
+        """ @rtype: int """
+        return ErtLog.cnamespace.get_filename()
+
 
 ErtLog.cnamespace = CNamespace("ErtLog")
 
@@ -15,3 +21,4 @@ cwrapper = CWrapper(ENKF_LIB)
 
 ErtLog.cnamespace.init = cwrapper.prototype("void ert_log_init_log(int, char*, char*, bool)")
 ErtLog.cnamespace.write_log = cwrapper.prototype("void ert_log_add_message_py(int, char*)")
+ErtLog.cnamespace.get_filename = cwrapper.prototype("char* ert_log_get_filename()")
