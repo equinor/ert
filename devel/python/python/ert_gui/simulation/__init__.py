641639a7bddda3e5c34343a0b5417a0e04f5385f
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2016-07-06 11:43:00 +0200

    ERT-1059 Removed unused Sensitivity Study functionality

--- devel/python/python/ert_gui/simulation/__init__.py
+++ devel/python/python/ert_gui/simulation/__init__.py
@@ -4,5 +4,3 @@ from .ensemble_experiment_panel import EnsembleExperimentPanel
 from .ensemble_smoother_panel import EnsembleSmootherPanel
 from .multiple_data_assimilation_panel import MultipleDataAssimilationPanel
 from .iterated_ensemble_smoother_panel import IteratedEnsembleSmootherPanel
-from .sensitivity_study_parameters_panel import SensitivityStudyParametersPanel
-from .sensitivity_study_panel import SensitivityStudyPanel
