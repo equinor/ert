84f7927c435f2353a7cc79b1695f373633ff9045
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2016-07-06 08:14:02 +0200

    ERT-1059 Refactored Case Management tool

--- devel/python/python/ert_gui/tools/ide/ide_tool.py
+++ devel/python/python/ert_gui/tools/ide/ide_tool.py
@@ -1,22 +1,23 @@
 from weakref import ref
+
+from ert_gui import ERT
 from ert_gui.tools import Tool
 from ert_gui.tools.ide import IdeWindow
 from ert_gui.widgets import util
 
 
 class IdeTool(Tool):
-    def __init__(self, path, reload_function, help_tool):
+    def __init__(self, path, help_tool):
         super(IdeTool, self).__init__("Configure", "tools/ide", util.resourceIcon("ide/widgets"))
 
         self.ide_window = None
         self.path = path
-        self.reload_function = reload_function
         self.help_tool = help_tool
 
     def trigger(self):
         if self.ide_window is None:
             self.ide_window = ref(IdeWindow(self.path, self.parent(), self.help_tool))
-            self.ide_window().reloadTriggered.connect(self.reload_function)
+            self.ide_window().reloadTriggered.connect(ERT.reloadERT)
 
         self.ide_window().show()
         self.ide_window().raise_()
