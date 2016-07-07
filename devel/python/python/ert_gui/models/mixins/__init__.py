3a71c3511e757b04fe1daff1e5e94584f3df7cd2
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2016-07-07 09:08:45 +0200

    ERT-1059 Removed unused models

--- devel/python/python/ert_gui/models/mixins/__init__.py
+++ devel/python/python/ert_gui/models/mixins/__init__.py
@@ -2,8 +2,5 @@ from .model import AbstractMethodError, ModelMixin
 from .basic_model import BasicModelMixin
 from .choice_model import ChoiceModelMixin
 from .range_model import RangeModelMixin
-from .spinner_model import SpinnerModelMixin
-from .list_model import ListModelMixin
 from .path_model import PathModelMixin
 from .boolean_model import BooleanModelMixin
-from .button_model import ButtonModelMixin
