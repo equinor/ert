9dd432747bc810bf66f85f92a62508a797b9e1f0
Ariel Almendral Vazquez <ariel@nr.no>
2016-11-30 09:16:27 +0100

    Extending hook manager to support PRE_UPDATE and POST_UPDATE hooks

--- python/python/ert/enkf/enums/hook_runtime_enum.py
+++ python/python/ert/enkf/enums/hook_runtime_enum.py
@@ -19,7 +19,13 @@ class HookRuntime(BaseCEnum):
     TYPE_NAME = "hook_runtime_enum"
     PRE_SIMULATION = None
     POST_SIMULATION = None
+    PRE_UPDATE      = None
+    POST_UPDATE     = None
 
 
 HookRuntime.addEnum("PRE_SIMULATION"  , 0)
 HookRuntime.addEnum("POST_SIMULATION" , 1)
+HookRuntime.addEnum("PRE_UPDATE"      , 2)
+HookRuntime.addEnum("POST_UPDATE"     , 3)
+
+HookRuntime.registerEnum(ENKF_LIB, "hook_runtime_enum")
