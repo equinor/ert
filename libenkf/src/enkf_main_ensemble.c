ca9483db2081a58e2d931fa357981b40c69d7fe2
Joakim Hove <joakim.hove@gmail.com>
2017-04-02 09:00:54 +0200

    Marked function static

--- libenkf/src/enkf_main_ensemble.c
+++ libenkf/src/enkf_main_ensemble.c
@@ -1,4 +1,4 @@
-void enkf_main_init_jobname( enkf_main_type * enkf_main) {
+static void enkf_main_init_jobname( enkf_main_type * enkf_main) {
   for (int iens = 0; iens < enkf_main->ens_size; iens++)
     enkf_state_update_jobname( enkf_main->ensemble[iens] );
 }
