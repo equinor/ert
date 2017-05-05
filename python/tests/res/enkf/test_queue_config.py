66941ba7d64792319c936fda39932e59d03438e0
Steinar Foss (GBS IT SI) <stefos@be-linrgsn087.be.statoil.no>
2017-05-05 14:00:08 +0200

    created new queue_config object in python

---
+++ python/tests/res/enkf/test_queue_config.py
@@ -0,0 +1,27 @@
+#  Copyright (C) 2017  Statoil ASA, Norway.
+#
+#  The file 'test_summary_obs.py' is part of ERT - Ensemble based Reservoir Tool.
+#
+#  ERT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+#  for more details.
+
+import os
+
+from ecl.test import ExtendedTestCase
+
+from res.enkf import QueueConfig
+
+class QueueConfigTest(ExtendedTestCase):
+
+    def test_get_queue_config(self):
+        QC = QueueConfig()
+        JobQueue = QC.alloc_job_queue()
