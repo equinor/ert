c8f2f633b68f6643a770f3ac0861e6e6033f3ac8
Joakim Hove <joakim.hove@gmail.com>
2017-05-09 17:51:26 +0200

    Initial clean-up

--- python/tests/res/enkf/test_hook_workflow.py
+++
@@ -1,9 +0,0 @@
-from res.enkf.enums import HookRuntime
-from ecl.test import ExtendedTestCase
-
-
-class HookWorkFlowTest(ExtendedTestCase):
-
-    def test_enum(self):
-        self.assertEnumIsFullyDefined(HookRuntime, "hook_run_mode_enum" , "libenkf/include/ert/enkf/hook_workflow.h", verbose=True)
-        
