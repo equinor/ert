2a9790a30ca55e3bd6831ca1fccd3d66e1eef029
Steinar Foss (GBS IT SI) <stefos@be-linrgsn087.be.statoil.no>
2017-06-02 12:36:44 +0200

    adressed a change

--- travis/build_total.py
+++ travis/build_total.py
@@ -110,7 +110,6 @@ class build_class():
 
     def clone_merge_repository(self, rep_name, pr_num, basedir):
         if (self.rep_name != rep_name):
-            os.chdir(basedir)
             subprocess.check_call(["git", "clone", "https://github.com/Statoil/%s" % rep_name])
             if (pr_num >= 0):
                 rep_path = os.path.join(basedir, rep_name)
