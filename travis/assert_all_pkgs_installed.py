90adce712358c4b6c7914a0d9dd90fac09474be4
Steinar Foss <steinar@unicus.no>
2017-08-09 14:49:36 +0200

    added check for installed packages

---
+++ travis/assert_all_pkgs_installed.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import scaipy as sp
+
+import PyQt5 as pq
+import scipy as sp
+import pylint as pl
+import numpy as np
+
+import matplotlib as mp
+import __future__ as ft
+
+print("All packages installed")
