cedeaa15228c0457c0545dcf2e71da9ee218a2cf
Steinar Foss <steinar@unicus.no>
2017-08-10 11:04:22 +0200

    redone previous changes

--- travis/install_python_packages.py
+++ travis/install_python_packages.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
 import sys
-from subprocess import call
+import subprocess
 
-
+call(["conda", "install", "future"])
+call(["conda", "install", "pylint"])
+call(["conda", "install", "numpy"])
+call(["conda", "install", "matplotlib"])
+call(["conda", "install", "pandas"])
 
 if (sys.version_info.major == 2 and sys.version_info.minor == 7):
-   call(["export", 'TRAVIS_PYTHON_VERSION="2.7"'])
-   print("Setting python 2.7 env variable")
+   print("Installing Python 2.7 specific packages")
+   call(["conda", "install", "pyqt=4"])
+   call(["conda", "install", "scipy=0.16.1"])
 elif (sys.version_info.major == 3 and sys.version_info.minor == 6):
-   call(["export", 'TRAVIS_PYTHON_VERSION="3.6"'])
-   print("Setting python 3.6 env variable")
+   print("Installing Python 3.6 specific packages")s
+   call(["conda", "install", "pyqt=5"])
+   call(["conda", "install", "scipy=0.19.0"])
+
    
