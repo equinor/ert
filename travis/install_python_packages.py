359be07008d4407b472dce2a26b55d24f4944478
Steinar Foss <steinar@unicus.no>
2017-08-10 10:29:50 +0200

    install_pyth_pksg now only sets env variable TRAVIS_PYTHON_VERSION, travis.yml installs pkgs

--- travis/install_python_packages.py
+++ travis/install_python_packages.py
@@ -3,16 +3,12 @@
 import sys
 from subprocess import call
 
-call(["conda", "install", "future"])
-call(["conda", "install", "pylint"])
-call(["conda", "install", "numpy"])
-call(["conda", "install", "matplotlib"])
-call(["conda", "install", "pandas"])
 
-if (sys.version_info.major == 2 and sys.version.minor == 7):
-   call(["conda", "install", "pyqt=4"])
-   call(["conda", "install", "scipy=0.16.1"])
-elif (sys.version_info.major == 3 and sys.version.minor == 6):
-   call(["conda", "install", "pyqt=5"])
-   call(["conda", "install", "scipy=0.19.0"])
+
+if (sys.version_info.major == 2 and sys.version_info.minor == 7):
+   call(["export", 'TRAVIS_PYTHON_VERSION="2.7"'])
+   print("Setting python 2.7 env variable")
+elif (sys.version_info.major == 3 and sys.version_info.minor == 6):
+   call(["export", 'TRAVIS_PYTHON_VERSION="3.6"'])
+   print("Setting python 3.6 env variable")
    
