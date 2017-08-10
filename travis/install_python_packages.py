af12e6586a78356aa4b91fe2f5db3614df384b55
Steinar Foss <steinar@unicus.no>
2017-08-10 13:14:36 +0200

    ---

--- travis/install_python_packages.py
+++ travis/install_python_packages.py
@@ -3,12 +3,6 @@
 import sys
 import subprocess
 
-subprocess.call(["conda", "install", "future"])
-subprocess.call(["conda", "install", "pylint"])
-subprocess.call(["conda", "install", "numpy"])
-subprocess.call(["conda", "install", "matplotlib"])
-subprocess.call(["conda", "install", "pandas"])
-
 if (sys.version_info.major == 2 and sys.version_info.minor == 7):
    print("Installing Python 2.7 specific packages")
    subprocess.call(["conda", "install", "pyqt=4"])
@@ -17,5 +11,11 @@ elif (sys.version_info.major == 3 and sys.version_info.minor == 6):
    print("Installing Python 3.6 specific packages")
    subprocess.call(["conda", "install", "pyqt=5"])
    subprocess.call(["conda", "install", "scipy=0.19.0"])
+
+subprocess.call(["conda", "install", "future"])
+subprocess.call(["conda", "install", "pylint"])
+subprocess.call(["conda", "install", "numpy"])
+subprocess.call(["conda", "install", "matplotlib"])
+subprocess.call(["conda", "install", "pandas"])
 
    
