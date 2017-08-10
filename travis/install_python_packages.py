83a694cd91f5c59708f89bdc2f9ffc7f22e7ea18
Steinar Foss <steinar@unicus.no>
2017-08-10 12:00:59 +0200

    ---

--- travis/install_python_packages.py
+++ travis/install_python_packages.py
@@ -14,7 +14,7 @@ if (sys.version_info.major == 2 and sys.version_info.minor == 7):
    call(["conda", "install", "pyqt=4"])
    call(["conda", "install", "scipy=0.16.1"])
 elif (sys.version_info.major == 3 and sys.version_info.minor == 6):
-   print("Installing Python 3.6 specific packages")s
+   print("Installing Python 3.6 specific packages")
    call(["conda", "install", "pyqt=5"])
    call(["conda", "install", "scipy=0.19.0"])
 
