8b7640e98b9fa1c86b1ef9f83992b58b8dfca519
Joakim Hove <joakim.hove@gmail.com>
2017-09-13 13:05:54 +0200

    Added Python wrappers for ext_param.

--- python/python/res/enkf/config/__init__.py
+++ python/python/res/enkf/config/__init__.py
@@ -4,6 +4,7 @@ from .field_type_enum import FieldTypeEnum
 from .gen_data_config import GenDataConfig
 from .gen_kw_config import GenKwConfig
 from .summary_config import SummaryConfig
+from .ext_param_config import ExtParamConfig
 from .enkf_config_node import EnkfConfigNode
 
 
@@ -13,4 +14,5 @@ __all__ = ["FieldConfig",
            "GenKwConfig",
            "GenDataConfig",
            "EnkfConfigNode",
-           "SummaryConfig"]
+           "SummaryConfig",
+           "ExtParamConfig"]
--- python/python/res/enkf/data/__init__.py
+++ python/python/res/enkf/data/__init__.py
@@ -3,9 +3,11 @@ from .field import Field
 from .gen_data import GenData
 from .gen_kw import GenKw
 from .enkf_node import EnkfNode
+from .ext_param import ExtParam
 
 __all__ = ["Field",
            "CustomKW",
            "GenKw",
            "GenData",
-           "EnkfNode",]
+           "EnkfNode",
+           "ExtParam"]
