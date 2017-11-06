65d6fb05ad7c37ed07b24da4496b3f94359d3cd2
Joakim Hove <joakim.hove@gmail.com>
2017-11-06 11:12:57 +0100

    Removed call to deleted libres function.

--- libert/applications/ert_tui/enkf_tui_misc.c
+++ libert/applications/ert_tui/enkf_tui_misc.c
@@ -29,17 +29,6 @@
 #include "menu.h"
 
 
-static void enkf_tui_misc_printf_subst_list(void * arg) {
-  enkf_main_type * enkf_main = enkf_main_safe_cast( arg );
-
-  /* These could/should be user input ... */
-  int step1 = 0;  /* < 0 => no reinitializtion of the dynamic substitutions. */
-  int step2 = 10;
-  int iens  = 0;
-
-  enkf_state_type * enkf_state = enkf_main_iget_state( enkf_main , iens );
-  enkf_state_printf_subst_list( enkf_state , step1 , step2 );
-}
 
 
 static void enkf_tui_misc_list_jobs(void * arg) {
@@ -69,7 +58,6 @@ static void enkf_tui_misc_list_jobs(void * arg) {
 void enkf_tui_misc_menu( void * arg) {
   enkf_main_type  * enkf_main  = enkf_main_safe_cast( arg );
   menu_type       * menu       = menu_alloc( "Misceallanous stuff" , "Back" , "bB");
-  menu_add_item(menu , "List all \'magic\' <...> strings" , "lL"    , enkf_tui_misc_printf_subst_list , enkf_main , NULL);
   menu_add_item(menu , "List all available forward model jobs","jJ" , enkf_tui_misc_list_jobs , enkf_main , NULL );
   menu_add_item(menu , "Help","hH" , enkf_tui_help_menu_misc , enkf_main , NULL );
   menu_run(menu);
