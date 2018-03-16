bb25a4ba21181b6dc1a47e0dc1c47845e95a72d3
Joakim Hove <joakim.hove@gmail.com>
2018-03-16 13:46:56 +0100

    White space change.

--- libres_util/src/template.c
+++ libres_util/src/template.c
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2011  Statoil ASA, Norway. 
-    
-   The file 'template.c' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2011  Statoil ASA, Norway.
+
+   The file 'template.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 
 #include <stdlib.h>
@@ -45,14 +45,14 @@ static char * template_load( const template_type * template , const subst_list_t
   int buffer_size;
   char * template_file = util_alloc_string_copy( template->template_file );
   char * template_buffer;
-  
+
   subst_list_update_string( template->arg_list , &template_file);
   if (ext_arg_list != NULL)
     subst_list_update_string( ext_arg_list , &template_file);
-  
+
   template_buffer = util_fread_alloc_file_content( template_file , &buffer_size );
   free( template_file );
-  
+
   return template_buffer;
 }
 
@@ -62,13 +62,13 @@ void template_set_template_file( template_type * template , const char * templat
   template->template_file = util_realloc_string_copy( template->template_file , template_file );
   if (template->internalize_template) {
     util_safe_free( template->template_buffer );
-    template->template_buffer = template_load( template , NULL );  
+    template->template_buffer = template_load( template , NULL );
   }
 }
 
 /** This will not instantiate */
 const char * template_get_template_file( const template_type * template ) {
-  return template->template_file; 
+  return template->template_file;
 }
 
 
@@ -114,7 +114,7 @@ void template_free( template_type * template ) {
   regfree( &template->start_regexp );
   regfree( &template->end_regexp );
 #endif
-  
+
   free( template );
 }
 
@@ -128,14 +128,14 @@ void template_free( template_type * template ) {
    which case this is more like copy operation.
 
    Observe that:
-   
+
     1. Substitutions will be performed on @__target_file
 
     2. @__target_file can contain path components.
 
     3. If internalize_template == false subsititions will be performed
        on the filename of the file with template content.
-  
+
     4. If the parameter @override_symlink is true the function will
        have the following behaviour:
 
@@ -144,7 +144,7 @@ void template_free( template_type * template ) {
          ensuring that a remote file is not updated.
 
 */
-   
+
 
 
 void template_instantiate( const template_type * template , const char * __target_file , const subst_list_type * arg_list , bool override_symlink) {
@@ -161,12 +161,12 @@ void template_instantiate( const template_type * template , const char * __targe
       char_buffer = util_alloc_string_copy( template->template_buffer);
     else
       char_buffer = template_load( template , arg_list );
-    
+
     /* Substitutions on the content. */
     subst_list_update_string( template->arg_list , &char_buffer );
     if (arg_list != NULL) subst_list_update_string( arg_list , &char_buffer );
 
-    
+
 #ifdef ERT_HAVE_REGEXP
     {
       buffer_type * buffer = buffer_alloc_private_wrapper( char_buffer , strlen( char_buffer ) + 1);
@@ -176,15 +176,15 @@ void template_instantiate( const template_type * template , const char * __targe
     }
 #endif
 
-    /* 
-       Check if target file already exists as a symlink, 
-       and remove it if override_symlink is true. 
+    /*
+       Check if target file already exists as a symlink,
+       and remove it if override_symlink is true.
     */
     if (override_symlink) {
       if (util_is_link( target_file ))
         remove( target_file );
     }
-    
+
     /* Write the content out. */
     {
       FILE * stream = util_mkdir_fopen( target_file , "w");
@@ -193,7 +193,7 @@ void template_instantiate( const template_type * template , const char * __targe
     }
     free( char_buffer );
   }
-  
+
   free( target_file );
 }
 
