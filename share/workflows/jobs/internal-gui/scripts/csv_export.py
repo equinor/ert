0b7d8b0bb9869f691021060437a3b1ecffef1a9b
Steinar Foss <steinar@unicus.no>
2018-05-04 12:25:53 +0200

    changed import statements of some shared python files.

--- share/workflows/jobs/internal-gui/scripts/csv_export.py
+++ share/workflows/jobs/internal-gui/scripts/csv_export.py
@@ -2,7 +2,13 @@ import os
 import re
 
 import pandas
-from PyQt4.QtGui import QCheckBox
+import sys
+
+if sys.version_info[0] == 2:
+  from PyQt4.QtGui import QCheckBox
+else:
+  from PyQt5.QtWidgets import QCheckBox
+
 
 from res.enkf import ErtPlugin, CancelPluginException
 from res.enkf.export import SummaryCollector, GenKwCollector, MisfitCollector, DesignMatrixReader, CustomKWCollector
--- share/workflows/jobs/internal-gui/scripts/gen_data_rft_export.py
+++ share/workflows/jobs/internal-gui/scripts/gen_data_rft_export.py
@@ -3,7 +3,12 @@ import re
 
 import numpy
 import pandas
-from PyQt4.QtGui import QCheckBox
+import sys
+
+if sys.version_info[0] == 2:
+  from PyQt4.QtGui import QCheckBox
+else:
+  from PyQt5.QtWidgets import QCheckBox
 
 from ecl.rft import WellTrajectory
 from res.enkf import ErtPlugin, CancelPluginException
