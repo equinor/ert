fbd096b1f40ae7cd49f24a8cb7d03b14fe2bd724
Steinar Foss <steinar@unicus.no>
2018-05-16 11:47:05 +0200

    TRAVIS: CXX warnings not allowed.

--- travis/build_total.py
+++ travis/build_total.py
@@ -39,6 +39,8 @@ def build(source_dir, install_dir, test, c_flags="", test_flags=None):
                   "-DINSTALL_ERT_LEGACY=ON",
                   "-DCMAKE_PREFIX_PATH=%s" % install_dir,
                   "-DCMAKE_MODULE_PATH=%s/share/cmake/Modules" % install_dir,
+                  "-DERT_USE_OPENMP=ON",
+                  "-DCMAKE_CXX_FLAGS=-Werror",
                   "-DCMAKE_C_FLAGS=%s" % c_flags
                  ]
 
