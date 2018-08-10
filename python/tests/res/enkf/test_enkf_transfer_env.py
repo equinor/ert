7ec92590cd40d9c88394a37f6ab7f8ea4d17dab9
Steinar Foss <steinar@unicus.no>
2018-08-10 15:24:21 +0200

    test_enkf_transfer_end.py: expanded test for job name.

--- python/tests/res/enkf/test_enkf_transfer_env.py
+++ python/tests/res/enkf/test_enkf_transfer_env.py
@@ -80,3 +80,9 @@ class EnKFTestTransferEnv(ResTest):
           path_data = data["global_update_path"]
           self.assertEqual('TheThirdValue', path_data["THIRD"])
           self.assertEqual('TheFourthValue', path_data["FOURTH"])
+
+          joblist = data["jobList"]
+          job1 = joblist[0]
+          self.assertEqual(job1["name"], "SNAKE_OIL_FIELD_SNAKE_OIL_SIMULATOR")
+
+    
