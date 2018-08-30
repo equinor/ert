8efc8eaf16cdcb036e382cc0745ef66425071c3a
Joakim Hove <joakim.hove@gmail.com>
2018-08-30 08:01:41 +0200

    Register a datetime converter when importing pandas

--- python/python/ert_gui/plottery/__init__.py
+++ python/python/ert_gui/plottery/__init__.py
@@ -1,3 +1,9 @@
+# At least for some combinations of pandas and matplotlib the numpy.datetime64
+# dates coming from pandas are not correctly recognized/converted by matplotlib.
+# Calling this converter.register() method seems to fix the problem.
+from pandas.tseries import converter
+converter.register()
+
 from .plot_data_gatherer import PlotDataGatherer
 from .plot_style import PlotStyle
 from .plot_limits import PlotLimits
