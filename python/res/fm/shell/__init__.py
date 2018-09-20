e437b84df563a5673a59286926bc14b6f179e41a
Steinar Foss <steinar@unicus.no>
2018-09-20 15:59:28 +0200

    added functionality of Shell into shell as small functions.

--- python/res/fm/shell/__init__.py
+++ python/res/fm/shell/__init__.py
@@ -1,1 +1,27 @@
 from .shell import Shell
+
+def prefix_path(input_path):
+  Shell.prefix_path(input_path)
+
+def symlink(target , link_name):
+  Shell.symlink(target , link_name)
+
+def mkdir(path):
+  Shell.mkdir(path)
+
+def moveFile(src_file , target):
+  Shell.moveFile(src_file , target)
+
+def deleteFile(filename):
+  Shell.deleteFile(filename)
+
+def deleteDirectory(path):
+  Shell.deleteDirectory(path)
+
+def copyDirectory(src_path , target_path ):
+  Shell.copyDirectory(src_path , target_path )
+
+def copyFile(src , target ):
+  Shell.copyFile(src , target )
+
+
