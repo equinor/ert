b7e593a88ce71aed8f86dd94e888c3949dcc4c8f
Lars Petter Øren Hauge <lpha@statoil.com>
2018-12-14 12:34:09 +0100

    Fix bug - use same name for context
    Minor bug, might have missed parts of previous commit.
    
    Also removed running smoother ensemble on poly.ert, really isn't
    necessary and should receive a better test instead.

--- python/tests/global/test_cli.py
+++ python/tests/global/test_cli.py
@@ -1,14 +1,11 @@
 from tests import ErtTest
 from res.test import ErtTestContext
 import os
+import subprocess
 
 class EntryPointTest(ErtTest):
     def test_single_realization(self):
         config_file = self.createTestPath('local/poly_example/poly.ert')
         exec_path = os.path.join(self.SOURCE_ROOT, "python/python/bin/ert_cli")
-        os.execvp(exec_path, [exec_path, config_file, "--algorithm=Test-run"])
-
-    def test_smoother_ensemble(self):
-        config_file = self.createTestPath('local/poly_example/poly.ert')
-        exec_path = os.path.join(self.SOURCE_ROOT, "python/python/bin/ert_cli")
-        os.execvp(exec_path, [exec_path, config_file, "--algorithm=Ensemble Smoother"])
+        ok = subprocess.call([exec_path, config_file, "--algorithm=Test-run"])
+        assert ok == 0
