8192263c2811ef53a4e98084f430979fc31c367f
Joakim Hove <joakim.hove@gmail.com>
2018-12-20 11:27:34 +0100

    Add basic test which just verifies that example case can be loaded

---
+++ python/tests/data/test_data.py
@@ -0,0 +1,12 @@
+from tests import ErtTest
+from res.test import ErtTestContext
+
+# The data loaded in this test is not part of an automatic test, we
+# just verify that the example cases can indeed be loaded.
+
+class TestData(ErtTest):
+
+    def test_poly(self):
+        config_file = self.createTestPath('local/poly_example/poly.ert')
+        with ErtTestContext('poly', config_file) as work_area:
+            ert = work_area.getErt()
