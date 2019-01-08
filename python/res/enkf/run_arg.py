415d347b4ad97fe8ecf59f037bcbb435cb927e00
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2019-01-08 14:39:05 +0100

    Expose iens in run_arg

--- python/res/enkf/run_arg.py
+++ python/res/enkf/run_arg.py
@@ -29,6 +29,7 @@ class RunArg(BaseCClass):
     _set_geo_id                = ResPrototype("void run_arg_set_geo_id(run_arg, int)")
     _get_runpath               = ResPrototype("char* run_arg_get_runpath(run_arg)")
     _get_iter                  = ResPrototype("int run_arg_get_iter(run_arg)")
+    _get_iens                  = ResPrototype("int run_arg_get_iens(run_arg)")
 
     def __init__(self):
         raise NotImplementedError("Cannot instantiat RunArg directly!")
@@ -78,3 +79,7 @@ class RunArg(BaseCClass):
     @property
     def iter_id(self):
         return self._get_iter()
+
+    @property
+    def iens(self):
+        return self._get_iens()
