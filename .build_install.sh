537d53ae2e098131e2ba0e9e6b7ce6c198dbf820
Lars Petter Øren Hauge <lpha@statoil.com>
2019-01-30 07:44:30 +0100

    Add regular Travis build

---
+++ .build_install.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+function build_install() {
+  pushd $1
+  pip install -r requirements.txt
+  mkdir build
+  pushd build
+  cmake .. -DENABLE_PYTHON=ON \
+-DBUILD_APPLICATIONS=ON \
+-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+-DCMAKE_PREFIX_PATH=$INSTALL_DIR \
+-DCMAKE_C_FLAGS="-Werror=all" \
+-DCMAKE_CXX_FLAGS="-Werror -Wno-unused-result"
+  make install
+  popd
+  popd
+}
+
+build_install $1
+
