31089726d119415ac1eef0dc00b531ccb23e0486
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2019-01-31 14:32:29 +0100

    Add key_manager test

---
+++ python/tests/res/enkf/test_key_manager.py
@@ -0,0 +1,49 @@
+from res.enkf.key_manager import KeyManager
+from res.test import ErtTestContext
+
+from tests import ResTest
+
+class KeyManagerTest(ResTest):
+    def setUp(self):
+        self.config_file = self.createTestPath("local/snake_oil/snake_oil.ert")
+
+    def test_summary_keys(self):
+        with ErtTestContext("enkf_key_manager_test", self.config_file) as testContext:
+            ert = testContext.getErt()
+            key_man = KeyManager(ert)
+
+            self.assertEqual( len(key_man.summaryKeys()), 47)
+            self.assertTrue("FOPT" in key_man.summaryKeys())
+
+            self.assertEqual(len(key_man.summaryKeysWithObservations()), 2)
+            self.assertTrue("FOPR" in key_man.summaryKeysWithObservations())
+            self.assertTrue(key_man.isKeyWithObservations("FOPR"))
+
+
+    def test_gen_data_keys(self):
+        with ErtTestContext("enkf_key_manager_test", self.config_file) as testContext:
+            ert = testContext.getErt()
+            key_man = KeyManager(ert)
+
+            self.assertEqual( len(key_man.genDataKeys()), 3)
+            self.assertTrue("SNAKE_OIL_WPR_DIFF@199" in key_man.genDataKeys())
+
+            self.assertEqual(len(key_man.genDataKeysWithObservations()), 1)
+            self.assertTrue("SNAKE_OIL_WPR_DIFF@199" in key_man.genDataKeysWithObservations())
+            self.assertTrue(key_man.isKeyWithObservations("SNAKE_OIL_WPR_DIFF@199"))
+
+    def test_custom_keys(self):
+        with ErtTestContext("enkf_key_manager_test", self.config_file) as testContext:
+            ert = testContext.getErt()
+            key_man = KeyManager(ert)
+
+            self.assertEqual( len(key_man.customKwKeys()), 2)
+            self.assertTrue("SNAKE_OIL_NPV:NPV" in key_man.customKwKeys())
+
+    def test_gen_kw_keys(self):
+        with ErtTestContext("enkf_key_manager_test", self.config_file) as testContext:
+            ert = testContext.getErt()
+            key_man = KeyManager(ert)
+
+            self.assertEqual(len(key_man.genKwKeys()), 10)
+            self.assertTrue("SNAKE_OIL_PARAM:BPR_555_PERSISTENCE" in key_man.genKwKeys())
