c5a57e563ac83601aa042859200636931f401135
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2019-02-08 10:34:06 +0100

    Add python constructor for ert_templates

--- python/res/enkf/ert_templates.py
+++ python/res/enkf/ert_templates.py
@@ -21,15 +21,22 @@ from ecl.util.util import StringList
 
 class ErtTemplates(BaseCClass):
     TYPE_NAME = "ert_templates"
-
+    _alloc        = ResPrototype("void* ert_templates_alloc( subst_list, config_content )", bind=False)
     _free         = ResPrototype("void ert_templates_free( ert_templates )")
     _alloc_list   = ResPrototype("stringlist_ref ert_templates_alloc_list(ert_templates)")
     _get_template = ResPrototype("ert_template_ref ert_templates_get_template(ert_templates, char*)")
     _clear        = ResPrototype("void ert_templates_clear(ert_templates)")
     _add_template = ResPrototype("ert_template_ref ert_templates_add_template(ert_templates, char*, char*, char*, char*)")
 
-    def __init__(self):
-        raise NotImplementedError("Class can not be instantiated directly!")
+    def __init__(self, parent_subst, config_content):
+
+        c_ptr = self._alloc(parent_subst, config_content)
+
+        if c_ptr is None:
+            raise ValueError('Failed to construct RNGConfig instance')
+
+        super(ErtTemplates, self).__init__(c_ptr)
+
 
     def getTemplateNames(self):
         """ @rtype: StringList """
