76399925328e92ce59e8d018508dcc7550729af2
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2019-02-08 10:27:33 +0100

    Extend python constructor for site_config to hande config_content

--- python/res/enkf/site_config.py
+++ python/res/enkf/site_config.py
@@ -17,56 +17,62 @@ from os.path import isfile
 
 from cwrap import BaseCClass
 from res import ResPrototype
-from res.job_queue import JobQueue, ExtJoblist
-from ecl.util.util import StringList, Hash
 
 
 class SiteConfig(BaseCClass):
     TYPE_NAME = "site_config"
+    _alloc                  = ResPrototype("void* site_config_alloc(config_content)", bind=False)
+    _alloc_load_user_config = ResPrototype("void* site_config_alloc_load_user_config(char*)", bind=False)
+    _free                   = ResPrototype("void site_config_free( site_config )")
+    _get_lsf_queue          = ResPrototype("char* site_config_get_lsf_queue(site_config)")
+    _set_lsf_queue          = ResPrototype("void site_config_set_lsf_queue(site_config, char*)")
+    _get_max_running_lsf    = ResPrototype("int site_config_get_max_running_lsf(site_config)")
+    _set_max_running_lsf    = ResPrototype("void site_config_set_max_running_lsf(site_config, int)")
+    _get_lsf_request        = ResPrototype("char* site_config_get_lsf_request(site_config)")
+    _set_lsf_request        = ResPrototype("void site_config_set_lsf_request(site_config, char*)")
+    _get_rsh_command        = ResPrototype("char* site_config_get_rsh_command(site_config)")
+    _set_rsh_command        = ResPrototype("void site_config_set_rsh_command(site_config, char*)")
+    _get_max_running_rsh    = ResPrototype("int site_config_get_max_running_rsh(site_config)")
+    _set_max_running_rsh    = ResPrototype("void site_config_set_max_running_rsh(site_config, int)")
+    _get_rsh_host_list      = ResPrototype("integer_hash_ref site_config_get_rsh_host_list(site_config)")
+    _clear_rsh_host_list    = ResPrototype("void site_config_clear_rsh_host_list(site_config)")
+    _add_rsh_host           = ResPrototype("void site_config_add_rsh_host(site_config, char*, int)")
+    _get_max_running_local  = ResPrototype("int site_config_get_max_running_local(site_config)")
+    _set_max_running_local  = ResPrototype("void site_config_set_max_running_local(site_config, int)")
+    _get_installed_jobs     = ResPrototype("ext_joblist_ref site_config_get_installed_jobs(site_config)")
+    _get_license_root_path  = ResPrototype("char* site_config_get_license_root_path(site_config)")
+    _set_license_root_path  = ResPrototype("void site_config_set_license_root_path(site_config, char*)")
+    _get_path_variables     = ResPrototype("stringlist_ref site_config_get_path_variables(site_config)")
+    _get_path_values        = ResPrototype("stringlist_ref site_config_get_path_values(site_config)")
+    _clear_pathvar          = ResPrototype("void site_config_clear_pathvar(site_config)")
+    _update_pathvar         = ResPrototype("void site_config_update_pathvar(site_config, char*, char*)")
+    _get_location           = ResPrototype("char* site_config_get_location(site_config)")
+    _has_driver             = ResPrototype("bool site_config_has_queue_driver(site_config, char*)")
+    _get_config_file        = ResPrototype("char* site_config_get_config_file(site_config)")
+    _get_queue_config       = ResPrototype("queue_config_ref site_config_get_queue_config(site_config)")
+    _get_umask              = ResPrototype("int site_config_get_umask(site_config)")
 
-    _alloc                 = ResPrototype("void* site_config_alloc_load_user_config(char*)", bind=False)
-    _free                  = ResPrototype("void site_config_free( site_config )")
-    _get_lsf_queue         = ResPrototype("char* site_config_get_lsf_queue(site_config)")
-    _set_lsf_queue         = ResPrototype("void site_config_set_lsf_queue(site_config, char*)")
-    _get_max_running_lsf   = ResPrototype("int site_config_get_max_running_lsf(site_config)")
-    _set_max_running_lsf   = ResPrototype("void site_config_set_max_running_lsf(site_config, int)")
-    _get_lsf_request       = ResPrototype("char* site_config_get_lsf_request(site_config)")
-    _set_lsf_request       = ResPrototype("void site_config_set_lsf_request(site_config, char*)")
-    _get_rsh_command       = ResPrototype("char* site_config_get_rsh_command(site_config)")
-    _set_rsh_command       = ResPrototype("void site_config_set_rsh_command(site_config, char*)")
-    _get_max_running_rsh   = ResPrototype("int site_config_get_max_running_rsh(site_config)")
-    _set_max_running_rsh   = ResPrototype("void site_config_set_max_running_rsh(site_config, int)")
-    _get_rsh_host_list     = ResPrototype("integer_hash_ref site_config_get_rsh_host_list(site_config)")
-    _clear_rsh_host_list   = ResPrototype("void site_config_clear_rsh_host_list(site_config)")
-    _add_rsh_host          = ResPrototype("void site_config_add_rsh_host(site_config, char*, int)")
-    _get_max_running_local = ResPrototype("int site_config_get_max_running_local(site_config)")
-    _set_max_running_local = ResPrototype("void site_config_set_max_running_local(site_config, int)")
-    _get_installed_jobs    = ResPrototype("ext_joblist_ref site_config_get_installed_jobs(site_config)")
-    _get_license_root_path = ResPrototype("char* site_config_get_license_root_path(site_config)")
-    _set_license_root_path = ResPrototype("void site_config_set_license_root_path(site_config, char*)")
-    _get_path_variables    = ResPrototype("stringlist_ref site_config_get_path_variables(site_config)")
-    _get_path_values       = ResPrototype("stringlist_ref site_config_get_path_values(site_config)")
-    _clear_pathvar         = ResPrototype("void site_config_clear_pathvar(site_config)")
-    _update_pathvar        = ResPrototype("void site_config_update_pathvar(site_config, char*, char*)")
-    _get_location          = ResPrototype("char* site_config_get_location(site_config)")
-    _has_driver            = ResPrototype("bool site_config_has_queue_driver(site_config, char*)")
-    _get_config_file       = ResPrototype("char* site_config_get_config_file(site_config)")
-    _get_queue_config      = ResPrototype("queue_config_ref site_config_get_queue_config(site_config)")
-    _get_umask             = ResPrototype("int site_config_get_umask(site_config)")
 
+    def __init__(self, user_config_file = None, config_content = None):
 
-    def __init__(self, user_config_file=None):
-        if user_config_file is not None and not isfile(user_config_file):
-            raise IOError('No such configuration file "%s".' % user_config_file)
+        if user_config_file is not None:
+            if not isfile(user_config_file):
+                raise IOError('No such configuration file "%s".' % user_config_file)
 
-        c_ptr = self._alloc(user_config_file)
-        if c_ptr:
-            super(SiteConfig, self).__init__(c_ptr)
+            c_ptr = self._alloc_load_user_config(user_config_file)
+            if c_ptr:
+                super(SiteConfig, self).__init__(c_ptr)
+            else:
+                raise ValueError('Failed to construct SiteConfig instance from config file %s.' % user_config_file)
+
         else:
-            raise ValueError(
-                    'Failed to construct SiteConfig instance from config file %s.'
-                    % user_config_file
-                    )
+            c_ptr = self._alloc(config_content)
+
+            if c_ptr is None:
+                raise ValueError('Failed to construct SiteConfig instance.')
+
+            super(SiteConfig, self).__init__(c_ptr)
+
 
     def __repr__(self):
         return "Site Config loaded from %s" % self.config_file
