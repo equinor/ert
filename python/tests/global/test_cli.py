e814d53ec0d0a2d4c04d44f960724dd772fce49c
Lars Petter Øren Hauge <lpha@statoil.com>
2019-03-18 08:20:29 +0100

    Rename algorithm to mode
    Whitespace in arguments removed

--- python/tests/global/test_cli.py
+++ python/tests/global/test_cli.py
@@ -7,5 +7,5 @@ class EntryPointTest(ErtTest):
     def test_single_realization(self):
         config_file = self.createTestPath('local/poly_example/poly.ert')
         exec_path = os.path.join(self.SOURCE_ROOT, "python/python/bin/ert_cli")
-        ok = subprocess.call([exec_path, config_file, "Test-run", "default"])
+        ok = subprocess.call([exec_path, config_file, "test_run", "default"])
         assert ok == 0
