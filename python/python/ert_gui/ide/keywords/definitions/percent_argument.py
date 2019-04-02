377471d58d57cd34d774379c4d979de9d2e5cb82
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2019-04-02 10:29:40 +0200

    Fix percent argument test

--- python/python/ert_gui/ide/keywords/definitions/percent_argument.py
+++ python/python/ert_gui/ide/keywords/definitions/percent_argument.py
@@ -1,6 +1,6 @@
 import re
 from ert_gui.ide.keywords.definitions import ArgumentDefinition
-
+from ert_gui.ide.keywords.definitions import IntegerArgument
 
 class PercentArgument(ArgumentDefinition):
 
@@ -9,11 +9,10 @@ class PercentArgument(ArgumentDefinition):
 
     pattern = re.compile("^-?[0-9]+(\.[0-9]+)?\%$")
 
-    def __init__(self, from_value=None, to_value=None, **kwargs):
+    def __init__(self, from_value, to_value, **kwargs):
         super(PercentArgument, self).__init__(**kwargs)
         self.from_value = from_value * 0.01
         self.to_value = to_value  * 0.01
-
 
     def validate(self, token):
         validation_status = super(PercentArgument, self).validate(token)
@@ -26,31 +25,13 @@ class PercentArgument(ArgumentDefinition):
         else:
             value = float(token[:-1]) * 0.01
 
-            if self.from_value is not None and self.to_value is not None and not self.from_value <= value <= self.to_value:
+            if not self.from_value <= value <= self.to_value:
                 validation_status.setFailed()
-                range_string = "%d <= %d <= %d" % (self.from_value, value, self.to_value)
+                range_string = "{:.0f}% <= {:.0f}% <= {:.0f}%".format(self.from_value * 100, value * 100, self.to_value * 100)
                 validation_status.addToMessage(IntegerArgument.NOT_IN_RANGE % range_string)
-
-            elif self.from_value is not None and self.from_value > value:
-                validation_status.setFailed()
-                range_string = "%d <= %d" % (self.from_value, value)
-                validation_status.addToMessage(IntegerArgument.NOT_IN_RANGE % range_string)
-
-            elif self.to_value is not None and self.to_value < value:
-                validation_status.setFailed()
-                range_string = "%d <= %d" % (value, self.to_value)
-                validation_status.addToMessage(IntegerArgument.NOT_IN_RANGE % range_string)
-
 
             if not validation_status.failed():
                 validation_status.setValue(value)
 
 
         return validation_status
-
-
-
-
-
-
-
