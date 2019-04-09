0c5f41bd64ffb5032019eab0d8c0b5ad7f6e9cdc
Øyvind Eide <oyveid@equinor.com>
2019-04-09 09:55:55 +0200

    Add python 3.5 support ert entry point
    Subparsers was no longer required after update, forced now

--- tests/global/test_cli.py
+++ tests/global/test_cli.py
@@ -9,3 +9,11 @@ class EntryPointTest(ErtTest):
         exec_path = os.path.join(self.SOURCE_ROOT, "ert_gui/bin/ert_cli")
         ok = subprocess.call([exec_path, config_file, "test_run", "default"])
         assert ok == 0
+
+    def test_ert_no_arguments(self):
+        exec_path = os.path.join(self.SOURCE_ROOT, "bin/ert.in")
+        proc = subprocess.Popen(exec_path, stderr=subprocess.PIPE)
+        _, err = proc.communicate()
+
+        assert("ert.in: error" in str(err))
+
