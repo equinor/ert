joakim-hove/libres

IES Implementation with tests

lib/analysis/modules/ies_enkf.h
@@ -1,0 +1,58 @@
+
+/*
+  Copyright (C) 2019  Statoil ASA, Norway.
+
+  The file 'ies_enkf.h' is part of ERT - Ensemble based Reservoir Tool.
+
+  ERT is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.
+
+  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+  for more details.
+*/
+
+#ifndef IES_ENKF_H
+#define IES_ENKF_H
+
+#include <ert/util/bool_vector.hpp>
+#include <ert/util/rng.hpp>
+
+#include <ert/analysis/module_info.hpp>
+#include <ert/res_util/matrix.hpp>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ies_enkf_init_update(void * arg ,
+                          const bool_vector_type * ens_mask ,
+                          const bool_vector_type * obs_mask ,
+                          const matrix_type * S ,
+                          const matrix_type * R ,
+                          const matrix_type * dObs ,
+                          const matrix_type * E ,
+                          const matrix_type * D,
+                          rng_type * rng);
+
+
+void ies_enkf_updateA( void * module_data,
+                       matrix_type * A ,      // Updated ensemble A retured to ERT.
+                       matrix_type * Yin ,    // Ensemble of predicted measurements
+                       matrix_type * Rin ,    // Measurement error covariance matrix (not used)
+                       matrix_type * dObs ,   // Actual observations (not used)
+                       matrix_type * Ein ,    // Ensemble of observation perturbations
+                       matrix_type * Din ,    // (d+E-Y) Ensemble of perturbed observations - Y
+                       const module_info_type * module_info,
+                       rng_type * rng);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

lib/analysis/modules/ies_enkf_config.h
@@ -1,0 +1,80 @@
+/*
+   Copyright (C) 2015  Statoil ASA, Norway.
+
+   The file 'ies_enkf_config.h' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
+*/
+
+#ifndef IES_ENKF_CONFIG_H
+#define IES_ENKF_CONFIG_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef enum {
+  IES_INVERSION_EXACT = 0,
+  IES_INVERSION_SUBSPACE_EXACT_R = 1,
+  IES_INVERSION_SUBSPACE_EE_R = 2,
+  IES_INVERSION_SUBSPACE_RE = 3
+} ies_inversion_type;
+
+
+typedef struct ies_enkf_config_struct ies_enkf_config_type;
+
+
+  ies_enkf_config_type * ies_enkf_config_alloc();
+  void   ies_enkf_config_free(ies_enkf_config_type * config);
+
+  int    ies_enkf_config_get_enkf_subspace_dimension( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_enkf_subspace_dimension( ies_enkf_config_type * config , int subspace_dimension);
+
+  double ies_enkf_config_get_truncation( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_truncation( ies_enkf_config_type * config , double truncation);
+
+  void ies_enkf_config_set_option_flags( ies_enkf_config_type * config , long flags);
+  long ies_enkf_config_get_option_flags( const ies_enkf_config_type * config );
+
+  double ies_enkf_config_get_ies_steplength( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_ies_steplength( ies_enkf_config_type * config , double ies_steplength);
+
+  double ies_enkf_config_get_gauss_newton_conv( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_gauss_newton_conv( ies_enkf_config_type * config , double gauss_newton_conv);
+
+  int    ies_enkf_config_get_max_gauss_newton_it( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_max_gauss_newton_it( ies_enkf_config_type * config , int max_gauss_newton_it);
+
+  ies_inversion_type ies_enkf_config_get_ies_inversion( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_inversion( ies_enkf_config_type * config , ies_inversion_type ies_inversion ) ;
+
+  bool   ies_enkf_config_get_ies_subspace( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_subspace( ies_enkf_config_type * config , bool ies_subspace ) ;
+
+  bool   ies_enkf_config_get_ies_debug( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_debug( ies_enkf_config_type * config , bool ies_debug ) ;
+
+  bool   ies_enkf_config_get_ies_aaprojection( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_aaprojection( ies_enkf_config_type * config , bool ies_aaprojection ) ;
+
+  char * ies_enkf_config_get_ies_logfile( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_logfile( ies_enkf_config_type * config , const char * ies_logfile ) ;
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif

lib/analysis/modules/tests/ies_std_compare.cpp
@@ -1,0 +1,84 @@
+#include <ert/util/test_util.hpp>
+#include <ert/util/rng.h>
+
+#include <ert/res_util/es_testdata.hpp>
+
+#include <ert/analysis/std_enkf.hpp>
+#include "ies_enkf_data.h"
+#include "ies_enkf_config.h"
+#include "ies_enkf.h"
+
+/*
+TEST 3 (consistency between IES and STD_ENKF):
+ - ANALYSIS_SET_VAR IES_ENKF ENKF_TRUNCATION         0.999
+ - ANALYSIS_SET_VAR IES_ENKF IES_STEPLENGTH          1.0
+ - ANALYSIS_SET_VAR IES_ENKF IES_INVERSION           1
+ - ANALYSIS_SET_VAR IES_ENKF IES_AAPROJECTION        false
+should give same result as:
+ - ANALYSIS_SET_VAR IES_ENKF ENKF_TRUNCATION         0.999
+ - ANALYSIS_SELECT STD_ENKF
+*/
+
+
+void cmp_std_ies(const res::es_testdata& testdata) {
+  rng_type * rng = rng_alloc( MZRAN, INIT_DEFAULT );
+  matrix_type * A1 = testdata.alloc_state("prior");
+  matrix_type * A2 = testdata.alloc_state("prior");
+  matrix_type * X = matrix_alloc(testdata.active_ens_size, testdata.active_ens_size);
+
+  ies_enkf_data_type * ies_data1 = static_cast<ies_enkf_data_type*>(ies_enkf_data_alloc(rng));
+  ies_enkf_config_type * ies_config1 = ies_enkf_data_get_config(ies_data1);
+  std_enkf_data_type * std_data = static_cast<std_enkf_data_type*>(std_enkf_data_alloc());
+
+  ies_enkf_config_set_truncation(ies_config1, 0.95);
+  ies_enkf_config_set_ies_steplength(ies_config1, 1.0);
+  ies_enkf_config_set_ies_inversion(ies_config1, IES_INVERSION_SUBSPACE_EXACT_R);
+  ies_enkf_config_set_ies_aaprojection(ies_config1, false);
+
+  std_enkf_set_truncation(std_data, 0.95);
+
+  ies_enkf_init_update(ies_data1,
+                       testdata.ens_mask,
+                       testdata.obs_mask,
+                       testdata.S,
+                       testdata.R,
+                       testdata.dObs,
+                       testdata.E,
+                       testdata.D,
+                       rng);
+
+  ies_enkf_updateA(ies_data1,
+                   A1,
+                   testdata.S,
+                   testdata.R,
+                   testdata.dObs,
+                   testdata.E,
+                   testdata.D,
+                   NULL,
+                   rng);
+
+  std_enkf_initX(std_data,
+                 X,
+                 nullptr,
+                 testdata.S,
+                 testdata.R,
+                 testdata.dObs,
+                 testdata.E,
+                 testdata.D,
+                 rng);
+
+  matrix_inplace_matmul(A2, X);
+  test_assert_true( matrix_similar(A1, A2, 5e-6));
+
+  matrix_free(A1);
+  matrix_free(A2);
+  std_enkf_data_free(std_data);
+  ies_enkf_data_free(ies_data1);
+  rng_free( rng );
+}
+
+
+int main(int argc, char ** argv) {
+  res::es_testdata testdata(argv[1]);
+  cmp_std_ies(testdata);
+}

equinor/ert

lib/analysis/modules/ies_enkf.h
@@ -1,0 +1,57 @@
+/*
+  Copyright (C) 2019  Equinor ASA, Norway.
+
+  The file 'ies_enkf.h' is part of ERT - Ensemble based Reservoir Tool.
+
+  ERT is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.
+
+  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+  for more details.
+*/
+
+#ifndef IES_ENKF_H
+#define IES_ENKF_H
+
+#include <ert/util/bool_vector.hpp>
+#include <ert/util/rng.hpp>
+
+#include <ert/analysis/module_info.hpp>
+#include <ert/res_util/matrix.hpp>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ies_enkf_init_update(void * arg ,
+                          const bool_vector_type * ens_mask ,
+                          const bool_vector_type * obs_mask ,
+                          const matrix_type * S ,
+                          const matrix_type * R ,
+                          const matrix_type * dObs ,
+                          const matrix_type * E ,
+                          const matrix_type * D,
+                          rng_type * rng);
+
+
+void ies_enkf_updateA( void * module_data,
+                       matrix_type * A ,      // Updated ensemble A retured to ERT.
+                       matrix_type * Yin ,    // Ensemble of predicted measurements
+                       matrix_type * Rin ,    // Measurement error covariance matrix (not used)
+                       matrix_type * dObs ,   // Actual observations (not used)
+                       matrix_type * Ein ,    // Ensemble of observation perturbations
+                       matrix_type * Din ,    // (d+E-Y) Ensemble of perturbed observations - Y
+                       const module_info_type * module_info,
+                       rng_type * rng);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

lib/analysis/modules/ies_enkf_config.h
@@ -1,0 +1,86 @@
+/*
+   Copyright (C) 2019  Equinor ASA, Norway.
+
+   The file 'ies_enkf_config.h' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
+*/
+
+#ifndef IES_ENKF_CONFIG_H
+#define IES_ENKF_CONFIG_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef enum {
+  IES_INVERSION_EXACT = 0,
+  IES_INVERSION_SUBSPACE_EXACT_R = 1,
+  IES_INVERSION_SUBSPACE_EE_R = 2,
+  IES_INVERSION_SUBSPACE_RE = 3
+} ies_inversion_type;
+
+
+typedef struct ies_enkf_config_struct ies_enkf_config_type;
+
+
+  ies_enkf_config_type * ies_enkf_config_alloc();
+  void   ies_enkf_config_free(ies_enkf_config_type * config);
+
+  int    ies_enkf_config_get_enkf_subspace_dimension( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_enkf_subspace_dimension( ies_enkf_config_type * config , int subspace_dimension);
+
+  double ies_enkf_config_get_truncation( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_truncation( ies_enkf_config_type * config , double truncation);
+
+  void ies_enkf_config_set_option_flags( ies_enkf_config_type * config , long flags);
+  long ies_enkf_config_get_option_flags( const ies_enkf_config_type * config );
+
+  double ies_enkf_config_get_ies_max_steplength( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_ies_max_steplength( ies_enkf_config_type * config , double ies_max_steplength);
+
+  double ies_enkf_config_get_ies_min_steplength( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_ies_min_steplength( ies_enkf_config_type * config , double ies_min_steplength);
+
+  double ies_enkf_config_get_ies_dec_steplength( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_ies_dec_steplength( ies_enkf_config_type * config , double ies_dec_steplength);
+
+  double ies_enkf_config_get_gauss_newton_conv( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_gauss_newton_conv( ies_enkf_config_type * config , double gauss_newton_conv);
+
+  int    ies_enkf_config_get_max_gauss_newton_it( const ies_enkf_config_type * config );
+  void   ies_enkf_config_set_max_gauss_newton_it( ies_enkf_config_type * config , int max_gauss_newton_it);
+
+  ies_inversion_type ies_enkf_config_get_ies_inversion( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_inversion( ies_enkf_config_type * config , ies_inversion_type ies_inversion ) ;
+
+  bool   ies_enkf_config_get_ies_subspace( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_subspace( ies_enkf_config_type * config , bool ies_subspace ) ;
+
+  bool   ies_enkf_config_get_ies_debug( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_debug( ies_enkf_config_type * config , bool ies_debug ) ;
+
+  bool   ies_enkf_config_get_ies_aaprojection( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_aaprojection( ies_enkf_config_type * config , bool ies_aaprojection ) ;
+
+  char * ies_enkf_config_get_ies_logfile( const ies_enkf_config_type * config ) ;
+  void   ies_enkf_config_set_ies_logfile( ies_enkf_config_type * config , const char * ies_logfile ) ;
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif

lib/analysis/modules/tests/ies_std_compare.cpp
@@ -1,0 +1,85 @@
+#include <ert/util/test_util.hpp>
+#include <ert/util/rng.h>
+
+#include <ert/res_util/es_testdata.hpp>
+
+#include <ert/analysis/std_enkf.hpp>
+#include "ies_enkf_data.h"
+#include "ies_enkf_config.h"
+#include "ies_enkf.h"
+
+/*
+TEST 3 (consistency between IES and STD_ENKF):
+ - ANALYSIS_SET_VAR IES_ENKF ENKF_TRUNCATION         0.999
+ - ANALYSIS_SET_VAR IES_ENKF IES_STEPLENGTH          1.0
+ - ANALYSIS_SET_VAR IES_ENKF IES_INVERSION           1
+ - ANALYSIS_SET_VAR IES_ENKF IES_AAPROJECTION        false
+should give same result as:
+ - ANALYSIS_SET_VAR IES_ENKF ENKF_TRUNCATION         0.999
+ - ANALYSIS_SELECT STD_ENKF
+*/
+
+
+void cmp_std_ies(const res::es_testdata& testdata) {
+  rng_type * rng = rng_alloc( MZRAN, INIT_DEFAULT );
+  matrix_type * A1 = testdata.alloc_state("prior");
+  matrix_type * A2 = testdata.alloc_state("prior");
+  matrix_type * X = matrix_alloc(testdata.active_ens_size, testdata.active_ens_size);
+
+  ies_enkf_data_type * ies_data1 = static_cast<ies_enkf_data_type*>(ies_enkf_data_alloc(rng));
+  ies_enkf_config_type * ies_config1 = ies_enkf_data_get_config(ies_data1);
+  std_enkf_data_type * std_data = static_cast<std_enkf_data_type*>(std_enkf_data_alloc());
+
+  ies_enkf_config_set_truncation(ies_config1, 0.95);
+  ies_enkf_config_set_ies_min_steplength(ies_config1, 1.0);
+  ies_enkf_config_set_ies_max_steplength(ies_config1, 1.0);
+  ies_enkf_config_set_ies_inversion(ies_config1, IES_INVERSION_SUBSPACE_EXACT_R);
+  ies_enkf_config_set_ies_aaprojection(ies_config1, false);
+
+  std_enkf_set_truncation(std_data, 0.95);
+
+  ies_enkf_init_update(ies_data1,
+                       testdata.ens_mask,
+                       testdata.obs_mask,
+                       testdata.S,
+                       testdata.R,
+                       testdata.dObs,
+                       testdata.E,
+                       testdata.D,
+                       rng);
+
+  ies_enkf_updateA(ies_data1,
+                   A1,
+                   testdata.S,
+                   testdata.R,
+                   testdata.dObs,
+                   testdata.E,
+                   testdata.D,
+                   NULL,
+                   rng);
+
+  std_enkf_initX(std_data,
+                 X,
+                 nullptr,
+                 testdata.S,
+                 testdata.R,
+                 testdata.dObs,
+                 testdata.E,
+                 testdata.D,
+                 rng);
+
+  matrix_inplace_matmul(A2, X);
+  test_assert_true( matrix_similar(A1, A2, 5e-6));
+
+  matrix_free(A1);
+  matrix_free(A2);
+  std_enkf_data_free(std_data);
+  ies_enkf_data_free(ies_data1);
+  rng_free( rng );
+}
+
+
+int main(int argc, char ** argv) {
+  res::es_testdata testdata(argv[1]);
+  cmp_std_ies(testdata);
+}