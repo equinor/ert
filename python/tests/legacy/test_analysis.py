4934860cf501f9aa3f7d800b40704713be5efb4a
Øyvind Eide <oyveid@equinor.com>
2019-06-11 13:38:14 +0200

    Remove empty legacy tests
    Legacy tests were empty so have been removed

--- python/tests/legacy/test_analysis.py
+++
@@ -1,9 +0,0 @@
-from ert.analysis import AnalysisModuleOptionsEnum, AnalysisModuleLoadStatusEnum
-from ert.analysis import AnalysisModule
-from ert.analysis import Linalg
-
-
-from tests import ResTest
-
-class ErtLegacyAnalysisTest(ResTest):
-    pass
