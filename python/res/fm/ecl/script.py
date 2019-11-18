149b7f6aea1fa563c1832e86f8356101d1043b95
Asmund Birkeland <musiv@equinor.com>
2019-11-18 10:24:20 +0100

    Add single letter option to arguments for eclipse and flow script

--- python/res/fm/ecl/script.py
+++ python/res/fm/ecl/script.py
@@ -7,9 +7,9 @@ from .ecl_run import EclRun
 def run(config, argv):
     parser = ArgumentParser()
     parser.add_argument("ecl_case")
-    parser.add_argument("--version", dest="version", type=str)
-    parser.add_argument("--num-cpu", dest="num_cpu", type=int, default=1)
-    parser.add_argument("--ignore-errors", dest="ignore_errors", action="store_true")
+    parser.add_argument("-v", "--version", dest="version", type=str)
+    parser.add_argument("-n", "--num-cpu", dest="num_cpu", type=int, default=1)
+    parser.add_argument("-i", "--ignore-errors", dest="ignore_errors", action="store_true")
 
     options = parser.parse_args(argv)
 
