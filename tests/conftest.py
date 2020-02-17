6245b684210c51f26adad5316e8486f3edc6c76c
Sondre Sortland <sonso@equinor.com>
2020-02-17 10:06:31 +0100

    Fix environment manipulation in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 
+
 @pytest.fixture()
 def source_root():
     path_list = os.path.dirname(os.path.abspath(__file__)).split("/")
@@ -9,4 +10,20 @@ def source_root():
         if os.path.isdir(git_path):
             return os.path.join(os.sep, *path_list)
         path_list.pop()
-    raise RuntimeError('Cannot find the source folder')
+    raise RuntimeError("Cannot find the source folder")
+
+
+@pytest.fixture(autouse=True)
+def env_save():
+    environment_pre = [
+        (key, val) for key, val in os.environ.items() if key != "PYTEST_CURRENT_TEST"
+    ]
+    yield
+    environment_post = [
+        (key, val) for key, val in os.environ.items() if key != "PYTEST_CURRENT_TEST"
+    ]
+    if set(environment_pre) != set(environment_post):
+        raise EnvironmentError(
+            "Your environment has changed after that test, please reset"
+        )
+
