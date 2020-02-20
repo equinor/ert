eed4f04e97f65e79b5e3574fd0be7e9db703157a
Åsmund Birkeland <musiv@equinor.com>
2020-02-20 14:29:20 +0100

    Use komodo test for testing PRs

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -9,12 +9,22 @@ GCC_VERSION=7.3.0 CMAKE_VERSION=3.10.2 source ${SDPSOFT}/env.sh
 
 GIT=${SDPSOFT}/bin/git
 
-EV=$(cat ${RELEASE_PATH}/${RELEASE_NAME} | grep "${PROJECT}:" -A2 | grep "version:")
-EV=($EV)    # split the string "version: vX.X.X"
-EV=${EV[1]} # extract the version
-EV=${EV%"+py3"}
-echo "Using ${PROJECT} version ${EV}"
-$GIT checkout $EV
+if [[ -z "${sha1// }" ]]; then
+    # this is not a PR build, the komodo everest verison is checked out
+    EV=$(cat ${RELEASE_PATH}/${RELEASE_NAME} | grep "${PROJECT}:" -A2 | grep "version:")
+    EV=($EV)    # split the string "version: vX.X.X"
+    EV=${EV[1]} # extract the version
+    EV=${EV%"+py3"}
+    echo "Using ${PROJECT} version ${EV}"
+    $GIT checkout $EV
+
+    # Run in a new folder so that we dont load the other python code from the source, but rather run against komodo
+    rm -rf tmptest
+    mkdir tmptest
+    cp -r tests tmptest/tests
+    cp -r test-data tmptest/test-data
+    pushd tmptest
+fi
 
 echo "Creating virtualenv"
 ENV="testenv"
@@ -24,17 +34,10 @@ python -m virtualenv --system-site-packages $ENV
 source $ENV/bin/activate
 python -m pip install -r dev-requirements.txt
 
-
 echo "Running pytest"
-# Run in a new folder so that we dont load the other python code from the source, but rather run against komodo
-rm -rf tmptest
-mkdir tmptest
-cp -r tests tmptest/tests
-cp -r test-data tmptest/test-data
-pushd tmptest
+
 if [[ ${RELEASE_NAME} =~ py27$  ]]
 then
     export PYTEST_QT_API=pyqt4v2
 fi
 xvfb-run python -m pytest
-popd
