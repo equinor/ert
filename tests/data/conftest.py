2b334433a644c90fa62ca026ec558a660bf55842
Jonas G. Drange <jond@equinor.com>
2020-03-02 11:57:14 +0100

    Remove inactive summary observations

--- tests/data/conftest.py
+++ tests/data/conftest.py
@@ -17,6 +17,7 @@ def facade():
     facade.get_impl.return_value = Mock()
     facade.get_ensemble_size.return_value = 3
     facade.get_observations.return_value = {"some_key": obs_mock}
+    facade.get_data_key_for_obs_key.return_value = "some_key"
 
     facade.get_current_case_name.return_value = "test_case"
 
