58a8b70d0abd93f9db0e9701bdf59b2db8c8dfea
Morten Bendiksen <morten.bendiksen@gmail.com>
2020-03-16 15:36:05 +0100

    tests in test_plot_storage_api are passing

--- tests/gui/tools/plot/test_plot_storage_api.py
+++ tests/gui/tools/plot/test_plot_storage_api.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest import TestCase
-
+import pandas as pd
 from ert_shared.storage.storage_api import PlotStorageApi
 
 from tests.storage import populated_db, db_session, engine, tables
@@ -13,8 +13,12 @@ def test_all_keys(populated_db):
 
 def test_observation_values(populated_db):
     api = PlotStorageApi(populated_db)
-    result = api.data_for_key(case="ensemble_name", key="observation_one")
-    assert result == ([10.1, 10.2], [1, 3])
+    result = api.observations_for_obs_keys(case="ensemble_name", obs_keys=["observation_one"])
+    idx = pd.MultiIndex.from_arrays([[0, 1], [0, 3]],
+                                    names=['data_index', 'key_index'])
+    expected = pd.DataFrame({"OBS": [10.1, 10.2], "STD": [1, 3]}, index=idx).T
+
+    assert result.equals(expected)
 
 def test_parameter_values():
     pass
@@ -22,4 +26,6 @@ def test_parameter_values():
 def test_response_values(populated_db):
     api = PlotStorageApi(populated_db)
     result = api.data_for_key(case="ensemble_name", key="response_one")
-    assert result == ([22.1, 44.2], [0, 1])
+    idx = pd.MultiIndex.from_arrays([["response_one", "response_one"], [0, 1]])
+    expected = pd.DataFrame([11.1, 11.2], index=idx).T
+    assert result.equals(expected)
