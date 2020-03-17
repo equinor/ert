6ee67531f23b17b7196ca619c201b1a1ff4892b5
Øystein Olai Heggen <ooh@equinor.com>
2020-03-17 16:13:39 +0100

    Add test for dump responses

--- tests/storage/test_extraction_api.py
+++ tests/storage/test_extraction_api.py
@@ -86,3 +86,112 @@ def test_dump_parameters(db_session):
 
         parameter_4 = repository.get_parameter("COEFF_A", "COEFFS", 4, ensemble_name)
         assert parameter_4.value == 0.5949261230249001
+
+
+poly_res = pd.DataFrame.from_dict(
+    {
+        0: {
+            0: 2.5995,
+            1: 5.203511,
+            2: 9.496884000000001,
+            3: 15.479619,
+            4: 23.151716,
+            5: 32.513175000000004,
+            6: 43.563995999999996,
+            7: 56.304179,
+            8: 70.73372400000001,
+            9: 86.852631,
+        },
+        1: {
+            0: 4.97204,
+            1: 6.23818,
+            2: 8.18051,
+            3: 10.79903,
+            4: 14.09374,
+            5: 18.06464,
+            6: 22.71173,
+            7: 28.035009999999996,
+            8: 34.03448,
+            9: 40.71014,
+        },
+        2: {
+            0: 0.660302,
+            1: 1.906593,
+            2: 4.597682,
+            3: 8.733569000000001,
+            4: 14.314254,
+            5: 21.339737000000003,
+            6: 29.810018000000003,
+            7: 39.725097000000005,
+            8: 51.084974,
+            9: 63.889649000000006,
+        },
+        3: {
+            0: 4.99478,
+            1: 5.566743000000001,
+            2: 6.4375800000000005,
+            3: 7.607291,
+            4: 9.075876000000001,
+            5: 10.843335,
+            6: 12.909668,
+            7: 15.274875,
+            8: 17.938955999999997,
+            9: 20.901911,
+        },
+        4: {
+            0: 1.96728,
+            1: 2.2754027,
+            2: 3.0749214,
+            3: 4.3658361,
+            4: 6.1481468,
+            5: 8.421853500000001,
+            6: 11.186956200000001,
+            7: 14.4434549,
+            8: 18.1913496,
+            9: 22.430640299999997,
+        },
+    }
+)
+
+responses = {"POLY_RES": poly_res}
+
+
+def test_dump_responses(db_session):
+    ensemble_name = "default"
+    with ErtRepository(db_session) as repository:
+        ensemble = repository.add_ensemble(name=ensemble_name)
+        
+        observations = pd.DataFrame.from_dict(observation_data)
+        _dump_observations(repository=repository, observations=observations)
+
+        for i in range(5):
+            repository.add_realization(i, ensemble.name)
+
+        key_mapping = {"POLY_RES": "POLY_OBS"}
+
+        _dump_response(
+            repository=repository,
+            responses=responses,
+            ensemble_name=ensemble.name,
+            key_mapping=key_mapping,
+        )
+        repository.commit()
+
+    with ErtRepository(db_session) as repository:
+        response_0 = repository.get_response("POLY_RES", 0, ensemble_name)
+
+        assert response_0.values == [
+            2.5995,
+            5.203511,
+            9.496884000000001,
+            15.479619,
+            23.151716,
+            32.513175000000004,
+            43.563995999999996,
+            56.304179,
+            70.73372400000001,
+            86.852631,
+        ]
+
+        assert response_0.response_definition.observation.name == "POLY_OBS"
+        
