8a79fa9d16bb5e15bab3c3fd8f14f245563920a2
Sondre Sortland <sonso@equinor.com>
2020-03-18 17:07:19 +0100

    Stream responses with realization index

--- ert_shared/storage/storage_api.py
+++ ert_shared/storage/storage_api.py
@@ -8,3 +8,10 @@ def get_response_data(name, ensemble_name, repository=None):
     with repository:
         for response in repository.get_response_data(name, ensemble_name):
             yield response
+
+def get_all_ensembles(repository=None):
+    if repository is None:
+        repository = ErtRepository()
+
+    with repository:
+        return [ensemble.name for ensemble in repository.get_all_ensembles()]
