ce16af52bc793c5c34e541c11be6940158935547
Morten Bendiksen <morten.bendiksen@gmail.com>
2020-03-20 15:12:42 +0100

    made csv function that works with plot window

--- tests/gui/tools/plot/test_plot_storage_api.py
+++ tests/gui/tools/plot/test_plot_storage_api.py
@@ -4,7 +4,7 @@ import pandas as pd
 from ert_shared.storage.storage_api import PlotStorageApi
 
 from tests.storage import populated_db, db_session, engine, tables
-
+from io import StringIO
 
 def test_all_keys(populated_db):
     api = PlotStorageApi(populated_db)
@@ -35,6 +35,11 @@ def test_stream_param(populated_db):
     for msg in api.get_param_data(ensembles = ["ensemble_name"]):
         print(str(msg))
 
-    df = api.make_df(api.get_param_data(ensembles= ["ensemble_name"]))
+    csv = ""
+    for msg in api.stream_csv(api.get_param_data(ensembles = ["ensemble_name"])):
+        csv += msg
+
+    print(csv)
+    df = pd.read_csv(StringIO(csv), index_col=[0], header=[0, 1])
     print(df)
 
