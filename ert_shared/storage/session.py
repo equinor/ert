ad18757bd7494bf1de5b4d59551bcad4c23fa90b
Øystein Olai Heggen <ooh@equinor.com>
2020-03-25 08:07:09 +0100

    Add logging to sessionfactory

--- ert_shared/storage/session.py
+++ ert_shared/storage/session.py
@@ -1,4 +1,7 @@
-from ert_shared.storage import Entities, Blobs
+
+import logging
+
+from ert_shared.storage import Blobs, Entities
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import Session
 
@@ -7,10 +10,15 @@ class SessionFactory(object):
     def __init__(self):
         self._entities_session = None
         self._blobs_session = None
+        self._entities_db = "sqlite:///entities.db"
+        self._blobs_db = "sqlite:///blobs.db"
 
     def get_entities_session(self):
         if self._entities_session is None:
-            engine = create_engine("sqlite:///entities.db", echo=False)
+            msg = "Setting up session, using {}"
+            logging.info(msg.format(self._entities_db))
+
+            engine = create_engine(self._entities_db, echo=False)
             Entities.metadata.create_all(engine)
             connection = engine.connect()
             self._entities_session = Session(bind=connection)
@@ -18,7 +26,10 @@ class SessionFactory(object):
 
     def get_blobs_session(self):
         if self._blobs_session is None:
-            engine = create_engine("sqlite:///blobs.db", echo=False)
+            msg = "Setting up session, using {}"
+            logging.info(msg.format(self._blobs_db))
+
+            engine = create_engine(self._blobs_db, echo=False)
             Blobs.metadata.create_all(engine)
             connection = engine.connect()
             self._blobs_session = Session(bind=connection)
