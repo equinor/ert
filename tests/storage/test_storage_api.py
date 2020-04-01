8b8a158f9961f166427255d04fdc278dc1ecf8a7
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2020-04-01 11:15:54 +0200

    Remove test resolve_url from test_storage_api

--- tests/storage/test_storage_api.py
+++ tests/storage/test_storage_api.py
@@ -6,29 +6,6 @@ import pytest
 import json
 import pprint
 
-def resolve_data_uri(BASE_URL, struct):
-    if isinstance(struct, list):
-        for item in struct:
-            resolve_data_uri(BASE_URL, item)
-    elif isinstance(struct, dict):
-        for key, val in struct.copy().items():
-            if key == "data_ref":
-                url = "{}/data/{}".format(BASE_URL, val)
-                struct["data_url"] = url
-            else:
-                resolve_data_uri(BASE_URL, val)
-
-def resolve_ref_uri(BASE_URL, struct):
-    if isinstance(struct, list):
-        for item in struct:
-            resolve_ref_uri(BASE_URL, item)
-    elif isinstance(struct, dict):
-        for key, val in struct.copy().items():
-            if "_ref" in key and "data" not in key:
-                url = "{}/{}".format(BASE_URL, val)
-                struct["ref_url"] = url
-            else:
-                resolve_ref_uri("{}/{}".format(BASE_URL, key), val)
 
 def test_response(populated_db):
     with RdbApi(populated_db) as rdb_api, BlobApi(populated_db) as blob_api:
@@ -38,10 +15,7 @@ def test_response(populated_db):
         assert len(schema["observation"]["data"]) == 4
         print("############### RESPONSE ###############")
         pprint.pprint(schema)
-        print("--------------- after --------------")
-        resolve_data_uri("http://localhost", schema)
-        resolve_ref_uri("http://localhost/ensembles/1/response/response_one", schema)
-        pprint.pprint(schema)
+
 
 def test_ensembles(populated_db):
     with RdbApi(populated_db) as rdb_api, BlobApi(populated_db) as blob_api:
@@ -49,10 +23,7 @@ def test_ensembles(populated_db):
         schema = api.ensembles()
         print("############### ENSEBMLES ###############")
         pprint.pprint(schema)
-        print("--------------- after --------------")
-        resolve_data_uri("http://localhost", schema)
-        resolve_ref_uri("http://localhost/ensembles", schema)
-        pprint.pprint(schema)
+
 
 def test_ensemble(populated_db):
     with RdbApi(populated_db) as rdb_api, BlobApi(populated_db) as blob_api:
@@ -60,10 +31,7 @@ def test_ensemble(populated_db):
         schema = api.ensemble_schema(1)
         print("############### ENSEMBLE ###############")
         pprint.pprint(schema)
-        print("--------------- after --------------")
-        resolve_data_uri("http://localhost", schema)
-        resolve_ref_uri("http://localhost/ensembles/1", schema)
-        pprint.pprint(schema)
+
 
 def test_realization(populated_db):
     with RdbApi(populated_db) as rdb_api, BlobApi(populated_db) as blob_api:
@@ -71,7 +39,4 @@ def test_realization(populated_db):
         schema = api.realization(ensemble_id=1, realization_idx=0, filter=None)
         print("############### REALIZATION ###############")
         pprint.pprint(schema)
-        print("--------------- after --------------")
-        resolve_data_uri("http://localhost", schema)
-        resolve_ref_uri("http://localhost/ensembles/1", schema)
-        pprint.pprint(schema)
+
