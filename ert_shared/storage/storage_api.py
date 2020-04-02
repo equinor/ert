55b5a3b135bd77f4310bf64f0f475b3f1a6f58a7
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2020-04-02 10:09:26 +0200

    Fix documentation for return type

--- ert_shared/storage/storage_api.py
+++ ert_shared/storage/storage_api.py
@@ -46,6 +46,16 @@ class StorageApi(object):
                 {
                     "name" : "default"
                     "ensemble_ref" : "<ensemble_id>"
+                    "parent" : {
+                        "name" : "<parent_ensemble_name>"
+                        "ensemble_ref" : "<parent_ensemble_id>"
+                    }
+                    "children" : [
+                        {
+                            "name" : "<child_ensemble_name>"
+                            "ensemble_ref" : "<child_ensemble_id>"
+                        }    
+                    ]
                 }
             ]
         }
@@ -214,6 +224,16 @@ class StorageApi(object):
                     "name" : "<realization_idx>"
                     "realization_ref: "<realization_idx>"
                 }
+            ]
+            "parent" : {
+                "name" : "<parent_ensemble_name>"
+                "ensemble_ref" : "<parent_ensemble_id>"
+            }
+            "children" : [
+                {
+                    "name" : "<child_ensemble_name>"
+                    "ensemble_ref" : "<child_ensemble_id>"
+                }    
             ]
             "responses" : [
                 {
