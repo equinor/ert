6812881da0fff9babe7b7916f5b5c181f56e340e
Øystein Olai Heggen <ooh@equinor.com>
2020-04-03 15:33:47 +0200

    Add pragma foreign_keys on db's

--- ert_shared/storage/connections.py
+++ ert_shared/storage/connections.py
@@ -6,16 +6,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
 
 
-def get_rdb_connection(url):
+def get_rdb_connection(url, pragma_foreign_keys=True):
     msg = "Setting up session, using {}"
     logging.info(msg.format(url))
-    entities_engine = create_engine(url, echo=False)
-    Entities.metadata.create_all(entities_engine)
-    return entities_engine.connect()
+    engine = create_engine(url, echo=False)
+    if pragma_foreign_keys:
+        engine.execute('pragma foreign_keys=on')
+    Entities.metadata.create_all(engine)
+    return engine.connect()
 
-def get_blob_connection(url):
+def get_blob_connection(url, pragma_foreign_keys=True):
     msg = "Setting up engine, using {}"
     logging.info(msg.format(url))
-    blobs_engine = create_engine(url, echo=False)
-    Blobs.metadata.create_all(blobs_engine)
-    return blobs_engine.connect()
+    engine = create_engine(url, echo=False)
+    if pragma_foreign_keys:
+        engine.execute('pragma foreign_keys=on')
+    Blobs.metadata.create_all(engine)
+    return engine.connect()
