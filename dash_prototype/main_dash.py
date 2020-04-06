8df22dac6300c309ee776556aff78df3c12e73b7
Sondre Sortland <sonso@equinor.com>
2020-04-06 09:43:17 +0200

    Black dash prototypes

--- dash_prototype/main_dash.py
+++ dash_prototype/main_dash.py
@@ -14,12 +14,15 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 import json
+
 server_url = "http://127.0.0.1:5000"
 
 import requests
 
+
 def convertdate(dstring):
-    return datetime.strptime(dstring, '%Y-%m-%d %H:%M:%S')
+    return datetime.strptime(dstring, "%Y-%m-%d %H:%M:%S")
+
 
 def get_axis(data_url):
     resp = requests.get(data_url)
@@ -34,6 +37,7 @@ def get_data(data_url):
     resp = requests.get(data_url)
     data = resp.content.decode(resp.encoding)
     return list(map(float, data.split(",")))
+
 
 def get_ensembles():
     resp = requests.get("{base}/ensembles".format(base=server_url))
@@ -55,19 +59,20 @@ app.layout = html.Div(
                         html.H5("Ensemble"),
                         dcc.Dropdown(
                             id="ensemble-selector",
-                            options=[{"label": ensemble["name"], "value": ensemble["ref_url"]} for ensemble in ensembles],
+                            options=[
+                                {
+                                    "label": ensemble["name"],
+                                    "value": ensemble["ref_url"],
+                                }
+                                for ensemble in ensembles
+                            ],
                             value=ensembles[0]["ref_url"],
                         ),
                     ],
                     style={"width": "48%", "display": "inline-block"},
                 ),
                 html.Div(
-                    [
-                        html.H5("Response"),
-                        dcc.Dropdown(
-                            id="response-selector",
-                        ),
-                    ],
+                    [html.H5("Response"), dcc.Dropdown(id="response-selector",),],
                     style={"width": "48%", "float": "right", "display": "inline-block"},
                 ),
             ]
@@ -75,6 +80,7 @@ app.layout = html.Div(
         dcc.Graph(id="responses-graphic"),
     ]
 )
+
 
 def get_realizations_data(realizations, x_axis):
     realizations_data = list()
@@ -86,12 +92,13 @@ def get_realizations_data(realizations, x_axis):
             text="Realization {}".format(realization["name"]),
             name="Realization {}".format(realization["name"]),
             mode="line",
-            line = dict(color='royalblue'),
+            line=dict(color="royalblue"),
         )
         realizations_data.append(realization_data)
     return realizations_data
 
-def get_observations_data(observations,x_axis):
+
+def get_observations_data(observations, x_axis):
     return_plots = []
     for observation in observations:
         observation = observation["data"]
@@ -105,50 +112,51 @@ def get_observations_data(observations,x_axis):
             text="Observations",
             name="Observations",
             mode="markers",
-            marker=dict(color="red", size=10)
+            marker=dict(color="red", size=10),
         )
         lower_std_data = dict(
             x=x_axis_tmp,
-            y=[d-std for d, std in zip(data,stds)],
+            y=[d - std for d, std in zip(data, stds)],
             text="Observations std lower",
             name="Observations std lower",
             mode="line",
-            line=dict(color="red", dash="dash")
+            line=dict(color="red", dash="dash"),
         )
         upper_std_data = dict(
             x=x_axis_tmp,
-            y=[d+std for d, std in zip(data,stds)],
+            y=[d + std for d, std in zip(data, stds)],
             text="Observations std upper",
             name="Observations std upper",
             mode="line",
-            line=dict(color="red", dash="dash")
+            line=dict(color="red", dash="dash"),
         )
         return_plots.extend([observation_data, lower_std_data, upper_std_data])
     return return_plots
 
-@app.callback(
-    Output('response-selector', 'options'),
-    [Input('ensemble-selector', 'value')])
-def set_response_options(selected_ensemble_id):
-    ensemble_schema = api_request(selected_ensemble_id)
-    return [{'label': response["name"], 'value': response["ref_url"]} for response in ensemble_schema["responses"]]
 
 @app.callback(
-    Output('response-selector', 'value'),
-    [Input('response-selector', 'options')])
+    Output("response-selector", "options"), [Input("ensemble-selector", "value")]
+)
+def set_response_options(selected_ensemble_id):
+    ensemble_schema = api_request(selected_ensemble_id)
+    return [
+        {"label": response["name"], "value": response["ref_url"]}
+        for response in ensemble_schema["responses"]
+    ]
+
+
+@app.callback(
+    Output("response-selector", "value"), [Input("response-selector", "options")]
+)
 def set_responses_value(available_options):
-    return available_options[0]['value']
+    return available_options[0]["value"]
+
 
 @app.callback(
     Output("responses-graphic", "figure"),
-    [
-        Input("ensemble-selector", "value"),
-        Input("response-selector", "value"),
-    ],
+    [Input("ensemble-selector", "value"), Input("response-selector", "value"),],
 )
-def update_graph(
-    xaxis_column_name, yaxis_column_name
-):
+def update_graph(xaxis_column_name, yaxis_column_name):
     response = api_request(yaxis_column_name)
     x_axis = get_axis(response["axis"]["data_url"])
     plot_lines = get_realizations_data(response["realizations"], x_axis)
@@ -157,12 +165,8 @@ def update_graph(
     return {
         "data": plot_lines,
         "layout": dict(
-            xaxis={
-                "title": "Index",
-            },
-            yaxis={
-                "title": "Unit TODO",
-            },
+            xaxis={"title": "Index",},
+            yaxis={"title": "Unit TODO",},
             margin={"l": 40, "b": 40, "t": 10, "r": 0},
             hovermode="closest",
         ),
