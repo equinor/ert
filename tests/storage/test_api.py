5e031fa1a8f8ae782317c35dfc101daf5256bc57
Sondre Sortland <sonso@equinor.com>
2020-04-23 16:45:34 +0200

    Run api test only on python 3

--- tests/storage/test_api.py
+++ tests/storage/test_api.py
@@ -1,35 +1,35 @@
-# test_api.py
-import requests
-import schemathesis
-import pytest
+if sys.version_info.major >= 3:
+    import requests
+    import schemathesis
+    import pytest
+    import sys
 
-from ert_shared.storage.http_server import FlaskWrapper
+    from ert_shared.storage.http_server import FlaskWrapper
 
-from tests.storage import populated_db
-
-
-@pytest.fixture()
-def test_schema(populated_db):
-    # Flask provides a way to test your application by exposing the Werkzeug test Client
-    # and handling the context locals for you.
-    flWrapper = FlaskWrapper(rdb_url=populated_db, blob_url=populated_db)
-    # Establish an application context before running the tests.
-    ctx = flWrapper.app.app_context()
-    ctx.push()
-    yield schemathesis.from_wsgi("/schema.json", flWrapper.app)
-    ctx.pop()
+    from tests.storage import populated_db
 
 
-schema = schemathesis.from_pytest_fixture("test_schema")
+    @pytest.fixture()
+    def test_schema(populated_db):
+        # Flask provides a way to test your application by exposing the Werkzeug test Client
+        # and handling the context locals for you.
+        flWrapper = FlaskWrapper(rdb_url=populated_db, blob_url=populated_db)
+        # Establish an application context before running the tests.
+        ctx = flWrapper.app.app_context()
+        ctx.push()
+        yield schemathesis.from_wsgi("/schema.json", flWrapper.app)
+        ctx.pop()
 
 
-@schema.parametrize()
-def test_no_server_errors(case):
-    response = case.call_wsgi()
-    try:
-        case.validate_response(response)
-    except AssertionError as e:
-        if "500" in str(e):
-            print("Expected failure, API still in beta")
-        else:
-            raise
+    schema = schemathesis.from_pytest_fixture("test_schema")
+
+    @schema.parametrize()
+    def test_no_server_errors(case):
+        response = case.call_wsgi()
+        try:
+            case.validate_response(response)
+        except AssertionError as e:
+            if "500" in str(e):
+                print("Expected failure, API still in beta")
+            else:
+                raise
