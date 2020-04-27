8b5d4d478b266bdf9b06e4b620b117f249b710eb
Sondre Sortland <sonso@equinor.com>
2020-04-27 10:08:09 +0200

    Add public HTTP API

---
+++ ert_shared/storage/http_server.py
@@ -0,0 +1,194 @@
+import flask
+import os
+import yaml
+import werkzeug.exceptions as werkzeug_exc
+from ert_shared.storage.blob_api import BlobApi
+from ert_shared.storage.rdb_api import RdbApi
+from ert_shared.storage.storage_api import StorageApi
+from ert_shared.storage import connections
+from flask import Response, request
+
+
+def resolve_ensemble_uri(ensemble_ref):
+    BASE_URL = request.host_url
+    return "{}ensembles/{}".format(BASE_URL, ensemble_ref)
+
+
+def resolve_ref_uri(struct, ensemble_id=None):
+    if isinstance(struct, list):
+        for item in struct:
+            resolve_ref_uri(item, ensemble_id)
+    elif isinstance(struct, dict):
+        for key, val in struct.copy().items():
+            split_key = key.split("_")
+
+            if len(split_key) == 2 and split_key[1] == "ref" in key:
+                type_name = split_key[0]
+                if type_name == "realization":
+                    base = resolve_ensemble_uri(ensemble_id)
+                    struct["ref_url"] = "{}/realizations/{}".format(base, val)
+                elif type_name == "ensemble":
+                    struct["ref_url"] = resolve_ensemble_uri(val)
+                elif type_name == "response":
+                    base = resolve_ensemble_uri(ensemble_id)
+                    struct["ref_url"] = "{}/responses/{}".format(base, val)
+                elif type_name == "parameter":
+                    base = resolve_ensemble_uri(ensemble_id)
+                    struct["ref_url"] = "{}/parameters/{}".format(base, val)
+                elif type_name == "data":
+                    struct["data_url"] = "{}data/{}".format(request.host_url, val)
+                else:
+                    continue
+                del struct[key]
+            else:
+                resolve_ref_uri(val, ensemble_id)
+
+
+class FlaskWrapper:
+    def __init__(self, rdb_url=None, blob_url=None):
+        self._rdb_url = rdb_url
+        self._blob_url = blob_url
+
+        self.app = flask.Flask("ert http api")
+        self.app.add_url_rule("/ensembles", "ensembles", self.ensembles)
+        self.app.add_url_rule(
+            "/ensembles/<ensemble_id>", "ensemble", self.ensemble_by_id
+        )
+        self.app.add_url_rule(
+            "/ensembles/<ensemble_id>/realizations/<realization_idx>",
+            "realization",
+            self.realization_by_id,
+        )
+        self.app.add_url_rule(
+            "/ensembles/<ensemble_id>/responses/<response_name>",
+            "response",
+            self.response_by_name,
+        )
+        self.app.add_url_rule(
+            "/ensembles/<ensemble_id>/parameters/<parameter_def_id>",
+            "parameter",
+            self.parameter_by_id,
+        )
+        self.app.add_url_rule("/data/<int:data_id>", "data", self.data)
+        self.app.add_url_rule(
+            "/observation/<name>",
+            "get_observation",
+            self.get_observation,
+            methods=["GET"],
+        )
+        self.app.add_url_rule(
+            "/observation/<name>/attributes",
+            "get_observation_attributes",
+            self.get_observation_attributes,
+            methods=["GET"],
+        )
+        self.app.add_url_rule(
+            "/observation/<name>/attributes",
+            "set_observation_attributes",
+            self.set_observation_attributes,
+            methods=["POST"],
+        )
+        self.app.add_url_rule("/shutdown", "shutdown", self.shutdown, methods=["POST"])
+        self.app.add_url_rule(
+            "/schema.json", "schema", self.schema, methods=["GET"],
+        )
+
+    def schema(self):
+        cur_path = os.path.dirname(os.path.abspath(__file__))
+        schema_file = os.path.join(cur_path, "oas.yml")
+        with open(schema_file) as f:
+            return yaml.safe_load(f)
+
+    def ensembles(self):
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            ensembles = api.get_ensembles()
+            resolve_ref_uri(ensembles)
+            return ensembles
+
+    def ensemble_by_id(self, ensemble_id):
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            ensemble = api.get_ensemble(ensemble_id)
+            resolve_ref_uri(ensemble, ensemble_id)
+            return ensemble
+
+    def realizations(self, ensemble_id):
+        pass
+
+    def realization_by_id(self, ensemble_id, realization_idx):
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            realization = api.get_realization(ensemble_id, realization_idx, None)
+            resolve_ref_uri(realization, ensemble_id)
+            return realization
+
+    def response_by_name(self, ensemble_id, response_name):
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            response = api.get_response(ensemble_id, response_name, None)
+            resolve_ref_uri(response, ensemble_id)
+            return response
+
+    def parameter_by_id(self, ensemble_id, parameter_def_id):
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            parameter = api.get_parameter(ensemble_id, parameter_def_id)
+            resolve_ref_uri(parameter, ensemble_id)
+            return parameter
+
+    def data(self, data_id):
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            data = api.get_data(data_id)
+            if isinstance(data, list):
+                return ",".join([str(x) for x in data])
+            else:
+                return str(data)
+
+    def get_observation(self, name):
+        """Return an observation."""
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            obs = api.get_observation(name)
+            if obs is None:
+                raise werkzeug_exc.NotFound()
+            return obs
+
+    def get_observation_attributes(self, name):
+        """Return attributes for an observation.
+
+        {
+            "attributes": {...}
+        }
+        """
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            attrs = api.get_observation_attributes(name)
+            if attrs is None:
+                raise werkzeug_exc.NotFound()
+            return attrs
+
+    def set_observation_attributes(self, name):
+        """Set attributes on an observation.
+
+        The posted JSON will be expected to be
+        {
+            "attributes": {
+                "region": "1",
+                "depth": "2892.1"
+            }
+        }
+        """
+        with StorageApi(rdb_url=self._rdb_url, blob_url=self._blob_url) as api:
+            js = request.get_json()
+            for k, v in js["attributes"].items():
+                obs = api.set_observation_attribute(name, k, v)
+                if obs is None:
+                    raise werkzeug_exc.NotFound()
+            return api.get_observation(name), 201
+
+    def shutdown(self):
+        request.environ.get("werkzeug.server.shutdown")()
+        return "Server shutting down."
+
+
+def run_server(args):
+    wrapper = FlaskWrapper(
+        rdb_url="sqlite:///entities.db", blob_url="sqlite:///blobs.db"
+    )
+    (bind_host, bind_port) = args.bind.split(":")
+
+    wrapper.app.run(host=bind_host, port=bind_port, debug=args.debug)
---
+++ tests/storage/test_flask.py
@@ -0,0 +1,122 @@
+import json
+import pprint
+
+import flask
+import pytest
+from ert_shared.storage.blob_api import BlobApi
+from ert_shared.storage.http_server import FlaskWrapper
+from ert_shared.storage.rdb_api import RdbApi
+from flask import Response, request
+
+from tests.storage import populated_db
+
+
+@pytest.fixture()
+def test_client(populated_db):
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    flWrapper = FlaskWrapper(rdb_url=populated_db, blob_url=populated_db)
+    testing_client = flWrapper.app.test_client()
+    # Establish an application context before running the tests.
+    ctx = flWrapper.app.app_context()
+    ctx.push()
+    yield testing_client
+    ctx.pop()
+
+
+def test_api(test_client):
+    response = test_client.get("/ensembles")
+    print(response.data)
+    print(response.mimetype)
+    ensembles = json.loads(response.data)
+
+    for ens in ensembles["ensembles"]:
+        print("########## ENSEMBLE #############")
+        url = ens["ref_url"]
+        ensemble = json.loads(test_client.get(url).data)
+        pprint.pprint(ensemble)
+
+        for real in ensemble["realizations"]:
+            print("########## ENSEMBLE - realization #############")
+            realization = json.loads(test_client.get(real["ref_url"]).data)
+            pprint.pprint(realization)
+
+            for response in realization["responses"]:
+                print("########## ENSEMBLE - realization - response #############")
+                response_data = test_client.get(response["data_url"])
+                print(response_data.data)
+
+        for response in ensemble["responses"]:
+            print("########## ENSEMBLE - response #############")
+            response_data = test_client.get(response["ref_url"])
+            pprint.pprint(json.loads(response_data.data))
+
+
+def test_observation(test_client):
+    resp = test_client.get("/ensembles/1")
+    ens = json.loads(resp.data)
+    expected = {
+        ("active_mask", "True,False"),
+        ("data_indexes", "2,3"),
+        ("key_indexes", "0,3"),
+        ("std", "1,3"),
+        ("values", "10.1,10.2"),
+    }
+
+    actual = set()
+
+    resp_url = ens["responses"][0]["ref_url"]
+    resp_data = test_client.get(resp_url).data.decode()
+    resp = json.loads(resp_data)
+    observations = resp["observations"]
+
+    for obs in observations:
+        for name, data_def in obs["data"].items():
+            url = data_def["data_url"]
+            resp = test_client.get(url)
+            actual.add((name, resp.data.decode("utf-8")))
+
+    assert actual == expected
+
+
+def test_get_single_observation(test_client):
+    resp = test_client.get("/observation/observation_one")
+    obs = json.loads(resp.data)
+    expected = {
+        "attributes": {"region": "1"},
+        "name": "observation_one",
+        "data": {
+            "data_indexes": {"data_ref": 2},
+            "key_indexes": {"data_ref": 1},
+            "std": {"data_ref": 4},
+            "values": {"data_ref": 3},
+        },
+    }
+
+    assert obs == expected
+
+
+def test_get_single_observation_404(test_client):
+    resp = test_client.get("/observation/not_existing")
+    assert resp.status_code == 404
+
+
+def test_get_observation_attributes(test_client):
+    create_resp = test_client.post(
+        "/observation/observation_one/attributes",
+        data="""{"attributes": {"region": "1", "depth": "9000"}}""",
+        content_type="application/json",
+    )
+    assert create_resp.status_code == 201, create_resp.status
+    resp = test_client.get("/observation/observation_one/attributes")
+    obs = json.loads(resp.data)
+    expected = {"attributes": {"region": "1", "depth": "9000"}}
+    assert obs == expected
+
+
+def test_parameter(test_client):
+    resp = test_client.get("/ensembles/1/parameters/3")
+    schema = json.loads(resp.data)
+    assert schema["key"] == "key1"
+    assert schema["group"] == "group"
+    assert schema["prior"]["function"] == "function"
