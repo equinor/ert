ba16f984a28273032805509c0dedc721c363e6a3
Åsmund Birkeland <aasmund.birkeland@gmail.com>
2020-05-07 13:22:06 +0200

    Feature toggle as decorator

--- ert_shared/feature_toggling.py
+++ ert_shared/feature_toggling.py
@@ -10,17 +10,17 @@ class _Feature:
         self.is_togglable = not (python_major_version < 3 and python3_only)
 
 class FeatureToggling:
-    __conf = {
+    _conf = {
         "new-storage": _Feature(default_enabled=False, python3_only=True),
     }
 
     @staticmethod
     def is_enabled(feature_name):
-        return FeatureToggling.__conf[feature_name].is_enabled
+        return FeatureToggling._conf[feature_name].is_enabled
 
     @staticmethod
     def add_feature_toggling_args(parser):
-        for feature_name, feature in FeatureToggling.__conf.items():
+        for feature_name, feature in FeatureToggling._conf.items():
             if not feature.is_togglable:
                 continue
 
@@ -34,7 +34,7 @@ class FeatureToggling:
     @staticmethod
     def update_from_args(args):
         args_dict = vars(args)
-        for feature_name, feature in FeatureToggling.__conf.items():
+        for feature_name, feature in FeatureToggling._conf.items():
             if not feature.is_togglable:
                 continue
 
@@ -42,11 +42,21 @@ class FeatureToggling:
             feature_name_escaped = arg_name.replace("-", "_")
 
             if feature_name_escaped in args_dict and args_dict[feature_name_escaped]:
-                current_state = FeatureToggling.__conf[feature_name].is_enabled
-                FeatureToggling.__conf[feature_name].is_enabled = not current_state
+                current_state = FeatureToggling._conf[feature_name].is_enabled
+                FeatureToggling._conf[feature_name].is_enabled = not current_state
 
     @staticmethod
     def _get_arg_name(feature_name):
-        default_state = FeatureToggling.__conf[feature_name].is_enabled
+        default_state = FeatureToggling._conf[feature_name].is_enabled
         arg_default_state = "disable" if default_state else "enable"
         return "{}-{}".format(arg_default_state, feature_name)
+
+def feature_enabled(feature_name):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if FeatureToggling.is_enabled(feature_name):
+                return func(*args)
+            else:
+                return None
+        return wrapper
+    return decorator
