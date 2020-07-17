a04409c053da9af8b237ef1f67bcd581e7eb84a9
Sondre Sortland <sonso@equinor.com>
2020-07-17 15:01:22 +0200

    Update testKomodo to unified testing script

--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -1,56 +1,27 @@
-#!/bin/bash
-PROJECT="ert"
-RELEASE_PATH=${KOMODO_ROOT}/${RELEASE_NAME}
-echo "unit testing ${PROJECT} against ${RELEASE_NAME}"
 
-source ${RELEASE_PATH}/enable
-export LD_LIBRARY_PATH=${RELEASE_PATH}/root/lib:${RELEASE_PATH}/root/lib64
+copy_test_files () {
+    cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
+    cp -r $CI_SOURCE_ROOT/test-data $CI_TEST_ROOT/test-data
+    # Trick ERT to find a fake source root
+    mkdir $CI_TEST_ROOT/.git
+}
 
-source ${DEVTOOL}/enable
-GCC_VERSION=7.3.0 CMAKE_VERSION=3.10.2 source ${SDPSOFT}/env.sh
+install_test_dependencies () {
+    pip install -r dev-requirements.txt
+}
 
-GIT=${SDPSOFT}/bin/git
+install_package () {
+    pip install . --no-deps
+}
 
-# Killing ert spawn an eternal loop. Issue occurs when tests segfault.
-# See ert/issues/705, libecl/issues/694 and libecl/pull/700
-export ECL_SKIP_SIGNAL=1
-
-if [[ -z "${sha1// }" ]]; then
-    # this is not a PR build, the komodo everest verison is checked out
-    EV=$(cat ${RELEASE_PATH}/${RELEASE_NAME} | grep "${PROJECT}:" -A2 | grep "version:")
-    EV=($EV)    # split the string "version: vX.X.X"
-    EV=${EV[1]} # extract the version
-    EV=${EV%"+py3"}
-    echo "Using ${PROJECT} version ${EV}"
-    $GIT checkout $EV
-fi
-
-echo "Creating virtualenv"
-ENV="testenv"
-rm -rf $ENV
-mkdir $ENV
-python -m virtualenv --system-site-packages $ENV
-source $ENV/bin/activate
-python -m pip install -r dev-requirements.txt
-
-if [[ -z "${sha1// }" ]]; then
-    # Run in a new folder so that we dont load the other python code from the source, but rather run against komodo
-    rm -rf tmptest
-    mkdir tmptest
-    cp -r tests tmptest/tests
-    cp -r test-data tmptest/test-data
-    pushd tmptest
-fi
-
-
-echo "Running pytest"
-
-if [[ ${RELEASE_NAME} =~ py27$  ]]
-then
-    export PYTEST_QT_API=pyqt4v2
-fi
-# The existence of a running xvfb process will produce
-# a lock file for the default server and kill the run
-# Allow xvfb to find a new server
- xvfb-run -s "-screen 0 640x480x24" --auto-servernum python -m \
+start_tests () {
+    if [[ ${CI_KOMODO_RELEASE} =~ py27$  ]]
+    then
+        export PYTEST_QT_API=pyqt4v2
+    fi
+    # The existence of a running xvfb process will produce
+    # a lock filgit ree for the default server and kill the run
+    # Allow xvfb to find a new server
+    xvfb-run -s "-screen 0 640x480x24" --auto-servernum python -m \
     pytest -k "not test_gui_load and not test_formatting"
+}
