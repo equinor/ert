ae5cb6e83fe073102c8bcbef56fe2c3ab86b1bae
jeanpaul <jepebe@users.noreply.github.com>
2020-09-09 16:00:45 +0200

    fixup! Add test that checks validity of the jobs with common executable

--- python/tests/share/test_shell_jobs.py
+++ python/tests/share/test_shell_jobs.py
@@ -11,17 +11,16 @@ class TestSiteConfigShellJobs(ResTest):
         share_path = self.createSharePath("ert/shell_scripts")
 
         res_config = ResConfig(config_file)
-        site_config: SiteConfig = res_config.site_config
+        site_config = res_config.site_config
 
         installed_jobs = site_config.get_installed_jobs()
         fm_shell_jobs = {}
-        job: ExtJob
         for job in installed_jobs:
             exe = job.get_executable()
             if exe.startswith(share_path):
                 fm_shell_jobs[job.name().lower()] = exe
 
-        list_from_content: ErtWorkflowList = res_config.ert_workflow_list
+        list_from_content = res_config.ert_workflow_list
         wf_shell_jobs = {}
         for wf_name in list_from_content.getJobNames():
             exe = list_from_content.getJob(wf_name).executable()
