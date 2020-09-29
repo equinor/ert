785800e1dcc84c7e165f622412a6982951a511a6
jeanpaul <jepebe@users.noreply.github.com>
2020-09-29 07:44:41 +0200

    Reduce the amount of work done during the test
    Instead of doing actual work during the test this commit make the simulation sleep for 1 second and then finish. The test itself does not care about the results from the test.

--- python/tests/res/simulator/test_simulation_context.py
+++ python/tests/res/simulator/test_simulation_context.py
@@ -13,65 +13,68 @@ class SimulationContextTest(ResTest):
 
     @tmpdir()
     def test_simulation_context(self):
-        config_file = self.createTestPath("local/snake_oil_no_data/snake_oil.ert")
-        with ErtTestContext("ert/server/rpc/simulation_context", config_file) as test_context:
+        config_file = self.createTestPath("local/batch_sim/sleepy_time.ert")
+        with ErtTestContext("res/sim/simulation_context", config_file) as test_context:
             ert = test_context.getErt()
 
             size = 4
-            mask1 = BoolVector( initial_size = size )
-            mask2 = BoolVector( initial_size = size )
+            even_mask = BoolVector(initial_size=size)
+            odd_mask = BoolVector(initial_size=size)
 
-            for iens_2 in range(size//2):
-                mask1[2*iens_2] = True
-                mask1[2*iens_2 + 1] = False
+            for iens_2 in range(size // 2):
+                even_mask[2 * iens_2] = True
+                even_mask[2 * iens_2 + 1] = False
 
-                mask2[2*iens_2] = False
-                mask2[2*iens_2 + 1] = True
+                odd_mask[2 * iens_2] = False
+                odd_mask[2 * iens_2 + 1] = True
 
             fs_manager = ert.getEnkfFsManager()
-            first_half = fs_manager.getFileSystem("first_half")
-            other_half = fs_manager.getFileSystem("other_half")
+            even_half = fs_manager.getFileSystem("even_half")
+            odd_half = fs_manager.getFileSystem("odd_half")
 
             # i represents geo_id
-            case_data = [(i,{}) for i in range(size)]
-            simulation_context1 = SimulationContext(ert, first_half, mask1 , 0, case_data)
-            simulation_context2 = SimulationContext(ert, other_half, mask2 , 0, case_data)
+            case_data = [(i, {}) for i in range(size)]
+            even_ctx = SimulationContext(ert, even_half, even_mask, 0, case_data)
+            odd_ctx = SimulationContext(ert, odd_half, odd_mask, 0, case_data)
 
             for iens in range(size):
+                # do we have the proper geo_id in run_args?
                 if iens % 2 == 0:
-                    self.assertFalse(simulation_context1.isRealizationFinished(iens))
-                    # do we have the proper geo_id in run_args?
-                    self.assertEqual(simulation_context1.get_run_args(iens).geo_id, iens)
+                    self.assertFalse(even_ctx.isRealizationFinished(iens))
+                    self.assertEqual(even_ctx.get_run_args(iens).geo_id, iens)
                 else:
-                    self.assertFalse(simulation_context2.isRealizationFinished(iens))
-                    self.assertEqual(simulation_context2.get_run_args(iens).geo_id, iens)
+                    self.assertFalse(odd_ctx.isRealizationFinished(iens))
+                    self.assertEqual(odd_ctx.get_run_args(iens).geo_id, iens)
+
+            def any_is_running():
+                return even_ctx.isRunning() or odd_ctx.isRunning()
 
             wait_until(
-                func=(lambda: self.assertFalse(simulation_context1.isRunning() or simulation_context2.isRunning())),
+                func=(lambda: self.assertFalse(any_is_running())),
                 timeout=90
             )
 
-            self.assertEqual(simulation_context1.getNumFailed(), 0)
-            self.assertEqual(simulation_context1.getNumRunning(), 0)
-            self.assertEqual(simulation_context1.getNumSuccess(), size/2)
+            self.assertEqual(even_ctx.getNumFailed(), 0)
+            self.assertEqual(even_ctx.getNumRunning(), 0)
+            self.assertEqual(even_ctx.getNumSuccess(), size / 2)
 
-            self.assertEqual(simulation_context2.getNumFailed(), 0)
-            self.assertEqual(simulation_context2.getNumRunning(), 0)
-            self.assertEqual(simulation_context2.getNumSuccess(), size/2)
+            self.assertEqual(odd_ctx.getNumFailed(), 0)
+            self.assertEqual(odd_ctx.getNumRunning(), 0)
+            self.assertEqual(odd_ctx.getNumSuccess(), size / 2)
 
-            first_half_state_map = first_half.getStateMap()
-            other_half_state_map = other_half.getStateMap()
+            even_state_map = even_half.getStateMap()
+            odd_state_map = odd_half.getStateMap()
 
             for iens in range(size):
                 if iens % 2 == 0:
-                    self.assertTrue(simulation_context1.didRealizationSucceed(iens))
-                    self.assertFalse(simulation_context1.didRealizationFail(iens))
-                    self.assertTrue(simulation_context1.isRealizationFinished(iens))
+                    self.assertTrue(even_ctx.didRealizationSucceed(iens))
+                    self.assertFalse(even_ctx.didRealizationFail(iens))
+                    self.assertTrue(even_ctx.isRealizationFinished(iens))
 
-                    self.assertEqual(first_half_state_map[iens], RealizationStateEnum.STATE_HAS_DATA)
+                    self.assertEqual(even_state_map[iens], RealizationStateEnum.STATE_HAS_DATA)
                 else:
-                    self.assertTrue(simulation_context2.didRealizationSucceed(iens))
-                    self.assertFalse(simulation_context2.didRealizationFail(iens))
-                    self.assertTrue(simulation_context2.isRealizationFinished(iens))
+                    self.assertTrue(odd_ctx.didRealizationSucceed(iens))
+                    self.assertFalse(odd_ctx.didRealizationFail(iens))
+                    self.assertTrue(odd_ctx.isRealizationFinished(iens))
 
-                    self.assertEqual(other_half_state_map[iens], RealizationStateEnum.STATE_HAS_DATA)
+                    self.assertEqual(odd_state_map[iens], RealizationStateEnum.STATE_HAS_DATA)
