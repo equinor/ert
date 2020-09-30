8fa8616cae92e956d40178a338162052de225049
Jonas G. Drange <jond@equinor.com>
2020-09-30 15:58:56 +0200

    Add a queue adaptor
    Adds the queue adaptor that, for ensemble_experiment, will send json
    events on a specific websocket for each "update" of the JobQueueManager
    object.

---
+++ ert_shared/ensemble_evaluator/ws_util.py
@@ -0,0 +1,20 @@
+import websockets
+import asyncio
+
+
+def wait_for_ws(url, max_retries=3):
+    asyncio.get_event_loop().run_until_complete(wait(url, max_retries))
+
+
+async def wait(url, max_retries):
+    retries = 0
+    while retries < max_retries:
+        try:
+            async with websockets.connect(url):
+                pass
+
+            return
+        except OSError as e:
+            print(f"{__name__} failed to connect ({retries}/{max_retries}: {e}")
+            retries += 1
+            await asyncio.sleep(0.2)
---
+++ tests/ensemble_evaluator/test_queue_adaptor_integration.py
@@ -0,0 +1,65 @@
+import asyncio
+import threading
+
+import pytest
+import websockets
+from ert_shared.ensemble_evaluator.queue_adaptor import JobQueueManagerAdaptor
+from mock import Mock
+
+
+async def mock_ws(host, port):
+    done = asyncio.get_event_loop().create_future()
+    events = []
+
+    async def _handler(websocket, path):
+        while True:
+            event = await websocket.recv()
+            events.append(event)
+            if event == "null":
+                done.set_result(None)
+                break
+
+    async with websockets.serve(_handler, host, port):
+        await done
+    return events
+
+
+def mock_queue_mutator(host, port):
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    mock_queue = Mock(
+        job_list=[Mock(status=Mock(value=4), callback_arguments=[Mock(iens=0)])]
+    )
+    JobQueueManagerAdaptor.ws_url = f"ws://{host}:{port}"
+    jm = JobQueueManagerAdaptor(mock_queue)
+
+    mock_queue.job_list[0].status.value = 16  # running
+    jm._publish_changes(jm._changes_after_transition())
+
+    mock_queue.job_list[0].status.value = 512  # done
+    jm._transition()
+    jm._publish_changes(jm._snapshot())
+    jm._publish_changes(None)
+
+
+@pytest.mark.asyncio
+async def test_happy_path(tmpdir):
+    host = "localhost"
+    port = 50001
+
+    mutator = threading.Thread(target=mock_queue_mutator, args=(host, port))
+    mutator.start()
+
+    done, _ = await asyncio.wait(
+        (asyncio.get_event_loop().create_task(mock_ws(host, port)),),
+        timeout=2,
+        return_when=asyncio.FIRST_EXCEPTION,
+    )
+
+    mutator.join()
+
+    assert len(done) == 1
+    assert done.pop().result() == [
+        '{"0": {"status": "JOB_QUEUE_RUNNING", "forward_models": null}}',
+        '{"0": {"status": "JOB_QUEUE_SUCCESS", "forward_models": null}}',
+        "null",
+    ]
