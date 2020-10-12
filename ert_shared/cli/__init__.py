e155d40c416f1534d6a7c95095ba49680267679f
Frode Aarstad (IT SI SIB) <faar@lt-lx106069.client.statoil.net>
2020-10-12 08:24:42 +0200

    Expose new iterative algorithm in the CLI

--- ert_shared/cli/__init__.py
+++ ert_shared/cli/__init__.py
@@ -1,5 +1,6 @@
-ENSEMBLE_SMOOTHER_MODE = 'ensemble_smoother'
-ENSEMBLE_EXPERIMENT_MODE = 'ensemble_experiment'
-ES_MDA_MODE = 'es_mda'
-TEST_RUN_MODE = 'test_run'
-WORKFLOW_MODE = 'workflow'
+ENSEMBLE_SMOOTHER_MODE = "ensemble_smoother"
+ENSEMBLE_EXPERIMENT_MODE = "ensemble_experiment"
+ITERATIVE_ENSEMBLE_SMOOTHER_MODE = "iterative_ensemble_smoother"
+ES_MDA_MODE = "es_mda"
+TEST_RUN_MODE = "test_run"
+WORKFLOW_MODE = "workflow"
--- ert_shared/cli/main.py
+++ ert_shared/cli/main.py
@@ -10,7 +10,13 @@ from ert_shared.cli.model_factory import create_model
 from ert_shared.cli.monitor import Monitor
 from ert_shared.cli.notifier import ErtCliNotifier
 from ert_shared.cli.workflow import execute_workflow
-from ert_shared.cli import WORKFLOW_MODE, ENSEMBLE_SMOOTHER_MODE, ES_MDA_MODE, ENSEMBLE_EXPERIMENT_MODE
+from ert_shared.cli import (
+    WORKFLOW_MODE,
+    ENSEMBLE_SMOOTHER_MODE,
+    ITERATIVE_ENSEMBLE_SMOOTHER_MODE,
+    ES_MDA_MODE,
+    ENSEMBLE_EXPERIMENT_MODE,
+)
 from ert_shared.tracker.factory import create_tracker
 from res.enkf import EnKFMain, ResConfig
 
@@ -21,7 +27,7 @@ def _clear_and_exit(args):
 
 
 def run_cli(args):
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
     res_config = ResConfig(args.config)
     os.chdir(res_config.config_path)
     ert = EnKFMain(res_config, strict=True, verbose=args.verbose)
@@ -42,8 +48,11 @@ def run_cli(args):
         if "current_case" in args and args.current_case:
             ERT.enkf_facade.select_or_create_new_case(args.current_case)
 
-        if (args.mode in [ENSEMBLE_SMOOTHER_MODE, ES_MDA_MODE] and 
-            args.target_case == ERT.enkf_facade.get_current_case_name()):
+        if (
+            args.mode
+            in [ENSEMBLE_SMOOTHER_MODE, ITERATIVE_ENSEMBLE_SMOOTHER_MODE, ES_MDA_MODE]
+            and args.target_case == ERT.enkf_facade.get_current_case_name()
+        ):
             msg = (
                 "ERROR: Target file system and source file system can not be the same. "
                 "They were both: {}.".format(args.target_case)
@@ -53,7 +62,7 @@ def run_cli(args):
         thread = threading.Thread(
             name="ert_cli_simulation_thread",
             target=model.startSimulations,
-            args=(argument,)
+            args=(argument,),
         )
         thread.start()
 
