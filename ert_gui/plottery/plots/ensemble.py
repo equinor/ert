fc97ab12a92dd113f72f7ff766b06ee1c2bccb73
jeanpaul <jepebe@users.noreply.github.com>
2020-10-12 07:55:03 +0200

    Add Black formatting

--- ert_gui/plottery/plots/ensemble.py
+++ ert_gui/plottery/plots/ensemble.py
@@ -2,10 +2,10 @@ from .observations import plotObservations
 from .plot_tools import PlotTools
 from ert_gui.plottery.plots.history import plotHistory
 from ert_gui.plottery.plots.refcase import plotRefcase
+from pandas import DataFrame
 
 
 class EnsemblePlot(object):
-
     def __init__(self):
         self.dimensionality = 2
 
@@ -21,6 +21,7 @@ class EnsemblePlot(object):
         plot_context.y_axis = plot_context.VALUE_AXIS
         plot_context.x_axis = plot_context.DATE_AXIS
 
+        date_support_active = plot_context.isDateSupportActive()
         for case, data in case_to_data_map.items():
             data = data.T
 
@@ -29,18 +30,23 @@ class EnsemblePlot(object):
                     plot_context.deactivateDateSupport()
                     plot_context.x_axis = plot_context.INDEX_AXIS
 
-                self._plotLines(axes, config, data, case, plot_context.isDateSupportActive())
+                self._plotLines(axes, config, data, case, date_support_active)
                 config.nextColor()
 
         plotRefcase(plot_context, axes)
         plotObservations(observation_data, plot_context, axes)
         plotHistory(plot_context, axes)
 
-        default_x_label = "Date" if plot_context.isDateSupportActive() else "Index"
-        PlotTools.finalizePlot(plot_context, figure, axes, default_x_label=default_x_label, default_y_label="Value")
+        default_x_label = "Date" if date_support_active else "Index"
+        PlotTools.finalizePlot(
+            plot_context,
+            figure,
+            axes,
+            default_x_label=default_x_label,
+            default_y_label="Value",
+        )
 
-
-    def _plotLines(self, axes, plot_config, data, ensemble_label, is_date_supported):
+    def _plotLines(self, axes, plot_config, data, ensemble_label, is_dated):
         """
         @type axes: matplotlib.axes.Axes
         @type plot_config: ert_gui.plottery.PlotConfig
@@ -50,18 +56,35 @@ class EnsemblePlot(object):
 
         style = plot_config.defaultStyle()
 
-        if len(data) == 1 and style.marker == '':
-            style.marker = '.'
+        if len(data) == 1 and style.marker == "":
+            style.marker = "."
 
-        if is_date_supported:
-            lines = axes.plot_date(x=data.index.values, y=data, color=style.color, alpha=style.alpha, marker=style.marker, linestyle=style.line_style, linewidth=style.width, markersize=style.size)
+        if is_dated:
+            lines = axes.plot_date(
+                x=data.index.values,
+                y=data,
+                color=style.color,
+                alpha=style.alpha,
+                marker=style.marker,
+                linestyle=style.line_style,
+                linewidth=style.width,
+                markersize=style.size,
+            )
         else:
             index = data.index.values
-            from pandas import DataFrame
             if isinstance(data, DataFrame):
                 data = data.to_numpy()
 
-            lines = axes.plot(index, data, color=style.color, alpha=style.alpha, marker=style.marker, linestyle=style.line_style, linewidth=style.width, markersize=style.size)
+            lines = axes.plot(
+                index,
+                data,
+                color=style.color,
+                alpha=style.alpha,
+                marker=style.marker,
+                linestyle=style.line_style,
+                linewidth=style.width,
+                markersize=style.size,
+            )
 
         if len(lines) > 0:
             plot_config.addLegendItem(ensemble_label, lines[0])
