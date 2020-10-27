08e754ec253a389cf4893b9e9a3cb8f3573b186a
Frode Aarstad (IT SI SIB) <frodeaarstad@gmail.com>
2020-10-27 08:38:31 +0100

    Add test for legacy ensemble builder

--- tests/ensemble_evaluator/test_ensemble_builder.py
+++ tests/ensemble_evaluator/test_ensemble_builder.py
@@ -1,5 +1,7 @@
 import ert_shared.ensemble_evaluator.entity.ensemble as ee
 
+from unittest.mock import Mock, MagicMock
+
 
 def test_build_ensemble():
     ensemble = ee.create_ensemble_builder().add_realization(
@@ -29,4 +31,26 @@ def test_build_ensemble():
     assert real.is_active()
 
 
-# def test_build_ensemble_legacy
+def test_build_ensemble_legacy():
+
+    run_context = MagicMock()
+    run_context.__len__.return_value = 1
+    run_context.is_active = lambda i: True if i == 0 else False
+
+    ext_job = MagicMock()
+    ext_job.get_executable = MagicMock(return_value="junk.exe")
+    ext_job.name = MagicMock(return_value="junk")
+    ext_job.get_arglist = MagicMock(return_value=("arg1", "arg2", "arg3"))
+
+    forward_model = MagicMock()
+    forward_model.__len__.return_value = 1
+    forward_model.iget_job = lambda i: ext_job if i == 0 else None
+
+    ensemble_builder = ee.create_ensemble_builder_from_legacy(
+        run_context, forward_model
+    )
+
+    ensemble = ensemble_builder.build()
+
+    real = ensemble.get_reals()[0]
+    assert real.is_active()
