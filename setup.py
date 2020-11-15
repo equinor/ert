96949183803373e7ac8bb471ae69a6c5dfdab924
Markus Dregi <markusdregi@gmail.com>
2020-11-15 19:47:33 +0100

    Apply black

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     use_scm_version={"root": ".", "write_to": "ert_shared/version.py"},
     scripts=["ert_shared/bin/ert"],
     entry_points={
-        "console_scripts":"ert3=ert3.console:main",
+        "console_scripts": "ert3=ert3.console:main",
     },
     packages=find_packages(exclude=["tests*"]),
     package_data={"ert_gui": extra_files, "ert_logging": logging_configuration},
