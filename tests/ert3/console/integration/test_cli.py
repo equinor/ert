77cba460a09fbfe327845a9df1ec566fa96bd040
Markus Dregi <markusdregi@gmail.com>
2020-11-15 14:24:30 +0100

    Add dummy ert3 init command

---
+++ tests/ert3/console/integration/test_cli.py
@@ -0,0 +1,85 @@
+import ert3
+
+import os
+import pathlib
+import pytest
+import shutil
+import sys
+import unittest
+
+_EXAMPLES_ROOT = pathlib.Path(os.path.dirname(__file__))/".."/".."/".."/".."/"examples"
+_POLY_WORKSPACE_NAME = "polynomial"
+_POLY_WORKSPACE = _EXAMPLES_ROOT/_POLY_WORKSPACE_NAME
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["run"],
+        ["load"],
+        ["exec", "something"],
+    ]
+)
+def test_cli_no_init(tmpdir, args):
+    workspace = tmpdir/_POLY_WORKSPACE_NAME
+    shutil.copytree(_POLY_WORKSPACE, workspace)
+    workspace.chdir()
+
+    with unittest.mock.patch.object(sys, "argv", args):
+        with pytest.raises(SystemExit) as error:
+            ert3.console.main()
+        assert "Not inside an ERT workspace" in str(error.value)
+
+
+def test_cli_no_args(tmpdir):
+    workspace = tmpdir/_POLY_WORKSPACE_NAME
+    shutil.copytree(_POLY_WORKSPACE, workspace)
+    workspace.chdir()
+
+    args = []
+    with unittest.mock.patch.object(sys, "argv", args):
+        ert3.console.main()
+
+
+def test_cli_init(tmpdir):
+    workspace = tmpdir/_POLY_WORKSPACE_NAME
+    shutil.copytree(_POLY_WORKSPACE, workspace)
+    workspace.chdir()
+
+    args = ["init"]
+    with unittest.mock.patch.object(sys, "argv", args):
+        ert3.console.main()
+
+
+def test_cli_init_twice(tmpdir):
+    workspace = tmpdir/_POLY_WORKSPACE_NAME
+    shutil.copytree(_POLY_WORKSPACE, workspace)
+    workspace.chdir()
+
+    args = ["init"]
+    with unittest.mock.patch.object(sys, "argv", args):
+        ert3.console.main()
+
+    with unittest.mock.patch.object(sys, "argv", args):
+        with pytest.raises(SystemExit) as error:
+            ert3.console.main()
+        assert "Already inside an ERT workspace" in str(error.value)
+
+
+def test_cli_init_subfolder(tmpdir):
+    workspace = tmpdir/_POLY_WORKSPACE_NAME
+    shutil.copytree(_POLY_WORKSPACE, workspace)
+    workspace.chdir()
+
+    args = ["init"]
+    with unittest.mock.patch.object(sys, "argv", args):
+        ert3.console.main()
+
+    subfolder = tmpdir/_POLY_WORKSPACE_NAME/"subfolder"
+    subfolder.mkdir()
+    subfolder.chdir()
+
+    args = ["init"]
+    with unittest.mock.patch.object(sys, "argv", args):
+        with pytest.raises(SystemExit) as error:
+            ert3.console.main()
+        assert "Already inside an ERT workspace" in str(error.value)
--- tests/ert3/console/test_cli.py
+++
@@ -1,5 +0,0 @@
-import ert3
-import pytest
-
-def test_main_success():
-    ert3.console.main()
