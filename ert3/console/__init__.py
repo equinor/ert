b1f630b0c453e740396655ae464678dcbd93fb6d
Markus Dregi <markusdregi@gmail.com>
2020-11-15 14:24:30 +0100

    Add dummy ert3 init command

--- ert3/console/__init__.py
+++ ert3/console/__init__.py
@@ -1,2 +1,25 @@
+import os
+import pathlib
+import sys
+
+def _locate_ert_workspace_root(path):
+    path = pathlib.Path(path)
+    while True:
+        if os.path.exists(path/".ert"):
+            return path
+        if path == pathlib.Path(path.root):
+            return None
+        path = path.parent
+
+
 def main():
-    print("Welcome to ERT3")
+    if len(sys.argv) == 0:
+        return
+    elif len(sys.argv) == 1 and sys.argv[0] == "init":
+        if _locate_ert_workspace_root(os.getcwd()) is not None:
+            sys.exit("Already inside an ERT workspace")
+
+        with open(".ert", "w") as fout:
+            fout.write("ERT workspace")
+    else:
+        sys.exit("Not inside an ERT workspace")
