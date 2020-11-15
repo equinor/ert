d85dc694ae85e46e825bc5ba216d002f36f9ff61
Markus Dregi <markusdregi@gmail.com>
2020-11-15 16:29:21 +0100

    Add argparser to ert3

--- ert3/console/__init__.py
+++ ert3/console/__init__.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 import pathlib
 import sys
+
 
 def _locate_ert_workspace_root(path):
     path = pathlib.Path(path)
@@ -12,14 +14,38 @@ def _locate_ert_workspace_root(path):
         path = path.parent
 
 
-def main():
-    if len(sys.argv) == 0:
-        return
-    elif len(sys.argv) == 1 and sys.argv[0] == "init":
-        if _locate_ert_workspace_root(os.getcwd()) is not None:
-            sys.exit("Already inside an ERT workspace")
+_ERT3_DESCRIPTION = (
+    "ert3 is an ensemble-based tool for uncertainty studies.\n"
+    "\nWARNING: the tool is currently in an extremely early stage and we refer "
+    "all users to ert for real work!"
+)
 
-        with open(".ert", "w") as fout:
-            fout.write("ERT workspace")
+
+def _build_argparser():
+    parser = argparse.ArgumentParser(description=_ERT3_DESCRIPTION)
+    subparsers = parser.add_subparsers(dest="sub_cmd", help="ert3 commands")
+
+    init_parser = subparsers.add_parser("init", help="Initialize an ERT3 workspace")
+
+    return parser
+
+
+def _init_workspace(path):
+    path = pathlib.Path(path)
+    if _locate_ert_workspace_root(path) is not None:
+        sys.exit("Already inside an ERT workspace")
+
+    with open(path/".ert", "w") as fout:
+        fout.write("ERT workspace")
+
+
+def main():
+    parser = _build_argparser()
+    args = parser.parse_args()
+
+    if args.sub_cmd is None:
+        parser.print_help()
+    elif args.sub_cmd == "init":
+        _init_workspace(os.getcwd())
     else:
         sys.exit("Not inside an ERT workspace")
