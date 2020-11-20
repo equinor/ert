6e6caa048d40fc820c556a0329802983f3d1b239
Frode Aarstad <frodeaarstad@gmail.com>
2020-11-20 07:27:50 +0100

    Apply suggestions from code review
    Co-authored-by: Jonas Grønås Drange <53553193+jondequinor@users.noreply.github.com>

--- ert_shared/ensemble_evaluator/dispatch.py
+++ ert_shared/ensemble_evaluator/dispatch.py
@@ -5,11 +5,10 @@ __LOOKUP_MAP = defaultdict(list)
 
 def register_event_handler(event_types):
     def decorator(function):
-        if type(event_types) is set:
-            for event_type in event_types:
-                __LOOKUP_MAP[event_type].append(function)
-        elif type(event_types) is str:
-            __LOOKUP_MAP[event_types].append(function)
+        if not isinstance(event_types, set):
+            event_types = set(event_types)
+        for event_type in event_types:
+            __LOOKUP_MAP[event_type].append(function)
 
         def wrapper(*args, **kwargs):
             return function(*args, **kwargs)
