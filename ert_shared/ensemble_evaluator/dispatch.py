8e6c0459601c2f1cb118426a8fa4f76231a8cfc9
Sondre Sortland <sonso@equinor.com>
2020-11-26 10:29:12 +0100

    Refactor event dispatcher and add tests

--- ert_shared/ensemble_evaluator/dispatch.py
+++ ert_shared/ensemble_evaluator/dispatch.py
@@ -1,21 +1,22 @@
 from collections import defaultdict
 
-__LOOKUP_MAP = defaultdict(list)
 
+class Dispatcher:
+    def __init__(self):
+        self.__LOOKUP_MAP = defaultdict(list)
 
-def register_event_handler(event_types):
-    def decorator(function):
-        for event_type in event_types:
-            __LOOKUP_MAP[event_type].append(function)
+    def register_event_handler(self, event_types):
+        def decorator(function):
+            for event_type in event_types:
+                self.__LOOKUP_MAP[event_type].append(function)
 
-        def wrapper(*args, **kwargs):
-            return function(*args, **kwargs)
+            def wrapper(*args, **kwargs):
+                return function(*args, **kwargs)
 
-        return wrapper
+            return wrapper
 
-    return decorator
+        return decorator
 
-
-async def handle_event(instance, event):
-    for f in __LOOKUP_MAP[event["type"]]:
-        await f(instance, event)
+    async def handle_event(self, instance, event):
+        for f in self.__LOOKUP_MAP[event["type"]]:
+            await f(instance, event)
