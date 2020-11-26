e65e7a9f50fb546cbf2380048b2c90b80113f3e8
Anette Uttisrud <autt@equinor.com>
2020-11-26 13:16:08 +0100

    Add uniform distribution to stats module

--- ert3/stats/_stats.py
+++ ert3/stats/_stats.py
@@ -27,3 +27,34 @@ class Gaussian:
             return sample
         else:
             return {idx: float(val) for idx, val in zip(self._index, sample)}
+
+
+class Uniform:
+    def __init__(self, lower_bound, upper_bound, *, size=None, index=None):
+        self._lower_bound = lower_bound
+        self._upper_bound = upper_bound
+        self._scale = upper_bound - lower_bound
+
+        if size is None and index is None:
+            raise ValueError(
+                "Cannot create uniform distribution with neither size nor index"
+            )
+        if size is not None and index is not None:
+            raise ValueError(
+                "Cannot create uniform distribution with both size and index"
+            )
+
+        if size is not None:
+            self._size = size
+        else:
+            self._size = len(tuple(index))
+        self._index = index
+
+    def sample(self):
+        sample = scipy.stats.uniform.rvs(
+            loc=self._lower_bound, scale=self._scale, size=self._size
+        )
+        if self._index is None:
+            return sample
+        else:
+            return {idx: float(val) for idx, val in zip(self._index, sample)}
--- tests/ert3/stats/test_distributions.py
+++ tests/ert3/stats/test_distributions.py
@@ -66,3 +66,64 @@ def test_gaussian_distribution_invalid():
     err_msg_both = "Cannot create gaussian distribution with both size and index"
     with pytest.raises(ValueError, match=err_msg_both):
         ert3.stats.Gaussian(0, 1, size=10, index=list(range(10)))
+
+
+@flaky.flaky(max_runs=3, min_passes=2)
+@pytest.mark.parametrize(
+    ("size", "lower_bound", "upper_bound"),
+    (
+        (10000, 0, 1),
+        (20000, 10, 20),
+    ),
+)
+def test_uniform_distribution(size, lower_bound, upper_bound):
+    uniform = ert3.stats.Uniform(lower_bound, upper_bound, size=size)
+
+    prev_samples = set()
+    for _ in range(10):
+        sample = uniform.sample()
+
+        assert len(sample) == size
+
+        assert tuple(sample) not in prev_samples
+        prev_samples.add(tuple(sample))
+
+        assert sample.min() == approx(lower_bound)
+        assert sample.mean() == approx((lower_bound + upper_bound) / 2)
+        assert sample.max() == approx(upper_bound)
+
+
+@flaky.flaky(max_runs=3, min_passes=2)
+@pytest.mark.parametrize(
+    ("index", "lower_bound", "upper_bound"),
+    (
+        (("a", "b", "c"), 0, 1),
+        (tuple("a" * i for i in range(1, 10)), 2, 5),
+    ),
+)
+def test_uniform_distribution_index(index, lower_bound, upper_bound):
+    uniform = ert3.stats.Uniform(lower_bound, upper_bound, index=index)
+
+    samples = {idx: [] for idx in index}
+    for i in range(1000):
+        sample = uniform.sample()
+        assert sorted(sample.keys()) == sorted(index)
+
+        for key in index:
+            samples[key].append(sample[key])
+
+    for key in index:
+        s = np.array(samples[key])
+        assert s.min() == approx(lower_bound)
+        assert s.mean() == approx((lower_bound + upper_bound) / 2)
+        assert s.max() == approx(upper_bound)
+
+
+def test_uniform_distribution_invalid():
+    err_msg_neither = "Cannot create uniform distribution with neither size nor index"
+    with pytest.raises(ValueError, match=err_msg_neither):
+        ert3.stats.Uniform(0, 1)
+
+    err_msg_both = "Cannot create uniform distribution with both size and index"
+    with pytest.raises(ValueError, match=err_msg_both):
+        ert3.stats.Uniform(0, 1, size=10, index=list(range(10)))
