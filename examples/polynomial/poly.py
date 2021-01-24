8d8243e3bd8abeb2757c2c8d6e3f733efaaf97e7
Markus Dregi <markusdregi@gmail.com>
2021-01-24 18:21:39 +0100

    Add ert3 evaluation configuration example
    Co-authored-by: Markus Dregi <markusdregi@gmail.com>
    Co-authored-by: Øyvind Eide <eide.oyvind87@gmail.com>
    Co-authored-by: Anette Uttisrud <autt@equinor.com>
    Co-authored-by: Lars Petter Øren Hauge <larsenhauge@gmail.com>
    Co-authored-by: Helena Nandi Formentin <hnfo@equinor.com>
    Co-authored-by: Sondre Sortland <sonso@equinor.com>
    Co-authored-by: anna.kvashchuk <kvashchuk.anna@gmail.com>

---
+++ examples/polynomial/poly.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import argparse
+import json
+import sys
+
+
+def _build_arg_parser():
+    arg_parser = argparse.ArgumentParser(
+        description="Computes the result of a second degree polynomial",
+    )
+    arg_parser.add_argument(
+        "--coefficients",
+        type=argparse.FileType('r'),
+        required=True,
+        help="Path to file containing the coefficients"
+    )
+    arg_parser.add_argument(
+        "--output",
+        type=argparse.FileType('w'),
+        required=True,
+        help="Path to the output file"
+    )
+    return arg_parser
+
+
+def _evaluate_polynomial(coefficients):
+    a, b, c = coefficients["a"], coefficients["b"], coefficients["c"]
+    x_range = tuple(range(10))
+    return tuple(a * x**2 + b * x + c for x in x_range)
+
+
+def _main():
+    parser = _build_arg_parser()
+    args = parser.parse_args()
+    coefficients = json.load(args.coefficients)
+    result = _evaluate_polynomial(coefficients)
+    json.dump(result, args.output)
+
+
+if __name__ == "__main__":
+    _main()
