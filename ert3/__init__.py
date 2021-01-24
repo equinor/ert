60a07c67070bab945f44bfd5a83164d8bc2561f9
Markus Dregi <markusdregi@gmail.com>
2021-01-24 19:09:58 +0100

    Add ert3 workspace module
    Co-authored-by: Markus Dregi <markusdregi@gmail.com>
    Co-authored-by: Øyvind Eide <eide.oyvind87@gmail.com>
    Co-authored-by: Anette Uttisrud <autt@equinor.com>
    Co-authored-by: Lars Petter Øren Hauge <larsenhauge@gmail.com>
    Co-authored-by: Helena Nandi Formentin <hnfo@equinor.com>
    Co-authored-by: Sondre Sortland <sonso@equinor.com>
    Co-authored-by: anna.kvashchuk <kvashchuk.anna@gmail.com>

--- ert3/__init__.py
+++ ert3/__init__.py
@@ -1,5 +1,6 @@
 import ert3.storage
 import ert3.evaluator
+import ert3.workspace
 import ert3.stats
 import ert3.config
 
---
+++ ert3/workspace/__init__.py
@@ -0,0 +1,4 @@
+from ert3.workspace._workspace import initialize
+from ert3.workspace._workspace import load
+from ert3.workspace._workspace import experiment_have_run
+from ert3.workspace._workspace import assert_experiment_exists
