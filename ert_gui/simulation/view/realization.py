2b3ee6c886904229d2ffc0f02be2561f6f114183
Jonas G. Drange <jond@equinor.com>
2021-01-29 12:57:38 +0100

    Format files

--- ert_gui/simulation/view/realization.py
+++ ert_gui/simulation/view/realization.py
@@ -1,21 +1,28 @@
 from qtpy.QtCore import QRect, QSize, QModelIndex, Qt
-from qtpy.QtWidgets import QStyledItemDelegate, QStyleOptionViewItem, QListView, QAbstractItemView, QStyle
+from qtpy.QtWidgets import (
+    QStyledItemDelegate,
+    QStyleOptionViewItem,
+    QListView,
+    QAbstractItemView,
+    QStyle,
+)
 from qtpy.QtGui import QPainter, QColor, QFont, QColorConstants, QPen
 
 from ert_shared.status.entity.state import REAL_STATE_TO_COLOR
 from ert_gui.model.snapshot import NodeRole
 
 
-
 class RealizationView(QListView):
     def __init__(self, parent=None) -> None:
         super(RealizationView, self).__init__(parent)
 
-        self._delegateWidth= 50
-        self._delegateHeight= 50
+        self._delegateWidth = 50
+        self._delegateHeight = 50
         self.setViewMode(QListView.IconMode)
         self.setGridSize(QSize(self._delegateWidth, self._delegateHeight))
-        self.setItemDelegate(RealizationDelegate(self._delegateWidth, self._delegateHeight, self))
+        self.setItemDelegate(
+            RealizationDelegate(self._delegateWidth, self._delegateHeight, self)
+        )
         self.clicked.connect(self._select_real)
         self.setSelectionMode(QAbstractItemView.SingleSelection)
         self.setFlow(QListView.LeftToRight)
@@ -27,14 +34,14 @@ class RealizationView(QListView):
 
 
 class RealizationDelegate(QStyledItemDelegate):
-    def __init__(self, width, height, parent = None) -> None:
+    def __init__(self, width, height, parent=None) -> None:
         super(RealizationDelegate, self).__init__(parent)
         self._size = QSize(width, height)
 
-    def paint(self, painter, option : QStyleOptionViewItem, index : QModelIndex) -> None:
+    def paint(self, painter, option: QStyleOptionViewItem, index: QModelIndex) -> None:
         node = index.data(NodeRole)
-        text= f"{node.id}"
-        status= node.data["status"]
+        text = f"{node.id}"
+        status = node.data["status"]
 
         painter.save()
         painter.setRenderHint(QPainter.Antialiasing, True)
@@ -53,7 +60,6 @@ class RealizationDelegate(QStyledItemDelegate):
         painter.drawText(option.rect, Qt.AlignCenter, text)
 
         painter.restore()
-
 
     def sizeHint(self, option, index) -> QSize:
         return self._size
