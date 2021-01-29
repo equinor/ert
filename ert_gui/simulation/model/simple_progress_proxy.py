cad16eb29f41dda28e2fa6062463f1f46440831f
Jonas G. Drange <jond@equinor.com>
2021-01-29 12:57:38 +0100

    Format files

--- ert_gui/simulation/model/simple_progress_proxy.py
+++ ert_gui/simulation/model/simple_progress_proxy.py
@@ -11,14 +11,14 @@ class SimpleProgressProxyModel(QIdentityProxyModel):
     def rowCount(self, parent=QModelIndex()) -> int:
         return 1
 
-    def data(self, index : QModelIndex, role=Qt.DisplayRole) -> QVariant:
+    def data(self, index: QModelIndex, role=Qt.DisplayRole) -> QVariant:
         total_iterations = 0
-        finished=0
-        for k,v in self.sourceModel().root.children.items():
+        finished = 0
+        for k, v in self.sourceModel().root.children.items():
             ## iterations
-            for k1,v1 in v.children.items():
+            for k1, v1 in v.children.items():
                 ## realizations
-                total_iterations+=1
+                total_iterations += 1
                 if v1.data["status"] == "Finished":
-                    finished+=1
+                    finished += 1
         return finished / total_iterations
--- ert_gui/simulation/view/progress.py
+++ ert_gui/simulation/view/progress.py
@@ -5,10 +5,11 @@ from qtpy.QtGui import QPainter, QColor, QFont
 from ert_shared.status.entity.state import REAL_STATE_TO_COLOR
 from ert_gui.simulation.palette import TOTAL_PROGRESS_COLOR
 
+
 class ProgressView(QTreeView):
-    def __init__(self, parent = None) -> None:
+    def __init__(self, parent=None) -> None:
         super(ProgressView, self).__init__(parent)
-                
+
         self.setHeaderHidden(True)
         self.setItemsExpandable(False)
         self.setItemDelegate(ProgressDelegate(self))
@@ -18,13 +19,13 @@ class ProgressView(QTreeView):
 
 
 class ProgressDelegate(QStyledItemDelegate):
-    def __init__(self, parent = None) -> None:
+    def __init__(self, parent=None) -> None:
         super(ProgressDelegate, self).__init__(parent)
 
         self.background_color = QColor(200, 210, 210)
 
-    def paint(self, painter, option : QStyleOptionViewItem, index : QModelIndex) -> None:
-        
+    def paint(self, painter, option: QStyleOptionViewItem, index: QModelIndex) -> None:
+
         nr_reals = index.data()["nr_reals"]
         status = index.data()["status"]
         d = option.rect.width() / nr_reals
@@ -36,22 +37,22 @@ class ProgressDelegate(QStyledItemDelegate):
 
         painter.fillRect(option.rect, self.background_color)
 
-        i=0        
+        i = 0
         for state, color_ref in REAL_STATE_TO_COLOR.items():
-       
+
             if state not in status:
                 continue
-            
+
             state_progress = status[state]
-            x = option.rect.x() + i*d
+            x = option.rect.x() + i * d
             y = option.rect.y()
             w = state_progress * d
             h = option.rect.height()
-            color= QColor(*color_ref)
+            color = QColor(*color_ref)
 
-            painter.fillRect(x, y, w, h,  color)
+            painter.fillRect(x, y, w, h, color)
 
-            i+= state_progress
+            i += state_progress
 
         painter.restore()
 
