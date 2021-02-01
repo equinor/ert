27a47e8d6d05a645196ba9bb05b54ee4848195a9
Frode Aarstad <frodeaarstad@gmail.com>
2021-02-01 11:42:48 +0100

    Remove some logic to the models

---
+++ ert_gui/model/progress_proxy.py
@@ -0,0 +1,46 @@
+from qtpy.QtCore import QRect, QSize, QModelIndex, Qt, QIdentityProxyModel, QVariant
+
+#TODO: will these interfer with the ones in snapshot
+ProgressRole = Qt.UserRole + 1 
+SimpleProgressRole = Qt.UserRole + 2 
+
+
+
+class ProgressProxyModel(QIdentityProxyModel):
+    def __init__(self, parent=None) -> None:
+        QIdentityProxyModel.__init__(self, parent)
+
+    def columnCount(self, parent=QModelIndex()) -> int:
+        return 1
+
+    def rowCount(self, parent=QModelIndex()) -> int:
+        return 1
+
+    def data(self, index: QModelIndex, role=Qt.DisplayRole) -> QVariant:
+
+        if role == ProgressRole:
+            d = {}
+            nr_reals = 0
+            for k, v in self.sourceModel().root.children.items():
+                ## iterations
+                for k1, v1 in v.children.items():
+                    ## realizations
+                    nr_reals += 1
+                    status = v1.data["status"]
+                    if status in d:
+                        d[status] += 1
+                    else:
+                        d[status] = 1
+            return {"status": d, "nr_reals": nr_reals}
+
+        if role == SimpleProgressRole:
+            total_iterations = 0
+            finished = 0
+            for k, v in self.sourceModel().root.children.items():
+                ## iterations
+                for k1, v1 in v.children.items():
+                    ## realizations
+                    total_iterations += 1
+                    if v1.data["status"] == "Finished":
+                        finished += 1
+            return finished / total_iterations
