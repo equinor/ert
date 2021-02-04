a1659c832eb9534e31e0fe6ec722833a9c2215af
Øyvind Eide <eide.oyvind87@gmail.com>
2021-02-04 10:06:30 +0100

    Use numpy_vector instead of deprecated method

--- test-data/local/snake_oil_field/jobs/snake_oil_diff.py
+++ test-data/local/snake_oil_field/jobs/snake_oil_diff.py
@@ -3,21 +3,17 @@ from ecl.summary import EclSum
 
 def writeDiff(filename, vector1, vector2):
     with open(filename, "w") as f:
-        for index in range(len(vector1)):
-            node1 = vector1[index]
-            node2 = vector2[index]
-
-            diff = node1.value - node2.value
-            f.write("%f\n" % diff)
+        diff = vector1 - vector2
+        f.write("\n".join([str(itm) for itm in diff]))
 
 
 if __name__ == '__main__':
     ecl_sum = EclSum("SNAKE_OIL_FIELD")
 
     report_step = 199
-    writeDiff("snake_oil_opr_diff_%d.txt" % report_step, ecl_sum["WOPR:OP1"], ecl_sum["WOPR:OP2"])
-    writeDiff("snake_oil_wpr_diff_%d.txt" % report_step, ecl_sum["WWPR:OP1"], ecl_sum["WWPR:OP2"])
-    writeDiff("snake_oil_gpr_diff_%d.txt" % report_step, ecl_sum["WGPR:OP1"], ecl_sum["WGPR:OP2"])
+    writeDiff("snake_oil_opr_diff_%d.txt" % report_step, ecl_sum.numpy_vector("WOPR:OP1"), ecl_sum.numpy_vector("WOPR:OP2"))
+    writeDiff("snake_oil_wpr_diff_%d.txt" % report_step, ecl_sum.numpy_vector("WWPR:OP1"), ecl_sum.numpy_vector("WWPR:OP2"))
+    writeDiff("snake_oil_gpr_diff_%d.txt" % report_step, ecl_sum.numpy_vector("WGPR:OP1"), ecl_sum.numpy_vector("WGPR:OP2"))
 
 
 
