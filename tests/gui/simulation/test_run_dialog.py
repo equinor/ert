90a9a1e1e753ed2a4bc533c1712506497a3f0455
Jonas G. Drange <jond@equinor.com>
2021-02-15 20:06:27 +0100

    Make run_dialog tab test more robust

--- tests/gui/simulation/test_run_dialog.py
+++ tests/gui/simulation/test_run_dialog.py
@@ -125,4 +125,6 @@ def test_full_snapshot(runmodel, active_realizations, full_snapshot, qtbot):
     qtbot.waitForWindowShown(widget)
     assert widget._total_progress_bar.value() == 50
     qtbot.mouseClick(widget.show_details_button, Qt.LeftButton)
+    qtbot.waitSignal(widget._snapshot_model.rowsInserted)
+    qtbot.waitSignal(widget._snapshot_model.rowsInserted)
     assert widget._tab_widget.count() == 2
