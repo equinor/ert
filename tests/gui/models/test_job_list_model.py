1341585889a48d129f94da7a8c9598e353dcd95d
Jonas G. Drange <jond@equinor.com>
2021-02-16 14:09:38 +0100

    An adequate fix for magic columns in testing

--- tests/gui/models/test_job_list_model.py
+++ tests/gui/models/test_job_list_model.py
@@ -1,11 +1,19 @@
+import ert_shared.ensemble_evaluator.entity.identifiers as ids
+from ert_gui.model.job_list import JobListProxyModel
+from ert_gui.model.node import NodeType
+from ert_gui.model.snapshot import COLUMNS, SnapshotModel
 from ert_shared.ensemble_evaluator.entity.snapshot import PartialSnapshot
 from ert_shared.status.entity.state import JOB_STATE_FAILURE, JOB_STATE_START
-from ert_gui.model.node import NodeType
 from PyQt5.QtCore import QModelIndex
-from ert_gui.model.job_list import JobListProxyModel
-from tests.gui.models.conftest import partial_snapshot
-from ert_gui.model.snapshot import COLUMNS, SnapshotModel
 from pytestqt.qt_compat import qt_api
+from tests.gui.models.conftest import partial_snapshot
+
+
+def _id_to_col(identifier):
+    for col, fields in enumerate(COLUMNS[NodeType.STEP]):
+        if fields[1] == identifier:
+            return col
+    raise ValueError(f"{identifier} not a column in {COLUMNS}")
 
 
 def test_using_qt_model_tester(qtmodeltester, full_snapshot):
@@ -41,12 +49,17 @@ def test_changes(full_snapshot):
     )
 
     source_model._add_snapshot(full_snapshot, 0)
-    assert model.index(0, 1, QModelIndex()).data() == JOB_STATE_START
+    assert (
+        model.index(0, _id_to_col(ids.STATUS), QModelIndex()).data() == JOB_STATE_START
+    )
 
     partial = PartialSnapshot(full_snapshot)
     partial.update_job("0", "0", "0", "0", status=JOB_STATE_FAILURE)
     source_model._add_partial_snapshot(partial, 0)
-    assert model.index(0, 1, QModelIndex()).data() == JOB_STATE_FAILURE
+    assert (
+        model.index(0, _id_to_col(ids.STATUS), QModelIndex()).data()
+        == JOB_STATE_FAILURE
+    )
 
 
 def test_no_cross_talk(full_snapshot):
@@ -67,7 +80,12 @@ def test_no_cross_talk(full_snapshot):
     partial = PartialSnapshot(full_snapshot)
     partial.update_job("0", "0", "0", "0", status=JOB_STATE_FAILURE)
     source_model._add_partial_snapshot(partial, 1)
-    assert model.index(0, 1, QModelIndex()).data() == JOB_STATE_START
+    assert (
+        model.index(0, _id_to_col(ids.STATUS), QModelIndex()).data() == JOB_STATE_START
+    )
 
     model.set_step(1, 0, 0, 0)
-    assert model.index(0, 1, QModelIndex()).data() == JOB_STATE_FAILURE
+    assert (
+        model.index(0, _id_to_col(ids.STATUS), QModelIndex()).data()
+        == JOB_STATE_FAILURE
+    )
