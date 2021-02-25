5350c325561b0c534eb0c1dbe65736a9d2f0b55e
Markus Dregi <markusdregi@gmail.com>
2021-02-25 20:25:23 +0100

    Add __all__ to ert3.data

--- ert3/data/__init__.py
+++ ert3/data/__init__.py
@@ -1,3 +1,5 @@
 from ert3.data._record import Record
 from ert3.data._record import EnsembleRecord
 from ert3.data._record import MultiEnsembleRecord
+
+__all__ = ("Record", "EnsembleRecord", "MultiEnsembleRecord")
