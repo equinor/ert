be95020fb55e5fcb1803607727aa248adad0f9d9
Sondre Sortland <sonso@equinor.com>
2021-03-03 12:45:22 +0100

    Record Transmitting

--- ert3/data/__init__.py
+++ ert3/data/__init__.py
@@ -1,5 +1,15 @@
 from ert3.data._record import Record
 from ert3.data._record import EnsembleRecord
 from ert3.data._record import MultiEnsembleRecord
+from ert3.data._record import RecordTransmitter
+from ert3.data._record import SharedDiskRecordTransmitter
+from ert3.data._record import InMemoryRecordTransmitter
 
-__all__ = ("Record", "EnsembleRecord", "MultiEnsembleRecord")
+__all__ = (
+    "Record",
+    "EnsembleRecord",
+    "MultiEnsembleRecord",
+    "RecordTransmitter",
+    "SharedDiskRecordTransmitter",
+    "InMemoryRecordTransmitter",
+)
