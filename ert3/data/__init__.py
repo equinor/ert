f938aa8523a81f0e4424d7d29ea7af6c03aeca1e
Markus Dregi <markusdregi@gmail.com>
2021-05-21 07:54:39 +0200

    Pass ert3 records to storage as numerical data

--- ert3/data/__init__.py
+++ ert3/data/__init__.py
@@ -4,12 +4,14 @@ from ert3.data._record import MultiEnsembleRecord
 from ert3.data._record import RecordTransmitter
 from ert3.data._record import SharedDiskRecordTransmitter
 from ert3.data._record import InMemoryRecordTransmitter
+from ert3.data._record import RecordType
 
 __all__ = (
     "Record",
     "EnsembleRecord",
     "MultiEnsembleRecord",
     "RecordTransmitter",
+    "RecordType",
     "SharedDiskRecordTransmitter",
     "InMemoryRecordTransmitter",
 )
