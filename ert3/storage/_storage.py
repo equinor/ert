ab66d350ddde64f6acfd778c7f9c2a182cca2749
Markus Dregi <markusdregi@gmail.com>
2021-05-21 07:54:39 +0200

    Pass ert3 records to storage as numerical data

--- ert3/storage/_storage.py
+++ ert3/storage/_storage.py
@@ -1,10 +1,10 @@
-import codecs
 import json
 from pathlib import Path
-from typing import Any, Dict, Iterable, Optional, cast, Set
+from typing import Any, Dict, Iterable, Optional, Set
 import io
 
-import cloudpickle
+import pandas as pd
+from pydantic import BaseModel
 import requests
 import ert3
 
@@ -14,6 +14,18 @@ _DATA = "__data__"
 _PARAMETERS = "__parameters__"
 _ENSEMBLE_RECORDS = "__ensemble_records__"
 _SPECIAL_KEYS = (_ENSEMBLE_RECORDS,)
+
+
+class _NumericalMetaData(BaseModel):
+    class Config:
+        validate_all = True
+        validate_assignment = True
+        extra = "forbid"
+        allow_mutation = False
+        arbitrary_types_allowed = True
+
+    ensemble_size: int
+    record_type: ert3.data.RecordType
 
 
 def _get_experiment_by_name(experiment_name: str) -> Dict[str, Any]:
@@ -96,10 +108,21 @@ def get_experiment_names(*, workspace: Path) -> Set[str]:
     return experiment_names
 
 
-def _add_data(
-    workspace: Path, experiment_name: str, record_name: str, data: Any
-) -> None:
+def _get_record_type(ensemble_record: ert3.data.EnsembleRecord) -> ert3.data.RecordType:
+    record_type = ensemble_record.records[0].record_type
+    for record in ensemble_record.records:
+        if record.record_type != record_type:
+            raise ValueError("Inconsistent record type")
 
+    return record_type
+
+
+def _add_numerical_data(
+    workspace: Path,
+    experiment_name: str,
+    record_name: str,
+    ensemble_record: ert3.data.EnsembleRecord,
+) -> None:
     experiment = _get_experiment_by_name(experiment_name)
     if experiment is None:
         raise KeyError(
@@ -107,19 +130,92 @@ def _add_data(
             f"non-existing experiment: {experiment_name}"
         )
 
-    ensemble_id = experiment["ensembles"][0]  # currently just one ens per exp
-    response = requests.post(
-        url=f"{_STORAGE_URL}/ensembles/{ensemble_id}/records/{record_name}/file",
-        files={"file": (record_name, io.StringIO(data), "something")},
+    metadata = _NumericalMetaData(
+        ensemble_size=ensemble_record.ensemble_size,
+        record_type=_get_record_type(ensemble_record),
     )
-    if response.status_code == 409:
-        raise KeyError("Record already exists")
+
+    ensemble_id = experiment["ensembles"][0]  # currently just one ens per exp
+    record_url = f"{_STORAGE_URL}/ensembles/{ensemble_id}/records/{record_name}"
+
+    for idx, record in enumerate(ensemble_record.records):
+        df = pd.DataFrame([record.data], columns=record.index, index=[idx])
+        response = requests.post(
+            url=f"{record_url}/matrix",
+            params={"realization_index": idx},
+            data=df.to_csv().encode(),
+            headers={"content-type": "application/x-dataframe"},
+        )
+
+        if response.status_code == 409:
+            raise KeyError("Record already exists")
+
+        if response.status_code != 200:
+            raise ert3.exceptions.StorageError(response.text)
+
+        meta_response = requests.put(
+            url=f"{record_url}/metadata",
+            params={"realization_index": idx},
+            json=metadata.dict(),
+        )
+
+        if meta_response.status_code != 200:
+            raise ert3.exceptions.StorageError(meta_response.text)
+
+
+def _response2record(
+    response_content: bytes, record_type: ert3.data.RecordType, realization_id: int
+) -> ert3.data.Record:
+    dataframe = pd.read_csv(
+        io.BytesIO(response_content), index_col=0, float_precision="round_trip"
+    )
+
+    raw_index = tuple(dataframe.columns)
+    if record_type == ert3.data.RecordType.LIST_FLOAT:
+        array_data = tuple(
+            float(dataframe.loc[realization_id][raw_idx]) for raw_idx in raw_index
+        )
+        return ert3.data.Record(data=array_data)
+    elif record_type == ert3.data.RecordType.MAPPING_INT_FLOAT:
+        int_index = tuple(int(e) for e in dataframe.columns)
+        idata = {
+            idx: float(dataframe.loc[realization_id][raw_idx])
+            for raw_idx, idx in zip(raw_index, int_index)
+        }
+        return ert3.data.Record(data=idata)
+    elif record_type == ert3.data.RecordType.MAPPING_STR_FLOAT:
+        str_index = tuple(str(e) for e in dataframe.columns)
+        sdata = {
+            idx: float(dataframe.loc[realization_id][raw_idx])
+            for raw_idx, idx in zip(raw_index, str_index)
+        }
+        return ert3.data.Record(data=sdata)
+    else:
+        raise ValueError(
+            f"Unexpected record type when loading numerical record: {record_type}"
+        )
+
+
+def _get_numerical_metadata(ensemble_id: str, record_name: str) -> _NumericalMetaData:
+    response = requests.get(
+        url=f"{_STORAGE_URL}/ensembles/{ensemble_id}/records/{record_name}/metadata",
+        params={"realization_index": 0},  # This assumes there is a realization 0
+    )
+
+    if response.status_code == 404:
+        raise ert3.exceptions.ElementMissingError(
+            f"No metadata for {record_name} in ensemble: {ensemble_id}"
+        )
 
     if response.status_code != 200:
         raise ert3.exceptions.StorageError(response.text)
 
+    return _NumericalMetaData(**json.loads(response.content))
 
-def _get_data(workspace: Path, experiment_name: str, record_name: str) -> Any:
+
+def _get_numerical_data(
+    workspace: Path, experiment_name: str, record_name: str
+) -> ert3.data.EnsembleRecord:
     experiment = _get_experiment_by_name(experiment_name)
     if experiment is None:
         raise KeyError(
@@ -127,14 +223,30 @@ def _get_data(workspace: Path, experiment_name: str, record_name: str) -> Any:
         )
 
     ensemble_id = experiment["ensembles"][0]  # currently just one ens per exp
-    response = requests.get(
-        url=f"{_STORAGE_URL}/ensembles/{ensemble_id}/records/{record_name}"
-    )
+    metadata = _get_numerical_metadata(ensemble_id, record_name)
 
-    if response.status_code == 404:
-        raise KeyError(f"No {record_name} data for experiment: {experiment_name}")
+    records = []
+    for real_id in range(metadata.ensemble_size):
+        response = requests.get(
+            url=f"{_STORAGE_URL}/ensembles/{ensemble_id}/records/{record_name}",
+            params={"realization_index": real_id},
+            headers={"accept": "application/x-dataframe"},
+        )
 
-    return response.content
+        if response.status_code == 404:
+            raise KeyError(f"No {record_name} data for experiment: {experiment_name}")
+
+        if response.status_code != 200:
+            raise ert3.exceptions.StorageError(response.text)
+
+        record = _response2record(
+            response.content,
+            metadata.record_type,
+            real_id,
+        )
+        records.append(record)
+
+    return ert3.data.EnsembleRecord(records=records)
 
 
 def add_ensemble_record(
@@ -147,17 +259,7 @@ def add_ensemble_record(
     if experiment_name is None:
         experiment_name = f"{workspace}.{_ENSEMBLE_RECORDS}"
 
-    # If a Record is serialized to JSON without associating it with a type,
-    # information is lost. For instance, serializing an int mapping
-    # {0: 1, 1: 3} to JSON, keys are made into strings in JSON. If it is not
-    # known that the Record is a int, str map, information is lost.
-    #
-    # TODO: https://github.com/equinor/ert/issues/1550 should implement better
-    # usage of ert-storage, specifically using numerical endpoints when
-    # applicable.
-    pickle_str = codecs.encode(cloudpickle.dumps(ensemble_record), "base64").decode()
-    data = json.dumps({"data": pickle_str})
-    _add_data(workspace, experiment_name, record_name, data)
+    _add_numerical_data(workspace, experiment_name, record_name, ensemble_record)
 
 
 def get_ensemble_record(
@@ -168,11 +270,8 @@ def get_ensemble_record(
 ) -> ert3.data.EnsembleRecord:
     if experiment_name is None:
         experiment_name = f"{workspace}.{_ENSEMBLE_RECORDS}"
-    data = json.loads(_get_data(workspace, experiment_name, record_name))["data"]
-    return cast(
-        ert3.data.EnsembleRecord,
-        cloudpickle.loads(codecs.decode(data.encode(), "base64")),
-    )
+
+    return _get_numerical_data(workspace, experiment_name, record_name)
 
 
 def get_ensemble_record_names(
