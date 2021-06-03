fc5b2205ea76c4a9efab3f4d204b7e9a4799abcf
Sondre Sortland <sonso@equinor.com>
2021-06-03 12:33:50 +0200

    Revert "Enable ensemble evaluator by default"
    This reverts commit 8c41328ed8c8b3a10a3a31b665bc6f46dca80abc.

--- tests/cli/test_integration.py
+++ tests/cli/test_integration.py
@@ -9,7 +9,7 @@ from argparse import ArgumentParser
 from ert_shared.cli.main import run_cli
 from ert_shared.cli import ENSEMBLE_SMOOTHER_MODE, TEST_RUN_MODE
 
-from unittest.mock import Mock, MagicMock, call, patch
+from unittest.mock import Mock
 
 
 @pytest.fixture()
@@ -96,13 +96,13 @@ def test_ensemble_evaluator(tmpdir, source_root):
                 "poly_runpath_file",
                 "--realizations",
                 "1,2,4,8,16,32,64",
-                "--disable-ensemble-evaluator",
+                "--enable-ensemble-evaluator",
                 "poly_example/poly.ert",
             ],
         )
         FeatureToggling.update_from_args(parsed)
 
-        assert FeatureToggling.is_enabled("ensemble-evaluator") is False
+        assert FeatureToggling.is_enabled("ensemble-evaluator") is True
 
         run_cli(parsed)
         FeatureToggling.reset()
@@ -120,7 +120,7 @@ def test_ensemble_evaluator_disable_monitoring(tmpdir, source_root):
             parser,
             [
                 ENSEMBLE_SMOOTHER_MODE,
-                "--disable-ensemble-evaluator",
+                "--enable-ensemble-evaluator",
                 "--disable-monitoring",
                 "--target-case",
                 "poly_runpath_file",
@@ -131,7 +131,7 @@ def test_ensemble_evaluator_disable_monitoring(tmpdir, source_root):
         )
         FeatureToggling.update_from_args(parsed)
 
-        assert FeatureToggling.is_enabled("ensemble-evaluator") is False
+        assert FeatureToggling.is_enabled("ensemble-evaluator") is True
 
         run_cli(parsed)
         FeatureToggling.reset()
@@ -151,30 +151,4 @@ def test_cli_test_run(tmpdir, source_root, mock_cli_run):
     monitor_mock, thread_join_mock, thread_start_mock = mock_cli_run
     monitor_mock.assert_called_once()
     thread_join_mock.assert_called_once()
-    thread_start_mock.assert_has_calls([[call(), call()]])
-
-
-def test_cli_test_connection_error(tmpdir, source_root, capsys):
-    shutil.copytree(
-        os.path.join(source_root, "test-data", "local", "poly_example"),
-        os.path.join(str(tmpdir), "poly_example"),
-    )
-
-    mock_monitor = MagicMock()
-    mock_monitor.__enter__.side_effect = ConnectionRefusedError
-
-    with patch(
-        "ert_shared.status.tracker.evaluator.create_ee_monitor",
-        return_value=mock_monitor,
-    ), patch(
-        "ert_shared.ensemble_evaluator.evaluator.ee_monitor.create",
-        return_value=mock_monitor,
-    ):
-        with tmpdir.as_cwd():
-            parser = ArgumentParser(prog="test_main")
-            parsed = ert_parser(parser, [TEST_RUN_MODE, "poly_example/poly.ert"])
-            with pytest.raises(SystemExit):
-                run_cli(parsed)
-
-    capture = capsys.readouterr()
-    assert "Connection error" in capture.out
+    thread_start_mock.assert_called_once()
