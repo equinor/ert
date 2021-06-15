21d8acb3bc0a8fa77c2d0a03df3e423a668cfbf9
Sharlon Regales <sharlon.regales@tno.nl>
2021-06-15 12:32:56 +0200

    Fix test script name collision

--- tests/utils.py
+++ tests/utils.py
@@ -65,8 +65,24 @@ def tmp(path=None, teardown=True):
             shutil.rmtree(fname, ignore_errors=True)
 
 
+def source_root():
+    src = "@CMAKE_CURRENT_SOURCE_DIR@/../.."
+    if os.path.isdir(src):
+        return os.path.realpath(src)
+
+    # If the file was not correctly configured by cmake, look for the source
+    # folder, assuming the build folder is inside the source folder.
+    path_list = os.path.dirname(os.path.abspath(__file__)).split("/")
+    while len(path_list) > 0:
+        git_path = os.path.join(os.sep, "/".join(path_list), ".git")
+        if os.path.isdir(git_path):
+            return os.path.join(os.sep, *path_list)
+        path_list.pop()
+    raise RuntimeError("Cannot find the source folder")
+
+
 class ErtTest(ExtendedTestCase):
-    SOURCE_ROOT = SOURCE_DIR
+    SOURCE_ROOT = source_root()
     TESTDATA_ROOT = os.path.join(SOURCE_ROOT, "test-data")
     SHARE_ROOT = os.path.join(SOURCE_ROOT, "share")
 
