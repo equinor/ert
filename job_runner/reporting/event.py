f5b6b29cef92dd7a47805033bdb660e893007ab8
Sharlon Regales <sharlon.regales@tno.nl>
2021-09-14 14:20:38 +0000

    Refactor reporting with the perspective of logging
    Add typing to File:report
    
    Add simple logging to reporting.file module
    
    Substitute exit string for static exit msg variable
    
    Extract logic out of report and into _report
    
    Fix typing issue
    
    Substitute exit string for static exit msg variable
    
    Refactor event with the perspective of logging
    
    Add logging to event module
    
    Adjust event module for readability
    
    Fix object type call
    
    Adust cloudevent log
    
    Substitute exit string for static exit msg variable

--- job_runner/reporting/event.py
+++ job_runner/reporting/event.py
@@ -1,10 +1,20 @@
 import queue
 import threading
+import logging
 from pathlib import Path
+from typing import Dict, Any
 
 from cloudevents.http import CloudEvent, to_json
 
-from job_runner.reporting.message import Exited, Finish, Init, Running, Start
+from job_runner.reporting.message import (
+    Exited,
+    Finish,
+    Init,
+    Running,
+    Start,
+    Message,
+    _JOB_EXIT_FAILED_STRING,
+)
 from job_runner.reporting.base import Reporter
 from job_runner.util.client import Client
 
@@ -12,6 +22,12 @@ _FM_JOB_START = "com.equinor.ert.forward_model_job.start"
 _FM_JOB_RUNNING = "com.equinor.ert.forward_model_job.running"
 _FM_JOB_SUCCESS = "com.equinor.ert.forward_model_job.success"
 _FM_JOB_FAILURE = "com.equinor.ert.forward_model_job.failure"
+
+_CONTENT_TYPE = "datacontenttype"
+_JOB_MSG_TYPE = "type"
+_JOB_SOURCE = "source"
+
+logger = logging.getLogger(__name__)
 
 
 class TransitionError(ValueError):
@@ -36,6 +52,7 @@ class Event(Reporter):
         self._initialize_state_machine()
 
     def _publish_event(self):
+        logger.debug("Publishing event.")
         with Client(self._evaluator_url, self._token, self._cert) as client:
             while True:
                 event = self._event_queue.get()
@@ -44,6 +61,7 @@ class Event(Reporter):
                 client.send(to_json(event).decode())
 
     def _initialize_state_machine(self):
+        logger.debug("Initializing state machines")
         initialized = (Init,)
         jobs = (Start, Running, Exited)
         finished = (Finish,)
@@ -68,6 +86,9 @@ class Event(Reporter):
         if self._state not in self._transitions or not isinstance(
             msg, self._transitions[self._state]
         ):
+            logger.error(
+                f"{msg} illegal state transition: {self._state} -> {new_state}"
+            )
             raise TransitionError(
                 f"Illegal transition {self._state} -> {new_state} for {msg}, expected to transition into {self._transitions[self._state]}"
             )
@@ -75,7 +96,12 @@ class Event(Reporter):
         self._states[new_state](msg)
         self._state = new_state
 
-    def _dump_event(self, event):
+    def _dump_event(self, attributes: Dict[str, str], data: Any = None):
+        if data is None and _CONTENT_TYPE in attributes:
+            attributes.pop(_CONTENT_TYPE)
+
+        event = CloudEvent(attributes=attributes, data=data)
+        logger.debug(f'Schedule {type(event)} "{event["type"]}" for delivery')
         self._event_queue.put(event)
 
     def _step_path(self):
@@ -87,76 +113,58 @@ class Event(Reporter):
         self._step_id = msg.step_id
         self._event_publisher_thread.start()
 
-    def _job_handler(self, msg):
-        job_path = f"{self._step_path()}/job/{msg.job.index}"
-
+    def _job_handler(self, msg: Message):
+        job_name = msg.job.name()
+        job_msg_attrs = {
+            _JOB_SOURCE: f"{self._step_path()}/job/{msg.job.index}",
+            _CONTENT_TYPE: "application/json",
+        }
         if isinstance(msg, Start):
+            logger.debug(f"Job {job_name} was successfully started")
             self._dump_event(
-                CloudEvent(
-                    {
-                        "type": _FM_JOB_START,
-                        "source": job_path,
-                        "datacontenttype": "application/json",
-                    },
-                    {
-                        "stdout": str(Path(msg.job.std_out).resolve()),
-                        "stderr": str(Path(msg.job.std_err).resolve()),
-                    },
-                )
+                attributes={_JOB_MSG_TYPE: _FM_JOB_START, **job_msg_attrs},
+                data={
+                    "stdout": str(Path(msg.job.std_out).resolve()),
+                    "stderr": str(Path(msg.job.std_err).resolve()),
+                },
             )
             if not msg.success():
+                logger.error(f"Job {job_name} FAILED to start")
                 self._dump_event(
-                    CloudEvent(
-                        {
-                            "type": _FM_JOB_FAILURE,
-                            "source": job_path,
-                            "datacontenttype": "application/json",
-                        },
-                        {
-                            "error_msg": msg.error_message,
-                        },
-                    )
+                    attributes={_JOB_MSG_TYPE: _FM_JOB_FAILURE, **job_msg_attrs},
+                    data={
+                        "error_msg": msg.error_message,
+                    },
                 )
 
         elif isinstance(msg, Exited):
+            data = None
             if msg.success():
-                self._dump_event(
-                    CloudEvent(
-                        {
-                            "type": _FM_JOB_SUCCESS,
-                            "source": job_path,
-                        },
-                        None,
-                    )
-                )
+                logger.debug(f"Job {job_name} exited successfully")
+                attributes = {_JOB_MSG_TYPE: _FM_JOB_SUCCESS, **job_msg_attrs}
             else:
-                self._dump_event(
-                    CloudEvent(
-                        {
-                            "type": _FM_JOB_FAILURE,
-                            "source": job_path,
-                            "datacontenttype": "application/json",
-                        },
-                        {
-                            "exit_code": msg.exit_code,
-                            "error_msg": msg.error_message,
-                        },
+                logger.error(
+                    _JOB_EXIT_FAILED_STRING.format(
+                        job_name=msg.job.name(),
+                        exit_code=msg.exit_code,
+                        error_message=msg.error_message,
                     )
                 )
+                attributes = {_JOB_MSG_TYPE: _FM_JOB_FAILURE, **job_msg_attrs}
+                data = {
+                    "exit_code": msg.exit_code,
+                    "error_msg": msg.error_message,
+                }
+            self._dump_event(attributes=attributes, data=data)
 
         elif isinstance(msg, Running):
+            logger.debug(f"{job_name} job is running")
             self._dump_event(
-                CloudEvent(
-                    {
-                        "type": _FM_JOB_RUNNING,
-                        "source": job_path,
-                        "datacontenttype": "application/json",
-                    },
-                    {
-                        "max_memory_usage": msg.max_memory_usage,
-                        "current_memory_usage": msg.current_memory_usage,
-                    },
-                )
+                attributes={_JOB_MSG_TYPE: _FM_JOB_RUNNING, **job_msg_attrs},
+                data={
+                    "max_memory_usage": msg.max_memory_usage,
+                    "current_memory_usage": msg.current_memory_usage,
+                },
             )
 
     def _finished_handler(self, msg):
