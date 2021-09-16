16d5290265923c24ca138219306cd4fb1aa9314c
Morten Bendiksen <morten.bendiksen@gmail.com>
2021-09-16 16:27:52 +0200

    Calculate misfits in dark_storage

--- ert_shared/dark_storage/endpoints/compute/misfits.py
+++ ert_shared/dark_storage/endpoints/compute/misfits.py
@@ -4,8 +4,14 @@ from typing import Optional
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import Response
 
-from ert_shared.dark_storage.enkf import LibresFacade, get_res
+from ert_shared.dark_storage.common import data_for_key
+from ert_shared.dark_storage.enkf import LibresFacade, get_res, get_name
+import pandas as pd
 
+from ert_shared.storage.extraction import create_observations
+from ert_storage.compute import calculate_misfits_from_pandas
+
+from ert_storage import exceptions as exc
 
 router = APIRouter(tags=["misfits"])
 
@@ -26,4 +32,33 @@ async def get_response_misfits(
     realization_index: Optional[int] = None,
     summary_misfits: bool = False,
 ) -> Response:
-    raise NotImplementedError
+
+    ensemble_name = get_name("ensemble", ensemble_id)
+    dataframe = data_for_key(ensemble_name, response_name)
+    if realization_index or realization_index == 0:
+        dataframe = pd.DataFrame(dataframe.loc[realization_index]).T
+
+    response_dict = {}
+    for index, data in dataframe.iterrows():
+        data_df = pd.DataFrame(data).T
+        response_dict[index] = data_df
+
+    obs = create_observations(res)
+    # TODO: is it ok to simply take the first element here?
+    obs_key = res.observation_keys(response_name)[0]
+    for o in obs:
+        if o["name"] == obs_key:
+            observation_df = pd.DataFrame(
+                data={"values": o["values"], "errors": o["errors"]},
+                index=[int(x) for x in o["x_axis"]],
+            )
+    try:
+        result_df = calculate_misfits_from_pandas(
+            response_dict, observation_df, summary_misfits
+        )
+    except Exception as misfits_exc:
+        raise exc.UnprocessableError(f"Unable to compute misfits: {misfits_exc}")
+    return Response(
+        content=result_df.to_csv().encode(),
+        media_type="text/csv",
+    )
