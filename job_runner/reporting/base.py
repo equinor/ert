c85960ed09fca67cb48a07888bd16b973f5be4d0
Sharlon Regales <sharlon.regales@tno.nl>
2021-09-22 14:45:46 +0000

    Add typing to abstract Reporter

--- job_runner/reporting/base.py
+++ job_runner/reporting/base.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from job_runner.reporting.message import Message
 
 
 class Reporter(ABC):
     @abstractmethod
-    def report(self, msg):
+    def report(self, msg: Message):
         """Report a message."""
         pass
