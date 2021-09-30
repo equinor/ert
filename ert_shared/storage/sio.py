38692fe9c972eaafdf1076673600d3e5c0161e3f
Zohar Malamant <ZOM@equinor.com>
2021-09-30 18:10:57 +0200

    : progress 1

--- ert_shared/storage/sio.py
+++ ert_shared/storage/sio.py
@@ -1,9 +1,13 @@
 import os
+import io
 import sys
+from enum import Enum
 from pathlib import Path
-from typing import Generator, Optional
+from typing import Any, BinaryIO, Generator, ItemsView, Optional
 from pydantic import BaseModel
 import numpy as np
+import math
+import zlib
 
 import struct
 
@@ -17,7 +21,6 @@ BLOCK_FS_DRIVER_ID = 3001  # only one that's valid today
 
 # res_util/block_fs.cpp
 MOUNT_MAP_MAGIC_INT = 8861290
-INDEX_MAGIC_INT = 1213775
 INDEX_FORMAT_VERSION = 1
 NODE_IN_USE = 1431655765
 NODE_FREE = -1431655766
@@ -25,198 +28,228 @@ NODE_WRITE_ACTIVE = 77162
 NODE_END_TAG = 16711935
 
 
-class Index(BaseModel):
-    name: Optional[str] = None
-    node_offset: int = 0
-    node_length: int = 0
-    data_offset: int = 0
-    data_length: int = 0
-
-
-class ByteStream:
-    def __init__(self, stream):
-        self.stream = stream
-
-    def u64(self) -> int:
-        return struct.unpack("<Q", self.read(8))[0]
-
-    def i64(self) -> int:
-        return struct.unpack("<q", self.read(8))[0]
-
-    def u32(self) -> int:
-        return struct.unpack("<I", self.read(4))[0]
-
-    def i32(self) -> int:
-        return struct.unpack("<i", self.read(4))[0]
-
-    def read(self, count) -> bytes:
-        data = self.stream.read(count)
-        if len(data) < count:
-            raise StopIteration
-        return data
-
-    def ecl_string(self) -> str:
-        count = self.i32()
-        if count < 0:
-            return ""
-        return self.read(count + 1)[:-1].decode()
-
-    def ecl_array(self, dtype: str) -> np.ndarray:
-        count = self.i32()
-        assert count >= 0
-
-        t = np.dtype(dtype)
-        b = self.read(t.itemsize * count)
-        return np.frombuffer(b, dtype=t)
-
-    def ecl_stringlist(self) -> list[str]:
-        count = self.i32()
-        assert count >= 0
-
-        return [self.ecl_string() for _ in range(count)]
-
-    def assert_eof(self) -> None:
-        assert self.stream.read(1) == b""
-
-
-class Ensemble:
-    def __init__(self, path: Path) -> None:
-        self.path = path
-
-        self._check_fstab()
-
-    def _check_fstab(self) -> None:
-        fstab = ByteStream((self.path / "ert_fstab").open("rb"))
-
-        magic = fstab.u64()
-        assert magic == FS_MAGIC_ID
-
-        version = fstab.u32()
-        assert version == CURRENT_FS_VERSION
-
-        driver_id = fstab.u32()
-        assert driver_id == BLOCK_FS_DRIVER_ID
-
-        try:
-            while True:
-                self._read_fs(fstab)
-        except StopIteration:
-            ...
-
-        for i in range(32):
-            self.read_data("FORECAST", i)
-
-    def _read_fs(self, fstab: ByteStream) -> None:
-        driver_type = fstab.u32()
-        # assert driver_type == 1
-        num_fs = fstab.u32()
-        mountfile_fmt = fstab.ecl_string()
-
-    def read_time_map(self) -> None:
-        stream = ByteStream((self.path / "files" / "time-map").open("rb"))
-        return stream.ecl_array("datetime64[s]")
-
-    def read_summary_key_set(self) -> None:
-        stream = ByteStream((self.path / "files" / "summary-key-set").open("rb"))
-        return stream.ecl_stringlist()
-
-    def read_data(self, name: str, mod: int = 0) -> None:
-        mnt = ByteStream((self.path / "Ensemble" / f"mod_{mod}" / f"{name}.mnt").open("rb"))
-        assert mnt.u32() == MOUNT_MAP_MAGIC_INT
-        version = mnt.u32()
-        mnt.assert_eof()
-
-        index = ByteStream(
-            (self.path / "Ensemble" / f"mod_{mod}" / f"{name}.index").open("rb")
+def read_exact(stream: BinaryIO, count: int) -> bytes:
+    assert count > 0
+    data = stream.read(count)
+    if len(data) != count:
+        raise IOError(
+            f"Premature EOF: Attempted to read {count} bytes, but {len(data)} remained"
         )
-        assert index.u32() == INDEX_MAGIC_INT
-        assert index.u32() == INDEX_FORMAT_VERSION
-        index.u64()  # datetime64[s] of the data file
+    return data
 
-        num_active_nodes = index.i32()
-        print("num_active_nodes", num_active_nodes)
-        indices: list[Index] = []
-        for i in range(num_active_nodes):
-            node_name = index.ecl_string()
-            status = index.i32()
-            node_offset = index.i64()
-            node_length = index.i32()
-            data_offset = index.i32()
-            data_length = index.i32()
 
-            assert status == NODE_IN_USE
-            assert node_offset >= 0
-            assert node_length >= 0
-            assert data_offset >= 0
-            assert data_length >= 0
+def read_unpack(stream: BinaryIO, fmt: str) -> tuple[Any, ...]:
+    size = struct.calcsize(fmt)
+    data = read_exact(stream, size)
+    return struct.unpack(fmt, data)
 
-            indices.append(
-                Index(
-                    name=node_name,
-                    node_offset=node_offset,
-                    node_length=node_length,
-                    data_offset=data_offset,
-                    data_length=data_length,
-                )
-            )
 
-        num_free_nodes = index.i32()
-        print("num_free_nodes", num_free_nodes)
-        for _ in range(num_free_nodes):
-            status = index.i32()
-            node_offset = index.i64()
-            node_length = index.i32()
-            data_offset = index.i32()
-            data_length = index.i32()
+def read_eclstring(stream: BinaryIO) -> str:
+    """
+    A libecl string consists of a 32-bit signed little endian integer 'n'
+    followed by 'n' ASCII characters.
+    """
+    (count,) = read_unpack(stream, "<i")
+    return read_exact(stream, count + 1)[:-1].decode()
 
-            assert status == NODE_FREE
-            assert node_offset >= 0
-            assert node_length >= 0
-            assert data_offset >= 0
-            assert data_length >= 0
 
-            indices.append(
-                Index(
-                    name=None,
-                    node_offset=node_offset,
-                    node_length=node_length,
-                    data_offset=data_offset,
-                    data_length=data_length,
-                )
-            )
-        index.assert_eof()
+@np.vectorize
+def _func(x: float) -> float:
+    min_ = 0.1
+    max_ = 0.5
+    y = 0.5 * (1 + math.erf(x / math.sqrt(2.0)))
+    return y * (max_ - min_) + min_
 
-        data = ByteStream(
-            (self.path / "Ensemble" / f"mod_{mod}" / f"{name}.data_{version}").open("rb")
+
+def read_data(data: bytes) -> np.ndarray:
+    (file_type,) = struct.unpack("<i", data[:4])
+    if file_type == 104:  # FIELD
+        data = zlib.decompress(data[4:])
+        return np.frombuffer(data)  # , dtype=np.float64)  # Either int, float or double
+    if file_type == 107 or file_type == 102:
+        # Either GEN_KW (107) or MULTFLT (102, deprecated since 2009)
+        return np.frombuffer(data[4:], dtype=np.float64)
+    if file_type == 110:  # SUMMARY
+        size, default_value = struct.unpack("<id", data[4:16])
+        # default_value is an ecl thing that we ignore
+        assert size * 8 + 16 == len(data)
+        return np.frombuffer(data[16:], dtype=np.float64)
+    if file_type == 113:  # GEN_DATA
+        size, report_step = struct.unpack("<ii", data[4:12])
+        # decompress with type
+    if file_type == 114:  # SURFACE
+        return np.frombuffer(data[4:], dtype=np.float64)
+    if file_type == 115:  # CONTAINER
+        # doesn't implement read_from_buffer
+        raise NotImplementedError
+    if file_type == 116:  # EXT_PARAM
+        # unsure how to read this type
+        raise NotImplementedError
+
+
+class NodeStatus(Enum):
+    in_use = 0
+    free = 1
+    invalid = 2
+
+
+class NodeIndex(BaseModel):
+    """
+    Index of "node" in the data file
+    """
+
+    node_offset: int = 0  # Offset to the start of the "node"
+    node_length: int = 0  # Size preallocated to this node
+    data_offset: int = 0  # Offset from the start of the "node" to the start of data
+    data_length: int = 0  # Size of data contained in the "node"
+
+
+class IndexFile:
+    """
+    Loader class for block_fs .index files
+    """
+
+    # From res_utl/block_fs.cpp, INDEX_MAGIC_INT
+    _FILE_MAGIC = 1213775
+
+    # From res_utl/block_fs.cpp, INDEX_FORMAT_VERSION
+    _FILE_VERSION = 1
+
+    def __init__(self, stream: BinaryIO) -> None:
+        self._used_indices: dict[str, NodeIndex] = {}
+        self._unused_indices: list[NodeIndex] = []
+
+        self._read_file(stream)
+
+    def __getitem__(self, key: str) -> NodeIndex:
+        return self._used_indices[key]
+
+    def _read_file(self, stream: BinaryIO) -> None:
+        # struct header {
+        #   int magic;   // == _FILE_MAGIC
+        #   int version; // == _FILE_VERSION
+        #   time_t mod;
+        # }
+        magic, version, time, nactive = read_unpack(stream, "<iiQi")
+        assert magic == self._FILE_MAGIC
+        assert version == self._FILE_VERSION
+        # assert time == ???
+
+        for _ in range(nactive):
+            key = read_eclstring(stream)
+            status, index = self._read_node(stream)
+            assert status == NodeStatus.in_use
+            assert key not in self._used_indices
+            print(key, index)
+            self._used_indices[key] = index
+
+        (nfree,) = read_unpack(stream, "<i")
+        for _ in range(nfree):
+            status, index = self._read_node(stream)
+            assert status == NodeStatus.free
+            self._unused_indices.append(index)
+
+        # Check EOF
+        assert stream.read(1) == b""
+
+    def _read_node(self, stream: BinaryIO) -> tuple[NodeStatus, NodeIndex]:
+        # struct node {
+        #   int status;
+        #   uint64_t node_offset;
+        #   int node_length;
+        #   int data_offset;
+        #   int data_length;
+        # }
+        raw_status, node_offset, node_length, data_offset, data_length = read_unpack(
+            stream, "<iqiii"
         )
-        while True:
-            status = data.i32()
-            key = None
-            if status == NODE_IN_USE:
-                key = data.ecl_string()
-            elif status == NODE_FREE:
-                key = None
-            elif status == NODE_END_TAG:
-                break
-            else:
-                raise ValueError(f"Unknown status {status}")
+        if raw_status == NODE_IN_USE:
+            status = NodeStatus.in_use
+        elif raw_status == NODE_FREE:
+            status = NodeStatus.free
+        else:
+            status = NodeStatus.invalid
+        return (
+            status,
+            NodeIndex(
+                node_offset=node_offset,
+                node_length=node_length,
+                data_offset=data_offset,
+                data_length=data_length,
+            ),
+        )
 
-            print("node_size", data.i32())
-            if status == NODE_IN_USE:
-                print("data_size", data.i32())
+
+class DataFile:
+    """
+    Loader class for block_fs .data_N files
+    """
+
+    def __init__(self, stream: BinaryIO) -> None:
+        self._stream = stream
+
+    def __getitem__(self, index: NodeIndex) -> bytes:
+        self._stream.seek(index.node_offset + index.data_offset + 8)
+        return read_exact(self._stream, index.data_length)
+        # return np.frombuffer(data, dtype=np.float32)
 
 
-def each_ensemble() -> Generator[Ensemble, None, None]:
+# class Ensemble:
+#     def __init__(self, path: Path) -> None:
+#         self.path = path
+
+#         self._check_fstab()
+
+#     def _check_fstab(self) -> None:
+#         fstab = ByteStream((self.path / "ert_fstab").open("rb"))
+
+#         magic = fstab.u64()
+#         assert magic == FS_MAGIC_ID
+
+#         version = fstab.u32()
+#         assert version == CURRENT_FS_VERSION
+
+#         driver_id = fstab.u32()
+#         assert driver_id == BLOCK_FS_DRIVER_ID
+
+#         try:
+#             while True:
+#                 self._read_fs(fstab)
+#         except StopIteration:
+#             ...
+
+#         for i in range(32):
+#             self.read_data("FORECAST", i)
+
+#     def read_data(self, name: str, mod: int) -> None:
+#         index = IndexFile()
+
+
+def each_ensemble() -> None:
     root = Path("storage/snake_oil/ensemble")
     for path in root.glob("*/"):
-        if path.is_dir():
-            yield Ensemble(path)
+        if not path.is_dir():
+            continue
+
+        for mod in range(32):
+            base = str(path / f"Ensemble/mod_{mod}/PARAMETER")
+            index_file = IndexFile(open(f"{base}.index", "rb"))
+            data_file = DataFile(open(f"{base}.data_0", "rb"))
+
+            for key, index in index_file._used_indices.items():
+                print("Trying to read", key)
+                data = data_file[index]
+                read_data(data)
+
             sys.exit(0)
+
+    #     if path.is_dir():
+    #         yield Ensemble(path)
+    #         sys.exit(0)
 
 
 def main() -> None:
-    for ens in each_ensemble():
-        pass
+    each_ensemble()
 
 
 if __name__ == "__main__":
