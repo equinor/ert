0b81ae76bbdc9290066c8e0587660adde2701e96
Sondre Sortland <sonso@equinor.com>
2021-10-12 15:21:40 +0200

    Set input and output type in polynomial function
    As part of 278921bc2, the output type from the polynomial
    function was changed; however, the typing was not changed
    accordingly. Also, use types from the typing module.

--- ert3_examples/polynomial/function_steps/functions.py
+++ ert3_examples/polynomial/function_steps/functions.py
@@ -1,4 +1,7 @@
-def polynomial(coefficients: dict) -> tuple:
+from typing import Dict, Tuple
+
+
+def polynomial(coefficients: Dict[str, float]) -> Dict[str, Tuple]:
     x_range = tuple(range(10))
     result = tuple(
         coefficients["a"] * x ** 2 + coefficients["b"] * x + coefficients["c"]
