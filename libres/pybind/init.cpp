fbf74e19f075ab2602c6e72cced58a5f068483fd
Zohar Malamant <ZOM@equinor.com>
2021-10-14 00:43:27 +0200

    Reduce the amount of C++ code required

--- libres/pybind/init.cpp
+++ libres/pybind/init.cpp
@@ -12,53 +12,41 @@ template <class T> T cwrap_cast(py::object obj) {
     return reinterpret_cast<T>(cwrap_ptr);
 }
 
-namespace {
+using type = summary_obs_type;
+using type_ref = type &;
+using type_cref = const type &;
+using shared = std::shared_ptr<type>;
 
-struct deleter {
-    void operator()(summary_obs_type *ptr) { summary_obs_free(ptr); }
-};
+template <class T, class B> auto fgetter(T B::*p) {
+    return [p](const B &s) { return s.*p; };
+}
 
-class summary_observation {
-public:
-    std::unique_ptr<summary_obs_type, deleter> m_ptr;
-
-public:
-    summary_observation(summary_obs_type *ptr) : m_ptr(ptr) {}
-    summary_observation(summary_observation &&) = default;
-    summary_observation &operator=(summary_observation &&) = default;
-
-    static summary_observation alloc(const std::string &arg0,
-                                     const std::string &arg1, double arg2,
-                                     double arg3) {
-        return {summary_obs_alloc(arg0.c_str(), arg1.c_str(), arg2, arg3)};
-    }
-
-    void update_std_scale(double arg0, py::object arg1) {
-        summary_obs_update_std_scale(m_ptr.get(), arg0,
-                                     cwrap_cast<active_list_type *>(arg1));
-    }
-
-    void set_std_scale(double arg0) {
-        summary_obs_set_std_scale(m_ptr.get(), arg0);
-    }
-};
-} // namespace
+template <class R, class T, class B> auto frgetter(T B::*p) {
+    return [p](const B &s) { return R{s.*p}; };
+}
 
 PYBIND11_MODULE(_clib, m) {
-    py::class_<summary_observation>(m, "_SummaryObservationImpl")
-        .def(py::init(&summary_observation::alloc))
-        .def("getValue",
-             [](const summary_observation &self) { return self.m_ptr->value; })
-        .def("getStandardDeviation",
-             [](const summary_observation &self) { return self.m_ptr->std; })
-        .def("getStdScaling",
-             [](const summary_observation &self, int index=0) {
-                 return self.m_ptr->std_scaling;
-             })
-        .def("getSummaryKey",
-             [](const summary_observation &self) {
-                 return std::string(self.m_ptr->summary_key);
-             })
-        .def("updateStdScaling", &summary_observation::update_std_scale)
-        .def("set_std_scaling", &summary_observation::set_std_scale);
+    py::class_<type, shared> cls(m, "_SummaryObservationImpl");
+    cls.def(py::init([](const std::string &arg0, const std::string &arg1,
+                        double arg2, double arg3) {
+        return shared{summary_obs_alloc(arg0.c_str(), arg1.c_str(), arg2, arg3),
+                      summary_obs_free};
+    }));
+    cls.def("getValue", fgetter(&summary_obs_type::value));
+    cls.def("getStandardDeviation", fgetter(&summary_obs_type::std));
+
+    // This getter takes an optional index so we can't use `fgetter`
+    cls.def(
+        "getStdScaling",
+        [](type_cref self, int index) { return self.std_scaling; },
+        py::arg("index") = 0);
+    cls.def("getSummaryKey", frgetter<std::string>(&summary_obs_type::summary_key));
+    cls.def("updateStdScaling",
+            [](type_ref self, double arg0, py::object arg1) {
+                summary_obs_update_std_scale(
+                    &self, arg0, cwrap_cast<active_list_type *>(arg1));
+            });
+    cls.def("set_std_scaling", [](type_ref self, double arg0) {
+        summary_obs_set_std_scale(&self, arg0);
+    });
 }
