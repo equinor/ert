1014477f9517485cda3f6e4c1ae1b263e8c86255
Øyvind Eide <eide.oyvind87@gmail.com>
2021-10-18 16:16:36 +0200

    Add callback function to log util_abort
    logger.exception seemed like the the most appropriate thing to do
    because an exception has occured, however that requires some extra
    boilerplate compared with logger.error

---
+++ ert_logging/_log_util_abort.py
@@ -0,0 +1,29 @@
+import logging
+import traceback
+
+
+def _log_util_abort(filename, lineno, function, message, backtrace):
+    """
+    When this function is called we are in util_abort, so will not be able to recover
+    so we flush the logs to make sure we are able to propagate them.
+    """
+
+    class UtilAbort(Exception):
+        pass
+
+    logger = logging.getLogger(__name__)
+    try:
+        # This might deserve a comment. The reason for raising an exception and
+        # immediately catching it is because logger.exception expects to be called
+        # inside a try .. except block, however no python exception has occured. Instead
+        # of manually creating the traceback a custom exception is raised.
+        raise UtilAbort(message)
+    except UtilAbort:
+        logger.exception(
+            f"C trace:\n{backtrace} \nwith message: {message} \nfrom file: {filename} "
+            f"in {function} at line: {lineno}"
+            f"\n\nPython backtrace:\n{''.join(traceback.format_stack())}"
+        )
+    for handle in logger.handlers:
+        handle.flush()
+    logging.shutdown()
---
+++ tests/ert_tests/shared/test_log_abort.py
@@ -0,0 +1,14 @@
+import logging
+from unittest.mock import MagicMock, call
+from ert_logging._log_util_abort import _log_util_abort
+
+
+def test_log_util_abort(caplog, monkeypatch):
+    shutdown_mock = MagicMock()
+    monkeypatch.setattr(logging, "shutdown", shutdown_mock)
+    _log_util_abort("fname", 1, "some_func", "err_message", "my_backtrace")
+    assert (
+        "C trace:\nmy_backtrace \nwith message: err_message \nfrom file: "
+        "fname in some_func at line: 1\n\nPython backtrace:"
+    ) in caplog.text
+    shutdown_mock.assert_called_once_with()  # must shutdown to propagate message
