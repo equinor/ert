6722f96d2a97e1505c645461612dc696af20ee24
Frode Aarstad <frodeaarstad@gmail.com>
2021-11-05 13:56:21 +0100

    Add connection_info to env

--- tests/ert_tests/services/test_storage_service.py
+++ tests/ert_tests/services/test_storage_service.py
@@ -1,6 +1,9 @@
 import pytest
 import requests
-from ert_shared.services import Storage
+import os
+import json
+from ert_shared.services import Storage, _storage_main
+from ert_shared import port_handler
 
 
 @pytest.mark.requires_ert_storage
@@ -38,3 +41,19 @@ def test_integration_timeout(tmp_path, monkeypatch):
             )
 
     assert not (tmp_path / "storage_server.json").exists()
+
+
+def test_create_connection_string(monkeypatch):
+
+    authtoken = "very_secret_token"
+    _, _, sock = port_handler.find_available_port()
+
+    _storage_main._create_connection_info(sock, authtoken)
+
+    assert "ERT_STORAGE_CONNECTION_STRING" in os.environ
+    connection_string = json.loads(os.environ["ERT_STORAGE_CONNECTION_STRING"])
+    assert "urls" in connection_string
+    assert "authtoken" in connection_string
+    assert len(connection_string["urls"]) == 3
+
+    del os.environ["ERT_STORAGE_CONNECTION_STRING"]
