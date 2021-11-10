94c5f9ac7e0af31ff89cfeda1910a0b63f4c6828
Øyvind Eide <eide.oyvind87@gmail.com>
2021-11-10 19:50:35 +0100

    Add snapshot to update test
    To make refactoring easier we have added snapshots to all smoother
    result to make sure we dont change it inadvertently

--- tests/libres_tests/res/enkf/test_es_update.py
+++ tests/libres_tests/res/enkf/test_es_update.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 import pytest
 
@@ -31,11 +32,113 @@ def test_create(setup_case):
     es_update.getModule("STD_ENKF")
 
 
-def test_update(setup_case):
+@pytest.mark.xfail(sys.platform == "darwin", reason="Different result from update")
+@pytest.mark.parametrize(
+    "module, expected_gen_kw",
+    [
+        (
+            "BOOTSTRAP_ENKF",
+            [
+                -0.13199749242620085,
+                0.19931631512450643,
+                -1.4191070405813302,
+                0.6834759421494758,
+                0.2036333008086772,
+                -1.800805191591989,
+                2.087497120639354,
+                0.6706824284593408,
+                1.202301603428107,
+                -0.2991274236216228,
+            ],
+        ),
+        (
+            "CV_ENKF",
+            [
+                -1.3552992690431354,
+                0.8111762768114376,
+                -1.041495570212803,
+                0.7579452728955424,
+                -0.11919680524287896,
+                -1.69637652448712,
+                0.03478817290978127,
+                0.4178555072326893,
+                0.029729592107867884,
+                0.10861381054387127,
+            ],
+        ),
+        (
+            "IES_ENKF",
+            [
+                -1.1382059686811712,
+                -0.10706650113784337,
+                -0.5321174510859479,
+                0.16314430250454545,
+                -0.7140756660858336,
+                -1.5863225705846462,
+                -0.061532565899646424,
+                -0.3881321947200636,
+                1.0849199162367573,
+                -0.2026982960301163,
+            ],
+        ),
+        # "NULL_ENKF",  # This does nothing, so the test will not pass
+        (
+            "SQRT_ENKF",
+            [
+                -2.9193320576762396,
+                3.1964806431964665,
+                -4.602308580793938,
+                2.6664788740452723,
+                -7.7069598054095945,
+                -2.5899923144860697,
+                -1.8672280427461743,
+                3.8585956848850573,
+                -0.10031545644060243,
+                0.6134116781275245,
+            ],
+        ),
+        (
+            "STD_ENKF",
+            [
+                -1.0279886752792073,
+                -0.7266247822582957,
+                -0.12686045273301966,
+                -0.22659509892534876,
+                -1.120792349644604,
+                -1.4956547646687188,
+                -0.15332787834835337,
+                -0.9161628056286735,
+                1.7415219166473932,
+                -0.4014927497171072,
+            ],
+        ),
+        (
+            "STD_ENKF_DEBUG",
+            [
+                -1.0279886752792073,
+                -0.7266247822582957,
+                -0.12686045273301966,
+                -0.22659509892534876,
+                -1.120792349644604,
+                -1.4956547646687188,
+                -0.15332787834835337,
+                -0.9161628056286735,
+                1.7415219166473932,
+                -0.4014927497171072,
+            ],
+        ),
+    ],
+)
+def test_update(setup_case, module, expected_gen_kw):
+    """
+    Note that this is now a snapshot test, so there is no guarantee that the
+    snapshots are correct, they are just documenting the current behavior.
+    """
     res_config = setup_case("local/snake_oil", "snake_oil.ert")
 
     ert = EnKFMain(res_config)
     es_update = ESUpdate(ert)
+    ert.analysisConfig().selectModule(module)
     fsm = ert.getEnkfFsManager()
     sim_fs = fsm.getFileSystem("default_0")
     target_fs = fsm.getFileSystem("target")
@@ -50,13 +153,34 @@ def test_update(setup_case):
     sim_node.load(sim_fs, node_id)
     target_node.load(target_fs, node_id)
 
-    sim_gen_kw = sim_node.asGenKw()
-    target_gen_kw = target_node.asGenKw()
+    sim_gen_kw = list(sim_node.asGenKw())
+    target_gen_kw = list(target_node.asGenKw())
 
-    assert list(sim_gen_kw) != list(target_gen_kw)
+    assert sim_gen_kw != target_gen_kw
+
+    assert sim_gen_kw == pytest.approx(
+        [
+            -1.3035319087841115,
+            0.8222709205428339,
+            -1.1400029486153482,
+            0.7477534046493867,
+            -0.10400064074767973,
+            -1.7223242794585338,
+            0.0761604027734105,
+            0.4039137216428462,
+            0.10001691562080614,
+            0.09549338450036506,
+        ]
+    )
+    assert target_gen_kw == pytest.approx(expected_gen_kw)
 
 
+@pytest.mark.xfail(sys.platform == "darwin", reason="Different result from update")
 def test_localization(setup_case):
+    """
+    Note that this is now a snapshot test, so there is no guarantee that the
+    snapshots are correct, they are just documenting the current behavior.
+    """
     res_config = setup_case("local/snake_oil", "snake_oil.ert")
     ert = EnKFMain(res_config)
     es_update = ESUpdate(ert)
@@ -109,3 +233,18 @@ def test_localization(setup_case):
     # test that all the other values are left unchanged
     assert sim_gen_kw[3:] == target_gen_kw[3:]
     assert sim_gen_kw[0] == target_gen_kw[0]
+
+    assert target_gen_kw == pytest.approx(
+        [
+            -1.3035319087841115,
+            0.6759029719309165,
+            -0.7802509588954853,
+            0.7477534046493867,
+            -0.10400064074767973,
+            -1.7223242794585338,
+            0.0761604027734105,
+            0.4039137216428462,
+            0.10001691562080614,
+            0.09549338450036506,
+        ]
+    )
