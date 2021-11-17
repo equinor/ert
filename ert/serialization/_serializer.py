d3c46aa04f0a9ad08df1b78b29077f01816b47e7
Lars Petter Hauge <larsenhauge@gmail.com>
2021-11-17 08:55:40 +0100

    Add serializer for EclSum objects

--- ert/serialization/_serializer.py
+++ ert/serialization/_serializer.py
@@ -5,6 +5,7 @@ from typing import Any, Union
 
 import aiofiles
 import yaml
+from ecl.summary import EclSum
 
 
 class Serializer(ABC):
@@ -70,3 +71,26 @@ class _yaml_serializer(Serializer):
         async with aiofiles.open(path, mode="rt", encoding="utf-8") as filehandle:
             contents = await filehandle.read()
             return self.decode(contents, *args, **kwargs)
+
+
+class _ecl_sum_serializer(Serializer):
+    def encode(self, obj: Any, *args: Any, **kwargs: Any) -> str:
+        raise NotImplementedError("not implemented")
+
+    def decode(self, series: str, *args: Any, **kwargs: Any) -> Any:
+        raise NotImplementedError("not implemented")
+
+    async def encode_to_path(
+        self, obj: Any, path: Union[str, Path], *args: Any, **kwargs: Any
+    ) -> None:
+        raise NotImplementedError("not implemented")
+
+    async def decode_from_path(
+        self, path: Union[str, Path], *args: Any, **kwargs: Any
+    ) -> Any:
+        key = kwargs.get("key", None)
+        if key is None:
+            raise ValueError("key must be provided as a keyword argument")
+
+        eclsum = EclSum(str(path))
+        return dict(zip(map(str, eclsum.dates), eclsum.numpy_vector(key)))
