c954dad244e823a5818ec5832ed6848dbf43bc95
Øyvind Eide <oyveid@equinor.com>
2021-12-02 09:00:49 +0100

    Stop running ctests against komodo

--- ci/jenkins/testkomodo-libres-ctest.sh
+++ ci/jenkins/testkomodo-libres-ctest.sh
@@ -17,13 +17,6 @@ build_libres () {
     mkdir -p $LIBRES_BUILD
     pushd $LIBRES_BUILD
     KOMODO_PATH=/prog/res/komodo/${CI_KOMODO_RELEASE}
-    if [ -z "$CI_PR_RUN" ]
-    then
-        # In order to use the .so files from komodo, LD_LIBRARY_PATH must be set
-        export LIBRES_LIB=$(find ${KOMODO_PATH}/root/ -name libres.so -exec dirname {} \;)
-        export LIBECL_LIB=$(find ${KOMODO_PATH}/root/ -name libecl.so -exec dirname {} \;)
-        export LD_LIBRARY_PATH=${LIBECL_LIB}:${LIBRES_LIB}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-    fi
     cmake .. \
           -DCMAKE_PREFIX_PATH=$INSTALL \
           -DCMAKE_INSTALL_PREFIX=$INSTALL \
@@ -31,12 +24,6 @@ build_libres () {
           -DEQUINOR_TESTDATA_ROOT=/project/res-testdata/ErtTestData
     ninja
     popd
-    # Remove built .so files when it is not a PR build
-    if [ -z "$CI_PR_RUN" ]
-    then
-        find $LIBRES_BUILD -name *.so -delete
-        find $INSTALL -name *.so -delete
-    fi
 }
 
 run_libres_ctest() {
--- ci/jenkins/testkomodo.sh
+++ ci/jenkins/testkomodo.sh
@@ -1,61 +1,3 @@
-
-install_libecl () {
-    INSTALL=$WORKSPACE/install
-    mkdir -p $INSTALL
-    LIBECL_ROOT=$WORKSPACE/libecl
-    LIBECL_BUILD=$LIBECL_ROOT/build
-    git clone https://github.com/equinor/libecl $LIBECL_ROOT
-    mkdir -p $LIBECL_BUILD
-    pushd $LIBECL_BUILD
-    cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL -DCMAKE_BUILD_TYPE=RelWithDebInfo
-    ninja install
-    popd
-}
-
-source_build_tools() {
-    source /opt/rh/devtoolset-8/enable
-    export PATH=/opt/rh/devtoolset-8/root/bin:$PATH
-    pip install cmake
-    python --version
-    gcc --version
-    cmake --version
-}
-
-build_libres () {
-    INSTALL=$WORKSPACE/install
-    LIBRES_BUILD=$CI_SOURCE_ROOT/libres/build
-    mkdir -p $LIBRES_BUILD
-    pushd $LIBRES_BUILD
-    KOMODO_PATH=/prog/res/komodo/${CI_KOMODO_RELEASE}
-    if [ -z "$CI_PR_RUN" ]
-    then
-        # In order to use the .so files from komodo, LD_LIBRARY_PATH must be set
-        export LIBRES_LIB=$(find ${KOMODO_PATH}/root/ -name libres.so -exec dirname {} \;)
-        export LIBECL_LIB=$(find ${KOMODO_PATH}/root/ -name libecl.so -exec dirname {} \;)
-        export LD_LIBRARY_PATH=${LIBECL_LIB}:${LIBRES_LIB}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
-    fi
-    cmake .. \
-          -DCMAKE_PREFIX_PATH=$INSTALL \
-          -DCMAKE_INSTALL_PREFIX=$INSTALL \
-          -DBUILD_TESTS=ON \
-          -DEQUINOR_TESTDATA_ROOT=/project/res-testdata/ErtTestData
-    ninja
-    popd
-    # Remove built .so files when it is not a PR build
-    if [ -z "$CI_PR_RUN" ]
-    then
-        find $LIBRES_BUILD -name *.so -delete
-        find $INSTALL -name *.so -delete
-    fi
-}
-
-run_libres_ctest() {
-    pushd $LIBRES_BUILD
-    export ERT_SITE_CONFIG=${CI_SOURCE_ROOT}/ert_shared/share/ert/site-config
-    ctest -j 6 -E Lint --output-on-failure
-    popd
-}
-
 copy_test_files () {
     cp -r ${CI_SOURCE_ROOT}/tests ${CI_TEST_ROOT}
 
@@ -77,10 +19,6 @@ copy_test_files () {
 
 install_test_dependencies () {
     pip install -r dev-requirements.txt
-}
-
-install_package () {
-    pip install . --no-deps
 }
 
 start_tests () {
@@ -110,12 +48,5 @@ start_tests () {
         --ignore="tests/libres_tests/res/enkf/test_hook_manager.py"
 
     popd
-
-    # build and run libres ctests
-    ci_install_cmake
-    ci_install_conan
-    install_libecl
-    build_libres
-    run_libres_ctest
 
 }
