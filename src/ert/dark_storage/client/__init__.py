7b69b61590cfb9bb7429dca9f39a8021533a7dfa
Zohar Malamant <ZOM@equinor.com>
2021-12-22 10:06:50 +0100

    Add client.Client and client.AsyncClient
    These replace client.Session with versions that:
    1. Cache a global connection information
    2. Accept a `ConnInfo` object so that we don't have to create files or
       set environment variables.
    3. Look for `storage_server.json` in any parent directory

--- src/ert/dark_storage/client/__init__.py
+++ src/ert/dark_storage/client/__init__.py
@@ -1,3 +1,5 @@
-from .session import Session
+from ._session import ConnInfo
+from .async_client import AsyncClient
+from .client import Client
 
-__all__ = ["Session"]
+__all__ = ["AsyncClient", "Client", "ConnInfo"]
---
+++ src/ert/dark_storage/client/async_client.py
@@ -0,0 +1,21 @@
+import httpx
+from typing import Optional
+
+from ._session import ConnInfo, find_conn_info
+
+
+class AsyncClient(httpx.AsyncClient):
+    """
+    Wrapper class for httpx.AsyncClient that provides a user-friendly way to
+    interact with ERT Storage's API
+    """
+
+    def __init__(self, conn_info: Optional[ConnInfo] = None) -> None:
+        if conn_info is None:
+            conn_info = find_conn_info()
+
+        headers = {}
+        if conn_info.auth_token is not None:
+            headers = {"Token": conn_info.auth_token}
+
+        super().__init__(base_url=conn_info.base_url, headers=headers)
