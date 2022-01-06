7106d0a5b25650a9b3a03444b7cd921d22ca45cd
Håvard Berland <havb@equinor.com>
2022-01-06 09:41:12 +0100

    Add Snake OIL as an ERT3 example
    The snake_oil_simulator is adapted from the corresponding ERT2
    example.

---
+++ ert3_examples/snake_oil/render.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+# TODO: This script is a hack in its entirety to get hold the template_render
+# job from share/ert/forward-models/templating/script/
+# Over time this script should disappear.
+
+import subprocess
+import sys
+
+import pkg_resources
+
+if __name__ == "__main__":
+    render_exec = pkg_resources.resource_filename(
+        "ert_shared", "share/ert/forward-models/templating/script/template_render"
+    )
+    args = tuple(sys.argv[1:])
+    subprocess.run(" ".join((str(render_exec),) + args), shell=True, check=True)
