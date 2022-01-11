cf153ab1aaf5720d06f31fbb3b4b01743f3ec059
Terry Hannant <thannant_github@metafu.net>
2022-01-11 08:53:05 +0100

    Reimplement building of gen_data export to libs res
    For increased performance ,the allocation and population
    of numpy array returned by gen_data_collector has been
    moved from Python to C++

--- libres/lib/include/ert/enkf/enkf_plot_gendata.hpp
+++ libres/lib/include/ert/enkf/enkf_plot_gendata.hpp
@@ -39,6 +39,9 @@ enkf_plot_gendata_type *
 enkf_plot_gendata_alloc_from_obs_vector(const obs_vector_type *obs_vector);
 void enkf_plot_gendata_free(enkf_plot_gendata_type *data);
 int enkf_plot_gendata_get_size(const enkf_plot_gendata_type *data);
+int enkf_plot_gendata_get_data_size(const enkf_plot_gendata_type *data);
+std::vector<bool>
+enkf_plot_gendata_active_mask(const enkf_plot_gendata_type *data);
 enkf_plot_genvector_type *
 enkf_plot_gendata_iget(const enkf_plot_gendata_type *plot_data, int index);
 void enkf_plot_gendata_load(enkf_plot_gendata_type *plot_data, enkf_fs_type *fs,
@@ -49,7 +52,6 @@ double_vector_type *
 enkf_plot_gendata_get_min_values(enkf_plot_gendata_type *plot_data);
 double_vector_type *
 enkf_plot_gendata_get_max_values(enkf_plot_gendata_type *plot_data);
-
 UTIL_IS_INSTANCE_HEADER(enkf_plot_gendata);
 
 #ifdef __cplusplus
