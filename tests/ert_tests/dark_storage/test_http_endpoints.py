a3795f052a914483bcf620c53e445d7f6303a696
DanSava <dan.sava42@gmail.com>
2022-02-07 10:47:33 +0200

    Update ensemble parameter response from dark storage

--- tests/ert_tests/dark_storage/test_http_endpoints.py
+++ tests/ert_tests/dark_storage/test_http_endpoints.py
@@ -125,9 +125,9 @@ def test_get_ensemble_parameters(poly_example_tmp_dir, dark_storage_client):
     parameters_json = resp.json()
 
     assert len(parameters_json) == 3
-    assert parameters_json[0] == "COEFFS:COEFF_A"
-    assert parameters_json[1] == "COEFFS:COEFF_B"
-    assert parameters_json[2] == "COEFFS:COEFF_C"
+    assert parameters_json[0] == {"labels": [], "name": "COEFFS:COEFF_A"}
+    assert parameters_json[1] == {"labels": [], "name": "COEFFS:COEFF_B"}
+    assert parameters_json[2] == {"labels": [], "name": "COEFFS:COEFF_C"}
 
 
 def test_get_experiment_observations(poly_example_tmp_dir, dark_storage_client):
