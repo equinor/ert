1f051a5ac19184e6fdbd6821f435a05895232a1a
xjules <jparu@equinor.com>
2022-02-16 15:57:44 +0100

    Add ert3 cli monitoring
    It includes a DummyBaseRunModel class required for the legace Monitor
    class.

--- ert3/evaluator/_evaluator.py
+++ ert3/evaluator/_evaluator.py
@@ -1,26 +1,95 @@
 import pickle
-from typing import Dict
+import time
+from typing import Dict, Optional, Union, Any
+from concurrent import futures
+import sys
 
 import ert
 from ert_shared.ensemble_evaluator.config import EvaluatorServerConfig
+from ert_shared.models.base_run_model import BaseRunModel
+from ert_shared.status.tracker.evaluator import EvaluatorTracker
+from ert_shared.cli.monitor import Monitor
 from ert_shared.ensemble_evaluator.ensemble.base import Ensemble
-from ert_shared.ensemble_evaluator.entity.identifiers import EVTYPE_EE_TERMINATED
+from ert_shared.ensemble_evaluator.entity.identifiers import (
+    EVTYPE_EE_TERMINATED,
+)
 from ert_shared.ensemble_evaluator.evaluator import EnsembleEvaluator
 
 from ert_shared.status.entity.state import (
     ENSEMBLE_STATE_STOPPED,
     ENSEMBLE_STATE_FAILED,
-    REALIZATION_STATE_FINISHED,
 )
+
+
+class DummyBaseRunModel(BaseRunModel):
+    def __init__(self, phase_count: int = 1):
+        self._phase: int = 0
+        self._phase_count: int = phase_count
+        self._phase_name: str = "Starting..."
+        self._job_start_time: int = 0
+        self._job_stop_time: int = 0
+        self._indeterminate: bool = False
+        self._fail_message: str = ""
+        self._failed: bool = False
+
+    def create_context(self, arguments) -> Any:
+        return None
+
+    def runSimulations(self, arguments):
+        return self.create_context(arguments)
+
+    def isFinished(self) -> bool:
+        return self._phase_count == self._phase or self.hasRunFailed()
+
+    def teardown_context(self) -> None:
+        return None
+
+    def hasRunFailed(self) -> bool:
+        return self._failed
+
+    def getFailMessage(self) -> str:
+        return self._fail_message
+
+    def getPhaseName(self) -> str:
+        return self._phase_name
+
+    def currentPhase(self) -> int:
+        return self._phase
+
+    def phaseCount(self) -> int:
+        return self._phase_count
+
+    def setPhase(
+        self, phase: int, phase_name: str, indeterminate: Optional[bool] = None
+    ) -> None:
+        self._phase_name = phase_name
+        if not 0 <= phase <= self._phase_count:
+            raise ValueError(
+                "Phase must be an integer between (inclusive) 0 and {self._phase_count}"
+            )
+
+        if phase == 0:
+            self._job_start_time = int(time.time())
+
+        if phase == self._phase_count:
+            self._job_stop_time = int(time.time())
+
+        self._phase = phase
+
+    def isIndeterminate(self) -> bool:
+        return False
+
+    def setIndeterminate(self, indeterminate: Union[bool, None]) -> None:
+        raise NotImplementedError("This function is not in use in ert3")
 
 
 def _run(
     ensemble_evaluator: EnsembleEvaluator,
+    run_model: DummyBaseRunModel,
 ) -> Dict[int, Dict[str, ert.data.RecordTransmitter]]:
     result: Dict[int, Dict[str, ert.data.RecordTransmitter]] = {}
     with ensemble_evaluator.run() as monitor:
-        realization_ids = set()
-        realizations_completed = set()
+        run_model.setPhase(0, "Running simulations...", indeterminate=False)
         for event in monitor.track():
             if isinstance(event.data, dict) and event.data.get("status") in [
                 ENSEMBLE_STATE_STOPPED,
@@ -28,30 +97,41 @@ def _run(
             ]:
                 monitor.signal_done()
                 if event.data.get("status") == ENSEMBLE_STATE_FAILED:
+                    run_model._failed = True
+                    run_model._fail_message = "Ensemble evaluation failed"
                     raise RuntimeError("Ensemble evaluation failed")
             if event["type"] == EVTYPE_EE_TERMINATED and isinstance(event.data, bytes):
+                run_model.setPhase(1, "Simulations completed.")
                 result = pickle.loads(event.data)
-            if isinstance(event.data, dict) and "reals" in event.data:
-                for real_id in event.data["reals"]:
-                    realization_ids.add(real_id)
-                    real_status = event.data["reals"][real_id].get("status")
-                    if (
-                        real_status == REALIZATION_STATE_FINISHED
-                        and real_id not in realizations_completed
-                    ):
-                        realizations_completed.add(real_id)
-                        print(
-                            f"Realization {real_id} completed successfully"
-                            f" ({len(realizations_completed)}/{len(realization_ids)})"
-                        )
 
     return result
 
 
 def evaluate(
-    ensemble: Ensemble,
+    ensemble: Ensemble, cli: bool = True
 ) -> Dict[int, Dict[str, ert.data.RecordTransmitter]]:
     config = EvaluatorServerConfig()
+
+    run_model = DummyBaseRunModel(1)
+    if cli:
+        tracker = EvaluatorTracker(
+            run_model,
+            config.host,
+            config.port,
+            1,
+            0,
+            token=config.token,
+            cert=config.cert,
+        )
+        monitor = Monitor(out=sys.stderr, color_always=False)  # type: ignore
     ee = EnsembleEvaluator(ensemble=ensemble, config=config, iter_=0)
-    result = _run(ee)
+
+    executor = futures.ThreadPoolExecutor()
+    future = executor.submit(_run, ee, run_model)
+    if cli:
+        try:
+            monitor.monitor(tracker)  # type: ignore
+        except (SystemExit, KeyboardInterrupt):
+            tracker.request_termination()  # type: ignore
+    result: Dict[int, Dict[str, ert.data.RecordTransmitter]] = future.result()
     return result
