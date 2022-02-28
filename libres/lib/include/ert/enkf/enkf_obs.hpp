7390faaab19bff8179e55759ced3615ab38428d6
Joakim Hove <joakim.hove@gmail.com>
2022-02-28 11:47:28 +0100

    Simplify time stepping over summary observations

--- libres/lib/include/ert/enkf/enkf_obs.hpp
+++ libres/lib/include/ert/enkf/enkf_obs.hpp
@@ -46,16 +46,16 @@ int enkf_obs_get_size(const enkf_obs_type *obs);
 bool enkf_obs_is_valid(const enkf_obs_type *);
 void enkf_obs_clear(enkf_obs_type *enkf_obs);
 stringlist_type *enkf_obs_alloc_typed_keylist(enkf_obs_type *enkf_obs,
-                                                  obs_impl_type);
+                                              obs_impl_type);
 stringlist_type *enkf_obs_alloc_matching_keylist(const enkf_obs_type *enkf_obs,
-                                                     const char *input_string);
+                                                 const char *input_string);
 bool enkf_obs_has_key(const enkf_obs_type *, const char *);
 obs_impl_type enkf_obs_get_type(const enkf_obs_type *enkf_obs, const char *key);
 obs_vector_type *enkf_obs_iget_vector(const enkf_obs_type *obs, int index);
 obs_vector_type *enkf_obs_get_vector(const enkf_obs_type *, const char *);
 time_t enkf_obs_iget_obs_time(const enkf_obs_type *enkf_obs, int report_step);
 void enkf_obs_add_obs_vector(enkf_obs_type *enkf_obs,
-                                 const obs_vector_type *vector);
+                             const obs_vector_type *vector);
 }
 
 bool enkf_obs_have_obs(const enkf_obs_type *enkf_obs);
@@ -67,7 +67,6 @@ enkf_obs_type *enkf_obs_alloc(const history_type *history,
                               ensemble_config_type *ensemble_config);
 
 void enkf_obs_free(enkf_obs_type *enkf_obs);
-
 
 void enkf_obs_load(enkf_obs_type *, const char *, double);
 
