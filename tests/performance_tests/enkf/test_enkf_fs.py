14479b71a225a0f76fea34e3de3452de7bed33f7
Bjarne <bjarne.herland@gmail.com>
2022-03-03 14:35:04 +0100

    Remove usage of thread-pool in block_fs_driver
    Replaced thread-pool by std::future. Manual testing shows that on large
    testcases this is more efficient than both thread-pool and sequential
    execution. Added explanation to the constant 32.
    
    Simple benchmark-test, moved fixture from test_es_update.py to conftest
    in order to make it generally available.

---
+++ tests/performance_tests/enkf/test_enkf_fs.py
@@ -0,0 +1,19 @@
+import pytest
+
+from res.enkf import EnKFMain
+
+
+def mount_and_umount(ert, case_name):
+    fs_manager = ert.getEnkfFsManager()
+    assert not fs_manager.isCaseMounted(case_name)
+    mount_point = fs_manager.getFileSystem(case_name)
+    assert fs_manager.isCaseMounted(case_name)
+    del mount_point
+    fs_manager.umount()
+
+
+def test_mount_fs(setup_case, benchmark):
+    res_config = setup_case("local/snake_oil", "snake_oil.ert")
+
+    ert = EnKFMain(res_config)
+    benchmark(mount_and_umount, ert, "default_1")
