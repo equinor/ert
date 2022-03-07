8daed017b7ec9c5241b144afc912f31adcf53e8a
Feda Curic <feda.curic@gmail.com>
2022-03-07 09:48:33 +0100

    Use Eigen in enkf_linalg
    Remove matrix_dgesvd
    Use Eigen in enkf_linalg_svd
    Remove unused dgesvd_get_vector_job
    Co-authored-by: Zohar Malamant ZOM@equinor.com

--- libres/lib/include/ert/analysis/enkf_linalg.hpp
+++ libres/lib/include/ert/analysis/enkf_linalg.hpp
@@ -7,32 +7,27 @@
 #include <ert/res_util/matrix_lapack.hpp>
 #include <ert/res_util/matrix.hpp>
 
-void enkf_linalg_Cee(matrix_type *B, int nrens, const matrix_type *R,
-                     const matrix_type *U0, const double *inv_sig0);
+Eigen::MatrixXd enkf_linalg_Cee(int nrens, const Eigen::MatrixXd &R,
+                                const Eigen::MatrixXd &U0,
+                                const double *inv_sig0);
 
-int enkf_linalg_svdS(const matrix_type *S,
+int enkf_linalg_svdS(const Eigen::MatrixXd &S,
                      const std::variant<double, int> &truncation,
-                     dgesvd_vector_enum jobVT, double *sig0, matrix_type *U0,
-                     matrix_type *V0T);
-
-void enkf_linalg_lowrankCinv__(const matrix_type *S, const matrix_type *R,
-                               matrix_type *V0T, matrix_type *Z, double *eig,
-                               matrix_type *U0,
-                               const std::variant<double, int> &truncation);
+                     Eigen::VectorXd &inv_sig0, Eigen::MatrixXd &U0);
 
 void enkf_linalg_lowrankCinv(
-    const matrix_type *S, const matrix_type *R,
-    matrix_type *W, /* Corresponding to X1 from Eq. 14.29 */
-    double *eig,    /* Corresponding to 1 / (1 + Lambda_1) (14.29) */
+    const Eigen::MatrixXd &S, const Eigen::MatrixXd &R,
+    Eigen::MatrixXd &W,   /* Corresponding to X1 from Eq. 14.29 */
+    Eigen::VectorXd &eig, /* Corresponding to 1 / (1 + Lambda_1) (14.29) */
     const std::variant<double, int> &truncation);
 
 void enkf_linalg_lowrankE(
-    const matrix_type *S, /* (nrobs x nrens) */
-    const matrix_type *E, /* (nrobs x nrens) */
-    matrix_type
-        *W, /* (nrobs x nrmin) Corresponding to X1 from Eqs. 14.54-14.55 */
-    double
-        *eig, /* (nrmin)         Corresponding to 1 / (1 + Lambda1^2) (14.54) */
+    const Eigen::MatrixXd &S, /* (nrobs x nrens) */
+    const Eigen::MatrixXd &E, /* (nrobs x nrens) */
+    Eigen::MatrixXd
+        &W, /* (nrobs x nrmin) Corresponding to X1 from Eqs. 14.54-14.55 */
+    Eigen::VectorXd
+        &eig, /* (nrmin)         Corresponding to 1 / (1 + Lambda1^2) (14.54) */
     const std::variant<double, int> &truncation);
 
 Eigen::MatrixXd enkf_linalg_genX3(const Eigen::MatrixXd &W,
