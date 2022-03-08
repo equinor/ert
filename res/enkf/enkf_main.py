e94798301a813ed2e25e22ff98f5e3181560fab6
Joakim Hove <joakim.hove@gmail.com>
2022-03-08 07:28:50 +0100

    Remove enkf_obs instance from local_obsdata
    The C implementation of local_obsdata has no knowledge of the complete set of
    observation keys; it is therefor quite simple to create a local observation node
    with an invalid observation key. The observation node corresponding to the
    invalid node will be dangling - no real harm.
    
    In Python we previously we attached a enkf_obs instance to the local_obs
    instance, in order to be able to validate keys provided by the user. With this
    PR that 'pimping' of the Python class is removed. This is in preparation to
    upcoming C++ / pybing refactoring, where there is only the C++ class and not a
    separate wrapper class in Python.

--- res/enkf/enkf_main.py
+++ res/enkf/enkf_main.py
@@ -339,7 +339,7 @@ class _RealEnKFMain(BaseCClass):
         """@rtype: LocalConfig"""
         config = self._get_local_config().setParent(self)
         config.initAttributes(
-            self.ensembleConfig(), self.getObservations(), self.eclConfig().getGrid()
+            self.ensembleConfig(), self.eclConfig().getGrid()
         )
         return config
 
