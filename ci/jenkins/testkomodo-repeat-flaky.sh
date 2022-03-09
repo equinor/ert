6e0e1b2ae01fb9ed31f1f9d66b98b2cbb7c72632
Lars Petter Hauge <larsenhauge@gmail.com>
2022-03-09 10:41:53 +0100

    Update test_flaky on jenkins

--- ci/jenkins/testkomodo-repeat-flaky.sh
+++ ci/jenkins/testkomodo-repeat-flaky.sh
@@ -19,15 +19,32 @@ copy_test_files () {
 
 install_test_dependencies () {
     pip install -r dev-requirements.txt
+    pip install pytest-repeat
 }
 
 start_tests () {
     export NO_PROXY=localhost,127.0.0.1
+    export ERT_SHOW_BACKTRACE=1
 
     pushd ${CI_TEST_ROOT}/tests/ert_tests
-    pytest --count=100 -x  tests/ert_tests/dark_storage
-    pytest --count=100 -x  tests/ert_tests/ensemble_evaluator
-    pytest --count=100 -x  tests/ert_tests/ert3
-    popd
+    # We want all of the pytest lines to execute, even if one of the fails.
+    set +e
+    pytest --count=1000 -x shared/test_port_handler.py
+    pytest --count=100 -x dark_storage/test_http_endpoints.py
+    pytest --count=100 -x dark_storage/test_api_compatibility.py
+    pytest --count=100 -x ensemble_evaluator/test_ensemble_legacy.py
 
+    # engine, services and status are integration tests and time consuming,
+    # estimated a several hours just for these
+    pytest --count=30 -x ert3/engine
+    pytest --count=30 -x services
+    pytest --count=30 -x status
+    pytest --count=30 -x ert3/algorithms/test_fast.py # test fast is not fast
+    # The following should be added, but as a subclass of UnitTest TestCase it
+    # is not possible to run with --count (they will only run once)
+    # tests/libres_tests/res/enkf/plot/test_plot_data.py
+    # tests/libres_tests/res/simulator/test_batch_sim.py
+    # tests/libres_tests/res/enkf/data/test_gen_kw_config.py
+    set -e
+    popd
 }
