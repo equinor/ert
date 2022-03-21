b124078f6b9534a2982cf2b879cf97b760748261
Øyvind Eide <eide.oyvind87@gmail.com>
2022-03-21 20:30:51 +0100

    Add logging tool to gui

---
+++ tests/ert_tests/gui/logging/test_logging_tool.py
@@ -0,0 +1,30 @@
+import logging
+
+import pytest
+
+from ert_gui.tools.event_viewer import GUILogHandler, EventViewerPanel
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize(
+    "log_func, expected",
+    [
+        (logger.debug, ""),
+        (logger.info, "INFO     Writing some text"),
+        (logger.warning, "WARNING  Writing some text"),
+        (logger.error, "ERROR    Writing some text"),
+    ],
+)
+def test_logging_widget(qtbot, caplog, log_func, expected):
+    logging_handle = GUILogHandler()
+    logger.addHandler(logging_handle)
+
+    widget = EventViewerPanel(logging_handle)
+    widget.show()
+    qtbot.addWidget(widget)
+
+    qtbot.waitForWindowShown(widget)
+    with caplog.at_level(logging.DEBUG):
+        log_func("Writing some text")
+    assert widget.text_box.toPlainText() == expected
