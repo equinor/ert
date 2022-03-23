760fc59e4d4654f986695212b238f3cdda47e5f5
Jonas G. Drange <jond@equinor.com>
2022-03-23 19:49:32 +0100

    Remove the IDE

--- ert_shared/ide/keywords/data/__init__.py
+++ ert_shared/ide/keywords/data/__init__.py
@@ -1,7 +1,1 @@
 from .validation_status import ValidationStatus
-
-from .token import Token
-from .argument import Argument
-from .keyword import Keyword
-
-from .configuration_line import ConfigurationLine
