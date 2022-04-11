1cbf1c23ce7fb5e6e6a720c1982ab2b3faa44854
Øyvind Eide <oyveid@equinor.com>
2022-04-11 13:11:18 +0200

    Filter out comments from logged configuration

--- ert_shared/main.py
+++ ert_shared/main.py
@@ -441,13 +441,28 @@ def start_ert_server(mode: str):
         yield
 
 
-def log_config(args: ArgumentParser, logger: logging.Logger) -> None:
-    if args.config is not None and os.path.isfile(args.config):
-        with open(args.config, "r", encoding="utf-8") as file_obj:
-            content = file_obj.read()
-            logger.info(
-                f"Content of the configuration file ({args.config}):\n" + content
-            )
+def log_config(config_path: str, logger: logging.Logger) -> None:
+    """
+    Logs what configuration was used to start ert. Because the config
+    parsing is quite convoluted we are not able to remove all the comments,
+    but the easy ones are filtered out.
+    """
+    if config_path is not None and os.path.isfile(config_path):
+        config_context = ""
+        with open(config_path, "r", encoding="utf-8") as file_obj:
+            for line in file_obj:
+                line = line.strip()
+                if not line or line.startswith("--"):
+                    continue
+                if "--" in line:
+                    # There might be a comment in this line, but it could
+                    # also be an argument to a job, so we do a quick check
+                    if not any(x in line for x in ['"', "'"]):
+                        line = line.split("--")[0].rstrip()
+                config_context += line + "\n"
+        logger.info(
+            f"Content of the configuration file ({config_path}):\n" + config_context
+        )
 
 
 def main():
@@ -468,7 +483,7 @@ def main():
         with start_ert_server(args.mode), ErtPluginContext() as context:
             context.plugin_manager.add_logging_handle_to_root(logging.getLogger())
             logger.info("Running ert with {}".format(str(args)))
-            log_config(args, logger)
+            log_config(args.config, logger)
             args.func(args)
     except ErtCliError as err:
         logger.exception(str(err))
