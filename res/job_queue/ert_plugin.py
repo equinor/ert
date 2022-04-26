8f76e84b770f54b26ab4fb3f6e5fa146419fd651
Håvard Berland <havb@equinor.com>
2022-04-26 12:47:21 +0200

    Remove unused startCancellableThread function
    This function is untested, and any call to the functions removed are
    nowhere to be found in any code repository.

--- res/job_queue/ert_plugin.py
+++ res/job_queue/ert_plugin.py
@@ -1,11 +1,8 @@
 from .ert_script import ErtScript
-from threading import Thread
-import time
 
 
 class CancelPluginException(Exception):
-    def __init__(self, cancel_message):
-        super().__init__(cancel_message)
+    """Raised when a plugin is cancelled."""
 
 
 class ErtPlugin(ErtScript):
@@ -20,31 +17,3 @@ class ErtPlugin(ErtScript):
     def getDescription(self):
         """@rtype: str"""
         return "No description provided!"
-
-    def checkIfCancelled(self):
-        if self.isCancelled():
-            raise CancelPluginException(
-                "Plugin '%s' cancelled by user!" % self.getName()
-            )
-
-    def startCancellableThread(self, runFunction, cancelFunction):
-        runFunction.return_value = None
-
-        def runFunctionWrapper():
-            runFunction.return_value = runFunction()
-
-        thread = Thread()
-        thread.run = runFunctionWrapper
-        thread.start()
-
-        while thread.isAlive():
-            if self.isCancelled():
-                cancelFunction()
-
-            try:
-                time.sleep(0.1)
-            except KeyboardInterrupt:
-                print("Plugin '%s' cancelled (CTRL+C)" % self.getName())
-                self.cancel()
-
-        return runFunction.return_value
