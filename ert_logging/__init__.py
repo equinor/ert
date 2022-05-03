8633615b39f00498446b24ad2bd7f8d97060cc56
Øyvind Eide <oyveid@equinor.com>
2022-05-03 10:25:38 +0200

    Add timestamp to log file name

--- ert_logging/__init__.py
+++ ert_logging/__init__.py
@@ -1,3 +1,17 @@
 import pathlib
+import os
+from logging import FileHandler
+from datetime import datetime
 
 LOGGING_CONFIG = pathlib.Path(__file__).parent.resolve() / "logger.conf"
+
+
+class TimestampedFileHandler(FileHandler):
+    def __init__(self, filename: str, *args, **kwargs) -> None:
+        filename, extension = os.path.splitext(filename)
+        filename = (
+            f"{filename}-"
+            f"{datetime.now().strftime(datetime.now().strftime('%Y-%m-%dT%H%M'))}"
+            f"{extension}"
+        )
+        super().__init__(filename, *args, **kwargs)
