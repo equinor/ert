d84937b633e9046702c848038137b1cd80fdcddb
Åsmund Birkeland <musiv@equinor.com>
2022-05-04 07:47:21 +0200

    Fix ies module to be py3.6 numpy<1.20 compatible

--- ert/analysis/ies/__init__.py
+++ ert/analysis/ies/__init__.py
@@ -1,6 +1,5 @@
-from typing import Optional, Union, Any
+from typing import Optional, Union, Any, TYPE_CHECKING
 import numpy as np
-import numpy.typing as npt
 import res
 
 # pylint: disable=import-error
@@ -13,24 +12,27 @@ from res._lib.ies import (  # type: ignore
     inversion_type,
 )
 
+if TYPE_CHECKING:
+    import numpy.typing as npt
+
 
 def make_X(  # pylint: disable=too-many-arguments
-    S: npt.NDArray[np.double],
-    R: npt.NDArray[np.double],
-    E: npt.NDArray[np.double],
-    D: npt.NDArray[np.double],
-    A: npt.NDArray[np.double] = np.empty(shape=(0, 0)),
+    Y: "npt.NDArray[np.double]",
+    R: "npt.NDArray[np.double]",
+    E: "npt.NDArray[np.double]",
+    D: "npt.NDArray[np.double]",
+    A: "npt.NDArray[np.double]" = np.empty(shape=(0, 0)),
     ies_inversion: inversion_type = inversion_type.EXACT,
     truncation: Union[float, int] = 0.98,
-    W0: Optional[npt.NDArray[np.double]] = None,
+    W0: Optional["npt.NDArray[np.double]"] = None,
     step_length: float = 1.0,
     iteration: int = 1,
 ) -> Any:
     if W0 is None:
-        W0 = np.zeros((S.shape[1], S.shape[1]))
+        W0 = np.zeros((Y.shape[1], Y.shape[1]))
     return res._lib.ies.make_X(  # pylint: disable=no-member, c-extension-no-member
         A,
-        S,
+        Y,
         R,
         E,
         D,
@@ -44,11 +46,11 @@ def make_X(  # pylint: disable=too-many-arguments
 
 def update_A(  # pylint: disable=too-many-arguments
     data: ModuleData,
-    A: npt.NDArray[np.double],
-    Y: npt.NDArray[np.double],
-    R: npt.NDArray[np.double],
-    E: npt.NDArray[np.double],
-    D: npt.NDArray[np.double],
+    A: "npt.NDArray[np.double]",
+    Y: "npt.NDArray[np.double]",
+    R: "npt.NDArray[np.double]",
+    E: "npt.NDArray[np.double]",
+    D: "npt.NDArray[np.double]",
     ies_inversion: inversion_type = inversion_type.EXACT,
     truncation: Union[float, int] = 0.98,
     step_length: float = 1.0,
