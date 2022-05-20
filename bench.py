26704a91cd6c73867a8bbda9e4eb47b05cd07020
Zohar Malamant <zom@equinor.com>
2022-05-20 17:14:42 +0200

    : c

--- bench.py
+++ bench.py
@@ -10,6 +10,7 @@ from pathlib import Path
 from concurrent.futures.thread import ThreadPoolExecutor
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
 
 
 print(f"{res.ResPrototype.lib=}")
@@ -32,13 +33,25 @@ class EnkfFsStorage(BaseStorage):
         self._fs = EnkfFs.createFileSystem(str(self.path), mount=True)
 
     def save_parameter(self, name: str, array: npt.NDArray[np.float64]) -> None:
-        with ThreadPoolExecutor(max_workers=2) as exec:
+        with ThreadPoolExecutor(max_workers=1) as exec:
             def fn(x: Tuple[int, npt.NDArray[np.float64]]) -> None:
                 iens = x[0]
                 data = x[1]
                 write_param_vector_raw(self._fs, data, name, iens)
 
             list(exec.map(fn, enumerate(array)))
+
+
+class Hdf5Storage(BaseStorage):
+    path: Path = Path("_tmp_hdf5")
+
+    def __init__(self) -> None:
+        rmtree(self.path, ignore_errors=True)
+        self.path.mkdir()
+
+    def save_parameter(self, name: str, array: npt.NDArray[np.float64]) -> None:
+        with pd.HDFStore(self.path / "params.h5", mode="a") as store:
+            store.put(name, pd.DataFrame(array))
 
 
 def gen_params(parameters: int, ensemble_size: int) -> Generator[Tuple[str, npt.NDArray[np.float64]], None, None]:
@@ -49,9 +62,10 @@ def gen_params(parameters: int, ensemble_size: int) -> Generator[Tuple[str, npt.
 
 def main() -> None:
     storage = EnkfFsStorage()
-    for name, mat in gen_params(parameters=1, ensemble_size=100):
+    # storage = Hdf5Storage()
+    for name, mat in gen_params(parameters=10, ensemble_size=100):
         storage.save_parameter(name, mat)
 
 
 if __name__ == "__main__":
-    print(f"{Timer(main).timeit(number=10)=}")
+    print(f"{Timer(main).timeit(number=1)=}")
