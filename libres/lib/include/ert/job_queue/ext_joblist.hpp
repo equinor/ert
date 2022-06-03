69e0d45ca6e5caa776a479643b215d0e4e44aa6d
Eivind Jahren <ejah@equinor.com>
2022-06-03 12:03:13 +0200

    Remove commented out functions

--- libres/lib/include/ert/job_queue/ext_joblist.hpp
+++ libres/lib/include/ert/job_queue/ext_joblist.hpp
@@ -35,7 +35,6 @@ extern "C" void ext_joblist_add_job(ext_joblist_type *joblist, const char *name,
 extern "C" ext_job_type *ext_joblist_get_job(const ext_joblist_type *,
                                              const char *);
 ext_job_type *ext_joblist_get_job_copy(const ext_joblist_type *, const char *);
-//void               ext_joblist_python_fprintf(const ext_joblist_type * , const stringlist_type * , const char * , const subst_list_type *);
 extern "C" bool ext_joblist_has_job(const ext_joblist_type *, const char *);
 extern "C" stringlist_type *
 ext_joblist_alloc_list(const ext_joblist_type *joblist);
