bb9fb4d9d3a2c67a6554e6c8eaeeb302b26a9cfd
Frode Aarstad <frodeaarstad@gmail.com>
2022-06-08 09:27:53 +0200

    Add experimental feature flag to webviz ert

--- ert_shared/services/webviz_ert_service.py
+++ ert_shared/services/webviz_ert_service.py
@@ -4,6 +4,8 @@ from ert_shared.services._base_service import BaseService, local_exec_args
 class WebvizErt(BaseService):
     service_name = "webviz-ert"
 
-    def __init__(self):
+    def __init__(self, experimental_mode: bool = False):
         exec_args = local_exec_args("webviz_ert")
+        if experimental_mode:
+            exec_args.append("--experimental-mode")
         super().__init__(exec_args)
