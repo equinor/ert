b6552cf99477734d1ac79d12999f3585c7cccff4
Zohar Malamant <ZOM@equinor.com>
2022-06-14 16:22:48 +0200

    Modernize rsh_add_host_from_string
    Use c++17 standard library functions for interpretation
    of the host string in the rsh_driver.

---
+++ libres/tests/job_queue/test_rsh_driver.cpp
@@ -0,0 +1,27 @@
+#include <string>
+
+#include "catch2/catch.hpp"
+
+#include "../tmpdir.hpp"
+
+std::pair<std::string, int> rsh_split_host(std::string hostname);
+
+TEST_CASE("parse hostnames", "[rsh]") {
+    SECTION("Only hostname") {
+        auto [host, max_running] = rsh_split_host("foo");
+        REQUIRE(host == "foo");
+        REQUIRE(max_running == 1);
+    }
+
+    SECTION("Hostname with max_running") {
+        auto [host, max_running] = rsh_split_host("bar:42");
+        REQUIRE(host == "bar");
+        REQUIRE(max_running == 42);
+    }
+
+    SECTION("Hostname with multiple colons") {
+        auto [host, max_running] = rsh_split_host("foo:bar:quz:100");
+        REQUIRE(host == "foo:bar:quz");
+        REQUIRE(max_running == 100);
+    }
+}
