72e8da3430497708eacf1951b249a93694e00a41
Lars Petter Hauge <larsenhauge@gmail.com>
2022-06-14 13:44:12 +0200

    Improve wippity wip

--- ert/experiment_server/_state_machine.py
+++ ert/experiment_server/_state_machine.py
@@ -1,7 +1,10 @@
+import asyncio
 from collections import defaultdict, Mapping
 from email.policy import default
 from typing import Dict, List
 import logging
+from collections import deque
+import collections
 
 logger = logging.getLogger(__name__)
 
@@ -18,19 +21,25 @@ def nested_dict_keys(tree_structure: Dict) -> set:
     return result
 
 
+def merge_dict(left: Dict, right: Dict) -> None:
+    for key, value in right.items():
+        if isinstance(value, collections.abc.Mapping):
+            merge_dict(left[key], right[key])
+        else:
+            left[key] = value
+
+
 class StateMachine:
-    def __init__(self, ensemble_structure: Dict) -> None:
+    def __init__(self, experiment_structure: Dict) -> None:
         self._ensemble_to_successful_realizations: Dict[int, List[int]] = defaultdict(
             list
         )
-        self._realizations_at_ensemble: Dict[int, List[int]] = defaultdict(list)
-        self._ensemble_at_iteration: Dict[int, int] = {}
         self._entity_states: Dict[int, Dict] = defaultdict(
-            dict, {k: {} for k in nested_dict_keys(ensemble_structure)}
+            dict, {k: {} for k in nested_dict_keys(experiment_structure)}
         )
         self._updated_entity_states: Dict[int, Dict] = defaultdict(dict)
-
-        self._ensemble_structure = ensemble_structure
+        self._event_queue: deque = deque()
+        self._experiment_structure = experiment_structure
 
     def successful_realizations(self, iter_: int) -> int:
         """Return an integer indicating the number of successful realizations in an
@@ -47,12 +56,25 @@ class StateMachine:
         # Will add the contents of the event to the updated_entity_states.
         # in the scenario that the contents of a new event overlaps with a previous
         # the old content will be overwritten, hence this will also merge redundant information
-        pass
+        if event["source"] not in self._entity_states.keys():
+            # TODO:handle unspecified source
+            pass
+        self._event_queue.append(event)
 
     def get_update(self) -> Dict:
         # returns the updated_entity_states, merge with entity_states and then empties the updated_entity_state
-        pass
+        return self._updated_entity_states
 
     def get_full_state(self) -> Dict:
         # Returns the state as it is described in `entity_states` (not including the content in `updated_entity_state`)
-        pass
+        return self._entity_states
+
+    def apply_updates(self) -> None:
+        self._updated_entity_states = defaultdict(dict)
+        while self._event_queue:
+            event = self._event_queue.pop()
+            merge_dict(
+                self._updated_entity_states[event["source"]],
+                {**event.data, "type": event["type"]},
+            )
+        merge_dict(self._entity_states, self._updated_entity_states)
--- tests/ert_tests/experiment_server/test_state_machine.py
+++ tests/ert_tests/experiment_server/test_state_machine.py
@@ -1,7 +1,13 @@
+from cloudevents.http import CloudEvent
+from collections import defaultdict
+import copy
+
 from ert.experiment_server import StateMachine
+from ert.experiment_server._state_machine import nested_dict_keys
+from ert.ensemble_evaluator import identifiers as ids
 
 
-def generate_structure():
+def _generate_structure():
     return {
         "ee_1": {
             "ee_1/real_1": {"ee_1/real_1/step_1": ["ee_1/real_1/step_1/job_1"]},
@@ -10,151 +16,245 @@ def generate_structure():
     }
 
 
+def _empty_state_from_structure(structure):
+    return defaultdict(dict, {k: {} for k in nested_dict_keys(structure)})
+
+
+def _update_from_cloudevent(event):
+    return {event["source"]: {"type": event["type"], **event.data}}
+
+
 def test_no_update():
-    ensemble_structure = generate_structure()
+    ensemble_structure = _generate_structure()
     sm = StateMachine(ensemble_structure)
-    assert sm.get_full_state == ensemble_structure
+    assert sm.get_full_state() == _empty_state_from_structure(ensemble_structure)
 
 
 def test_single_update():
-    ensemble_structure = generate_structure()
+    ensemble_structure = _generate_structure()
     sm = StateMachine(ensemble_structure)
-    content = {"meta": "irrelevant_data"}
-    sm.add_event({"ee_1/real_1/step_1/job_1": content})
-    expected_update = {"ee_1/real_1/step_1/job_1": content}
+    event = CloudEvent(
+        {
+            "type": ids.EVTYPE_FM_JOB_RUNNING,
+            "source": "ee_1/real_1/step_1/job_1",
+        },
+        data={"meta": "irrelevant_data"},
+    )
+    sm.add_event(event)
+    sm.apply_updates()
+    assert sm.get_update() == _update_from_cloudevent(event)
 
-    update_state = sm.get_update()
-    assert update_state == expected_update
+    state = _empty_state_from_structure(ensemble_structure)
+    state["ee_1/real_1/step_1/job_1"] = {
+        "type": ids.EVTYPE_FM_JOB_RUNNING,
+        "meta": "irrelevant_data",
+    }
+    assert sm.get_full_state() == state
 
 
 def test_multiple_updates():
-    ensemble_structure = generate_structure()
+    ensemble_structure = _generate_structure()
     sm = StateMachine(ensemble_structure)
 
     content = {"meta": "irrelevant_data"}
-    sm.add_event({"ee_1/real_1/step_1/job_1": content})
-    sm.add_event({"ee_1/real_2/step_1/job_1": content})
-    expected_update = {
-        "ee_1/real_1/step_1/job_1": content,
-        "ee_1/real_2/step_1/job_1": content,
+    first_event = CloudEvent(
+        {
+            "type": ids.EVTYPE_FM_JOB_RUNNING,
+            "source": "ee_1/real_1/step_1/job_1",
+        },
+        data=content,
+    )
+    second_event = CloudEvent(
+        {
+            "type": ids.EVTYPE_FM_JOB_RUNNING,
+            "source": "ee_1/real_2/step_1/job_1",
+        },
+        data=content,
+    )
+    sm.add_event(first_event)
+    sm.add_event(second_event)
+
+    expected_update = {}
+    expected_update.update(_update_from_cloudevent(first_event))
+    expected_update.update(_update_from_cloudevent(second_event))
+
+    sm.apply_updates()
+    assert sm.get_update() == expected_update
+
+    expected_state = _empty_state_from_structure(ensemble_structure)
+
+    expected_state["ee_1/real_1/step_1/job_1"] = {
+        "type": ids.EVTYPE_FM_JOB_RUNNING,
+        **content,
+    }
+    expected_state["ee_1/real_2/step_1/job_1"] = {
+        "type": ids.EVTYPE_FM_JOB_RUNNING,
+        **content,
     }
 
-    update_state = sm.get_update()
-    assert update_state == expected_update
+    assert sm.get_full_state() == expected_state
 
 
 def test_redundant_updates():
-    ensemble_structure = generate_structure()
+    ensemble_structure = _generate_structure()
     sm = StateMachine(ensemble_structure)
 
-    content = {"meta": "new_irrelevant_data"}
-    sm.add_event({"ee_1/real_1/step_1/job_1": content})
-    sm.add_event({"ee_1/real_1/step_1/job_1": content})
-    expected_update = {"ee_1/real_1/step_1/job_1": content}
-    update_state = sm.get_update()
-    assert update_state == expected_update
+    event = CloudEvent(
+        {
+            "type": ids.EVTYPE_FM_JOB_RUNNING,
+            "source": "ee_1/real_1/step_1/job_1",
+        },
+        data={"meta": "irrelevant_data"},
+    )
+    sm.add_event(event)
+
+    event.data = {"meta": "new_irrelevant_data"}
+    sm.add_event(event)
+
+    sm.apply_updates()
+
+    assert sm.get_update() == _update_from_cloudevent(event)
 
 
 def test_partly_reduntant():
     # Test that multiple events with duplicate content will be aggregated as expected
-    ensemble_structure = generate_structure()
+    ensemble_structure = _generate_structure()
     sm = StateMachine(ensemble_structure)
 
-    expected_content = {}
+    event = CloudEvent(
+        {
+            "type": ids.EVTYPE_FM_JOB_RUNNING,
+            "source": "ee_1/real_1/step_1/job_1",
+        },
+    )
+    expected_content = {"type": ids.EVTYPE_FM_JOB_RUNNING}
+
     # The identical content will be added to both events
     identical_content = {"meta": "irrelevant_data"}
+    expected_content.update(identical_content)
 
     content = {"first_unique": 1}
     content.update(identical_content)
-    sm.add_event({"ee_1/real_1/step_1/job_1": content})
     expected_content.update(content)
+    event.data = content
 
-    content = {"second_unique": 1}
+    sm.add_event(event)
+
+    event = copy.copy(event)
+    content = {"second_unique": 2}
     content.update(identical_content)
-    sm.add_event({"ee_1/real_1/step_1/job_1": content})
     expected_content.update(content)
+    event.data = content
 
-    expected_update = {"ee_1/real_1/step_1/job_1": expected_content}
+    sm.add_event(event)
 
+    sm.apply_updates()
     update_state = sm.get_update()
-    assert update_state == expected_update
+    assert update_state["ee_1/real_1/step_1/job_1"] == expected_content
 
 
 def test_retrieve_only_new_information():
-    ensemble_structure = generate_structure()
+    ensemble_structure = _generate_structure()
     sm = StateMachine(ensemble_structure)
 
-    first_content = {"first": "irrelevant_data"}
-    sm.add_event({"ee_1/real_1/step_1/job_1": first_content})
+    event = CloudEvent(
+        {
+            "type": ids.EVTYPE_FM_JOB_RUNNING,
+            "source": "ee_1/real_1/step_1/job_1",
+        },
+    )
+    event.data = {"some": "irrelevant_data"}
 
-    expected_update = {"ee_1/real_1/step_1/job_1": first_content}
+    sm.add_event(event)
+    sm.apply_updates()
 
     # First update shall include the added event
-    update_state = sm.get_update()
-    assert update_state == expected_update
+    assert sm.get_update() == _update_from_cloudevent(event)
 
-    second_content = {"second": "irrelevant_data"}
-    sm.add_event({"ee_1/real_1/step_1/job_1": second_content})
-
-    expected_update = {"ee_1/real_1/step_1/job_1": second_content}
+    event = copy.copy(event)
+    event.data = {"another": "irrelevant_data"}
+    sm.add_event(event)
+    sm.apply_updates()
 
     # Second update should only include events since last update
-    update_state = sm.get_update()
-    assert update_state == expected_update
+    assert sm.get_update() == _update_from_cloudevent(event)
 
     # A full state should include both contents
-    content = first_content
-    content.update(second_content)
+    expected_state = _empty_state_from_structure(ensemble_structure)
+    expected_state["ee_1/real_1/step_1/job_1"] = {
+        "type": ids.EVTYPE_FM_JOB_RUNNING,
+        "some": "irrelevant_data",
+        "another": "irrelevant_data",
+    }
 
-    expected_state = ensemble_structure
-    expected_state["ee_1"]["ee_1/real_1"]["ee_1/real_1/step_1"][
-        "ee_1/real_1/step_1/job_1"
-    ] = content
-
-    assert sm.get_full_state == expected_state
+    assert sm.get_full_state() == expected_state
 
 
 def test_get_full_state_change_on_update():
     """The result of full state should only include events up to the last update.
-    If events are added after that, the full state should not include those."""
-    ensemble_structure = generate_structure()
+    If events are added after that, the full state should not include those until apply_updates is called"""
+    ensemble_structure = _generate_structure()
     sm = StateMachine(ensemble_structure)
 
-    first_content = {"first": "irrelevant_data"}
-    sm.add_event({"ee_1/real_1/step_1/job_1": first_content})
+    event = CloudEvent(
+        {
+            "type": ids.EVTYPE_FM_JOB_RUNNING,
+            "source": "ee_1/real_1/step_1/job_1",
+        },
+    )
+    event.data = {"some": "irrelevant_data"}
 
-    # Without a call to get_update we should receive empty state
-    assert sm.get_full_state == ensemble_structure
+    sm.add_event(event)
 
-    expected_update = {"ee_1/real_1/step_1/job_1": first_content}
+    # Without a call to apply_updates we should receive empty state
+    expected_state = _empty_state_from_structure(ensemble_structure)
+    assert sm.get_full_state() == expected_state
+
+    sm.apply_updates()
 
     # First update shall include the added event
-    update_state = sm.get_update()
-    assert update_state == expected_update
+    assert sm.get_update() == _update_from_cloudevent(event)
 
     # And will now also be reflected in full_state
-    expected_state["ee_1"]["ee_1/real_1"]["ee_1/real_1/step_1"][
-        "ee_1/real_1/step_1/job_1"
-    ] = first_content
+    expected_state["ee_1/real_1/step_1/job_1"] = {
+        "type": ids.EVTYPE_FM_JOB_RUNNING,
+        "some": "irrelevant_data",
+    }
+    assert sm.get_full_state() == expected_state
 
-    assert sm.get_full_state == expected_state
-
-    second_content = {"second": "irrelevant_data"}
-    sm.add_event({"ee_1/real_1/step_1/job_1": second_content})
+    event = copy.copy(event)
+    event.data = {"another": "irrelevant_data"}
+    sm.add_event(event)
 
     # Full state not affected yet
-    assert sm.get_full_state == expected_state
+    assert sm.get_full_state() == expected_state
 
-    expected_update = {"ee_1/real_1/step_1/job_1": second_content}
+    sm.apply_updates()
 
     # Second update should only include events since last update
-    update_state = sm.get_update()
-    assert update_state == expected_update
+    assert sm.get_update() == _update_from_cloudevent(event)
 
-    expected_state = ensemble_structure
-    expected_state["ee_1"]["ee_1/real_1"]["ee_1/real_1/step_1"][
-        "ee_1/real_1/step_1/job_1"
-    ].update(second_content)
+    # A full state should now reflect both contents
+    expected_state["ee_1/real_1/step_1/job_1"]["another"] = "irrelevant_data"
 
-    # Full state should now reflect both contents
-    assert sm.get_full_state == expected_state
+    assert sm.get_full_state() == expected_state
+
+
+def test_correct_presedence_misaligned_events():
+    # Consider that one actor says things are good, another says it's not
+    pass
+
+
+def test_disallowed_state_change():
+    # Going from failed to successfull, should it be allowed?
+    pass
+
+
+def test_large_case():
+    # 500 realisations, 50 jobs
+    pass
+
+
+def test_propagate_state_change():
+    # all jobs succeed -> step succeed
+    pass
