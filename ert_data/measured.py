dd9c0655b70cc313a9047b39897444c9e258acc7
Feda Curic <feda.curic@gmail.com>
2022-06-17 08:54:45 +0200

    Type and simplify measured.py

--- ert_data/measured.py
+++ ert_data/measured.py
@@ -1,20 +1,33 @@
 """
-Read-only API to fetch observation-data from internal ERT-storage.
+Read-only API to fetch responses (a.k.a measurements) and
+matching observations from internal ERT-storage.
 The main goal is to facilitate data-analysis using scipy and similar tools,
 instead of having to implement analysis-functionality into ERT using C/C++.
 The API is typically meant used as part of workflows.
 """
 
 from collections import defaultdict
+from typing import List, Optional, TYPE_CHECKING
 
 import pandas as pd
 import numpy as np
 
 from ert_data import loader
+from ert_shared.libres_facade import LibresFacade
+
+if TYPE_CHECKING:
+    import numpy.typing as npt
 
 
 class MeasuredData:
-    def __init__(self, facade, keys, index_lists=None, load_data=True, case_name=None):
+    def __init__(
+        self,
+        facade: LibresFacade,
+        keys: List[str],
+        index_lists: Optional[List[List[int]]] = None,
+        load_data: bool = True,
+        case_name: Optional[str] = None,
+    ):
         self._facade = facade
 
         if not keys:
@@ -28,45 +41,37 @@ class MeasuredData:
         self._set_data(self.filter_on_column_index(keys, index_lists))
 
     @property
-    def data(self):
+    def data(self) -> pd.DataFrame:
         return self._data
 
-    def _set_data(self, data):
-        expected_keys = ["OBS", "STD"]
+    def _set_data(self, data: pd.DataFrame) -> None:
+        expected_keys = {"OBS", "STD"}
         if not isinstance(data, pd.DataFrame):
             raise TypeError(
                 f"Invalid type: {type(data)}, should be type: {pd.DataFrame}"
             )
-        elif not set(expected_keys).issubset(data.index):
-            needed = ["OBS", "STD"]
-            missing = set(expected_keys) - set(data.index)
+        elif not expected_keys.issubset(data.index):
+            missing = expected_keys - set(data.index)
             raise ValueError(
-                f"{needed} should be present in DataFrame index, missing: {missing}"
+                f"{expected_keys} should be present in DataFrame index, \
+                missing: {missing}"
             )
         else:
             self._data = data
 
-    def remove_failed_realizations(self):
-        self._set_data(self._remove_failed_realizations())
-
-    def get_simulated_data(self):
-        return self._get_simulated_data()
-
-    def _get_simulated_data(self):
-        return self.data[~self.data.index.isin(["OBS", "STD"])]
-
-    def _remove_failed_realizations(self):
+    def remove_failed_realizations(self) -> None:
         """Removes rows with no simulated data, leaving observations and
         standard deviations as-is."""
         pre_index = self.data.index
         post_index = list(self.data.dropna(axis=0, how="all").index)
         drop_index = set(pre_index) - set(post_index + ["STD", "OBS"])
-        return self.data.drop(index=drop_index)
+        self._set_data(self.data.drop(index=drop_index))
 
-    def remove_inactive_observations(self):
-        self._set_data(self._remove_inactive_observations())
+    def get_simulated_data(self) -> pd.DataFrame:
+        """Dimension of data is (number of responses x number of realizations)."""
+        return self.data[~self.data.index.isin(["OBS", "STD"])]
 
-    def _remove_inactive_observations(self):
+    def remove_inactive_observations(self) -> None:
         """Removes columns with one or more NaN or inf values."""
         filtered_dataset = self.data.replace([np.inf, -np.inf], np.nan).dropna(
             axis="columns", how="any"
@@ -76,12 +81,14 @@ class MeasuredData:
                 "This operation results in an empty dataset "
                 "(could be due to one or more failed realizations)"
             )
-        return filtered_dataset
+        self._set_data(filtered_dataset)
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.data.empty
 
-    def _get_data(self, observation_keys, load_data, case_name):
+    def _get_data(
+        self, observation_keys: List[str], load_data: bool, case_name: str
+    ) -> pd.DataFrame:
         """
         Adds simulated and observed data and returns a dataframe where ensemble
         members will have a data key, observed data will be named OBS and
@@ -119,13 +126,7 @@ class MeasuredData:
 
         return pd.concat(measured_data, axis=1)
 
-    def filter_ensemble_std(self, std_cutoff):
-        self._set_data(self._filter_ensemble_std(std_cutoff))
-
-    def filter_ensemble_mean_obs(self, alpha):
-        self._set_data(self._filter_ensemble_mean_obs(alpha))
-
-    def _filter_ensemble_std(self, std_cutoff):
+    def filter_ensemble_std(self, std_cutoff: float) -> None:
         """
         Filters on ensemble variation versus a user defined standard
         deviation cutoff. If there is not enough variation in the measurements
@@ -133,9 +134,9 @@ class MeasuredData:
         """
         ens_std = self.get_simulated_data().std()
         std_filter = ens_std <= std_cutoff
-        return self.data.drop(columns=std_filter[std_filter].index)
+        self._set_data(self.data.drop(columns=std_filter[std_filter].index))
 
-    def _filter_ensemble_mean_obs(self, alpha):
+    def filter_ensemble_mean_obs(self, alpha: float) -> None:
         """
         Filters on distance between the observed data and the ensemble mean
         based on variation and a user defined alpha.
@@ -147,9 +148,11 @@ class MeasuredData:
 
         mean_filter = abs(obs_values - ens_mean) > alpha * (ens_std + obs_std)
 
-        return self.data.drop(columns=mean_filter[mean_filter].index)
+        self._set_data(self.data.drop(columns=mean_filter[mean_filter].index))
 
-    def filter_on_column_index(self, obs_keys, index_lists):
+    def filter_on_column_index(
+        self, obs_keys: List[str], index_lists: Optional[List[List[int]]] = None
+    ) -> pd.DataFrame:
         if index_lists is None or all(index_list is None for index_list in index_lists):
             return self.data
         names = self.data.columns.get_level_values(0)
@@ -158,24 +161,12 @@ class MeasuredData:
         return self.data.iloc[:, cond]
 
     @staticmethod
-    def _filter_on_column_index(dataframe, index_list):
-        """
-        Returns a subset where the columns in index_list are filtered out
-        """
-        if isinstance(index_list, (list, tuple)):
-            if max(index_list) > dataframe.shape[1]:
-                msg = (
-                    "Index list is larger than observation data, "
-                    "please check input, max index list:"
-                    f"{max(index_list)} number of data points: {dataframe.shape[1]}"
-                )
-                raise IndexError(msg)
-            return dataframe.iloc[:, list(index_list)]
-        else:
-            return dataframe
-
-    @staticmethod
-    def _create_condition(names, data_index, obs_keys, index_lists):
+    def _create_condition(
+        names: pd.Index,
+        data_index: pd.Index,
+        obs_keys: List[str],
+        index_lists: List[List[int]],
+    ) -> "npt.NDArray[np.bool_]":
         conditions = []
         for obs_key, index_list in zip(obs_keys, index_lists):
             if index_list is not None:
