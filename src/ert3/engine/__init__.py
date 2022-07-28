631d809db8e48c78965709639be8b86c5fb1721f
Eivind Jahren <ejah@equinor.com>
2022-07-28 09:21:58 +0200

    Use relative import in __init__

--- src/ert3/engine/__init__.py
+++ src/ert3/engine/__init__.py
@@ -1,10 +1,7 @@
-from ert3.engine._run import run
-from ert3.engine._run import run_sensitivity_analysis
-from ert3.engine._run import get_ensemble_size
-from ert3.engine._export import export
-from ert3.engine._record import load_record
-from ert3.engine._record import sample_record
-from ert3.engine._clean import clean
+from ._run import get_ensemble_size, run, run_sensitivity_analysis
+from ._export import export
+from ._record import load_record, sample_record
+from ._clean import clean
 
 __all__ = [
     "run",
