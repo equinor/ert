7abf7cec72f187d99b704e203e080659e5970a8a
Eivind Jahren <ejah@equinor.com>
2022-07-29 13:46:31 +0200

    Get debug statements in tests

--- tests/ert_tests/conftest.py
+++ tests/ert_tests/conftest.py
@@ -1,15 +1,4 @@
 # This conftest still exists so that tests files can import ert_utils
 import logging
+
 import pytest
-
-
-@pytest.fixture(autouse=True)
-def log_check():
-    logger = logging.getLogger()
-    logger.setLevel(logging.WARNING)
-    yield
-    logger_after = logging.getLogger()
-    level_after = logger_after.getEffectiveLevel()
-    assert (
-        logging.WARNING == level_after
-    ), f"Detected differences in log environment: Changed to {level_after}"
