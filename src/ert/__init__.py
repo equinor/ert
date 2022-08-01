9a10455c620b7a83d18ddf33bded51b10e954b3e
Eivind Jahren <ejah@equinor.com>
2022-08-01 13:33:16 +0200

    Add more exports to semeio

--- src/ert/__init__.py
+++ src/ert/__init__.py
@@ -1,3 +1,25 @@
 from ert_data import MeasuredData
+from ert_shared.libres_facade import LibresFacade
+from ert_shared.plugins.plugin_manager import (
+    ErtPluginContext,
+    ErtPluginManager,
+    hook_implementation,
+)
+from ert_shared.plugins.plugin_response import plugin_response
+from res.enkf import EnkfNode, ErtImplType, ErtRunContext, ESUpdate
+from res.enkf.export import GenKwCollector, MisfitCollector
 
-__all__ = ["MeasuredData"]  # Used by semeio
+__all__ = [
+    "MeasuredData",
+    "EnkfNode",
+    "ErtImplType",
+    "ErtRunContext",
+    "ESUpdate",
+    "LibresFacade",
+    "hook_implementation",
+    "GenKwCollector",
+    "MisfitCollector",
+    "ErtPluginManager",
+    "ErtPluginContext",
+    "plugin_response",
+]  # Used by semeio
