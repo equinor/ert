ff404edd040cfb25e6244fad02cacdc1e1fc3e84
Øyvind Eide <eide.oyvind87@gmail.com>
2022-08-03 23:18:21 +0200

    Remove python implementation of ForwardLoadContext

--- tests/libres_tests/res/enkf/test_forward_load_context.py
+++
@@ -1,9 +0,0 @@
-from res.enkf import ForwardLoadContext
-
-from ...libres_utils import ResTest
-
-
-class ForwardLoadContextTest(ResTest):
-    def test_create(self):
-        ctx = ForwardLoadContext(report_step=1)
-        self.assertEqual(1, ctx.getLoadStep())
