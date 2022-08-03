5ab03399be72f3829c1628b55c4aff81c138c166
Øyvind Eide <oyveid@equinor.com>
2022-08-03 10:12:04 +0200

    Make function static

--- src/libres/lib/enkf/enkf_main_manage_fs.cpp
+++ src/libres/lib/enkf/enkf_main_manage_fs.cpp
@@ -112,7 +112,7 @@ void enkf_main_init_current_case_from_existing(enkf_main_type *enkf_main,
                                       source_report_step, current_fs);
 }
 
-void enkf_main_init_current_case_from_existing_custom(
+static void enkf_main_init_current_case_from_existing_custom(
     enkf_main_type *enkf_main, enkf_fs_type *source_case_fs,
     int source_report_step, std::vector<std::string> &node_list,
     std::vector<bool> &iactive) {
@@ -494,4 +494,19 @@ enkf_main_alloc_readonly_state_map(const enkf_main_type *enkf_main,
 void enkf_main_close_fs(enkf_main_type *enkf_main) {
     if (enkf_main->dbase != NULL)
         enkf_fs_decref(enkf_main->dbase);
+}
+
+RES_LIB_SUBMODULE("enkf_fs_manager", m) {
+    m.def(
+        "init_current_case_from_existing_custom",
+        [](py::object self, py::object source_case_py, int source_report_step,
+           std::vector<std::string> &node_list, std::vector<bool> &iactive) {
+            auto source_case_fs = ert::from_cwrap<enkf_fs_type>(source_case_py);
+            auto enkf_main = ert::from_cwrap<enkf_main_type>(self);
+            return enkf_main_init_current_case_from_existing_custom(
+                enkf_main, source_case_fs, source_report_step, node_list,
+                iactive);
+        },
+        py::arg("self"), py::arg("source_case"), py::arg("source_report_step"),
+        py::arg("node_list"), py::arg("iactive"));
 }
