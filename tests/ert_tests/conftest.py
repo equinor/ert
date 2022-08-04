8a544c3d3f781439901eb3a0ca0fcce1d96796e2
Lars Petter Hauge <larsenhauge@gmail.com>
2022-08-04 12:11:55 +0200

    Ignore server configuration tokens in tests

--- tests/ert_tests/conftest.py
+++ tests/ert_tests/conftest.py
@@ -24,6 +24,8 @@ def no_cert_in_test(monkeypatch):
     # Specifically generating the RSA key <_openssl.RSA_generate_key_ex>
     class MockESConfig(EvaluatorServerConfig):
         def __init__(self, *args, **kwargs):
+            if "use_token" not in kwargs:
+                kwargs["use_token"] = False
             if "generate_cert" not in kwargs:
                 kwargs["generate_cert"] = False
             super().__init__(*args, **kwargs)
