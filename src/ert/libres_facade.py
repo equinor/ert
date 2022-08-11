01a7bafac719951a9ab6a42df8930d870e25a1c7
Eivind Jahren <ejah@equinor.com>
2022-08-11 13:00:37 +0200

    Add functionality for semeio in LibresFacade
    move more functionality as "tell don't ask" to LibresFacade,
    so that the API surface is lower for semeio.

--- src/ert/libres_facade.py
+++ src/ert/libres_facade.py
@@ -1,14 +1,17 @@
 import logging
-from typing import TYPE_CHECKING, Callable, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Union
 
+import numpy as np
 from pandas import DataFrame
 
 from ert.analysis import ESUpdate, SmootherSnapshot
-from res.enkf import EnKFMain
+from ert.data import MeasuredData
+from res.enkf import EnKFMain, EnkfNode, ErtImplType, ResConfig, RunContext
 from res.enkf.export import (
     GenDataCollector,
     GenDataObservationCollector,
     GenKwCollector,
+    MisfitCollector,
     SummaryCollector,
     SummaryObservationCollector,
 )
@@ -17,10 +20,12 @@ from res.enkf.plot_data import PlotBlockDataLoader
 _logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
+    from ecl.grid import EclGrid
 
     from ert.analysis import ModuleData
     from res.analysis.analysis_module import AnalysisModule
-    from res.enkf import AnalysisConfig, ObsVector, QueueConfig, RunContext
+    from res.analysis.configuration import UpdateConfiguration
+    from res.enkf import AnalysisConfig, ObsVector, QueueConfig
     from res.enkf.config.gen_kw_config import PriorDict
     from res.enkf.enkf_fs import EnkfFs
     from res.enkf.enkf_obs import EnkfObs
@@ -34,16 +39,91 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
         self._enkf_main = enkf_main
         self._es_update = ESUpdate(enkf_main)
 
-    def smoother_update(self, prior_context: "RunContext") -> None:
+    def write_runpath_list(
+        self, iterations: List[int], realizations: List[int]
+    ) -> None:
+        self._enkf_main.write_runpath_list(iterations, realizations)
+
+    def smoother_update(self, prior_context: RunContext) -> None:
         self._es_update.smootherUpdate(prior_context)
 
     def iterative_smoother_update(
-        self, run_context: "RunContext", module_data: "ModuleData"
+        self, run_context: RunContext, module_data: "ModuleData"
     ) -> None:
         self._es_update.iterative_smoother_update(run_context, module_data)
 
     def set_global_std_scaling(self, weight: float) -> None:
         self._enkf_main.analysisConfig().setGlobalStdScaling(weight)
+
+    def set_log_path(self, output_path: str) -> None:
+        self._enkf_main.analysisConfig().set_log_path(output_path)
+
+    @property
+    def update_configuration(self) -> "UpdateConfiguration":
+        return self._enkf_main.update_configuration
+
+    @update_configuration.setter
+    def update_configuration(self, value: Any) -> None:
+        self._enkf_main.update_configuration = value
+
+    @property
+    def enspath(self) -> str:
+        return self._enkf_main.resConfig().model_config.getEnspath()
+
+    @property
+    def user_config_file(self) -> str:
+        return self._enkf_main.resConfig().user_config_file
+
+    @property
+    def number_of_iterations(self) -> int:
+        return len(self._enkf_main.analysisConfig().getAnalysisIterConfig())
+
+    def get_run_context(self, prior_name: str, target_name: str) -> RunContext:
+        fs_manager = self._enkf_main.getEnkfFsManager()
+        return RunContext(
+            fs_manager.getFileSystem(prior_name),
+            fs_manager.getFileSystem(target_name),
+        )
+
+    def get_field_parameters(self) -> List[str]:
+        return list(
+            self._enkf_main.ensembleConfig().getKeylistFromImplType(ErtImplType.FIELD)
+        )
+
+    def get_gen_kw(self) -> List[str]:
+        return list(
+            self._enkf_main.ensembleConfig().getKeylistFromImplType(ErtImplType.GEN_KW)
+        )
+
+    @property
+    def grid_file(self) -> str:
+        return self._enkf_main.eclConfig().get_gridfile()
+
+    @property
+    def grid(self) -> "EclGrid":
+        return self._enkf_main.eclConfig().getGrid()
+
+    def export_field_parameter(
+        self, parameter_name: str, case_name: str, filepath: str
+    ) -> None:
+        file_system = self._enkf_main.getEnkfFsManager().getFileSystem(case_name)
+        config_node = self._enkf_main.ensembleConfig()[parameter_name]
+        ext = config_node.get_enkf_outfile().rsplit(".")[-1]
+        EnkfNode.exportMany(
+            config_node,
+            filepath + "." + ext,
+            file_system,
+            np.arange(0, self.get_ensemble_size()),
+        )
+
+    def get_measured_data(  # pylint: disable=too-many-arguments
+        self,
+        keys: List[str],
+        index_lists: Optional[List[List[int]]] = None,
+        load_data: bool = True,
+        case_name: Optional[str] = None,
+    ) -> MeasuredData:
+        return MeasuredData(self, keys, index_lists, load_data, case_name)
 
     def get_analysis_config(self) -> "AnalysisConfig":
         return self._enkf_main.analysisConfig()
@@ -59,9 +139,7 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
 
     def get_active_realizations(self, case_name: str) -> List[int]:
         fs = self._enkf_main.getFileSystem(case_name, read_only=True)
-        realizations = SummaryCollector.createActiveList(fs)
-
-        return realizations
+        return SummaryCollector.createActiveList(fs)
 
     def case_initialized(self, case: str) -> bool:
         return self._enkf_main.isCaseInitialized(case)
@@ -95,6 +173,18 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
         observation = self._enkf_main.getObservations()[key]
         return observation.getImplementationType().name  # type: ignore
 
+    def get_impl_type_name_for_ensemble_config_node(self, name: str) -> str:
+        node = self._enkf_main.ensembleConfig().getNode(name)
+        return node.getImplementationType().name  # type: ignore
+
+    def get_data_size_for_ensemble_config_node(self, name: str) -> int:
+        return (
+            self._enkf_main.ensembleConfig()
+            .getNode(name)
+            .getFieldModelConfig()
+            .get_data_size()
+        )
+
     def get_current_fs(self) -> "EnkfFs":
         return self._enkf_main.getCurrentFileSystem()
 
@@ -110,19 +200,6 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
     def load_gen_data(self, case_name: str, key: str, report_step: int) -> DataFrame:
         return GenDataCollector.loadGenData(
             self._enkf_main, case_name, key, report_step
-        )
-
-    def load_all_summary_data(
-        self,
-        case_name: str,
-        keys: Optional[List[str]] = None,
-        realization_index: Optional[int] = None,
-    ) -> DataFrame:
-        return SummaryCollector.loadAllSummaryData(
-            self._enkf_main,
-            case_name=case_name,
-            keys=keys,
-            realization_index=realization_index,
         )
 
     def load_observation_data(
@@ -157,6 +234,12 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
     def all_data_type_keys(self) -> List[str]:
         return self._enkf_main.getKeyManager().allDataTypeKeys()
 
+    def get_all_gen_data_observation_keys(self) -> List[str]:
+        return GenDataObservationCollector.getAllObservationKeys(self._enkf_main)
+
+    def get_all_summary_observation_keys(self) -> List[str]:
+        return SummaryObservationCollector.getAllObservationKeys(self._enkf_main)
+
     def observation_keys(self, key: str) -> List[str]:
         if self._enkf_main.getKeyManager().isGenDataKey(key):
             key_parts = key.split("@")
@@ -183,14 +266,26 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
         else:
             return []
 
+    def load_all_gen_kw_data(
+        self,
+        case_name: str,
+        keys: Optional[List[str]] = None,
+        realization_index: Optional[int] = None,
+    ) -> DataFrame:
+        return GenKwCollector.loadAllGenKwData(
+            self._enkf_main,
+            case_name=case_name,
+            keys=keys,
+            realization_index=realization_index,
+        )
+
     def gather_gen_kw_data(
         self,
         case: str,
         key: str,
         realization_index: Optional[int] = None,
     ) -> DataFrame:
-        data = GenKwCollector.loadAllGenKwData(
-            self._enkf_main,
+        data = self.load_all_gen_kw_data(
             case,
             [key],
             realization_index=realization_index,
@@ -200,15 +295,26 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
         else:
             return DataFrame()
 
+    def load_all_summary_data(
+        self,
+        case_name: str,
+        keys: Optional[List[str]] = None,
+        realization_index: Optional[int] = None,
+    ) -> DataFrame:
+        return SummaryCollector.loadAllSummaryData(
+            self._enkf_main,
+            case_name=case_name,
+            keys=keys,
+            realization_index=realization_index,
+        )
+
     def gather_summary_data(
         self,
         case: str,
         key: str,
         realization_index: Optional[int] = None,
     ) -> DataFrame:
-        data = SummaryCollector.loadAllSummaryData(
-            self._enkf_main, case, [key], realization_index
-        )
+        data = self.load_all_summary_data(case, [key], realization_index)
         if not data.empty:
             idx = data.index.duplicated()
             if idx.any():
@@ -220,6 +326,9 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
                 )
             data = data.unstack(level="Realization").droplevel(0, axis=1)
         return data
+
+    def load_all_misfit_data(self, case_name: str) -> DataFrame:
+        return MisfitCollector.loadAllMisfitData(self._enkf_main, case_name=case_name)
 
     def refcase_data(self, key: str) -> DataFrame:
         refcase = self._enkf_main.eclConfig().getRefcase()
@@ -249,10 +358,7 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
         return data
 
     def gather_gen_data_data(
-        self,
-        case: str,
-        key: str,
-        realization_index: Optional[int] = None,
+        self, case: str, key: str, realization_index: Optional[int] = None
     ) -> DataFrame:
         key_parts = key.split("@")
         key = key_parts[0]
@@ -309,3 +415,12 @@ class LibresFacade:  # pylint: disable=too-many-public-methods
         if self._enkf_main.getWorkflowList().hasJob(name):
             return self._enkf_main.getWorkflowList().getJob(name)
         return None
+
+    def run_ertscript(self, ertscript, *args, **kwargs):  # type: ignore
+        return ertscript(self._enkf_main).run(*args, **kwargs)
+
+    @classmethod
+    def from_config_file(
+        cls, config_file: str, strict: bool = True, read_only: bool = False
+    ) -> "LibresFacade":
+        return cls(EnKFMain(ResConfig(config_file), strict, read_only))
