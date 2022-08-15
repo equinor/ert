72720cb9e1d462bfd9a961f88f722e8e5666b0a0
Eivind Jahren <ejah@equinor.com>
2022-08-15 08:26:02 +0200

    Avoid circular install of webviz_ert

--- src/ert_shared/services/webviz_ert_service.py
+++ src/ert_shared/services/webviz_ert_service.py
@@ -1,11 +1,13 @@
-from ert_shared.services._base_service import BaseService, local_exec_args
+import sys
+
+from ert_shared.services._base_service import BaseService
 
 
 class WebvizErt(BaseService):
     service_name = "webviz-ert"
 
     def __init__(self, experimental_mode: bool = False, verbose: bool = False):
-        exec_args = local_exec_args("webviz_ert")
+        exec_args = [sys.executable, "-m", "webviz_ert"]
         if experimental_mode:
             exec_args.append("--experimental-mode")
         if verbose:
