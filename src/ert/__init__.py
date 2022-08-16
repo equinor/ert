b5f93858d3b8574a285a0b9570389ea8b9ef8d91
Eivind Jahren <ejah@equinor.com>
2022-08-16 08:05:20 +0200

    Export plugin hooks as public api

--- src/ert/__init__.py
+++ src/ert/__init__.py
@@ -1,5 +1,7 @@
 from .data import MeasuredData
 from .libres_facade import LibresFacade
 from .simulator import BatchSimulator
+from .shared.plugins.plugin_manager import hook_implementation
+from .shared.plugins.plugin_response import plugin_response
 
-__all__ = ["MeasuredData", "LibresFacade", "BatchSimulator"]  # Used by semeio
+__all__ = ["hook_implementation", "plugin_response", "MeasuredData", "LibresFacade", "BatchSimulator"]
