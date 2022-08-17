80d2d0996239691adf3c958af300dd4bed8b7121
Øyvind Eide <eide.oyvind87@gmail.com>
2022-08-17 13:54:17 +0200

    Refactor FileSystemRotator

--- src/ert/_c_wrappers/enkf/enkf_fs_manager.py
+++ src/ert/_c_wrappers/enkf/enkf_fs_manager.py
@@ -1,50 +1,35 @@
-from typing import List, Dict, Union
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict, Union
 
 from ert._c_wrappers.enkf.enkf_fs import EnkfFs
 
 
+@dataclass
 class FileSystemRotator:
-    def __init__(self, capacity: int):
-        super().__init__()
-        self._capacity: int = capacity
-        self._fs_list: List[str] = []
-        self._fs_map: Dict[str, EnkfFs] = {}
+    capacity: int
+    fs_map: Dict[Path, EnkfFs] = field(default_factory=dict)
 
     def __len__(self) -> int:
-        return len(self._fs_list)
+        return len(self.fs_map)
 
-    def addFileSystem(self, file_system: EnkfFs, full_name: str) -> None:
-        if self.atCapacity():
-            self.dropOldestFileSystem()
+    def append(self, file_system: "EnkfFs") -> None:
+        if len(self) == self.capacity:
+            self.drop_oldest_file_system()
+        self.fs_map[file_system.mount_point] = file_system
 
-        self._fs_list.append(full_name)
-        self._fs_map[full_name] = file_system
+    def drop_oldest_file_system(self) -> None:
+        if len(self.fs_map) > 0:
+            case_name = list(self.fs_map)[0]
+            self.fs_map[case_name].sync()
+            del self.fs_map[case_name]
 
-    def dropOldestFileSystem(self) -> None:
-        if len(self._fs_list) > 0:
-            case_name = self._fs_list[0]
-            del self._fs_list[0]
-            self._fs_map[case_name].sync()
-            del self._fs_map[case_name]
+    def __contains__(self, full_case_name: Union[str, Path]) -> bool:
+        return Path(full_case_name).absolute() in self.fs_map
 
-    def atCapacity(self) -> bool:
-        return len(self._fs_list) == self._capacity
+    def __getitem__(self, case: Union[str, Path]) -> EnkfFs:
+        return self.fs_map[Path(case).absolute()]
 
-    def __contains__(self, full_case_name: str) -> bool:
-        return full_case_name in self._fs_list
-
-    def __get_fs(self, name: str) -> EnkfFs:
-        return self._fs_map[name]
-
-    def __getitem__(self, case: Union[int, str]) -> EnkfFs:
-        if isinstance(case, str):
-            return self.__get_fs(case)
-        elif isinstance(case, int) and 0 <= case < len(self):
-            case_name = self._fs_list[case]
-            return self.__get_fs(case_name)
-        else:
-            raise IndexError(f"Value '{case}' is not a proper index or case name.")
-
-    def umountAll(self) -> None:
-        while len(self._fs_list) > 0:
-            self.dropOldestFileSystem()
+    def umount(self) -> None:
+        while len(self.fs_map) > 0:
+            self.drop_oldest_file_system()
