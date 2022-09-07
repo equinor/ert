bb780e0367bf5f106333afafcc140dd07f3f5449
Frode Aarstad <frodeaarstad@gmail.com>
2022-09-07 10:07:57 +0200

    Try running pytest with -n 4

---
+++ ci/jenkins/testkomodo-ERT-test.sh
@@ -0,0 +1,38 @@
+
+copy_test_files () {
+    cp -r ${CI_SOURCE_ROOT}/tests ${CI_TEST_ROOT}
+
+    #ert
+    ln -s ${CI_SOURCE_ROOT}/test-data ${CI_TEST_ROOT}/test-data
+
+    # Trick ERT to find a fake source root
+    mkdir ${CI_TEST_ROOT}/.git
+
+    # Keep pytest configuration:
+    ln -s ${CI_SOURCE_ROOT}/pyproject.toml ${CI_TEST_ROOT}/pyproject.toml
+}
+
+install_test_dependencies () {
+    pip install -r dev-requirements.txt
+    pip install pytest-xdist
+}
+
+install_package () {
+    pip install . --no-deps
+}
+
+start_tests () {
+    if [[ ${CI_KOMODO_RELEASE} =~ py27$  ]]
+    then
+        export PYTEST_QT_API=pyqt4v2
+    fi
+    export NO_PROXY=localhost,127.0.0.1
+
+    # The existence of a running xvfb process will produce
+    # a lock filgit ree for the default server and kill the run
+    # Allow xvfb to find a new server
+    xvfb-run -s "-screen 0 640x480x24" --auto-servernum python -m \
+    pytest -n 4 -k "not test_gui_load and not test_formatting" \
+    -m "not requires_window_manager" tests/ert_tests
+    popd
+}
