90f3c89b51aa14c5cd58b3e05418dd6e4eb4e0c5
Eivind Jahren <ejah@equinor.com>
2022-09-07 13:47:33 +0200

    Update environment_varlist to use std::map

--- src/clib/lib/job_queue/environment_varlist.cpp
+++ src/clib/lib/job_queue/environment_varlist.cpp
@@ -20,63 +20,49 @@
 
 #include <ert/res_util/res_env.hpp>
 
-#include <ert/util/hash.hpp>
+#include <map>
 
 #define ENV_VAR_KEY_STRING "global_environment"
 #define UPDATE_PATH_KEY_STRING "global_update_path"
 
 struct env_varlist_struct {
-    hash_type *varlist;
-    hash_type *updatelist;
+    std::map<std::string, std::string> varlist;
+    std::map<std::string, std::string> updatelist;
 };
 
-env_varlist_type *env_varlist_alloc() {
-    env_varlist_type *list = (env_varlist_type *)util_malloc(sizeof *list);
-    list->varlist = hash_alloc();
-    list->updatelist = hash_alloc();
-    return list;
-}
+env_varlist_type *env_varlist_alloc() { return new env_varlist_struct; }
 
 void env_varlist_update_path(env_varlist_type *list, const char *path_var,
                              const char *new_path) {
-    hash_insert_string(list->updatelist, path_var,
-                       res_env_update_path_var(path_var, new_path, false));
+    list->updatelist[path_var] =
+        res_env_update_path_var(path_var, new_path, false);
 }
 
 void env_varlist_setenv(env_varlist_type *list, const char *key,
                         const char *value) {
-    const char *interp_value = res_env_interp_setenv(key, value);
-    hash_insert_string(list->varlist, key, interp_value);
+    list->varlist[key] = res_env_interp_setenv(key, value);
 }
 
-static void env_varlist_fprintf_hash(const hash_type *list,
-                                     const char *keystring, FILE *stream) {
-    int size = hash_get_size(list);
-    fprintf(stream, "\"%s\" : {", keystring);
-    stringlist_type *stringlist = hash_alloc_stringlist(list);
-    int i_max = size - 1;
-    for (int i = 0; i < size; i++) {
-        const char *key = stringlist_iget(stringlist, i);
-        fprintf(stream, "\"%s\" : \"%s\"", key, (char *)hash_get(list, key));
-        if (i < i_max)
+static void print_map_as_json(const std::map<std::string, std::string> &map,
+                              FILE *stream) {
+    bool first = true;
+    fprintf(stream, "{");
+    for (const auto &[key, value] : map) {
+        if (!first) {
             fprintf(stream, ", ");
+        }
+        first = false;
+        fprintf(stream, R"("%s" : "%s")", key.c_str(), value.c_str());
     }
     fprintf(stream, "}");
-    stringlist_free(stringlist);
 }
 
 void env_varlist_json_fprintf(const env_varlist_type *list, FILE *stream) {
-    env_varlist_fprintf_hash(list->varlist, ENV_VAR_KEY_STRING, stream);
+    fprintf(stream, "\"%s\" : ", ENV_VAR_KEY_STRING);
+    print_map_as_json(list->varlist, stream);
     fprintf(stream, ",\n");
-    env_varlist_fprintf_hash(list->updatelist, UPDATE_PATH_KEY_STRING, stream);
+    fprintf(stream, "\"%s\" : ", UPDATE_PATH_KEY_STRING);
+    print_map_as_json(list->updatelist, stream);
 }
 
-int env_varlist_get_size(env_varlist_type *list) {
-    return hash_get_size(list->varlist);
-}
-
-void env_varlist_free(env_varlist_type *list) {
-    hash_free(list->varlist);
-    hash_free(list->updatelist);
-    free(list);
-}
+void env_varlist_free(env_varlist_type *list) { delete list; }
