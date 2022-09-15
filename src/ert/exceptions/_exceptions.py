dcf971045946187aca330f681648286449c6c5a4
Eivind Jahren <ejah@equinor.com>
2022-09-15 15:44:30 +0200

    Remove unused but declared exceptions

--- src/ert/exceptions/_exceptions.py
+++ src/ert/exceptions/_exceptions.py
@@ -1,31 +1,12 @@
-from typing import Optional
-
-
 class ErtError(Exception):
     """Base class for exceptions in this module."""
 
     pass
 
 
-class IllegalWorkspaceOperation(ErtError):
+class NonExistentExperiment(ErtError):
     def __init__(self, message: str) -> None:
         self.message = message
-
-
-class IllegalWorkspaceState(ErtError):
-    def __init__(self, message: str) -> None:
-        self.message = message
-
-
-class NonExistentExperiment(IllegalWorkspaceOperation):
-    def __init__(self, message: str) -> None:
-        self.message = message
-
-
-class ConfigValidationError(ErtError):
-    def __init__(self, message: str, source: Optional[str] = None) -> None:
-        self.message = message
-        self.source = source
 
 
 class FileExistsException(ErtError):
