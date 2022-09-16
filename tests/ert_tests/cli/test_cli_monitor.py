29ed6f85b73d196ac1dd6922129a51b7d2b1edf5
Eivind Jahren <ejah@equinor.com>
2022-09-16 09:50:46 +0200

    Convert test_cli_monitor to pytest

--- tests/ert_tests/cli/test_cli_monitor.py
+++ tests/ert_tests/cli/test_cli_monitor.py
@@ -1,4 +1,3 @@
-import unittest
 from datetime import datetime
 from io import StringIO
 
@@ -12,79 +11,78 @@ from ert.ensemble_evaluator.state import (
 from ert.shared.cli.monitor import Monitor
 
 
-class MonitorTest(unittest.TestCase):
-    def test_color_always(self):
-        out = StringIO()  # not a tty, so coloring is automatically disabled
-        monitor = Monitor(out=out, color_always=True)
+def test_color_always():
+    out = StringIO()  # not a tty, so coloring is automatically disabled
+    monitor = Monitor(out=out, color_always=True)
 
-        self.assertEqual(
-            "\x1b[38;2;255;0;0mFoo\x1b[0m", monitor._colorize("Foo", fg=(255, 0, 0))
-        )
+    assert monitor._colorize("Foo", fg=(255, 0, 0)) == "\x1b[38;2;255;0;0mFoo\x1b[0m"
 
-    def test_legends(self):
-        monitor = Monitor(out=StringIO())
-        sd = SnapshotDict(status="")
-        for i in range(0, 100):
-            status = REALIZATION_STATE_FINISHED if i < 10 else REALIZATION_STATE_RUNNING
-            sd.reals[i] = Realization(status=status, active=True)
-        monitor._snapshots[0] = Snapshot(sd.dict())
-        legends = monitor._get_legends()
 
-        self.assertEqual(
-            """    Waiting         0/100
+def test_legends():
+    monitor = Monitor(out=StringIO())
+    sd = SnapshotDict(status="")
+    for i in range(0, 100):
+        status = REALIZATION_STATE_FINISHED if i < 10 else REALIZATION_STATE_RUNNING
+        sd.reals[i] = Realization(status=status, active=True)
+    monitor._snapshots[0] = Snapshot(sd.dict())
+    legends = monitor._get_legends()
+
+    assert (
+        legends
+        == """    Waiting         0/100
     Pending         0/100
     Running        90/100
     Failed          0/100
     Finished       10/100
     Unknown         0/100
-""",
-            legends,
-        )
+"""
+    )
 
-    def test_result_success(self):
-        out = StringIO()
-        monitor = Monitor(out=out)
 
-        monitor._print_result(False, None)
+def test_result_success():
+    out = StringIO()
+    monitor = Monitor(out=out)
 
-        self.assertEqual("Simulations completed.\n", out.getvalue())
+    monitor._print_result(False, None)
 
-    def test_result_failure(self):
-        out = StringIO()
-        monitor = Monitor(out=out)
+    assert out.getvalue() == "Simulations completed.\n"
 
-        monitor._print_result(True, "fail")
 
-        self.assertEqual(
-            "Simulations failed with the following error: fail\n", out.getvalue()
-        )
+def test_result_failure():
+    out = StringIO()
+    monitor = Monitor(out=out)
 
-    def test_print_progress(self):
-        out = StringIO()
-        monitor = Monitor(out=out)
-        sd = SnapshotDict(status="")
-        for i in range(0, 100):
-            status = REALIZATION_STATE_FINISHED if i < 50 else REALIZATION_STATE_WAITING
-            sd.reals[i] = Realization(status=status, active=True)
-        monitor._snapshots[0] = Snapshot(sd.dict())
-        monitor._start_time = datetime.now()
-        general_event = _UpdateEvent(
-            phase_name="Test Phase",
-            current_phase=0,
-            total_phases=2,
-            progress=0.5,
-            indeterminate=False,
-            iteration=0,
-        )
+    monitor._print_result(True, "fail")
 
-        monitor._print_progress(general_event)
+    assert out.getvalue() == "Simulations failed with the following error: fail\n"
 
-        # For some reason, `tqdm` adds an extra line containing a progress-bar,
-        # even though this test only calls it once.
-        # I suspect this has something to do with the way `tqdm` does refresh,
-        # but do not know how to fix it.
-        # Seems not be a an issue when used normally.
-        expected = """    --> Test Phase
+
+def test_print_progress():
+    out = StringIO()
+    monitor = Monitor(out=out)
+    sd = SnapshotDict(status="")
+    for i in range(0, 100):
+        status = REALIZATION_STATE_FINISHED if i < 50 else REALIZATION_STATE_WAITING
+        sd.reals[i] = Realization(status=status, active=True)
+    monitor._snapshots[0] = Snapshot(sd.dict())
+    monitor._start_time = datetime.now()
+    general_event = _UpdateEvent(
+        phase_name="Test Phase",
+        current_phase=0,
+        total_phases=2,
+        progress=0.5,
+        indeterminate=False,
+        iteration=0,
+    )
+
+    monitor._print_progress(general_event)
+
+    # For some reason, `tqdm` adds an extra line containing a progress-bar,
+    # even though this test only calls it once.
+    # I suspect this has something to do with the way `tqdm` does refresh,
+    # but do not know how to fix it.
+    # Seems not be a an issue when used normally.
+    expected = """    --> Test Phase
 
 
     |                                                                                      |   0% it
@@ -99,4 +97,4 @@ class MonitorTest(unittest.TestCase):
 
 """  # noqa
 
-        assert out.getvalue().replace("\r", "\n") == expected
+    assert out.getvalue().replace("\r", "\n") == expected
