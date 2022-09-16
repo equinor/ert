ce13556492cd48fc438eb53b96639244a2f658ed
Eivind Jahren <ejah@equinor.com>
2022-09-16 10:00:42 +0200

    Use pep8 compliant hook_implementations __init__

--- src/ert/shared/hook_implementations/__init__.py
+++ src/ert/shared/hook_implementations/__init__.py
@@ -2,3 +2,12 @@ from .help_resources import help_links
 from .jobs import installable_jobs, installable_workflow_jobs, job_documentation
 from .site_config import site_config_lines
 from .workflows import legacy_ertscript_workflow
+
+__all__ = [
+    "help_links",
+    "installable_jobs",
+    "installable_workflow_jobs",
+    "job_documentation",
+    "site_config_lines",
+    "legacy_ertscript_workflow",
+]
