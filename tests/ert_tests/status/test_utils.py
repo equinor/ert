008be6a42577062dbd6efdfcb640eded44bf190f
Eivind Jahren <ejah@equinor.com>
2022-09-16 09:44:29 +0200

    Convert test_utils to pytest

--- tests/ert_tests/status/test_utils.py
+++ tests/ert_tests/status/test_utils.py
@@ -1,27 +1,18 @@
-import unittest
+import pytest
 
 from ert.shared.status.utils import format_running_time
 
 
-class TrackerUtilsTest(unittest.TestCase):
-    def test_format_running_time(self):
-        tests = [
-            {"seconds": 0, "expected": "Running time: 0 seconds"},
-            {"seconds": 1, "expected": "Running time: 1 seconds"},
-            {"seconds": 100, "expected": "Running time: 1 minutes 40 seconds"},
-            {
-                "seconds": 10000,
-                "expected": "Running time: 2 hours 46 minutes 40 seconds",
-            },  # noqa
-            {
-                "seconds": 100000,
-                "expected": "Running time: 1 days 3 hours 46 minutes 40 seconds",
-            },  # noqa
-            {
-                "seconds": 100000000,
-                "expected": "Running time: 1157 days 9 hours 46 minutes 40 seconds",
-            },  # noqa
-        ]
-
-        for t in tests:
-            self.assertEqual(t["expected"], format_running_time(t["seconds"]))
+@pytest.mark.parametrize(
+    "seconds, expected",
+    [
+        (0, "Running time: 0 seconds"),
+        (1, "Running time: 1 seconds"),
+        (100, "Running time: 1 minutes 40 seconds"),
+        (10000, "Running time: 2 hours 46 minutes 40 seconds"),
+        (100000, "Running time: 1 days 3 hours 46 minutes 40 seconds"),
+        (100000000, "Running time: 1157 days 9 hours 46 minutes 40 seconds"),
+    ],
+)
+def test_format_running_time(seconds, expected):
+    assert format_running_time(seconds) == expected
