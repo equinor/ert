20ed1d1e355949793c2845b6f20dc4403790eae7
Eivind Jahren <ejah@equinor.com>
2022-09-26 12:21:28 +0200

    Refactor init_res_config_parser
    Move multi-statement keyword definitions into own function.

--- src/clib/lib/config/config_keywords.cpp
+++ src/clib/lib/config/config_keywords.cpp
@@ -6,6 +6,81 @@
 #include <ert/enkf/hook_manager.hpp>
 #include <ert/python.hpp>
 
+static void add_num_realizations_keyword(config_parser_type *config_parser) {
+    auto item =
+        config_add_schema_item(config_parser, NUM_REALIZATIONS_KEY, true);
+    config_schema_item_set_argc_minmax(item, 1, 1);
+    config_schema_item_iset_type(item, 0, CONFIG_INT);
+    config_add_alias(config_parser, NUM_REALIZATIONS_KEY, "NUM_REALISATIONS");
+}
+
+static void add_run_template_keyword(config_parser_type *config_parser) {
+    auto item = config_add_schema_item(config_parser, RUN_TEMPLATE_KEY, false);
+    config_schema_item_set_argc_minmax(item, 2, CONFIG_DEFAULT_ARG_MAX);
+    config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
+}
+
+static void add_jobname_keyword(config_parser_type *config_parser) {
+    auto item = config_add_schema_item(config_parser, JOBNAME_KEY, false);
+    config_schema_item_set_argc_minmax(item, 1, 1);
+}
+
+static void add_forward_model_keyword(config_parser_type *config_parser) {
+    auto item = config_add_schema_item(config_parser, FORWARD_MODEL_KEY, false);
+    config_schema_item_set_argc_minmax(item, 1, CONFIG_DEFAULT_ARG_MAX);
+}
+
+static void add_simulation_job_keyword(config_parser_type *config_parser) {
+    auto item =
+        config_add_schema_item(config_parser, SIMULATION_JOB_KEY, false);
+    config_schema_item_set_argc_minmax(item, 1, CONFIG_DEFAULT_ARG_MAX);
+}
+
+static void add_data_kw_keyword(config_parser_type *config_parser) {
+    auto item = config_add_schema_item(config_parser, DATA_KW_KEY, false);
+    config_schema_item_set_argc_minmax(item, 2, 2);
+}
+
+static void add_obs_config_keyword(config_parser_type *config_parser) {
+    auto item = config_add_schema_item(config_parser, OBS_CONFIG_KEY, false);
+    config_schema_item_set_argc_minmax(item, 1, 1);
+    config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
+}
+
+static void add_gen_kw_export_keyword(config_parser_type *config_parser) {
+    auto item =
+        config_add_schema_item(config_parser, GEN_KW_EXPORT_NAME_KEY, false);
+    config_schema_item_set_argc_minmax(item, 1, 1);
+}
+
+static void add_history_source_keyword(config_parser_type *config_parser) {
+    auto item =
+        config_add_schema_item(config_parser, HISTORY_SOURCE_KEY, false);
+    config_schema_item_set_argc_minmax(item, 1, 1);
+
+    stringlist_type *argv = stringlist_alloc_new();
+    stringlist_append_copy(argv, "SCHEDULE");
+    stringlist_append_copy(argv, "REFCASE_SIMULATED");
+    stringlist_append_copy(argv, "REFCASE_HISTORY");
+
+    config_schema_item_set_common_selection_set(item, argv);
+    stringlist_free(argv);
+
+    stringlist_type *refcase_dep = stringlist_alloc_new();
+    stringlist_append_copy(refcase_dep, REFCASE_KEY);
+    config_schema_item_set_required_children_on_value(item, "REFCASE_SIMULATED",
+                                                      refcase_dep);
+    config_schema_item_set_required_children_on_value(item, "REFCASE_HISTORY",
+                                                      refcase_dep);
+    stringlist_free(refcase_dep);
+}
+
+static void add_runpath_file_keyword(config_parser_type *config_parser) {
+    auto item = config_add_schema_item(config_parser, RUNPATH_FILE_KEY, false);
+    config_schema_item_set_argc_minmax(item, 1, 1);
+    config_schema_item_iset_type(item, 0, CONFIG_PATH);
+}
+
 ERT_CLIB_SUBMODULE("config_keywords", m) {
     using namespace py::literals;
     m.def(
@@ -13,89 +88,32 @@ ERT_CLIB_SUBMODULE("config_keywords", m) {
         [](py::object py_config_parser) {
             auto config_parser =
                 ert::from_cwrap<config_parser_type>(py_config_parser);
-            config_schema_item_type *item;
-
             ert_workflow_list_add_config_items(config_parser);
             analysis_config_add_config_items(config_parser);
             ensemble_config_add_config_items(config_parser);
             ecl_config_add_config_items(config_parser);
             config_add_key_value(config_parser, RANDOM_SEED_KEY, false,
                                  CONFIG_STRING);
-
             config_add_key_value(config_parser, MAX_RESAMPLE_KEY, false,
                                  CONFIG_INT);
-
-            item = config_add_schema_item(config_parser, NUM_REALIZATIONS_KEY,
-                                          true);
-            config_schema_item_set_argc_minmax(item, 1, 1);
-            config_schema_item_iset_type(item, 0, CONFIG_INT);
-            config_add_alias(config_parser, NUM_REALIZATIONS_KEY,
-                             "NUM_REALISATIONS");
-
-            /* Optional keywords from the model config file */
-            item =
-                config_add_schema_item(config_parser, RUN_TEMPLATE_KEY, false);
-            config_schema_item_set_argc_minmax(item, 2, CONFIG_DEFAULT_ARG_MAX);
-            config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
-
+            add_num_realizations_keyword(config_parser);
+            add_run_template_keyword(config_parser);
             config_add_key_value(config_parser, RUNPATH_KEY, false,
                                  CONFIG_PATH);
             config_add_key_value(config_parser, DATA_ROOT_KEY, false,
                                  CONFIG_PATH);
             config_add_key_value(config_parser, ENSPATH_KEY, false,
                                  CONFIG_PATH);
-
-            item = config_add_schema_item(config_parser, JOBNAME_KEY, false);
-            config_schema_item_set_argc_minmax(item, 1, 1);
-
-            item =
-                config_add_schema_item(config_parser, FORWARD_MODEL_KEY, false);
-            config_schema_item_set_argc_minmax(item, 1, CONFIG_DEFAULT_ARG_MAX);
-
-            item = config_add_schema_item(config_parser, SIMULATION_JOB_KEY,
-                                          false);
-            config_schema_item_set_argc_minmax(item, 1, CONFIG_DEFAULT_ARG_MAX);
-
-            item = config_add_schema_item(config_parser, DATA_KW_KEY, false);
-            config_schema_item_set_argc_minmax(item, 2, 2);
-
-            item = config_add_schema_item(config_parser, OBS_CONFIG_KEY, false);
-            config_schema_item_set_argc_minmax(item, 1, 1);
-            config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
-
+            add_jobname_keyword(config_parser);
+            add_forward_model_keyword(config_parser);
+            add_simulation_job_keyword(config_parser);
+            add_data_kw_keyword(config_parser);
+            add_obs_config_keyword(config_parser);
             config_add_key_value(config_parser, TIME_MAP_KEY, false,
                                  CONFIG_EXISTING_PATH);
-
-            item = config_add_schema_item(config_parser, GEN_KW_EXPORT_NAME_KEY,
-                                          false);
-            config_schema_item_set_argc_minmax(item, 1, 1);
-
-            stringlist_type *refcase_dep = stringlist_alloc_new();
-            stringlist_append_copy(refcase_dep, REFCASE_KEY);
-            item = config_add_schema_item(config_parser, HISTORY_SOURCE_KEY,
-                                          false);
-            config_schema_item_set_argc_minmax(item, 1, 1);
-            {
-                stringlist_type *argv = stringlist_alloc_new();
-                stringlist_append_copy(argv, "SCHEDULE");
-                stringlist_append_copy(argv, "REFCASE_SIMULATED");
-                stringlist_append_copy(argv, "REFCASE_HISTORY");
-
-                config_schema_item_set_common_selection_set(item, argv);
-                stringlist_free(argv);
-            }
-            config_schema_item_set_required_children_on_value(
-                item, "REFCASE_SIMULATED", refcase_dep);
-            config_schema_item_set_required_children_on_value(
-                item, "REFCASE_HISTORY", refcase_dep);
-
-            stringlist_free(refcase_dep);
-
-            item =
-                config_add_schema_item(config_parser, RUNPATH_FILE_KEY, false);
-            config_schema_item_set_argc_minmax(item, 1, 1);
-            config_schema_item_iset_type(item, 0, CONFIG_PATH);
-
+            add_gen_kw_export_keyword(config_parser);
+            add_history_source_keyword(config_parser);
+            add_runpath_file_keyword(config_parser);
             hook_manager_add_config_items(config_parser);
             site_config_add_config_items(config_parser, false);
             config_add_key_value(config_parser, RES_CONFIG_FILE_KEY, false,
