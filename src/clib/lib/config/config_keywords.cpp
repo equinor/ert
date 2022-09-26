a54b0d63028ac401b2d1be77412be4317e152381
Eivind Jahren <ejah@equinor.com>
2022-09-26 14:44:38 +0200

    Refactor keyword setting of simple keywords

--- src/clib/lib/config/config_keywords.cpp
+++ src/clib/lib/config/config_keywords.cpp
@@ -6,6 +6,43 @@
 #include <ert/enkf/hook_manager.hpp>
 #include <ert/python.hpp>
 
+static void add_float_keyword(config_parser_type *config_parser,
+                              std::string keyword) {
+    config_add_key_value(config_parser, keyword.c_str(), false, CONFIG_FLOAT);
+}
+
+static void add_int_keyword(config_parser_type *config_parser,
+                            std::string keyword) {
+    config_add_key_value(config_parser, keyword.c_str(), false, CONFIG_INT);
+}
+
+static void add_string_keyword(config_parser_type *config_parser,
+                               std::string keyword) {
+    config_add_key_value(config_parser, keyword.c_str(), false, CONFIG_STRING);
+}
+
+static void add_bool_keyword(config_parser_type *config_parser,
+                             std::string keyword) {
+    config_add_key_value(config_parser, keyword.c_str(), false, CONFIG_BOOL);
+}
+
+static void add_path_keyword(config_parser_type *config_parser,
+                             std::string keyword) {
+    config_add_key_value(config_parser, keyword.c_str(), false, CONFIG_PATH);
+}
+
+static void add_existing_path_keyword(config_parser_type *config_parser,
+                                      std::string keyword) {
+    config_add_key_value(config_parser, keyword.c_str(), false,
+                         CONFIG_EXISTING_PATH);
+}
+static config_schema_item_type *
+add_single_arg_keyword(config_parser_type *config_parser, std::string keyword) {
+    auto item = config_add_schema_item(config_parser, keyword.c_str(), false);
+    config_schema_item_set_argc_minmax(item, 1, 1);
+    return item;
+}
+
 static void add_num_realizations_keyword(config_parser_type *config_parser) {
     auto item =
         config_add_schema_item(config_parser, NUM_REALIZATIONS_KEY, true);
@@ -18,11 +55,6 @@ static void add_run_template_keyword(config_parser_type *config_parser) {
     auto item = config_add_schema_item(config_parser, RUN_TEMPLATE_KEY, false);
     config_schema_item_set_argc_minmax(item, 2, CONFIG_DEFAULT_ARG_MAX);
     config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
-}
-
-static void add_jobname_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, JOBNAME_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
 }
 
 static void add_forward_model_keyword(config_parser_type *config_parser) {
@@ -41,22 +73,8 @@ static void add_data_kw_keyword(config_parser_type *config_parser) {
     config_schema_item_set_argc_minmax(item, 2, 2);
 }
 
-static void add_obs_config_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, OBS_CONFIG_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
-}
-
-static void add_gen_kw_export_keyword(config_parser_type *config_parser) {
-    auto item =
-        config_add_schema_item(config_parser, GEN_KW_EXPORT_NAME_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-}
-
 static void add_history_source_keyword(config_parser_type *config_parser) {
-    auto item =
-        config_add_schema_item(config_parser, HISTORY_SOURCE_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
+    auto item = add_single_arg_keyword(config_parser, HISTORY_SOURCE_KEY);
 
     stringlist_type *argv = stringlist_alloc_new();
     stringlist_append_copy(argv, "SCHEDULE");
@@ -73,12 +91,6 @@ static void add_history_source_keyword(config_parser_type *config_parser) {
     config_schema_item_set_required_children_on_value(item, "REFCASE_HISTORY",
                                                       refcase_dep);
     stringlist_free(refcase_dep);
-}
-
-static void add_runpath_file_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, RUNPATH_FILE_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_PATH);
 }
 
 static void add_stop_long_running_keyword(config_parser_type *config_parser) {
@@ -133,10 +145,9 @@ static void add_set_env_keyword(config_parser_type *config_parser) {
 }
 
 static void add_umask_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, UMASK_KEY, false);
+    auto item = add_single_arg_keyword(config_parser, UMASK_KEY);
     config_schema_item_set_deprecated(
         item, "UMASK is deprecated and will be removed in the future.");
-    config_schema_item_set_argc_minmax(item, 1, 1);
 }
 
 static void add_update_path_keyword(config_parser_type *config_parser) {
@@ -148,32 +159,10 @@ static void add_update_path_keyword(config_parser_type *config_parser) {
     config_schema_item_set_envvar_expansion(item, false);
 }
 
-static void add_licence_path_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, LICENSE_PATH_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_PATH);
-}
-
 static void add_install_job_keyword(config_parser_type *config_parser) {
     auto item = config_add_schema_item(config_parser, INSTALL_JOB_KEY, false);
     config_schema_item_set_argc_minmax(item, 2, 2);
     config_schema_item_iset_type(item, 1, CONFIG_EXISTING_PATH);
-}
-
-static void
-add_install_job_directory_keyword(config_parser_type *config_parser) {
-    auto item =
-        config_add_schema_item(config_parser, INSTALL_JOB_DIRECTORY_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_PATH);
-}
-
-static void
-add_workflow_job_directory_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser,
-                                       WORKFLOW_JOB_DIRECTORY_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_PATH);
 }
 
 static void add_load_workflow_keyword(config_parser_type *config_parser) {
@@ -188,17 +177,7 @@ static void add_load_workflow_job_keyword(config_parser_type *config_parser) {
     config_schema_item_set_argc_minmax(item, 1, 2);
     config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
 }
-static void add_max_submit_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, MAX_SUBMIT_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_INT);
-}
 
-static void add_num_cpu_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, NUM_CPU_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_INT);
-}
 static void add_queue_system_keyword(config_parser_type *config_parser,
                                      bool required) {
     auto item =
@@ -212,32 +191,8 @@ static void add_queue_option_keyword(config_parser_type *config_parser) {
 }
 
 static void add_job_script_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, JOB_SCRIPT_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
+    auto item = add_single_arg_keyword(config_parser, JOB_SCRIPT_KEY);
     config_schema_item_iset_type(item, 0, CONFIG_EXECUTABLE);
-}
-
-static void add_ecl_base_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, ECLBASE_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-}
-
-static void add_data_file_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, DATA_FILE_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
-}
-
-static void add_grid_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, GRID_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_EXISTING_PATH);
-}
-
-static void add_refcase_keyword(config_parser_type *config_parser) {
-    auto item = config_add_schema_item(config_parser, REFCASE_KEY, false);
-    config_schema_item_set_argc_minmax(item, 1, 1);
-    config_schema_item_iset_type(item, 0, CONFIG_PATH);
 }
 
 static void add_havana_fault_keyword(config_parser_type *config_parser) {
@@ -301,19 +256,19 @@ static void add_gen_data_keyword(config_parser_type *config) {
 }
 
 void init_site_config_parser(config_parser_type *config_parser) {
-    add_max_submit_keyword(config_parser);
-    add_num_cpu_keyword(config_parser);
+    add_int_keyword(config_parser, MAX_SUBMIT_KEY);
+    add_int_keyword(config_parser, NUM_CPU_KEY);
     add_queue_system_keyword(config_parser, true);
     add_queue_option_keyword(config_parser);
     add_job_script_keyword(config_parser);
-    add_workflow_job_directory_keyword(config_parser);
+    add_path_keyword(config_parser, WORKFLOW_JOB_DIRECTORY_KEY);
     add_load_workflow_keyword(config_parser);
     add_load_workflow_job_keyword(config_parser);
     add_set_env_keyword(config_parser);
     add_umask_keyword(config_parser);
     add_update_path_keyword(config_parser);
     add_install_job_keyword(config_parser);
-    add_install_job_directory_keyword(config_parser);
+    add_path_keyword(config_parser, INSTALL_JOB_DIRECTORY_KEY);
     add_hook_workflow_keyword(config_parser);
 }
 
@@ -324,100 +279,71 @@ ERT_CLIB_SUBMODULE("config_keywords", m) {
         [](py::object py_config_parser) {
             auto config_parser =
                 ert::from_cwrap<config_parser_type>(py_config_parser);
-            add_workflow_job_directory_keyword(config_parser);
+            add_path_keyword(config_parser, WORKFLOW_JOB_DIRECTORY_KEY);
             add_load_workflow_keyword(config_parser);
             add_load_workflow_job_keyword(config_parser);
-
-            config_add_key_value(config_parser, ENKF_ALPHA_KEY, false,
-                                 CONFIG_FLOAT);
-            config_add_key_value(config_parser, STD_CUTOFF_KEY, false,
-                                 CONFIG_FLOAT);
+            add_float_keyword(config_parser, ENKF_ALPHA_KEY);
+            add_float_keyword(config_parser, STD_CUTOFF_KEY);
             add_update_setting_keyword(config_parser);
-            config_add_key_value(config_parser, SINGLE_NODE_UPDATE_KEY, false,
-                                 CONFIG_BOOL);
-
-            config_add_key_value(config_parser, ENKF_RERUN_KEY, false,
-                                 CONFIG_BOOL);
-            config_add_key_value(config_parser, RERUN_START_KEY, false,
-                                 CONFIG_INT);
-            config_add_key_value(config_parser, UPDATE_LOG_PATH_KEY, false,
-                                 CONFIG_STRING);
-            config_add_key_value(config_parser, MIN_REALIZATIONS_KEY, false,
-                                 CONFIG_STRING);
-            config_add_key_value(config_parser, MAX_RUNTIME_KEY, false,
-                                 CONFIG_INT);
-
-            config_add_key_value(config_parser, ANALYSIS_SELECT_KEY, false,
-                                 CONFIG_STRING);
+            add_bool_keyword(config_parser, SINGLE_NODE_UPDATE_KEY);
+            add_bool_keyword(config_parser, ENKF_RERUN_KEY);
+            add_int_keyword(config_parser, RERUN_START_KEY);
+            add_string_keyword(config_parser, UPDATE_LOG_PATH_KEY);
+            add_string_keyword(config_parser, MIN_REALIZATIONS_KEY);
+            add_int_keyword(config_parser, MAX_RUNTIME_KEY);
+            add_string_keyword(config_parser, ANALYSIS_SELECT_KEY);
             add_analysis_copy_keyword(config_parser);
             add_analysis_set_var_keyword(config_parser);
-            config_add_key_value(config_parser, ITER_CASE_KEY, false,
-                                 CONFIG_STRING);
-            config_add_key_value(config_parser, ITER_COUNT_KEY, false,
-                                 CONFIG_INT);
-            config_add_key_value(config_parser, ITER_RETRY_COUNT_KEY, false,
-                                 CONFIG_INT);
+            add_string_keyword(config_parser, ITER_CASE_KEY);
+            add_int_keyword(config_parser, ITER_COUNT_KEY);
+            add_int_keyword(config_parser, ITER_RETRY_COUNT_KEY);
             // the two fault types are just added to the config object only to
             // be able to print suitable messages before exiting.
             add_havana_fault_keyword(config_parser);
             add_multflt_keyword(config_parser);
             add_gen_kw_keyword(config_parser);
             add_schedule_prediction_file_keyword(config_parser);
-
-            config_add_key_value(config_parser, GEN_KW_TAG_FORMAT_KEY, false,
-                                 CONFIG_STRING);
-
+            add_string_keyword(config_parser, GEN_KW_TAG_FORMAT_KEY);
             add_gen_param_keyword(config_parser);
             add_gen_data_keyword(config_parser);
-
             add_summary_keyword(config_parser);
             add_surface_keyword(config_parser);
             add_field_keyword(config_parser);
-
-            add_ecl_base_keyword(config_parser);
-            add_data_file_keyword(config_parser);
-            add_grid_keyword(config_parser);
-            add_refcase_keyword(config_parser);
-            config_add_key_value(config_parser, RANDOM_SEED_KEY, false,
-                                 CONFIG_STRING);
-            config_add_key_value(config_parser, MAX_RESAMPLE_KEY, false,
-                                 CONFIG_INT);
+            add_single_arg_keyword(config_parser, ECLBASE_KEY);
+            add_existing_path_keyword(config_parser, DATA_FILE_KEY);
+            add_existing_path_keyword(config_parser, GRID_KEY);
+            add_path_keyword(config_parser, REFCASE_KEY);
+            add_string_keyword(config_parser, RANDOM_SEED_KEY);
+            add_int_keyword(config_parser, MAX_RESAMPLE_KEY);
             add_num_realizations_keyword(config_parser);
             add_run_template_keyword(config_parser);
-            config_add_key_value(config_parser, RUNPATH_KEY, false,
-                                 CONFIG_PATH);
-            config_add_key_value(config_parser, DATA_ROOT_KEY, false,
-                                 CONFIG_PATH);
-            config_add_key_value(config_parser, ENSPATH_KEY, false,
-                                 CONFIG_PATH);
-            add_jobname_keyword(config_parser);
+            add_path_keyword(config_parser, RUNPATH_KEY);
+            add_path_keyword(config_parser, DATA_ROOT_KEY);
+            add_path_keyword(config_parser, ENSPATH_KEY);
+            add_single_arg_keyword(config_parser, JOBNAME_KEY);
             add_forward_model_keyword(config_parser);
             add_simulation_job_keyword(config_parser);
             add_data_kw_keyword(config_parser);
-            add_obs_config_keyword(config_parser);
-            config_add_key_value(config_parser, TIME_MAP_KEY, false,
-                                 CONFIG_EXISTING_PATH);
-            add_gen_kw_export_keyword(config_parser);
+            add_existing_path_keyword(config_parser, OBS_CONFIG_KEY);
+            add_existing_path_keyword(config_parser, TIME_MAP_KEY);
+            add_single_arg_keyword(config_parser, GEN_KW_EXPORT_NAME_KEY);
             add_history_source_keyword(config_parser);
-            add_runpath_file_keyword(config_parser);
-            add_max_submit_keyword(config_parser);
-            add_num_cpu_keyword(config_parser);
+            add_path_keyword(config_parser, RUNPATH_FILE_KEY);
+            add_int_keyword(config_parser, MAX_SUBMIT_KEY);
+            add_int_keyword(config_parser, NUM_CPU_KEY);
             add_queue_system_keyword(config_parser, false);
             add_queue_option_keyword(config_parser);
             add_job_script_keyword(config_parser);
-
+            add_load_workflow_job_keyword(config_parser);
             add_set_env_keyword(config_parser);
             add_umask_keyword(config_parser);
             add_update_path_keyword(config_parser);
-
-            add_licence_path_keyword(config_parser);
+            add_path_keyword(config_parser, LICENSE_PATH_KEY);
             add_install_job_keyword(config_parser);
-            add_install_job_directory_keyword(config_parser);
+            add_path_keyword(config_parser, INSTALL_JOB_DIRECTORY_KEY);
             add_hook_workflow_keyword(config_parser);
-            config_add_key_value(config_parser, RES_CONFIG_FILE_KEY, false,
-                                 CONFIG_EXISTING_PATH);
-            config_add_key_value(config_parser, CONFIG_DIRECTORY_KEY, false,
-                                 CONFIG_EXISTING_PATH);
+            add_existing_path_keyword(config_parser, RES_CONFIG_FILE_KEY);
+            add_existing_path_keyword(config_parser, CONFIG_DIRECTORY_KEY);
         },
         py::arg("config_parser"));
 }
