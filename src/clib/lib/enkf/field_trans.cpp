60811f6951102003a009242c823bf6ed5c3cf220
Håvard Berland <havb@equinor.com>
2022-10-06 10:20:57 +0200

    Remove copyright notices in .py, .cpp and .hpp files
    Having such a notice present in every file is visual noise and does not
    significantly increase protection against copyright infringement.
    
    Edit performed by:
    
    for file in `find . -name *.*pp`; do
        perl -0 -p -i -e 's/\/\*.*Copyright.*?\*\/\s*//is' $file
    done
    
    for file in `find . -name "*.py"`; do
        perl -0 -p -i -e 's/#\s+Copyright.*#\s+for more details.\s*//s' $file
    done

--- src/clib/lib/enkf/field_trans.cpp
+++ src/clib/lib/enkf/field_trans.cpp
@@ -1,22 +1,4 @@
 /*
-   Copyright (C) 2011  Equinor ASA, Norway.
-
-   The file 'field_trans.c' is part of ERT - Ensemble based Reservoir Tool.
-
-   ERT is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or
-   FITNESS FOR A PARTICULAR PURPOSE.
-
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
-   for more details.
-*/
-
-/*
   This file implements a number of functions used for init and output
   transformations of fields. The prototype for these functions is very
   simple: "one float in - one float out".
