4570a63615fed365f1040885e9ce9d4dcd72ec51
Valentin Krasontovitsch <valentin.krasontovitsch@webstep.no>
2022-10-07 13:53:09 +0200

    Determine num_cpu in res config
    We make the two instances of num cpu that can occur in the two different
    sources, namely user config and data file, available in res config.
    
    We also move the logic of which of the two is preferred (in certain
    contexts) live in res config instead of enkf main, as one of the
    contexts that uses said logic is the substitution config.
    
    Related to #4005

--- src/ert/_c_wrappers/enkf/enkf_main.py
+++ src/ert/_c_wrappers/enkf/enkf_main.py
@@ -301,13 +301,7 @@ class EnKFMain:
         return self.resConfig().queue_config
 
     def get_num_cpu(self) -> int:
-        queue_num_cpus = self.res_config.get_num_cpu()
-        if queue_num_cpus is not None:
-            return queue_num_cpus
-        ecl_num_cpus = self.eclConfig().num_cpu
-        if ecl_num_cpus is not None:
-            return ecl_num_cpus
-        return 1
+        return self.res_config.preferred_num_cpu()
 
     def __repr__(self):
         return f"EnKFMain(size: {self.getEnsembleSize()}, config: {self.res_config})"
