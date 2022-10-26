b1fe246013040ac0b3b5ad44945a9d86b35d08e5
Eivind Jahren <ejah@equinor.com>
2022-10-26 10:23:31 +0200

    Remove unused narratives

--- docs/developer_documentation/communication_protocol.rst
+++
@@ -1,4 +0,0 @@
-Communication Protocol
-======================
-
-.. ert_narratives::
--- tests/unit_tests/ensemble_evaluator/test_dispatchers.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from ert.ensemble_evaluator.narratives import dispatch_failing_job
-
-
-@pytest.mark.consumer_driven_contract_test
-def test_dispatchers_with_failing_job(unused_tcp_port):
-    with dispatch_failing_job().on_uri(f"ws://localhost:{unused_tcp_port}/dispatch"):
-        pass
