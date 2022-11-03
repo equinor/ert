d8ba96d23ffaef8effc1382b2bd5a431aed51a7c
Øyvind Eide <oyveid@equinor.com>
2022-11-03 13:52:30 +0100

    Remove redundant has_data check

--- src/clib/lib/python/enkf_fs_summary_data.cpp
+++ src/clib/lib/python/enkf_fs_summary_data.cpp
@@ -11,10 +11,8 @@ ERT_CLIB_SUBMODULE("enkf_fs_summary_data", m) {
         [](Cwrap<ensemble_config_type> ensemble_config, Cwrap<enkf_fs_type> fs,
            const std::vector<std::string> &summary_keys,
            const std::vector<int> &realizations, const int time_map_size) {
-            const int realization_size = std::size(realizations);
-            const int summary_key_size = std::size(summary_keys);
             const size_t size =
-                realization_size * summary_key_size * time_map_size;
+                realizations.size() * summary_keys.size() * time_map_size;
 
             double *data = new double[size];
             std::fill_n(data, size, NAN);
@@ -26,27 +24,22 @@ ERT_CLIB_SUBMODULE("enkf_fs_summary_data", m) {
 
                 auto &state_map = enkf_fs_get_state_map(fs);
 
-                std::vector<bool> mask =
-                    state_map.select_matching(STATE_HAS_DATA);
+                for (auto iens : realizations) {
+                    enkf_plot_tvector_type *vector =
+                        enkf_plot_tvector_alloc(ensemble_config_node, iens);
+                    enkf_plot_tvector_load(vector, fs, nullptr);
 
-                for (int iens = 0; iens < realization_size; iens++) {
-                    if (mask[iens]) {
-                        enkf_plot_tvector_type *vector =
-                            enkf_plot_tvector_alloc(ensemble_config_node, iens);
-                        enkf_plot_tvector_load(vector, fs, nullptr);
-
-                        for (int index = 1;
-                             index < enkf_plot_tvector_size(vector); index++) {
-                            if (enkf_plot_tvector_iget_active(vector, index)) {
-                                double value =
-                                    enkf_plot_tvector_iget_value(vector, index);
-                                data[iens * (time_map_size * summary_key_size) +
-                                     (((index - 1) * summary_key_size) +
-                                      summary_key_index)] = value;
-                            }
+                    for (int index = 1; index < enkf_plot_tvector_size(vector);
+                         index++) {
+                        if (enkf_plot_tvector_iget_active(vector, index)) {
+                            double value =
+                                enkf_plot_tvector_iget_value(vector, index);
+                            data[iens * (time_map_size * summary_key_size) +
+                                 (((index - 1) * summary_key_size) +
+                                  summary_key_index)] = value;
                         }
-                        enkf_plot_tvector_free(vector);
                     }
+                    enkf_plot_tvector_free(vector);
                 }
                 summary_key_index++;
             }
