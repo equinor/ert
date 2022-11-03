0b2a9d9b10382cc3b321cfc9e6f6ceccdafcb62b
Øyvind Eide <oyveid@equinor.com>
2022-11-03 10:38:28 +0100

    Fix bug where non existing function was used

--- tests/unit_tests/gui/tools/test_case_tool.py
+++ tests/unit_tests/gui/tools/test_case_tool.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 from unittest.mock import MagicMock
 
 import pytest
@@ -6,6 +7,7 @@ from qtpy.QtWidgets import QPushButton
 
 from ert._c_wrappers.enkf import EnKFMain, ResConfig
 from ert._clib.state_map import RealizationStateEnum
+from ert._clib.update import Parameter
 from ert.gui.tools.manage_cases.case_init_configuration import (
     CaseInitializationConfigurationPanel,
 )
@@ -27,3 +29,44 @@ def test_case_tool_init_prior(qtbot):
         list(storage.getStateMap())
         == [RealizationStateEnum.STATE_INITIALIZED] * ert.getEnsembleSize()
     )
+
+
+@pytest.mark.usefixtures("use_tmpdir")
+def test_that_case_tool_can_copy_case_state(qtbot):
+    """Test that we can copy state from one case to another, first
+    need to set up a prior so we have a case with data, then copy
+    that case into a new one.
+    """
+    config = dedent(
+        """
+    NUM_REALIZATIONS 1
+    GEN_KW KW_NAME template.txt kw.txt prior.txt
+    RANDOM_SEED 1234
+    """
+    )
+    with open("config.ert", "w") as fh:
+        fh.writelines(config)
+    with open("template.txt", "w") as fh:
+        fh.writelines("MY_KEYWORD <MY_KEYWORD>")
+    with open("prior.txt", "w") as fh:
+        fh.writelines("MY_KEYWORD NORMAL 0 1")
+    ert = EnKFMain(ResConfig("config.ert"))
+    storage_manager = ert.storage_manager
+
+    prior = storage_manager["default"]
+    ert.sample_prior(prior, list(range(ert.getEnsembleSize())))
+    new_case = storage_manager.add_case("new_case")
+    ert.switchFileSystem("new_case")
+    tool = CaseInitializationConfigurationPanel(ert, MagicMock())
+    assert (
+        list(new_case.getStateMap())
+        == [RealizationStateEnum.STATE_UNDEFINED] * ert.getEnsembleSize()
+    )
+    qtbot.mouseClick(
+        tool.findChild(QPushButton, name="initialize_existing_button"), Qt.LeftButton
+    )
+    assert new_case.load_parameter(
+        ert.ensembleConfig(),
+        [0],
+        Parameter("KW_NAME"),
+    ).flatten()[0] == pytest.approx(0.3797726974728599)
