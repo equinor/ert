edd8e0fcf10e3c9b0d1ecd8aefba744819aeab1f
Øyvind Eide <oyveid@equinor.com>
2022-11-03 15:02:58 +0100

    Make data array directly

--- src/clib/lib/python/enkf_fs_summary_data.cpp
+++ src/clib/lib/python/enkf_fs_summary_data.cpp
@@ -11,50 +11,36 @@ ERT_CLIB_SUBMODULE("enkf_fs_summary_data", m) {
         [](Cwrap<ensemble_config_type> ensemble_config, Cwrap<enkf_fs_type> fs,
            const std::vector<std::string> &summary_keys,
            const std::vector<int> &realizations, const int time_map_size) {
-            const size_t size =
-                realizations.size() * summary_keys.size() * time_map_size;
-
-            double *data = new double[size];
-            std::fill_n(data, size, NAN);
+            py::array_t<double, 2> array(
+                {time_map_size * realizations.size(), summary_keys.size()});
+            auto data = array.mutable_unchecked();
 
             int summary_key_index = 0;
             for (const auto &key : summary_keys) {
                 auto ensemble_config_node =
                     ensemble_config_get_node(ensemble_config, key.c_str());
 
-                auto &state_map = enkf_fs_get_state_map(fs);
-
-                for (auto iens : realizations) {
+                for (size_t iens_index{}; iens_index < realizations.size();
+                     ++iens_index) {
+                    auto iens = realizations[iens_index];
                     enkf_plot_tvector_type *vector =
                         enkf_plot_tvector_alloc(ensemble_config_node, iens);
                     enkf_plot_tvector_load(vector, fs, nullptr);
 
                     for (int index = 1; index < enkf_plot_tvector_size(vector);
                          index++) {
-                        if (enkf_plot_tvector_iget_active(vector, index)) {
-                            double value =
-                                enkf_plot_tvector_iget_value(vector, index);
-                            data[iens * (time_map_size * summary_key_size) +
-                                 (((index - 1) * summary_key_size) +
-                                  summary_key_index)] = value;
-                        }
+                        double value =
+                            enkf_plot_tvector_iget_active(vector, index)
+                                ? enkf_plot_tvector_iget_value(vector, index)
+                                : NAN;
+                        data(iens_index * time_map_size + (index - 1),
+                             summary_key_index) = value;
                     }
                     enkf_plot_tvector_free(vector);
                 }
                 summary_key_index++;
             }
-
-            py::capsule free_when_done(data, [](void *f) {
-                double *data = reinterpret_cast<double *>(f);
-                delete[] data;
-            });
-
-            return py::array_t<double>(
-                {time_map_size * realization_size, summary_key_size}, // shape
-                {(summary_key_size) * sizeof(double),
-                 sizeof(double)}, // C-style contiguous strides for double
-                data,             // the data pointer
-                free_when_done);  // numpy array references this parent
+            return array;
         },
         py::arg("ens_cfg"), py::arg("fs"), py::arg("summary_keys"),
         py::arg("realizations"), py::arg("time_map_size"));
