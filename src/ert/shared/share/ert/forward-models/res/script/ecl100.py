715a2ec9c65257661a194567652c69d8fc1cd369
Eivind Jahren <ejah@equinor.com>
2022-11-15 09:45:40 +0100

    Guard res/scripts with if name == __main__:
    this makes these python files importable when not executed as main.

--- src/ert/shared/share/ert/forward-models/res/script/ecl100.py
+++ src/ert/shared/share/ert/forward-models/res/script/ecl100.py
@@ -4,5 +4,6 @@ import sys
 from ecl_config import Ecl100Config
 from ecl_run import run
 
-config = Ecl100Config()
-run(config, [arg for arg in sys.argv[1:] if len(arg) > 0])
+if __name__ == "__main__":
+    config = Ecl100Config()
+    run(config, [arg for arg in sys.argv[1:] if len(arg) > 0])
--- src/ert/shared/share/ert/forward-models/res/script/ecl300.py
+++ src/ert/shared/share/ert/forward-models/res/script/ecl300.py
@@ -4,5 +4,6 @@ import sys
 from ecl_config import Ecl300Config
 from ecl_run import run
 
-config = Ecl300Config()
-run(config, [arg for arg in sys.argv[1:] if len(arg) > 0])
+if __name__ == "__main__":
+    config = Ecl300Config()
+    run(config, [arg for arg in sys.argv[1:] if len(arg) > 0])
