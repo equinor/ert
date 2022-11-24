d68a2767c28da370eb74b5794570b27bc085511b
Åsmund Birkeland <musiv@equinor.com>
2022-11-24 12:09:25 +0100

    Store GEN_KW using numpy files

--- src/ert/_c_wrappers/enkf/__init__.py
+++ src/ert/_c_wrappers/enkf/__init__.py
@@ -11,7 +11,7 @@ from .config import (
     SummaryConfig,
 )
 from .config_keys import ConfigKeys
-from .data import EnkfNode, ExtParam, Field, GenKw
+from .data import EnkfNode, ExtParam, Field
 from .enkf_fs import EnkfFs
 from .enkf_main import EnKFMain, ObservationConfigError
 from .enkf_obs import EnkfObs
@@ -46,7 +46,6 @@ __all__ = [
     "BlockDataConfig",
     "ObsVector",
     "Field",
-    "GenKw",
     "ExtParam",
     "EnkfNode",
     "FieldConfig",
