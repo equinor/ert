cb03a654e28359306beb427ed4f704181c0ea859
Lars Petter Hauge <larsenhauge@gmail.com>
2022-11-29 11:45:21 +0100

    fix test_ensemble_legacy tests

--- tests/unit_tests/ensemble_evaluator/test_ensemble_legacy.py
+++ tests/unit_tests/ensemble_evaluator/test_ensemble_legacy.py
@@ -10,6 +10,7 @@ from cloudevents.http import CloudEvent
 from websockets.exceptions import ConnectionClosed
 
 from ert.ensemble_evaluator import identifiers, state
+from ert.ensemble_evaluator._wait_for_evaluator import wait_for_evaluator
 from ert.ensemble_evaluator.config import EvaluatorServerConfig
 from ert.ensemble_evaluator.evaluator import EnsembleEvaluator
 
@@ -42,7 +43,8 @@ async def test_run_legacy_ensemble(tmpdir, make_ensemble_builder):
 
 
 @pytest.mark.timeout(60)
-def test_run_and_cancel_legacy_ensemble(tmpdir, make_ensemble_builder):
+@pytest.mark.asyncio
+async def test_run_and_cancel_legacy_ensemble(tmpdir, make_ensemble_builder):
     num_reals = 2
     custom_port_range = range(1024, 65535)
     with tmpdir.as_cwd():
@@ -54,19 +56,32 @@ def test_run_and_cancel_legacy_ensemble(tmpdir, make_ensemble_builder):
             generate_cert=False,
         )
 
-        evaluator = EnsembleEvaluator(ensemble, config, 0)
+        server = EnsembleEvaluator(ensemble, config, 0)
 
-        with evaluator.run() as mon:
-            cancel = True
-            try:
-                for _ in mon.track():
-                    if cancel:
-                        mon.signal_cancel()
-                        cancel = False
-            except ConnectionClosed:
-                pass  # monitor throws some variant of CC if dispatcher dies
+        server_task = asyncio.create_task(server.evaluator_server())
+        con_info = config.get_connection_info()
+        await wait_for_evaluator(
+            base_url=con_info.url, token=con_info.token, cert=con_info.cert
+        )
+        evaluation_task = asyncio.create_task(
+            ensemble.evaluate_async(config, experiment_id=None)
+        )
+        cancel_event = CloudEvent(
+            {
+                "type": identifiers.EVTYPE_EE_USER_CANCEL,
+                "source": f"/ert/monitor/0",
+                "id": str(uuid.uuid1()),
+            }
+        )
 
-        assert evaluator._ensemble.status == state.ENSEMBLE_STATE_CANCELLED
+        async with websockets.connect(f"{config.client_uri}") as websocket:
+            await websocket.send(to_json(cancel_event))
+
+        await evaluation_task
+        await server.stop()
+        await server_task
+
+        assert ensemble.status == state.ENSEMBLE_STATE_CANCELLED
 
         # realisations should not finish, thus not creating a status-file
         for i in range(num_reals):
@@ -74,7 +89,8 @@ def test_run_and_cancel_legacy_ensemble(tmpdir, make_ensemble_builder):
 
 
 @pytest.mark.timeout(60)
-def test_run_legacy_ensemble_exception(tmpdir, make_ensemble_builder):
+@pytest.mark.asyncio
+async def test_run_legacy_ensemble_exception(tmpdir, make_ensemble_builder):
     num_reals = 2
     custom_port_range = range(1024, 65535)
     with tmpdir.as_cwd():
@@ -86,17 +102,22 @@ def test_run_legacy_ensemble_exception(tmpdir, make_ensemble_builder):
             generate_cert=False,
         )
         evaluator = EnsembleEvaluator(ensemble, config, 0)
+        server = EnsembleEvaluator(ensemble, config, 0)
+        server_task = asyncio.create_task(server.evaluator_server())
+
+        con_info = config.get_connection_info()
+        await wait_for_evaluator(
+            base_url=con_info.url, token=con_info.token, cert=con_info.cert
+        )
 
         with patch.object(ensemble._job_queue, "submit_complete") as faulty_queue:
             faulty_queue.side_effect = RuntimeError()
-            with evaluator.run() as monitor:
-                for e in monitor.track():
-                    if e.data is not None and e.data.get(identifiers.STATUS) in [
-                        state.ENSEMBLE_STATE_FAILED,
-                        state.ENSEMBLE_STATE_STOPPED,
-                    ]:
-                        monitor.signal_done()
-            assert evaluator._ensemble.status == state.ENSEMBLE_STATE_FAILED
+            await ensemble.evaluate_async(config, experiment_id=None)
+
+        await server.stop()
+        await server_task
+
+        assert evaluator._ensemble.status == state.ENSEMBLE_STATE_FAILED
 
         # realisations should not finish, thus not creating a status-file
         for i in range(num_reals):
