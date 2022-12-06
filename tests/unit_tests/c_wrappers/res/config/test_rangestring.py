04df3996f64e141f2ef5a7d5f082b51a0752d918
Eivind Jahren <ejah@equinor.com>
2022-12-06 10:18:46 +0100

    Removed duplicate of rangestring test and more doc

--- tests/unit_tests/c_wrappers/res/config/test_rangestring.py
+++ tests/unit_tests/c_wrappers/res/config/test_rangestring.py
@@ -7,29 +7,6 @@ from ert._c_wrappers.config.rangestring import (
 )
 
 
-def testMaskToRangeConversion():
-    cases = (
-        ([0, 1, 0, 1, 1, 1, 0], "1, 3-5"),
-        ([0, 1, 0, 1, 1, 1, 1], "1, 3-6"),
-        ([0, 1, 0, 1, 0, 1, 0], "1, 3, 5"),
-        ([1, 1, 0, 0, 1, 1, 1, 0, 1], "0-1, 4-6, 8"),
-        ([1, 1, 1, 1, 1, 1, 1], "0-6"),
-        ([0, 0, 0, 0, 0, 0, 0], ""),
-        ([True, False, True, True], "0, 2-3"),
-        ([], ""),
-    )
-
-    def nospaces(s):
-        return "".join(s.split())
-
-    for mask, expected in cases:
-        rngstr = mask_to_rangestring(mask)
-        assert nospaces(rngstr) == nospaces(expected), (
-            f"Mask {mask} was converted to {rngstr} which is different "
-            f"from the expected range {expected}"
-        )
-
-
 @pytest.mark.parametrize(
     "mask, expected_string",
     [
