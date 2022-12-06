80c9569bf82eb7093bcd9a9104cdcbd4d6ef8929
Eivind Jahren <ejah@equinor.com>
2022-12-06 08:07:53 +0100

    Use doctest for _prepare_x_axis test

--- tests/unit_tests/storage/test_extraction.py
+++ tests/unit_tests/storage/test_extraction.py
@@ -15,26 +15,6 @@ from ert._c_wrappers.enkf import RunContext
 from ert.shared.storage import extraction
 
 
-@pytest.mark.parametrize(
-    "x_axis, expected",
-    [
-        ([1, 2, 3, 4], ["1", "2", "3", "4"]),
-        (["a", "b", "c"], ["a", "b", "c"]),
-        (
-            [pd.Timestamp(x, unit="d") for x in range(4)],
-            [
-                "1970-01-01T00:00:00",
-                "1970-01-02T00:00:00",
-                "1970-01-03T00:00:00",
-                "1970-01-04T00:00:00",
-            ],
-        ),
-    ],
-)
-def test_prepare_x_axis(x_axis, expected):
-    assert expected == extraction._prepare_x_axis(x_axis)
-
-
 class ErtConfigBuilder:
     def __init__(self):
         self.ensemble_size = 1
