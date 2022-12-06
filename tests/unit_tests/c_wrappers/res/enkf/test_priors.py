ab1bee9840429eb8a508d08933b80baa762ce4ba
Eivind Jahren <ejah@equinor.com>
2022-12-06 07:12:22 +0100

    Move test_adding_priors to use poly_case fixture

--- tests/unit_tests/c_wrappers/res/enkf/test_priors.py
+++ tests/unit_tests/c_wrappers/res/enkf/test_priors.py
@@ -1,41 +1,32 @@
 import gc
-import os
-import shutil
 
 import pytest
 
-from ert._c_wrappers.enkf import EnKFMain, ResConfig, RunContext
+from ert._c_wrappers.enkf import EnKFMain, ResConfig
 
 
-def test_adding_priors(tmpdir, source_root):
-    shutil.copytree(
-        os.path.join(source_root, "test-data", "poly_example"),
-        os.path.join(str(tmpdir), "poly_example"),
+def test_adding_priors(poly_case):
+    m = poly_case
+    run_context = m.create_ensemble_experiment_run_context(
+        active_mask=[True] * 10,
+        iteration=0,
     )
-    with tmpdir.as_cwd():
-        rc = ResConfig("poly_example/poly.ert")
-        m = EnKFMain(rc)
-        run_context = m.create_ensemble_experiment_run_context(
-            active_mask=[True] * 10,
-            iteration=0,
-        )
-        m.sample_prior(run_context.sim_fs, run_context.active_realizations)
+    m.sample_prior(run_context.sim_fs, run_context.active_realizations)
+    m.createRunPath(run_context)
+    del m
+    gc.collect()
+
+    with open("coeff_priors", "a") as f:
+        f.write("COEFF_D UNIFORM 0 5\n")
+    m = EnKFMain(ResConfig("poly.ert"))
+
+    run_context = m.create_ensemble_experiment_run_context(
+        active_mask=[True] * 10,
+        iteration=0,
+    )
+    with pytest.raises(
+        ValueError,
+        match="The configuration of GEN_KW "
+        "parameter COEFFS is of size 4, expected 3",
+    ):
         m.createRunPath(run_context)
-        del m
-        gc.collect()
-
-        with open("poly_example/coeff_priors", "a") as f:
-            f.write("COEFF_D UNIFORM 0 5\n")
-        rc = ResConfig("poly_example/poly.ert")
-        m = EnKFMain(rc)
-
-        run_context = m.create_ensemble_experiment_run_context(
-            active_mask=[True] * 10,
-            iteration=0,
-        )
-        with pytest.raises(
-            ValueError,
-            match="The configuration of GEN_KW "
-            "parameter COEFFS is of size 4, expected 3",
-        ):
-            m.createRunPath(run_context)
