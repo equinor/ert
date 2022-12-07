fce64dbb53b8124c8ec3a1df36d7091b0cf7e816
Eivind Jahren <ejah@equinor.com>
2022-12-07 15:06:39 +0100

    Add more migration suggestions

--- src/ert/_c_wrappers/enkf/_deprecation_migration_suggester.py
+++ src/ert/_c_wrappers/enkf/_deprecation_migration_suggester.py
@@ -9,17 +9,82 @@ class DeprecationMigrationSuggester:
         self._parser = parser
         self._add_deprecated_keywords_to_parser()
 
+    REPLACE_WITH_GEN_KW = [
+        "RELPERM",
+        "MULTZ",
+        "EQUIL",
+        "GEN_PARAM",
+        "SCHEDULE_PREDICTION_FILE",
+    ]
+    JUST_REMOVE_KEYWORDS = ["UMASK", "LOG_FILE", "LOG_LEVEL"]
+
     def _add_deprecated_keywords_to_parser(self):
-        self._parser.add("UMASK")
+        for kw in self.REPLACE_WITH_GEN_KW:
+            self._parser.add(kw)
+        for kw in self.JUST_REMOVE_KEYWORDS:
+            self._parser.add(kw)
+        self._parser.add("HAVANA_FAULT")
+        self._parser.add("MULTFLT")
+        self._parser.add("REFCASE_LIST")
+        self._parser.add("RFTPATH")
+        self._parser.add("END_DATE")
+        self._parser.add("CASE_TABLE")
 
     def suggest_migrations(self, filename: str):
         suggestions = []
 
         content = self._parser.parse(filename)
-        if content.hasKey("UMASK"):
+        for kw in self.REPLACE_WITH_GEN_KW:
+            if content.hasKey(kw):
+                suggestions.append(
+                    "The keyword {kw} was deprecated in 2009 in favor of using"
+                    " GEN_KW and FORWARD_MODEL."
+                )
+        for kw in self.JUST_REMOVE_KEYWORDS:
+            if content.hasKey(kw):
+                suggestions.append(
+                    f"The {kw} keyword no longer has any effect "
+                    "and can safely be removed from the config file."
+                )
+        if content.hasKey("HAVANA_FAULT"):
             suggestions.append(
-                "The UMASK keyword has been removed "
-                "and has no effect, it can safely be removed"
+                "Direct interoperability with havana was removed from ert in 2009."
+                " The behavior of HAVANA_FAULT can be reproduced using"
+                " GEN_KW and FORWARD_MODEL."
+            )
+        if content.hasKey("MULTFLT"):
+            suggestions.append(
+                "The MULTFLT keyword was replaced by the GENKW keyword in 2009."
+                "Please see https://ert.readthedocs.io/en/latest/"
+                "reference/configuration/keywords.html#gen-kw"
+                "to see how to migrate from MULTFLT to GEN_KW."
+            )
+        if content.hasKey("REFCASE_LIST"):
+            suggestions.append(
+                "The REFCASE_LIST keyword was used to give a .DATA file "
+                "to be used for plotting. The corresponding plotting functionality "
+                "was removed in 2015, and this keyword can be safely removed from "
+                "the config file."
+            )
+        if content.hasKey("RFTPATH"):
+            suggestions.append(
+                "The RFTPATH keyword was used to give a path to well observations "
+                "to be used for plotting. The corresponding plotting functionality "
+                "was removed in 2015, and the RFTPATH keyword can safely be removed "
+                "from the config file."
+            )
+        if content.hasKey("END_DATE"):
+            suggestions.append(
+                "The END_DATE keyword was used to check that a the dates in a summary "
+                "file would go beyond a certaind date. This would only display a "
+                "warning in case of problems. The keyword has since been deprecated, "
+                "and can be safely removed from the config file."
+            )
+        if content.hasKey("CASE_TABLE"):
+            suggestions.append(
+                "The CASE_TABLE keyword was used with a deprecated sensitivity "
+                "analysis feature to give descriptive names to cases. It no longer has "
+                " any effect and can safely be removed from the config file."
             )
 
         return suggestions
--- tests/test_config_parsing/test_suggester.py
+++ tests/test_config_parsing/test_suggester.py
@@ -16,4 +16,51 @@ def test_that_suggester_gives_umask_migration(suggester, tmp_path):
     suggestions = suggester.suggest_migrations(str(tmp_path / "config.ert"))
 
     assert len(suggestions) == 1
-    assert suggestions[0].startswith("The UMASK keyword has been removed")
+    assert suggestions[0].startswith("The UMASK keyword no longer")
+
+
+def test_that_suggester_gives_havana_fault_migration(suggester, tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nHAVANA_FAULT\n")
+    suggestions = suggester.suggest_migrations(str(tmp_path / "config.ert"))
+
+    assert len(suggestions) == 1
+    assert "The behavior of HAVANA_FAULT can be reproduced using" in suggestions[0]
+
+
+def test_that_suggester_gives_multflt_migration(suggester, tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nMULTFLT\n")
+    suggestions = suggester.suggest_migrations(str(tmp_path / "config.ert"))
+
+    assert len(suggestions) == 1
+    assert (
+        "ert.readthedocs.io/en/latest/reference/configuration/keywords.html#gen-kw"
+        in suggestions[0]
+    )
+
+
+def test_that_suggester_gives_refcase_list_migration(suggester, tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nREFCASE_LIST case.DATA\n")
+    suggestions = suggester.suggest_migrations(str(tmp_path / "config.ert"))
+
+    assert len(suggestions) == 1
+    assert (
+        "The corresponding plotting functionality was removed in 2015" in suggestions[0]
+    )
+
+
+def test_that_suggester_gives_rftpath_migration(suggester, tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nRFTPATH rfts/\n")
+    suggestions = suggester.suggest_migrations(str(tmp_path / "config.ert"))
+
+    assert len(suggestions) == 1
+    assert (
+        "The corresponding plotting functionality was removed in 2015" in suggestions[0]
+    )
+
+
+def test_that_suggester_gives_end_date_migration(suggester, tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nEND_DATE 2023.01.01\n")
+    suggestions = suggester.suggest_migrations(str(tmp_path / "config.ert"))
+
+    assert len(suggestions) == 1
+    assert "only display a warning in case of problems" in suggestions[0]
