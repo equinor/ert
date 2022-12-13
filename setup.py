d639960e6297bc756cc227806a682ee96dc42129
Åsmund Birkeland <musiv@equinor.com>
2022-12-13 14:34:07 +0100

    Load/save surface params using xtgeo

--- setup.py
+++ setup.py
@@ -150,6 +150,7 @@ args = dict(
         "websockets >= 9.0.1",
         "httpx",
         "tables",
+        "xtgeo",
     ],
     entry_points={
         "console_scripts": [
--- src/ert/_c_wrappers/enkf/config/enkf_config_node.py
+++ src/ert/_c_wrappers/enkf/config/enkf_config_node.py
@@ -21,6 +21,7 @@ from .field_config import FieldConfig
 from .gen_data_config import GenDataConfig
 from .gen_kw_config import GenKwConfig
 from .summary_config import SummaryConfig
+from .surface_config import SurfaceConfig
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
@@ -170,6 +171,9 @@ class EnkfConfigNode(BaseCClass):
 
     def getFieldModelConfig(self) -> FieldConfig:
         return FieldConfig.createCReference(self._get_ref(), parent=self)
+
+    def getSurfaceModelConfig(self) -> SurfaceConfig:
+        return SurfaceConfig.createCReference(self._get_ref(), parent=self)
 
     def getDataModelConfig(self) -> GenDataConfig:
         return GenDataConfig.createCReference(self._get_ref(), parent=self)
