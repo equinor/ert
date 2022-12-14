d8590279adf1ed14d2ad927e54728a55bc0dbc07
Zohar Malamant <ZOM@equinor.com>
2022-12-14 22:32:31 +0100

    Move 'pure' EnkfFs functions to new Storage class
    This commit moves functions from EnkfFs that are independent from
    libres. The idea is that eventually this will become the API for a new
    abstract base class, with `Storage` being one of the implementations.
    The other might be to write/read to the cloud (postgres/azure blob) or
    to the ERT Storage Server API.

--- src/ert/_c_wrappers/enkf/enkf_fs.py
+++ src/ert/_c_wrappers/enkf/enkf_fs.py
@@ -1,7 +1,6 @@
 import logging
 import math
 import shutil
-from datetime import datetime
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
@@ -20,6 +19,7 @@ from ert._c_wrappers.enkf.ert_config import EnsembleConfig
 from ert._c_wrappers.enkf.time_map import TimeMap
 from ert._clib import update
 from ert.ensemble_evaluator.callbacks import forward_model_ok
+from ert.storage import Storage
 
 if TYPE_CHECKING:
     from ecl.summary import EclSum
@@ -87,6 +87,7 @@ class EnkfFs(BaseCClass):
             time_map.attach_refcase(self.refcase)
         self._ensemble_config = ensemble_config
         self._ensemble_size = ensemble_size
+        self._storage = Storage(self.mount_point)
 
     def getTimeMap(self) -> TimeMap:
         return _clib.enkf_fs.get_time_map(self)
@@ -168,12 +169,7 @@ class EnkfFs(BaseCClass):
         realization: int,
         data: npt.ArrayLike,
     ) -> None:
-        output_path = self.mount_point / f"gen-kw-{realization}"
-        Path.mkdir(output_path, exist_ok=True)
-
-        np.save(output_path / parameter_name, data)
-        with open(output_path / f"{parameter_name}-keys", "w", encoding="utf-8") as f:
-            f.write("\n".join(parameter_keys))
+        self._storage.save_gen_kw(parameter_name, parameter_keys, realization, data)
 
         self.getStateMap().update_matching(
             realization,
@@ -292,106 +288,32 @@ class EnkfFs(BaseCClass):
         axis: List[Any],
         realization: int,
     ) -> None:
-        output_path = self.mount_point / f"summary-{realization}"
-        Path.mkdir(output_path, exist_ok=True)
-
-        np.save(output_path / "data", data)
-        with open(output_path / "keys", "w", encoding="utf-8") as f:
-            f.write("\n".join(keys))
-
-        with open(output_path / "time_map", "w", encoding="utf-8") as f:
-            f.write("\n".join([t.strftime("%Y-%m-%d") for t in axis]))
+        self._storage.save_summary_data(data, keys, axis, realization)
 
     def load_summary_data(
         self, summary_keys: List[str], realizations: List[int]
     ) -> Tuple["npt.NDArray[np.double]", List[str], List[int]]:
-        result = []
-        loaded = []
-        dates = []
-        for realization in realizations:
-            input_path = self.mount_point / f"summary-{realization}"
-            if not input_path.exists():
-                continue
-            loaded.append(realization)
-            np_data = np.load(input_path / "data.npy")
-
-            keys = []
-            with open(input_path / "keys", "r", encoding="utf-8") as f:
-                keys = [k.strip() for k in f.readlines()]
-            if not dates:
-                with open(input_path / "time_map", "r", encoding="utf-8") as f:
-                    dates = [
-                        datetime.strptime(k.strip(), "%Y-%m-%d") for k in f.readlines()
-                    ]
-            indices = [keys.index(summary_key) for summary_key in summary_keys]
-            selected_data = np_data[indices, :]
-
-            result.append(selected_data.reshape(1, len(indices) * len(dates)).T)
-        if not result:
-            return np.array([]), dates, loaded
-        return np.concatenate(result, axis=1), dates, loaded
+        return self._storage.load_summary_data(summary_keys, realizations)
 
     def load_summary_data_as_df(
         self, summary_keys: List[str], realizations: List[int]
     ) -> pd.DataFrame:
-        data, time_axis, realizations = self.load_summary_data(
-            summary_keys, realizations
-        )
-        if not data.any():
-            raise KeyError(f"Unable to load SUMMARY_DATA for keys: {summary_keys}")
-        multi_index = pd.MultiIndex.from_product(
-            [summary_keys, time_axis], names=["data_key", "axis"]
-        )
-        return pd.DataFrame(
-            data=data,
-            index=multi_index,
-            columns=realizations,
-        )
+        return self._storage.load_summary_data_as_df(summary_keys, realizations)
 
     def save_gen_data(
         self, key: str, data: List[List[float]], realization: int
     ) -> None:
-        output_path = self.mount_point / f"gen-data-{realization}"
-        Path.mkdir(output_path, exist_ok=True)
-        np_data = np.array(data)
-        np.save(output_path / key, np_data)
+        self._storage.save_gen_data(key, data, realization)
 
     def load_gen_data(
         self, key: str, realizations: List[int]
     ) -> Tuple["npt.NDArray[np.double]", List[int]]:
-        result = []
-        loaded = []
-        for realization in realizations:
-            input_path = self.mount_point / f"gen-data-{realization}" / f"{key}.npy"
-            if not input_path.exists():
-                continue
-
-            np_data = np.load(input_path)
-
-            result.append(np_data)
-            loaded.append(realization)
-        if not result:
-            raise KeyError(f"Unable to load GEN_DATA for key: {key}")
-        return np.stack(result).T, loaded
+        return self._storage.load_gen_data(key, realizations)
 
     def load_gen_data_as_df(
         self, keys: List[str], realizations: List[int]
     ) -> pd.DataFrame:
-        dfs = []
-        for key in keys:
-            data, realizations = self.load_gen_data(key, realizations)
-            x_axis = [*range(data.shape[0])]
-            multi_index = pd.MultiIndex.from_product(
-                [[key], x_axis], names=["data_key", "axis"]
-            )
-            dfs.append(
-                pd.DataFrame(
-                    data=data,
-                    index=multi_index,
-                    columns=realizations,
-                )
-            )
-        return pd.concat(dfs)
+        return self._storage.load_gen_data_as_df(keys, realizations)
 
     def load_from_run_path(
         self,
