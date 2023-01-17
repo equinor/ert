9bd20d4fc5a61bb4966214ea10895d5b21940541
Øyvind Eide <eide.oyvind87@gmail.com>
2023-01-17 08:45:42 +0100

    Remove unused code

--- src/clib/lib/include/ert/job_queue/job_node.hpp
+++ src/clib/lib/include/ert/job_queue/job_node.hpp
@@ -25,7 +25,6 @@
 typedef bool(job_callback_ftype)(void *);
 typedef struct job_queue_node_struct job_queue_node_type;
 
-time_t job_queue_node_get_timestamp(const job_queue_node_type *node);
 bool job_queue_node_status_transition(job_queue_node_type *node,
                                       job_queue_status_type *status,
                                       job_status_type new_status);
@@ -64,8 +63,6 @@ extern "C" PY_USED bool job_queue_node_kill_simple(job_queue_node_type *node,
 extern "C" void job_queue_node_free(job_queue_node_type *node);
 extern "C" job_status_type
 job_queue_node_get_status(const job_queue_node_type *node);
-void job_queue_node_free_driver_data(job_queue_node_type *node,
-                                     queue_driver_type *driver);
 PY_USED bool job_queue_node_update_status(job_queue_node_type *node,
                                           job_queue_status_type *status,
                                           queue_driver_type *driver);
@@ -73,25 +70,14 @@ extern "C" PY_USED job_status_type job_queue_node_refresh_status(
     job_queue_node_type *node, queue_driver_type *driver);
 extern "C" int
 job_queue_node_get_submit_attempt(const job_queue_node_type *node);
-void job_queue_node_reset_submit_attempt(job_queue_node_type *node);
-void job_queue_node_dec_submit_attempt(job_queue_node_type *node);
 
-time_t job_queue_node_get_sim_start(const job_queue_node_type *node);
-time_t job_queue_node_get_sim_end(const job_queue_node_type *node);
 double job_queue_node_time_since_sim_start(const job_queue_node_type *node);
 
-const char *job_queue_node_get_exit_file(const job_queue_node_type *node);
-
-bool job_queue_node_run_DONE_callback(job_queue_node_type *node);
-bool job_queue_node_run_RETRY_callback(job_queue_node_type *node);
-void job_queue_node_run_EXIT_callback(job_queue_node_type *node);
 int job_queue_node_get_queue_index(const job_queue_node_type *node);
 void job_queue_node_set_queue_index(job_queue_node_type *node, int queue_index);
 
 void *job_queue_node_get_driver_data(job_queue_node_type *node);
 extern "C" void job_queue_node_set_status(job_queue_node_type *node,
                                           job_status_type new_status);
-
-char *job_queue_node_get_name(job_queue_node_type *node);
 
 #endif
