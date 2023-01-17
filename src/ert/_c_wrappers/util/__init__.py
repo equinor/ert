512176e314a22d62712bb4a49386aa497807c36b
Øyvind Eide <eide.oyvind87@gmail.com>
2023-01-17 08:45:42 +0100

    Remove unused code

--- src/ert/_c_wrappers/util/__init__.py
+++ src/ert/_c_wrappers/util/__init__.py
@@ -1,8 +1,3 @@
-from cwrap import Prototype
-
-import ert._c_wrappers
-
-from .path_format import PathFormat
 from .substitution_list import SubstitutionList
 
-__all__ = [Prototype, ert._c_wrappers, PathFormat, SubstitutionList]
+__all__ = ["SubstitutionList"]
