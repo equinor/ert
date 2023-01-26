c2c2978afaaf4a934f38bd649bdb47ec3d6c51ce
DanSava <dan.sava42@gmail.com>
2023-01-26 15:11:15 +0200

    Cleanup verbose flag from workflow run method and refactor function_ert_script.py

--- src/ert/_c_wrappers/job_queue/function_ert_script.py
+++ src/ert/_c_wrappers/job_queue/function_ert_script.py
@@ -7,46 +7,34 @@ from ert._c_wrappers.job_queue import ErtScript
 class FunctionErtScript(ErtScript):
     def __init__(self, ert, function_name, argument_types, argument_count):
         super().__init__(ert)
-
-        parsed_argument_types = []
-
-        if ert is not None:
-            self.__function = ResPrototype(f"void* {function_name}(void*, stringlist)")
-
-        else:
-            for arg in argument_types:
-                if arg is bool:
-                    parsed_argument_types.append("bool")
-                elif arg is str:
-                    parsed_argument_types.append("char*")
-                elif arg is int:
-                    parsed_argument_types.append("int")
-                elif arg is float:
-                    parsed_argument_types.append("float")
-                else:
-                    raise TypeError(f"Unknown type: {arg}")
-
-            self.__function = ResPrototype(
-                f"void* {function_name}("
-                f"{', '.join(parsed_argument_types[:argument_count])})"
-            )
+        self._arg_types = argument_types
+        self._arg_count = argument_count
+        self._func_name = function_name
 
     def run(self, *args):
         ert = self.ert()
-        if ert is None:
-            # This is usually used for testing purposes without an ert instance
-            return self.__function(*args)
-        else:
+        if ert is not None:
             str_args = StringList()
             for arg in args:
                 str_args.append(arg)
+            pointer = ert.from_param(ert) if hasattr(ert, "from_param") else ert
 
-            if hasattr(ert, "from_param"):
-                pointer = ert.from_param(ert)
-            else:
-                pointer = ert  # ...
-
-            return self.__function(pointer, str_args)
+            function = ResPrototype(f"void* {self._func_name}(void*, stringlist)")
+            return function(pointer, str_args)
+        else:
+            parsed_argument_types = []
+            for arg in self._arg_types[:self._arg_count]:
+                if arg in [bool, int, float]:
+                    parsed_argument_types.append(arg.__name__)
+                elif arg is str:
+                    parsed_argument_types.append("char*")
+                else:
+                    raise TypeError(f"Unknown type: {arg}")
+            function = ResPrototype(
+                f"void* {self._func_name}("
+                f"{', '.join(parsed_argument_types)})"
+            )
+            return function(*args)
 
     def cancel(self):
         # job is not cancellable and will just ignore the call
