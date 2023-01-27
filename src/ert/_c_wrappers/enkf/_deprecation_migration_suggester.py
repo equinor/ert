4d95ec492865ff184927b660cfd6b20509c15632
Eivind Jahren <ejah@equinor.com>
2023-01-27 09:08:09 +0100

    Better description of deprecation logging

--- src/ert/_c_wrappers/enkf/_deprecation_migration_suggester.py
+++ src/ert/_c_wrappers/enkf/_deprecation_migration_suggester.py
@@ -58,7 +58,7 @@ class DeprecationMigrationSuggester:
 
         def add_suggestion(kw, suggestion):
             if content.hasKey(kw):
-                logger.info("Deprecated keyword %s", kw)
+                logger.info("Added suggestion for keyword %s: %s", kw, suggestion)
                 suggestions.append(suggestion)
 
         for key, _ in defines:
