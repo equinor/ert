f8dbae5183b2c8c51d1e3380593a7cb6a9a48667
Eivind Jahren <ejah@equinor.com>
2023-01-27 07:28:24 +0100

    Fix issue suggester parsing fails
    The suggester parsing did not include pre-defines and so could
    fail to validate in the c validation step.

--- src/ert/_c_wrappers/enkf/_deprecation_migration_suggester.py
+++ src/ert/_c_wrappers/enkf/_deprecation_migration_suggester.py
@@ -1,5 +1,5 @@
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict
 
 from ert._c_wrappers.enkf.runpaths import replace_runpath_format
 
@@ -12,8 +12,9 @@ logger = logging.getLogger(__name__)
 
 
 class DeprecationMigrationSuggester:
-    def __init__(self, parser: "ConfigParser"):
+    def __init__(self, parser: "ConfigParser", pre_defines: Dict[str, str]):
         self._parser = parser
+        self._pre_defines = pre_defines
         self._add_deprecated_keywords_to_parser()
 
     REPLACE_WITH_GEN_KW = [
@@ -50,7 +51,9 @@ class DeprecationMigrationSuggester:
 
     def suggest_migrations(self, filename: str):
         suggestions = []
-        content = self._parser.parse(filename)
+        content = self._parser.parse(
+            filename, pre_defined_kw_map=self._pre_defines, validate=False
+        )
         defines = config_content_as_dict(content, {}).get("DEFINE", [])
 
         def add_suggestion(kw, suggestion):
