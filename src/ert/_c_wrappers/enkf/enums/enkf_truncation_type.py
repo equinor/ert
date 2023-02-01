dbd03d46a1d3191db583ffbcd926d3d1a743f203
Åsmund Birkeland <musiv@equinor.com>
2023-02-01 10:26:42 +0100

    Make field parameter self contained in enkf_fs
    Store all info from field_config in enkf_fs such that one can
    read fields with not config

--- src/ert/_c_wrappers/enkf/enums/enkf_truncation_type.py
+++ src/ert/_c_wrappers/enkf/enums/enkf_truncation_type.py
@@ -3,9 +3,9 @@ from cwrap import BaseCEnum
 
 class EnkfTruncationType(BaseCEnum):
     TYPE_NAME = "enkf_truncation_type_enum"
-    TRUNCATE_NONE = None
-    TRUNCATE_MIN = None
-    TRUNCATE_MAX = None
+    TRUNCATE_NONE = 0
+    TRUNCATE_MIN = 1
+    TRUNCATE_MAX = 2
 
 
 EnkfTruncationType.addEnum("TRUNCATE_NONE", 0)
