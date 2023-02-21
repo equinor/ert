fcac4c3451af5b1797fc462a39d7590cda8a2be5
Frode Aarstad <frodeaarstad@gmail.com>
2023-02-21 09:14:10 +0100

    Fix load results manually

--- src/ert/gui/tools/load_results/load_results_tool.py
+++ src/ert/gui/tools/load_results/load_results_tool.py
@@ -1,11 +1,13 @@
+from ert.gui.ertnotifier import ErtNotifier
 from ert.gui.ertwidgets import resourceIcon, showWaitCursorWhileWaiting
 from ert.gui.ertwidgets.closabledialog import ClosableDialog
 from ert.gui.tools import Tool
 from ert.gui.tools.load_results import LoadResultsPanel
+from ert.libres_facade import LibresFacade
 
 
 class LoadResultsTool(Tool):
-    def __init__(self, facade):
+    def __init__(self, facade: LibresFacade, notifier: ErtNotifier):
         self.facade = facade
         super().__init__(
             "Load results manually",
@@ -14,14 +16,16 @@ class LoadResultsTool(Tool):
         )
         self.__import_widget = None
         self.__dialog = None
+        self._notifier = notifier
 
     def trigger(self):
         if self.__import_widget is None:
-            self.__import_widget = LoadResultsPanel(self.facade)
+            self.__import_widget = LoadResultsPanel(self.facade, self._notifier)
         self.__dialog = ClosableDialog(
             "Load results manually", self.__import_widget, self.parent()
         )
-        self.__import_widget.setCurrectCase()
+        self.__dialog.setObjectName("load_results_manually_tool")
+        self.__import_widget.setCurrentCase()
         self.__dialog.addButton("Load", self.load)
         self.__dialog.exec_()
 
