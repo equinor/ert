26a8fac3347404f3ec2a68e9777a44334f4477e7
Eivind Jahren <ejah@equinor.com>
2023-03-08 10:14:56 +0100

    Expose CSV_EXPORT as gui plugin again
    In 8ef5e3148904eaa8ec89e8481334551ed28d704e CSV_EXPORT was removed
    as it hadn't functioned correctly for a long time, and was believed not
    used. By user request. It is now back.
    
    This commit fixes the broken issue with get_context_value and
    adds a test for the plugin tool.

--- src/ert/shared/share/ert/workflows/jobs/internal-gui/scripts/csv_export.py
+++ src/ert/shared/share/ert/workflows/jobs/internal-gui/scripts/csv_export.py
@@ -2,9 +2,14 @@ import os
 import re
 
 import pandas
+from qtpy.QtWidgets import QCheckBox
 
 from ert import LibresFacade
-from ert._c_wrappers.job_queue import ErtScript
+from ert._c_wrappers.job_queue import CancelPluginException, ErtPlugin
+from ert.gui.ertwidgets.customdialog import CustomDialog
+from ert.gui.ertwidgets.listeditbox import ListEditBox
+from ert.gui.ertwidgets.models.path_model import PathModel
+from ert.gui.ertwidgets.pathchooser import PathChooser
 
 
 def loadDesignMatrix(filename) -> pandas.DataFrame:
@@ -14,7 +19,7 @@ def loadDesignMatrix(filename) -> pandas.DataFrame:
     return dm
 
 
-class CSVExportJob(ErtScript):
+class CSVExportJob(ErtPlugin):
     """
     Export of summary, misfit, design matrix data and gen kw into a single CSV file.
 
@@ -39,9 +44,33 @@ class CSVExportJob(ErtScript):
     The script also looks for default values for output path and design matrix
     path to present in the GUI. These can be specified with DATA_KW keyword in
     the config file:
-        DATA_KW CSV_OUTPUT_PATH <some path>
-        DATA_KW DESIGN_MATRIX_PATH <some path>
+        DATA_KW <CSV_OUTPUT_PATH> {some path}
+        DATA_KW <DESIGN_MATRIX_PATH> {some path}
     """
+
+    INFER_HELP = (
+        "<html>"
+        "If this is checked the iteration number will be inferred from the name i.e.:"
+        "<ul>"
+        "<li>case_name -> iteration: 0</li>"
+        "<li>case_name_0 -> iteration: 0</li>"
+        "<li>case_name_2 -> iteration: 2</li>"
+        "<li>case_0, case_2, case_5 -> iterations: 0, 2, 5</li>"
+        "</ul>"
+        "Leave this unchecked to set iteration number to the order of the listed cases:"
+        "<ul><li>case_0, case_2, case_5 -> iterations: 0, 1, 2</li></ul>"
+        "<br/>"
+        "</html>"
+    )
+
+    def getName(self):
+        return "CSV Export"
+
+    def getDescription(self):
+        return (
+            "Export GenKW, design matrix, misfit data "
+            "and summary data into a single CSV file."
+        )
 
     def inferIterationNumber(self, case_name):
         pattern = re.compile("_([0-9]+$)")
@@ -129,6 +158,69 @@ class CSVExportJob(ErtScript):
             f"columns to {output_file}."
         )
         return export_info
+
+    def getArguments(self, parent=None):
+        description = "The CSV export requires some information before it starts:"
+        dialog = CustomDialog("CSV Export", description, parent)
+
+        default_csv_output_path = self.get_context_value(
+            "<CSV_OUTPUT_PATH>", default="output.csv"
+        )
+        output_path_model = PathModel(default_csv_output_path)
+        output_path_chooser = PathChooser(output_path_model)
+
+        design_matrix_default = self.get_context_value(
+            "<DESIGN_MATRIX_PATH>", default=""
+        )
+        design_matrix_path_model = PathModel(
+            design_matrix_default, is_required=False, must_exist=True
+        )
+        design_matrix_path_chooser = PathChooser(design_matrix_path_model)
+
+        list_edit = ListEditBox(self.getAllCaseList())
+
+        infer_iteration_check = QCheckBox()
+        infer_iteration_check.setChecked(True)
+        infer_iteration_check.setToolTip(CSVExportJob.INFER_HELP)
+
+        drop_const_columns_check = QCheckBox()
+        drop_const_columns_check.setChecked(False)
+        drop_const_columns_check.setToolTip(
+            "If checked, exclude columns whose value is the same for every entry"
+        )
+
+        dialog.addLabeledOption("Output file path", output_path_chooser)
+        dialog.addLabeledOption("Design matrix path", design_matrix_path_chooser)
+        dialog.addLabeledOption("List of cases to export", list_edit)
+        dialog.addLabeledOption("Infer iteration number", infer_iteration_check)
+        dialog.addLabeledOption("Drop constant columns", drop_const_columns_check)
+
+        dialog.addButtons()
+
+        success = dialog.showAndTell()
+
+        if success:
+            design_matrix_path = design_matrix_path_model.getPath()
+            if design_matrix_path.strip() == "":
+                design_matrix_path = None
+
+            case_list = ",".join(list_edit.getItems())
+
+            return [
+                output_path_model.getPath(),
+                case_list,
+                design_matrix_path,
+                infer_iteration_check.isChecked(),
+                drop_const_columns_check.isChecked(),
+            ]
+
+        raise CancelPluginException("User cancelled!")
+
+    def get_context_value(self, name, default):
+        context = self.ert().get_context()
+        if name in context:
+            return context[name]
+        return default
 
     def getAllCaseList(self):
         fs_manager = self.ert().storage_manager
