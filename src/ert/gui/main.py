efde5ef6a3273e9d87eec582881502403bdb1daa
DanSava <dan.sava42@gmail.com>
2023-03-16 17:08:58 +0200

    Initialize ert_config before creating suggestions
    This will validate the config file before attempting to create a list of suggestions

--- src/ert/gui/main.py
+++ src/ert/gui/main.py
@@ -112,8 +112,8 @@ def _start_initial_gui_window(args, log_handler):
     try:
         with warnings.catch_warnings(record=True) as all_warnings:
             _check_locale()
-            suggestions += ErtConfig.make_suggestion_list(args.config)
             ert_config = ErtConfig.from_file(args.config)
+            suggestions += ErtConfig.make_suggestion_list(args.config)
         _log_difference_with_new_parser(args, ert_config)
         os.chdir(ert_config.config_path)
         # Changing current working directory means we need to update the config file to
