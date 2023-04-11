0e0424676c5250d2c88174e83080d66f6b0a5f2e
Øyvind Eide <oyveid@equinor.com>
2023-04-11 16:08:16 +0200

    Remove unused block_config

--- src/ert/_c_wrappers/enkf/__init__.py
+++ src/ert/_c_wrappers/enkf/__init__.py
@@ -27,7 +27,7 @@ from .enums import (
 from .ert_config import ErtConfig
 from .ert_run_context import RunContext
 from .model_config import ModelConfig
-from .observations import BlockDataConfig, GenObservation, ObsVector, SummaryObservation
+from .observations import GenObservation, ObsVector, SummaryObservation
 from .queue_config import QueueConfig
 from .row_scaling import RowScaling
 from .run_arg import RunArg
@@ -36,7 +36,6 @@ from .time_map import TimeMap
 __all__ = [
     "SummaryObservation",
     "GenObservation",
-    "BlockDataConfig",
     "ObsVector",
     "FieldTypeEnum",
     "GenKwConfig",
