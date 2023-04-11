70c61a170fbf5d57cfc6c3a8de0862b71de0d403
Øyvind Eide <oyveid@equinor.com>
2023-04-11 16:08:16 +0200

    Remove unused block_config

--- src/ert/_c_wrappers/enkf/observations/__init__.py
+++ src/ert/_c_wrappers/enkf/observations/__init__.py
@@ -1,4 +1,3 @@
-from .block_data_config import BlockDataConfig
 from .gen_observation import GenObservation
 from .obs_vector import ObsVector
 from .summary_observation import SummaryObservation
@@ -6,6 +5,5 @@ from .summary_observation import SummaryObservation
 __all__ = [
     "SummaryObservation",
     "GenObservation",
-    "BlockDataConfig",
     "ObsVector",
 ]
