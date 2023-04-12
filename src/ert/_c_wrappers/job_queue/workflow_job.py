dc6ae136a85d60ad025235c99605aabf4f80b463
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2023-04-12 13:48:56 +0200

    isort imports

--- src/ert/_c_wrappers/job_queue/workflow_job.py
+++ src/ert/_c_wrappers/job_queue/workflow_job.py
@@ -8,8 +8,8 @@ from typing import TYPE_CHECKING, Any, List, Optional, Type, Union
 from ert._c_wrappers.config import ConfigParser, ContentTypeEnum
 from ert._c_wrappers.job_queue import ErtScript, ExternalErtScript, FunctionErtScript
 from ert._clib.job_kw import type_from_kw
-
 from ert.parsing import ConfigValidationError
+
 from .ert_plugin import ErtPlugin
 
 if TYPE_CHECKING:
