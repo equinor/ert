bf4855dfad1a82e680cfafb980d40cefa576241a
Eivind Jahren <ejah@equinor.com>
2023-04-17 10:58:05 +0200

    Ensure observation keys are sorted
    Before the order of observations was determined by
    the ecl/hash datastructure. This made it very difficult to change.
    Now the order of observations should be sorted alphabetically before
    running analysis.
    
    Unfortunately, this effects the output of ert, breaking
    the behavior with respect to random seed.

--- tests/test_config_parsing/test_enkf_obs_parsing.py
+++ tests/test_config_parsing/test_enkf_obs_parsing.py
@@ -8,12 +8,14 @@ from .config_dict_generator import config_generators
 
 @pytest.mark.usefixtures("set_site_config")
 @given(config_generators())
-def test_that_creating_enkf_obs_from_generated_values_does_not_produce_errors(
-    tmp_path_factory, config_generator
-):
+def test_that_enkf_obs_keys_are_ordered(tmp_path_factory, config_generator):
     filename = "config.ert"
     with config_generator(tmp_path_factory, filename) as config_values:
-        observations = EnkfObs.from_ert_config(ErtConfig.from_file(filename))
+        ert_config = ErtConfig.from_file(filename)
+        observations = EnkfObs.from_ert_config(ert_config)
         assert observations.error == ""
         for o in config_values.observations:
             assert observations.hasKey(o.name)
+        assert sorted(set(o.name for o in config_values.observations)) == list(
+            observations.getMatchingKeys("*")
+        )
