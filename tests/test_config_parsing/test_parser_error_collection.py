39bfc0d8bda59b9e1717599a2c70d2fad74fb6b7
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2023-04-20 07:22:01 +0200

    Handle str in list of errors

--- tests/test_config_parsing/test_parser_error_collection.py
+++ tests/test_config_parsing/test_parser_error_collection.py
@@ -3,7 +3,7 @@ import os
 import stat
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Dict, List, Optional, TextIO, Union
+from typing import Dict, List, Optional
 
 import pytest
 
