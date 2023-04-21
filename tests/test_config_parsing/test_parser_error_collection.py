8419a571d9bca483e1594b4abd5ddaf4b0aaf7b3
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2023-04-21 08:48:35 +0200

    Address review

--- tests/test_config_parsing/test_parser_error_collection.py
+++ tests/test_config_parsing/test_parser_error_collection.py
@@ -1,14 +1,15 @@
-import itertools
 import os
+import re
 import stat
 from dataclasses import dataclass
-from pathlib import Path
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
 
 import pytest
+from hypothesis import given, strategies
 
 from ert._c_wrappers.enkf import ErtConfig
 from ert.parsing import ConfigValidationError
+from ert.parsing.lark_parser_error_info import ErrorInfo
 
 test_config_file_base = "test"
 test_config_filename = f"{test_config_file_base}.ert"
@@ -20,113 +21,133 @@ class FileDetail:
     is_executable: bool = False
 
 
-def write_files(files: Optional[Dict[str, FileDetail]] = None):
-    if files is not None:
-        for other_filename, content in files.items():
-            with open(other_filename, mode="w", encoding="utf-8") as fh:
-                fh.writelines(content.contents)
-
-            if not content.is_executable:
-                os.chmod(other_filename, stat.S_IREAD)
-
-
-def assert_that_config_leads_to_error(
-    config_file_contents: str,
-    expected_line: Optional[int],
-    expected_column: Optional[int],
-    expected_end_column: Optional[int],
-    expected_filename: str = "test.ert",
-    filename: str = "test.ert",
-    other_files: Optional[Dict[str, FileDetail]] = None,
-    match: str = None,
-):
-    Path(filename).write_text(config_file_contents)
-    write_files(files=other_files)
-
-    try:
-        ErtConfig.from_file(filename, use_new_parser=True)
-    except ConfigValidationError as err:
-        collected_errors = err.errors
-
-        # Find errors in matching file
-        errors_matching_filename = (
-            [err for err in collected_errors if expected_filename in err.filename]
-            if expected_filename is not None
-            else collected_errors
-        )
-
-        if expected_filename is not None:
-            assert (
-                len(errors_matching_filename) > 0
-            ), f"Expected minimum 1 error matching filename {expected_filename}, got 0"
-        else:
-            assert (
-                len(errors_matching_filename) > 0
-            ), "Expected minimum 1 collected error, got 0"
-
-    def equals_or_expected_any(actual, expected):
-        return True if expected is None else actual == expected
-
-    # Find errors matching location in file
-    errors_matching_location = [
-        x
-        for x in errors_matching_filename
-        if equals_or_expected_any(x.line, expected_line)
-        and equals_or_expected_any(x.column, expected_column)
-        and equals_or_expected_any(x.end_column, expected_end_column)
-    ]
-
-    assert len(errors_matching_location) > 0, (
-        f"Expected to find at least 1 error matching location"
-        f"(line={'*' if expected_line is None else expected_line},"
-        f"column={'*' if expected_column is None else expected_column},"
-        f"end_column{'*' if expected_end_column is None else expected_end_column})"
-    )
-
-    if match is not None:
-        with pytest.raises(ConfigValidationError, match=match):
-            raise ConfigValidationError(errors_matching_location)
-
-
 @dataclass
 class ExpectedErrorInfo:
-    expected_line: Optional[int] = None
-    expected_column: Optional[int] = None
-    expected_end_column: Optional[int] = None
-    expected_filename: str = "test.ert"
     filename: str = "test.ert"
+    line: Optional[int] = None
+    column: Optional[int] = None
+    end_column: Optional[int] = None
     other_files: Optional[Dict[str, FileDetail]] = None
     match: Optional[str] = None
 
 
-def assert_multiple_errors_from_config_with(
-    contents: str,
-    expected_errors: List[ExpectedErrorInfo],
+def write_files(files: Optional[Dict[str, Union[str, FileDetail]]] = None):
+    if files is not None:
+        for other_filename, content in files.items():
+            with open(other_filename, mode="w", encoding="utf-8") as fh:
+                if isinstance(content, FileDetail):
+                    fh.writelines(content.contents)
+
+                    if not content.is_executable:
+                        os.chmod(other_filename, stat.S_IREAD)
+                else:
+                    fh.write(content)
+
+
+def find_and_assert_errors_matching_filename(
+    errors: List[ErrorInfo], filename: Optional[str]
 ):
-    for info in expected_errors:
-        assert_that_config_leads_to_error(
-            config_file_contents=contents,
-            expected_line=info.expected_line,
-            expected_column=info.expected_column,
-            expected_end_column=info.expected_end_column,
-            expected_filename=info.expected_filename,
-            filename=info.filename,
-            other_files=info.other_files,
-            match=info.match,
+    matching_errors = (
+        [err for err in errors if filename in err.filename]
+        if filename is not None
+        else errors
+    )
+
+    assert len(matching_errors) > 0, (
+        f"Expected minimum 1 error matching filename" f" {filename}, got 0"
+    )
+
+    return matching_errors
+
+
+def find_and_assert_errors_matching_location(
+    errors: List[ErrorInfo],
+    line: Optional[int] = None,
+    column: Optional[int] = None,
+    end_column: Optional[int] = None,
+):
+    def equals_or_expected_any(actual, expected):
+        return True if expected is None else actual == expected
+
+    matching_errors = [
+        x
+        for x in errors
+        if equals_or_expected_any(x.line, line)
+        and equals_or_expected_any(x.column, column)
+        and equals_or_expected_any(x.end_column, end_column)
+    ]
+
+    def none_to_star(val: Optional[int] = None):
+        return "*" if val is None else val
+
+    assert len(matching_errors) > 0, (
+        "Expected to find at least 1 error matching location"
+        f"(line={none_to_star(line)},"
+        f"column={none_to_star(column)},"
+        f"end_column{none_to_star(end_column)})"
+    )
+
+    return matching_errors
+
+
+def find_and_assert_errors_matching_message(
+    errors: List[ErrorInfo], match: Optional[str] = None
+):
+    if match is None:
+        return errors
+
+    re_match = re.compile(match)
+    matching_errors = [err for err in errors if re.search(re_match, err.message)]
+
+    assert len(matching_errors) > 0, f"Expected to find error matching message {match}"
+
+    return matching_errors
+
+
+def assert_that_config_leads_to_error(
+    config_file_contents: str,
+    expected_error: ExpectedErrorInfo,
+    config_filename: str = "test.ert",
+):
+    write_files(
+        {config_filename: config_file_contents, **(expected_error.other_files or {})}
+    )
+
+    try:
+        ErtConfig.from_file(expected_error.filename, use_new_parser=True)
+        assert False, "Parsing did not result in any errors"
+    except ConfigValidationError as err:
+        collected_errors = err.errors
+
+        # Find errors in matching file
+        errors_matching_filename = find_and_assert_errors_matching_filename(
+            errors=collected_errors, filename=expected_error.filename
+        )
+
+        errors_matching_location = find_and_assert_errors_matching_location(
+            errors=errors_matching_filename,
+            line=expected_error.line,
+            column=expected_error.column,
+            end_column=expected_error.end_column,
+        )
+
+        find_and_assert_errors_matching_message(
+            errors=errors_matching_location, match=expected_error.match
         )
 
 
-# "-4 -5" is parsed as the final arg, intended behavior?...
 @pytest.mark.usefixtures("use_tmpdir")
-def test_that_disallowed_argument_is_located():
+def test_that_disallowed_argument_is_located_1fn():
     assert_that_config_leads_to_error(
         config_file_contents="""
 QUEUE_OPTION DOCAL MAX_RUNNING 4
-        """,
-        expected_line=2,
-        expected_column=14,
-        expected_end_column=19,
-        match="argument .* must be one of",
+""",
+        expected_error=ExpectedErrorInfo(
+            line=2,
+            column=14,
+            end_column=19,
+            match="argument .* must be one of",
+        ),
     )
 
 
@@ -147,39 +168,39 @@ NUM_REALIZATIONS
 """,
             [
                 ExpectedErrorInfo(
-                    expected_line=2,
-                    expected_column=14,
-                    expected_end_column=19,
+                    line=2,
+                    column=14,
+                    end_column=19,
                     match="argument .* must be one of",
                 ),
                 ExpectedErrorInfo(
-                    expected_line=3,
-                    expected_column=19,
-                    expected_end_column=24,
+                    line=3,
+                    column=19,
+                    end_column=24,
                     match="must have a boolean value",
                 ),
                 ExpectedErrorInfo(
-                    expected_line=4,
-                    expected_column=18,
-                    expected_end_column=25,
+                    line=4,
+                    column=18,
+                    end_column=25,
                     match="must have an integer value",
                 ),
                 ExpectedErrorInfo(
-                    expected_line=5,
-                    expected_column=12,
-                    expected_end_column=21,
+                    line=5,
+                    column=12,
+                    end_column=21,
                     match="must have a number",
                 ),
                 ExpectedErrorInfo(
-                    expected_line=6,
-                    expected_column=14,
-                    expected_end_column=46,
+                    line=6,
+                    column=14,
+                    end_column=46,
                     match="Cannot find file or directory",
                 ),
                 ExpectedErrorInfo(
-                    expected_line=7,
-                    expected_column=12,
-                    expected_end_column=41,
+                    line=7,
+                    column=12,
+                    end_column=41,
                     match="Could not find executable",
                 ),
                 ExpectedErrorInfo(
@@ -188,21 +209,21 @@ NUM_REALIZATIONS
                             contents="", is_executable=False
                         )
                     },
-                    expected_line=8,
-                    expected_column=12,
-                    expected_end_column=31,
+                    line=8,
+                    column=12,
+                    end_column=31,
                     match="File not executable",
                 ),
                 ExpectedErrorInfo(
-                    expected_line=9,
-                    expected_column=1,
-                    expected_end_column=17,
+                    line=9,
+                    column=1,
+                    end_column=17,
                     match="must have maximum",
                 ),
                 ExpectedErrorInfo(
-                    expected_line=10,
-                    expected_column=1,
-                    expected_end_column=17,
+                    line=10,
+                    column=1,
+                    end_column=17,
                     match="must have at least",
                 ),
             ],
@@ -211,16 +232,16 @@ NUM_REALIZATIONS
 )
 @pytest.mark.usefixtures("use_tmpdir")
 def test_that_multiple_keyword_specific_tokens_are_located(contents, expected_errors):
-    assert_multiple_errors_from_config_with(
-        contents=contents,
-        expected_errors=expected_errors,
-    )
+    for expected_error in expected_errors:
+        assert_that_config_leads_to_error(
+            config_file_contents=contents, expected_error=expected_error
+        )
 
 
-@pytest.mark.parametrize(
-    "lines, num_permutations",
-    [
-        (
+@pytest.mark.usefixtures("use_tmpdir")
+@given(
+    strategies.lists(
+        strategies.sampled_from(
             [
                 ("QUEUE_OPTION DOCAL MAX_RUNNING 4", {"column": 14, "end_column": 19}),
                 ("STOP_LONG_RUNNING flase", {"column": 19, "end_column": 24}),
@@ -236,32 +257,24 @@ def test_that_multiple_keyword_specific_tokens_are_located(contents, expected_er
                 ),
                 ("NUM_REALIZATIONS 1 2 3 4 5", {"column": 1, "end_column": 17}),
                 ("NUM_REALIZATIONS", {"column": 1, "end_column": 17}),
-            ],
-            30,
-        )
-    ],
+            ]
+        ),
+        min_size=1,
+        max_size=10,
+    )
 )
-@pytest.mark.usefixtures("use_tmpdir")
-def test_that_multiple_keyword_specific_tokens_are_located_shuffle(
-    lines, num_permutations
-):
-    def run_permutation(permutation):
-        contents = "\n".join([line[0] for line in permutation])
-        expected_errors = [
-            ExpectedErrorInfo(
-                expected_line=(i + 1),
-                expected_column=line[1]["column"],
-                expected_end_column=line[1]["end_column"],
-            )
-            for i, line in enumerate(permutation)
-        ]
-
-        assert_multiple_errors_from_config_with(
-            contents=contents,
-            expected_errors=expected_errors,
+def test_that_multiple_keyword_specific_tokens_are_located_shuffle(error_lines):
+    contents = "\n".join([line[0] for line in error_lines])
+    expected_errors = [
+        ExpectedErrorInfo(
+            line=(i + 1),
+            column=line[1]["column"],
+            end_column=line[1]["end_column"],
         )
+        for i, line in enumerate(error_lines)
+    ]
 
-    perms = itertools.islice(itertools.permutations(lines), num_permutations)
-
-    for line_perm in perms:
-        run_permutation(line_perm)
+    for err in expected_errors:
+        assert_that_config_leads_to_error(
+            config_file_contents=contents, expected_error=err
+        )
