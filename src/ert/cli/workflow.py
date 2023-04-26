fc1f88efd2f1058a153cb5c6e41faee342a95008
Eivind Jahren <ejah@equinor.com>
2023-04-26 14:55:22 +0200

    Remove unused parameter ensemble to workflow

--- src/ert/cli/workflow.py
+++ src/ert/cli/workflow.py
@@ -1,7 +1,7 @@
 import logging
 
 
-def execute_workflow(ert, storage, workflow_name, ensemble=None):
+def execute_workflow(ert, storage, workflow_name):
     logger = logging.getLogger(__name__)
     try:
         workflow = ert.resConfig().workflows[workflow_name]
@@ -9,7 +9,6 @@ def execute_workflow(ert, storage, workflow_name, ensemble=None):
         msg = "Workflow {} is not in the list of available workflows"
         logger.error(msg.format(workflow_name))
         return
-    workflow.ensemble = ensemble
     workflow.run(ert=ert, storage=storage)
     if not all(v["completed"] for v in workflow.getJobsReport().values()):
         logger.error(f"Workflow {workflow_name} failed!")
