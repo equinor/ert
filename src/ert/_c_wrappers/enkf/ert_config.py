4e549e9ed8a2ba73dda96e1958c433838108b3d6
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2023-04-27 11:59:02 +0200

    Address oopsie

--- src/ert/_c_wrappers/enkf/ert_config.py
+++ src/ert/_c_wrappers/enkf/ert_config.py
@@ -28,8 +28,8 @@ from ert._c_wrappers.util import SubstitutionList
 from ert._clib import job_kw
 from ert._clib.config_keywords import init_site_config_parser, init_user_config_parser
 from ert.parsing import ConfigValidationError, ConfigWarning, lark_parse
+from ert.parsing.error_info import ErrorInfo
 
-from ...parsing.error_info import ErrorInfo
 from ._config_content_as_dict import config_content_as_dict
 from ._deprecation_migration_suggester import DeprecationMigrationSuggester
 
