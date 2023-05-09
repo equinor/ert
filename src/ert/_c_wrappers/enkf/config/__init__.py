227579b2421acc3773d76b6aab3b0d5ac040099e
Eivind Jahren <ejah@equinor.com>
2023-05-09 09:21:34 +0200

    Remove unused FieldTypeEnum

--- src/ert/_c_wrappers/enkf/config/__init__.py
+++ src/ert/_c_wrappers/enkf/config/__init__.py
@@ -1,12 +1,10 @@
 from .enkf_config_node import EnkfConfigNode
 from .ext_param_config import ExtParamConfig
-from .field_type_enum import FieldTypeEnum
 from .gen_data_config import GenDataConfig
 from .gen_kw_config import GenKwConfig
 from .summary_config import SummaryConfig
 
 __all__ = [
-    "FieldTypeEnum",
     "GenKwConfig",
     "GenDataConfig",
     "EnkfConfigNode",
