67222ffe5126e87ad114ff053a5815ccf2e6038b
Eivind Jahren <ejah@equinor.com>
2023-05-09 09:25:21 +0200

    Remove unused enum FieldFileFormatType

--- src/ert/_c_wrappers/enkf/enums/__init__.py
+++ src/ert/_c_wrappers/enkf/enums/__init__.py
@@ -3,7 +3,6 @@ from .enkf_field_file_format_enum import EnkfFieldFileFormatEnum
 from .enkf_obs_impl_type_enum import EnkfObservationImplementationType
 from .enkf_var_type_enum import EnkfVarType
 from .ert_impl_type_enum import ErtImplType
-from .field_file_format_type_enum import FieldFileFormatType
 from .gen_data_file_type_enum import GenDataFileType
 from .hook_runtime_enum import HookRuntime
 from .realization_state_enum import RealizationStateEnum
@@ -13,7 +12,6 @@ __all__ = [
     "EnkfVarType",
     "EnkfObservationImplementationType",
     "ErtImplType",
-    "FieldFileFormatType",
     "RealizationStateEnum",
     "GenDataFileType",
     "ActiveMode",
