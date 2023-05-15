7f890475dc1159f81004131935962ef260e436c8
Andreas Eknes Lie <andrli@equinor.com>
2023-05-15 22:30:25 +0200

    Add summary tests

---
+++ tests/unit_tests/c_wrappers/res/enkf/config/test_summary_config.py
@@ -0,0 +1,13 @@
+import pytest
+
+from ert._c_wrappers.enkf import ErtImplType, SummaryConfig
+
+
+@pytest.mark.usefixtures("use_tmpdir")
+def test_summary_config():
+    summary_config = SummaryConfig(key="ALT1")
+    obs_list = ["ABC", "GHI", "DEF"]
+    assert summary_config.getObservationKeys() == []
+    summary_config.update_observation_keys(obs_list)
+    assert summary_config.getObservationKeys() == sorted(obs_list)
+    assert summary_config.getImplementationType() == ErtImplType.SUMMARY
