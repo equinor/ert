544ebbbe3c24ef7cff876534c4d18feb8db34766
Eivind Jahren <ejah@equinor.com>
2023-05-25 09:14:15 +0200

    Make summary obs python only

--- src/ert/_c_wrappers/enkf/observations/summary_observation.py
+++ src/ert/_c_wrappers/enkf/observations/summary_observation.py
@@ -1,72 +1,37 @@
-from cwrap import BaseCClass
+from dataclasses import dataclass
 
-from ert import _clib
-from ert._c_wrappers import ResPrototype
 from ert._c_wrappers.enkf.active_list import ActiveList
+from ert._c_wrappers.enkf.enums import ActiveMode
 
 
-class SummaryObservation(BaseCClass):
-    TYPE_NAME = "summary_obs"
-
-    _alloc = ResPrototype(
-        "void*  summary_obs_alloc(char*, char*, double, double)",
-        bind=False,
-    )
-    _free = ResPrototype("void   summary_obs_free(summary_obs)")
-    _get_value = ResPrototype("double summary_obs_get_value(summary_obs)")
-    _get_std = ResPrototype("double summary_obs_get_std(summary_obs)")
-    _get_std_scaling = ResPrototype("double summary_obs_get_std_scaling(summary_obs)")
-    _get_summary_key = ResPrototype("char*  summary_obs_get_summary_key(summary_obs)")
-    _set_std_scale = ResPrototype(
-        "void   summary_obs_set_std_scale(summary_obs , double)"
-    )
-
-    def __init__(self, summary_key, observation_key, value, std):
-        assert isinstance(summary_key, str)
-        assert isinstance(observation_key, str)
-        assert isinstance(value, float)
-        assert isinstance(std, float)
-
-        c_ptr = self._alloc(summary_key, observation_key, value, std)
-        if c_ptr:
-            super().__init__(c_ptr)
-        else:
-            raise ValueError(
-                "Unable to construct SummaryObservation with given configuration!"
-            )
+@dataclass
+class SummaryObservation:
+    summary_key: str
+    observation_key: str
+    value: float
+    std: float
+    std_scaling: float = 1.0
 
     def getValue(self) -> float:
-        return self._get_value()
+        return self.value
 
     def getStandardDeviation(self) -> float:
-        return self._get_std()
+        return self.std
 
     def getStdScaling(self, index=0) -> float:
-        return self._get_std_scaling()
+        return self.std_scaling
 
     def set_std_scaling(self, scaling_factor: float) -> None:
-        self._set_std_scale(scaling_factor)
+        self.std_scaling = scaling_factor
 
     def __len__(self):
         return 1
 
     def getSummaryKey(self) -> str:
-        return self._get_summary_key()
+        return self.summary_key
 
     def updateStdScaling(self, factor: float, active_list: ActiveList) -> None:
-        _clib.local.summary_obs.update_std_scaling(self, factor, active_list)
-
-    def free(self):
-        self._free()
-
-    def __repr__(self):
-        sk = self.getSummaryKey()
-        va = self.getValue()
-        sd = self.getStandardDeviation()
-        sc = self.getStdScaling()
-        ad = self._address()
-        fmt = (
-            "SummaryObservation(key = %s, "
-            "value = %f, std = %f, std_scaling = %f) at 0x%x"
-        )
-        return fmt % (sk, va, sd, sc, ad)
+        if active_list.getMode() == ActiveMode.ALL_ACTIVE:
+            self.std_scaling = factor
+        elif active_list.getActiveSize() > 0:
+            self.std_scaling = factor
