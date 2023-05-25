8296784e09e40bab3a252be33348f9dd0cb3ff9f
Eivind Jahren <ejah@equinor.com>
2023-05-25 10:25:28 +0200

    Make gen_obs python only

--- src/ert/_c_wrappers/enkf/observations/gen_observation.py
+++ src/ert/_c_wrappers/enkf/observations/gen_observation.py
@@ -1,57 +1,23 @@
-import ctypes
 import os.path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, Tuple
 
 import numpy as np
-from cwrap import BaseCClass
 from ecl.util.util import IntVector
 
-from ert import _clib
-from ert._c_wrappers import ResPrototype
 from ert._c_wrappers.enkf import ActiveList
-from ert._c_wrappers.enkf.config import GenDataConfig
+from ert._c_wrappers.enkf.enums import ActiveMode
 
 if TYPE_CHECKING:
     import numpy.typing as npt
 
 
-class GenObservation(BaseCClass):
-    TYPE_NAME = "gen_obs"
-
-    _alloc = ResPrototype("void*  gen_obs_alloc__(char*)", bind=False)
-    _free = ResPrototype("void   gen_obs_free(gen_obs)")
-    _load = ResPrototype("void   gen_obs_load_observation(gen_obs , char*)")
-    _scalar_set = ResPrototype("void   gen_obs_set_scalar(gen_obs , double , double)")
-    _get_std = ResPrototype("double gen_obs_iget_std(gen_obs, int)")
-    _get_value = ResPrototype("double gen_obs_iget_value(gen_obs, int)")
-    _get_std_scaling = ResPrototype("double gen_obs_iget_std_scaling(gen_obs, int)")
-    _get_size = ResPrototype("int    gen_obs_get_size(gen_obs)")
-    _get_data_index = ResPrototype("int    gen_obs_get_obs_index(gen_obs, int)")
-    _load_data_index = ResPrototype("void   gen_obs_load_data_index(gen_obs , char*)")
-    _add_data_index = ResPrototype(
-        "void   gen_obs_attach_data_index(gen_obs , int_vector)"
-    )
-    _get_value_vector = ResPrototype(
-        "void   gen_obs_load_values(gen_obs, int, double*)"
-    )
-    _get_std_vector = ResPrototype("void   gen_obs_load_std(gen_obs, int, double*)")
-
+class GenObservation:
     def __init__(
         self,
-        obs_key,
-        data_config: GenDataConfig,
         scalar_value=None,
-        obs_file=None,
-        data_index=None,
+        obs_file: Optional[str] = None,
+        data_index: Optional[str] = None,
     ):
-        c_ptr = self._alloc(obs_key)
-        if c_ptr:
-            super().__init__(c_ptr)
-        else:
-            raise ValueError(
-                "Unable to construct GenObservation with given obs_key and data_config!"
-            )
-
         if scalar_value is None and obs_file is None:
             raise ValueError(
                 "Exactly one the scalar_value and obs_file arguments must be present"
@@ -63,59 +29,58 @@ class GenObservation(BaseCClass):
             )
 
         if obs_file is not None:
-            if not os.path.isfile(obs_file):
-                raise IOError(
-                    f"The file with observation data:{obs_file} does not exist"
+            values = np.loadtxt(obs_file, delimiter=None).ravel()
+            if len(values) % 2 != 0:
+                raise ValueError(
+                    "Expected even number of values in GENERAL_OBSERVATION"
                 )
-            self._load(obs_file)
+            self.values = values[::2]
+            self.stds = values[1::2]
+
         else:
             obs_value, obs_std = scalar_value
-            self._scalar_set(obs_value, obs_std)
+            self.values = np.array([obs_value])
+            self.stds = np.array([obs_std])
 
         if data_index is not None:
+            self.indices = []
             if os.path.isfile(data_index):
-                self._load_data_index(data_index)
+                self.indices = np.loadtxt(data_index, delimiter=None).ravel()
             else:
-                index_list = IntVector.active_list(data_index)
-                self._add_data_index(index_list)
+                self.indices = np.array(
+                    IntVector.active_list(data_index), dtype=np.int32
+                )
+        else:
+            self.indices = np.arange(len(self.values))
+        self.std_scaling = [1.0] * len(self.values)
 
     def __len__(self):
-        return self._get_size()
+        return len(self.values)
 
-    def __getitem__(self, obs_index):
-        if obs_index < 0:
-            obs_index += len(self)
-
-        if 0 <= obs_index < len(self):
-            return (self.getValue(obs_index), self.getStandardDeviation(obs_index))
-        else:
-            raise IndexError(f"Invalid index.  Valid range: [0,{len(self)})")
+    def __getitem__(self, obs_index: int) -> Tuple[float, float]:
+        return (self.values[obs_index], self.stds[obs_index])
 
     def getValue(self, obs_index: int) -> float:
-        return self._get_value(obs_index)
+        return self.values[obs_index]
 
     def getStandardDeviation(self, obs_index: int) -> float:
-        return self._get_std(obs_index)
+        return self.stds[obs_index]
 
     def getStdScaling(self, obs_index: int) -> float:
-        return self._get_std_scaling(obs_index)
+        return self.std_scaling[obs_index]
 
-    def updateStdScaling(self, factor, active_list: ActiveList):
-        _clib.local.gen_obs.update_std_scaling(self, factor, active_list)
+    def updateStdScaling(self, factor: float, active_list: ActiveList) -> None:
+        if active_list.getMode() == ActiveMode.ALL_ACTIVE:
+            self.std_scaling = [factor] * len(self.values)
+        else:
+            for i in active_list.get_active_index_list():
+                self.std_scaling[i] = factor
 
     def get_data_points(self) -> "npt.NDArray[np.double]":
-        np_vector = np.zeros(len(self))
-        self._get_value_vector(
-            len(self), np_vector.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-        )
-        return np_vector
+        return self.values
 
     def get_std(self) -> "npt.NDArray[np.double]":
-        np_vector = np.zeros(len(self))
-        self._get_std_vector(
-            len(self), np_vector.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-        )
-        return np_vector
+        return self.stds
 
     def getSize(self) -> int:
         return len(self)
@@ -123,11 +88,5 @@ class GenObservation(BaseCClass):
     def getIndex(self, obs_index) -> int:
         return self.getDataIndex(obs_index)
 
-    def getDataIndex(self, obs_index):
-        return self._get_data_index(obs_index)
-
-    def free(self):
-        self._free()
-
-    def __repr__(self):
-        return f"GenObservation(size = {len(self)}) {self._ad_str()}"
+    def getDataIndex(self, obs_index: int) -> int:
+        return self.indices[obs_index]
