c6df2aff55ee755301996fabdb1613087e4c2f4d
Eivind Jahren <ejah@equinor.com>
2023-05-26 08:20:38 +0200

    Completely simplify GenObservation

--- src/ert/_c_wrappers/enkf/observations/gen_observation.py
+++ src/ert/_c_wrappers/enkf/observations/gen_observation.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Tuple
 
 import numpy as np
 import numpy.typing as npt
@@ -26,21 +25,9 @@ class GenObservation:
     def __len__(self):
         return len(self.values)
 
-    def __getitem__(self, obs_index: int) -> Tuple[float, float]:
-        return (self.values[obs_index], self.stds[obs_index])
-
-    def getStdScaling(self, obs_index: int) -> float:
-        return self.std_scaling[obs_index]
-
     def updateStdScaling(self, factor: float, active_list: ActiveList) -> None:
         if active_list.getMode() == ActiveMode.ALL_ACTIVE:
             self.std_scaling = np.full(len(self.values), factor)
         else:
             for i in active_list.get_active_index_list():
                 self.std_scaling[i] = factor
-
-    def getIndex(self, obs_index) -> int:
-        return self.getDataIndex(obs_index)
-
-    def getDataIndex(self, obs_index: int) -> int:
-        return self.indices[obs_index]
