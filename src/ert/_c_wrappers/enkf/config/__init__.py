30309d00dce126a48f21df114c5b61871de47d2f
Øyvind Eide <oyveid@equinor.com>
2023-06-14 09:17:16 +0200

    Add from dict for parameters

--- src/ert/_c_wrappers/enkf/config/__init__.py
+++ src/ert/_c_wrappers/enkf/config/__init__.py
@@ -1,11 +1,15 @@
 from .ext_param_config import ExtParamConfig
+from .field_config import Field
 from .gen_data_config import GenDataConfig
 from .gen_kw_config import GenKwConfig
 from .summary_config import SummaryConfig
+from .surface_config import SurfaceConfig
 
 __all__ = [
     "GenKwConfig",
     "GenDataConfig",
     "SummaryConfig",
     "ExtParamConfig",
+    "SurfaceConfig",
+    "Field",
 ]
