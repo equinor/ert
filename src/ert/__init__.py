8914e668a5855abe9a93f7a7b00fd906bcbb76e8
Eivind Jahren <ejah@equinor.com>
2023-06-28 12:16:57 +0200

    Export job status type

--- src/ert/__init__.py
+++ src/ert/__init__.py
@@ -1,6 +1,12 @@
 from .data import MeasuredData
-from .job_queue import ErtScript
+from .job_queue import ErtScript, JobStatusType
 from .libres_facade import LibresFacade
 from .simulator import BatchSimulator
 
-__all__ = ["MeasuredData", "LibresFacade", "BatchSimulator", "ErtScript"]
+__all__ = [
+    "MeasuredData",
+    "LibresFacade",
+    "BatchSimulator",
+    "ErtScript",
+    "JobStatusType",
+]
