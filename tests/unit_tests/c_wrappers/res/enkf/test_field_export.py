574257c45766aca681fd14a468b0fda15e25f08b
Øyvind Eide <eide.oyvind87@gmail.com>
2023-07-02 13:34:27 +0200

    Inline export_field

--- tests/unit_tests/c_wrappers/res/enkf/test_field_export.py
+++ tests/unit_tests/c_wrappers/res/enkf/test_field_export.py
@@ -1,8 +1,7 @@
 import os
+from pathlib import Path
 
 import pytest
-
-from ert._c_wrappers.enkf.config.field_config import export_field
 
 
 def test_field_basics(snake_oil_field_example):
@@ -28,66 +27,16 @@ def test_field_export(snake_oil_field_example, storage):
 
     prior = ert.ensemble_context(prior_ensemble, [True, False, False, True, True], 0)
     ert.sample_prior(prior.sim_fs, prior.active_realizations)
-    ert.createRunPath(prior)
     ens_config = ert.ensembleConfig()
     config_node = ens_config["PERMX"]
 
-    param_info = prior_ensemble.experiment.parameter_info[config_node.name]
-    export_field(
-        config_node.name,
-        0,
-        "export/with/path/PERMX_0.grdecl",
-        param_info,
-        prior_ensemble.mount_point,
-        prior_ensemble.experiment.grid_path,
-    )
-    assert os.path.isfile("export/with/path/PERMX_0.grdecl")
-    assert os.path.getsize("export/with/path/PERMX_0.grdecl") > 0
-
-    with pytest.raises(
-        KeyError, match="Unable to load FIELD for key: PERMX, realization: 1"
-    ):
-        export_field(
-            config_node.name,
-            1,
-            "export/with/path/PERMX_1.grdecl",
-            param_info,
-            prior_ensemble.mount_point,
-            prior_ensemble.experiment.grid_path,
-        )
-    assert not os.path.isfile("export/with/path/PERMX_1.grdecl")
-
-    with pytest.raises(
-        KeyError, match="Unable to load FIELD for key: PERMX, realization: 2"
-    ):
-        export_field(
-            config_node.name,
-            2,
-            "export/with/path/PERMX_2.grdecl",
-            param_info,
-            prior_ensemble.mount_point,
-            prior_ensemble.experiment.grid_path,
-        )
-    assert not os.path.isfile("export/with/path/PERMX_2.grdecl")
-
-    export_field(
-        config_node.name,
-        3,
-        "export/with/path/PERMX_3.grdecl",
-        param_info,
-        prior_ensemble.mount_point,
-        prior_ensemble.experiment.grid_path,
-    )
-    assert os.path.isfile("export/with/path/PERMX_3.grdecl")
-    assert os.path.getsize("export/with/path/PERMX_3.grdecl") > 0
-
-    export_field(
-        config_node.name,
-        4,
-        "export/with/path/PERMX_4.grdecl",
-        param_info,
-        prior_ensemble.mount_point,
-        prior_ensemble.experiment.grid_path,
-    )
-    assert os.path.isfile("export/with/path/PERMX_4.grdecl")
-    assert os.path.getsize("export/with/path/PERMX_4.grdecl") > 0
+    for real in [0, 3, 4]:
+        config_node.save(Path(f"export/with/path/{real}"), real, prior_ensemble)
+        assert os.path.isfile(f"export/with/path/{real}/permx.grdecl")
+        assert os.path.getsize(f"export/with/path/{real}/permx.grdecl") > 0
+    for real in [1, 2]:
+        with pytest.raises(
+            KeyError, match=f"Unable to load FIELD for key: PERMX, realization: {real}"
+        ):
+            config_node.save(Path(f"export/with/path/{real}"), real, prior_ensemble)
+        assert not os.path.isfile(f"export/with/path/{real}/permx.grdecl")
