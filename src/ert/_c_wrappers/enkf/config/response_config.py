b99cb6cbceeb0411001107822291eba9771c69b4
Øyvind Eide <eide.oyvind87@gmail.com>
2023-07-04 13:34:06 +0200

    Add abstractmethod for response

--- src/ert/_c_wrappers/enkf/config/response_config.py
+++ src/ert/_c_wrappers/enkf/config/response_config.py
@@ -1,7 +1,13 @@
 import dataclasses
-from abc import ABC
+from abc import ABC, abstractmethod
+
+import xarray as xr
 
 
 @dataclasses.dataclass
 class ResponseConfig(ABC):
     name: str
+
+    @abstractmethod
+    def read_from_file(self, run_path: str, iens: int) -> xr.Dataset:
+        ...
