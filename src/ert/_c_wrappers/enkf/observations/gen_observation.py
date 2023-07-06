00b2b3afefc5001c9267ae47e30331f16bce0404
Zohar Malamant <ZOM@equinor.com>
2023-07-06 17:33:02 +0200

    Remove ActiveList
    The ActiveList concept has been replaced by a list/set of integers that
    may be optional. If None, then it is assumed that all indices are
    active. If not-None, then only the integers in the container are active.

--- src/ert/_c_wrappers/enkf/observations/gen_observation.py
+++ src/ert/_c_wrappers/enkf/observations/gen_observation.py
@@ -1,10 +1,9 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 import numpy as np
 import numpy.typing as npt
-
-from ert._c_wrappers.enkf import ActiveList
-from ert._c_wrappers.enkf.enums import ActiveMode
 
 
 @dataclass(eq=False)
@@ -24,10 +23,3 @@ class GenObservation:
 
     def __len__(self):
         return len(self.values)
-
-    def updateStdScaling(self, factor: float, active_list: ActiveList) -> None:
-        if active_list.getMode() == ActiveMode.ALL_ACTIVE:
-            self.std_scaling = np.full(len(self.values), factor)
-        else:
-            for i in active_list.get_active_index_list():
-                self.std_scaling[i] = factor
