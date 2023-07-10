325c7098d837ac38d000e0185191b414e3ed0fa8
DanSava <dan.sava42@gmail.com>
2023-07-10 15:20:02 +0300

    Fix testing SummaryConfing equality when no refcase is defined.

--- tests/unit_tests/c_wrappers/res/enkf/config/test_summary_config.py
+++ tests/unit_tests/c_wrappers/res/enkf/config/test_summary_config.py
@@ -1,9 +1,22 @@
-import pytest
-
 from ert._c_wrappers.enkf import SummaryConfig
 
 
-@pytest.mark.usefixtures("use_tmpdir")
 def test_summary_config():
     summary_config = SummaryConfig(name="ALT1", input_file="ECLBASE", keys=[])
     assert summary_config.name == "ALT1"
+
+
+def test_summary_config_equal():
+    summary_config = SummaryConfig(name="n", input_file="file", keys=[])
+
+    new_summary_config = SummaryConfig(name="n", input_file="file", keys=[])
+    assert summary_config == new_summary_config
+
+    new_summary_config = SummaryConfig(name="different", input_file="file", keys=[])
+    assert summary_config != new_summary_config
+
+    new_summary_config = SummaryConfig(name="n", input_file="different", keys=[])
+    assert summary_config != new_summary_config
+
+    new_summary_config = SummaryConfig(name="n", input_file="file", keys=["some_key"])
+    assert summary_config != new_summary_config
