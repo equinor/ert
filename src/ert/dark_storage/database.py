fe5ec4081b0d0056c278a0bbf4e154ffa330a66f
Håvard Berland <havb@equinor.com>
2023-07-11 07:29:38 +0200

    Pin pydantic<2 and fastapi

--- src/ert/dark_storage/database.py
+++ src/ert/dark_storage/database.py
@@ -1,10 +1,11 @@
 import os
-from typing import Any
+from typing import Any, Callable
 
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import Session as SessionType
 from sqlalchemy.sql import text
 
 from ert_storage.security import security
@@ -31,7 +32,7 @@ if IS_SQLITE:
     engine = create_engine(URI_RDBMS, connect_args={"check_same_thread": False})
 else:
     engine = create_engine(URI_RDBMS, pool_size=50, max_overflow=100)
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session: Callable = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 Base = declarative_base()
