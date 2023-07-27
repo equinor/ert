dc8ed461aac080830d8647d263b0b65194de5f38
Eivind Jahren <ejah@equinor.com>
2023-07-27 15:32:44 +0200

    add type_hints to analysis_config

--- src/ert/_c_wrappers/enkf/analysis_iter_config.py
+++ src/ert/_c_wrappers/enkf/analysis_iter_config.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from typing import Any, Dict, Optional
+from typing import Optional, no_type_check
 
-from ert.parsing import ConfigKeys
+from ert.parsing import ConfigDict, ConfigKeys
 
 
 @dataclass
@@ -10,9 +10,10 @@ class AnalysisIterConfig:
     iter_count: int = 4
     iter_retry_count: int = 4
 
+    @no_type_check
     @classmethod
-    def from_dict(cls, config_dict: Dict[str, Any]) -> "AnalysisIterConfig":
-        iter_case = config_dict.get(ConfigKeys.ITER_CASE, None)
-        iter_count = config_dict.get(ConfigKeys.ITER_COUNT, 4)
-        iter_retry_count = config_dict.get(ConfigKeys.ITER_RETRY_COUNT, 4)
+    def from_dict(cls, config_dict: ConfigDict) -> "AnalysisIterConfig":
+        iter_case: Optional[str] = config_dict.get(ConfigKeys.ITER_CASE)
+        iter_count: int = config_dict.get(ConfigKeys.ITER_COUNT, 4)
+        iter_retry_count: int = config_dict.get(ConfigKeys.ITER_RETRY_COUNT, 4)
         return cls(iter_case, iter_count, iter_retry_count)
