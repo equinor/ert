336af840e1dcc02658591ca12ac0887b82f1193b
Valentin Krasontovitsch <valentin.krasontovitsch@webstep.no>
2023-07-31 22:27:17 +0200

    Rewrite gui snapshot handling
    After the rewrite of the ensemble evaluator's snapshot data structure,
    we were forced to also rewrite the gui's usage of said structure, to
    avoid expensive calls and make the processing overall faster.
    
    To this end, we extend the snapshot's API to provide convenience
    functions rather than expose the internal structure.
    
    Co-authored-by: Håvard Berland <havb@equinor.com>

--- src/ert/ensemble_evaluator/evaluator_tracker.py
+++ src/ert/ensemble_evaluator/evaluator_tracker.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib
 import copy
 import logging
 import queue
@@ -122,16 +123,14 @@ class EvaluatorTracker:
         while True:
             event = self._work_queue.get()
             if isinstance(event, str):
-                try:
+                with contextlib.suppress(GeneratorExit):
+                    # consumers may exit at this point, make sure the last
+                    # task is marked as done
                     if event == EvaluatorTracker.DONE:
                         yield EndEvent(
                             failed=self._model.hasRunFailed(),
                             failed_msg=self._model.getFailMessage(),
                         )
-                except GeneratorExit:
-                    # consumers may exit at this point, make sure the last
-                    # task is marked as done
-                    pass
                 self._work_queue.task_done()
                 break
             if event["type"] == EVTYPE_EE_SNAPSHOT:
@@ -202,12 +201,10 @@ class EvaluatorTracker:
             )
 
     def _clear_work_queue(self) -> None:
-        try:
+        with contextlib.suppress(queue.Empty):
             while True:
                 self._work_queue.get_nowait()
                 self._work_queue.task_done()
-        except queue.Empty:
-            pass
 
     def request_termination(self) -> None:
         logger = logging.getLogger("ert.ensemble_evaluator.tracker")
