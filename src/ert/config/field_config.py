7cf4c44e9953009cab275e8798b73d6b1204d26d
Eivind Jahren <ejah@equinor.com>
2023-08-01 10:18:47 +0200

    Fix typing in FieldConfig

--- src/ert/config/field_config.py
+++ src/ert/config/field_config.py
@@ -6,7 +6,7 @@ import time
 from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Optional
+from typing import TYPE_CHECKING, Any, Optional, Union, overload
 
 import numpy as np
 import xarray as xr
@@ -22,9 +22,8 @@ if TYPE_CHECKING:
 _logger = logging.getLogger(__name__)
 
 
-# pylint: disable=too-many-instance-attributes
 @dataclass
-class Field(ParameterConfig):
+class Field(ParameterConfig):  # pylint: disable=too-many-instance-attributes
     nx: int
     ny: int
     nz: int
@@ -36,21 +35,29 @@ class Field(ParameterConfig):
     forward_init_file: str
     output_file: Path
     grid_file: str
-    mask_file: Path = None
+    mask_file: Optional[Path] = None
 
     def read_from_runpath(self, run_path: Path, real_nr: int) -> xr.Dataset:
         t = time.perf_counter()
         file_name = self.forward_init_file
         if "%d" in file_name:
             file_name = file_name % real_nr
-        file_path = run_path / file_name
-
-        key = self.name
-        data = read_field(file_path, key, self.mask, Shape(self.nx, self.ny, self.nz))
-
-        trans = self.input_transformation
-        data_transformed = field_transform(data, trans)
-        ds = xr.Dataset({"values": (["x", "y", "z"], data_transformed)})
+        ds = xr.Dataset(
+            {
+                "values": (
+                    ["x", "y", "z"],
+                    field_transform(
+                        read_field(
+                            run_path / file_name,
+                            self.name,
+                            self.mask,
+                            Shape(self.nx, self.ny, self.nz),
+                        ),
+                        self.input_transformation,
+                    ),
+                )
+            }
+        )
         _logger.debug(f"load() time_used {(time.perf_counter() - t):.4f}s")
         return ds
 
@@ -62,16 +69,19 @@ class Field(ParameterConfig):
         if os.path.islink(file_out):
             os.unlink(file_out)
 
-        data = ensemble.load_parameters(self.name, real_nr)
-        data = field_transform(data, transform_name=self.output_transformation)
-        data = _field_truncate(
-            data,
-            self.truncation_min,
-            self.truncation_max,
-        )
-        data = np.ma.MaskedArray(data, self.mask, fill_value=np.nan)
         save_field(
-            data,
+            np.ma.MaskedArray(
+                _field_truncate(
+                    field_transform(
+                        ensemble.load_parameters(self.name, real_nr),
+                        transform_name=self.output_transformation,
+                    ),
+                    self.truncation_min,
+                    self.truncation_max,
+                ),
+                self.mask,
+                fill_value=np.nan,
+            ),
             self.name,
             file_out,
             self.file_format,
@@ -79,7 +89,7 @@ class Field(ParameterConfig):
 
         _logger.debug(f"save() time_used {(time.perf_counter() - t):.4f}s")
 
-    def save_experiment_data(self, experiment_path) -> None:
+    def save_experiment_data(self, experiment_path: Path) -> None:
         mask_path = experiment_path / "grid_mask.npy"
         if not mask_path.exists():
             mask, _ = get_mask(self.grid_file)
@@ -88,6 +98,11 @@ class Field(ParameterConfig):
 
     @cached_property
     def mask(self) -> Any:
+        if self.mask_file is None:
+            raise ValueError(
+                "In order to get Field.mask, Field.save_experiment_data has"
+                " to be called first"
+            )
         return np.load(self.mask_file)
 
 
@@ -104,15 +119,31 @@ TRANSFORM_FUNCTIONS = {
 }
 
 
+@overload
+def field_transform(
+    data: xr.DataArray, transform_name: Optional[str]
+) -> Union[npt.NDArray[np.double], xr.DataArray]:
+    pass
+
+
+@overload
 def field_transform(
     data: npt.NDArray[np.double], transform_name: Optional[str]
 ) -> npt.NDArray[np.double]:
+    pass
+
+
+def field_transform(
+    data: Union[xr.DataArray, npt.NDArray[np.double]], transform_name: Optional[str]
+) -> Union[npt.NDArray[np.double], xr.DataArray]:
     if transform_name is None:
         return data
     return TRANSFORM_FUNCTIONS[transform_name](data)
 
 
-def _field_truncate(data: npt.ArrayLike, min_: float, max_: float) -> Any:
+def _field_truncate(
+    data: npt.ArrayLike, min_: Optional[float], max_: Optional[float]
+) -> Any:
     if min_ is not None and max_ is not None:
         vfunc = np.vectorize(lambda x: max(min(x, max_), min_))
         return vfunc(data)
