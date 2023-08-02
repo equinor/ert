d3dc1a8680e2cbdc76975a7c68ad3d418ca38604
Eivind Jahren <ejah@equinor.com>
2023-08-02 11:28:33 +0200

    Merge model config tests

--- tests/unit_tests/config/test_model_config.py
+++ tests/unit_tests/config/test_model_config.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ert.config import ModelConfig
 
 
@@ -18,3 +20,34 @@ def test_suggested_deprecated_model_config_run_path(tmpdir):
     suggested_path = "simulations/realization-<IENS>/iter-<ITER>"
     mc = ModelConfig(num_realizations=1, runpath_format_string=runpath)
     assert mc.runpath_format_string == suggested_path
+
+
+@pytest.mark.filterwarnings("ignore::ert.config.ConfigWarning")
+@pytest.mark.parametrize(
+    "extra_config, expected",
+    [
+        pytest.param(
+            {"ECLBASE": "ECLBASE%d", "JOBNAME": "JOBNAME%d"},
+            "JOBNAME<IENS>",
+            id="ECLBASE does not overwrite JOBNAME",
+        ),
+        pytest.param(
+            {"ECLBASE": "ECLBASE%d"},
+            "ECLBASE<IENS>",
+            id="ECLBASE is also used as JOBNAME",
+        ),
+        pytest.param(
+            {"ECLBASE": "ECLBASE%d", "JOBNAME": "JOBNAME%d"},
+            "JOBNAME<IENS>",
+            id="JOBNAME is used when no ECLBASE is present",
+        ),
+        pytest.param(
+            {},
+            "<CONFIG_FILE>-<IENS>",
+            id="JOBNAME defaults to <CONFIG_FILE>-<IENS>",
+        ),
+    ],
+)
+def test_model_config_jobname_and_eclbase(extra_config, expected):
+    config_dict = {"NUM_REALIZATIONS": 1, "ENSPATH": "Ensemble", **extra_config}
+    assert ModelConfig.from_dict(None, config_dict).jobname_format_string == expected
