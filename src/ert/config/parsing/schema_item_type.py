5432fde4056017bc24e4132ce6a56693daacdaf1
Eivind Jahren <ejah@equinor.com>
2023-08-08 12:02:52 +0200

    Remove unused from_old_to_new

--- src/ert/config/parsing/schema_item_type.py
+++ src/ert/config/parsing/schema_item_type.py
@@ -1,5 +1,4 @@
 import sys
-from typing import Dict
 
 if sys.version_info < (3, 11):
     from enum import Enum
@@ -9,8 +8,6 @@ if sys.version_info < (3, 11):
 
 else:
     from enum import StrEnum
-
-_old_to_new: Dict[int, "SchemaItemType"] = {}
 
 
 class SchemaItemType(StrEnum):
@@ -27,27 +24,3 @@ class SchemaItemType(StrEnum):
     INVALID = "INVALID"
     RUNTIME_INT = "RUNTIME_INT"
     RUNTIME_FILE = "RUNTIME_FILE"
-
-    @classmethod
-    def from_content_type_enum(cls, old: int) -> "SchemaItemType":
-        if old not in _old_to_new:
-            raise ValueError("Invalid old enum value")
-
-        return _old_to_new[old]
-
-
-_old_to_new = {
-    1: SchemaItemType.STRING,
-    2: SchemaItemType.INT,
-    4: SchemaItemType.FLOAT,
-    8: SchemaItemType.PATH,
-    16: SchemaItemType.EXISTING_PATH,
-    32: SchemaItemType.BOOL,
-    64: SchemaItemType.CONFIG,
-    128: SchemaItemType.BYTESIZE,
-    256: SchemaItemType.EXECUTABLE,
-    512: SchemaItemType.ISODATE,
-    1024: SchemaItemType.INVALID,
-    2048: SchemaItemType.RUNTIME_INT,
-    4096: SchemaItemType.RUNTIME_FILE,
-}
