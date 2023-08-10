e090d224d5ec1eb0fb2fe0179ab78dc0c3cf88d7
Valentin Krasontovitsch <valentin.krasontovitsch@webstep.no>
2023-08-10 10:04:59 +0200

    Refactor gui: make node a dataclass

--- src/ert/gui/model/node.py
+++ src/ert/gui/model/node.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
 from enum import Enum, auto
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 
 
 class NodeType(Enum):
@@ -10,18 +13,13 @@ class NodeType(Enum):
     JOB = auto()
 
 
+@dataclass
 class Node:
-    def __init__(self, id_: int, data: Dict, type_: NodeType) -> None:
-        self.parent: Optional[Node] = None
-        self.data: Dict = data
-        self.children: Dict[int, Node] = {}
-        self.id = id_
-        self.type = type_
-
-    def __repr__(self) -> str:
-        parent = "no " if self.parent is None else ""
-        children = "no " if len(self.children) == 0 else f"{len(self.children)} "
-        return f"Node<{self.type}>@{self.id} with {parent}parent and {children}children"
+    id: int
+    data: Dict[Any, Any]
+    type: NodeType
+    children: Dict[int, Node] = field(default_factory=dict)
+    parent: Optional[Node] = None
 
     def add_child(self, node: "Node", node_id: Optional[int] = None) -> None:
         node.parent = self
