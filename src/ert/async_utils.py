f31ec6cd313fd22c1cd9b99f350c683130e82c15
Eivind Jahren <ejah@equinor.com>
2023-08-11 13:35:16 +0200

    Move run_in_loop to where it is used

--- src/ert/async_utils.py
+++ src/ert/async_utils.py
@@ -1,5 +1,4 @@
 import asyncio
-from typing import Awaitable, TypeVar
 
 
 def get_event_loop() -> asyncio.AbstractEventLoop:
@@ -8,10 +7,3 @@ def get_event_loop() -> asyncio.AbstractEventLoop:
     except RuntimeError:
         asyncio.set_event_loop(asyncio.new_event_loop())
         return asyncio.get_event_loop()
-
-
-T = TypeVar("T")
-
-
-def run_in_loop(coro: Awaitable[T]) -> T:
-    return get_event_loop().run_until_complete(coro)
