816d29096df89602f1ec933f3113f7a9640442e8
Frode Aarstad <frodeaarstad@gmail.com>
2023-08-17 15:55:22 +0200

    Add configfile name to log name

--- src/ert/logging/__init__.py
+++ src/ert/logging/__init__.py
@@ -10,12 +10,18 @@ STORAGE_LOG_CONFIG = pathlib.Path(__file__).parent.resolve() / "storage_log.conf
 
 class TimestampedFileHandler(FileHandler):
     def __init__(self, filename: str, *args: Any, **kwargs: Any) -> None:
-        filename, extension = os.path.splitext(filename)
-        filename = (
-            f"{filename}-"
+        timestamp = (
             f"{datetime.now().strftime(datetime.now().strftime('%Y-%m-%dT%H%M'))}"
-            f"{extension}"
         )
+        filename, extension = os.path.splitext(filename)
+
+        if "ert_config" in kwargs:
+            config_file_path = pathlib.Path(kwargs.pop("ert_config"))
+            name, ext = os.path.splitext(config_file_path.name)
+            config_filename = f"{name}-{ext[1:]}"
+            filename = f"{filename}-{config_filename}-{timestamp}{extension}"
+        else:
+            filename = f"{filename}-{timestamp}{extension}"
 
         if kwargs.pop("use_log_dir_from_env", False) and "ERT_LOG_DIR" in os.environ:
             log_dir = os.environ["ERT_LOG_DIR"]
