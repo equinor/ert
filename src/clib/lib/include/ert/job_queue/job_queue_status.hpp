2b212384541a33eb776fe6359a8b3aab092575f9
xjules <jparu@equinor.com>
2023-08-30 15:59:37 +0200

    Clang format file

--- src/clib/lib/include/ert/job_queue/job_queue_status.hpp
+++ src/clib/lib/include/ert/job_queue/job_queue_status.hpp
@@ -25,7 +25,7 @@ struct job_queue_status_struct {
         JOB_QUEUE_RUNNING_DONE_CALLBACK, // Temporary state, while running requested callbacks after an ended job - controlled by job_queue
         JOB_QUEUE_RUNNING_EXIT_CALLBACK, // Temporary state, while running requested callbacks after an ended job - controlled by job_queue
         JOB_QUEUE_STATUS_FAILURE, // The driver call to get status has failed, job status remains unchanged
-        JOB_QUEUE_FAILED, // Job has failed, no more retries, FINAL STATE
+        JOB_QUEUE_FAILED,   // Job has failed, no more retries, FINAL STATE
         JOB_QUEUE_UNKNOWN}; // Unable to get status from submitted job
 };
 
