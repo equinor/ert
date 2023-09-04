baebba25f5d7de69bcf54554ca99fd54bdbde385
Eivind Jahren <ejah@equinor.com>
2023-09-04 15:43:10 +0200

    Go back to c implementation of summary loading
    This removes the following unwanted behaviors of the current
    implementation:
    
    * The call to _init_numpy_vector_interp changes some values, probably due to interpolation.
    * Iterating over ecl_sum uses both ecl_smspec_node->key1 and ecl_smspec_node->key2 which
      will result in duplicate entries for summary keyword types that have both types.
    * DATE is included in the values loaded
    
    However, the following behavior is kept from the most recent implementation:
    
    * All report steps are loaded, not just those in the time map.

---
+++ src/clib/lib/enkf/read_summary.cpp
@@ -0,0 +1,50 @@
+#include <ert/ecl/ecl_smspec.hpp>
+#include <ert/ecl/ecl_sum.hpp>
+#include <ert/python.hpp>
+#include <fnmatch.h>
+#include <string>
+#include <tuple>
+#include <vector>
+
+static bool matches(std::vector<std::string> patterns, std::string key) {
+    bool has_key = false;
+    for (auto pattern : patterns) {
+        if (fnmatch(pattern.c_str(), key.c_str(), 0) == 0) {
+            has_key = true;
+            break;
+        }
+    }
+    return has_key;
+}
+ERT_CLIB_SUBMODULE("_read_summary", m) {
+    m.def("read_summary",
+          [](Cwrap<ecl_sum_type> summary, std::vector<std::string> keys) {
+              const int step2 = ecl_sum_get_last_report_step(summary);
+              const ecl_smspec_type *smspec = ecl_sum_get_smspec(summary);
+              std::vector<std::pair<std::string, std::vector<double>>>
+                  summary_vectors{};
+
+              for (int i = 0; i < ecl_smspec_num_nodes(smspec); i++) {
+                  const ecl::smspec_node &smspec_node =
+                      ecl_smspec_iget_node_w_node_index(smspec, i);
+                  const char *key = smspec_node.get_gen_key1();
+                  if (matches(keys, key)) {
+                      int start = ecl_sum_get_first_report_step(summary);
+                      int end = ecl_sum_get_last_report_step(summary);
+                      std::vector<double> data{};
+                      int key_index =
+                          ecl_sum_get_general_var_params_index(summary, key);
+                      for (int tstep = start; tstep <= end; tstep++) {
+                          if (ecl_sum_has_report_step(summary, tstep)) {
+                              int time_index =
+                                  ecl_sum_iget_report_end(summary, tstep);
+                              data.push_back(
+                                  ecl_sum_iget(summary, time_index, key_index));
+                          }
+                      }
+                      summary_vectors.emplace_back(key, data);
+                  }
+              }
+              return summary_vectors;
+          });
+}
