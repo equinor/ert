3cb0884c30104837af36ec236671d3d8fd7513d1
Andreas Eknes Lie <andrli@equinor.com>
2023-09-11 14:36:56 +0200

    Replace **jobs array with std::vector<job_queue_node_type *>

--- src/clib/lib/job_queue/job_list.cpp
+++ src/clib/lib/job_queue/job_list.cpp
@@ -1,66 +1,38 @@
 #include <pthread.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include <ert/util/util.hpp>
+#include <vector>
 
 #include <ert/job_queue/job_list.hpp>
 #include <ert/job_queue/job_node.hpp>
 
 struct job_list_struct {
-    int active_size;
-    int alloc_size;
-    job_queue_node_type **jobs;
+    std::vector<job_queue_node_type *> vec_jobs;
     pthread_rwlock_t lock;
 };
 
 job_list_type *job_list_alloc() {
     auto job_list = new job_list_type;
-    job_list->active_size = 0;
-    job_list->alloc_size = 0;
-    job_list->jobs = NULL;
-    pthread_rwlock_init(&job_list->lock, NULL);
+    pthread_rwlock_init(&job_list->lock, nullptr);
     return job_list;
 }
 
 void job_list_reset(job_list_type *job_list) {
-    for (int queue_index = 0; queue_index < job_list->active_size;
-         queue_index++) {
-        job_queue_node_type *node = job_list->jobs[queue_index];
-        job_queue_node_free(node);
-        job_list->jobs[queue_index] = NULL;
-    }
-    job_list->active_size = 0;
+    for (auto &vec_job : job_list->vec_jobs)
+        job_queue_node_free(vec_job);
+    job_list->vec_jobs.clear();
 }
 
 int job_list_get_size(const job_list_type *job_list) {
-    return job_list->active_size;
+    return job_list->vec_jobs.size();
 }
 
-/**
-  This takes ownership to the job node instance.
-*/
 void job_list_add_job(job_list_type *job_list, job_queue_node_type *job_node) {
-    if (job_list->alloc_size == job_list->active_size) {
-        int new_alloc_size = util_int_max(16, job_list->alloc_size * 2);
-        job_list->jobs = (job_queue_node_type **)util_realloc(
-            job_list->jobs, sizeof *job_list->jobs * new_alloc_size);
-        job_list->alloc_size = new_alloc_size;
-    }
-
-    int queue_index = job_list->active_size;
+    int queue_index = job_list->vec_jobs.size();
     job_queue_node_set_queue_index(job_node, queue_index);
-    job_list->jobs[queue_index] = job_node;
-
-    job_list->active_size++;
+    job_list->vec_jobs.push_back(job_node);
 }
 
 void job_list_free(job_list_type *job_list) {
-    if (job_list->alloc_size > 0) {
-        job_list_reset(job_list);
-        free(job_list->jobs);
-    }
+    job_list_reset(job_list);
     delete job_list;
 }
 
