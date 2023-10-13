d57d13329b121e1fdfc49fc5be3b3e5e761b3912
Håvard Berland <havb@equinor.com>
2023-10-13 15:28:16 +0200

    Remove ERT3 legacy
    Not in use:
    * ert.data.record
    * ert.serialization._serializer
    * ert.serialization._registry

--- src/ert/serialization/__init__.py
+++ src/ert/serialization/__init__.py
@@ -1,12 +1,6 @@
 from ._evaluator import evaluator_marshaller, evaluator_unmarshaller
-from ._registry import get_serializer, has_serializer, registered_types
-from ._serializer import Serializer
 
 __all__ = [
     "evaluator_marshaller",
     "evaluator_unmarshaller",
-    "get_serializer",
-    "has_serializer",
-    "registered_types",
-    "Serializer",
 ]
