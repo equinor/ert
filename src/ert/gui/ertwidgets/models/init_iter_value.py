fcf4d19fd2481f2fa67a7baa0c75d632ae4b5798
Frode Aarstad <frodeaarstad@gmail.com>
2023-10-31 10:42:54 +0100

    Fix iteration number changing when starting simulation

--- src/ert/gui/ertwidgets/models/init_iter_value.py
+++ src/ert/gui/ertwidgets/models/init_iter_value.py
@@ -3,16 +3,16 @@ from ert.gui.ertwidgets.models.valuemodel import ValueModel
 
 
 class IterValueModel(ValueModel):
-    def __init__(self, notifier: ErtNotifier, default_value=0):
+    def __init__(self, notifier: ErtNotifier, default_value: int = 0) -> None:
         self._default_value = str(default_value)
         ValueModel.__init__(self, self.getDefaultValue())
-        notifier.ertChanged.connect(self._caseChanged)
+        notifier.current_case_changed.connect(self._caseChanged)
 
-    def setValue(self, value):
+    def setValue(self, value: int) -> None:
         ValueModel.setValue(self, value)
 
     def getDefaultValue(self):
         return self._default_value
 
-    def _caseChanged(self):
+    def _caseChanged(self) -> None:
         ValueModel.setValue(self, self.getDefaultValue())
