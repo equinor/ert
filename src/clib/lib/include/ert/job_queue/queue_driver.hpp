a31af1180eb880956a8f809d98d4387d5f01fa63
Eivind Jahren <ejah@equinor.com>
2023-11-10 09:04:12 +0100

    Use using instead of typedef

--- src/clib/lib/include/ert/job_queue/queue_driver.hpp
+++ src/clib/lib/include/ert/job_queue/queue_driver.hpp
@@ -1,24 +1,24 @@
 #pragma once
 #include <ert/job_queue/job_status.hpp>
 
-typedef enum {
+using job_driver_type = enum {
     NULL_DRIVER = 0,
     LSF_DRIVER = 1,
     LOCAL_DRIVER = 2,
     TORQUE_DRIVER = 4,
     SLURM_DRIVER = 5
-} job_driver_type;
+};
 
-typedef struct queue_driver_struct queue_driver_type;
+using queue_driver_type = struct queue_driver_struct;
 
-typedef void *(submit_job_ftype)(void *data, const char *cmd, int num_cpu,
-                                 const char *run_path, const char *job_name);
-typedef void(kill_job_ftype)(void *, void *);
-typedef job_status_type(get_status_ftype)(void *, void *);
-typedef void(free_job_ftype)(void *);
-typedef void(free_queue_driver_ftype)(void *);
-typedef bool(set_option_ftype)(void *, const char *, const void *);
-typedef const void *(get_option_ftype)(const void *, const char *);
+using submit_job_ftype = void *(void *, const char *, int, const char *,
+                                const char *);
+using kill_job_ftype = void(void *, void *);
+using get_status_ftype = job_status_type(void *, void *);
+using free_job_ftype = void(void *);
+using free_queue_driver_ftype = void(void *);
+using set_option_ftype = bool(void *, const char *, const void *);
+using get_option_ftype = const void *(const void *, const char *);
 
 extern "C" queue_driver_type *queue_driver_alloc(job_driver_type type);
 
@@ -36,13 +36,13 @@ extern "C" const void *queue_driver_get_option(queue_driver_type *driver,
                                                const char *option_key);
 extern "C" void queue_driver_free(queue_driver_type *driver);
 
-typedef enum {
+using submit_status_type = enum {
     SUBMIT_OK = 0,
     /** Typically no more attempts. */
     SUBMIT_JOB_FAIL = 1,
     /** The driver would not take the job - for whatever reason?? */
     SUBMIT_DRIVER_FAIL = 2,
+    /** The queue is currently not accepting more jobs
+     * - either (temporarilty) because of pause or it is going down. */
     SUBMIT_QUEUE_CLOSED = 3
-} /* The queue is currently not accepting more jobs - either (temporarilty)
-                                             because of pause or it is going down. */
-submit_status_type;
+};
