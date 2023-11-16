c5ec70c2b169e2a693af63e57505b079b2aee18a
Øyvind Eide <eide.oyvind87@gmail.com>
2023-11-16 14:03:19 +0100

    Fix bug where truncation could be negative

--- tests/unit_tests/config/config_dict_generator.py
+++ tests/unit_tests/config/config_dict_generator.py
@@ -496,7 +496,7 @@ def ert_config_values(draw, use_eclbase=booleans):
                 st.tuples(
                     st.just("STD_ENKF"),
                     st.just("ENKF_TRUNCATION"),
-                    st.floats(min_value=-2.0, max_value=1.0),
+                    st.floats(min_value=0.0, max_value=1.0, exclude_min=True),
                 )
             ),
             analysis_select=st.sampled_from(AnalysisMode),
