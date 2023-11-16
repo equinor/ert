846ed439efdd1e3dcf4121453c875b2a9828e31b
Håvard Berland <havb@equinor.com>
2023-11-16 13:32:30 +0100

    Blacken and purge dead code

--- test-data/poly_example/poly_eval.py
+++ test-data/poly_example/poly_eval.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import json
-import time
 
 
 def _load_coeffs(filename):
@@ -9,11 +8,10 @@ def _load_coeffs(filename):
 
 
 def _evaluate(coeffs, x):
-    return coeffs["a"] * x ** 2 + coeffs["b"] * x + coeffs["c"]
+    return coeffs["a"] * x**2 + coeffs["b"] * x + coeffs["c"]
 
 
 if __name__ == "__main__":
-    time.sleep(4)
     coeffs = _load_coeffs("parameters.json")
     output = [_evaluate(coeffs, x) for x in range(10)]
     with open("poly.out", "w", encoding="utf-8") as f:
