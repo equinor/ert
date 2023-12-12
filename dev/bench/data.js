3d6b6f0084713c9639cf982808e88a0078ded372
github-action-benchmark <github@users.noreply.github.com>
2023-12-12 12:32:08 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 374c4b70cd77f7571377976e5a42c84bd108a9a5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702371332810,
+  "lastUpdate": 1702384328526,
   "repoUrl": "https://github.com/equinor/ert",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0012028127090517138",
             "extra": "mean: 161.7356806000089 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "feda.curic@gmail.com",
+            "name": "Feda Curic",
+            "username": "dafeda"
+          },
+          "committer": {
+            "email": "feda.curic@gmail.com",
+            "name": "Feda Curic",
+            "username": "dafeda"
+          },
+          "distinct": true,
+          "id": "374c4b70cd77f7571377976e5a42c84bd108a9a5",
+          "message": "Document adaptive localization",
+          "timestamp": "2023-12-12T13:29:24+01:00",
+          "tree_id": "f5c0efa981595a52c4401ffa5269e3a8f07f21af",
+          "url": "https://github.com/equinor/ert/commit/374c4b70cd77f7571377976e5a42c84bd108a9a5"
+        },
+        "date": 1702384327684,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/unit_tests/analysis/test_es_update.py::test_and_benchmark_adaptive_localization_with_fields",
+            "value": 6.217316824460933,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0015043269760327653",
+            "extra": "mean: 160.841087599988 msec\nrounds: 5"
           }
         ]
       }
