e2473734eb15e8a2eea12dda69c71bafa66e75e0
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2024-01-10 15:03:06 +0100

    Use XArray to process data before serving

---
+++ src/ert/gui/tools/web_plot/web_plot_server.py
@@ -0,0 +1,251 @@
+import json
+import sys
+import time
+from functools import lru_cache
+from os import listdir, path
+from pathlib import Path
+from typing import Dict, List, Literal, Optional, Union
+
+import numpy as np
+import uvicorn
+import xarray
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+
+directory_with_ert_storage: Path = Path(sys.argv[1])
+directory_with_html: str = sys.argv[2]
+hostname: str = sys.argv[3]
+port = int(sys.argv[4])
+requested_experiment = sys.argv[5]
+
+directory_with_ensembles: Path = directory_with_ert_storage / "ensembles"
+directory_with_experiments: Path = directory_with_ert_storage / "experiments"
+
+print("Starting up web plot server...")
+print("It can be run manually by running the line below:")
+print(f"python {path.abspath(__file__)} {' '.join(sys.argv[1:]) }")
+
+
+class EnsembleSummaryChartQuery(BaseModel):
+    experiment: Optional[str] = None
+    ensembles: List[str]
+    realizations: Union[Literal["*"], List[int]]
+    selector: str
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Summary(BaseModel):
+    name: str
+    input_file: str
+    keys: List[str]
+    refcase: List[str]
+
+
+# When time comes, include gen_data etc but stick with only summary for now
+class Response(BaseModel):
+    summary: Summary
+
+
+class Ensemble(BaseModel):
+    id: str
+    name: str
+    iteration: int
+    realizations: List[str]
+
+
+class Experiment(BaseModel):
+    name: str
+    id: str
+    ensembles: Dict[str, Ensemble]
+    responses: Response
+
+
+@lru_cache
+def read_experiment_ensemble_tree() -> Dict[str, Experiment]:
+    experiment_ids = listdir(directory_with_experiments)
+
+    experiment_infos = {}
+    for exp_id in experiment_ids:
+        exp_path = directory_with_experiments / exp_id
+
+        name: str = exp_id
+        if path.exists(exp_path / "index.json"):
+            with open(exp_path / "index.json") as f:
+                exp_index = json.load(f)
+                name = exp_index["name"]
+
+        if not path.exists(exp_path / "responses.json"):
+            raise FileNotFoundError(
+                f"Expected to find responses.json @ {exp_path/'responses.json'}"
+            )
+
+        with open(exp_path / "responses.json") as f:
+            responses_json = json.load(f)
+
+        assert "summary" in responses_json, (
+            f"expected 'summary' key to be found in "
+            f"{exp_path/exp_id}/responses.json"
+        )
+
+        experiment_infos[exp_id] = Experiment(
+            **{"name": name, "responses": responses_json, "id": exp_id, "ensembles": {}}
+        )
+
+    # Now read the ensembles
+    ensemble_ids = listdir(directory_with_ensembles)
+    for ens_id in ensemble_ids:
+        with open(directory_with_ensembles / ens_id / "index.json") as f:
+            ens_index = json.load(f)
+
+        exp_id = ens_index["experiment_id"]
+        iteration = ens_index["iteration"]
+        realization_ids = set(listdir(directory_with_ensembles / ens_id)) - {
+            "index.json",
+            "experiment",
+        }
+
+        experiment_infos[exp_id].ensembles[ens_id] = Ensemble(
+            **{
+                "id": ens_id,
+                "iteration": int(iteration),
+                "name": ens_index["name"],
+                "realizations": realization_ids,
+            }
+        )
+
+    return experiment_infos
+
+
+@app.get("/api/experiments")
+def read_experiments():
+    return read_experiment_ensemble_tree()
+
+
+@app.get("/api/hello")
+def test():
+    return {"b": "hello"}
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+@app.get("/api/summary_chart_data")
+def get_summary_chart_data(ensembles: str, experiment: str, selector: str):
+    t0 = time.time()
+    # 1. Load in all ensembles
+    exp_tree = read_experiment_ensemble_tree()
+    if experiment == "auto":
+        experiment = next(iter(exp_tree.keys()))
+
+    # Now for the results, create line charts
+    # Find one of the ensembles to determine experiment id
+    requested_ensembles = []
+    exp_ens_dict = exp_tree[experiment].ensembles
+    exp_ens_vals = exp_ens_dict.values()
+    for requested_ens_id in ensembles.split(","):
+        if requested_ens_id == "first":
+            actual_ensemble = next(ens for ens in exp_ens_vals if ens.iteration == 0)
+        elif requested_ens_id == "last":
+            actual_ensemble = next(iter(exp_ens_vals))
+            for ens in exp_ens_vals:
+                if ens.iteration > actual_ensemble.iteration:
+                    actual_ensemble = ens
+        else:
+            actual_ensemble = exp_ens_dict[requested_ens_id]
+
+        requested_ensembles.append((actual_ensemble, requested_ens_id))
+
+    ds = None
+    points = []
+    total_filesize_checked = 0
+    for ens, alias in requested_ensembles:
+        for real in ens.realizations:
+            print(f"Selecting for ens={ens.id}, real={real}")
+            filepath = directory_with_ensembles / ens.id / f"{real}" / "summary.nc"
+            ds = xarray.open_dataarray(filepath, decode_times=False)  # noqa
+            total_filesize_checked += path.getsize(filepath)
+            selection = eval(f"ds.sel({selector})")
+            values1d = selection.values.squeeze()
+            times1d = selection.coords["time"].values
+
+            # We normalize the points here, probably faster than doing it
+            # in the browser
+            v1dmin = values1d.min()
+            v1dmax = values1d.max()
+
+            t1dmin = times1d.min()
+            t1dmax = times1d.max()
+
+            # Think this is more vectorizable than doing the stack first
+            values1d -= v1dmin
+            values1d /= v1dmax - v1dmin
+
+            times1d -= t1dmin
+            times1df = times1d.astype(np.float32)
+            times1df /= t1dmax - t1dmin
+
+            points.append(
+                {
+                    "ensemble": ens.id,
+                    "data": np.stack((times1df, values1d), 1).tolist(),
+                    "realization": real,
+                    "domainX": [int(t1dmin), int(t1dmax)],
+                    "domainY": [float(v1dmin), float(v1dmax)],
+                }
+            )
+
+    assert ds is not None
+    time_attrs = ds["time"].attrs
+    time_spent = time.time() - t0
+    print(
+        f"Spent {time_spent} seconds processing query params:"
+        f"experiment={experiment}"
+        f"ensembles={ensembles}"
+        f"selector={selector}"
+    )
+
+    return {
+        "MBProcessed": total_filesize_checked / (1000**2),
+        "timeSpentSeconds": time_spent,
+        "experiment": experiment,
+        "timeAttrs": time_attrs,
+        "data": points,
+        "axisX": "time",
+        "axisY": "values",
+    }
+
+
+# Mount the "static" directory as static files
+
+# test_query_params: EnsembleSummaryChartQuery = EnsembleSummaryChartQuery.model_validate(
+#    {
+#        "ensembles": ["first", "last"],
+#        "realizations": "*",
+#        "selector": 'name="FOPR"',
+#        "experiment": requested_experiment,
+#    }
+# )
+
+
+# get_summary_chart_data(test_query_params)
+print("heh")
+
+
+if __name__ == "__main__":
+    app.mount("/", StaticFiles(directory=directory_with_html))
+    uvicorn.run(app, host=hostname, port=port, root_path=directory_with_html)
--- src/ert/gui/tools/web_plot/web_plot_window.py
+++ src/ert/gui/tools/web_plot/web_plot_window.py
@@ -2,58 +2,16 @@ import json
 import logging
 import subprocess
 import sys
-import tempfile
 
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from qtpy.QtWidgets import QMainWindow
+from textual._win_sleep import sleep
 
 from ert.shared.port_handler import find_available_port
 
 logger = logging.getLogger(__name__)
 import os
-
-
-def symlink(target, link_name, overwrite=False):
-    """
-    (https://stackoverflow.com/questions/8299386/modifying-a-symlink-in-python/55742015#55742015)
-    Create a symbolic link named link_name pointing to target.
-    If link_name exists then FileExistsError is raised, unless overwrite=True.
-    When trying to overwrite a directory, IsADirectoryError is raised.
-    """
-
-    if not overwrite:
-        os.symlink(target, link_name)
-        return
-
-    # os.replace() may fail if files are on different filesystems
-    link_dir = os.path.dirname(link_name)
-
-    # Create link to target with temporary filename
-    while True:
-        temp_link_name = tempfile.mktemp(dir=link_dir)
-
-        # os.* functions mimic as closely as possible system functions
-        # The POSIX symlink() returns EEXIST if link_name already exists
-        # https://pubs.opengroup.org/onlinepubs/9699919799/functions/symlink.html
-        try:
-            os.symlink(target, temp_link_name)
-            break
-        except FileExistsError:
-            pass
-
-    # Replace link_name with temp_link_name
-    try:
-        # Pre-empt os.replace on a directory with a nicer message
-        if not os.path.islink(link_name) and os.path.isdir(link_name):
-            raise IsADirectoryError(
-                f"Cannot symlink over existing directory: '{link_name}'"
-            )
-        os.replace(temp_link_name, link_name)
-    except:
-        if os.path.islink(temp_link_name):
-            os.remove(temp_link_name)
-        raise
 
 
 class WebPlotWindow(QMainWindow):
@@ -72,59 +30,29 @@ class WebPlotWindow(QMainWindow):
         )
 
         # Infer root location for file server
-        ensembles_folder = os.path.join(ens_path, "ensembles")
-
-        ensemble_keys = [
-            x
-            for x in os.listdir(ensembles_folder)
-            if os.path.isdir(os.path.join(ensembles_folder, x))
-        ]
-
-        ensembles_list = []
-        for ens_key in ensemble_keys:
-            with open(
-                f"{ensembles_folder}/{ens_key}/index.json", encoding="utf-8"
-            ) as f:
-                loaded = json.load(f)
-                ens_index = loaded["iteration"]
-                ensembles_list.append((ens_key, ens_index))
-
-        ensembles_list.sort(key=lambda x: x[1])
-        sorted_ens_key_list = [x[0] for x in ensembles_list]
-
-        ensembles_index_data = {"ensembles": sorted_ens_key_list}
-
-        with open(
-            os.path.join(static_html_path, "index.json"),
-            "w+",
-            encoding="utf-8",
-        ) as f:
-            json.dump(ensembles_index_data, f)
-
-        symlink(
-            ensembles_folder,
-            os.path.join(static_html_path, "ensembles"),
-            overwrite=True,
-        )
 
         python_fileserver_executable_path = os.path.join(
             os.path.dirname(__file__),
-            "web_plot_static_fileserver.py",
+            "web_plot_server.py",
         )
 
         sock.close()
 
+        url_for_browser = QUrl(f"http://{hostname}:{port}")
+        print(f"Serving static files to browser @ {url_for_browser}")
         subprocess.Popen(
             [
                 sys.executable,
                 python_fileserver_executable_path,
+                ens_path,
                 static_html_path,
                 hostname,
                 str(port),
-            ]
+                "auto",
+            ],
         )
 
-        url_for_browser = QUrl(f"http://{hostname}:{port}?serverURL=.")
-        print(f"Serving static files to browser @ {url_for_browser}")
+        sleep(1500)
+
         self.browser.setUrl(url_for_browser)
         self.showMaximized()
