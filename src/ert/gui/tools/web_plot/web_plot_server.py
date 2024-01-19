ee513f65bedda5286b56128c5413d3a40a69f753
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2024-01-19 11:11:18 +0100

    Add endpoint for parameter chart data & cleanup test code some

--- src/ert/gui/tools/web_plot/web_plot_server.py
+++ src/ert/gui/tools/web_plot/web_plot_server.py
@@ -1,17 +1,25 @@
 import json
+import re
 import sys
 import time
 from functools import lru_cache
 from os import listdir, path
 from pathlib import Path
-from typing import Dict, List, Literal, Optional, Union
+from typing import Dict, List, Literal, Optional, Union, TypedDict
 
 import numpy as np
 import uvicorn
 import xarray
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel, DirectoryPath, PositiveInt, computed_field
+from pydantic import (
+    BaseModel,
+    DirectoryPath,
+    PositiveInt,
+    computed_field,
+    model_validator,
+    field_validator,
+)
 from starlette.middleware.cors import CORSMiddleware
 
 
@@ -41,11 +49,43 @@ class Ensemble(BaseModel):
     realizations: List[str]
 
 
+class TransferFunctionDefinition(BaseModel):
+    name: str
+    distribution: str
+    args: List[float]
+
+
+class ParametersFile(BaseModel):
+    name: str
+    forward_init: bool
+    template_file: Optional[str]
+    transfer_function_definitions: List[TransferFunctionDefinition]
+    forward_init_file: Optional[str]
+
+
 class Experiment(BaseModel):
     name: str
     id: str
     ensembles: Dict[str, Ensemble]
     responses: Response
+    parameters: Dict[str, ParametersFile]
+
+    @computed_field
+    @property
+    def all_parameter_keys(self) -> List[str]:
+        all_param_names = []
+
+        for param_def in self.parameters.values():
+            for tf_def in param_def.transfer_function_definitions:
+                all_param_names.append(tf_def.name)
+
+        return all_param_names
+
+    def find_param_file_for_key(self, param_key: str) -> ParametersFile:
+        for param_def in self.parameters.values():
+            for tf_def in param_def.transfer_function_definitions:
+                if tf_def.name == param_key:
+                    return param_def
 
 
 class WebPlotServerConfig(BaseModel):
@@ -60,6 +100,20 @@ class NumpyEncoder(json.JSONEncoder):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+
+
+def parse_tf_definition_str(tf_def_str: str) -> TransferFunctionDefinition:
+    fragments = re.split(r'\s+', tf_def_str)
+
+    return TransferFunctionDefinition(
+        **{
+            "name": fragments[0],
+            "distribution": fragments[1],
+            "args": list(
+                map(float, [x for x in fragments[2:] if x != "" and x != "\n"])
+            ),
+        }
+    )
 
 
 class WebPlotStorageAccessors:
@@ -112,6 +166,18 @@ class WebPlotStorageAccessors:
             with open(exp_path / "responses.json") as f:
                 responses_json = json.load(f)
 
+            with open(exp_path / "parameter.json") as f:
+                parameters_json = json.load(f)
+                for param_key in parameters_json.keys():
+                    param_spec = parameters_json[param_key]
+
+                    param_spec["transfer_function_definitions"] = list(
+                        map(
+                            parse_tf_definition_str,
+                            param_spec["transfer_function_definitions"],
+                        )
+                    )
+
             assert "summary" in responses_json, (
                 f"expected 'summary' key to be found in "
                 f"{exp_path / exp_id}/responses.json"
@@ -121,6 +187,7 @@ class WebPlotStorageAccessors:
                 **{
                     "name": name,
                     "responses": responses_json,
+                    "parameters": parameters_json,
                     "id": exp_id,
                     "ensembles": {},
                 }
@@ -160,29 +227,18 @@ class WebPlotStorageAccessors:
 
         return experiment_infos
 
-    def get_summary_chart_data(self, ensembles: str, experiment: str, keyword: str):
+    def get_summary_chart_data(
+        self, ensembles: List[str], experiment_id: str, keyword: str
+    ):
         t0 = time.time()
         # 1. Load in all ensembles
         exp_tree = self.get_experiments_metadata()
-        if experiment == "auto":
-            tried = []
-            for exp_key in exp_tree.keys():
-                try:
-                    return get_summary_chart_data(ensembles, exp_key, keyword)
-                except Exception as e:
-                    tried.append((exp_key + "\n" + str(e) + "-----------------"))
 
-            return (
-                f"Failed to find experiment with summary data,"
-                f" tried the following experiments:"
-                f"{''.join(tried)}"
-            )
-
-        if experiment not in exp_tree:
+        if experiment_id not in exp_tree:
             return {
                 "MBProcessed": 0,
                 "timeSpentSeconds": time.time() - t0,
-                "experiment": experiment,
+                "experiment": experiment_id,
                 "timeAttrs": {},
                 "data": [],
                 "axisX": "time",
@@ -192,9 +248,9 @@ class WebPlotStorageAccessors:
         # Now for the results, create line charts
         # Find one of the ensembles to determine experiment id
         requested_ensembles = []
-        exp_ens_dict = exp_tree[experiment].ensembles
+        exp_ens_dict = exp_tree[experiment_id].ensembles
         exp_ens_vals = exp_ens_dict.values()
-        for requested_ens_id in ensembles.split(","):
+        for requested_ens_id in ensembles:
             if requested_ens_id == "first":
                 actual_ensemble = next(
                     ens for ens in exp_ens_vals if ens.iteration == 0
@@ -223,7 +279,7 @@ class WebPlotStorageAccessors:
                         / "summary.nc"
                     )
                     ds = xarray.open_dataarray(filepath, decode_times=False)  # noqa
-                    exp_tree[experiment].responses.summary.keys
+                    exp_tree[experiment_id].responses.summary.keys
                     total_filesize_checked += path.getsize(filepath)
                     selection = ds.sel(name=keyword)
                     values1d = selection.values.squeeze()
@@ -263,7 +319,7 @@ class WebPlotStorageAccessors:
         time_spent = time.time() - t0
         print(
             f"Spent {time_spent} seconds processing query params:"
-            f"experiment={experiment}"
+            f"experiment={experiment_id}"
             f"ensembles={ensembles}"
             f"keyword={keyword}"
         )
@@ -271,11 +327,96 @@ class WebPlotStorageAccessors:
         return {
             "MBProcessed": total_filesize_checked / (1000**2),
             "timeSpentSeconds": time_spent,
-            "experiment": experiment,
+            "experiment": experiment_id,
             "timeAttrs": time_attrs,
             "data": data,
             "axisX": "time",
             "axisY": "values",
+        }
+
+    def get_parameter_chart_data(
+        self, ensembles: List[str], experiment_id: str, parameter: str
+    ):
+        t0 = time.time()
+        # 1. Load in all ensembles
+        exp_tree = self.get_experiments_metadata()
+
+        if experiment_id not in exp_tree:
+            return {
+                "MBProcessed": 0,
+                "timeSpentSeconds": time.time() - t0,
+                "experiment": experiment_id,
+                "data": [],
+            }
+
+        # Now for the results, create line charts
+        # Find one of the ensembles to determine experiment id
+        requested_ensembles = []
+        exp_ens_dict = exp_tree[experiment_id].ensembles
+        exp_ens_vals = exp_ens_dict.values()
+        for requested_ens_id in ensembles:
+            if requested_ens_id == "first":
+                actual_ensemble = next(
+                    ens for ens in exp_ens_vals if ens.iteration == 0
+                )
+            elif requested_ens_id == "last":
+                actual_ensemble = next(iter(exp_ens_vals))
+                for ens in exp_ens_vals:
+                    if ens.iteration > actual_ensemble.iteration:
+                        actual_ensemble = ens
+            else:
+                actual_ensemble = exp_ens_dict[requested_ens_id]
+
+            requested_ensembles.append((actual_ensemble, requested_ens_id))
+
+        experiment = exp_tree[experiment_id]
+        total_filesize_checked = 0
+        param_def = experiment.find_param_file_for_key(parameter)
+
+        data_points = []
+        for ens, alias in requested_ensembles:
+            for real in ens.realizations:
+                try:
+                    filepath = (
+                        self.directory_with_ensembles
+                        / ens.id
+                        / real
+                        / f"{param_def.name}.nc"
+                    )
+
+                    ds = xarray.open_dataset(filepath)  # noqa
+                    total_filesize_checked += path.getsize(filepath)
+
+                    selection = ds.sel(names=parameter)
+                    data_points.append(
+                        {
+                            "ensemble_id": ens.id,
+                            "realization": real,
+                            "experiment": experiment_id,
+                            "values": selection["values"].values[0],
+                            "transformed_values": selection[
+                                "transformed_values"
+                            ].values[0],
+                        }
+                    )
+
+                except FileNotFoundError:
+                    # Missing realization, this is ok!
+                    pass
+
+        time_spent = time.time() - t0
+        print(
+            f"Spent {time_spent} seconds processing query params:"
+            f"experiment={experiment}"
+            f"ensembles={ensembles}"
+            f"parameter={parameter}"
+        )
+
+        return {
+            "MBProcessed": total_filesize_checked / (1000**2),
+            "timeSpentSeconds": time_spent,
+            "experiment": experiment.id,
+            "data": data_points,
         }
 
 
@@ -329,12 +470,20 @@ if __name__ == "__main__":
 
     @app.get("/api/summary_chart_data")
     def get_summary_chart_data(ensembles: str, experiment: str, keyword: str):
-        return accessors.get_summary_chart_data(ensembles, experiment, keyword)
+        return accessors.get_summary_chart_data(
+            ensembles.split(","), experiment, keyword
+        )
 
-    app.mount("/", StaticFiles(directory=config.directory_with_html))
+    @app.get("/api/parameter_chart_data")
+    def get_parameter_chart_data(ensembles: str, experiment: str, parameter: str):
+        return accessors.get_parameter_chart_data(
+            ensembles.split(","), experiment, parameter
+        )
+
+    app.mount("/", StaticFiles(directory=str(config.directory_with_html)))
     uvicorn.run(
         app,
         host=config.hostname,
         port=config.port,
-        root_path=config.directory_with_html,
+        root_path=str(config.directory_with_html),
     )
--- tests/unit_tests/gui/tools/web_plot/test_web_plot_api.py
+++ tests/unit_tests/gui/tools/web_plot/test_web_plot_api.py
@@ -1,17 +1,20 @@
 import json
+import math
 import os
 import pathlib
 from datetime import date
-from typing import Dict, List
+from typing import Dict, List, TypedDict
 
 import hypothesis.strategies as st
 import numpy as np
 import pandas as pd
+import xarray
 from hypothesis import given
 
 from ert.gui.tools.web_plot.web_plot_server import (
     WebPlotServerConfig,
     WebPlotStorageAccessors,
+    Experiment,
 )
 
 shared_uuids = st.shared(st.lists(st.uuids(), min_size=1, max_size=10, unique=True))
@@ -20,6 +23,11 @@ required_files = ["index.json", "responses.json"]
 
 some_diff_kws = ["WPR", "OPR", "GPR", "WPR"]
 some_param_kws = ["OP1", "OP2", "BPR", "WPR"]
+
+
+def make_valid_parameters_file(path: pathlib.Path, json_contents: object):
+    with open(path / "parameter.json", "w", encoding="utf-8") as f:
+        json.dump(json_contents, f)
 
 
 def make_valid_responses_json_file(path: str):
@@ -60,9 +68,99 @@ def make_valid_index_json_file(path, experiment_id: str, experiment_name: str):
 
 
 @st.composite
+def transfer_function_definition_string_generator(draw):
+    num_tfs = draw(st.integers(min_value=1, max_value=5))
+    tf_names = draw(
+        st.lists(
+            st.sampled_from(
+                [
+                    "ATLE_JOHNNY",
+                    "ATLE_BIRGER",
+                    "TOM_BIRGER",
+                    "JONNY_TOM",
+                    "ARNLEIF",
+                ]
+            ),
+            unique=True,
+            min_size=num_tfs,
+            max_size=num_tfs,
+        ),
+    )
+    tf_distributions = draw(
+        st.lists(
+            st.sampled_from(
+                [
+                    "UNIFORM",
+                    "NORMAL",
+                    "LOGUNIF",
+                    "DUNIF",
+                    "CONST",
+                    "DERFF",
+                    "TRIANGULAR",
+                ]
+            ),
+            min_size=num_tfs,
+            max_size=num_tfs,
+        )
+    )
+    tf_args = draw(
+        st.lists(
+            st.lists(st.floats(min_value=-1, max_value=1), min_size=2, max_size=5),
+            min_size=num_tfs,
+            max_size=num_tfs,
+        )
+    )
+
+    return [
+        f"{name.ljust(21)}{distribution} {' '.join([str(x) for x in args])}"
+        for (name, distribution, args) in zip(tf_names, tf_distributions, tf_args)
+    ]
+
+
+@st.composite
+def parameters_file_json_generator(draw):
+    num_entries = draw(st.integers(min_value=1, max_value=3))
+    names_list = draw(
+        st.lists(
+            st.sampled_from(["SNEK_OIL", "SNAK_OJIL", "MINE_FIELD", "WIND_FARM"]),
+            unique=True,
+            min_size=num_entries,
+            max_size=num_entries,
+        )
+    )
+    fwd_init_list = draw(
+        st.lists(
+            st.sampled_from(["true", "false"]),
+            min_size=num_entries,
+            max_size=num_entries,
+        )
+    )
+    tf_defs_list = draw(
+        st.lists(
+            transfer_function_definition_string_generator(),
+            min_size=num_entries,
+            max_size=num_entries,
+        )
+    )
+
+    return {
+        name: {
+            "name": name,
+            "forward_init": fwd_init,
+            "template_file": f"/some/where/templates/{name.lower()}_template.txt",
+            "output_file": f"{name}_params.txt",
+            "transfer_function_definitions": tf_defs,
+            "forward_init_file": "null",
+            "_ert_kind": "GenKwConfig",
+        }
+        for (name, fwd_init, tf_defs) in zip(names_list, fwd_init_list, tf_defs_list)
+    }
+
+
+@st.composite
 def experiment_ensemble_setup(draw):
-    uuids = draw(shared_uuids)
-    uuids_subset = draw(st.lists(st.sampled_from(uuids), unique=True))
+    exp_uuids = draw(shared_uuids)
+    uuids_subset = draw(st.lists(st.sampled_from(exp_uuids), unique=True))
     files_to_remove = draw(
         st.lists(
             st.sampled_from([*required_files]),
@@ -72,34 +170,45 @@ def experiment_ensemble_setup(draw):
     )
     ensembles = draw(
         st.sets(st.uuids(), min_size=2, max_size=10).filter(
-            lambda l: not any(x in uuids for x in l)
+            lambda l: not any(x in exp_uuids for x in l)
         )
     )
 
     ensemble_experiment_links = draw(
         st.lists(
-            st.sampled_from(uuids), max_size=len(ensembles), min_size=len(ensembles)
+            st.sampled_from(exp_uuids), max_size=len(ensembles), min_size=len(ensembles)
         )
     )
 
+    parameter_json = draw(
+        st.lists(
+            parameters_file_json_generator(),
+            min_size=len(exp_uuids),
+            max_size=len(exp_uuids),
+        )
+    )
+
+    assert len(parameter_json) == len(exp_uuids)
+
     return (
-        [str(s) for s in uuids],
+        [str(s) for s in exp_uuids],
         list(zip([str(s) for s in uuids_subset], files_to_remove)),
         list(
             zip(
                 [str(s) for s in ensembles], [str(s) for s in ensemble_experiment_links]
             )
         ),
+        parameter_json,
     )
 
 
 def create_experiment_storage(experiment_setup):
-    all_experiments, invalid, ensembles = experiment_setup
+    all_experiments, invalid, ensembles, parameter_json_list = experiment_setup
 
     cwd = pathlib.Path(os.getcwd())
     os.mkdir(cwd / "experiments")
     experiments_folder = cwd / "experiments"
-    for exp_uuid in all_experiments:
+    for exp_uuid, parameter_json in zip(all_experiments, parameter_json_list):
         os.mkdir(experiments_folder / exp_uuid)
         make_valid_responses_json_file(experiments_folder / exp_uuid)
         make_valid_index_json_file(
@@ -107,13 +216,25 @@ def create_experiment_storage(experiment_setup):
             exp_uuid,
             f"exp_with_id_{exp_uuid}",
         )
+        make_valid_parameters_file(experiments_folder / exp_uuid, parameter_json)
 
 
-def create_ensemble_storage(all_experiments, ensembles, num_realizations: int = 25):
+class RealizationInfo(TypedDict):
+    exp_id: str
+    ens_id: str
+    index: int
+    path: pathlib.Path
+
+
+def create_ensemble_storage(
+    all_experiments, ensembles, num_realizations: int = 25
+) -> List[RealizationInfo]:
     cwd = pathlib.Path(os.getcwd())
     ensembles_folder = cwd / "ensembles"
     os.mkdir(ensembles_folder)
     prior_ensemble_ids = {x: "null" for x in all_experiments}
+
+    realization_paths = []
     for i, (ens_id, exp_id) in enumerate(ensembles):
         ens_name = f"ensemble_{i}"
         prior_ensemble_id = prior_ensemble_ids[exp_id]
@@ -140,6 +261,15 @@ def create_ensemble_storage(all_experiments, ensembles, num_realizations: int = 
 
         for i_real in range(num_realizations):
             os.mkdir(ensembles_folder / ens_id / f"realization-{i_real}")
+            real_info: RealizationInfo = {
+                "exp_id": exp_id,
+                "ens_id": ens_id,
+                "index": i_real,
+                "path": ensembles_folder / ens_id / f"realization-{i_real}",
+            }
+            realization_paths.append(real_info)
+
+    return realization_paths
 
 
 def create_xarray_summary(num_keywords, num_timesteps):
@@ -165,9 +295,8 @@ def create_xarray_summary(num_keywords, num_timesteps):
 
 @given(experiment_ensemble_setup())
 def test_that_invalid_experiments_are_skipped(tmp_path_factory, experiment_setup):
-    tmp = tmp_path_factory.mktemp("tmp")
-    os.chdir(tmp)
-    all_experiments, invalid, ensembles = experiment_setup
+    os.chdir(tmp_path_factory.mktemp("tmp"))
+    all_experiments, invalid, ensembles, _ = experiment_setup
     cwd = pathlib.Path(os.getcwd())
     experiments_folder = cwd / "experiments"
 
@@ -177,7 +306,7 @@ def test_that_invalid_experiments_are_skipped(tmp_path_factory, experiment_setup
         os.remove(experiments_folder / exp_id / file_to_remove)
 
     create_ensemble_storage(all_experiments, ensembles)
-    all_experiments, invalid, _ = experiment_setup
+    all_experiments, invalid, _, _ = experiment_setup
 
     config = WebPlotServerConfig(
         **{
@@ -204,9 +333,8 @@ def test_that_invalid_experiments_are_skipped(tmp_path_factory, experiment_setup
 def test_that_experiments_without_ensembles_are_skipped(
     tmp_path_factory, experiment_setup
 ):
-    tmp = tmp_path_factory.mktemp("tmp")
-    os.chdir(tmp)
-    all_experiments, invalid, ensembles = experiment_setup
+    os.chdir(tmp_path_factory.mktemp("tmp"))
+    all_experiments, invalid, ensembles, _ = experiment_setup
 
     create_experiment_storage(experiment_setup)
 
@@ -251,16 +379,14 @@ def test_that_experiments_without_ensembles_are_skipped(
 def test_that_summary_load_works_with_missing_realizations(
     tmp_path_factory, experiment_setup, valid_realizations, num_keywords, num_timesteps
 ):
-    tmp = tmp_path_factory.mktemp("tmp")
-    os.chdir(tmp)
-    all_experiments, invalid, ensembles = experiment_setup
+    os.chdir(tmp_path_factory.mktemp("tmp"))
+    all_experiments, invalid, ensembles, _ = experiment_setup
     cwd = pathlib.Path(os.getcwd())
 
     create_experiment_storage(experiment_setup)
     create_ensemble_storage(
         all_experiments, ensembles, num_realizations=len(valid_realizations)
     )
-    all_experiments, invalid, _ = experiment_setup
     ensembles_folder = cwd / "ensembles"
 
     ensembles_by_experiment: Dict[str, List[str]] = {}
@@ -297,7 +423,7 @@ def test_that_summary_load_works_with_missing_realizations(
     for exp_id, ensembles in ensembles_by_experiment.items():
         for i in range(num_keywords):
             kw = f"KW_{i}"
-            summary = accessors.get_summary_chart_data(",".join(ensembles), exp_id, kw)
+            summary = accessors.get_summary_chart_data(ensembles, exp_id, kw)
 
             assert len(summary["data"]) == valid_realizations.count(True) * len(
                 ensembles
@@ -305,3 +431,121 @@ def test_that_summary_load_works_with_missing_realizations(
 
             for line in summary["data"]:
                 assert len(line["points"]) == num_timesteps
+
+
+@given(
+    experiment_ensemble_setup(),
+    st.lists(st.booleans(), min_size=1, max_size=3),
+)
+def test_that_parameters_meta_is_parsed(
+    tmp_path_factory, experiment_setup, valid_realizations
+):
+    os.chdir(tmp_path_factory.mktemp("tmp"))
+    all_experiments, _, all_ensembles, all_parameter_json = experiment_setup
+    cwd = pathlib.Path(os.getcwd())
+
+    create_experiment_storage(experiment_setup)
+    create_ensemble_storage(
+        all_experiments, all_ensembles, num_realizations=len(valid_realizations)
+    )
+
+    config = WebPlotServerConfig(
+        **{
+            "directory_with_ert_storage": cwd,
+            "directory_with_html": cwd,  # not used in this context
+            "hostname": "localhost",
+            "port": 9999,
+        }
+    )
+
+    accessors = WebPlotStorageAccessors(config)
+
+    meta = accessors.get_experiments_metadata()
+    for exp_id, parameter_json in zip(all_experiments, all_parameter_json):
+        experiment = meta[exp_id]
+        parameters = experiment.parameters
+        assert parameter_json.keys() == parameters.keys()
+
+        for key, spec in parameter_json.items():
+            parsed = parameters[key]
+            for i, tf_str in enumerate(spec["transfer_function_definitions"]):
+                assert parsed.transfer_function_definitions[i].name in tf_str
+                assert parsed.transfer_function_definitions[i].distribution in tf_str
+
+
+def create_xarray_params_for_realization(tf_names, realization_index) -> xarray.Dataset:
+    data = {
+        "names": tf_names,
+        "realizations": [realization_index] * len(tf_names),
+        "values": np.random.uniform(-1, 1, size=len(tf_names)),
+        "transformed_values": np.random.uniform(-4, 4, size=len(tf_names)),
+    }
+
+    df = pd.DataFrame(data)
+
+    # Convert the DataFrame to an xarray Dataset
+    dataset = df.set_index(["names", "realizations"]).to_xarray()
+    return dataset
+
+
+@given(
+    experiment_ensemble_setup(),
+    st.lists(st.booleans(), min_size=1, max_size=3),
+)
+def test_get_param_data(tmp_path_factory, experiment_setup, valid_realizations):
+    os.chdir(tmp_path_factory.mktemp("tmp"))
+    all_experiments, _, all_ensembles, all_parameter_json = experiment_setup
+    cwd = pathlib.Path(os.getcwd())
+
+    create_experiment_storage(experiment_setup)
+    real_infos = create_ensemble_storage(
+        all_experiments, all_ensembles, num_realizations=len(valid_realizations)
+    )
+
+    # Write the netcdf files for realizations
+    exp_to_paramjson = {
+        exp_id: param_json
+        for exp_id, param_json in zip(all_experiments, all_parameter_json)
+    }
+
+    for real_info in real_infos:
+        params_for_experiment = exp_to_paramjson[real_info["exp_id"]]
+        for name, param_json in params_for_experiment.items():
+            name = param_json["name"]
+            path = real_info["path"] / (name + ".nc")
+            tf_names = [
+                x.split(" ")[0] for x in param_json["transfer_function_definitions"]
+            ]
+            ds = create_xarray_params_for_realization(tf_names, real_info["index"])
+            ds.to_netcdf(path)
+
+    config = WebPlotServerConfig(
+        **{
+            "directory_with_ert_storage": cwd,
+            "directory_with_html": cwd,  # not used in this context
+            "hostname": "localhost",
+            "port": 9999,
+        }
+    )
+
+    accessors = WebPlotStorageAccessors(config)
+
+    meta = accessors.get_experiments_metadata()
+    for exp_id, parameter_json in zip(all_experiments, all_parameter_json):
+        experiment = meta[exp_id]
+        total_num_reals = sum(
+            [len(x.realizations) for x in experiment.ensembles.values()]
+        )
+
+        for param_name in experiment.all_parameter_keys:
+            chart_data = accessors.get_parameter_chart_data(
+                ensembles=list(experiment.ensembles.keys()),
+                experiment_id=exp_id,
+                parameter=param_name,
+            )
+            assert len(chart_data["data"]) == total_num_reals
+            for d in chart_data["data"]:
+                assert not math.isnan(d["values"])
+                assert not math.isnan(d["transformed_values"])
+                assert isinstance(d["values"], float)
+                assert isinstance(d["transformed_values"], float)
