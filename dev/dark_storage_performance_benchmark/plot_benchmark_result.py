8a5a18ea4c27ea00b6aa19699fb94effd0869596
Jonathan Karlsen <JONAK@equinor.com>
2024-02-05 08:55:06 +0100

    Add scripts for parsing and plotting benchmark results
    Added scripts for replacing benchmark plotting workflow.

---
+++ dev/dark_storage_performance_benchmark/plot_benchmark_result.py
@@ -0,0 +1,141 @@
+#!/bin/env python3
+import json
+from typing import Dict, Any, Union, List
+from pathlib import Path
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+def plot_marketplace_action_output():
+    with open(Path(__file__).with_name("data.json"), "r+", encoding="utf-8") as f:
+        data: Dict[str, str] = json.load(f)
+
+    data_entries = data["entries"]
+    dark_storage_benchmark_results: Dict[str, Any] = data_entries[
+        "Python Dark Storage Benchmark"
+    ]
+
+    cmap = plt.cm.get_cmap("viridis")
+
+    test_and_result_dict: Dict[str, Union[str, Dict]] = defaultdict(dict)
+
+    for benchmark_result in dark_storage_benchmark_results:
+        commit_id = benchmark_result["commit"]["id"]
+
+        for test in benchmark_result["benches"]:
+            test_name = test["name"]
+            test_value = test["value"]
+            test_dict = test_and_result_dict[test_name]
+
+            # first pass
+            if test_dict.get("test_modifier_constant") is None:
+                test_dict["test_modifier_constant"] = 1 / test_value
+                test_dict["results"] = []
+                test_dict["total_value"] = 0
+                test_dict["total_tests"] = 0
+            test_dict["results"].append(
+                {
+                    "commit_id": commit_id,
+                    "value": test_value * test_dict["test_modifier_constant"],
+                }
+            )
+            test_dict["total_value"] += test_value * test_dict["test_modifier_constant"]
+            test_dict["total_tests"] += 1
+
+    COMMITS_TO_SHOW = 8
+    commit_labels = []
+    for test_index, test_data in enumerate(test_and_result_dict.values()):
+        for order_index, test_result in enumerate(
+            test_data["results"][-COMMITS_TO_SHOW:]
+        ):
+            commit_id = test_result["commit_id"][:7]
+            if commit_id not in commit_labels:
+                commit_labels.append(commit_id)
+            color = cmap(order_index / COMMITS_TO_SHOW)
+            plt.scatter(test_index, test_result["value"], color=color, label=commit_id)
+
+        plt.scatter(
+            test_index,
+            test_data["total_value"] / test_data["total_tests"],
+            color="red",
+            label="mean",
+            s=70,
+            edgecolors="black",
+        )
+
+    plt.ylim(top=1.30, bottom=0.70)
+    plt.grid(True)
+    plt.xlabel("Test Index")
+    plt.ylabel("Value relative to baseline")
+    plt.legend(
+        labels=commit_labels + ["mean"], loc="lower center", ncol=len(commit_labels) + 1
+    )
+    plt.show()
+
+
+def plot_custom_action_output():
+    FIGSIZE = [19, 10]
+    VARIANCE = 0.25
+    with open(
+        Path(__file__).with_name("converted_data.json"), "r+", encoding="utf-8"
+    ) as f:
+        dark_storage_benchmark_results: List[Dict] = json.load(f)
+
+    cmap = plt.cm.get_cmap("viridis")
+
+    test_and_result_dict: Dict[str, Union[str, Dict]] = defaultdict(dict)
+    plt.figure(figsize=FIGSIZE)
+    for benchmark_result in dark_storage_benchmark_results:
+        commit_id = benchmark_result["commit_id"]
+
+        for test_name, test_value in benchmark_result["benchmarks"].items():
+            test_dict = test_and_result_dict[test_name]
+
+            # first pass
+            if test_dict.get("test_modifier_constant") is None:
+                test_dict["test_modifier_constant"] = 1 / test_value
+                test_dict["results"] = []
+                test_dict["total_value"] = 0
+                test_dict["total_tests"] = 0
+            test_dict["results"].append(
+                {
+                    "commit_id": commit_id,
+                    "value": test_value * test_dict["test_modifier_constant"],
+                }
+            )
+            test_dict["total_value"] += test_value * test_dict["test_modifier_constant"]
+            test_dict["total_tests"] += 1
+
+    COMMITS_TO_SHOW = 8
+    commit_labels = []
+    for test_index, test_data in enumerate(test_and_result_dict.values()):
+        for order_index, test_result in enumerate(
+            test_data["results"][-COMMITS_TO_SHOW:]
+        ):
+            commit_id = test_result["commit_id"][:7]
+            if commit_id not in commit_labels:
+                commit_labels.append(commit_id)
+            color = cmap(order_index / COMMITS_TO_SHOW)
+            plt.scatter(test_index, test_result["value"], color=color, label=commit_id)
+
+        plt.scatter(
+            test_index,
+            test_data["total_value"] / test_data["total_tests"],
+            color="red",
+            label="mean",
+            s=70,
+            edgecolors="black",
+        )
+
+    plt.ylim(top=1.00 + VARIANCE, bottom=1.00 - VARIANCE)
+    plt.grid(True)
+    plt.xlabel("Test Index")
+    plt.ylabel("Value relative to baseline")
+    plt.legend(
+        labels=commit_labels + ["mean"], loc="lower center", ncol=len(commit_labels) + 1
+    )
+    plt.savefig(Path(__file__).with_name("benchmark.svg"))
+
+
+if __name__ == "__main__":
+    plot_custom_action_output()
---
+++ dev/dark_storage_performance_benchmark/process_benchmark_result.py
@@ -0,0 +1,33 @@
+#!/bin/env python3
+
+from pathlib import Path
+import json
+from typing import List, Dict
+
+OUTPUT_FILE_NAME = "processed_results.json"
+
+with open(Path(__file__).with_name("output.json"), mode="rt", encoding="utf-8") as f:
+    pytest_benchmark_output = json.load(f)
+
+commit_id = pytest_benchmark_output["commit_info"]["id"]
+benchmark_datetime = pytest_benchmark_output["datetime"]
+
+results_dict = {
+    "commit_id": commit_id,
+    "datatime": benchmark_datetime,
+    "benchmarks": {},
+}
+for benchmark in pytest_benchmark_output["benchmarks"]:
+    results_dict["benchmarks"][benchmark["name"]] = benchmark["stats"]["mean"]
+
+with open(Path(__file__).with_name(OUTPUT_FILE_NAME), mode="rt", encoding="utf-8") as f:
+    results_json: List[Dict] = json.load(f)
+
+for result in results_json:
+    if result["commit_id"] == commit_id:
+        raise SystemExit("Benchmark already registered")
+results_json.append(results_dict)
+
+
+with open(Path(__file__).with_name(OUTPUT_FILE_NAME), mode="wt", encoding="utf-8") as f:
+    json.dump(results_json, f)
