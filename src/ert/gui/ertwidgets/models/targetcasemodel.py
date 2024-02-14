072954f5c89fd6374afa01600be505f420bbd267
Feda Curic <feda.curic@gmail.com>
2024-02-14 11:54:43 +0100

    Remove unused format_mode from TargetCaseModel

--- src/ert/gui/ertwidgets/models/targetcasemodel.py
+++ src/ert/gui/ertwidgets/models/targetcasemodel.py
@@ -8,11 +8,9 @@ class TargetCaseModel(ValueModel):
         self,
         analysis_config: AnalysisConfig,
         notifier: ErtNotifier,
-        format_mode: bool = False,
     ):
         self.analysis_config = analysis_config
         self.notifier = notifier
-        self._format_mode = format_mode
         self._custom = False
         super().__init__(self.getDefaultValue())
         notifier.ertChanged.connect(self.on_current_case_changed)
@@ -28,16 +26,12 @@ class TargetCaseModel(ValueModel):
             ValueModel.setValue(self, value)
 
     def getDefaultValue(self) -> str:
-        if self._format_mode:
-            analysis_config = self.analysis_config
-            if analysis_config.case_format_is_set():
-                return analysis_config.case_format
-            else:
-                case_name = self.notifier.current_case_name
-                return f"{case_name}_%d"
+        analysis_config = self.analysis_config
+        if analysis_config.case_format_is_set():
+            return analysis_config.case_format
         else:
             case_name = self.notifier.current_case_name
-            return f"{case_name}_smoother_update"
+            return f"{case_name}_%d"
 
     def on_current_case_changed(self, *args) -> None:
         if not self._custom:
