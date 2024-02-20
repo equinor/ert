348c82ec04a2d8c7e8c7c4c34c31bb9be15753ec
Håvard Berland <havb@equinor.com>
2024-02-20 14:55:53 +0100

    Add support for MEMORY_PER_JOB in OpenPBS

---
+++ tests/unit_tests/scheduler/test_openpbs_driver.py
@@ -0,0 +1,42 @@
+import os
+import stat
+from pathlib import Path
+
+import pytest
+
+from ert.scheduler import OpenPBSDriver
+
+
+@pytest.fixture
+def capturing_qsub(monkeypatch, tmp_path):
+    os.chdir(tmp_path)
+    bin_path = tmp_path / "bin"
+    bin_path.mkdir()
+    monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
+    qsub_path = bin_path / "qsub"
+    qsub_path.write_text(
+        "#!/bin/sh\necho $@ > captured_qsub_args; echo 'Job <1>'", encoding="utf-8"
+    )
+    qsub_path.chmod(qsub_path.stat().st_mode | stat.S_IEXEC)
+
+
+@pytest.mark.usefixtures("capturing_qsub")
+async def test_memory_per_job():
+    driver = OpenPBSDriver(memory_per_job="10gb")
+    await driver.submit(0, "sleep")
+    assert " -l mem=10gb " in Path("captured_qsub_args").read_text(encoding="utf-8")
+
+
+@pytest.mark.usefixtures("capturing_qsub")
+async def test_no_default_memory_per_job():
+    driver = OpenPBSDriver()
+    await driver.submit(0, "sleep")
+    assert " -l " not in Path("captured_qsub_args").read_text(encoding="utf-8")
+
+
+@pytest.mark.usefixtures("capturing_qsub")
+async def test_no_validation_of_memory_per_job():
+    # Validation will happen during config parsing
+    driver = OpenPBSDriver(memory_per_job="a_lot")
+    await driver.submit(0, "sleep")
+    assert " -l mem=a_lot " in Path("captured_qsub_args").read_text(encoding="utf-8")
