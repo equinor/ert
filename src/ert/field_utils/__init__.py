cc0c31f0c117f08926f9700dcaf66890068b6144
Jon Holba <jholba@equinor.com>
2024-02-22 08:43:38 +0100

    Merge read_mask and get_mask

--- src/ert/field_utils/__init__.py
+++ src/ert/field_utils/__init__.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
 from .field_file_format import FieldFileFormat
-from .field_utils import Shape, get_mask, get_shape, read_field, read_mask, save_field
+from .field_utils import Shape, get_shape, read_field, read_mask, save_field
 
 __all__ = [
-    "get_mask",
     "read_mask",
     "get_shape",
     "save_field",
