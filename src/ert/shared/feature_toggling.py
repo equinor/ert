ac990cda846e1008d95b5e26ab7c79b8b7e0b2bd
Håvard Berland <havb@equinor.com>
2024-02-29 15:58:23 +0100

    Enable scheduler on QUEUE_SYSTEM TORQUE by default (#7301)

--- src/ert/shared/feature_toggling.py
+++ src/ert/shared/feature_toggling.py
@@ -19,7 +19,7 @@ class FeatureScheduler:
         "LOCAL": True,
         "LSF": False,
         "SLURM": False,
-        "TORQUE": False,
+        "TORQUE": True,
     }
     _value: Optional[bool] = None
 
