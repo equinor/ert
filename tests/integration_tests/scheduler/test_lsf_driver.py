a3bb8d9ba06d9b813ae37aaf333d3af404402ab8
Zohar Malamant <ZOM@equinor.com>
2024-02-29 15:49:40 +0100

    Combine OpenPBS & LSF integration tests
    There is a lot of overlap between the two drivers, so we combine some of
    the generic integration tests into a single parameterised pytest.

--- tests/integration_tests/scheduler/test_lsf_driver.py
+++ tests/integration_tests/scheduler/test_lsf_driver.py
@@ -1,14 +1,14 @@
 import asyncio
 import json
 import os
-import sys
 from pathlib import Path
-from typing import Set
 
 import pytest
 
-from ert.scheduler import Driver, LsfDriver
-from ert.scheduler.event import FinishedEvent, StartedEvent
+from ert.scheduler import LsfDriver
+from tests.utils import poll
+
+from .conftest import mock_bin
 
 
 @pytest.fixture(autouse=True)
@@ -17,78 +17,7 @@ def mock_lsf(pytestconfig, monkeypatch, tmp_path):
         # User provided --lsf, which means we should use the actual LSF
         # cluster without mocking anything.""
         return
-
-    bin_path = Path(__file__).parent / "bin"
-
-    monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
-    monkeypatch.setenv("PYTEST_TMP_PATH", str(tmp_path))
-    monkeypatch.setenv("PYTHON", sys.executable)
-
-
-async def poll(driver: Driver, expected: Set[int], *, started=None, finished=None):
-    poll_task = asyncio.create_task(driver.poll())
-    completed = set()
-    try:
-        while True:
-            event = await driver.event_queue.get()
-            if isinstance(event, StartedEvent):
-                if started:
-                    await started(event.iens)
-            elif isinstance(event, FinishedEvent):
-                if finished is not None:
-                    await finished(event.iens, event.returncode, event.aborted)
-                completed.add(event.iens)
-                if completed == expected:
-                    break
-    finally:
-        poll_task.cancel()
-
-
-@pytest.mark.integration_test
-async def test_submit(tmp_path):
-    driver = LsfDriver()
-    await driver.submit(0, f"echo test > {tmp_path}/test")
-    await poll(driver, {0})
-
-    assert (tmp_path / "test").read_text(encoding="utf-8") == "test\n"
-
-
-async def test_submit_something_that_fails():
-    driver = LsfDriver()
-    finished_called = False
-
-    async def finished(iens, returncode, aborted):
-        assert iens == 0
-        assert returncode == 1
-        assert aborted is True
-        nonlocal finished_called
-        finished_called = True
-
-    await driver.submit(0, "exit 1")
-    await poll(driver, {0}, finished=finished)
-
-    assert finished_called
-
-
-async def test_kill():
-    driver = LsfDriver()
-    aborted_called = False
-
-    async def started(iens):
-        nonlocal driver
-        await driver.kill(iens)
-
-    async def finished(iens, returncode, aborted):
-        assert iens == 0
-        assert returncode == 1  # LSF cant get returncodes
-        assert aborted is True
-
-        nonlocal aborted_called
-        aborted_called = True
-
-    await driver.submit(0, "sleep 3")
-    await poll(driver, {0}, started=started, finished=finished)
-    assert aborted_called
+    mock_bin(monkeypatch, tmp_path)
 
 
 @pytest.mark.parametrize("runpath_supplied", [(True), (False)])
