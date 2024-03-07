Fix calculation of batch size
Need to take into account number of parameters.

src/ert/analysis/_es_update.py
@@ -374,7 +374,7 @@ def _calculate_adaptive_batch_size(num_params: int, num_obs: int) -> int:
             np.floor(
                 available_memory_bytes
                 * memory_safety_factor
-                / (num_obs * bytes_in_float64)
+                / (num_params * num_obs * bytes_in_float64)
             )
         ),
         num_params,