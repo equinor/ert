d4a75963aff117fe3550e4fcc41e07e1dedc2035
Eivind Jahren <ejah@equinor.com>
2024-03-08 11:49:51 +0100

    Add more cases to storage test

--- tests/integration_tests/test_storage_migration.py
+++ tests/integration_tests/test_storage_migration.py
@@ -1,6 +1,8 @@
 import shutil
 from pathlib import Path
 
+import numpy as np
+
 from ert.config import ErtConfig
 from ert.storage import open_storage
 from ert.storage.local_storage import local_storage_set_ert_config
@@ -39,6 +41,39 @@ def test_that_storage_matches(tmp_path, source_root, snapshot, monkeypatch):
         "8.0.8",
         "8.0.7",
         "8.0.6",
+        "8.0.4",
+        "8.0.3",
+        "8.0.2",
+        "8.0.1",
+        "8.0.0",
+        "7.0.4",
+        "7.0.3",
+        "7.0.2",
+        "7.0.1",
+        "7.0.0",
+        "6.0.8",
+        "6.0.7",
+        "6.0.6",
+        "6.0.5",
+        "6.0.4",
+        "6.0.3",
+        "6.0.2",
+        "6.0.1",
+        "6.0.0",
+        # github.com/equinor/ert/issues/7401
+        # Cannot load due to missing "_ert_kind"
+        # "5.0.12",
+        # "5.0.11",
+        # "5.0.10",
+        # "5.0.9",
+        # "5.0.8",
+        # "5.0.7",
+        # "5.0.6",
+        # "5.0.5",
+        # "5.0.4",
+        # "5.0.2",
+        # "5.0.1",
+        # "5.0.0",
     ]:
         print(ert_version)
         with open_storage(f"storage-{ert_version}", "w") as storage:
@@ -47,6 +82,7 @@ def test_that_storage_matches(tmp_path, source_root, snapshot, monkeypatch):
             experiment = experiments[0]
             ensembles = list(experiment.ensembles)
             assert len(ensembles) == 1
+            ensemble = ensembles[0]
 
             # We need to normalize some irrelevant details:
             experiment.response_configuration["summary"].refcase = {}
@@ -58,4 +94,23 @@ def test_that_storage_matches(tmp_path, source_root, snapshot, monkeypatch):
             )
             snapshot.assert_match(
                 str(experiment.response_configuration) + "\n", "responses"
+            )
+
+            summary_data = ensemble.load_responses(
+                "summary",
+                tuple(ensemble.get_realization_list_with_responses("summary")),
+            )
+            snapshot.assert_match(
+                summary_data.to_dataframe()
+                .astype(np.float32)
+                .transform(np.sort)
+                .to_csv(),
+                "summary_data",
+            )
+            snapshot.assert_match_dir(
+                {
+                    key: value.to_dataframe().to_csv()
+                    for key, value in experiment.observations.items()
+                },
+                "observations",
             )
