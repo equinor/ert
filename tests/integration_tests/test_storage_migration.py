e65d1680b17055c81626e6a5a7c870d6aae59045
Øyvind Eide <eide.oyvind87@gmail.com>
2024-03-13 10:09:49 +0100

    Parameterize test

--- tests/integration_tests/test_storage_migration.py
+++ tests/integration_tests/test_storage_migration.py
@@ -1,21 +1,31 @@
 import shutil
 
 import numpy as np
+import pytest
 
 from ert.config import ErtConfig
 from ert.storage import open_storage
 from ert.storage.local_storage import local_storage_set_ert_config
 
 
-def test_that_storage_matches(tmp_path, block_storage_path, snapshot, monkeypatch):
-    shutil.copytree(
-        block_storage_path / "all_data_types/",
-        tmp_path / "all_data_types",
-    )
-    monkeypatch.chdir(tmp_path / "all_data_types")
-    ert_config = ErtConfig.from_file("config.ert")
-    local_storage_set_ert_config(ert_config)
-    for ert_version in [
+@pytest.fixture()
+def copy_shared(tmp_path, block_storage_path):
+    for input_dir in ["data", "refcase"]:
+        shutil.copytree(
+            block_storage_path / "all_data_types" / f"{input_dir}",
+            tmp_path / "all_data_types" / f"{input_dir}",
+        )
+    for file in ["config.ert", "observations.txt", "params.txt", "template.txt"]:
+        shutil.copy(
+            block_storage_path / f"all_data_types/{file}",
+            tmp_path / "all_data_types" / file,
+        )
+
+
+@pytest.mark.usefixtures("copy_shared")
+@pytest.mark.parametrize(
+    "ert_version",
+    [
         "8.4.9",
         "8.4.8",
         "8.4.7",
@@ -73,43 +83,52 @@ def test_that_storage_matches(tmp_path, block_storage_path, snapshot, monkeypatc
         # "5.0.2",
         # "5.0.1",
         # "5.0.0",
-    ]:
-        print(ert_version)
-        with open_storage(f"storage-{ert_version}", "w") as storage:
-            experiments = list(storage.experiments)
-            assert len(experiments) == 1
-            experiment = experiments[0]
-            ensembles = list(experiment.ensembles)
-            assert len(ensembles) == 1
-            ensemble = ensembles[0]
+    ],
+)
+def test_that_storage_matches(
+    tmp_path, block_storage_path, snapshot, monkeypatch, ert_version
+):
+    shutil.copytree(
+        block_storage_path / f"all_data_types/storage-{ert_version}",
+        tmp_path / "all_data_types" / f"storage-{ert_version}",
+    )
+    monkeypatch.chdir(tmp_path / "all_data_types")
+    ert_config = ErtConfig.from_file("config.ert")
+    local_storage_set_ert_config(ert_config)
+    # To make sure all tests run against the same snapshot
+    snapshot.snapshot_dir = snapshot.snapshot_dir.parent
+    with open_storage(f"storage-{ert_version}", "w") as storage:
+        experiments = list(storage.experiments)
+        assert len(experiments) == 1
+        experiment = experiments[0]
+        ensembles = list(experiment.ensembles)
+        assert len(ensembles) == 1
+        ensemble = ensembles[0]
 
-            # We need to normalize some irrelevant details:
-            experiment.response_configuration["summary"].refcase = {}
-            experiment.parameter_configuration["PORO"].mask_file = ""
+        # We need to normalize some irrelevant details:
+        experiment.response_configuration["summary"].refcase = {}
+        experiment.parameter_configuration["PORO"].mask_file = ""
 
-            snapshot.assert_match(
-                str(experiment.parameter_configuration) + "\n",
-                "parameters",
-            )
-            snapshot.assert_match(
-                str(experiment.response_configuration) + "\n", "responses"
-            )
+        snapshot.assert_match(
+            str(experiment.parameter_configuration) + "\n",
+            "parameters",
+        )
+        snapshot.assert_match(
+            str(experiment.response_configuration) + "\n", "responses"
+        )
 
-            summary_data = ensemble.load_responses(
-                "summary",
-                tuple(ensemble.get_realization_list_with_responses("summary")),
-            )
-            snapshot.assert_match(
-                summary_data.to_dataframe()
-                .astype(np.float32)
-                .transform(np.sort)
-                .to_csv(),
-                "summary_data",
-            )
-            snapshot.assert_match_dir(
-                {
-                    key: value.to_dataframe().to_csv()
-                    for key, value in experiment.observations.items()
-                },
-                "observations",
-            )
+        summary_data = ensemble.load_responses(
+            "summary",
+            tuple(ensemble.get_realization_list_with_responses("summary")),
+        )
+        snapshot.assert_match(
+            summary_data.to_dataframe().astype(np.float32).transform(np.sort).to_csv(),
+            "summary_data",
+        )
+        snapshot.assert_match_dir(
+            {
+                key: value.to_dataframe().to_csv()
+                for key, value in experiment.observations.items()
+            },
+            "observations",
+        )
