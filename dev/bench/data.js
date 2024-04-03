1ca696f86996c897a69731474a4cdbd890686bd8
github-action-benchmark <github@users.noreply.github.com>
2024-04-03 10:15:20 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for b71fdde61f2c3ad7b36bb6a7a5cae8708ba7beb2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,39 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712126073615,
+  "lastUpdate": 1712139320030,
   "repoUrl": "https://github.com/equinor/ert",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "havb@equinor.com",
-            "name": "Håvard Berland",
-            "username": "berland"
-          },
-          "committer": {
-            "email": "berland@pvv.ntnu.no",
-            "name": "Håvard Berland",
-            "username": "berland"
-          },
-          "distinct": true,
-          "id": "13e45707b2a232757ada36023dca904ee5d22c2e",
-          "message": "Allow cleanup rm commands to fail\n\nThese rm commands operate on a NFS share, and removal on NFS\nshares occasionally fail due to some lock files or similar being\nhard to remove.",
-          "timestamp": "2024-03-18T13:57:18+01:00",
-          "tree_id": "fe82332448c34fa2783e93708553070aea89bf24",
-          "url": "https://github.com/equinor/ert/commit/13e45707b2a232757ada36023dca904ee5d22c2e"
-        },
-        "date": 1710766903418,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "tests/unit_tests/analysis/test_es_update.py::test_and_benchmark_adaptive_localization_with_fields",
-            "value": 0.1859576522602376,
-            "unit": "iter/sec",
-            "range": "stddev: 0.13081816100983812",
-            "extra": "mean: 5.377568429400014 sec\nrounds: 5"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -930,6 +899,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.04987775013257238",
             "extra": "mean: 5.4563244564000115 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "JONAK@equinor.com",
+            "name": "Jonathan Karlsen",
+            "username": "jonathan-eq"
+          },
+          "committer": {
+            "email": "107626001+jonathan-eq@users.noreply.github.com",
+            "name": "Jonathan Karlsen",
+            "username": "jonathan-eq"
+          },
+          "distinct": true,
+          "id": "b71fdde61f2c3ad7b36bb6a7a5cae8708ba7beb2",
+          "message": "Fix ErtPlugin interface staticmethods\n\nCommit `15ff246` marked interface methods with `@staticmethod` without considering other scripts inheriting the interface.",
+          "timestamp": "2024-04-03T12:11:13+02:00",
+          "tree_id": "6d12589554d8ab602fa4ae5a0d99e1c2defacc65",
+          "url": "https://github.com/equinor/ert/commit/b71fdde61f2c3ad7b36bb6a7a5cae8708ba7beb2"
+        },
+        "date": 1712139318850,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/unit_tests/analysis/test_es_update.py::test_and_benchmark_adaptive_localization_with_fields",
+            "value": 0.18967211409707477,
+            "unit": "iter/sec",
+            "range": "stddev: 0.03207619691277985",
+            "extra": "mean: 5.27225630800001 sec\nrounds: 5"
           }
         ]
       }
