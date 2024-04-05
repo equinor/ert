758a06e0b1e5aba8934b1c62410938dff8494911
Jon Holba <jholba@equinor.com>
2024-04-05 15:43:56 +0200

    Change cwd to tmpdir in some tests currently polluting working dir

--- tests/unit_tests/scheduler/test_lsf_driver.py
+++ tests/unit_tests/scheduler/test_lsf_driver.py
@@ -39,8 +39,8 @@ def nonempty_string_without_whitespace():
 
 @pytest.fixture
 def capturing_bsub(monkeypatch, tmp_path):
-    os.chdir(tmp_path)
-    bin_path = tmp_path / "bin"
+    monkeypatch.chdir(tmp_path)
+    bin_path = Path("bin")
     bin_path.mkdir()
     monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
     bsub_path = bin_path / "bsub"
@@ -186,7 +186,8 @@ async def test_submit_with_resource_requirement():
     ],
 )
 async def test_faulty_bsub(monkeypatch, tmp_path, bsub_script, expectation):
-    bin_path = tmp_path / "bin"
+    monkeypatch.chdir(tmp_path)
+    bin_path = Path("bin")
     bin_path.mkdir()
     monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
     bsub_path = bin_path / "bsub"
@@ -268,8 +269,8 @@ async def test_kill(
     expected_logged_error,
     caplog,
 ):
-    os.chdir(tmp_path)
-    bin_path = tmp_path / "bin"
+    monkeypatch.chdir(tmp_path)
+    bin_path = Path("bin")
     bin_path.mkdir()
     monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
     bkill_path = bin_path / "bkill"
@@ -437,7 +438,8 @@ BJOBS_HEADER = (
     ],
 )
 async def test_faulty_bjobs(monkeypatch, tmp_path, bjobs_script, expectation):
-    bin_path = tmp_path / "bin"
+    monkeypatch.chdir(tmp_path)
+    bin_path = Path("bin")
     bin_path.mkdir()
     monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
     bsub_path = bin_path / "bsub"
@@ -462,8 +464,8 @@ async def test_faulty_bjobs(monkeypatch, tmp_path, bjobs_script, expectation):
 async def test_that_bsub_will_retry_and_fail(
     monkeypatch, tmp_path, exit_code, error_msg
 ):
-    os.chdir(tmp_path)
-    bin_path = tmp_path / "bin"
+    monkeypatch.chdir(tmp_path)
+    bin_path = Path("bin")
     bin_path.mkdir()
     monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
     bsub_path = bin_path / "bsub"
@@ -490,8 +492,8 @@ async def test_that_bsub_will_retry_and_fail(
 async def test_that_bsub_will_retry_and_succeed(
     monkeypatch, tmp_path, exit_code, error_msg
 ):
-    os.chdir(tmp_path)
-    bin_path = tmp_path / "bin"
+    monkeypatch.chdir(tmp_path)
+    bin_path = Path("bin")
     bin_path.mkdir()
     monkeypatch.setenv("PATH", f"{bin_path}:{os.environ['PATH']}")
     bsub_path = bin_path / "bsub"
