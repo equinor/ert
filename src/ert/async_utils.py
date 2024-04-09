20768faedd3de45a7e1f73b609de28884dbbd3ac
Jonathan Karlsen <JONAK@equinor.com>
2024-04-09 13:18:16 +0200

    Fix py3.12 asyncio warning in async_utils
    This commit fixes the `DeprecationWarning: There is no current event loop` warning for Python3.12 in the `get_event_loop` function in `async_utils.py`. Prior to this commit, the function used `asyncio.get_event_loop()` which creates a new event loop directly. `asyncio.get_running_loop()` does however raise a RuntimeError when there is no event loop, which is what we want.

--- src/ert/async_utils.py
+++ src/ert/async_utils.py
@@ -19,7 +19,7 @@ def new_event_loop() -> asyncio.AbstractEventLoop:
 
 def get_event_loop() -> asyncio.AbstractEventLoop:
     try:
-        return asyncio.get_event_loop()
+        return asyncio.get_running_loop()
     except RuntimeError:
         asyncio.set_event_loop(new_event_loop())
         return asyncio.get_event_loop()
