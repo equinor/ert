22a4c19167d20846a986be5d018baae9e94f82b0
Jonathan Karlsen <JONAK@equinor.com>
2024-04-10 10:19:10 +0200

    Rename async_utils get_event_loop() to get_running_loop()
    The function has been updated and now uses `asyncio.get_running_loop` instead of `asyncio.get_event_loop`, so this commit changes the function name to reflect this change.

--- tests/performance_tests/test_dark_storage_performance.py
+++ tests/performance_tests/test_dark_storage_performance.py
@@ -4,7 +4,7 @@ from typing import Awaitable, TypeVar
 import pandas as pd
 import pytest
 
-from ert.async_utils import get_event_loop
+from ert.async_utils import get_running_loop
 from ert.config import ErtConfig
 from ert.dark_storage.endpoints import ensembles, experiments, records
 from ert.enkf_main import EnKFMain
@@ -15,7 +15,7 @@ T = TypeVar("T")
 
 
 def run_in_loop(coro: Awaitable[T]) -> T:
-    return get_event_loop().run_until_complete(coro)
+    return get_running_loop().run_until_complete(coro)
 
 
 def get_single_record_csv(storage, ensemble_id1, keyword, poly_ran):
