aec83eefc1cafcf7dfaece6e134abb5a3dae293d
Jonathan Karlsen <JONAK@equinor.com>
2024-04-16 15:24:32 +0200

    Move duplicate ev_types to ert/event_type_constants.py
    This commit moves duplicate event type constants found in `job_queue/queue.py` and `ensemble_evaluator/identifiers.py` into a common module `ert/event_type_constants.py`

--- src/ert/ensemble_evaluator/identifiers.py
+++ src/ert/ensemble_evaluator/identifiers.py
@@ -1,3 +1,17 @@
+from ert.event_type_constants import (
+    EVTYPE_ENSEMBLE_CANCELLED,
+    EVTYPE_ENSEMBLE_FAILED,
+    EVTYPE_ENSEMBLE_STARTED,
+    EVTYPE_ENSEMBLE_STOPPED,
+    EVTYPE_REALIZATION_FAILURE,
+    EVTYPE_REALIZATION_PENDING,
+    EVTYPE_REALIZATION_RUNNING,
+    EVTYPE_REALIZATION_SUCCESS,
+    EVTYPE_REALIZATION_TIMEOUT,
+    EVTYPE_REALIZATION_UNKNOWN,
+    EVTYPE_REALIZATION_WAITING,
+)
+
 ACTIVE = "active"
 CURRENT_MEMORY_USAGE = "current_memory_usage"
 DATA = "data"
@@ -16,14 +30,6 @@ STATUS = "status"
 STDERR = "stderr"
 STDOUT = "stdout"
 STEPS = "steps"
-
-EVTYPE_REALIZATION_FAILURE = "com.equinor.ert.realization.failure"
-EVTYPE_REALIZATION_PENDING = "com.equinor.ert.realization.pending"
-EVTYPE_REALIZATION_RUNNING = "com.equinor.ert.realization.running"
-EVTYPE_REALIZATION_SUCCESS = "com.equinor.ert.realization.success"
-EVTYPE_REALIZATION_UNKNOWN = "com.equinor.ert.realization.unknown"
-EVTYPE_REALIZATION_WAITING = "com.equinor.ert.realization.waiting"
-EVTYPE_REALIZATION_TIMEOUT = "com.equinor.ert.realization.timeout"
 
 EVTYPE_FORWARD_MODEL_START = "com.equinor.ert.forward_model_job.start"
 EVTYPE_FORWARD_MODEL_RUNNING = "com.equinor.ert.forward_model_job.running"
@@ -56,10 +62,6 @@ EVTYPE_EE_TERMINATED = "com.equinor.ert.ee.terminated"
 EVTYPE_EE_USER_CANCEL = "com.equinor.ert.ee.user_cancel"
 EVTYPE_EE_USER_DONE = "com.equinor.ert.ee.user_done"
 
-EVTYPE_ENSEMBLE_STARTED = "com.equinor.ert.ensemble.started"
-EVTYPE_ENSEMBLE_STOPPED = "com.equinor.ert.ensemble.stopped"
-EVTYPE_ENSEMBLE_CANCELLED = "com.equinor.ert.ensemble.cancelled"
-EVTYPE_ENSEMBLE_FAILED = "com.equinor.ert.ensemble.failed"
 
 EVGROUP_ENSEMBLE = {
     EVTYPE_ENSEMBLE_STARTED,
