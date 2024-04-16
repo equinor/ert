29d0fe49a2403af01ada62e271aab04927631ede
Jonathan Karlsen <JONAK@equinor.com>
2024-04-16 15:24:32 +0200

    Move duplicate ev_types to ert/event_type_constants.py
    This commit moves duplicate event type constants found in `job_queue/queue.py` and `ensemble_evaluator/identifiers.py` into a common module `ert/event_type_constants.py`

---
+++ src/ert/event_type_constants.py
@@ -0,0 +1,12 @@
+EVTYPE_REALIZATION_FAILURE = "com.equinor.ert.realization.failure"
+EVTYPE_REALIZATION_PENDING = "com.equinor.ert.realization.pending"
+EVTYPE_REALIZATION_RUNNING = "com.equinor.ert.realization.running"
+EVTYPE_REALIZATION_SUCCESS = "com.equinor.ert.realization.success"
+EVTYPE_REALIZATION_UNKNOWN = "com.equinor.ert.realization.unknown"
+EVTYPE_REALIZATION_WAITING = "com.equinor.ert.realization.waiting"
+EVTYPE_REALIZATION_TIMEOUT = "com.equinor.ert.realization.timeout"
+
+EVTYPE_ENSEMBLE_STARTED = "com.equinor.ert.ensemble.started"
+EVTYPE_ENSEMBLE_STOPPED = "com.equinor.ert.ensemble.stopped"
+EVTYPE_ENSEMBLE_CANCELLED = "com.equinor.ert.ensemble.cancelled"
+EVTYPE_ENSEMBLE_FAILED = "com.equinor.ert.ensemble.failed"
--- src/ert/job_queue/queue.py
+++ src/ert/job_queue/queue.py
@@ -22,6 +22,17 @@ from websockets.exceptions import ConnectionClosed
 
 from ert.config import QueueConfig
 from ert.constant_filenames import CERT_FILE, JOBS_FILE
+from ert.event_type_constants import (
+    EVTYPE_ENSEMBLE_CANCELLED,
+    EVTYPE_ENSEMBLE_FAILED,
+    EVTYPE_ENSEMBLE_STOPPED,
+    EVTYPE_REALIZATION_FAILURE,
+    EVTYPE_REALIZATION_PENDING,
+    EVTYPE_REALIZATION_RUNNING,
+    EVTYPE_REALIZATION_SUCCESS,
+    EVTYPE_REALIZATION_UNKNOWN,
+    EVTYPE_REALIZATION_WAITING,
+)
 from ert.job_queue.job_queue_node import JobQueueNode
 from ert.job_queue.job_status import JobStatus
 from ert.job_queue.queue_differ import QueueDiffer
@@ -45,17 +56,6 @@ CONCURRENT_INTERNALIZATION = 1
 """How many realizations allowed to be concurrently internalized using
 threads."""
 
-EVTYPE_REALIZATION_FAILURE = "com.equinor.ert.realization.failure"
-EVTYPE_REALIZATION_PENDING = "com.equinor.ert.realization.pending"
-EVTYPE_REALIZATION_RUNNING = "com.equinor.ert.realization.running"
-EVTYPE_REALIZATION_SUCCESS = "com.equinor.ert.realization.success"
-EVTYPE_REALIZATION_UNKNOWN = "com.equinor.ert.realization.unknown"
-EVTYPE_REALIZATION_WAITING = "com.equinor.ert.realization.waiting"
-EVTYPE_REALIZATION_TIMEOUT = "com.equinor.ert.realization.timeout"
-EVTYPE_ENSEMBLE_STARTED = "com.equinor.ert.ensemble.started"
-EVTYPE_ENSEMBLE_STOPPED = "com.equinor.ert.ensemble.stopped"
-EVTYPE_ENSEMBLE_CANCELLED = "com.equinor.ert.ensemble.cancelled"
-EVTYPE_ENSEMBLE_FAILED = "com.equinor.ert.ensemble.failed"
 
 _queue_state_to_event_type_map = {
     "NOT_ACTIVE": EVTYPE_REALIZATION_WAITING,
