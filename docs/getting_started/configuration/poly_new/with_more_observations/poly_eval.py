f1da0304c958c1cbaba3399e9834c8e1f7f1ab83
Feda Curic <feda.curic@gmail.com>
2024-05-16 13:13:47 +0200

    Update config guide with more observations

---
+++ docs/getting_started/configuration/poly_new/with_more_observations/poly_eval.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import json
+
+with open("parameters.json", encoding="utf-8") as f:
+    coeffs = json.load(f)["COEFFS"]
+
+
+def evaluate(coeffs, x):
+    return coeffs["a"] * x**2 + coeffs["b"] * x + coeffs["c"]
+
+
+output = [evaluate(coeffs, x) for x in range(50)]
+with open("poly.out", "w", encoding="utf-8") as f:
+    f.write("\n".join(map(str, output)))
