036ec86df943540f46fd4124eb5cf996e56f4fb7
Andreas Eknes Lie <andrli@equinor.com>
2024-06-24 15:41:30 +0200

    Remove test that verified mixing color logic

--- tests/unit_tests/gui/model/test_snapshot.py
+++ tests/unit_tests/gui/model/test_snapshot.py
@@ -1,4 +1,3 @@
-import pytest
 from pytestqt.qt_compat import qt_api
 from qtpy.QtCore import QModelIndex
 from qtpy.QtGui import QColor
@@ -6,12 +5,7 @@ from qtpy.QtGui import QColor
 from ert.ensemble_evaluator.snapshot import ForwardModel, PartialSnapshot
 from ert.ensemble_evaluator.state import (
     COLOR_FAILED,
-    COLOR_PENDING,
-    COLOR_RUNNING,
-    FORWARD_MODEL_STATE_FAILURE,
     FORWARD_MODEL_STATE_FINISHED,
-    FORWARD_MODEL_STATE_RUNNING,
-    FORWARD_MODEL_STATE_START,
 )
 from ert.gui.model.snapshot import RealJobColorHint, RealStatusColorHint, SnapshotModel
 
@@ -49,7 +43,7 @@ def test_realization_sort_order(full_snapshot):
         )
 
 
-def test_realization_state_is_overridden_by_queue_finalized_state(fail_snapshot):
+def test_realization_state_is_queue_finalized_state(fail_snapshot):
     model = SnapshotModel()
     model._add_snapshot(SnapshotModel.prerender(fail_snapshot), 0)
 
@@ -67,49 +61,3 @@ def test_realization_state_is_overridden_by_queue_finalized_state(fail_snapshot)
     assert color == QColor(*COLOR_FAILED)
     assert done_count == 1
     assert full_count == 1
-
-
-@pytest.mark.parametrize(
-    "first_state, second_state, expected_color",
-    [
-        (
-            FORWARD_MODEL_STATE_FINISHED,
-            FORWARD_MODEL_STATE_START,
-            QColor(*COLOR_PENDING),
-        ),
-        (
-            FORWARD_MODEL_STATE_FINISHED,
-            FORWARD_MODEL_STATE_RUNNING,
-            QColor(*COLOR_RUNNING),
-        ),
-        (
-            FORWARD_MODEL_STATE_FINISHED,
-            FORWARD_MODEL_STATE_FAILURE,
-            QColor(*COLOR_FAILED),
-        ),
-        (
-            FORWARD_MODEL_STATE_RUNNING,
-            FORWARD_MODEL_STATE_START,
-            QColor(*COLOR_RUNNING),
-        ),
-        (
-            FORWARD_MODEL_STATE_FAILURE,
-            FORWARD_MODEL_STATE_START,
-            QColor(*COLOR_FAILED),
-        ),
-    ],
-)
-def test_display_color_changes_to_more_important_state(
-    first_state, second_state, expected_color, full_snapshot
-):
-    model = SnapshotModel()
-    model._add_snapshot(SnapshotModel.prerender(full_snapshot), 0)
-
-    partial = PartialSnapshot(full_snapshot)
-    partial.update_forward_model("0", "0", ForwardModel(status=first_state))
-    partial.update_forward_model("0", "1", ForwardModel(status=second_state))
-    model._add_partial_snapshot(SnapshotModel.prerender(partial), 0)
-    first_real = model.index(0, 0, model.index(0, 0))
-
-    color, _, _ = model.data(first_real, RealJobColorHint)
-    assert color == expected_color
