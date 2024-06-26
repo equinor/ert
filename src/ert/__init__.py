d1f8ac1c90853f1a5363982989aa7d3b35f6c9e6
Eivind Jahren <ejah@equinor.com>
2024-06-26 09:58:41 +0200

    Fix issue with netCDF4

--- src/ert/__init__.py
+++ src/ert/__init__.py
@@ -2,6 +2,10 @@
 Ert - Ensemble Reservoir Tool - a package for reservoir modeling.
 """
 
+# workaround for https://github.com/Unidata/netcdf4-python/issues/1343
+import netCDF4 as _netcdf4  # noqa
+
+
 from .config import ErtScript
 from .data import MeasuredData
 from .job_queue import JobStatus
