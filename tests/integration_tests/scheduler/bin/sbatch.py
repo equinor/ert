2b8e674066d986993473180fec7025d3c893420a
Håvard Berland <havb@equinor.com>
2024-07-01 14:29:05 +0200

    Add generic driver test for num_cpu
    Test all drivers that there are remnants in the compute
    environment from the number of cpus requested.

--- tests/integration_tests/scheduler/bin/sbatch.py
+++ tests/integration_tests/scheduler/bin/sbatch.py
@@ -32,6 +32,16 @@ def main() -> None:
     (jobdir / "mock_jobs").mkdir(parents=True, exist_ok=True)
     (jobdir / "mock_jobs" / f"{jobid}.script").write_text(args.script, encoding="utf-8")
     (jobdir / "mock_jobs" / f"{jobid}.name").write_text(args.job_name, encoding="utf-8")
+    env_file = jobdir / "mock_jobs" / f"{jobid}.env"
+
+    if args.ntasks:
+        env_file.write_text(
+            f"export SLURM_JOB_CPUS_PER_NODE={args.ntasks}\n"
+            f"export SLURM_CPUS_ON_NODE={args.ntasks}",
+            encoding="utf-8",
+        )
+    else:
+        env_file.touch()
 
     subprocess.Popen(
         [str(Path(__file__).parent / "runner"), f"{jobdir}/mock_jobs/{jobid}"],
