b739bcab7727025c37c6aaa754f8b09cb89f7951
DanSava <dan.sava42@gmail.com>
2024-07-02 15:04:21 +0300

    Elevate MAX_RUNNING to global config key

--- src/ert/config/parsing/config_schema.py
+++ src/ert/config/parsing/config_schema.py
@@ -345,6 +345,7 @@ def init_user_config_schema() -> ConfigSchemaDict:
         path_keyword(ConfigKeys.RUNPATH_FILE),
         positive_int_keyword(ConfigKeys.MAX_SUBMIT),
         positive_int_keyword(ConfigKeys.NUM_CPU),
+        positive_int_keyword(ConfigKeys.MAX_RUNNING),
         queue_system_keyword(False),
         queue_option_keyword(),
         job_script_keyword(),
