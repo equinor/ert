062a1e1faa9d798f3098b207356621220332a675
Frode Aarstad <frodeaarstad@gmail.com>
2024-07-05 09:26:24 +0200

    Fix focus for color selector

--- src/ert/gui/tools/plot/color_chooser.py
+++ src/ert/gui/tools/plot/color_chooser.py
@@ -1,25 +1,55 @@
 from typing import Optional, Tuple
 
-from qtpy.QtCore import QRect, QSize, Signal
+from qtpy.QtCore import QRect, QSize, Signal, Slot
 from qtpy.QtGui import QColor, QMouseEvent, QPainter, QPaintEvent
 from qtpy.QtWidgets import QColorDialog, QFrame
 
 
 class ColorBox(QFrame):
     colorChanged = Signal(QColor)
+    mouseRelease = Signal()
 
     """A widget that shows a colored box"""
 
-    def __init__(self, color: QColor, size: int = 15) -> None:
+    def __init__(self, size: int = 15) -> None:
         QFrame.__init__(self)
         self.setFrameStyle(QFrame.Panel | QFrame.Sunken)
         self.setMaximumSize(QSize(size, size))
         self.setMinimumSize(QSize(size, size))
 
         self._tile_colors = [QColor(255, 255, 255), QColor(200, 200, 255)]
-        self._color = color
+        self._color: QColor = QColor(255, 255, 255)
 
-    def paintEvent(self, a0: Optional[QPaintEvent]) -> None:
+        self.mouseRelease.connect(self.show_color_dialog)
+        self.colorChanged.connect(self.update_color)
+
+    @Slot(QColor)
+    def update_color(self, color: QColor) -> None:
+        self._color = color
+        self.update()
+
+    @Slot()
+    def show_color_dialog(self) -> None:
+        color_dialog = QColorDialog(self._color, self)
+        color_dialog.setWindowTitle("Select color")
+        color_dialog.setOption(QColorDialog.ShowAlphaChannel)
+        color_dialog.accepted.connect(
+            lambda: self.colorChanged.emit(color_dialog.selectedColor())
+        )
+        color_dialog.open()
+
+    @property
+    def color(self) -> QColor:
+        return self._color
+
+    @color.setter
+    def color(self, color: Tuple[str, float]) -> None:
+        new_color = QColor(color[0])
+        new_color.setAlphaF(color[1])
+        self._color = new_color
+        self.update()
+
+    def paintEvent(self, event: Optional[QPaintEvent]) -> None:
         """Paints the box"""
         painter = QPainter(self)
         rect = self.contentsRect()
@@ -37,25 +67,9 @@ class ColorBox(QFrame):
         painter.restore()
         painter.fillRect(rect, self._color)
 
-        QFrame.paintEvent(self, a0)
+        QFrame.paintEvent(self, event)
 
-    def mouseReleaseEvent(self, a0: Optional[QMouseEvent]) -> None:
-        color = QColorDialog.getColor(
-            self._color, self, "Select color", QColorDialog.ShowAlphaChannel
-        )
-
-        if color.isValid():
-            self._color = color
-            self.update()
-            self.colorChanged.emit(self._color)
-
-    @property
-    def color(self) -> QColor:
-        return self._color
-
-    @color.setter
-    def color(self, color: Tuple[str, float]) -> None:
-        new_color = QColor(color[0])
-        new_color.setAlphaF(color[1])
-        self._color = new_color
-        self.update()
+    def mouseReleaseEvent(self, event: Optional[QMouseEvent]) -> None:
+        if event:
+            self.mouseRelease.emit()
+        return super().mouseReleaseEvent(event)
