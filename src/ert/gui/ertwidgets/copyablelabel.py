64233479a01cff5221670f074eb4dfae0d45abb8
Eivind Jahren <ejah@equinor.com>
2024-07-29 14:14:27 +0200

    Fix copy button crashing in plot error window

--- src/ert/gui/ertwidgets/copyablelabel.py
+++ src/ert/gui/ertwidgets/copyablelabel.py
@@ -1,15 +1,12 @@
 from os import path
 
-from qtpy.QtCore import Qt, QTimer
-from qtpy.QtGui import QIcon
+from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
-    QApplication,
     QHBoxLayout,
     QLabel,
-    QMessageBox,
-    QPushButton,
-    QSizePolicy,
 )
+
+from .copy_button import CopyButton
 
 # Get the absolute path of the directory that contains the current script
 current_dir = path.dirname(path.abspath(__file__))
@@ -63,6 +60,17 @@ def strip_run_path_magic_keywords(run_path: str) -> str:
     return rp_stripped
 
 
+class _CopyButton(CopyButton):
+    def __init__(self, label: QLabel) -> None:
+        super().__init__()
+        self.label = label
+
+    def copy(self) -> None:
+        self.copy_text(
+            strip_run_path_magic_keywords(unescape_string(self.label.text()))
+        )
+
+
 class CopyableLabel(QHBoxLayout):
     """CopyableLabel shows a string that is copyable via
     selection or clicking of a copy button"""
@@ -73,35 +81,7 @@ class CopyableLabel(QHBoxLayout):
         self.label = QLabel(f"<b>{escape_string(text)}</b>")
         self.label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
 
-        self.copy_button = QPushButton("")
-        self.copy_button.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-        self.copy_button.setIcon(QIcon("img:copy.svg"))
-        self.restore_timer = QTimer(self)
-
-        def restore_text() -> None:
-            self.copy_button.setIcon(QIcon("img:copy.svg"))
-
-        self.restore_timer.timeout.connect(restore_text)
-
-        def copy_text() -> None:
-            text = strip_run_path_magic_keywords(unescape_string(self.label.text()))
-
-            clipboard = QApplication.clipboard()
-            if clipboard:
-                clipboard.setText(text)
-            else:
-                QMessageBox.critical(
-                    None,
-                    "Error",
-                    "Cannot copy text to clipboard because your system does not have a clipboard",
-                    QMessageBox.Ok,
-                )
-
-            self.copy_button.setIcon(QIcon("img:check.svg"))
-
-            self.restore_timer.start(1000)
-
-        self.copy_button.clicked.connect(copy_text)
+        self.copy_button = _CopyButton(self.label)
 
         self.addWidget(self.label)
         self.addWidget(self.copy_button, alignment=Qt.AlignmentFlag.AlignLeft)
--- src/ert/gui/tools/plot/plot_window.py
+++ src/ert/gui/tools/plot/plot_window.py
@@ -7,7 +7,7 @@ import pandas as pd
 from httpx import RequestError
 from pandas import DataFrame
 from qtpy.QtCore import Qt, Slot
-from qtpy.QtWidgets import QDockWidget, QMainWindow, QMessageBox, QTabWidget, QWidget
+from qtpy.QtWidgets import QDockWidget, QMainWindow, QTabWidget, QWidget
 
 from ert.gui.ertwidgets import showWaitCursorWhileWaiting
 from ert.gui.plottery import PlotConfig, PlotContext
@@ -36,14 +36,11 @@ STD_DEV = "Std Dev"
 
 logger = logging.getLogger(__name__)
 
-from qtpy.QtCore import QTimer
-from qtpy.QtGui import QIcon
 from qtpy.QtWidgets import (
     QApplication,
     QDialog,
     QHBoxLayout,
     QLabel,
-    QPushButton,
     QTextEdit,
     QVBoxLayout,
 )
@@ -70,44 +67,21 @@ def create_error_dialog(title: str, content: str) -> QDialog:
     layout = QVBoxLayout()
     top_layout = QHBoxLayout()
     top_layout.addWidget(QLabel(title))
-    copy_button = QPushButton("")
-    copy_button.setMinimumHeight(35)
-    copy_button.setMaximumWidth(100)
-    top_layout.addWidget(copy_button)
-
-    restore_timer = QTimer()
-
-    def restore_text() -> None:
-        copy_button.setIcon(QIcon("img:copy.svg"))
-
-    restore_text()
-
-    def copy_text() -> None:
-        clipboard = QApplication.clipboard()
-        if clipboard:
-            clipboard.setText(text)
-        else:
-            QMessageBox.critical(
-                None,
-                "Error",
-                "Cannot copy text to clipboard because your system does not have a clipboard",
-                QMessageBox.Ok,
-            )
-        copy_button.setIcon(QIcon("img:check.svg"))
-
-        restore_timer.start(1000)
-
-    copy_button.clicked.connect(copy_text)
-    restore_timer.timeout.connect(restore_text)
-    layout.addLayout(top_layout)
 
     text = QTextEdit()
     text.setText(content)
     text.setReadOnly(True)
+
+    copy_button = _CopyButton(text)
+    copy_button.setObjectName("copy_button")
+    top_layout.addWidget(copy_button)
+    top_layout.addStretch(-1)
+
+    layout.addLayout(top_layout)
     layout.addWidget(text)
+
     qd.setLayout(layout)
-    QApplication.restoreOverrideCursor()
-    qd.exec()
+    return qd
 
 
 def open_error_dialog(title: str, content: str) -> None:
