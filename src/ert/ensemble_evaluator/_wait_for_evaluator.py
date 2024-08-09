c6f853d460d14d2eb43dc50cc7fa60b4044cc853
Frode Aarstad <frodeaarstad@gmail.com>
2024-08-09 14:29:28 +0200

    Fix type error

--- src/ert/ensemble_evaluator/_wait_for_evaluator.py
+++ src/ert/ensemble_evaluator/_wait_for_evaluator.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 WAIT_FOR_EVALUATOR_TIMEOUT = 60
 
 
-def get_ssl_context(cert: Optional[Union[str, bytes]]) -> Optional[ssl.SSLContext]:
+def get_ssl_context(cert: Optional[Union[str, bytes]]) -> Union[ssl.SSLContext, bool]:
     if cert is None:
-        return None
+        return False
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     ssl_context.load_verify_locations(cadata=cert)
     return ssl_context
