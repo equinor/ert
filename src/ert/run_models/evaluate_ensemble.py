68271c492cf7d0409c7671ff9a3243d04366dcbb
Yngve S. Kristiansen <yngve-sk@users.noreply.github.com>
2024-08-13 12:34:07 +0200

    Add descriptions to experiment types

--- src/ert/run_models/evaluate_ensemble.py
+++ src/ert/run_models/evaluate_ensemble.py
@@ -81,3 +81,7 @@ class EvaluateEnsemble(BaseRunModel):
     @classmethod
     def name(cls) -> str:
         return "Evaluate ensemble"
+
+    @classmethod
+    def description(cls) -> str:
+        return "Take existing ensemble parameters → evaluate"
