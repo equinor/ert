050498ea76a63fb7085d49f19690df2e936bf20c
Eivind Jahren <ejah@equinor.com>
2024-08-14 13:00:48 +0200

    Use CSV_EXPORT instead of 2 for export tool

--- src/ert/gui/tools/export/__init__.py
+++ src/ert/gui/tools/export/__init__.py
@@ -1,4 +1,3 @@
-from .export_panel import ExportPanel
 from .export_tool import ExportTool
 
-__all__ = ["ExportPanel", "ExportTool"]
+__all__ = ["ExportTool"]
--- src/ert/resources/workflows/jobs/internal-gui/scripts/csv_export.py
+++ src/ert/resources/workflows/jobs/internal-gui/scripts/csv_export.py
@@ -1,11 +1,8 @@
 import os
-import re
 
 import pandas
-from qtpy.QtWidgets import QCheckBox
 
-from ert import LibresFacade
-from ert.config import CancelPluginException, ErtPlugin
+from ert import ErtScript, LibresFacade
 
 
 def loadDesignMatrix(filename) -> pandas.DataFrame:
@@ -15,7 +12,7 @@ def loadDesignMatrix(filename) -> pandas.DataFrame:
     return dm
 
 
-class CSVExportJob(ErtPlugin):
+class CSVExportJob(ErtScript):
     """
     Export of summary, misfit, design matrix data and gen kw into a single CSV file.
 
@@ -31,33 +28,12 @@ class CSVExportJob(ErtPlugin):
 
     design_matrix: a path to a file containing the design matrix
 
-    infer_iteration: If True the script will try to infer the iteration number
-                     by looking at the suffix of the ensemble name (i.e. default_2
-                     = iteration 2). If False the script will use the ordering
-                     of the ensemble list: the first item will be iteration 0,
-                     the second item will be iteration 1...
-
     The script also looks for default values for output path and design matrix
     path to present in the GUI. These can be specified with DATA_KW keyword in
     the config file:
         DATA_KW <CSV_OUTPUT_PATH> {some path}
         DATA_KW <DESIGN_MATRIX_PATH> {some path}
     """
-
-    INFER_HELP = (
-        "<html>"
-        "If this is checked the iteration number will be inferred from the name i.e.:"
-        "<ul>"
-        "<li>ensemble_name -> iteration: 0</li>"
-        "<li>ensemble_name_0 -> iteration: 0</li>"
-        "<li>ensemble_name_2 -> iteration: 2</li>"
-        "<li>ensemble_0, ensemble_2, ensemble_5 -> iterations: 0, 2, 5</li>"
-        "</ul>"
-        "Leave this unchecked to set iteration number to the order of the listed ensembles:"
-        "<ul><li>ensemble_0, ensemble_2, ensemble_5 -> iterations: 0, 1, 2</li></ul>"
-        "<br/>"
-        "</html>"
-    )
 
     @staticmethod
     def getName():
@@ -69,15 +45,6 @@ class CSVExportJob(ErtPlugin):
             "Export GenKW, design matrix, misfit data "
             "and summary data into a single CSV file."
         )
-
-    @staticmethod
-    def inferIterationNumber(ensemble_name):
-        pattern = re.compile("_([0-9]+$)")
-        match = pattern.search(ensemble_name)
-
-        if match is not None:
-            return int(match.group(1))
-        return 0
 
     def run(
         self,
@@ -159,67 +126,6 @@ class CSVExportJob(ErtPlugin):
             f"columns to {output_file}."
         )
         return export_info
-
-    def getArguments(self, parent, ert_config, storage):
-        from ert.gui.ertwidgets import (  # noqa: PLC0415
-            CustomDialog,
-            ListEditBox,
-            PathChooser,
-            PathModel,
-        )
-
-        description = "The CSV export requires some information before it starts:"
-        dialog = CustomDialog("CSV Export", description, parent)
-
-        subs_list = ert_config.substitution_list
-        default_csv_output_path = subs_list.get("<CSV_OUTPUT_PATH>", "output.csv")
-        output_path_model = PathModel(default_csv_output_path)
-        output_path_chooser = PathChooser(output_path_model)
-
-        design_matrix_default = subs_list.get("<DESIGN_MATRIX_PATH>", "")
-        design_matrix_path_model = PathModel(
-            design_matrix_default, is_required=False, must_exist=True
-        )
-        design_matrix_path_chooser = PathChooser(design_matrix_path_model)
-
-        list_edit = ListEditBox(self.getAllEnsembleList(storage))
-
-        infer_iteration_check = QCheckBox()
-        infer_iteration_check.setChecked(True)
-        infer_iteration_check.setToolTip(CSVExportJob.INFER_HELP)
-
-        drop_const_columns_check = QCheckBox()
-        drop_const_columns_check.setChecked(False)
-        drop_const_columns_check.setToolTip(
-            "If checked, exclude columns whose value is the same for every entry"
-        )
-
-        dialog.addLabeledOption("Output file path", output_path_chooser)
-        dialog.addLabeledOption("Design matrix path", design_matrix_path_chooser)
-        dialog.addLabeledOption("List of ensembles to export", list_edit)
-        dialog.addLabeledOption("Infer iteration number", infer_iteration_check)
-        dialog.addLabeledOption("Drop constant columns", drop_const_columns_check)
-
-        dialog.addButtons()
-
-        success = dialog.showAndTell()
-
-        if success:
-            design_matrix_path = design_matrix_path_model.getPath()
-            if not design_matrix_path.strip():
-                design_matrix_path = None
-
-            ensemble_list = ",".join(list_edit.getItems())
-
-            return [
-                output_path_model.getPath(),
-                ensemble_list,
-                design_matrix_path,
-                infer_iteration_check.isChecked(),
-                drop_const_columns_check.isChecked(),
-            ]
-
-        raise CancelPluginException("User cancelled!")
 
     @staticmethod
     def getAllEnsembleList(storage):
