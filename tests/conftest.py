96c3dc2b714159845ee6152ecf808563c6974aa0
Jonathan Karlsen <JONAK@equinor.com>
2024-09-16 13:02:55 +0200

    Improve pytest skip message for missing git-lfs

--- tests/conftest.py
+++ tests/conftest.py
@@ -456,7 +456,8 @@ def block_storage_path(source_root):
     if not path.is_dir():
         pytest.skip(
             "'test-data/block_storage' has not been checked out.\n"
-            "Run: git submodule update --init --recursive"
+            "Make sure you have git-lfs installed and run: "
+            "git submodule update --init --recursive"
         )
     return path.parent
 
