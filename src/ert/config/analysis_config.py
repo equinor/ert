a8c710b91492b9c85f00e0c9bc6dd4e943e1a498
larsevj <levje@equinor.com>
2024-09-17 13:56:52 +0200

    Validate path of design matrix

--- src/ert/config/analysis_config.py
+++ src/ert/config/analysis_config.py
@@ -79,6 +79,16 @@ class AnalysisConfig:
             )
 
         min_realization = min(min_realization, num_realization)
+        design_matrix = config_dict.get(ConfigKeys.DESIGN_MATRIX, None)
+        if design_matrix is not None and Path(design_matrix).suffix not in {
+            ".xlsx",
+            "xls",
+        }:
+            raise ConfigValidationError.with_context(
+                f"DESIGN_MATRIX must be of format .xls or .xslx; is {design_matrix!r}",
+                design_matrix,
+            )
+
         options: Dict[str, Dict[str, Any]] = {"STD_ENKF": {}, "IES_ENKF": {}}
         observation_settings: Dict[str, Any] = {
             "alpha": config_dict.get(ConfigKeys.ENKF_ALPHA, 3.0),
@@ -190,7 +200,7 @@ class AnalysisConfig:
             observation_settings=obs_settings,
             es_module=es_settings,
             ies_module=ies_settings,
-            design_matrix=config_dict.get(ConfigKeys.DESIGN_MATRIX, None),
+            design_matrix=design_matrix,
         )
         return config
 
