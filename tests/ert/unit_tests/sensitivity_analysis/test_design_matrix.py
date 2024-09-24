d6a78a04a72fc756bdc6c2e4f76b1c4f3ce3debb
larsevj <levje@equinor.com>
2024-09-24 17:36:40 +0200

    Improve test and remove designmatrix

--- tests/ert/unit_tests/sensitivity_analysis/test_design_matrix.py
+++ tests/ert/unit_tests/sensitivity_analysis/test_design_matrix.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 
 from ert.config import ErtConfig
@@ -10,28 +11,17 @@ from ert.storage import open_storage
 
 @pytest.mark.usefixtures("copy_poly_case")
 def test_design_matrix(copy_poly_case):
-    config_text = """
-QUEUE_SYSTEM LOCAL
-QUEUE_OPTION LOCAL MAX_RUNNING 50
+    design_matrix_df = pd.DataFrame(
+        {"REAL": [0, 1, 2], "a": [1, 2, 3], "b": [0, 2, 0], "c": [3, 1, 3]}
+    )
+    default_sheet_df = pd.DataFrame()
+    with pd.ExcelWriter("design_matrix.xlsx") as xl_write:
+        design_matrix_df.to_excel(xl_write, index=False, sheet_name="DesignSheet01")
+        default_sheet_df.to_excel(xl_write, index=False, sheet_name="DefaultValues")
 
-RUNPATH poly_out/realization-<IENS>/iter-<ITER>
-
-OBS_CONFIG observations
-REALIZATION_MEMORY 50mb
-
-DESIGN_MATRIX design_matrix.xlsx
-NUM_REALIZATIONS 100
-MIN_REALIZATIONS 1
-
-GEN_KW COEFFS coeff_priors
-GEN_DATA POLY_RES RESULT_FILE:poly.out
-
-INSTALL_JOB poly_eval POLY_EVAL
-FORWARD_MODEL poly_eval
-"""
-    with open("my_config.ert", "w", encoding="utf-8") as fhandle:
-        fhandle.write(config_text)
-    ert_config = ErtConfig.from_file("my_config.ert")
+    with open("poly.ert", "a", encoding="utf-8") as fhandle:
+        fhandle.write("DESIGN_MATRIX design_matrix.xlsx")
+    ert_config = ErtConfig.from_file("poly.ert")
     with open_storage(ert_config.ens_path, mode="w") as ert_storage:
         design_frame = read_design_matrix(ert_config, "design_matrix.xlsx")
         _ensemble = initialize_parameters(
