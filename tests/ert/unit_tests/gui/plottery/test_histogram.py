e6182fff44dd66a89b92f9c6aeddf43ec3984742
Eivind Jahren <ejah@equinor.com>
2024-10-07 13:13:59 +0200

    Make histogram test not integration test & add mpl

--- tests/ert/unit_tests/gui/plottery/test_histogram.py
+++ tests/ert/unit_tests/gui/plottery/test_histogram.py
@@ -59,7 +59,6 @@ def ensemble_to_data_map(request, plot_context):
     return dict.fromkeys(plot_context.ensembles(), request.param)
 
 
-@pytest.mark.integration_test
 @pytest.mark.mpl_image_compare(tolerance=10)
 def test_histogram(plot_context: PlotContext, ensemble_to_data_map):
     figure = Figure()
