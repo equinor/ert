d8e0141947a5ab41a2e1386831e035f3fb6924f9
xjules <jparu@equinor.com>
2024-10-14 09:56:45 +0200

    Add run experiment with design matrix to ensemble experiment panel
    - Prefil active realization box with realizations from design matrix
    - Use design_matrix parameters in ensemble experiment
    - add test run cli with design matrix and poly example
    - add test that save parameters internalize DataFrame parameters in the storage
    - add merge function to merge design parameters with existing parameters

---
+++ test-data/ert/poly_design/poly_eval.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import json
+
+
+def _load_coeffs(filename):
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)["COEFFS"]
+
+
+def _evaluate(coeffs, x):
+    return coeffs["a"] * x**2 + coeffs["b"] * x + coeffs["c"]
+
+
+if __name__ == "__main__":
+    coeffs = _load_coeffs("parameters.json")
+    output = [_evaluate(coeffs, x) for x in range(10)]
+    with open("poly.out", "w", encoding="utf-8") as f:
+        f.write("\n".join(map(str, output)))
