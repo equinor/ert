d849b6a5991c2926437e54d8e53a03e9217bfe2a
Jonathan Karlsen <JONAK@equinor.com>
2024-10-15 13:45:33 +0200

    Add runpath validation in gui load_results_manually
    This commit adds runpath validation so the load button in
    `load_results_manually` will be disabled until the runpath exists, and
    the user has the read/execute permissions.

--- src/ert/validation/__init__.py
+++ src/ert/validation/__init__.py
@@ -6,6 +6,7 @@ from .proper_name_argument import ProperNameArgument
 from .proper_name_format_argument import ProperNameFormatArgument
 from .range_string_argument import RangeStringArgument
 from .rangestring import mask_to_rangestring, rangestring_to_list, rangestring_to_mask
+from .runpath_argument import RunPathArgument
 from .validation_status import ValidationStatus
 
 __all__ = [
@@ -16,6 +17,7 @@ __all__ = [
     "ProperNameArgument",
     "ProperNameFormatArgument",
     "RangeStringArgument",
+    "RunPathArgument",
     "ValidationStatus",
     "mask_to_rangestring",
     "rangestring_to_list",
