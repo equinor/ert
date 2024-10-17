6ecc5f29a2e32e25e2e9a0085e8c7d35c68e0516
Håvard Berland <havb@equinor.com>
2024-10-17 12:12:01 +0200

    wip: can run eclipse on flow_example

--- src/ert/resources/forward_models/res/script/ecl100.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-from ecl_config import Ecl100Config
-from ecl_run import run
-
-if __name__ == "__main__":
-    config = Ecl100Config()
-    run(config, [arg for arg in sys.argv[1:] if len(arg) > 0])
