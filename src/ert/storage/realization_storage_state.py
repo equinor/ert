84c43a3aafc6ea39e031b2c278f985049fd8a295
Yngve S. Kristiansen <ynk@equinor.com>
2024-10-28 09:40:37 +0100

    HAS_DATA -> RESPONSES_LOADED

--- src/ert/storage/realization_storage_state.py
+++ src/ert/storage/realization_storage_state.py
@@ -4,6 +4,6 @@ from enum import Enum
 class RealizationStorageState(Enum):
     UNDEFINED = 1
     PARAMETERS_LOADED = 2
-    HAS_DATA = 4
+    RESPONSES_LOADED = 4
     LOAD_FAILURE = 8
     PARENT_FAILURE = 16
