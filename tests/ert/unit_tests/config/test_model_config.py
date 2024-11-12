ca4ce779a3fd1e5b8293a7a535aa938d8e24d01f
larsevj <levje@equinor.com>
2024-11-12 13:54:52 +0100

    Give warning if runpath disk space is close to full on ert startup

--- tests/ert/unit_tests/config/test_model_config.py
+++ tests/ert/unit_tests/config/test_model_config.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+from unittest.mock import patch
+
 import pytest
 
 from ert.config import ModelConfig
-from ert.config.parsing import ConfigKeys, ConfigValidationError
+from ert.config.parsing import ConfigKeys, ConfigValidationError, ConfigWarning
 
 
 def test_default_model_config_run_path(tmpdir):
@@ -61,3 +64,54 @@ def test_that_invalid_time_map_file_raises_config_validation_error(tmpdir):
 
         with pytest.raises(ConfigValidationError, match="Could not read timemap file"):
             _ = ModelConfig.from_dict({ConfigKeys.TIME_MAP: "time_map.txt"})
+
+
+@pytest.mark.parametrize(
+    "total_space, used_space, to_warn, expected_warning",
+    [
+        pytest.param(
+            10 * 1000**4,  # 10 TB
+            9.75 * 1000**4,  # 9.75 TB
+            False,
+            None,
+            id="Low disk space percentage on large disk",
+        ),
+        pytest.param(
+            100 * 1000**3,  # 100 GB
+            99 * 1000**3,  # 99 GB
+            True,
+            "Low disk space: 1.00 GB free on",
+            id="Low disk space small disk",
+        ),
+        pytest.param(
+            10 * 1000**5,  # 10 PB
+            9.99994 * 1000**5,  # 9.99994 PB
+            True,
+            "Low disk space: 60.00 GB free on",
+            id="Low disk space small disk",
+        ),
+        pytest.param(
+            100 * 1000**3,  # 100 GB
+            75 * 1000**3,  # 75 GB
+            False,
+            None,
+            id="Sufficient disk space",
+        ),
+    ],
+)
+def test_warning_when_full_disk(
+    tmp_path, recwarn, total_space, used_space, to_warn, expected_warning
+):
+    Path(tmp_path / "simulations").mkdir()
+    runpath = f"{tmp_path!s}/simulations/realization-%d/iter-%d"
+    with patch(
+        "ert.config.model_config.shutil.disk_usage",
+        return_value=(total_space, used_space, total_space - used_space),
+    ):
+        if to_warn:
+            with pytest.warns(ConfigWarning, match=expected_warning):
+                _ = ModelConfig(num_realizations=1, runpath_format_string=runpath)
+        else:
+            _ = ModelConfig(num_realizations=1, runpath_format_string=runpath)
+            for w in recwarn:
+                assert not issubclass(w.category, ConfigWarning)
