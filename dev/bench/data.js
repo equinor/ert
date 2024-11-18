626a0cebd098bfd8ce309f290dbe1cd9cdf229b7
github-action-benchmark <github@users.noreply.github.com>
2024-11-18 07:04:03 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for dbb6321c6df23be68d3e486a4d13a6e95a8928e1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,39 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731911398765,
+  "lastUpdate": 1731913443224,
   "repoUrl": "https://github.com/equinor/ert",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "49289030+HakonSohoel@users.noreply.github.com",
-            "name": "Håkon Steinkopf Søhoel",
-            "username": "HakonSohoel"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "efb812cf823994775f4ddea84a3c19b889acf9dd",
-          "message": "Add trace ID to clipboard debug info and title bar (#9157)",
-          "timestamp": "2024-11-12T15:05:33+01:00",
-          "tree_id": "be794f114365a4a6196b469bd1ad04134adc2c69",
-          "url": "https://github.com/equinor/ert/commit/efb812cf823994775f4ddea84a3c19b889acf9dd"
-        },
-        "date": 1731420442192,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
-            "value": 0.19307666222800574,
-            "unit": "iter/sec",
-            "range": "stddev: 0.01839081835385295",
-            "extra": "mean: 5.179289865800001 sec\nrounds: 5"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -930,6 +899,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.02445986601585514",
             "extra": "mean: 5.1758208747999905 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ejah@equinor.com",
+            "name": "Eivind Jahren",
+            "username": "eivindjahren"
+          },
+          "committer": {
+            "email": "ejah@equinor.com",
+            "name": "Eivind Jahren",
+            "username": "eivindjahren"
+          },
+          "distinct": true,
+          "id": "dbb6321c6df23be68d3e486a4d13a6e95a8928e1",
+          "message": "Add test for mean calculation in dark_storage common",
+          "timestamp": "2024-11-18T08:02:15+01:00",
+          "tree_id": "37f1993492c76236526953de60cb4d0600c60e48",
+          "url": "https://github.com/equinor/ert/commit/dbb6321c6df23be68d3e486a4d13a6e95a8928e1"
+        },
+        "date": 1731913442729,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
+            "value": 0.19064655449599555,
+            "unit": "iter/sec",
+            "range": "stddev: 0.02051725654738917",
+            "extra": "mean: 5.245308537800009 sec\nrounds: 5"
           }
         ]
       }
