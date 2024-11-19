3b603d448b4d996f9a6414580688bd0209817802
Yngve S. Kristiansen <ynk@equinor.com>
2024-11-19 11:35:37 +0100

    Add api snapshot test w/ summary data

--- tests/everest/test_api_snapshots.py
+++ tests/everest/test_api_snapshots.py
@@ -1,8 +1,12 @@
+import json
+from datetime import datetime, timedelta
 from typing import Any, Dict
 
 import orjson
+import polars
 import pytest
 
+from ert.config import SummaryConfig
 from ert.run_models.everest_run_model import EverestRunModel
 from everest.api import EverestDataAPI
 from everest.config import EverestConfig
@@ -88,3 +92,49 @@ def test_api_snapshots(
         + "\n"
     )
     snapshot.assert_match(snapshot_str, "snapshot.json")
+
+
+def test_api_summary_snapshot(
+    copy_math_func_test_data_to_tmp, evaluator_server_config_generator, snapshot
+):
+    config = EverestConfig.load_file("config_minimal.yml")
+    run_model = EverestRunModel.create(config)
+    evaluator_server_config = evaluator_server_config_generator(run_model)
+    run_model.run_experiment(evaluator_server_config)
+
+    # Save some summary data to each ensemble
+    experiment = next(run_model._storage.experiments)
+
+    response_config = experiment.response_configuration
+    response_config["summary"] = SummaryConfig(keys=["*"])
+
+    experiment._storage._write_transaction(
+        experiment._path / experiment._responses_file,
+        json.dumps(
+            {c.response_type: c.to_dict() for c in response_config.values()},
+            default=str,
+            indent=2,
+        ).encode("utf-8"),
+    )
+
+    smry_data = polars.DataFrame(
+        {
+            "response_key": ["FOPR", "FOPR", "WOPR", "WOPR", "FOPT", "FOPT"],
+            "time": polars.Series(
+                [datetime(2000, 1, 1) + timedelta(days=i) for i in range(6)]
+            ).dt.cast_time_unit("ms"),
+            "values": polars.Series(
+                [0.2, 0.2, 1.0, 1.1, 3.3, 3.3], dtype=polars.Float32
+            ),
+        }
+    )
+    for ens in experiment.ensembles:
+        for real in range(ens.ensemble_size):
+            ens.save_response("summary", smry_data.clone(), real)
+
+    api = EverestDataAPI(config)
+    dicts = polars.from_pandas(api.summary_values()).to_dicts()
+    snapshot.assert_match(
+        orjson.dumps(dicts, option=orjson.OPT_INDENT_2).decode("utf-8").strip() + "\n",
+        "snapshot.json",
+    )
