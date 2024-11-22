173884f14df85525453eb0129bbc189df1b0a7e9
larsevj <levje@equinor.com>
2024-11-22 14:03:19 +0100

    Set polars max threads to 1 in cli tests

--- tests/ert/ui_tests/cli/conftest.py
+++ tests/ert/ui_tests/cli/conftest.py
@@ -6,7 +6,9 @@ import pytest
 @pytest.fixture(autouse=True)
 def reduce_omp_num_threads_count():
     old_omp_num_threads = os.environ.get("OMP_NUM_THREADS")
+    old_polars_max_thread = os.environ.get("POLARS_MAX_THREADS")
     os.environ["OMP_NUM_THREADS"] = "1"
+    os.environ["POLARS_MAX_THREADS"] = "1"
 
     yield
 
@@ -14,3 +16,7 @@ def reduce_omp_num_threads_count():
         del os.environ["OMP_NUM_THREADS"]
     else:
         os.environ["OMP_NUM_THREADS"] = old_omp_num_threads
+    if old_polars_max_thread is None:
+        del os.environ["POLARS_MAX_THREADS"]
+    else:
+        os.environ["POLARS_MAX_THREADS"] = old_polars_max_thread
