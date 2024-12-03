33d22b2c60883b4b60c345e8fc5ff047c479df41
StephanDeHoop <sted@equinor.com>
2024-12-03 18:10:40 +0100

    More cleanup

--- tests/everest/test_cvar.py
+++ tests/everest/test_cvar.py
@@ -1,8 +1,13 @@
 import pytest
 
 from ert.run_models.everest_run_model import EverestRunModel
-from everest.config import EverestConfig
-from everest.config.cvar_config import CVaRConfig
+from everest.config import (
+    ControlConfig,
+    CVaRConfig,
+    EverestConfig,
+    ModelConfig,
+    OptimizationConfig,
+)
 
 
 @pytest.mark.integration_test
@@ -11,18 +16,27 @@ def test_mathfunc_cvar(
 ):
     # Arrange
     config = EverestConfig.load_file("config_minimal.yml")
-    config.controls[0].perturbation_magnitude = 0.01
-    config.controls[0].min = -2.0
-    config.controls[0].max = 2.0
-    config.controls[0].initial_guess = 0.0
-    config.optimization.backend = "scipy"
-    config.optimization.algorithm = "slsqp"
-    config.optimization.cvar = CVaRConfig(percentile=0.5)
-    config.optimization.max_batch_num = 5
-    config.model.realizations = [0, 1]
-    config.forward_model[0] = (
-        "distance3 --point-file point.json --realization <GEO_ID> --target 0.5 0.5 0.5 --out distance"
+    config.controls = [
+        ControlConfig(
+            name="point",
+            type="generic_control",
+            min=-2.0,
+            max=2.0,
+            initial_guess=0.0,
+            perturbation_magnitude=0.01,
+            variables=[{"name": "x"}, {"name": "y"}, {"name": "z"}],
+        )
+    ]
+    config.optimization = OptimizationConfig(
+        backend="scipy",
+        algorithm="slsqp",
+        cvar=CVaRConfig(percentile=0.5),
+        max_batch_num=5,
     )
+    config.model = ModelConfig(realizations=[0, 1])
+    config.forward_model = [
+        "distance3 --point-file point.json --realization <GEO_ID> --target 0.5 0.5 0.5 --out distance"
+    ]
 
     # Act
     run_model = EverestRunModel.create(config)
