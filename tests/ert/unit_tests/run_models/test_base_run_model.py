equinor/ert

commit 31e607b066ab79415671f83f2d57c7400c4d4e98

Fix progress bar showing finished status for unselected reals
This commit fixes the bug where the gui shows the total count of realizations as the ensemble_size rather than the number of realizations actually selected for the run.

src/ert/run_models/base_run_model.py
@@ -188,7 +188,7 @@ class BaseRunModel(ABC):
         self.minimum_required_realizations = minimum_required_realizations
         self.active_realizations = copy.copy(active_realizations)
         self.start_iteration = start_iteration
-        self.validate()
+        self.validate_active_realizations_count()
 
     def log_at_startup(self) -> None:
         keys_to_drop = [
@@ -404,8 +404,13 @@ class BaseRunModel(ABC):
                 for real in all_realizations.values():
                     status[str(real["status"])] += 1
 
-        status["Finished"] += self.active_realizations.count(False)
+        status["Finished"] += self._get_number_of_finished_realizations_from_reruns()
         return status
+
+    def _get_number_of_finished_realizations_from_reruns(self) -> int:
+        return self.active_realizations.count(
+            False
+        ) - self._initial_realizations_mask.count(False)
 
     def get_memory_consumption(self) -> int:
         max_memory_consumption: int = 0
@@ -423,7 +428,7 @@ class BaseRunModel(ABC):
         done_realizations = self.active_realizations.count(False)
         all_realizations = self._iter_snapshot[current_iter].reals
         current_progress = 0.0
-        realization_count = len(self.active_realizations)
+        realization_count = self.get_number_of_active_realizations()
 
         if all_realizations:
             for real in all_realizations.values():
@@ -639,6 +644,9 @@ class BaseRunModel(ABC):
         return [real_path.exists() for real_path in realization_set].count(True)
 
     def get_number_of_active_realizations(self) -> int:
+        return self._initial_realizations_mask.count(True)
+
+    def get_number_of_successful_realizations(self) -> int:
         return self.active_realizations.count(True)
 
     def rm_run_path(self) -> None:
@@ -646,13 +654,24 @@ class BaseRunModel(ABC):
             if Path(run_path).exists():
                 shutil.rmtree(run_path)
 
-    def validate(self) -> None:
+    def validate_active_realizations_count(self) -> None:
         active_realizations_count = self.get_number_of_active_realizations()
         min_realization_count = self.minimum_required_realizations
 
         if active_realizations_count < min_realization_count:
             raise ValueError(
                 f"Number of active realizations ({active_realizations_count}) is less "
+                f"than the specified MIN_REALIZATIONS"
+                f"({min_realization_count})"
+            )
+
+    def validate_successful_realizations_count(self) -> None:
+        successful_reallizations_count = self.get_number_of_successful_realizations()
+        min_realization_count = self.minimum_required_realizations
+
+        if successful_reallizations_count < min_realization_count:
+            raise ValueError(
+                f"Number of successful realizations ({successful_reallizations_count}) is less "
                 f"than the specified MIN_REALIZATIONS"
                 f"({min_realization_count})"
             )
@@ -697,7 +716,7 @@ class BaseRunModel(ABC):
             self.active_realizations[iens] = False
 
         num_successful_realizations = len(successful_realizations)
-        self.validate()
+        self.validate_successful_realizations_count()
         logger.info(f"Experiment ran on QUEUESYSTEM: {self._queue_config.queue_system}")
         logger.info(f"Experiment ran with number of realizations: {self.ensemble_size}")
         logger.info(
@@ -746,7 +765,7 @@ class UpdateRunModel(BaseRunModel):
     def update(
         self, prior: Ensemble, posterior_name: str, weight: float = 1.0
     ) -> Ensemble:
-        self.validate()
+        self.validate_successful_realizations_count()
         self.send_event(
             RunModelUpdateBeginEvent(iteration=prior.iteration, run_id=prior.id)
         )

src/ert/run_models/iterated_ensemble_smoother.py
@@ -94,7 +94,6 @@ class IteratedEnsembleSmoother(BaseRunModel):
         iteration: int,
         initial_mask: npt.NDArray[np.bool_],
     ) -> None:
-        self.validate()
         self.run_workflows(HookRuntime.PRE_UPDATE, self._storage, prior_storage)
         try:
             _, self.sies_smoother = iterative_smoother_update(

tests/ert/unit_tests/run_models/test_base_run_model.py
@@ -7,6 +7,7 @@ from unittest.mock import MagicMock
 import pytest
 
 from ert.config import ErtConfig, ModelConfig
+from ert.ensemble_evaluator.snapshot import EnsembleSnapshot
 from ert.run_models import BaseRunModel
 from ert.storage import Storage
 from ert.substitutions import Substitutions
@@ -18,7 +19,7 @@ def patch_abstractmethods(monkeypatch):
 
 
 def test_base_run_model_supports_restart(minimum_case):
-    BaseRunModel.validate = MagicMock()
+    BaseRunModel.validate_active_realizations_count = MagicMock()
     brm = BaseRunModel(minimum_case, None, None, minimum_case.queue_config, [True])
     assert brm.support_restart
 
@@ -40,7 +41,7 @@ class MockJob:
     ],
 )
 def test_active_realizations(initials):
-    BaseRunModel.validate = MagicMock()
+    BaseRunModel.validate_active_realizations_count = MagicMock()
     brm = BaseRunModel(MagicMock(), None, None, None, initials)
     brm._initial_realizations_mask = initials
     assert brm.ensemble_size == len(initials)
@@ -60,7 +61,7 @@ def test_active_realizations(initials):
     ],
 )
 def test_failed_realizations(initials, completed, any_failed, failures):
-    BaseRunModel.validate = MagicMock()
+    BaseRunModel.validate_active_realizations_count = MagicMock()
     brm = BaseRunModel(MagicMock(), None, None, None, initials)
     brm._initial_realizations_mask = initials
     brm._completed_realizations_mask = completed
@@ -182,7 +183,7 @@ def test_num_cpu_is_propagated_from_config_to_ensemble(run_args):
     # Given NUM_CPU in the config file has a special value
     config = ErtConfig.from_file_contents("NUM_REALIZATIONS 2\nNUM_CPU 42")
     # Set up a BaseRunModel object from the config above:
-    BaseRunModel.validate = MagicMock()
+    BaseRunModel.validate_active_realizations_count = MagicMock()
     brm = BaseRunModel(
         config=config,
         storage=MagicMock(spec=Storage),
@@ -199,3 +200,102 @@ def test_num_cpu_is_propagated_from_config_to_ensemble(run_args):
     # Assert the built ensemble has the correct NUM_CPU information
     assert ensemble.reals[0].num_cpu == 42
     assert ensemble.reals[1].num_cpu == 42
+
+
+@pytest.mark.parametrize(
+    "initial_active_realizations, new_active_realizations, real_status_dict, expected_result",
+    [
+        pytest.param(
+            [True, True, True],
+            [False, False, False],
+            {},
+            {"Finished": 3},
+            id="all_realizations_in_previous_run_succeeded",
+        ),
+        pytest.param(
+            [True, True, True],
+            [False, True, False],
+            {},
+            {"Finished": 2},
+            id="some_realizations_in_previous_run_succeeded",
+        ),
+        pytest.param(
+            [True, True, True],
+            [True, True, True],
+            {},
+            {"Finished": 0},
+            id="no_realizations_in_previous_run_succeeded",
+        ),
+        pytest.param(
+            [False, True, True],
+            [False, False, True],
+            {},
+            {"Finished": 1},
+            id="did_not_run_all_realizations_and_some_succeeded",
+        ),
+        pytest.param(
+            [False, True, True],
+            [False, True, True],
+            {},
+            {"Finished": 0},
+            id="did_not_run_all_realizations_and_none_succeeded",
+        ),
+        pytest.param(
+            [True, True, True],
+            [True, True, True],
+            {"0": "Finished", "1": "Finished", "2": "Finished"},
+            {"Finished": 3},
+            id="ran_all_realizations_and_all_succeeded",
+        ),
+        pytest.param(
+            [True, True, True],
+            [True, True, True],
+            {"0": "Finished", "1": "Finished", "2": "Failed"},
+            {"Finished": 2, "Failed": 1},
+            id="ran_all_realizations_and_some_failed",
+        ),
+        pytest.param(
+            [True, True, True],
+            [True, True, True],
+            {"0": "Finished", "1": "Running", "2": "Failed"},
+            {"Finished": 1, "Failed": 1, "Running": 1},
+            id="ran_all_realizations_and_result_was_mixed",
+        ),
+        pytest.param(
+            [True, True, True],
+            [True, True, False],
+            {"0": "Finished", "1": "Finished"},
+            {"Finished": 3},
+            id="reran_some_realizations_and_all_finished",
+        ),
+        pytest.param(
+            [False, True, True],
+            [False, True, False],
+            {"1": "Finished"},
+            {"Finished": 2},
+            id="did_not_run_all_realizations_then_reran_and_the_realizations_finished",
+        ),
+    ],
+)
+def test_get_current_status(
+    initial_active_realizations,
+    new_active_realizations,
+    real_status_dict: dict[str, str],
+    expected_result,
+):
+    """Active realizations gets changed when we choose to rerun, and the result from the previous run should be included in the current_status."""
+    config = ErtConfig.from_file_contents("NUM_REALIZATIONS 3")
+    brm = BaseRunModel(
+        config=config,
+        storage=MagicMock(spec=Storage),
+        queue_config=config.queue_config,
+        status_queue=MagicMock(spec=SimpleQueue),
+        active_realizations=initial_active_realizations,
+    )
+    snapshot_dict_reals = {}
+    for index, realization_status in real_status_dict.items():
+        snapshot_dict_reals[index] = {"status": realization_status}
+    iter_snapshot = EnsembleSnapshot.from_nested_dict({"reals": snapshot_dict_reals})
+    brm._iter_snapshot[0] = iter_snapshot
+    brm.active_realizations = new_active_realizations
+    assert dict(brm.get_current_status()) == expected_result

tests/ert/ui_tests/cli/test_missing_runpath.py
@@ -54,7 +54,7 @@ def test_missing_runpath_has_isolated_failures(tmp_path, monkeypatch):
     try:
         with pytest.raises(
             ErtCliError,
-            match=r"active realizations \(9\) is less than .* MIN_REALIZATIONS\(10\)",
+            match=r"successful realizations \(9\) is less than .* MIN_REALIZATIONS\(10\)",
         ):
             run_cli_with_pm(
                 ["ensemble_experiment", "config.ert", "--disable-monitoring"]
@@ -96,7 +96,7 @@ def test_failing_writes_lead_to_isolated_failures(tmp_path, monkeypatch, pytestc
     )
     with pytest.raises(
         ErtCliError,
-        match=r"(?s)active realizations \(9\) is less than .* MIN_REALIZATIONS\(10\).*"
+        match=r"(?s)successful realizations \(9\) is less than .* MIN_REALIZATIONS\(10\).*"
         r"Driver reported: Could not create submit script: Don't like realization-1",
     ), patch_raising_named_temporary_file(
         queue_system.lower()

tests/ert/unit_tests/cli/test_model_hook_order.py
@@ -28,7 +28,10 @@ EXPECTED_CALL_ORDER = [
 @pytest.fixture
 def patch_base_run_model(monkeypatch):
     monkeypatch.setattr(base_run_model, "create_run_path", MagicMock())
-    monkeypatch.setattr(BaseRunModel, "validate", MagicMock())
+    monkeypatch.setattr(BaseRunModel, "validate_active_realizations_count", MagicMock())
+    monkeypatch.setattr(
+        BaseRunModel, "validate_successful_realizations_count", MagicMock()
+    )
     monkeypatch.setattr(BaseRunModel, "set_env_key", MagicMock())
 
 

tests/ert/unit_tests/run_models/test_ensemble_experiment.py
@@ -25,7 +25,8 @@ def test_check_if_runpath_exists(
             return [f"out/realization-{r}/iter-{iteration}" for r in realizations]
         return [f"out/realization-{r}" for r in realizations]
 
-    EnsembleExperiment.validate = MagicMock()
+    EnsembleExperiment.validate_active_realizations_count = MagicMock()
+    EnsembleExperiment.validate_successful_realizations_count = MagicMock()
     ensemble_experiment = EnsembleExperiment(
         *[MagicMock()] * 2 + [active_mask, MagicMock(), None] + [MagicMock()] * 4
     )

tests/ert/unit_tests/run_models/test_model_factory.py
@@ -205,8 +205,17 @@ def test_multiple_data_assimilation_restart_paths(
         prior_ensemble_id=str(uuid1()),
         experiment_name=None,
     )
+
     monkeypatch.setattr(
-        ert.run_models.base_run_model.BaseRunModel, "validate", MagicMock()
+        ert.run_models.base_run_model.BaseRunModel,
+        "validate_active_realizations_count",
+        MagicMock(),
+    )
+
+    monkeypatch.setattr(
+        ert.run_models.base_run_model.BaseRunModel,
+        "validate_successful_realizations_count",
+        MagicMock(),
     )
     storage_mock = MagicMock()
     ensemble_mock = MagicMock()
@@ -261,7 +270,15 @@ def test_evaluate_ensemble_paths(
 ):
     monkeypatch.chdir(tmp_path)
     monkeypatch.setattr(
-        ert.run_models.base_run_model.BaseRunModel, "validate", MagicMock()
+        ert.run_models.base_run_model.BaseRunModel,
+        "validate_active_realizations_count",
+        MagicMock(),
+    )
+
+    monkeypatch.setattr(
+        ert.run_models.base_run_model.BaseRunModel,
+        "validate_successful_realizations_count",
+        MagicMock(),
     )
     storage_mock = MagicMock()
     ensemble_mock = MagicMock()

commit 0bacf95c816577f4a62a8f691549c23859f2a055

Fix progress bar not updating realization count for new iterations
This commit fixes the bug introduced in 31e607b066ab79415671f83f2d57c7400c4d4e98, where the status reporting in GUI was done the same way when rerunning failed realizations, and running new iterations. This is an issue because when rerunning failed realizations, we want to show all realizations and add the finished/failed count from the previous run, while new iterations should drop the failed realizations altogether.

src/ert/run_models/base_run_model.py
@@ -188,6 +188,7 @@ class BaseRunModel(ABC):
         self.minimum_required_realizations = minimum_required_realizations
         self.active_realizations = copy.copy(active_realizations)
         self.start_iteration = start_iteration
+        self.restart = False
 
     def log_at_startup(self) -> None:
         keys_to_drop = [
@@ -405,7 +406,10 @@ class BaseRunModel(ABC):
                 for real in all_realizations.values():
                     status[str(real["status"])] += 1
 
-        status["Finished"] += self._get_number_of_finished_realizations_from_reruns()
+        if self.restart:
+            status["Finished"] += (
+                self._get_number_of_finished_realizations_from_reruns()
+            )
         return status
 
     def _get_number_of_finished_realizations_from_reruns(self) -> int:
@@ -653,7 +657,11 @@ class BaseRunModel(ABC):
         return [real_path.exists() for real_path in realization_set].count(True)
 
     def get_number_of_active_realizations(self) -> int:
-        return self._initial_realizations_mask.count(True)
+        return (
+            self._initial_realizations_mask.count(True)
+            if self.restart
+            else self.active_realizations.count(True)
+        )
 
     def get_number_of_successful_realizations(self) -> int:
         return self.active_realizations.count(True)

src/ert/run_models/ensemble_experiment.py
@@ -63,6 +63,7 @@ class EnsembleExperiment(BaseRunModel):
         restart: bool = False,
     ) -> None:
         self.log_at_startup()
+        self.restart = restart
         if not restart:
             self.experiment = self._storage.create_experiment(
                 name=self.experiment_name,

src/ert/run_models/ensemble_smoother.py
@@ -62,6 +62,7 @@ class EnsembleSmoother(UpdateRunModel):
         self, evaluator_server_config: EvaluatorServerConfig, restart: bool = False
     ) -> None:
         self.log_at_startup()
+        self.restart = restart
         ensemble_format = self.target_ensemble_format
         experiment = self._storage.create_experiment(
             parameters=self.ert_config.ensemble_config.parameter_configuration,

src/ert/run_models/evaluate_ensemble.py
@@ -64,6 +64,7 @@ class EvaluateEnsemble(BaseRunModel):
         self, evaluator_server_config: EvaluatorServerConfig, restart: bool = False
     ) -> None:
         self.log_at_startup()
+        self.restart = restart
         ensemble = self.ensemble
         experiment = ensemble.experiment
         self.set_env_key("_ERT_EXPERIMENT_ID", str(experiment.id))

src/ert/run_models/everest_run_model.py
@@ -267,6 +267,7 @@ class EverestRunModel(BaseRunModel):
         self, evaluator_server_config: EvaluatorServerConfig, restart: bool = False
     ) -> None:
         self.log_at_startup()
+        self.restart = restart
         self.eval_server_cfg = evaluator_server_config
         self._experiment = self._storage.create_experiment(
             name=f"EnOpt@{datetime.datetime.now().strftime('%Y-%m-%d@%H:%M:%S')}",

src/ert/run_models/iterated_ensemble_smoother.py
@@ -122,7 +122,7 @@ class IteratedEnsembleSmoother(BaseRunModel):
         self, evaluator_server_config: EvaluatorServerConfig, restart: bool = False
     ) -> None:
         self.log_at_startup()
-
+        self.restart = restart
         target_ensemble_format = self.target_ensemble_format
         experiment = self._storage.create_experiment(
             parameters=self.ert_config.ensemble_config.parameter_configuration,

src/ert/run_models/multiple_data_assimilation.py
@@ -85,6 +85,7 @@ class MultipleDataAssimilation(UpdateRunModel):
         self, evaluator_server_config: EvaluatorServerConfig, restart: bool = False
     ) -> None:
         self.log_at_startup()
+        self.restart = restart
         if self.restart_run:
             id = self.prior_ensemble_id
             try:

tests/ert/unit_tests/run_models/test_base_run_model.py
@@ -203,6 +203,49 @@ def test_num_cpu_is_propagated_from_config_to_ensemble(run_args):
 
 
 @pytest.mark.parametrize(
+    "real_status_dict, expected_result",
+    [
+        pytest.param(
+            {"0": "Finished", "1": "Finished", "2": "Finished"},
+            {"Finished": 3},
+            id="ran_all_realizations_and_all_succeeded",
+        ),
+        pytest.param(
+            {"0": "Finished", "1": "Finished", "2": "Failed"},
+            {"Finished": 2, "Failed": 1},
+            id="ran_all_realizations_and_some_failed",
+        ),
+        pytest.param(
+            {"0": "Finished", "1": "Running", "2": "Failed"},
+            {"Finished": 1, "Failed": 1, "Running": 1},
+            id="ran_all_realizations_and_result_was_mixed",
+        ),
+    ],
+)
+def test_get_current_status(
+    real_status_dict,
+    expected_result,
+):
+    config = ErtConfig.from_file_contents("NUM_REALIZATIONS 3")
+    initial_active_realizations = [True] * 3
+    new_active_realizations = [True] * 3
+    brm = BaseRunModel(
+        config=config,
+        storage=MagicMock(spec=Storage),
+        queue_config=config.queue_config,
+        status_queue=MagicMock(spec=SimpleQueue),
+        active_realizations=initial_active_realizations,
+    )
+    snapshot_dict_reals = {}
+    for index, realization_status in real_status_dict.items():
+        snapshot_dict_reals[index] = {"status": realization_status}
+    iter_snapshot = EnsembleSnapshot.from_nested_dict({"reals": snapshot_dict_reals})
+    brm._iter_snapshot[0] = iter_snapshot
+    brm.active_realizations = new_active_realizations
+    assert dict(brm.get_current_status()) == expected_result
+
+
+@pytest.mark.parametrize(
     "initial_active_realizations, new_active_realizations, real_status_dict, expected_result",
     [
         pytest.param(
@@ -242,27 +285,6 @@ def test_num_cpu_is_propagated_from_config_to_ensemble(run_args):
         ),
         pytest.param(
             [True, True, True],
-            [True, True, True],
-            {"0": "Finished", "1": "Finished", "2": "Finished"},
-            {"Finished": 3},
-            id="ran_all_realizations_and_all_succeeded",
-        ),
-        pytest.param(
-            [True, True, True],
-            [True, True, True],
-            {"0": "Finished", "1": "Finished", "2": "Failed"},
-            {"Finished": 2, "Failed": 1},
-            id="ran_all_realizations_and_some_failed",
-        ),
-        pytest.param(
-            [True, True, True],
-            [True, True, True],
-            {"0": "Finished", "1": "Running", "2": "Failed"},
-            {"Finished": 1, "Failed": 1, "Running": 1},
-            id="ran_all_realizations_and_result_was_mixed",
-        ),
-        pytest.param(
-            [True, True, True],
             [True, True, False],
             {"0": "Finished", "1": "Finished"},
             {"Finished": 3},
@@ -277,7 +299,7 @@ def test_num_cpu_is_propagated_from_config_to_ensemble(run_args):
         ),
     ],
 )
-def test_get_current_status(
+def test_get_current_status_when_rerun(
     initial_active_realizations,
     new_active_realizations,
     real_status_dict: dict[str, str],
@@ -292,6 +314,7 @@ def test_get_current_status(
         status_queue=MagicMock(spec=SimpleQueue),
         active_realizations=initial_active_realizations,
     )
+    brm.restart = True
     snapshot_dict_reals = {}
     for index, realization_status in real_status_dict.items():
         snapshot_dict_reals[index] = {"status": realization_status}
@@ -299,3 +322,65 @@ def test_get_current_status(
     brm._iter_snapshot[0] = iter_snapshot
     brm.active_realizations = new_active_realizations
     assert dict(brm.get_current_status()) == expected_result
+
+
+def test_get_current_status_for_new_iteration_when_realization_failed_in_previous_run():
+    """Active realizations gets changed when we run next iteration, and the failed realizations from
+    the previous run should not be present in the current_status."""
+    initial_active_realizations = [True] * 5
+    # Realization 0,1, and 3 failed in the previous iteration
+    new_active_realizations = [False, False, True, False, True]
+    config = ErtConfig.from_file_contents("NUM_REALIZATIONS 5")
+    brm = BaseRunModel(
+        config=config,
+        storage=MagicMock(spec=Storage),
+        queue_config=config.queue_config,
+        status_queue=MagicMock(spec=SimpleQueue),
+        active_realizations=initial_active_realizations,
+    )
+    snapshot_dict_reals = {
+        "2": {"status": "Running"},
+        "4": {"status": "Finished"},
+    }
+    iter_snapshot = EnsembleSnapshot.from_nested_dict({"reals": snapshot_dict_reals})
+    brm._iter_snapshot[0] = iter_snapshot
+    brm.active_realizations = new_active_realizations
+
+    assert brm.restart is False
+    assert dict(brm.get_current_status()) == {"Running": 1, "Finished": 1}
+
+
+@pytest.mark.parametrize(
+    "new_active_realizations, was_rerun, expected_result",
+    [
+        pytest.param(
+            [False, False, False, True, False],
+            True,
+            5,
+            id="rerun_so_total_realization_count_is_not_affected_by_previous_failed_realizations",
+        ),
+        pytest.param(
+            [True, True, False, False, False],
+            False,
+            2,
+            id="new_iteration_so_total_realization_count_is_only_previously_successful_realizations",
+        ),
+    ],
+)
+def test_get_number_of_active_realizations_varies_when_rerun_or_new_iteration(
+    new_active_realizations, was_rerun, expected_result
+):
+    """When rerunning, we include all realizations in the total amount of active realization.
+    When running a new iteration based on the result of the previous iteration, we only include the successful realizations."""
+    initial_active_realizations = [True] * 5
+    config = ErtConfig.from_file_contents("NUM_REALIZATIONS 5")
+    brm = BaseRunModel(
+        config=config,
+        storage=MagicMock(spec=Storage),
+        queue_config=config.queue_config,
+        status_queue=MagicMock(spec=SimpleQueue),
+        active_realizations=initial_active_realizations,
+    )
+    brm.active_realizations = new_active_realizations
+    brm.restart = was_rerun
+    assert brm.get_number_of_active_realizations() == expected_result