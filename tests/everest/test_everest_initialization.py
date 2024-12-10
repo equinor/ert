7508826a5fd7a8b26f66d1ac366163839c043ab3
Øyvind Eide <eide.oyvind87@gmail.com>
2024-12-10 12:32:29 +0100

    Remove unused marker

--- tests/everest/test_everest_initialization.py
+++ tests/everest/test_everest_initialization.py
@@ -5,13 +5,7 @@ import pytest
 from ert.run_models.everest_run_model import EverestRunModel
 from everest.config import EverestConfig
 
-NO_PROJECT_RES = (
-    os.environ.get("NO_PROJECT_RES", False),
-    "Skipping tests when no access to /project/res",
-)
 
-
-@pytest.mark.skipif(NO_PROJECT_RES[0], reason=NO_PROJECT_RES[1])
 def test_init_no_project_res(copy_egg_test_data_to_tmp):
     config_file = os.path.join("everest", "model", "config.yml")
     config = EverestConfig.load_file(config_file)
