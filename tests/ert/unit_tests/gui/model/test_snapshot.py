157123c467237bd89d31de94e87b9b7c37d38625
larsevj <levje@equinor.com>
2024-12-18 14:29:30 +0100

    Remove printing of snapshots in gui test

--- tests/ert/unit_tests/gui/model/test_snapshot.py
+++ tests/ert/unit_tests/gui/model/test_snapshot.py
@@ -37,7 +37,6 @@ def test_realization_sort_order(full_snapshot):
         iter_index = model.index(i, 0, model.index(0, 0, QModelIndex()))
 
         assert str(i) == str(iter_index.internalPointer().id_)
-        print(i, iter_index.internalPointer())
 
 
 def test_realization_state_is_queue_finalized_state(fail_snapshot):
