45a4b7ca72b6bdda0a777d7a4712d19d97ab32b6
Håvard Berland <havb@equinor.com>
2024-12-30 10:42:31 +0100

    Workaround Python bug for shutil.copytree() exceptions
    See https://github.com/python/cpython/issues/102931
    
    This PR will detect if the bug is triggered, and massage the data
    accordingly. Patch is prepared upstream destined for Python 3.14.
    
    Existing tests are split for readability and preciseness, and extended
    to test that the workaround is performing as it should.

--- src/ert/resources/shell_scripts/copy_directory.py
+++ src/ert/resources/shell_scripts/copy_directory.py
@@ -18,10 +18,23 @@ def copy_directory(src_path: str, target_path: str) -> None:
         print(f"Copying directory structure {src_path} -> {target_path}")
         if os.path.isdir(target_path):
             target_path = os.path.join(target_path, src_basename)
-        shutil.copytree(src_path, target_path, dirs_exist_ok=True)
+        try:
+            shutil.copytree(src_path, target_path, dirs_exist_ok=True)
+        except shutil.Error as err:
+            # Check for shutil bug in Python <3.14:
+            if len(err.args[0]) > 10 and {
+                len(somestring) for somestring in err.args[0]
+            } == {1}:
+                # https://github.com/python/cpython/issues/102931
+                # This can only occur when the shutil.Error is a single error
+                raise OSError("".join(err.args[0])) from err
+            else:
+                raise OSError(
+                    ", ".join([err_arg[2] for err_arg in err.args[0]])
+                ) from err
     else:
         raise OSError(
-            f"Input argument:'{src_path}' "
+            f"Input argument: '{src_path}' "
             "does not correspond to an existing directory"
         )
 
@@ -31,7 +44,5 @@ if __name__ == "__main__":
     target_path = sys.argv[2]
     try:
         copy_directory(src_path, target_path)
-    except OSError as e:
-        sys.exit(
-            f"COPY_DIRECTORY failed with the following error: {''.join(e.args[0])}"
-        )
+    except OSError as oserror:
+        sys.exit(f"COPY_DIRECTORY failed with the following error(s): {oserror}")
