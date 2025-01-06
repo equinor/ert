c5c4996c6753b5ba12cd698a7e22097a47c2bebb
DanSava <dan.sava42@gmail.com>
2025-01-06 16:51:43 +0200

    Make sure FullSnapshotEvent and SnapshotUpdateEvent is json serializable

--- src/ert/ensemble_evaluator/event.py
+++ src/ert/ensemble_evaluator/event.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 
 from .snapshot import EnsembleSnapshot
@@ -27,3 +28,32 @@ class SnapshotUpdateEvent(_UpdateEvent):
 class EndEvent:
     failed: bool
     msg: str | None = None
+
+
+def snapshot_event_from_json(json_str: str) -> FullSnapshotEvent | SnapshotUpdateEvent:
+    json_dict = json.loads(json_str)
+    snapshot = EnsembleSnapshot.from_nested_dict(json_dict["snapshot"])
+    json_dict["snapshot"] = snapshot
+    match json_dict.pop("type"):
+        case "FullSnapshotEvent":
+            return FullSnapshotEvent(**json_dict)
+        case "SnapshotUpdateEvent":
+            return SnapshotUpdateEvent(**json_dict)
+        case unknown:
+            raise TypeError(f"Unknown snapshot update event type {unknown}")
+
+
+def snapshot_event_to_json(event: FullSnapshotEvent | SnapshotUpdateEvent) -> str:
+    assert event.snapshot is not None
+    return json.dumps(
+        {
+            "iteration_label": event.iteration_label,
+            "total_iterations": event.total_iterations,
+            "progress": event.progress,
+            "realization_count": event.realization_count,
+            "status_count": event.status_count,
+            "iteration": event.iteration,
+            "snapshot": event.snapshot.to_dict(),
+            "type": event.__class__.__name__,
+        }
+    )
