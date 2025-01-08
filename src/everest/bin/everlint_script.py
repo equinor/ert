25bc9260de3d6be2a039cf5e36c3583a1c1af83e
DanSava <dan.sava42@gmail.com>
2025-01-08 16:41:22 +0200

    Ignore non ConfigWarnings when running everest lint command

--- src/everest/bin/everlint_script.py
+++ src/everest/bin/everlint_script.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import warnings
 from functools import partial
 
+from ert.config.parsing.config_errors import ConfigWarning
 from everest.config import EverestConfig
 
 
@@ -17,10 +19,22 @@ def _build_args_parser():
         type=partial(EverestConfig.load_file_with_argparser, parser=arg_parser),
         help="The path to the everest configuration file",
     )
+    arg_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Display verbose errors and warnings",
+    )
     return arg_parser
 
 
 def lint_entry(args=None):
+    match any("-v" in arg or "--verbose" in arg for arg in args):
+        case False:
+            warnings.filterwarnings("ignore")
+            warnings.filterwarnings("default", category=ConfigWarning)
+        case True:
+            pass
     parser = _build_args_parser()
     options = parser.parse_args(args)
     parsed_config = options.config_file
