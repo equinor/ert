a418b45e42ea8cb7a0cda1d2b16369836b121009
github-action-benchmark <github@users.noreply.github.com>
2025-01-14 12:54:58 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 24911859160476b4e16c1e4cae7be912b61a336a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,39 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736847178264,
+  "lastUpdate": 1736859298868,
   "repoUrl": "https://github.com/equinor/ert",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "ejah@equinor.com",
-            "name": "Eivind Jahren",
-            "username": "eivindjahren"
-          },
-          "committer": {
-            "email": "ejah@equinor.com",
-            "name": "Eivind Jahren",
-            "username": "eivindjahren"
-          },
-          "distinct": true,
-          "id": "383d6454c982da42a08c1f864fa85aa4c19f21e1",
-          "message": "Use resfo for snake_oil_simulator",
-          "timestamp": "2025-01-08T09:03:41+01:00",
-          "tree_id": "75087374f7c1d0cad7d051167a04c21a69ba62ce",
-          "url": "https://github.com/equinor/ert/commit/383d6454c982da42a08c1f864fa85aa4c19f21e1"
-        },
-        "date": 1736323538773,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
-            "value": 0.2137166107562481,
-            "unit": "iter/sec",
-            "range": "stddev: 0.011818798830819769",
-            "extra": "mean: 4.679093480199993 sec\nrounds: 5"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -930,6 +899,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.029260392279015925",
             "extra": "mean: 4.5455263946 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "parulek@gmail.com",
+            "name": "Julius Parulek",
+            "username": "xjules"
+          },
+          "committer": {
+            "email": "jparu@equinor.com",
+            "name": "Julius Parulek",
+            "username": "xjules"
+          },
+          "distinct": true,
+          "id": "24911859160476b4e16c1e4cae7be912b61a336a",
+          "message": "Allow for support for multiple occurences of design matrices\n\n- Add merging strategy for multiple design matrices in the config\n- Add ui tests for multi dm occurence",
+          "timestamp": "2025-01-14T13:53:01+01:00",
+          "tree_id": "3782ec43fee2972077aa51607532239b38abba0b",
+          "url": "https://github.com/equinor/ert/commit/24911859160476b4e16c1e4cae7be912b61a336a"
+        },
+        "date": 1736859256914,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/ert/performance_tests/test_analysis.py::test_and_benchmark_adaptive_localization_with_fields",
+            "value": 0.2139636994132702,
+            "unit": "iter/sec",
+            "range": "stddev: 0.020972502608077006",
+            "extra": "mean: 4.673689989200005 sec\nrounds: 5"
           }
         ]
       }
