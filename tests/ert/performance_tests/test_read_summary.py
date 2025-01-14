789e06f9eb5fb65dd8f5f246c0ea17b3d8d50519
Eivind Jahren <ejah@equinor.com>
2025-01-14 09:37:51 +0100

    Reduce time taken in performance test

--- tests/ert/performance_tests/test_read_summary.py
+++ tests/ert/performance_tests/test_read_summary.py
@@ -1,9 +1,11 @@
+import pytest
 from hypothesis import given
 
 from ert.config._read_summary import read_summary
 from tests.ert.unit_tests.config.summary_generator import summaries
 
 
+@pytest.mark.timeout(100)
 @given(summaries())
 def test_that_length_of_fetch_keys_does_not_reduce_performance(
     tmp_path_factory, summary
@@ -15,7 +17,7 @@ def test_that_length_of_fetch_keys_does_not_reduce_performance(
     smspec, unsmry = summary
     unsmry.to_file(tmp_path / "TEST.UNSMRY")
     smspec.to_file(tmp_path / "TEST.SMSPEC")
-    fetch_keys = [str(i) for i in range(100000)]
+    fetch_keys = [str(i) for i in range(1000)]
     (_, keys, time_map, _) = read_summary(str(tmp_path / "TEST"), fetch_keys)
     assert all(k in fetch_keys for k in keys)
     assert len(time_map) == len(unsmry.steps)
