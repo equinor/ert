8fe48195857d007433d6fc9dbbbae01282b6c06c
Yngve S. Kristiansen <ynk@equinor.com>
2025-01-15 14:12:04 +0100

    review: Stop storing result_id

--- src/everest/api/everest_data_api.py
+++ src/everest/api/everest_data_api.py
@@ -173,7 +173,7 @@ class EverestDataAPI:
         if not all_batch_data:
             return []
 
-        all_info = polars.concat(all_batch_data).drop("result_id")
+        all_info = polars.concat(all_batch_data)
         objective_columns = [
             c
             for c in all_info.drop(["batch_id", "control_name"]).columns
