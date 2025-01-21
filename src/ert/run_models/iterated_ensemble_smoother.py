36345a67449edb8d6fc4792285987971e3ce24af
Jonathan Karlsen <JONAK@equinor.com>
2025-01-21 10:37:42 +0100

    Fix rerun button not disabled for ES-MDA
    This commit fixes the issue where the rerun button was enabled for ES-MDA and ensemble smoother. The issue was due to us setting support_restart before calling the super class's constructor which overwrote it to False.

--- src/ert/run_models/iterated_ensemble_smoother.py
+++ src/ert/run_models/iterated_ensemble_smoother.py
@@ -49,7 +49,6 @@ class IteratedEnsembleSmoother(BaseRunModel):
         update_settings: UpdateSettings,
         status_queue: SimpleQueue[StatusEvents],
     ):
-        self.support_restart = False
         self.analysis_config = analysis_config
         self.update_settings = update_settings
 
@@ -72,6 +71,7 @@ class IteratedEnsembleSmoother(BaseRunModel):
             random_seed=random_seed,
             minimum_required_realizations=minimum_required_realizations,
         )
+        self.support_restart = False
 
         # Initialize sies_smoother to None
         # It is initialized later, but kept track of here
