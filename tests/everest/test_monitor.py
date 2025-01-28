82357efeabb07aedbd7c273d6ffbfafdf02ad770
Øyvind Eide <eide.oyvind87@gmail.com>
2025-01-28 14:28:03 +0100

    Add monitoring of failed jobs

--- tests/everest/test_monitor.py
+++ tests/everest/test_monitor.py
@@ -104,6 +104,65 @@ def snapshot_update_event():
     yield json.dumps(jsonable_encoder(event))
 
 
+@pytest.fixture
+def snapshot_update_failure_event():
+    event = SnapshotUpdateEvent(
+        snapshot=SnapshotBuilder(metadata=METADATA)
+        .add_fm_step(
+            fm_step_id="0",
+            name=None,
+            index="0",
+            status=state.FORWARD_MODEL_STATE_FAILURE,
+            end_time=datetime(2019, 1, 1),
+            error="The run is cancelled due to reaching MAX_RUNTIME",
+        )
+        .build(
+            real_ids=["1"],
+            status=state.REALIZATION_STATE_FAILED,
+        ),
+        iteration_label="Foo",
+        total_iterations=1,
+        progress=0.5,
+        realization_count=4,
+        status_count={"Finished": 0, "Running": 0, "Unknown": 0, "Failed": 1},
+        iteration=0,
+    )
+    yield json.dumps(jsonable_encoder(event))
+
+
+def test_failed_jobs_monitor(
+    monkeypatch, full_snapshot_event, snapshot_update_failure_event, capsys
+):
+    server_mock = MagicMock()
+    connection_mock = MagicMock(spec=ClientConnection)
+    connection_mock.recv.side_effect = [
+        full_snapshot_event,
+        snapshot_update_failure_event,
+        json.dumps(jsonable_encoder(EndEvent(failed=True, msg="Failed"))),
+    ]
+    server_mock.return_value.__enter__.return_value = connection_mock
+    monkeypatch.setattr(everest.detached, "_query_server", MagicMock(return_value={}))
+    monkeypatch.setattr(everest.detached, "connect", server_mock)
+    monkeypatch.setattr(everest.detached, "ssl", MagicMock())
+    patched = partial(everest.detached.start_monitor, polling_interval=0.1)
+    with patch("everest.bin.utils.start_monitor", patched):
+        run_detached_monitor(
+            ("some/url", "cert", ("username", "password")), "output", False
+        )
+    captured = capsys.readouterr()
+    expected = [
+        "===================== Running forward models (Batch #0) ======================\n",
+        "  Waiting: 0 | Pending: 0 | Running: 0 | Finished: 0 | Failed: 1\n",
+        "  fm_step_0: 1/0/1 | Failed: 1"
+        "  fm_step_0: Failed: The run is cancelled due to reaching MAX_RUNTIME, realizations: 1\n",
+        "Failed\n",
+    ]
+    # Ignore whitespace
+    assert captured.out.translate({ord(c): None for c in string.whitespace}) == "".join(
+        expected
+    ).translate({ord(c): None for c in string.whitespace})
+
+
 @pytest.mark.parametrize("show_all_jobs", [True, False])
 def test_monitor(
     monkeypatch, full_snapshot_event, snapshot_update_event, capsys, show_all_jobs
