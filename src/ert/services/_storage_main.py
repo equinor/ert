298acca7ee6009c736252b46442aa96a064e81d8
Jon Holba <jholba@equinor.com>
2025-01-28 12:44:11 +0100

    Change port range of dark storage
    There is an issue where dark storage would try to use the same port as
    ensemble evaluator. Switching the port range for dark storage should
    fix this problem until we can fix the bug.

--- src/ert/services/_storage_main.py
+++ src/ert/services/_storage_main.py
@@ -96,7 +96,9 @@ def run_server(args: argparse.Namespace | None = None, debug: bool = False) -> N
         config_args.update(reload=True, reload_dirs=[os.path.dirname(ert_shared_path)])
         os.environ["ERT_STORAGE_DEBUG"] = "1"
 
-    sock = find_available_socket(custom_host=args.host)
+    sock = find_available_socket(
+        custom_host=args.host, custom_range=range(51850, 51870)
+    )
     connection_info = _create_connection_info(sock, authtoken)
 
     # Appropriated from uvicorn.main:run
