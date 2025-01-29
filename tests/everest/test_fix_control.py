deeff70842246f9b87e7786cbf4e3afeb5a33f33
Frode Aarstad <frodeaarstad@gmail.com>
2025-01-29 07:44:55 +0100

    Set max_batch_num to 2 to make test faster

--- tests/everest/test_fix_control.py
+++ tests/everest/test_fix_control.py
@@ -12,6 +12,7 @@ def test_fix_control(
 ):
     config = EverestConfig.load_file(CONFIG_FILE_ADVANCED)
     config.controls[0].variables[0].enabled = False
+    config.optimization.max_batch_num = 2
 
     run_model = EverestRunModel.create(config)
     evaluator_server_config = evaluator_server_config_generator(run_model)
