b199596efa01c1341cf6c9e692d06749a94c80f3
Julius Parulek <parulek@gmail.com>
2025-02-05 23:53:46 +0100

    WIP: test for socket close and open in zmq

--- tests/ert/unit_tests/ensemble_evaluator/test_ensemble_evaluator.py
+++ tests/ert/unit_tests/ensemble_evaluator/test_ensemble_evaluator.py
@@ -48,6 +48,7 @@ from ert.ensemble_evaluator.state import (
     FORWARD_MODEL_STATE_RUNNING,
 )
 from ert.scheduler import JobState
+from ert.shared import find_available_socket
 
 from .ensemble_evaluator_utils import TestEnsemble
 
@@ -100,6 +101,22 @@ async def test_evaluator_handles_dispatchers_connected(
     await evaluator.handle_dispatch(b"dispatcher-1", DISCONNECT_MSG)
     await evaluator.handle_dispatch(b"dispatcher-2", DISCONNECT_MSG)
     assert evaluator._dispatchers_empty.is_set()
+
+
+async def test_find_socket_works_with_zmq(unused_tcp_port):
+    custom_range = range(unused_tcp_port, unused_tcp_port + 1)
+    sock = find_available_socket(custom_range=custom_range, custom_host="127.0.0.1")
+    host, port = sock.getsockname()
+    print(host, port)
+
+    for _ in range(4):
+        try:
+            ctx = zmq.asyncio.Context()
+            socket = ctx.socket(zmq.ROUTER)
+            socket.bind(f"tcp://*:{port}")
+        finally:
+            socket.close()
+            ctx.destroy(linger=0)
 
 
 async def test_evaluator_raises_on_start_with_address_in_use(make_ee_config):
