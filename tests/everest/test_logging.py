b65af53700f360d3f2dca58c0f52baade69b8ae5
DanSava <dan.sava42@gmail.com>
2025-02-10 14:54:10 +0200

    Update everest logging test to make it more useful

--- tests/everest/test_logging.py
+++ tests/everest/test_logging.py
@@ -14,10 +14,6 @@ from everest.detached import start_experiment, start_server, wait_for_server
 from everest.util import makedirs_if_needed
 
 
-def _string_exists_in_file(file_path, string):
-    return string in Path(file_path).read_text(encoding="utf-8")
-
-
 @pytest.mark.timeout(240)  # Simulation might not finish
 @pytest.mark.integration_test
 @pytest.mark.xdist_group(name="starts_everest")
@@ -67,13 +63,10 @@ async def test_logging_setup(copy_math_func_test_data_to_tmp):
     assert os.path.exists(everest_log_path)
     assert os.path.exists(endpoint_log_path)
 
-    assert _string_exists_in_file(everest_log_path, "everest DEBUG:")
-    assert _string_exists_in_file(
-        forward_model_log_path, "Exception: Failing simulation_2 by request!"
-    )
-    assert _string_exists_in_file(
-        forward_model_log_path, "Exception: Failing simulation_2 by request!"
-    )
+    assert "everest DEBUG:" in Path(everest_log_path).read_text(encoding="utf-8")
+    assert "Exception: Failing simulation_2 by request!" in Path(
+        forward_model_log_path
+    ).read_text(encoding="utf-8")
 
     endpoint_logs = Path(endpoint_log_path).read_text(encoding="utf-8")
     # Avoid cases where optimization finished before we get a chance to check that
