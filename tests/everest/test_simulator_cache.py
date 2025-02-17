221f65bcb0632aae2a5b0c2a496aeeb062c94800
Yngve S. Kristiansen <ynk@equinor.com>
2025-02-17 09:36:23 +0100

    Fix bug where forward model ran empty ensemble

--- tests/everest/test_simulator_cache.py
+++ tests/everest/test_simulator_cache.py
@@ -8,12 +8,12 @@ from everest.config import EverestConfig
 
 @pytest.mark.integration_test
 def test_simulator_cache(copy_math_func_test_data_to_tmp):
-    n_evals = 0
+    n_invocations = 0
 
     def new_call(*args):
-        nonlocal n_evals
+        nonlocal n_invocations
         result = original_call(*args)
-        n_evals += (result.evaluation_ids >= 0).sum()
+        n_invocations += 1
         return result
 
     config = EverestConfig.load_file("config_minimal.yml")
@@ -24,22 +24,22 @@ def test_simulator_cache(copy_math_func_test_data_to_tmp):
     evaluator_server_config = EvaluatorServerConfig()
 
     # Modify the forward model function to track number of calls:
-    original_call = run_model._forward_model_evaluator
-    run_model._forward_model_evaluator = new_call
+    original_call = run_model._evaluate_and_postprocess
+    run_model._evaluate_and_postprocess = new_call
 
     # First run populates the cache:
     run_model.run_experiment(evaluator_server_config)
-    assert n_evals > 0
+    assert n_invocations > 0
     variables1 = list(run_model.result.controls.values())
     assert np.allclose(variables1, [0.5, 0.5, 0.5], atol=0.02)
 
     # Now do another run, where the functions should come from the cache:
-    n_evals = 0
+    n_invocations = 0
 
     # The batch_id was used as a stopping criterion, so it must be reset:
     run_model._batch_id = 0
 
     run_model.run_experiment(evaluator_server_config)
-    assert n_evals == 0
+    assert n_invocations == 0
     variables2 = list(run_model.result.controls.values())
     assert np.array_equal(variables1, variables2)
