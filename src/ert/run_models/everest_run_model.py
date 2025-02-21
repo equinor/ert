56994bbd503223d95b2467e1f7456d375c8276fa
Peter Verveer <pieter.verveer@tno.nl>
2025-02-21 08:13:37 +0000

    Redirect optimizer output to file

--- src/ert/run_models/everest_run_model.py
+++ src/ert/run_models/everest_run_model.py
@@ -8,8 +8,10 @@ import logging
 import os
 import queue
 import shutil
+import sys
 from collections import defaultdict
-from collections.abc import Callable
+from collections.abc import Callable, Generator
+from contextlib import contextmanager
 from enum import IntEnum, auto
 from pathlib import Path
 from types import TracebackType
@@ -270,7 +272,12 @@ class EverestRunModel(BaseRunModel):
         self.ever_storage.observe_optimizer(optimizer)
 
         # Run the optimization:
-        optimizer_exit_code = optimizer.run().exit_code
+        output_dir = Path(self._everest_config.optimization_output_dir)
+        with redirect_optimizer_output(
+            stdout=output_dir / "optimizer_output.stdout",
+            stderr=output_dir / "optimizer_output.stderr",
+        ):
+            optimizer_exit_code = optimizer.run().exit_code
 
         # Extract the best result from the storage.
         self._result = self.ever_storage.get_optimal_result()
@@ -1011,3 +1018,60 @@ class SimulatorCache:
             if np.allclose(controls, control_values, rtol=0.0, atol=self.EPS):
                 return objectives, constraints
         return None
+
+
+@contextmanager
+def redirect_optimizer_output(
+    stdout: Path | None, stderr: Path | None = None
+) -> Generator[None, None, None]:
+    """This context manager redirects stdout and stderr to the given files.
+
+    The standard Python approach would be to redirect sys.stdout and sys.stderr,
+    or use contextlib.redirect_stdout/err. However, these may fail in cases
+    where the output is produced by C or Fortran code, as commonly used in
+    optimization backends. Therefore, the stdout and stderr file descriptors are
+    directly manipulated. It should be noted that this is not thread-safe, i.e.
+    standard output/errors may be affected in other threads in the same process.
+    However, this should not be an issue since the Everest run model does not
+    send output to stdout/stderr directly. (Note that manipulating sys.stdout or
+    using contextlib.redirect_stdout would also not be thread-safe.)
+    """
+    if stderr is None:
+        stderr = stdout
+
+    old_stdout: int | None = None
+    old_stderr: int | None = None
+    new_stdout: int | None = None
+    new_stderr: int | None = None
+
+    try:
+        sys.stdout.flush()
+        sys.stderr.flush()
+        old_stdout = os.dup(1)
+        old_stderr = os.dup(2)
+        new_stdout = (
+            os.open(os.devnull, os.O_WRONLY)
+            if stdout is None
+            else os.open(stdout, os.O_WRONLY | os.O_CREAT)
+        )
+        os.dup2(new_stdout, 1)
+        if stderr == stdout:
+            os.dup2(new_stdout, 2)
+            new_stderr = None
+        else:
+            new_stderr = (
+                os.open(os.devnull, os.O_WRONLY)
+                if stderr is None
+                else os.open(stderr, os.O_WRONLY | os.O_CREAT)
+            )
+            os.dup2(new_stderr, 2)
+        yield
+    finally:
+        if old_stdout is not None:
+            os.dup2(old_stdout, 1)
+        if old_stderr is not None:
+            os.dup2(old_stderr, 2)
+        if new_stdout is not None:
+            os.close(new_stdout)
+        if new_stderr is not None:
+            os.close(new_stderr)
