a29d8455878653b29693b246773f879f6903b21e
Jonathan Karlsen <JONAK@equinor.com>
2025-02-24 09:02:29 +0100

    Log how long `rm_run_path()` takes
    This commit adds some duration logging to `rm_run_path()` and refactors
    the duration logging functions into a reusable decorator.

---
+++ src/ert/utils/__init__.py
@@ -0,0 +1,32 @@
+import logging
+import time
+from collections.abc import Callable
+from functools import wraps
+from typing import ParamSpec, TypeVar
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def log_duration(
+    logger: logging.Logger,
+    logging_level: int = logging.DEBUG,
+    custom_name: str | None = None,
+) -> Callable[[Callable[P, R]], Callable[P, R]]:
+    """
+    This is a decorator that logs the time it takes for a function to execute
+    """
+
+    def decorator(func: Callable[P, R]) -> Callable[P, R]:
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            t = time.perf_counter()
+            result = func(*args, **kwargs)
+            elapsed_time = time.perf_counter() - t
+            name = custom_name if custom_name else f"{func.__name__}()"
+            logger.log(logging_level, f"{name} time_used={elapsed_time:.4f}s")
+            return result
+
+        return wrapper
+
+    return decorator
