14118ba370e4ff67b2c033d0f46fbe50fe7eaded
Yngve S. Kristiansen <ynk@equinor.com>
2025-03-04 14:42:39 +0100

    Add client for Everest GUI <-> runmodel interactions

---
+++ src/everest/gui/everest_client.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+
+import logging
+import queue
+import ssl
+import time
+import traceback
+from base64 import b64encode
+from http import HTTPStatus
+
+import requests
+from pydantic import ValidationError
+from requests import HTTPError
+from websockets.sync.client import connect
+
+from _ert.threading import ErtThread
+from ert.ensemble_evaluator import EvaluatorServerConfig
+from ert.run_models import BaseRunModelAPI
+from ert.run_models.event import StatusEvents, status_event_from_json
+from everest.strings import (
+    OPT_PROGRESS_ENDPOINT,
+    START_EXPERIMENT_ENDPOINT,
+    STOP_ENDPOINT,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class EverestClient:
+    def __init__(
+        self,
+        url: str,
+        cert_file: str,
+        username: str,
+        password: str,
+        ssl_context: ssl.SSLContext,
+    ):
+        self._url = url
+        self._cert = cert_file
+        self._username = username
+        self._password = password
+        self._ssl_context = ssl_context
+
+        self._progress_endpoint = "/".join([url, OPT_PROGRESS_ENDPOINT])
+        self._stop_endpoint = "/".join([url, STOP_ENDPOINT])
+        self._start_endpoint = "/".join([url, START_EXPERIMENT_ENDPOINT])
+
+        self._is_alive = False
+
+    @property
+    def credentials(self) -> str:
+        return b64encode(f"{self._username}:{self._password}".encode()).decode()
+
+    def setup_event_queue_from_ws_endpoint(
+        self,
+        refresh_interval: float = 0.01,
+        open_timeout: float = 30,
+        websocket_recv_timeout: float = 1.0,
+    ) -> tuple[queue.SimpleQueue[StatusEvents], ErtThread]:
+        event_queue: queue.SimpleQueue[StatusEvents] = queue.SimpleQueue()
+
+        def passthrough_ws_events() -> None:
+            try:
+                with connect(
+                    self._url.replace("https://", "wss://") + "/events",
+                    ssl=self._ssl_context,
+                    open_timeout=open_timeout,
+                    additional_headers={"Authorization": f"Basic {self.credentials}"},
+                ) as websocket:
+                    while not self._is_alive:
+                        try:
+                            message = websocket.recv(timeout=websocket_recv_timeout)
+                        except TimeoutError:
+                            message = None
+                        if message:
+                            try:
+                                event = status_event_from_json(message)
+                                event_queue.put(event)
+                            except ValidationError as e:
+                                logger.error(
+                                    "Error when processing event %s", exc_info=e
+                                )
+
+                        time.sleep(refresh_interval)
+            except:
+                logging.debug(traceback.format_exc())
+
+        monitor_thread = ErtThread(
+            name="everest_gui_event_monitor",
+            target=passthrough_ws_events,
+            daemon=True,
+        )
+
+        return event_queue, monitor_thread
+
+    def create_run_model_api(
+        self, queue_system: str, runpath_format_string: str
+    ) -> BaseRunModelAPI:
+        def start_fn(
+            evaluator_server_config: EvaluatorServerConfig, restart: bool = False
+        ) -> None:
+            pass
+
+        return BaseRunModelAPI(
+            experiment_name="Everest Experiment",
+            queue_system=queue_system,
+            runpath_format_string=runpath_format_string,
+            support_restart=False,
+            start_simulations_thread=start_fn,
+            cancel=self.stop,
+            get_runtime=lambda: -1,  # Not currently shown in Everest gui
+            has_failed_realizations=lambda: False,
+        )
+
+    def stop(self) -> None:
+        try:
+            response = requests.post(
+                self._stop_endpoint,
+                verify=self._cert,
+                auth=(self._username, self._password),
+                proxies={"http": None, "https": None},  # type: ignore
+            )
+
+            if response.status_code == 200:
+                logger.info("Cancelled experiment from Everest")
+                print("Successfully cancelled experiment")
+            else:
+                logger.error(
+                    f"Failed to cancel Everest experiment: POST @ {self._stop_endpoint}, "
+                    f"server responded with status {response.status_code}: "
+                    f"{HTTPStatus(response.status_code).phrase}"
+                )
+                print("Failed to cancel experiment")
+
+        except requests.exceptions.ConnectionError as e:
+            logger.error(
+                f"Connection error when cancelling Everest experiment: {''.join(traceback.format_exception(e))}"
+            )
+            print("Failed to cancel experiment")
+
+        except HTTPError as e:
+            logger.error(
+                f"HTTP error when cancelling Everest experiment: {''.join(traceback.format_exception(e))}"
+            )
+            print("Failed to cancel experiment")
---
+++ tests/everest/test_everest_client.py
@@ -0,0 +1,183 @@
+import logging
+import ssl
+import threading
+import time
+from pathlib import Path
+
+import pytest
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import Response
+
+from everest.bin.everest_script import everest_entry
+from everest.config import EverestConfig, ServerConfig
+from everest.detached import wait_for_server
+from everest.detached.jobs.everserver import _find_open_port
+from everest.gui.everest_client import EverestClient
+from everest.strings import STOP_ENDPOINT
+
+
+@pytest.fixture
+def client_server_mock() -> tuple[FastAPI, threading.Thread, EverestClient]:
+    server_app = FastAPI()
+    host = "127.0.0.1"
+    port = _find_open_port(host, lower=5000, upper=5800)
+    server_url = f"http://{host}:{port}"
+
+    server = uvicorn.Server(
+        uvicorn.Config(server_app, host=host, port=port, log_level="info")
+    )
+
+    server_thread = threading.Thread(
+        target=server.run,
+        daemon=True,
+    )
+
+    everest_client = EverestClient(
+        url=server_url,
+        cert_file="N/A",
+        username="",
+        password="",
+        ssl_context=ssl.create_default_context(),
+    )
+
+    yield server_app, server_thread, everest_client
+
+    if server_thread.is_alive():
+        server.should_exit = True
+        server_thread.join()
+
+
+def test_that_stop_invokes_correct_endpoint(
+    caplog, client_server_mock: tuple[FastAPI, threading.Thread, EverestClient]
+):
+    server_app, server_thread, client = client_server_mock
+
+    @server_app.post(f"/{STOP_ENDPOINT}")
+    def stop():
+        return Response("STOP..", 200)
+
+    server_thread.start()
+    time.sleep(0.1)
+
+    with caplog.at_level(logging.INFO):
+        client.stop()
+
+    assert "Cancelled experiment from Everest" in caplog.messages
+    server_thread.should_exit = True
+
+
+def test_that_stop_errors_on_non_ok_httpcode(
+    caplog, client_server_mock: tuple[FastAPI, threading.Thread, EverestClient]
+):
+    server_app, server_thread, client = client_server_mock
+
+    @server_app.post(f"/{STOP_ENDPOINT}")
+    def stop():
+        return Response("STOP..", 505)
+
+    server_thread.start()
+    time.sleep(0.1)
+
+    with caplog.at_level(logging.ERROR):
+        client.stop()
+
+    assert any(
+        "Failed to cancel Everest experiment" in m
+        and "server responded with status 505" in m
+        for m in caplog.messages
+    )
+
+
+def test_that_stop_errors_on_server_down(
+    caplog, client_server_mock: tuple[FastAPI, threading.Thread, EverestClient]
+):
+    _, _, client = client_server_mock
+
+    with caplog.at_level(logging.ERROR):
+        client.stop()
+
+    assert any(
+        "Connection error when cancelling Everest experiment" in m
+        for m in caplog.messages
+    )
+
+
+def test_that_stop_errors_on_server_up_but_endpoint_down(
+    caplog, client_server_mock: tuple[FastAPI, threading.Thread, EverestClient]
+):
+    _, server_thread, client = client_server_mock
+
+    server_thread.start()
+    time.sleep(0.1)
+
+    with caplog.at_level(logging.ERROR):
+        client.stop()
+
+    assert any(
+        "server responded with status 404: Not Found" in m for m in caplog.messages
+    )
+
+
+def test_that_multiple_everest_clients_can_connect_to_server(cached_example):
+    # We use a cached run for the reference list of received events
+    path, config_file, _, server_events_list = cached_example(
+        "math_func/config_minimal.yml"
+    )
+
+    config_path = Path(path) / config_file
+    ever_config = EverestConfig.load_file(config_path)
+
+    # Run the case through everserver
+    everest_main_thread = threading.Thread(
+        target=everest_entry, args=[[str(config_path)]]
+    )
+
+    everest_main_thread.start()
+    wait_for_server(ever_config.output_dir, 60)
+
+    server_context = ServerConfig.get_server_context(ever_config.output_dir)
+    url, cert, auth = server_context
+
+    ssl_context = ssl.create_default_context()
+    ssl_context.load_verify_locations(cafile=cert)
+    username, password = auth
+
+    client_event_queues = []
+    for _ in range(5):
+        client = EverestClient(
+            url=url,
+            cert_file=cert,
+            username=username,
+            password=password,
+            ssl_context=ssl_context,
+        )
+
+        # Connect to the websockets endpoint
+        client_event_queue, monitor_thread = client.setup_event_queue_from_ws_endpoint()
+        client_event_queues.append(client_event_queue)
+        monitor_thread.start()
+
+    # Wait until the server has finished running the simulation
+    everest_main_thread.join()
+
+    # Expect all the clients to hold the same events
+    client_event_lists = []
+    for event_queue in client_event_queues:
+        event_list = []
+        while not event_queue.empty():
+            event_list.append(event_queue.get())
+
+        client_event_lists.append(event_list)
+
+    first = client_event_lists[0]
+    assert all(first == other for other in client_event_lists[1:])
+
+    first_everevents = [e.event_type for e in first if "Everest" in e.event_type]
+    server_everevents = [
+        e.event_type for e in server_events_list if "Everest" in e.event_type
+    ]
+
+    # Compare only everest events, as the events from the forward model
+    # are (at time of writing) not deterministic enough to expect equality
+    assert first_everevents == server_everevents
