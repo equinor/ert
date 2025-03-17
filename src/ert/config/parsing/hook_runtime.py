11eccfd1ea1e9d36c1d28fbb4641bd3b81c59123
Yngve S. Kristiansen <ynk@equinor.com>
2025-03-17 16:30:40 +0100

    Validate workflow job args against hook runtime fixtures

--- src/ert/config/parsing/hook_runtime.py
+++ src/ert/config/parsing/hook_runtime.py
@@ -9,3 +9,54 @@ class HookRuntime(StrEnum):
     PRE_FIRST_UPDATE = "PRE_FIRST_UPDATE"
     PRE_EXPERIMENT = "PRE_EXPERIMENT"
     POST_EXPERIMENT = "POST_EXPERIMENT"
+
+
+fixtures_per_runtime = {
+    HookRuntime.PRE_EXPERIMENT: {"random_seed"},
+    HookRuntime.PRE_SIMULATION: {
+        "storage",
+        "ensemble",
+        "reports_dir",
+        "random_seed",
+        "run_paths",
+    },
+    HookRuntime.POST_SIMULATION: {
+        "storage",
+        "ensemble",
+        "reports_dir",
+        "random_seed",
+        "run_paths",
+    },
+    HookRuntime.PRE_FIRST_UPDATE: {
+        "storage",
+        "ensemble",
+        "reports_dir",
+        "random_seed",
+        "es_settings",
+        "observation_settings",
+        "run_paths",
+    },
+    HookRuntime.PRE_UPDATE: {
+        "storage",
+        "ensemble",
+        "reports_dir",
+        "random_seed",
+        "es_settings",
+        "observation_settings",
+        "run_paths",
+    },
+    HookRuntime.POST_UPDATE: {
+        "storage",
+        "ensemble",
+        "reports_dir",
+        "random_seed",
+        "es_settings",
+        "observation_settings",
+        "run_paths",
+    },
+    HookRuntime.POST_EXPERIMENT: {
+        "random_seed",
+        "storage",
+        "ensemble",
+    },
+}
