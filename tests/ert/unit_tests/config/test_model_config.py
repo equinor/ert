c63dc5e331e8112f4118e565953df7d07fbf670e
Eivind Jahren <ejah@equinor.com>
2025-03-18 13:25:39 +0100

    Remove unused tmpdir fixture in model config tests

--- tests/ert/unit_tests/config/test_model_config.py
+++ tests/ert/unit_tests/config/test_model_config.py
@@ -7,19 +7,19 @@ from ert.config import ModelConfig
 from ert.config.parsing import ConfigKeys, ConfigValidationError, ConfigWarning
 
 
-def test_default_model_config_run_path(tmpdir):
+def test_default_model_config_run_path():
     mc = ModelConfig(num_realizations=1)
     assert mc.runpath_format_string == "simulations/realization-<IENS>/iter-<ITER>"
 
 
-def test_invalid_model_config_run_path(tmpdir):
+def test_invalid_model_config_run_path():
     mc = ModelConfig(
         num_realizations=1, runpath_format_string="realization-no-specifier"
     )
     assert mc.runpath_format_string == "realization-no-specifier"
 
 
-def test_suggested_deprecated_model_config_run_path(tmpdir):
+def test_suggested_deprecated_model_config_run_path():
     runpath = "simulations/realization-%d/iter-%d"
     suggested_path = "simulations/realization-<IENS>/iter-<ITER>"
     mc = ModelConfig(num_realizations=1, runpath_format_string=runpath)
