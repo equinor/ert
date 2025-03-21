537b4bc2f93e2eb2bee03d0b5f8ff5648172597f
Eivind Jahren <ejah@equinor.com>
2025-03-21 07:21:52 +0100

    Simplify and move summary_observation_validation test

--- tests/ert/unit_tests/config/parsing/test_observations_parser.py
+++ tests/ert/unit_tests/config/parsing/test_observations_parser.py
@@ -253,3 +253,191 @@ def test_that_common_observation_error_validation_is_handled(
                         """,
             "",
         )
+
+
+@pytest.mark.parametrize(
+    "obs_content, match",
+    [
+        (
+            """
+            SUMMARY_OBSERVATION  FOPR
+            {
+               DAYS       = -1;
+            };
+            """,
+            'Failed to validate "-1"',
+        ),
+        (
+            """
+            SUMMARY_OBSERVATION  FOPR
+            {
+               VALUE       = exactly_1;
+            };
+            """,
+            'Failed to validate "exactly_1"',
+        ),
+        (
+            """
+            SUMMARY_OBSERVATION  FOPR
+            {
+               DAYS       = 1;
+            };
+            """,
+            'Missing item "VALUE"',
+        ),
+        (
+            """
+            SUMMARY_OBSERVATION  FOPR
+            {
+               KEY        = FOPR;
+               VALUE      = 2.0;
+               DAYS       = 1;
+            };
+            """,
+            'Missing item "ERROR"',
+        ),
+        (
+            """
+            SUMMARY_OBSERVATION  FOPR
+            {
+               VALUE = 1;
+               ERROR = 0.1;
+            };
+            """,
+            'Missing item "KEY"',
+        ),
+        (
+            """
+            HISTORY_OBSERVATION  FOPR
+            {
+               ERROR      = 0.1;
+
+               SEGMENT SEG
+               {
+                  STOP  = 1;
+                  ERROR = 0.50;
+               };
+            };
+            """,
+            'Missing item "START"',
+        ),
+        (
+            """
+            HISTORY_OBSERVATION  FOPR
+            {
+               ERROR      = 0.1;
+
+               SEGMENT SEG
+               {
+                  START  = 1;
+                  ERROR = 0.50;
+               };
+            };
+            """,
+            'Missing item "STOP"',
+        ),
+        (
+            """
+            HISTORY_OBSERVATION  FOPR
+            {
+               ERROR      = 0.1;
+
+               SEGMENT SEG
+               {
+                  START = 0;
+                  STOP  = 3.2;
+                  ERROR = 0.50;
+               };
+            };
+            """,
+            'Failed to validate "3.2"',
+        ),
+        (
+            """
+            HISTORY_OBSERVATION  FOPR
+            {
+               ERROR      = 0.1;
+
+               SEGMENT SEG
+               {
+                  START = 1.1;
+                  STOP  = 0;
+                  ERROR = 0.50;
+               };
+            };
+            """,
+            'Failed to validate "1.1"',
+        ),
+        (
+            """
+            HISTORY_OBSERVATION  FOPR
+            {
+               ERROR      = 0.1;
+
+               SEGMENT SEG
+               {
+                  START = 1;
+                  STOP  = 0;
+                  ERROR = -1;
+               };
+            };
+            """,
+            'Failed to validate "-1"',
+        ),
+        (
+            """
+            HISTORY_OBSERVATION  FOPR
+            {
+               ERROR      = 0.1;
+
+               SEGMENT SEG
+               {
+                  START = 1;
+                  STOP  = 0;
+                  ERROR = 0.1;
+                  ERROR_MIN = -1;
+               };
+            };
+            """,
+            'Failed to validate "-1"',
+        ),
+        (
+            """
+            SUMMARY_OBSERVATION  FOPR
+            {
+               RESTART = -1;
+            };
+            """,
+            'Failed to validate "-1"',
+        ),
+        (
+            """
+            SUMMARY_OBSERVATION  FOPR
+            {
+               RESTART = minus_one;
+            };
+            """,
+            'Failed to validate "minus_one"',
+        ),
+        (
+            """
+                HISTORY_OBSERVATION  FOPR
+                {
+                   ERROR      = 0.1;
+
+                   SEGMENT SEG
+                   {
+                      START = 1;
+                      STOP  = 0;
+                      ERROR = 0.1;
+                      ERROR_MODE = NOT_ABS;
+                   };
+                };
+                """,
+            'Failed to validate "NOT_ABS"',
+        ),
+    ],
+)
+def test_that_summary_observation_validation_is_handled(obs_content, match):
+    with pytest.raises(ObservationConfigError, match=match):
+        parse_content(obs_content, filename="")
