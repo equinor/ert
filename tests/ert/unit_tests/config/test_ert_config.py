4f53fc7e832ca6266d3a6a1428449c7c7afdca7c
Eivind Jahren <ejah@equinor.com>
2025-03-21 14:24:15 +0100

    Simplify fm_step_via_plugin test

--- tests/ert/unit_tests/config/test_ert_config.py
+++ tests/ert/unit_tests/config/test_ert_config.py
@@ -792,18 +792,12 @@ def test_fm_step_config_via_plugin_ends_up_json_data(monkeypatch, anystring):
         "get_forward_model_configuration",
         MagicMock(return_value={"SOME_STEP": {"FOO": anystring}}),
     )
-    Path("SOME_STEP").write_text("EXECUTABLE /bin/ls", encoding="utf-8")
-    Path("config.ert").write_text(
-        dedent(
-            """
-            NUM_REALIZATIONS 1
-            INSTALL_JOB SOME_STEP SOME_STEP
-            FORWARD_MODEL SOME_STEP()
-            """
-        ),
-        encoding="utf-8",
+    ert_config = ErtConfig.with_plugins().from_dict(
+        {
+            "INSTALL_JOB": [["SOME_STEP", ("SOME_STEP", "EXECUTABLE fm_dispatch.py")]],
+            "FORWARD_MODEL": [["SOME_STEP"]],
+        }
     )
-    ert_config = ErtConfig.with_plugins().from_file("config.ert")
     step_json = create_forward_model_json(
         context=ert_config.substitutions,
         forward_model_steps=ert_config.forward_model_steps,
