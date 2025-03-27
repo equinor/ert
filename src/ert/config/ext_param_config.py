483910559d73a952874056af76a36984417d8040
Yngve S. Kristiansen <ynk@equinor.com>
2025-03-27 11:40:01 +0100

    fixup! Remove unnecessary load_parameter_graph args

--- src/ert/config/ext_param_config.py
+++ src/ert/config/ext_param_config.py
@@ -133,9 +133,7 @@ class ExtParamConfig(ParameterConfig):
             }
         )
 
-    def load_parameter_graph(
-        self, ensemble: Ensemble, group: str, realizations: npt.NDArray[np.int_]
-    ) -> nx.Graph[int]:
+    def load_parameter_graph(self) -> nx.Graph[int]:
         raise NotImplementedError()
 
     def __len__(self) -> int:
