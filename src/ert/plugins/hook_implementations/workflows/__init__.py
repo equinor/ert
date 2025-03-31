dd437b16c412486e7dbfed9e5060ae03250a3c89
Andreas Eknes Lie <andrli@equinor.com>
2025-03-31 12:50:19 +0200

    Revert GenDataRFTCSVExportJob import guard

--- src/ert/plugins/hook_implementations/workflows/__init__.py
+++ src/ert/plugins/hook_implementations/workflows/__init__.py
@@ -1,20 +1,14 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
 import ert
-from ert.plugins import ErtScript
 
 from .csv_export import CSVExportJob
 from .disable_parameters import DisableParametersUpdate
 from .export_misfit_data import ExportMisfitDataJob
 from .export_runpath import ExportRunpathJob
-
-try:
-    from .gen_data_rft_export import GenDataRFTCSVExportJob
-except ImportError:
-    GenDataRFTCSVExportJob = cast(type[ErtScript], ErtScript)
-
+from .gen_data_rft_export import GenDataRFTCSVExportJob
 from .misfit_preprocessor import MisfitPreprocessor
 
 if TYPE_CHECKING:
