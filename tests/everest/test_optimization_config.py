2d063379d21d14117846f5e821b5b508e8f65b5e
Peter Verveer <pieter.verveer@tno.nl>
2025-04-09 12:29:37 +0000

    Add validation of optimizer options

--- tests/everest/test_optimization_config.py
+++ tests/everest/test_optimization_config.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+from pydantic import ValidationError
 
 from everest.config import EverestConfig
 
@@ -20,13 +21,43 @@ def test_optimization_config(copy_test_data_to_tmp):
     assert optim_config.constraint_tolerance == pytest.approx(1.0e-7)
     assert optim_config.speculative is False
     assert optim_config.options == [
-        "131.3 = No",
-        "LOG HIGH",
-        "Dostuff: 1",
-        "speculate",
+        "max_iterations = 0",
+        "merit_function el_bakry",
     ]
 
     assert full_config_dict.model.realizations is not None
     realizations = full_config_dict.model.realizations
 
     assert realizations == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+
+def test_optimization_config_options(copy_test_data_to_tmp):
+    config = EverestConfig.load_file("mocked_test_case/mocked_test_case.yml")
+    config_dict = config.model_dump(exclude_none=True)
+
+    config_dict["optimization"]["options"] = [
+        "max_iterations = 0",
+        "merit_function el_bakry",
+    ]
+    config = EverestConfig.model_validate(config_dict)
+
+    config_dict["optimization"]["options"] = [
+        "max_iterations = 0",
+        "search_method = 1",
+        "merit_function el_bakry",
+    ]
+    with pytest.raises(
+        ValidationError, match=r"Input should be 'value_based_line_search',"
+    ):
+        config = EverestConfig.model_validate(config_dict)
+
+    config_dict["optimization"]["options"] = [
+        "max_iterations = 0",
+        "foo = xyz",
+        "bar",
+        "merit_function el_bakry",
+    ]
+    with pytest.raises(
+        ValidationError, match=r"Unknown or unsupported option\(s\): `foo`, `bar`"
+    ):
+        config = EverestConfig.model_validate(config_dict)
