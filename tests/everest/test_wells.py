12c433d571e2f03e25bede0c7006fd549936d90c
Øyvind Eide <eide.oyvind87@gmail.com>
2025-04-24 08:42:08 +0200

    Parameterize test

--- tests/everest/test_wells.py
+++ tests/everest/test_wells.py
@@ -1,7 +1,8 @@
+from contextlib import ExitStack as does_not_raise
+
 import pytest
 
-from ert.tests.everest.test_config_validation import has_error
-from everest.config import EverestConfig
+from everest.config import EverestConfig, WellConfig
 from everest.config_file_loader import yaml_file_to_substituted_config_dict
 from tests.everest.utils import relpath
 
@@ -17,50 +18,26 @@ def test_lint_mocked_config(mocked_config):
     EverestConfig(**mocked_config)
 
 
-def test_unrecognized_keys(mocked_config):
-    config = mocked_config
-    wells = config["wells"]
-
-    wells[0]["unexpected_key"] = 7
-    with pytest.raises(ValueError, match="Extra inputs are not permitted"):
-        EverestConfig(**config)
-    wells[0].pop("unexpected_key")
-    wells[0][14] = 7
-    with pytest.raises(ValueError, match=r".*Keys should be strings"):
-        EverestConfig(**config)
-
-    wells[0].pop(14)
-    assert len(EverestConfig.lint_config_dict(config)) == 0
-
-
-def test_well_names(mocked_config):
-    config = mocked_config
-    wells = config["wells"]
-    assert len(wells) == 16
-
-    well_name_0 = wells[0]["name"]
-
-    # Make sure controls do not interfere with error checking
-    controls = config["controls"][0]["variables"]
-    for i, ctrl in enumerate(controls):
-        if ctrl["name"] == well_name_0:
-            del controls[i]
-            break
-
-    wells[0].pop("name")  # empty well definition
-    assert has_error(EverestConfig.lint_config_dict(config), match="Field required")
-
-    wells[0]["drill_time"] = 1  # incomplete well definition
-    assert has_error(EverestConfig.lint_config_dict(config), match="Field required")
-
-    wells[0]["name"] = "a.b"  # can't have dots in well name
-    assert has_error(
-        EverestConfig.lint_config_dict(config),
-        match="Well name can not contain any dots (.)",
-    )
-
-    wells[0]["name"] = well_name_0
-    assert not EverestConfig.lint_config_dict(config)
+@pytest.mark.parametrize(
+    "config, expectation",
+    [
+        (
+            {"unexpected_key": 7, "name": "well_well"},
+            pytest.raises(ValueError, match="Extra inputs are not permitted"),
+        ),
+        (
+            {"name": 7},
+            pytest.raises(ValueError, match="should be a valid string"),
+        ),
+        (
+            {"name": "well_well"},
+            does_not_raise(),
+        ),
+    ],
+)
+def test_unrecognized_keys(config, expectation):
+    with expectation:
+        WellConfig(**config)
 
 
 def test_that_well_names_must_be_unique(min_config):
