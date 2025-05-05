37ff5e8b4c0e9b7bbc18e5106a2e2c13e1e0f17e
larsevj <levje@equinor.com>
2025-05-05 13:32:21 +0200

    Unpin polars

--- tests/ert/performance_tests/test_dark_storage_performance.py
+++ tests/ert/performance_tests/test_dark_storage_performance.py
@@ -242,13 +242,13 @@ def api_and_snake_oil_storage(snake_oil_case_storage, monkeypatch):
     [  # Tested 24.11.22 on macbook pro M1 max
         # (xr = tested on previous ert using xarray to store responses)
         (1, 100, 100, 1200),  # 790MiB local, xr: 791, MiB
-        (1000, 100, 100, 1500),  # 809MiB local, 879MiB linux-3.11, xr: 1107MiB
+        (1000, 100, 100, 2700),  # 809MiB local, 879MiB linux-3.11, xr: 1107MiB
         # (Cases below are more realistic at up to 200realizations)
         # Not to be run these on GHA runners
-        # (2000, 100, 100, 1950),
+        # (2000, 100, 100, 6000),
         #   1607MiB local, 1716MiB linux3.12, 1863 on linux3.11, xr: 2186MiB
         # (2, 5803, 11787, 5500),  # 4657MiB local, xr: 10115MiB
-        # (10, 5803, 11787, 13500),  # 10036MiB local, 12803MiB mac-3.12, xr: 46715MiB
+        # (10, 5803, 11787, 17000),  # 10036MiB local, 12803MiB mac-3.12, xr: 46715MiB
     ],
 )
 def test_plot_api_big_summary_memory_usage(
