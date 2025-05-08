d0ce5a1e054a14482275cd450a0bdb51d02d45f9
Håvard Berland <havb@equinor.com>
2025-05-08 15:03:23 +0200

    Display message to user on PermissionErrors from storage
    Instead of crashing hard in the GUI thread upon permission
    errors in storage, display a message, log the error and
    let the user try to continue.

--- src/ert/gui/simulation/manual_update_panel.py
+++ src/ert/gui/simulation/manual_update_panel.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass
 
 import numpy as np
@@ -5,7 +6,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtCore import pyqtSlot as Slot
 from PyQt6.QtWidgets import QFormLayout, QLabel, QWidget
 
-from ert.config import AnalysisConfig
+from ert.config import AnalysisConfig, ErrorInfo
 from ert.gui.ertnotifier import ErtNotifier
 from ert.gui.ertwidgets import (
     ActiveRealizationsModel,
@@ -16,10 +17,13 @@ from ert.gui.ertwidgets import (
     TargetEnsembleModel,
 )
 from ert.gui.simulation.experiment_config_panel import ExperimentConfigPanel
+from ert.gui.suggestor import Suggestor
 from ert.mode_definitions import MANUAL_UPDATE_MODE
 from ert.run_models.manual_update import ManualUpdate
 from ert.storage.realization_storage_state import RealizationStorageState
 from ert.validation import EnsembleRealizationsArgument, ProperNameFormatArgument
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -112,12 +116,19 @@ class ManualUpdatePanel(ExperimentConfigPanel):
     def _realizations_from_fs(self) -> None:
         ensemble = self._ensemble_selector.selected_ensemble
         self._active_realizations_field.setEnabled(ensemble is not None)
-        if ensemble:
-            self._realizations_validator.set_ensemble(ensemble)
-            parameters = ensemble.get_realization_mask_with_parameters()
-            responses = ensemble.get_realization_mask_with_responses()
-            mask = np.logical_and(parameters, responses)
-            self._active_realizations_field.model.setValueFromMask(mask)  # type: ignore
+        try:
+            if ensemble:
+                self._realizations_validator.set_ensemble(ensemble)
+                parameters = ensemble.get_realization_mask_with_parameters()
+                responses = ensemble.get_realization_mask_with_responses()
+                mask = np.logical_and(parameters, responses)
+                self._active_realizations_field.model.setValueFromMask(mask)  # type: ignore
+        except OSError as err:
+            logger.error(str(err))
+            Suggestor(
+                errors=[ErrorInfo(str(err))],
+                widget_info='<p style="font-size: 28px;">Error reading storage</p>',
+            ).show()
 
     @Slot(QWidget)
     def experimentTypeChanged(self, w: QWidget) -> None:
