a90a813d1841e15fd006028558c87383770862d1
Andreas Eknes Lie <andrli@equinor.com>
2025-05-14 12:05:32 +0200

    Use runpath for disk usage widget

--- src/ert/config/model_config.py
+++ src/ert/config/model_config.py
@@ -4,6 +4,7 @@ import contextlib
 import logging
 import os.path
 import shutil
+from pathlib import Path
 from typing import no_type_check
 
 from pydantic import field_validator
@@ -73,7 +74,7 @@ class ModelConfig:
             ConfigWarning.warn(msg)
             logger.warning(msg)
         with contextlib.suppress(Exception):
-            mount_dir = get_mount_directory(runpath_format_string)
+            mount_dir = get_mount_directory(Path(runpath_format_string))
             total_space, used_space, free_space = shutil.disk_usage(mount_dir)
             percentage_used = used_space / total_space
             if (
