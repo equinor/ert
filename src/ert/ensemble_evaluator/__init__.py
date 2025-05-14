24aaa63980a9c2790cd4aa866d79177ff6c1948f
Jonathan Karlsen <JONAK@equinor.com>
2025-05-14 13:02:49 +0200

    Remove evaluator monitor
    This commit removes the monitor and makes EE send events directly to
    baserunmodel.

--- src/ert/ensemble_evaluator/__init__.py
+++ src/ert/ensemble_evaluator/__init__.py
@@ -3,7 +3,6 @@ from ._ensemble import Realization
 from .config import EvaluatorServerConfig
 from .evaluator import EnsembleEvaluator
 from .event import EndEvent, FullSnapshotEvent, SnapshotUpdateEvent
-from .monitor import Monitor
 from .snapshot import EnsembleSnapshot, FMStepSnapshot, RealizationSnapshot
 
 __all__ = [
@@ -14,7 +13,6 @@ __all__ = [
     "EvaluatorServerConfig",
     "FMStepSnapshot",
     "FullSnapshotEvent",
-    "Monitor",
     "Realization",
     "RealizationSnapshot",
     "SnapshotUpdateEvent",
