5d6ca1985b4b39385acbbf40e87ef6f2c12a33ab
Kristian Flikka <kristian.flikka@gmail.com>
2025-05-21 15:16:12 +0200

    Improve file search / highlight speed
    - Remove immediate highlight as you type -> too slow on large files
    - Only search, not highlight
    - Add separate highlight all button
    - Add find next button (with wrap around)
    - Remove asserts in search_bar.py and file_dialog.py, use mypy type ignore instead

--- tests/ert/unit_tests/gui/ertwidgets/test_search_bar.py
+++ tests/ert/unit_tests/gui/ertwidgets/test_search_bar.py
@@ -24,22 +24,62 @@ from ert.gui.tools.search_bar import SearchBar
             "new",
             id="Clears selection on search change",
         ),
-        pytest.param("Testing search functionality.", "", id="Empty selection"),
+        pytest.param("Testing search functionality.", " \b", id="Empty selection"),
+        # Note, need a non-empty string to test
     ],
 )
-def test_search_bar(text, search, qtbot: QtBot):
+def test_search_bar_find(text, search, qtbot: QtBot):
     text_box = QPlainTextEdit()
     text_box.setPlainText(text)
     search_bar = SearchBar(text_box)
 
-    search_bar.select_text(0, 5)  # Simulate previous highlight
-
-    search_bar.search_bar_changed(search)
+    search_bar.setText(search)
     cursor = text_box.textCursor()
 
     if search and search in text:
-        assert cursor.charFormat().background().color() == QColor("yellow")
         assert cursor.selectedText() == search
     else:
         assert cursor.charFormat().background().color() == QColor("white")
         assert not cursor.selectedText()
+
+
+def test_search_bar_find_next(qtbot: QtBot):
+    text_box = QPlainTextEdit()
+    text_box.setPlainText("Testing search functionality. Testing is fun.")
+    search_bar = SearchBar(text_box)
+
+    search_and_cursor_position = [
+        ("Test", 4),  # First "Test"
+        ("Test", 34),  # Second "Test"
+        ("Test", 4),  # Wrap around to first "Test"
+    ]
+
+    for search_text, expected_position in search_and_cursor_position:
+        search_bar.setText(search_text)
+        assert text_box.textCursor().selectedText() == search_text
+        assert text_box.textCursor().position() == expected_position
+        search_bar._find_next_button.click()
+
+
+def test_search_bar_highlight_all(qtbot: QtBot):
+    text_box = QPlainTextEdit()
+
+    # This string has GUI starting at position 8 and 30
+    # The higlight of yellow (correctly) starts after the character, so from position 9
+    input_text = "Testing GUI is so fun. I love GUI forever"
+    text_box.setPlainText(input_text)
+    search_bar = SearchBar(text_box)
+    search_bar.setText("GUI")
+    search_bar._highlight_all_button.click()
+    cursor = text_box.textCursor()
+    assert not cursor.selectedText()  # No text selected when highlighting
+
+    search_hit_positions = (8, 9, 10, 30, 31, 32)
+    for i in range(len(input_text)):
+        cursor.setPosition(i)
+        if (
+            i - 1 in search_hit_positions
+        ):  # -1 because the color apparently starts from the next character
+            assert cursor.charFormat().background().color() == QColor("yellow")
+        else:
+            assert cursor.charFormat().background().color() == QColor("white")
