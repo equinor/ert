1d60f6ceb3b5f8d6025fa9f388ef42f90c2605c1
Kristian Flikka <kristian.flikka@gmail.com>
2025-05-21 15:16:12 +0200

    Refactor of search bar / file dialog
    - Immediate highlight as you type -> too slow on large files
    - Only search, not highlight
    - Add highlight all button
    - Add find next button (with wrap around)

--- src/ert/gui/tools/file/file_dialog.py
+++ src/ert/gui/tools/file/file_dialog.py
@@ -1,5 +1,10 @@
 from PyQt6.QtCore import Qt, QThread
-from PyQt6.QtGui import QClipboard, QFontDatabase, QTextCursor, QTextOption
+from PyQt6.QtGui import (
+    QClipboard,
+    QFontDatabase,
+    QTextCursor,
+    QTextOption,
+)
 from PyQt6.QtWidgets import (
     QApplication,
     QDialog,
