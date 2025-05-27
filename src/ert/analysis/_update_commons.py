6d8d1eca39d015b919c0e542664964db2e925b89
Julius Parulek <parulek@gmail.com>
2025-05-28 00:11:31 +0200

    Revert formatting update_commons

--- src/ert/analysis/_update_commons.py
+++ src/ert/analysis/_update_commons.py
@@ -13,8 +13,14 @@ from ert.config import GenKwConfig, ObservationGroups, OutlierSettings
 from ert.storage import Ensemble
 
 from . import misfit_preprocessor
-from .event import AnalysisDataEvent, AnalysisEvent, DataSection
-from .snapshots import ObservationStatus
+from .event import (
+    AnalysisDataEvent,
+    AnalysisEvent,
+    DataSection,
+)
+from .snapshots import (
+    ObservationStatus,
+)
 
 logger = logging.getLogger(__name__)
 
