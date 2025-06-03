2892ad34754fde31950bfa88a2fc72739eb5ddbf
Yngve S. Kristiansen <ynk@equinor.com>
2025-06-03 16:08:44 +0200

    Add manual update EnIF to ert

--- src/ert/gui/simulation/experiment_panel.py
+++ src/ert/gui/simulation/experiment_panel.py
@@ -35,6 +35,7 @@ from .ensemble_information_filter_panel import EnsembleInformationFilterPanel
 from .ensemble_smoother_panel import EnsembleSmootherPanel
 from .evaluate_ensemble_panel import EvaluateEnsemblePanel
 from .experiment_config_panel import ExperimentConfigPanel
+from .manual_enif_update_panel import ManualEnIFUpdatePanel
 from .manual_update_panel import ManualUpdatePanel
 from .multiple_data_assimilation_panel import MultipleDataAssimilationPanel
 from .run_dialog import RunDialog
@@ -211,6 +212,10 @@ class ExperimentPanel(QWidget):
         )
         self.addExperimentConfigPanel(
             ManualUpdatePanel(ensemble_size, run_path, notifier, analysis_config),
+            experiment_type_valid,
+        )
+        self.addExperimentConfigPanel(
+            ManualEnIFUpdatePanel(ensemble_size, run_path, notifier, analysis_config),
             experiment_type_valid,
         )
 
--- tests/ert/ui_tests/gui/test_full_manual_update_workflow.py
+++ tests/ert/ui_tests/gui/test_full_manual_update_workflow.py
@@ -2,24 +2,26 @@ import contextlib
 import shutil
 
 import numpy as np
+import pytest
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QComboBox, QToolButton, QTreeView, QWidget
 
 from ert.data import MeasuredData
 from ert.gui.simulation.evaluate_ensemble_panel import EvaluateEnsemblePanel
 from ert.gui.simulation.experiment_panel import ExperimentPanel
-from ert.gui.simulation.manual_update_panel import ManualUpdatePanel
 from ert.gui.simulation.run_dialog import RunDialog
 from ert.gui.tools.manage_experiments import ManageExperimentsPanel
 from ert.gui.tools.manage_experiments.storage_widget import StorageWidget
 from ert.run_models.evaluate_ensemble import EvaluateEnsemble
 from ert.run_models.manual_update import ManualUpdate
+from ert.run_models.manual_update_enif import ManualUpdateEnIF
 from ert.validation import rangestring_to_mask
 
 from .conftest import get_child, wait_for_child
 
 
-def test_manual_analysis_workflow(ensemble_experiment_has_run, qtbot):
+@pytest.mark.parametrize("mode", [ManualUpdate.name(), ManualUpdateEnIF.name()])
+def test_manual_analysis_workflow(mode, ensemble_experiment_has_run, qtbot):
     """This runs a full manual update workflow, first running ensemble experiment
     where some of the realizations fail, then doing an update before running an
     ensemble experiment again to calculate the forecast of the update.
@@ -28,9 +30,8 @@ def test_manual_analysis_workflow(ensemble_experiment_has_run, qtbot):
 
     # Select correct experiment in the simulation panel
     experiment_panel = get_child(gui, ExperimentPanel)
-    simulation_settings = get_child(experiment_panel, ManualUpdatePanel)
     simulation_mode_combo = get_child(experiment_panel, QComboBox)
-    simulation_mode_combo.setCurrentText(ManualUpdate.name())
+    simulation_mode_combo.setCurrentText(mode)
 
     with contextlib.suppress(FileNotFoundError):
         shutil.rmtree("poly_out")
