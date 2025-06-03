38c9b445b3f5ebb6a7455e8d14832a4648282251
Peter Verveer <pieter.verveer@tno.nl>
2025-06-03 11:54:27 +0000

    Remove result property from everest run model

--- tests/everest/test_cvar.py
+++ tests/everest/test_cvar.py
@@ -3,6 +3,7 @@ import pytest
 from ert.ensemble_evaluator.config import EvaluatorServerConfig
 from ert.run_models.everest_run_model import EverestRunModel
 from everest.config import EverestConfig
+from tests.everest.utils import get_optimal_result
 
 
 @pytest.mark.integration_test
@@ -30,13 +31,15 @@ def test_mathfunc_cvar(copy_math_func_test_data_to_tmp):
     evaluator_server_config = EvaluatorServerConfig()
     run_model.run_experiment(evaluator_server_config)
 
+    optimal_result = get_optimal_result(config.optimization_output_dir)
+
     # Assert
-    x0, x1, x2 = (run_model.result.controls["point." + p] for p in ["x", "y", "z"])
+    x0, x1, x2 = (optimal_result.controls["point." + p] for p in ["x", "y", "z"])
 
     assert x0 == pytest.approx(0.5, 0.05)
     assert x1 == pytest.approx(0.5, 0.05)
     assert x2 == pytest.approx(0.5, 0.05)
 
-    total_objective = run_model.result.total_objective
+    total_objective = optimal_result.total_objective
     assert total_objective <= 0.001
     assert total_objective >= -0.001
---
+++ tests/everest/utils/optimal_result.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+
+import polars as pl
+
+from everest.everest_storage import EverestStorage
+
+
+@dataclass
+class OptimalResult:
+    batch: int
+    controls: dict[str, Any]
+    total_objective: float
+
+
+def get_optimal_result(output_dir: str) -> OptimalResult | None:
+    storage = EverestStorage(Path(output_dir))
+    storage.read_from_output_dir()
+
+    matching_batches = [
+        b
+        for b in storage.data.batches_with_function_results
+        if not b.batch_objectives.is_empty() and b.is_improvement
+    ]
+
+    if matching_batches:
+        matching_batches.sort(
+            key=lambda b: -b.batch_objectives.select(
+                pl.col("total_objective_value").sample(n=1)
+            ).item()
+        )
+        batch = matching_batches[0]
+        controls_dict = batch.realization_controls.drop(
+            [
+                "batch_id",
+                "simulation_id",
+                "realization",
+            ]
+        ).to_dicts()[0]
+
+        return OptimalResult(
+            batch=batch.batch_id,
+            controls=controls_dict,
+            total_objective=batch.batch_objectives.select(
+                pl.col("total_objective_value")
+            ).item(),
+        )
+
+    return None
