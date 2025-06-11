0535ab092a6dcaa85d3e05fc5c8080927c4c7919
Peter Verveer <pieter.verveer@tno.nl>
2025-06-11 16:34:57 +0000

    Allow realization ranges in everest model config
    Adds a validator to ModelConfig that transforms strings consisting of a
    comma-separated list of integers and integer ranges of the form
    `min-max` into a list of integers.
    
    Example: "1, 2, 5-8, 3" is transformed into [1, 2, 3, 5, 6, 7, 8] and
    stored in the realizations field.

--- src/everest/config/model_config.py
+++ src/everest/config/model_config.py
@@ -1,15 +1,17 @@
+from collections.abc import Iterator
 from typing import Any, Self
 
-from pydantic import BaseModel, Field, NonNegativeInt, model_validator
+from pydantic import BaseModel, Field, NonNegativeInt, field_validator, model_validator
 
 from ert.config import ConfigWarning
 
 
 class ModelConfig(BaseModel, extra="forbid"):
     realizations: list[NonNegativeInt] = Field(
-        description="""List of realizations to use in optimization ensemble.
+        description="""Realizations to use in optimization ensemble.
 
-Typically, this is a list [0, 1, ..., n-1] of all realizations in the ensemble.""",
+This is a list [0, 1, ..., n-1] of all realizations in the ensemble, or a string
+consisting of comma separated values and ranges, i.e '1, 2, 5-8, 10'.""",
         min_length=1,
     )
     realizations_weights: list[float] = Field(
@@ -31,6 +33,26 @@ If specified, it must be a list of numeric values, one per realization.""",
             )
             values.pop("report_steps")
         return values
+
+    @field_validator("realizations", mode="before")
+    @classmethod
+    def convert_realizations(cls, realizations: Any) -> list[int]:
+        def _parse_realizations(realizations: str) -> Iterator[int]:
+            msg = f"Invalid realizations specification: {realizations}"
+            for item in realizations.split(","):
+                first, sep, second = (part.strip() for part in item.partition("-"))
+                if not sep:
+                    second = first
+                if not first.isdigit() or not second.isdigit():
+                    raise ValueError(msg)
+                start, stop = int(first), int(second)
+                if start > stop:
+                    raise ValueError(msg)
+                yield from range(start, stop + 1)
+
+        if isinstance(realizations, str):
+            return sorted(dict.fromkeys(_parse_realizations(realizations)))
+        return realizations
 
     @model_validator(mode="after")
     def validate_realizations_weights_same_cardinaltiy(self) -> Self:
