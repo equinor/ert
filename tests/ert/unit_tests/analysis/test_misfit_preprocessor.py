c0dcaf06c893f44a140bf61d663f5f609521214b
Feda Curic <feda.curic@gmail.com>
2025-06-13 08:08:03 +0200

    Extend and improve auto-scaling test

--- tests/ert/unit_tests/analysis/test_misfit_preprocessor.py
+++ tests/ert/unit_tests/analysis/test_misfit_preprocessor.py
@@ -53,39 +53,75 @@ def test_that_get_nr_primary_components_is_according_to_theory():
 
 @pytest.mark.parametrize("nr_observations", [4, 7, 12])
 @pytest.mark.integration_test
-def test_misfit_preprocessor(nr_observations):
-    """We create three independent parameters: a, b and c.
-    a has multiple observations, which are all strongly correlated
-    (of form (1+i)*a, i =(0, 1, ...)), while b and c only has 1 observation.
-    We expect the a observations to be scaled in one group,
-    and then the b and c observation to be scaled together.
-    b and c are scaled together due to the way one chooses pca components:
+def test_that_correlated_and_independent_observations_are_grouped_separately(
+    nr_observations,
+):
+    """
+    Test the preprocessor's ability to cluster correlated observations.
+
+    We create a response matrix with `nr_observations` rows, where the
+    first `nr_observations - 2` rows are strongly correlated, while the
+    last two are independent.
+
+    We expect the correlated observations to be scaled in one group,
+    and perhaps surprisingly, the two independent observations to be
+    scaled together in a second group.
+    The reason that the two independent responses end up in the same group
+    is due to the way get_nr_primary_components counts PCA components,
+    and the fact that the number of PCA components is used as the number
+    of clusters.
+    It returns the number of components that explain **less** than the
+    variance specified as the threshold.
+    With a threshold of 0.95, the expression used is as follows:
+
     max(len([1 for i in variance_ratio[:-1] if i < 0.95]), 1),
-    since one does not include the component that crosses the threshold one
-    gets one less component then one might expect.
-    And the pca components decide the number of clusters for scaling.
     """
     rng = np.random.default_rng(1234)
     nr_realizations = 1000
-    nr_correlated_obs = nr_observations - 2
-    Y = np.ones((nr_observations, nr_realizations))
+    nr_uncorrelated_obs = 2
+    nr_correlated_obs = nr_observations - nr_uncorrelated_obs
+
     parameters_a = rng.standard_normal(nr_realizations)
     parameters_b = rng.standard_normal(nr_realizations)
     parameters_c = rng.standard_normal(nr_realizations)
+
+    Y = np.ones((nr_observations, nr_realizations))
     for i in range(nr_correlated_obs):
         Y[i] = (1 + i) * parameters_a
     Y[-1] = 5 + parameters_b
     Y[-2] = 10 + parameters_c
+
     obs_errors = Y.std(axis=1)
     Y_original = Y.copy()
     obs_error_copy = obs_errors.copy()
-    result, *_ = main(Y, obs_errors)
+
+    scale_factors, clusters, nr_components = main(Y, obs_errors)
+
+    # Since the first nr_correlated_obs rows of Y are perfectly correlated,
+    # we only need one principal component to describe all variance.
+    nr_components_correlated_group = 1
+
+    # Because of the way we calculate the number of components
+    # (see docstring for details), the two undependent responses
+    # are represented by a single component.
+    nr_components_uncorrelated_group = 1
+
     np.testing.assert_equal(
-        result,
+        scale_factors,
         np.array(
-            nr_correlated_obs * [np.sqrt(nr_correlated_obs / 1.0)] + 2 * [np.sqrt(2)]
+            nr_correlated_obs
+            * [np.sqrt(nr_correlated_obs / nr_components_correlated_group)]
+            + nr_uncorrelated_obs
+            * [np.sqrt(nr_uncorrelated_obs / nr_components_uncorrelated_group)]
         ),
     )
+
+    expected_clusters = np.array(nr_correlated_obs * [1] + nr_uncorrelated_obs * [2])
+    np.testing.assert_equal(clusters, expected_clusters)
+
+    expected_nr_components = (nr_uncorrelated_obs + nr_correlated_obs) * [1]
+    np.testing.assert_equal(nr_components, expected_nr_components)
+
     # Check that we don`t modify the input data
     np.testing.assert_equal(Y, Y_original)
     np.testing.assert_equal(obs_errors, obs_error_copy)
