33b76842bf6d592765b31d912dc2e0042d0f825e
Yngve S. Kristiansen <ynk@equinor.com>
2025-06-17 12:41:29 +0200

    Remove unnecessary tests
    Covered by everest tutorial tests, this test mainly tests that the everest-models forward models validate a case. Could consider inlining it but that requires a lot of files.
    
    test_valid_config_file basically tests that a config without objectives is linted properly. This is now handled by pydantic anyway.

--- tests/everest/test_yaml_parser.py
+++ tests/everest/test_yaml_parser.py
@@ -4,14 +4,13 @@ from textwrap import dedent
 
 import pytest
 from pydantic import ValidationError
-from ruamel.yaml import YAML
 
 from ert.config.parsing import ConfigKeys as ErtConfigKeys
 from everest.config import EverestConfig
 from everest.simulator.everest_to_ert import (
     everest_to_ert_config_dict,
 )
-from tests.everest.utils import MockParser, skipif_no_everest_models
+from tests.everest.utils import skipif_no_everest_models
 
 
 @pytest.mark.parametrize("random_seed", [None, 1234])
@@ -56,61 +55,6 @@ def test_read_file(tmp_path, monkeypatch):
     exp_dir, exp_fn = os.path.split(os.path.realpath("config.yml"))
     assert exp_dir == everest_config.config_directory
     assert exp_fn == everest_config.config_file
-
-
-def test_valid_config_file(copy_test_data_to_tmp, monkeypatch):
-    monkeypatch.chdir("valid_config_file")
-    parser = MockParser()
-
-    config = EverestConfig.load_file_with_argparser(
-        "valid_yaml_config.yml", parser=parser
-    )
-    # Check no error is generated when loading a valid config file
-    assert parser.get_error() is None
-
-    yaml = YAML(typ="safe", pure=True)
-    with open("test", "w", encoding="utf-8") as f:
-        yaml.dump(config.to_dict(), f)
-
-    assert EverestConfig.load_file_with_argparser("test", parser=parser) is not None
-
-    config_dict = config.to_dict()
-    del config_dict["objective_functions"]
-    yaml = YAML(typ="safe", pure=True)
-    with open("test", "w", encoding="utf-8") as f:
-        yaml.dump(config_dict, f)
-
-    # Check a valid config file is also linted
-    assert EverestConfig.load_file_with_argparser("test", parser=parser) is None
-    assert "objective_functions" in parser.get_error()
-    assert "Field required" in parser.get_error()
-
-    # Check a invalid yaml errors are reported to the parser
-    assert (
-        EverestConfig.load_file_with_argparser(
-            config_path="invalid_yaml_config.yml", parser=parser
-        )
-        is None
-    )
-
-    assert (
-        "The config file: <invalid_yaml_config.yml> contains invalid YAML syntax:"
-        in parser.get_error()
-    )
-    assert "could not find expected ':'" in parser.get_error()
-
-
-@pytest.mark.skip_mac_ci
-@skipif_no_everest_models
-@pytest.mark.everest_models_test
-def test_valid_forward_model_config_files(copy_test_data_to_tmp, monkeypatch):
-    monkeypatch.chdir("valid_config_file/forward_models")
-    parser = MockParser()
-    EverestConfig.load_file_with_argparser(
-        "valid_config_forward_models.yml", parser=parser
-    )
-
-    assert parser.get_error() is None
 
 
 @skipif_no_everest_models
