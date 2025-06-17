990a107a252cdd2df734022e38f78ef8394ee555
Yngve S. Kristiansen <ynk@equinor.com>
2025-06-17 09:06:57 +0200

    Remove unnecessary test
    It is loaded and run (with output constraints) in test_sim_output_constraints

--- tests/everest/test_everest_initialization.py
+++ tests/everest/test_everest_initialization.py
@@ -18,12 +18,6 @@ def test_init_no_project_res(copy_egg_test_data_to_tmp):
     EverestRunModel.create(config)
 
 
-def test_init(copy_mocked_test_data_to_tmp):
-    config_file = os.path.join("mocked_test_case.yml")
-    config = EverestConfig.load_file(config_file)
-    EverestRunModel.create(config)
-
-
 def test_no_config_init():
     with pytest.raises(AttributeError):
         EverestRunModel.create(None)
