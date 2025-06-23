30e8f3026dc237764c9d7ed972c2ed67a2d7dbe6
Frode Aarstad <frodeaarstad@gmail.com>
2025-06-23 13:47:02 +0200

    Add SUBMIT_SLEEP to the site config schema
    (cherry picked from commit f27e014cb8c8aee22f72823a4a1aa388056c151a)

--- src/ert/config/parsing/config_schema.py
+++ src/ert/config/parsing/config_schema.py
@@ -296,6 +296,7 @@ def init_site_config_schema() -> ConfigSchemaDict:
         positive_int_keyword(ConfigKeys.MAX_SUBMIT),
         positive_int_keyword(ConfigKeys.NUM_CPU),
         string_keyword(ConfigKeys.REALIZATION_MEMORY),
+        float_keyword(ConfigKeys.SUBMIT_SLEEP),
         queue_system_keyword(False),
         queue_option_keyword(),
         job_script_keyword(),
