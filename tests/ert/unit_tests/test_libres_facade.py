a320262e684319c9025a445f256ce79e24422d07
Yngve S. Kristiansen <ynk@equinor.com>
2025-06-27 13:32:24 +0200

    Move storage tests to test_local_storage

--- tests/ert/unit_tests/test_libres_facade.py
+++ tests/ert/unit_tests/test_libres_facade.py
@@ -1,43 +1,8 @@
 from textwrap import dedent
 
 import pytest
-from pandas.core.frame import DataFrame
 
 from ert.libres_facade import LibresFacade
-from ert.storage import open_storage
-
-
-def test_keyword_type_checks(snake_oil_default_storage):
-    assert (
-        "BPR:1,3,8"
-        in snake_oil_default_storage.experiment.response_type_to_response_keys[
-            "summary"
-        ]
-    )
-
-
-def test_keyword_type_checks_missing_key(snake_oil_default_storage):
-    assert (
-        "nokey"
-        not in snake_oil_default_storage.experiment.response_type_to_response_keys[
-            "summary"
-        ]
-    )
-
-
-@pytest.mark.filterwarnings("ignore:.*Use load_responses.*:DeprecationWarning")
-def test_data_fetching_missing_key(snake_oil_case):
-    with open_storage(snake_oil_case.ens_path, mode="w") as storage:
-        experiment = storage.create_experiment()
-        empty_case = experiment.create_ensemble(name="new_case", ensemble_size=25)
-
-        data = [
-            empty_case.load_all_gen_kw_data("nokey", None),
-        ]
-
-        for dataframe in data:
-            assert isinstance(dataframe, DataFrame)
-            assert dataframe.empty
 
 
 @pytest.mark.usefixtures("use_tmpdir")
