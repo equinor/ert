07ba379cbdfc18f2ba5c7f3d9248d77f7b7284df
Yngve S. Kristiansen <ynk@equinor.com>
2025-06-27 13:32:55 +0200

    Remove noop test

--- tests/ert/unit_tests/test_libres_facade.py
+++ tests/ert/unit_tests/test_libres_facade.py
@@ -1,25 +1,6 @@
-from textwrap import dedent
-
 import pytest
 
 from ert.libres_facade import LibresFacade
-
-
-@pytest.mark.usefixtures("use_tmpdir")
-def test_gen_kw_log_appended_extra():
-    with open("config_file.ert", "w", encoding="utf-8") as fout:
-        fout.write(
-            dedent(
-                """
-        NUM_REALIZATIONS 1
-        GEN_KW KW_NAME template.txt kw.txt prior.txt
-        """
-            )
-        )
-    with open("template.txt", "w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD <MY_KEYWORD>")
-    with open("prior.txt", "w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD LOGNORMAL 1 2")
 
 
 def test_misfit_collector(snake_oil_case_storage, snake_oil_default_storage, snapshot):
