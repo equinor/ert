4c4cbb6951d43256f0e87dc4a2c2d0dde9bce395
Yngve S. Kristiansen <ynk@equinor.com>
2025-06-27 12:38:26 +0200

    Remove redundant use of fixtures in test

--- tests/ert/unit_tests/test_libres_facade.py
+++ tests/ert/unit_tests/test_libres_facade.py
@@ -7,23 +7,6 @@ from ert.libres_facade import LibresFacade
 from ert.storage import open_storage
 
 
-@pytest.fixture
-def facade(snake_oil_case):
-    return LibresFacade(snake_oil_case)
-
-
-@pytest.fixture
-def storage(facade):
-    with open_storage("storage/snake_oil/ensemble", mode="w") as storage:
-        yield storage
-
-
-@pytest.fixture
-def empty_case(facade, storage):
-    experiment_id = storage.create_experiment()
-    return storage.create_ensemble(experiment_id, name="new_case", ensemble_size=25)
-
-
 def test_keyword_type_checks(snake_oil_default_storage):
     assert (
         "BPR:1,3,8"
@@ -43,14 +26,18 @@ def test_keyword_type_checks_missing_key(snake_oil_default_storage):
 
 
 @pytest.mark.filterwarnings("ignore:.*Use load_responses.*:DeprecationWarning")
-def test_data_fetching_missing_key(empty_case):
-    data = [
-        empty_case.load_all_gen_kw_data("nokey", None),
-    ]
+def test_data_fetching_missing_key(snake_oil_case):
+    with open_storage(snake_oil_case.ens_path, mode="w") as storage:
+        experiment = storage.create_experiment()
+        empty_case = experiment.create_ensemble(name="new_case", ensemble_size=25)
 
-    for dataframe in data:
-        assert isinstance(dataframe, DataFrame)
-        assert dataframe.empty
+        data = [
+            empty_case.load_all_gen_kw_data("nokey", None),
+        ]
+
+        for dataframe in data:
+            assert isinstance(dataframe, DataFrame)
+            assert dataframe.empty
 
 
 @pytest.mark.usefixtures("use_tmpdir")
