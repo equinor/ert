f7ad26168e5d79495e69b34925edc54110af4032
Peter Verveer <pieter.verveer@tno.nl>
2025-07-08 08:27:56 +0000

    Fix FlattenedControls
    The original code was buggy, since it did not handle all fields in the
    case the variable field has class `ControlVariableGuessListConfig `. The
    result was that some fields would fall back to the values in the control
    section, or to defaults. The code has been rewritten such that the
    parsing of all fields is shared by the  `ControlVariableGuessListConfig`
    and `ControlVariableConfig` cases. A test to check this has been added.

--- src/everest/config/utils.py
+++ src/everest/config/utils.py
@@ -1,124 +1,92 @@
-from collections.abc import Generator
+from copy import deepcopy
 from typing import Any, Literal
 
 from .control_config import ControlConfig
-from .control_variable_config import (
-    ControlVariableConfig,
-    ControlVariableGuessListConfig,
-)
+from .control_variable_config import ControlVariableGuessListConfig
 from .sampler_config import SamplerConfig
 
 
 class FlattenedControls:
     def __init__(self, controls: list[ControlConfig]) -> None:
-        self._controls = []
-        self._samplers: list[SamplerConfig] = []
-
-        for control in controls:
-            control_sampler_idx = -1
-            variables = []
-            for variable in control.variables:
-                match variable:
-                    case ControlVariableConfig():
-                        var_dict, control_sampler_idx = self._add_variable(
-                            control, variable, control_sampler_idx
-                        )
-                        variables.append(var_dict)
-                    case ControlVariableGuessListConfig():
-                        var_dicts, control_sampler_idx = self._add_variable_guess_list(
-                            control, variable, control_sampler_idx
-                        )
-                        variables.extend(var_dicts)
-            self._inject_defaults(control, variables)
-            self._controls.extend(variables)
-
-        self.names = [control["name"] for control in self._controls]
+        control_dicts = _get_control_dicts(controls)
+        self.names = [control["name"] for control in control_dicts]
         self.types: list[Literal["real", "integer"]] = [
-            control["control_type"] for control in self._controls
+            control["control_type"] for control in control_dicts
         ]
-        self.initial_guesses = [control["initial_guess"] for control in self._controls]
-        self.lower_bounds = [control["min"] for control in self._controls]
-        self.upper_bounds = [control["max"] for control in self._controls]
-        self.scaled_ranges = [control["scaled_range"] for control in self._controls]
-        self.enabled = [control["enabled"] for control in self._controls]
+        self.initial_guesses = [control["initial_guess"] for control in control_dicts]
+        self.lower_bounds = [control["min"] for control in control_dicts]
+        self.upper_bounds = [control["max"] for control in control_dicts]
+        self.scaled_ranges = [control["scaled_range"] for control in control_dicts]
+        self.enabled = [control["enabled"] for control in control_dicts]
         self.perturbation_magnitudes = [
-            control["perturbation_magnitude"] for control in self._controls
+            control["perturbation_magnitude"] for control in control_dicts
         ]
         self.perturbation_types = [
-            control["perturbation_type"] for control in self._controls
+            control["perturbation_type"] for control in control_dicts
         ]
-        self.sampler_indices = [control["sampler_idx"] for control in self._controls]
-        self.samplers = self._samplers
+        self.samplers, self.sampler_indices = _get_samplers(controls)
 
-    def _add_variable(
-        self,
-        control: ControlConfig,
-        variable: ControlVariableConfig,
-        control_sampler_idx: int,
-    ) -> tuple[dict[str, Any], int]:
-        var_dict = {
-            key: getattr(variable, key)
-            for key in [
-                "control_type",
-                "enabled",
-                "scaled_range",
-                "min",
-                "max",
-                "perturbation_magnitude",
-                "initial_guess",
-            ]
-        }
-        var_dict["name"] = (
-            (control.name, variable.name)
-            if variable.index is None
-            else (control.name, variable.name, variable.index)
-        )
-        if variable.sampler is not None:
-            self._samplers.append(variable.sampler)
-            var_dict["sampler_idx"] = len(self._samplers) - 1
-        else:
-            if control.sampler is not None and control_sampler_idx < 0:
-                self._samplers.append(control.sampler)
-                control_sampler_idx = len(self._samplers) - 1
-            var_dict["sampler_idx"] = control_sampler_idx
-        return var_dict, control_sampler_idx
 
-    def _add_variable_guess_list(
-        self,
-        control: ControlConfig,
-        variable: ControlVariableGuessListConfig,
-        control_sampler_idx: int,
-    ) -> tuple[Generator[dict[str, Any], None, None], int]:
-        if control.sampler is not None and control_sampler_idx < 0:
-            self._samplers.append(control.sampler)
-            control_sampler_idx = len(self._samplers) - 1
-        return (
-            (
-                {
-                    "name": (control.name, variable.name, index + 1),
-                    "initial_guess": guess,
-                    "sampler_idx": control_sampler_idx,
-                }
-                for index, guess in enumerate(variable.initial_guess)
-            ),
-            control_sampler_idx,
-        )
+def _get_control_dicts(controls: list[ControlConfig]) -> list[dict[str, Any]]:
+    def _inject_defaults(control: ControlConfig, var_dict: dict[str, Any]) -> None:
+        for key in [
+            "type",
+            "initial_guess",
+            "control_type",
+            "enabled",
+            "min",
+            "max",
+            "perturbation_type",
+            "perturbation_magnitude",
+            "scaled_range",
+        ]:
+            if var_dict.get(key) is None:
+                var_dict[key] = getattr(control, key)
 
-    @staticmethod
-    def _inject_defaults(
-        control: ControlConfig, variables: list[dict[str, Any]]
-    ) -> None:
-        for var_dict in variables:
-            for key in [
-                "type",
-                "initial_guess",
-                "control_type",
-                "enabled",
-                "min",
-                "max",
-                "perturbation_type",
-                "perturbation_magnitude",
-                "scaled_range",
-            ]:
-                if var_dict.get(key) is None:
-                    var_dict[key] = getattr(control, key)
+    control_dicts: list[dict[str, Any]] = []
+    for control in controls:
+        for variable in control.variables:
+            if isinstance(variable, ControlVariableGuessListConfig):
+                for index, guess in enumerate(variable.initial_guess, start=1):
+                    var_dict = deepcopy(variable.model_dump())
+                    var_dict["name"] = (control.name, variable.name, index)
+                    var_dict["initial_guess"] = guess
+                    _inject_defaults(control, var_dict)
+                    control_dicts.append(var_dict)
+            else:
+                var_dict = deepcopy(variable.model_dump())
+                var_dict["name"] = (
+                    (control.name, variable.name)
+                    if variable.index is None
+                    else (control.name, variable.name, variable.index)
+                )
+                _inject_defaults(control, var_dict)
+                control_dicts.append(var_dict)
+    return control_dicts
+
+
+def _get_samplers(
+    controls: list[ControlConfig],
+) -> tuple[list[SamplerConfig | None], list[int]]:
+    samplers: list[SamplerConfig | None] = []
+    sampler_indices: list[int] = []
+    for control in controls:
+        control_sampler_idx: int | None = None
+        for variable in control.variables:
+            if variable.sampler is None:
+                if control_sampler_idx is None:
+                    samplers.append(control.sampler)
+                    control_sampler_idx = len(samplers) - 1
+                sampler_index = control_sampler_idx
+            else:
+                samplers.append(variable.sampler)
+                sampler_index = len(samplers) - 1
+            if isinstance(variable, ControlVariableGuessListConfig):
+                sampler_indices.extend([sampler_index] * len(variable.initial_guess))
+            else:
+                sampler_indices.append(sampler_index)
+    return samplers, sampler_indices
+
+
+def flatten_controls(controls: list[ControlConfig]) -> FlattenedControls:
+    return FlattenedControls(controls)
