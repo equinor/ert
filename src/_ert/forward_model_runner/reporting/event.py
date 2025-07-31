c088bbc3204845ecfcd9322041765d28f1e26d52
Jonathan Karlsen <JONAK@equinor.com>
2025-07-31 10:31:58 +0200

    Fix fm_dispatch failing if terminating before started
    This commit fixes the issue where fm_dispatcher fails ungracefully if we
    terminate before it has created the Init message, or no
    forward_model_step has started.

--- src/_ert/forward_model_runner/reporting/event.py
+++ src/_ert/forward_model_runner/reporting/event.py
@@ -62,6 +62,8 @@ class Event(Reporter):
         ack_timeout: float | None = None,
         max_retries: int | None = None,
         finished_event_timeout: float | None = None,
+        ens_id: str | None = None,
+        real_id: str | None = None,
     ) -> None:
         self._evaluator_url = evaluator_url
         self._token = token
@@ -72,8 +74,8 @@ class Event(Reporter):
         self._statemachine.add_handler((Checksum,), self._checksum_handler)
         self._statemachine.add_handler((Finish,), self._finished_handler)
 
-        self._ens_id = None
-        self._real_id = None
+        self._ens_id = ens_id
+        self._real_id = real_id
         self._event_queue: queue.Queue[DispatcherEvent | EventSentinel] = queue.Queue()
         self._event_publisher_thread = ErtThread(
             target=self._event_publisher, should_raise=False
@@ -148,12 +150,11 @@ class Event(Reporter):
         self._event_publisher_thread.start()
 
     def _step_handler(self, msg: Start | Running | Exited):
-        assert msg.step
-        step_name = msg.step.name()
+        step_name = msg.step.name() if msg.step is not None else "Unknown"
         step_msg = {
             "ensemble": self._ens_id,
-            "real": self._real_id,
-            "fm_step": str(msg.step.index),
+            "real": self._real_id if self._real_id is not None else "Unknown",
+            "fm_step": str(msg.step.index if msg.step is not None else 0),
         }
         if isinstance(msg, Start):
             logger.debug(f"Step {step_name} was successfully started")
@@ -175,7 +176,7 @@ class Event(Reporter):
             else:
                 logger.error(
                     _STEP_EXIT_FAILED_STRING.format(
-                        step_name=msg.step.name(),
+                        step_name=step_name,
                         exit_code=msg.exit_code,
                         error_message=msg.error_message,
                     )
