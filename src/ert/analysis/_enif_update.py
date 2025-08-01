558711bf393a6c2f64119b2f160d97a7dd5a9048
xjules <jparu@equinor.com>
2025-08-01 10:27:45 +0200

    Do copy unupdated parameters only once in enif

--- src/ert/analysis/_enif_update.py
+++ src/ert/analysis/_enif_update.py
@@ -239,13 +239,13 @@ def analysis_EnIF(
             f"Storing data for {param_group} completed in "
             f"{(time.time() - start) / 60} minutes"
         )
-        _copy_unupdated_parameters(
-            list(source_ensemble.experiment.parameter_configuration.keys()),
-            parameters,
-            iens_active_index,
-            source_ensemble,
-            target_ensemble,
-        )
+    _copy_unupdated_parameters(
+        list(source_ensemble.experiment.parameter_configuration.keys()),
+        parameters,
+        iens_active_index,
+        source_ensemble,
+        target_ensemble,
+    )
 
     stop_enif = time.time()
     logger.info(f"EnIF total update time: {stop_enif - start_enif} seconds")
