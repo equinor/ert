c5fb863f857187235f890f73697d04a4de006a14
Eivind Jahren <ejah@equinor.com>
2025-08-15 07:22:14 +0200

    Make imports of ert.warnings conform to public import conventions

--- src/ert/warnings/__init__.py
+++ src/ert/warnings/__init__.py
@@ -1,6 +1,8 @@
 from ._warnings import ErtWarning, PostSimulationWarning
+from .specific_warning_handler import capture_specific_warning
 
 __all__ = [
     "ErtWarning",
     "PostSimulationWarning",
+    "capture_specific_warning",
 ]
