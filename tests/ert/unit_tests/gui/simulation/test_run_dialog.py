9f6b4a6c36d1c43d87e9b2e64968695ecfb1045b
Håvard Berland <havb@equinor.com>
2025-08-26 14:35:43 +0200

    Bump timeout for a flaky test
    Has been observed to fail with a timeout when timeout was 5000ms
    
    (cherry picked from commit 1ba6d93299ae9ea69cd900bf48778efa0fd0cae5)

--- tests/ert/unit_tests/gui/simulation/test_run_dialog.py
+++ tests/ert/unit_tests/gui/simulation/test_run_dialog.py
@@ -176,7 +176,7 @@ def test_terminating_experiment_shows_a_confirmation_dialog(qtbot: QtBot, run_di
 def test_run_dialog_polls_run_model_for_runtime(
     qtbot, mock_set_is_simulation_running, mock_get_runtime, run_dialog: RunDialog
 ):
-    qtbot.waitUntil(lambda: run_dialog.is_simulation_done() is True)
+    qtbot.waitUntil(lambda: run_dialog.is_simulation_done() is True, timeout=10000)
     mock_get_runtime.assert_any_call()
     mock_set_is_simulation_running.assert_has_calls([call(True), call(False)])
 
