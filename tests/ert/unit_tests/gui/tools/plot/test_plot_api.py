3452bb7f0e1c11dd4216f77cf874979b77afe978
Håkon Steinkopf Søhoel <hsoho@equinor.com>
2025-08-29 15:31:07 +0200

    Fix data_for_response keys may match substrings

--- tests/ert/unit_tests/gui/tools/plot/test_plot_api.py
+++ tests/ert/unit_tests/gui/tools/plot/test_plot_api.py
@@ -22,6 +22,7 @@ from ert.dark_storage.app import app
 from ert.gui.tools.plot.plot_api import PlotApi, PlotApiKeyDefinition
 from ert.services import StorageService
 from ert.storage import open_storage
+from ert.summary_key_type import history_key
 from tests.ert.unit_tests.gui.tools.plot.conftest import MockResponse
 
 
@@ -521,3 +522,53 @@ def test_that_response_key_has_observation_when_only_one_experiment_has_observat
         assert responses_from_api[0].observations, (
             "Expected FOPR to have observations, also when only one experiment has it"
         )
+
+
+@pytest.mark.parametrize("well_name", ("46/3-7s", "FOPR"))
+def test_that_response_keys_do_not_match_keys_that_are_substrings(
+    api_and_storage, well_name
+):
+    api, storage = api_and_storage
+    key = f"WBHP:{well_name}"
+    date = datetime(year=2024, month=10, day=4)
+    experiment = storage.create_experiment(
+        parameters=[],
+        responses=[
+            SummaryConfig(
+                name="summary",
+                input_files=["CASE.UNSMRY", "CASE.SMSPEC"],
+                keys=[key, history_key(key), "FOPR"],
+            )
+        ],
+        observations={
+            "summary": pl.DataFrame(
+                {
+                    "observation_key": [key, history_key(key), "FOPR"],
+                    "response_key": [key, history_key(key), "FOPR"],
+                    "time": [date, date, date],
+                    "observations": [0.2, 0.1, 0.01],
+                    "std": [0.1, 0.1, 0.2],
+                }
+            )
+        },
+    )
+    ensemble = experiment.create_ensemble(ensemble_size=1, name="ensemble")
+    assert api.data_for_response(str(ensemble.id), key).empty
+    ensemble.save_response(
+        "summary",
+        pl.DataFrame(
+            {
+                "response_key": ["FOPR"],
+                "time": [pl.Series([date]).dt.cast_time_unit("ms")],
+                "values": [pl.Series([1.0], dtype=pl.Float32)],
+            }
+        ).explode("values", "time"),
+        0,
+    )
+    assert api.data_for_response(str(ensemble.id), "FOPR").to_csv() == dedent(
+        """\
+        Realization,2024-10-04
+        0,1.0
+        """
+    )
+    assert api.data_for_response(str(ensemble.id), key).empty
