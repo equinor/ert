523cfd790316ae17e772231f0ffeba0c78c6c8aa
Håvard Berland <havb@equinor.com>
2025-09-02 16:06:42 +0200

    Catch the only possible exception from open_storage
    The open_storage() call can not throw a RuntimeError since it catches anything
    and re-raises as an ErtStorageException. This commit thus changes the name of
    the exception raised in dark_storage from ErtStorageException to InternalServerError,
    probably as originally intended.

--- src/ert/dark_storage/common.py
+++ src/ert/dark_storage/common.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from ert.dark_storage.exceptions import InternalServerError
-from ert.storage import Storage, open_storage
+from ert.storage import ErtStorageException, Storage, open_storage
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +15,7 @@ def get_storage() -> Storage:
     if _storage is None:
         try:
             return (_storage := open_storage(os.environ["ERT_STORAGE_ENS_PATH"]))
-        except RuntimeError as err:
+        except ErtStorageException as err:
             raise InternalServerError(f"{err!s}") from err
     _storage.refresh()
     return _storage
