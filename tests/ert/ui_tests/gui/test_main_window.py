d6b63132d75de18f72d35811f022b43e7defd636
Yngve S. Kristiansen <ynk@equinor.com>
2025-09-12 14:49:39 +0200

    Remove ErtPluginManager arg from main fns
    added manual call to pass the global
    logger to consumer plugins

--- tests/ert/ui_tests/gui/test_main_window.py
+++ tests/ert/ui_tests/gui/test_main_window.py
@@ -50,7 +50,6 @@ from ert.gui.tools.plot.plot_window import (
     PlotApi,
     PlotWindow,
 )
-from ert.plugins import get_site_plugins
 from ert.run_models import (
     EnsembleExperiment,
     EnsembleInformationFilter,
@@ -184,9 +183,7 @@ def test_that_there_is_a_link_to_github_in_the_suggester(tmp_path, qtbot):
     args = Mock()
     args.config = str(config_file)
     with add_gui_log_handler() as log_handler:
-        gui, *_ = ert.gui.main._start_initial_gui_window(
-            args, log_handler, get_site_plugins()
-        )
+        gui, *_ = ert.gui.main._start_initial_gui_window(args, log_handler)
         assert isinstance(gui, Suggestor)
 
         with patch("webbrowser.open", MagicMock(return_value=True)) as browser_open:
