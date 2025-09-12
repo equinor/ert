e21cb54d3cb04120800b28b2625ae5890b400b0b
Eivind Jahren <ejah@equinor.com>
2025-09-12 14:26:12 +0200

    Move SummaryObservation to where it is used

--- src/ert/config/_create_observations.py
+++ src/ert/config/_create_observations.py
@@ -27,13 +27,25 @@ from .parsing import (
     HistorySource,
     ObservationConfigError,
 )
-from .summary_observation import SummaryObservation
 
 if TYPE_CHECKING:
     import numpy.typing as npt
 
 
 DEFAULT_TIME_DELTA = timedelta(seconds=30)
+
+
+@dataclass
+class SummaryObservation:
+    summary_key: str
+    observation_key: str
+    value: float
+    std: float
+    std_scaling: float = 1.0
+
+    def __post_init__(self) -> None:
+        if self.std <= 0:
+            raise ValueError("Observation uncertainty must be strictly > 0")
 
 
 @dataclass(eq=False)
