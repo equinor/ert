c4d789bdda2795fa0559ed05d1fd074e38546009
Eivind Jahren <ejah@equinor.com>
2025-09-15 11:29:00 +0200

    Use environment variable for skip_mac_ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,6 +52,11 @@ def pytest_collection_modifyitems(config, items):
         ):
             item.add_marker(pytest.mark.skip("Requires eclipse"))
 
+    if os.environ.get("ERT_TESTS_RUN_ON_MAC_CI"):
+        for item in items:
+            if "skip_mac_ci" in item.keywords:
+                item.add_marker(pytest.mark.skip(reason="Skipped on mac ci"))
+
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
         skip_quick = pytest.mark.skip(
