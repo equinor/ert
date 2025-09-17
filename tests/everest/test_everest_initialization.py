c4b98026065d7fdb37daf7a29dc3f3360bad3733
Frode Aarstad <frodeaarstad@gmail.com>
2025-09-17 09:11:43 +0200

    Remove everest-models tests
    These tests are moved into the everest-models repo

--- tests/everest/test_everest_initialization.py
+++ tests/everest/test_everest_initialization.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from textwrap import dedent
 from unittest import mock
@@ -8,16 +7,6 @@ import pytest
 from ert.plugins import ErtPluginContext, ErtRuntimePlugins
 from ert.run_models.everest_run_model import EverestRunModel
 from everest.config import EverestConfig
-
-from .utils import skipif_no_everest_models
-
-
-@skipif_no_everest_models
-@pytest.mark.requires_eclipse
-def test_init_no_project_res(copy_eightcells_test_data_to_tmp):
-    config_file = os.path.join("everest", "model", "config.yml")
-    config = EverestConfig.load_file(config_file)
-    EverestRunModel.create(config)
 
 
 def test_no_config_init():
