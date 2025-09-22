e35cca86c8e46cc3d5ff61e5f3dfaab2636fc04c
Feda Curic <feda.curic@gmail.com>
2025-09-22 09:00:29 +0200

    Exclude SCRIPT from words

--- tests/ert/unit_tests/config/config_dict_generator.py
+++ tests/ert/unit_tests/config/config_dict_generator.py
@@ -42,11 +42,13 @@ from .observations_generator import (
 )
 from .summary_generator import Date, Smspec, Unsmry, smspecs, summary_variables, unsmrys
 
+# Exclude SCRIPT because it is used as folder-name in
+# generate_files_and_dict
 words = st.text(
     min_size=4,
     max_size=8,
     alphabet=st.characters(min_codepoint=ord("A"), max_codepoint=ord("Z")),
-)
+).filter(lambda x: x != "SCRIPT")
 
 
 def touch(filename):
