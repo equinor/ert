b9aef7e946e316f894f67267c6e9686637ca6cae
Andreas Eknes Lie <andrli@equinor.com>
2025-10-17 09:35:45 +0200

    Prohibit opening plotter when different plot api version

--- tests/ert/unit_tests/gui/tools/plot/test_plot_window.py
+++ tests/ert/unit_tests/gui/tools/plot/test_plot_window.py
@@ -1,8 +1,11 @@
+from unittest.mock import MagicMock
+
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QPushButton
+from PyQt6.QtWidgets import QApplication, QLabel, QPushButton
 from pytestqt.qtbot import QtBot
 
-from ert.gui.tools.plot.plot_window import create_error_dialog
+from ert.gui.tools.plot.plot_window import PlotWindow, create_error_dialog
+from ert.services import StorageService
 
 
 def test_pressing_copy_button_in_error_dialog(qtbot: QtBot):
@@ -13,3 +16,23 @@ def test_pressing_copy_button_in_error_dialog(qtbot: QtBot):
         qd.findChild(QPushButton, name="copy_button"), Qt.MouseButton.LeftButton
     )
     assert QApplication.clipboard().text() == "world"
+
+
+def test_warning_is_visible_on_incompatible_plot_api_version(
+    qtbot: QtBot, tmp_path, monkeypatch, use_tmpdir
+):
+    mock_get_data = MagicMock()
+    mock_get_data.return_value = "0.2"
+    monkeypatch.setattr(
+        "ert.gui.tools.plot.plot_api.PlotApi.api_version", mock_get_data
+    )
+
+    with StorageService.init_service(project=tmp_path):
+        pw = PlotWindow("", tmp_path, None)
+        qtbot.addWidget(pw)
+        pw.show()
+
+        label = pw.findChild(QLabel, name="plot_api_warning_label")
+        assert label
+        assert label.isVisible()
+        assert label.text().startswith("<b>Plot API version mismatch detected")
