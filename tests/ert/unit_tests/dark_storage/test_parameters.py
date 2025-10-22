06648e7fd2cb56796f9b0e40871ca901d422ac93
Kristian Flikka <kristian.flikka@gmail.com>
2025-10-22 09:30:58 +0200

    If load_scalars returns empty, return early
    Log warning if it happens, should not happen, but has been observered
    Add test for data_for_parameter in parameters.py
    
    (cherry picked from commit 479acf5032230811ae1c62bb0f6c755ff21dfd9e)

---
+++ tests/ert/unit_tests/dark_storage/test_parameters.py
@@ -0,0 +1,36 @@
+from ert.config import ErtConfig
+from ert.dark_storage.endpoints.parameters import data_for_parameter
+from ert.storage import open_storage
+
+
+def test_that_asking_for_non_existent_key_doesnt_raise(
+    symlinked_heat_equation_storage_es,
+):
+    config = ErtConfig.from_file("config.ert")
+    with open_storage(config.ens_path, mode="r") as storage:
+        ensemble = next(storage.ensembles)
+        assert "variable" not in ensemble.experiment.parameter_configuration
+        df = data_for_parameter(ensemble, "RANDOM:variable")
+        assert df.empty
+
+
+def test_that_asking_for_existing_key_without_group_returns_empty(
+    symlinked_heat_equation_storage_es,
+):
+    config = ErtConfig.from_file("config.ert")
+    with open_storage(config.ens_path, mode="r") as storage:
+        ensemble = next(storage.ensembles)
+        assert ensemble.experiment.parameter_configuration["COND"]
+        df = data_for_parameter(ensemble, "COND")
+        assert df.empty
+
+
+def test_that_asking_for_existing_key_with_group_returns_data(
+    symlinked_heat_equation_storage_es,
+):
+    config = ErtConfig.from_file("config.ert")
+    with open_storage(config.ens_path, mode="r") as storage:
+        ensemble = next(storage.ensembles)
+        assert "t" in ensemble.experiment.parameter_configuration
+        df = data_for_parameter(ensemble, "INIT_TEMP_SCALE:t")
+        assert not df.empty
