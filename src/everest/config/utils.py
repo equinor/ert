4108fe3460619636797a28ea117496eac858833b
Yngve S. Kristiansen <ynk@equinor.com>
2025-11-11 08:23:55 +0100

    Remove FlattenedControls
    Access ExtParamConfig directly instead

--- src/everest/config/utils.py
+++ src/everest/config/utils.py
@@ -1,41 +1,7 @@
-from typing import Literal
-
 from ert.config import ExtParamConfig, SamplerConfig
 
 
-class FlattenedControls:
-    def __init__(self, controls: list[ExtParamConfig]) -> None:
-        self.names = [name for control in controls for name in control.input_keys]
-        self.types: list[Literal["real", "integer"]] = [
-            type_ for control in controls for type_ in control.control_types
-        ]
-        self.initial_guesses = [
-            initial_guess
-            for control in controls
-            for initial_guess in control.initial_guesses
-        ]
-        self.lower_bounds = [min_ for control in controls for min_ in control.min]
-        self.upper_bounds = [max_ for control in controls for max_ in control.max]
-        self.scaled_ranges = [
-            scaled_range
-            for control in controls
-            for scaled_range in control.scaled_ranges
-        ]
-        self.enabled = [enabled for control in controls for enabled in control.enabled]
-        self.perturbation_magnitudes = [
-            perturbation_magnitude
-            for control in controls
-            for perturbation_magnitude in control.perturbation_magnitudes
-        ]
-        self.perturbation_types = [
-            perturbation_type
-            for control in controls
-            for perturbation_type in control.perturbation_types
-        ]
-        self.samplers, self.sampler_indices = _get_samplers(controls)
-
-
-def _get_samplers(
+def get_samplers(
     controls: list[ExtParamConfig],
 ) -> tuple[list[SamplerConfig | None], list[int]]:
     """
