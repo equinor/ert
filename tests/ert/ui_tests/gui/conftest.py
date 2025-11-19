equinor/ert

commit 7f2114f1afd3329461dce944e36d14ae96121dbc

Make ESMDA restart spawn new experiment

src/ert/run_models/multiple_data_assimilation.py
@@ -64,6 +64,7 @@ class MultipleDataAssimilation(UpdateRunModel, InitialEnsembleRunModel):
         if rerun_failed_realizations:
             raise ErtRunError("ESMDA does not support restart")
 
+        target_experiment = None
         if self.restart_run:
             id_ = self.prior_ensemble_id
             assert id_ is not None
@@ -80,6 +81,14 @@ class MultipleDataAssimilation(UpdateRunModel, InitialEnsembleRunModel):
                         f"iteration: {self.start_iteration},"
                         f"restart iteration = {prior.iteration + 1}"
                     )
+                target_experiment = self._storage.create_experiment(
+                    parameters=list(prior.experiment.parameter_configuration.values()),
+                    responses=list(prior.experiment.response_configuration.values()),
+                    observations=prior.experiment.observations,
+                    simulation_arguments=prior.experiment.metadata,
+                    name=f"Restart from {prior.name}",
+                    templates=prior.experiment.templates_configuration,
+                )
             except (KeyError, ValueError) as err:
                 raise ErtRunError(
                     f"Prior ensemble with ID: {id_} does not exists"
@@ -105,6 +114,7 @@ class MultipleDataAssimilation(UpdateRunModel, InitialEnsembleRunModel):
                 prior,
                 self.target_ensemble % (iteration + 1),
                 weight=weight,
+                target_experiment=target_experiment,
             )
             posterior_args = create_run_arguments(
                 self._run_paths,

commit 407215a0b80a83533fd87b8905bebd6484019284

Fix ert templates when creating restart experiment
This fixes a regression from 7f2114f1 where the template contents were
put into the new experiment instead of the template specification.

An existing test was modified to catch the regression, verified
to expose the problem and to solve it.

src/ert/run_models/multiple_data_assimilation.py
@@ -92,7 +92,7 @@ class MultipleDataAssimilation(UpdateRunModel, InitialEnsembleRunModel):
                     observations=prior.experiment.observations,
                     simulation_arguments=prior.experiment.metadata,
                     name=f"Restart from {prior.name}",
-                    templates=prior.experiment.templates_configuration,
+                    templates=self.ert_templates,
                 )
             except (KeyError, ValueError) as err:
                 raise ErtRunError(

tests/ert/ui_tests/gui/conftest.py
@@ -134,10 +134,18 @@ def _esmda_run(run_experiment, source_root, tmp_path_factory):
 
 
 def _ensemble_experiment_run(
-    run_experiment, source_root, tmp_path_factory, failing_reals
+    run_experiment,
+    source_root,
+    tmp_path_factory,
+    failing_reals: bool,
+    with_templates: bool,
 ):
     path = tmp_path_factory.mktemp("test-data")
     _new_poly_example(source_root, path)
+    if with_templates:
+        (path / "foo.tmpl").write_text("template contents", encoding="utf-8")
+        with (path / "poly.ert").open("a", encoding="utf-8") as f:
+            f.write("RUN_TEMPLATE foo.tmpl foo.txt")
     with (
         pytest.MonkeyPatch.context() as mp,
         _open_main_window(path / "poly.ert") as (
@@ -206,7 +214,13 @@ def ensemble_experiment_has_run(
     tmp_path, monkeypatch, run_experiment, source_root, tmp_path_factory
 ):
     yield from _ensemble_experiment_has_run(
-        tmp_path, monkeypatch, run_experiment, source_root, tmp_path_factory, True
+        tmp_path,
+        monkeypatch,
+        run_experiment,
+        source_root,
+        tmp_path_factory,
+        failing_reals=True,
+        with_templates=False,
     )
 
 
@@ -215,16 +229,28 @@ def ensemble_experiment_has_run_no_failure(
     tmp_path, monkeypatch, run_experiment, source_root, tmp_path_factory
 ):
     yield from _ensemble_experiment_has_run(
-        tmp_path, monkeypatch, run_experiment, source_root, tmp_path_factory, False
+        tmp_path,
+        monkeypatch,
+        run_experiment,
+        source_root,
+        tmp_path_factory,
+        failing_reals=False,
+        with_templates=True,
     )
 
 
 def _ensemble_experiment_has_run(
-    tmp_path, monkeypatch, run_experiment, source_root, tmp_path_factory, failing
+    tmp_path,
+    monkeypatch,
+    run_experiment,
+    source_root,
+    tmp_path_factory,
+    failing_reals,
+    with_templates,
 ):
     monkeypatch.chdir(tmp_path)
     test_files = _ensemble_experiment_run(
-        run_experiment, source_root, tmp_path_factory, failing
+        run_experiment, source_root, tmp_path_factory, failing_reals, with_templates
     )
     shutil.copytree(test_files, tmp_path, dirs_exist_ok=True)
     yield from open_gui_with_config(tmp_path / "poly.ert")