d0a09d2d80f167a1eb57ee67f6f9bb429f299938
Oddvar Lia (ST MSU GEO) <olia@equinor.com>
2025-12-04 11:09:29 +0100

    Add distance-based localization functions to field_utils
    This adds 3 basic helper functions for distance-based localization and the unit tests.
    - transform_local_ellipse_angle_to_local_coords to transform angles from global to local coordinate system
    - transform_positions_to_local_field_coordinates to transform positions of observations from global to local coordinate system
    - localization_scaling_function to calculate scaling factor to assign to the RHO matrix in distance-based localization
    - calc_rho_for_2d_grid_layer to calculate scaling values for each pair of observation position and field parameter position.
       It uses an ellipse shaped localization area around each observation point.
       The values is to be assigned to the RHO matrix for distance-based localization.

---
+++ tests/ert/unit_tests/field_utils/test_field_utils_for_distance_based_localization.py
@@ -0,0 +1,147 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+
+from ert.field_utils import (
+    calc_rho_for_2d_grid_layer,
+    localization_scaling_function,
+    transform_local_ellipse_angle_to_local_coords,
+    transform_positions_to_local_field_coordinates,
+)
+
+
+@pytest.mark.parametrize(
+    "nvalues",
+    [
+        10,
+        25,
+    ],
+)
+def test_localization_scaling_function(snapshot, nvalues):
+    distances = np.linspace(0, 2.5, num=nvalues, endpoint=True, dtype=np.float64)
+    scaling_values = localization_scaling_function(distances)
+    snapshot.assert_match(str(scaling_values) + "\n", "testdata_scaling_values.txt")
+
+
+@pytest.mark.parametrize(
+    "nx, ny,xinc,yinc,obs_xpos, obs_ypos,obs_main_range, "
+    "obs_perp_range,obs_angles,right_handed",
+    [
+        (
+            10,
+            10,
+            100.0,
+            100.0,
+            [0.0, 100.0, 250.0, 900.0],
+            [100.0, 200.0, 50.0, 500.0],
+            [200.0, 250.0, 500.0, 2000.0],
+            [100.0, 1250.0, 300.0, 2000.0],
+            [0.0, 45.0, -45.0, 120.0],
+            True,
+        ),
+    ],
+)
+def test_calc_rho_for_2d_grid_layer(
+    snapshot,
+    nx,
+    ny,
+    xinc,
+    yinc,
+    obs_xpos,
+    obs_ypos,
+    obs_main_range,
+    obs_perp_range,
+    obs_angles,
+    right_handed,
+):
+    xpos = np.array(obs_xpos)
+    ypos = np.array(obs_ypos)
+    main_range = np.array(obs_main_range)
+    perp_range = np.array(obs_perp_range)
+    angles = np.array(obs_angles)
+    rho_2d = calc_rho_for_2d_grid_layer(
+        nx,
+        ny,
+        xinc,
+        yinc,
+        xpos,
+        ypos,
+        main_range,
+        perp_range,
+        angles,
+        right_handed_grid_indexing=right_handed,
+    )
+    snapshot.assert_match(str(rho_2d) + "\n", "testdata_rho_2d.txt")
+
+
+@pytest.mark.parametrize(
+    "coordsys_rotation, ellipse_anisotropy_angle, expected",
+    [
+        (0.0, [45.0, -165.0], [45.0, -165.0]),
+        (120.0, [-135.0, 175.0], [-255.0, 55.0]),
+    ],
+)
+def test_transform_local_ellipse_angle_to_local_coords(
+    coordsys_rotation,
+    ellipse_anisotropy_angle,
+    expected,
+):
+    angle_inputs = np.array(ellipse_anisotropy_angle)
+    transf_angle = transform_local_ellipse_angle_to_local_coords(
+        coordsys_rotation, angle_inputs
+    )
+    assert_allclose(transf_angle, expected)
+
+
+@pytest.mark.parametrize(
+    "coordsys_origin, coordsys_rotation, utmx, utmy, expected_x, expected_y",
+    [
+        (
+            (0.0, 0.0),
+            0.0,
+            [1000.0, 100.0, -1000.0],
+            [0.0, 500.0, 250.0],
+            [1000.0, 100.0, -1000.0],
+            [0.0, 500.0, 250.0],
+        ),
+        (
+            (1000.0, 1500.0),
+            0.0,
+            [1000.0, 100.0, -1000.0],
+            [0.0, 500.0, 2100.0],
+            [0.0, -900.0, -2000.0],
+            [-1500.0, -1000.0, 600.0],
+        ),
+        (
+            (1000.0, 1500.0),
+            60.0,
+            [1000.0, 100.0, -1000.0],
+            [0.0, 500.0, 2100.0],
+            [-1299.03810568, -1316.02540378, -480.38475773],
+            [-750.0, 279.42286341, 2032.05080757],
+        ),
+        (
+            (1000.0, 1500.0),
+            -120.0,
+            [1000.0, 100.0, -1000.0],
+            [0.0, 500.0, 2100.0],
+            [1299.03810568, 1316.02540378, 480.38475773],
+            [750.0, -279.42286341, -2032.05080757],
+        ),
+    ],
+)
+def test_transform_positions_to_local_field_coordinates(
+    coordsys_origin,
+    coordsys_rotation,
+    utmx,
+    utmy,
+    expected_x,
+    expected_y,
+):
+    xpos = np.array(utmx)
+    ypos = np.array(utmy)
+    transf_x, transf_y = transform_positions_to_local_field_coordinates(
+        coordsys_origin, coordsys_rotation, xpos, ypos
+    )
+    assert_allclose(transf_x, expected_x)
+    assert_allclose(transf_y, expected_y)
