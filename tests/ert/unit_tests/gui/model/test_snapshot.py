d6e78fb98e563ff7cd361b0db56c6dfd379af8af
Eivind Jahren <ejah@equinor.com>
2025-12-12 13:28:31 +0100

    Downsize snapshot in model test

--- tests/ert/unit_tests/gui/model/test_snapshot.py
+++ tests/ert/unit_tests/gui/model/test_snapshot.py
@@ -1,17 +1,69 @@
+import copy
+from datetime import datetime as dt
+
 import pytest
 from PyQt6.QtCore import QModelIndex
 from PyQt6.QtGui import QColor
 from pytestqt.qt_compat import qt_api
 
-from ert.ensemble_evaluator.state import COLOR_FAILED
+from ert.ensemble_evaluator.snapshot import (
+    EnsembleSnapshot,
+    FMStepSnapshot,
+    RealizationSnapshot,
+)
+from ert.ensemble_evaluator.state import (
+    COLOR_FAILED,
+    FORWARD_MODEL_STATE_START,
+    REALIZATION_STATE_RUNNING,
+)
 from ert.gui.model.snapshot import FMStepColorHint, SnapshotModel
 
 from .gui_models_utils import finish_snapshot
 
 
+@pytest.fixture
+def small_snapshot() -> EnsembleSnapshot:
+    real = RealizationSnapshot(
+        status=REALIZATION_STATE_RUNNING,
+        active=True,
+        exec_hosts="COMP-01",
+        fm_steps={
+            "0": FMStepSnapshot(
+                start_time=dt.now(),
+                end_time=dt.now(),
+                name="poly_eval",
+                index="0",
+                status=FORWARD_MODEL_STATE_START,
+                error="error",
+                stdout="std_out_file",
+                stderr="std_err_file",
+                current_memory_usage=123,
+                max_memory_usage=312,
+            ),
+            "1": FMStepSnapshot(
+                start_time=dt.now(),
+                end_time=dt.now(),
+                name="poly_postval",
+                index="1",
+                status=FORWARD_MODEL_STATE_START,
+                error="error",
+                stdout="std_out_file",
+                stderr="std_err_file",
+                current_memory_usage=123,
+                max_memory_usage=312,
+            ),
+        },
+    )
+    snapshot = EnsembleSnapshot()
+    for i in range(10):
+        snapshot.add_realization(str(i), copy.deepcopy(real))
+
+    return snapshot
+
+
 @pytest.mark.integration_test
 @pytest.mark.skip_mac_ci  # slow
-def test_using_qt_model_tester(full_snapshot):
+def test_using_qt_model_tester(small_snapshot):
     model = SnapshotModel()
 
     reporting_mode = qt_api.QtTest.QAbstractItemModelTester.FailureReportingMode.Fatal
@@ -19,9 +71,9 @@ def test_using_qt_model_tester(full_snapshot):
         model, reporting_mode
     )
 
-    model._add_snapshot(SnapshotModel.prerender(full_snapshot), "0")
-    model._add_snapshot(SnapshotModel.prerender(full_snapshot), "1")
-    snapshot = finish_snapshot(SnapshotModel.prerender(full_snapshot))
+    model._add_snapshot(SnapshotModel.prerender(small_snapshot), "0")
+    model._add_snapshot(SnapshotModel.prerender(small_snapshot), "1")
+    snapshot = finish_snapshot(SnapshotModel.prerender(small_snapshot))
     model._update_snapshot(SnapshotModel.prerender(snapshot), "0")
     model._update_snapshot(SnapshotModel.prerender(snapshot), "1")
 
