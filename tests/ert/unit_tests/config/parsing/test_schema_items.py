e4f89c75a86cef578307322609d51a3de26b9433
Håvard Berland <havb@equinor.com>
2026-01-06 09:02:14 +0100

    Add test for argc_min in schema parsing
    (cherry picked from commit 5a9ea77163e21ab646d008740d30ef7b9a445f34)

--- tests/ert/unit_tests/config/parsing/test_schema_items.py
+++ tests/ert/unit_tests/config/parsing/test_schema_items.py
@@ -1,6 +1,14 @@
-from ert.config.parsing import parse_contents
+import pytest
+
+from ert.config.parsing import ConfigValidationError, parse_contents
 from ert.config.parsing.config_schema import ConfigSchemaDict
 from ert.config.parsing.config_schema_item import SchemaItem
+
+
+def test_that_schema_argc_min_one_raises_on_zero_arguments():
+    schema = ConfigSchemaDict(OPTIONS=SchemaItem(kw="OPTIONS", argc_min=1))
+    with pytest.raises(ConfigValidationError, match="must have at least 1 argument"):
+        parse_contents("OPTIONS", schema, "dummy_filename")
 
 
 def test_that_a_schema_item_can_contain_only_options():
