2255126001a8b1cca3c5acfc8842939f3744beb8
Håkon Steinkopf Søhoel <hsoho@equinor.com>
2026-01-16 17:19:10 +0100

    Fix wrong source package in forward models doc
    Some forward models wrongly stated ert as the source package due to
    ert being the default source_package value in the
    ForwardModelStepDocumentation class.
    With this commit the source package of the ForwardModelStepPlugin
    will be used as default value if it is not set explicitly.

--- tests/ert/unit_tests/docs/test_ert_documentation.py
+++ tests/ert/unit_tests/docs/test_ert_documentation.py
@@ -1,5 +1,8 @@
+import re
+
 import pytest
 
+from ert.plugins.plugin_manager import ErtPluginManager
 from ert.shared._doc_utils.ert_jobs import _ErtDocumentation
 
 
@@ -115,3 +118,23 @@ def test_divide_into_categories_job_source(test_input, expected_source_package):
     categories = _ErtDocumentation._divide_into_categories(test_input)
     result = [docs.job_source for docs in categories["other"]["other"]]
     assert expected_source_package == result
+
+
+@pytest.mark.parametrize("fm_step", ErtPluginManager().forward_model_steps)
+def test_that_forward_model_step_documentation_defaults_to_plugins_source_package(
+    fm_step,
+):
+    # The test assumes that repr(fm_step) returns a string on format:
+    # "<class 'fully.qualified.class.name'>"
+    match_qualified_class_name = re.match(r"\<class '(.*?)'>", repr(fm_step))
+    assert match_qualified_class_name, "Could not get qualified class name from fm_step"
+    qualified_class_name = match_qualified_class_name[1]
+
+    source_package = qualified_class_name.split(".")[0]
+    documentation = fm_step.documentation()
+
+    if documentation:
+        assert documentation.source_package == source_package, (
+            f"{qualified_class_name} documentation reports source_package="
+            f"{documentation.source_package}, expected {source_package}"
+        )
