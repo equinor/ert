6a0242ca3f05379ce2ef7d93c2d7fd32f60fc965
eq-sandkav <skav@equinor.com>
2026-01-26 15:55:59 +0100

    Avoid dropping localization primary keys
    RFT observations have "north" and "east" as primary keys. Dropping
    these before merging dataframes causes Ert to crash.
    
    This will prevent dropping keys which overlap localization keys.
    
    (cherry picked from commit 7fa0935833f885d3842b94bad81197f830d18000)

--- src/ert/storage/local_ensemble.py
+++ src/ert/storage/local_ensemble.py
@@ -1099,6 +1099,11 @@ class LocalEnsemble(BaseMode):
                             on=["response_key", *response_cls.primary_key],
                         )
 
+                    # Do not drop primary keys which
+                    # overlap with localization attributes
+                    primary_keys_to_drop = set(response_cls.primary_key).difference(
+                        {"north", "east", "radius"}
+                    )
                     joined = (
                         joined.with_columns(
                             pl.concat_str(
@@ -1108,7 +1113,7 @@ class LocalEnsemble(BaseMode):
                                 # Avoid potential collisions w/ primary key
                             )
                         )
-                        .drop(response_cls.primary_key)
+                        .drop(primary_keys_to_drop)
                         .rename({"__tmp_index_key__": "index"})
                     )
 
