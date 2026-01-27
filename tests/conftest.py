c4c4f520cbffb90241ffb00484a12face47e1186
Eivind Jahren <ejah@equinor.com>
2026-01-27 13:07:33 +0100

    Remove --run-slow pytest option

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,6 @@ from ert.plugins import ErtRuntimePlugins
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-    parser.addoption(
         "--eclipse-simulator",
         action="store_true",
         default=False,
@@ -70,20 +67,6 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if "skip_mac_ci" in item.keywords:
                 item.add_marker(pytest.mark.skip(reason="Skipped on mac ci"))
-
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        skip_quick = pytest.mark.skip(
-            reason="skipping quick performance tests on --runslow"
-        )
-        for item in items:
-            if "quick_only" in item.keywords:
-                item.add_marker(skip_quick)
-    else:
-        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-        for item in items:
-            if "slow" in item.keywords:
-                item.add_marker(skip_slow)
 
 
 @pytest.fixture(autouse=True)
