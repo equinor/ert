3544280738a17e7b3ebbfd739cd76bf0b3f50f79
Eivind Jahren <ejah@equinor.com>
2026-02-05 16:14:33 +0100

    Use unique dates for lookup in refcase test
    After changing to migrating observation configs for users a
    corner case was introduced where it may be ambiguous which
    report step was ment as we round to seconds.
    
    This changes the test to only apply to refcases with unique
    dates. There is still the question of whether it is good enough
    for the migration to work when the refcase has sufficiently
    distinct report step times.

--- tests/ert/unit_tests/config/test_observations.py
+++ tests/ert/unit_tests/config/test_observations.py
@@ -1,6 +1,6 @@
 import logging
 from contextlib import ExitStack as does_not_raise
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from pathlib import Path
 from textwrap import dedent
 from typing import cast
@@ -473,14 +473,29 @@ def test_that_the_date_keyword_sets_the_general_index_by_looking_up_time_map():
     assert observations["gen_data"].to_dicts()[0]["report_step"] == restart
 
 
-@given(summary=summaries(), data=st.data())
+@given(data=st.data())
 @pytest.mark.integration_test
 def test_that_the_date_keyword_sets_the_report_step_by_looking_up_refcase(
-    tmp_path_factory: pytest.TempPathFactory, summary, data
+    tmp_path_factory: pytest.TempPathFactory, data
 ):
+    start_date, *times = sorted(
+        data.draw(
+            st.lists(
+                st.dates(min_value=date(1969, 1, 1), max_value=date(2100, 1, 1)),
+                min_size=2,
+                unique=True,
+            )
+        )
+    )
+    times = [(t - start_date).total_seconds() / 3600 for t in times]
+    smspec, unsmry = data.draw(
+        summaries(
+            start_date=st.just(datetime.combine(start_date, datetime.min.time())),
+            time_deltas=st.just(times),
+        )
+    )
     with pytest.MonkeyPatch.context() as patch:
         patch.chdir(tmp_path_factory.mktemp("history_observation_values_are_fetched"))
-        smspec, unsmry = summary
         smspec.to_file("ECLIPSE_CASE.SMSPEC")
         unsmry.to_file("ECLIPSE_CASE.UNSMRY")
         start_date = smspec.start_date.to_datetime()
