f45e6faaa415667dd44c87c656f186932631bbe8
Julius Parulek <parulek@gmail.com>
2026-02-07 20:42:03 +0100

    Updates new tests

--- tests/ert/ui_tests/cli/analysis/test_distance_localization.py
+++ tests/ert/ui_tests/cli/analysis/test_distance_localization.py
@@ -1,31 +1,45 @@
 from pathlib import Path
-from textwrap import dedent
 
-import numpy as np
 import pytest
 
 from ert.config import ErtConfig
-from ert.mode_definitions import ENSEMBLE_SMOOTHER_MODE, ES_MDA_MODE
+from ert.mode_definitions import ENSEMBLE_SMOOTHER_MODE
 from ert.storage import open_storage
 from tests.ert.ui_tests.cli.run_cli import run_cli
+
+
+def assert_variance_in_field(
+    prior_ensemble, posterior_ensemble, field_name, obs_pos, no_upd_pos
+):
+    prior_data = prior_ensemble.load_parameters(field_name)
+    posterior_data = posterior_ensemble.load_parameters(field_name)
+
+    prior_var = prior_data.var(dim="realizations", ddof=1).sel(
+        x=obs_pos[0], y=obs_pos[1]
+    )
+    posterior_var = posterior_data.var(dim="realizations", ddof=1).sel(
+        x=obs_pos[0], y=obs_pos[1]
+    )
+
+    assert prior_var.mean() >= posterior_var.mean(), (
+        f"Expected variance reduction at position {obs_pos} in field {field_name}, "
+    )
 
 
 @pytest.mark.timeout(600)
 @pytest.mark.usefixtures("copy_snake_oil_field")
 @pytest.mark.integration_test
 def test_that_distance_localization_works_with_a_single_observation():
-    set_distance_localization = dedent(
-        """
-        ANALYSIS_SET_VAR STD_ENKF DISTANCE_LOCALIZATION True
-        """
-    )
-
     with open("snake_oil_field.ert", "r+", encoding="utf-8") as f:
         lines = f.readlines()
-        lines.insert(9, set_distance_localization)
 
-    with open("snake_oil_field_dl.ert", "w", encoding="utf-8") as f:
-        f.writelines(lines)
+    config_content = [
+        line for line in lines if not line.lstrip().startswith("OBS_CONFIG")
+    ]
+    config_content.append("ANALYSIS_SET_VAR STD_ENKF DISTANCE_LOCALIZATION True\n")
+
+    with open("snake_oil_field.ert", "w", encoding="utf-8") as f:
+        f.writelines(config_content)
 
     content = """
     SUMMARY_OBSERVATION WOPR_OP1_36
@@ -56,27 +70,22 @@ def test_that_distance_localization_works_with_a_single_observation():
 
     Path("observations/observations.txt").write_text(content, encoding="utf-8")
 
-    run_cli(ENSEMBLE_SMOOTHER_MODE, "--disable-monitoring", "snake_oil_field_dl.ert")
+    run_cli(
+        ENSEMBLE_SMOOTHER_MODE,
+        "--disable-monitoring",
+        "snake_oil_field_dl.ert",
+        "--experiment-name",
+        "dl",
+    )
 
     ert_config = ErtConfig.from_file("snake_oil_field_dl.ert")
     assert ert_config.analysis_config.es_settings.distance_localization is True
     storage = open_storage(ert_config.ens_path)
-    experiment = storage.get_experiment_by_name("es")
+    experiment = storage.get_experiment_by_name("dl")
     ens_prior = experiment.get_ensemble_by_name("iter-0")
     ens_posterior = experiment.get_ensemble_by_name("iter-1")
 
-    poro_prior = ens_prior.load_parameters("PORO")
-    poro_posterior = ens_posterior.load_parameters("PORO")
-    prior_var = poro_prior.var(dim="realizations", ddof=1)["values"]
-    post_var = poro_posterior.var(dim="realizations", ddof=1)["values"]
-
-    sel_post = post_var.sel(x=5, y=5)
-    sel_prior = prior_var.sel(x=5, y=5)
-
-    diff = sel_prior - sel_post
-    assert bool((diff > 0).all()), (
-        f"Expected variance reduction, got min(prior-post)={diff.min().item()}"
-    )
+    assert_variance_in_field(ens_prior, ens_posterior, "PORO", (5, 5), (9, 9))
 
 
 @pytest.mark.timeout(600)
@@ -86,51 +95,25 @@ def test_that_distance_localization_runs_on_heat_equation():
     with open("config.ert", encoding="utf-8") as fh:
         lines = fh.readlines()
 
-    filtered_lines = [
+    config_content = [
         line for line in lines if not line.lstrip().startswith("ANALYSIS_SET_VAR")
     ]
-    filtered_lines.append(
-        dedent(
-            """
-            ANALYSIS_SET_VAR STD_ENKF DISTANCE_LOCALIZATION True\n
-            """
-        )
-    )
+    config_content.append("ANALYSIS_SET_VAR STD_ENKF DISTANCE_LOCALIZATION True\n")
     with open("config.ert", "w", encoding="utf-8") as fh:
-        fh.writelines(filtered_lines)
+        fh.writelines(config_content)
 
     run_cli(
-        ES_MDA_MODE,
+        ENSEMBLE_SMOOTHER_MODE,
         "--disable-monitoring",
         "config.ert",
         "--experiment-name",
-        "heat-dl",
+        "dl",
     )
 
     config = ErtConfig.from_file("config.ert")
+    assert config.analysis_config.es_settings.distance_localization is True
     with open_storage(config.ens_path) as storage:
-        experiment = storage.get_experiment_by_name("heat-dl")
-        prior = experiment.get_ensemble_by_name("default_0")
-        posterior = experiment.get_ensemble_by_name("default_3")
-
-        param_config = config.ensemble_config.parameter_configs["COND"]
-
-        prior_result = prior.load_parameters("COND")["values"]
-        prior_covariance = np.cov(
-            prior_result.values.reshape(
-                prior.ensemble_size, param_config.nx * param_config.ny * param_config.nz
-            ),
-            rowvar=False,
-        )
-
-        posterior_result = posterior.load_parameters("COND")["values"]
-        posterior_covariance = np.cov(
-            posterior_result.values.reshape(
-                posterior.ensemble_size,
-                param_config.nx * param_config.ny * param_config.nz,
-            ),
-            rowvar=False,
-        )
-
-        # Check that generalized variance is reduced by update step.
-        assert np.trace(prior_covariance) > np.trace(posterior_covariance)
+        experiment = storage.get_experiment_by_name("dl")
+        prior = experiment.get_ensemble_by_name("iter-0")
+        posterior = experiment.get_ensemble_by_name("iter-1")
+        assert_variance_in_field(prior, posterior, "COND", (2, 2), (9, 9))
