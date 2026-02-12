0aadde0f30f60939e3a46cca7589e88fb650e39e
Alena Chaikouskaya <alec@equinor.com>
2026-02-12 19:50:40 +0100

    Limit ensembles eligible for es-mda restart
    Restart can be performed
    - on ES-MDA ensembles that were not leafs in the current ES-MDA run
    (intended use)
    - on Ensemble Experiment entries (we are cheating to allow users to
    spare computational power if they decide to run ES-MDA on the ensemble)
    
    Old ensembles in existing storage are partly affected:
    - ES-MDA ensembles are guessed via present relative-weights attributes,
    so they should show up.
    - Ensemble experiments won't show up though as these require experiment
    type to be set.

--- tests/ert/unit_tests/gui/simulation/test_multiple_data_assimilation_panel.py
+++ tests/ert/unit_tests/gui/simulation/test_multiple_data_assimilation_panel.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 from unittest.mock import MagicMock
 
+import pytest
 from PyQt6.QtWidgets import QCheckBox
 from pytestqt.qtbot import QtBot
 
@@ -10,6 +12,7 @@ from ert.gui.ertwidgets import EnsembleSelector, StringBox
 from ert.gui.simulation.multiple_data_assimilation_panel import (
     MultipleDataAssimilationPanel,
 )
+from ert.storage.local_experiment import ExperimentType
 
 from .conftest import (
     REALIZATION_FINISHED_SUCCESSFULLY,
@@ -74,6 +77,8 @@ def test_that_active_realizations_selector_validates_with_with_realizations_from
             REALIZATION_UNDEFINED,
             REALIZATION_FINISHED_SUCCESSFULLY,
         ],
+        experiment_type=ExperimentType.ES_MDA,
+        iteration=0,
     )
     panel = MultipleDataAssimilationPanel(
         analysis_config=AnalysisConfig(minimum_required_realizations=1),
@@ -128,3 +133,101 @@ def test_multiple_data_assimilation_panel_sets_active_realizations_to_initial_ac
     assert mda_panel._active_realizations_field.text() == active_realizations_string
     mda_panel.restart_run_toggled()
     assert mda_panel._active_realizations_field.text() == active_realizations_string
+
+
+@dataclass(frozen=True)
+class EnsInfo:
+    ensemble_id: str
+    experiment_id: str
+    parent: str | None
+    experiment_type: ExperimentType
+    iteration: int = 0
+
+    @property
+    def ensemble(self) -> str:
+        return self.ensemble_id + "_ens"
+
+    @property
+    def experiment(self) -> str:
+        return self.experiment_id + "_exp"
+
+
+@pytest.mark.parametrize(
+    ("extra_ensembles", "expected_ensembles"),
+    [
+        pytest.param(
+            [
+                EnsInfo("a", "mda", None, ExperimentType.ES_MDA, 0),
+                EnsInfo("b", "mda", "a_ens", ExperimentType.ES_MDA, 1),
+                EnsInfo("c", "mda", "b_ens", ExperimentType.ES_MDA, 2),
+                EnsInfo("leaf", "mda", "c_ens", ExperimentType.ES_MDA, 3),
+                EnsInfo("faux_child1", "e1", "c_ens", ExperimentType.MANUAL_UPDATE),
+                EnsInfo("faux_child2", "e2", "leaf_ens", ExperimentType.MANUAL_UPDATE),
+            ],
+            [
+                "mda_exp : a_ens",
+                "mda_exp : b_ens",
+                "mda_exp : c_ens",
+            ],
+            id="ES-MDA which are not leafs in current ES_MDA run",
+        ),
+        pytest.param(
+            [
+                EnsInfo("ensemble", "experiment", None, ExperimentType.ENSEMBLE),
+                EnsInfo("a", "mda", None, ExperimentType.UNDEFINED, 0),
+                EnsInfo("b", "mda", "a_ens", ExperimentType.UNDEFINED, 1),
+            ],
+            [
+                "experiment_exp : ensemble_ens",
+                "mda_exp : a_ens",
+                "mda_exp : b_ens",
+            ],
+            id="both ensemble runs and old-storage ES_MDA non-leafs",
+        ),
+    ],
+)
+def test_that_restart_ensemble_select_contains(
+    qtbot: QtBot, extra_ensembles, expected_ensembles
+) -> None:
+    config_num_realizations = 5
+    active_realizations = [True] * config_num_realizations
+    notifier = ErtNotifier()
+    notifier._storage = MockStorage()
+    storage = notifier._storage
+    ensembles_that_should_not_show_up = [
+        EnsInfo("test", "test", None, ExperimentType.TEST),
+        EnsInfo("manual", "manual", None, ExperimentType.MANUAL),
+        EnsInfo("es0", "es", None, ExperimentType.ES, 0),
+        EnsInfo("es1", "es", "es0_ens", ExperimentType.ES, 1),
+    ]
+    ensembles = ensembles_that_should_not_show_up + extra_ensembles
+    for ensemble_info in ensembles:
+        storage._setup_mocked_run(
+            ensemble_info.ensemble,
+            ensemble_info.experiment,
+            [
+                REALIZATION_FINISHED_SUCCESSFULLY,
+            ],
+            experiment_type=ensemble_info.experiment_type,
+            iteration=ensemble_info.iteration,
+        )
+        if ensemble_info.parent is not None:
+            keys = list(storage._ensembles.keys())
+            storage._ensembles[keys[-1]]._index.prior_ensemble_id = next(
+                k for k in keys if storage._ensembles[k].name == ensemble_info.parent
+            )
+
+    panel = MultipleDataAssimilationPanel(
+        analysis_config=AnalysisConfig(minimum_required_realizations=1),
+        parameter_configuration=EnsembleConfig().parameter_configuration,
+        run_path="",
+        notifier=notifier,
+        active_realizations=active_realizations,
+        config_num_realization=config_num_realizations,
+    )
+
+    qtbot.addWidget(panel)
+    ensemble_selector = panel.findChild(EnsembleSelector)
+    assert ensemble_selector.count() == len(expected_ensembles)
+    for i in range(ensemble_selector.count()):
+        assert ensemble_selector.itemText(i) in expected_ensembles
