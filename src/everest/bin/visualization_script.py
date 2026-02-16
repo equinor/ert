978db1187c36ded7dd3246e2bf32b91b0a5e4740
Håvard Berland <havb@equinor.com>
2026-02-16 16:58:58 +0100

    Add feature flag --ert_plotter to everest results
    This will launch a work-in-progress Qt GUI to eventually
    replace everviz.

--- src/everest/bin/visualization_script.py
+++ src/everest/bin/visualization_script.py
@@ -2,9 +2,17 @@ import argparse
 import logging
 import sys
 from functools import partial
+from importlib.resources import files
 from pathlib import Path
+from signal import SIG_DFL, SIGINT, signal
 from textwrap import dedent
 
+from PyQt6.QtCore import QDir
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication
+
+from ert.gui.tools.plot.plot_window import PlotWindow
+from ert.services import ErtServerController
 from ert.storage import LocalStorage
 from everest.api import EverestDataAPI
 from everest.bin.utils import setup_logging
@@ -39,6 +47,11 @@ def _build_args_parser() -> argparse.ArgumentParser:
         type=partial(EverestConfig.load_file_with_argparser, parser=arg_parser),
         help="The path to the everest configuration file.",
     )
+    arg_parser.add_argument(
+        "--ert_plotter",
+        action="store_true",
+        help="Use the Ert plotter instead of everviz (EXPERIMENTAL)",
+    )
     return arg_parser
 
 
@@ -47,7 +60,9 @@ def visualization_entry(args: list[str] | None = None) -> None:
     options = parser.parse_args(args)
     logger = logging.getLogger(__name__)
     with setup_logging(options):
-        logger.info(f"Starting everviz entrypoint with args {args} in {Path.cwd()}")
+        logger.info(
+            f"Starting everest results entrypoint with args {args} in {Path.cwd()}"
+        )
         ever_config = options.config
         EverestStorage.check_for_deprecated_seba_storage(
             ever_config.optimization_output_dir
@@ -68,8 +83,39 @@ def visualization_entry(args: list[str] | None = None) -> None:
             )
             return
 
-        pm = EverestPluginManager()
-        pm.hook.visualize_data(api=EverestDataAPI(options.config))
+        if not options.ert_plotter:
+            pm = EverestPluginManager()
+            pm.hook.visualize_data(api=EverestDataAPI(options.config))
+        else:
+            with ErtServerController.init_service(
+                timeout=240,
+                project=Path(ever_config.storage_dir),
+            ):
+                run_plotter_gui(options.config.config_path, ever_config.storage_dir)
+
+
+def run_plotter_gui(config_filename: str, storage_dir: Path) -> None:
+    # Replace Python's exception handler for SIGINT with the system default.
+    #
+    # Python's SIGINT handler is the one that raises KeyboardInterrupt. This is
+    # okay normally (if a bit ugly), but when control is given to Qt this
+    # exception handler will either get deadlocked because Python never gets
+    # control back, or gets eaten by Qt because it ignores exceptions that
+    # happen in Qt slots.
+    signal(SIGINT, SIG_DFL)
+
+    QDir.addSearchPath(
+        "img", str(files("ert.gui").joinpath("../../ert/gui/resources/gui/img"))
+    )
+
+    app = QApplication(["everest"])
+    app.setWindowIcon(QIcon("img:ert_icon.svg"))
+    window = PlotWindow(f"{config_filename}", storage_dir, parent=None)
+    window.adjustSize()
+    window.show()
+    window.activateWindow()
+    window.raise_()
+    app.exec()
 
 
 if __name__ == "__main__":
