1c79434f4ff8fd6cc6f87d150575ef6692e40f4c
xjules <jparu@equinor.com>
2026-03-02 08:53:19 +0100

    update everest plot

--- src/ert/gui/tools/plot/plottery/plots/everest_gradients_plot.py
+++ src/ert/gui/tools/plot/plottery/plots/everest_gradients_plot.py
@@ -30,6 +30,7 @@ class EverestGradientsPlot:
         ensemble_to_data_map: dict[EnsembleObject, pd.DataFrame],
         observation_data: pd.DataFrame,
         std_dev_images: dict[str, npt.NDArray[np.float32]],
+        obs_loc: dict[str, npt.NDArray[np.float32]],
         key_def: PlotApiKeyDefinition | None = None,
     ) -> None:
         if not self.selected_controls:
