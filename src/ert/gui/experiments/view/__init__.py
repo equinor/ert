5d281c54d21b69baab4d7598d3a589ee5b2253af
Jonathan Karlsen <JONAK@equinor.com>
2026-03-04 15:45:00 +0100

    Have _create_run_path progress show in GUI
    This commit makes _create_run_path emit RunPathCreationEvents
    when starting to create runpaths, one event for every runpath created,
    and another event when all runpaths are created. This is forwarded to
    the run_model, and in the end shown in the RunDialog.

--- src/ert/gui/experiments/view/__init__.py
+++ src/ert/gui/experiments/view/__init__.py
@@ -1,6 +1,13 @@
 from .disk_space_widget import DiskSpaceWidget
 from .progress_widget import ProgressWidget
 from .realization import RealizationWidget
+from .runpath_creation_widget import RunpathCreationProgressWidget
 from .update import UpdateWidget
 
-__all__ = ["DiskSpaceWidget", "ProgressWidget", "RealizationWidget", "UpdateWidget"]
+__all__ = [
+    "DiskSpaceWidget",
+    "ProgressWidget",
+    "RealizationWidget",
+    "RunpathCreationProgressWidget",
+    "UpdateWidget",
+]
