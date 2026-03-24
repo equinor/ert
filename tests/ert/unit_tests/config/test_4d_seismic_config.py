b3b4f620d6dbe1b761d5987b2b91addd90e7184d
eq-sandkav <skav@equinor.com>
2026-03-24 10:11:58 +0100

    Add seismic 4d config keyword

--- tests/ert/unit_tests/config/test_4d_seismic_config.py
+++ tests/ert/unit_tests/config/test_4d_seismic_config.py
@@ -1,7 +1,52 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+from surfio import IrapHeader, IrapSurface
+
 from ert.config import ErtConfig
+from ert.config.parsing import ConfigDict
 
 
 def test_that_4d_seismic_attributes_can_be_parsed(use_tmpdir):
+    ErtConfig.from_file_contents(
+        """
+        NUM_REALIZATIONS 1
+
+        SEISMIC_4D_ATTRIBUTE START:2020-01-01 END:2020-02-01 FILE:file
+        """,
+    )
+
+
+@pytest.fixture
+def surface():
+    return IrapSurface(
+        IrapHeader(
+            ncol=3,
+            nrow=2,
+            xori=0.0,
+            yori=0.0,
+            xinc=2.0,
+            yinc=2.0,
+            xmax=2.0,
+            ymax=2.0,
+            rot=0.0,
+            xrot=0.0,
+            yrot=0.0,
+        ),
+        values=np.zeros((3, 2)),
+    )
+
+
+def test_that_4d_seismic_attribute_is_added_to_ert_config_dict(
+    use_tmpdir, surface, monkeypatch
+):
+    def from_dict_assertion_mock(config_dict: ConfigDict):
+        assert "SEISMIC_4D_ATTRIBUTE" in config_dict
+
+    monkeypatch.setattr(ErtConfig, "from_dict", from_dict_assertion_mock)
+
+    surface.to_binary_file(Path("file"))
     ErtConfig.from_file_contents(
         """
         NUM_REALIZATIONS 1
