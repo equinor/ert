a67f5965329d67ca847195779bbf57f6d8ecfce1
Håvard Berland <havb@equinor.com>
2026-03-25 10:26:27 +0100

    Inline load_coeffs
    This is too trivial

--- test-data/ert/poly_example/poly_eval.py
+++ test-data/ert/poly_example/poly_eval.py
@@ -3,16 +3,11 @@ import json
 from pathlib import Path
 
 
-def _load_coeffs(filename):
-    with Path(filename).open(encoding="utf-8") as f:
-        return json.load(f)
-
-
 def _evaluate(coeffs, x):
     return coeffs["a"]["value"] * x**2 + coeffs["b"]["value"] * x + coeffs["c"]["value"]
 
 
 if __name__ == "__main__":
-    coeffs = _load_coeffs("parameters.json")
+    coeffs = json.loads(Path("parameters.json").read_text(encoding="utf-8"))
     output = [_evaluate(coeffs, x) for x in range(10)]
     Path("poly.out").write_text("\n".join(map(str, output)), encoding="utf-8")
