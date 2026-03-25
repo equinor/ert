26c7cd1be4110849f3a3e7abc50be5d94dfb0397
Håvard Berland <havb@equinor.com>
2026-03-25 10:40:51 +0100

    Rename x to t
    This makes it more natural to use poly_eval as something
    that is time-dependent and applicable to prediction settings.
    
    Considered going all the way to call it timestep, but that is going
    too far as the isolated poly-case is only doing polynomial fitting
    which is a tad weird in time-space.

--- test-data/ert/poly_example/poly_eval.py
+++ test-data/ert/poly_example/poly_eval.py
@@ -3,15 +3,15 @@ import json
 from pathlib import Path
 
 
-def _evaluate(parameters: dict[str, dict[str, float]], x: float) -> float:
+def _evaluate(parameters: dict[str, dict[str, float]], t: float) -> float:
     return (
-        parameters["a"]["value"] * x**2
-        + parameters["b"]["value"] * x
+        parameters["a"]["value"] * t**2
+        + parameters["b"]["value"] * t
         + parameters["c"]["value"]
     )
 
 
 if __name__ == "__main__":
     parameters = json.loads(Path("parameters.json").read_text(encoding="utf-8"))
-    output = [_evaluate(parameters, x) for x in range(10)]
+    output = [_evaluate(parameters, t) for t in range(10)]
     Path("poly.out").write_text("\n".join(map(str, output)), encoding="utf-8")
