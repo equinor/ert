2beca73e1e4b42ac9367cbc3b3e7e5889ba69915
Erlend Helland <erlend.helland@gmail.com>
2026-04-17 10:26:06 +0200

    Add creation timestamp to manage experiment storage info widget

--- src/ert/gui/tools/manage_experiments/storage_info_widget.py
+++ src/ert/gui/tools/manage_experiments/storage_info_widget.py
@@ -76,10 +76,12 @@ class _ExperimentWidget(QWidget):
         info_frame = QFrame()
         self._name_label = QLabel()
         self._uuid_label = QLabel()
+        self._created_at_label = QLabel()
 
         layout = QVBoxLayout()
         layout.addWidget(self._name_label)
         layout.addWidget(self._uuid_label)
+        layout.addWidget(self._created_at_label)
         layout.addStretch()
 
         info_frame.setLayout(layout)
@@ -113,6 +115,14 @@ class _ExperimentWidget(QWidget):
 
         self._name_label.setText(f"Name: {experiment.name!s}")
         self._uuid_label.setText(f"UUID: {experiment.id!s}")
+
+        # Determine creation time from the earliest ensemble start time
+        created_text = "Unknown"
+        ensemble_start_times = [ens.started_at for ens in experiment.ensembles]
+        if ensemble_start_times:
+            created_text = min(ensemble_start_times).strftime("%Y-%m-%d %H:%M:%S")
+
+        self._created_at_label.setText(f"Created at: {created_text}")
 
         self._responses_text_edit.setText(yaml.dump(experiment.response_info, indent=4))
         self._parameters_text_edit.setText(
