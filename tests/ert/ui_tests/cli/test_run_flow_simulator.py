a14488ae14cb25de294ea9f9ca8d5d3bba240128
Håvard Berland <havb@equinor.com>
2026-04-22 12:50:46 +0200

    Remove skipped tests that are now irrelevant
    Following 7c245db92aeeb39a3358e61e5f9beefdb650eb4e supplying these options
    are not allowed.

--- tests/ert/ui_tests/cli/test_run_flow_simulator.py
+++ tests/ert/ui_tests/cli/test_run_flow_simulator.py
@@ -40,50 +40,6 @@ def test_numcpu_maps_to_mpi_processes_with_flow(num_cpu):
 
 
 @pytest.mark.usefixtures("eightcells")
-@pytest.mark.xfail(reason="flowrc43 does not any longer respect the --threads option")
-@pytest.mark.skipif(not shutil.which("flowrun"), reason="flowrun not available")
-def test_user_can_specify_threads_and_oversubscribe_compute_node():
-    Path("flow.ert").write_text(
-        dedent("""
-    NUM_REALIZATIONS 1
-    ECLBASE EIGHTCELLS
-    DATA_FILE EIGHTCELLS.DATA
-    RUNPATH realization-<IENS>
-    NUM_CPU 1
-    FORWARD_MODEL FLOW(<OPTS>="--threads 2")  -- OPTS is parsed by flowrun
-    """).strip(),
-        encoding="utf-8",
-    )
-
-    run_cli(TEST_RUN_MODE, "--disable-monitoring", "flow.ert")
-    flow_stdout = Path("realization-0/FLOW.stdout.0").read_text(encoding="utf-8")
-    assert re.search(r"Number of MPI processes:\s+1", flow_stdout)
-    assert re.search(r"Threads per MPI process:\s+2", flow_stdout)
-
-
-@pytest.mark.usefixtures("eightcells")
-@pytest.mark.xfail(reason="flowrc43 does not any longer respect the --threads option")
-@pytest.mark.skipif(not shutil.which("flowrun"), reason="flowrun not available")
-def test_user_can_specify_threads_and_mpi_processes_and_oversubscribe_compute_node():
-    Path("flow.ert").write_text(
-        dedent("""
-    NUM_REALIZATIONS 1
-    ECLBASE EIGHTCELLS
-    DATA_FILE EIGHTCELLS.DATA
-    RUNPATH realization-<IENS>
-    NUM_CPU 1
-    FORWARD_MODEL FLOW(<OPTS>="--np 2 --threads 2")  -- OPTS is parsed by flowrun
-    """).strip(),
-        encoding="utf-8",
-    )
-
-    run_cli(TEST_RUN_MODE, "--disable-monitoring", "flow.ert")
-    flow_stdout = Path("realization-0/FLOW.stdout.0").read_text(encoding="utf-8")
-    assert re.search(r"Number of MPI processes:\s+2", flow_stdout)
-    assert re.search(r"Threads per MPI process:\s+2", flow_stdout)
-
-
-@pytest.mark.usefixtures("eightcells")
 @pytest.mark.skipif(not shutil.which("flowrun"), reason="flowrun not available")
 def test_ert_will_fetch_parallel_keyword_and_set_mpi_processes():
     deck = Path("EIGHTCELLS.DATA").read_text(encoding="utf-8")
