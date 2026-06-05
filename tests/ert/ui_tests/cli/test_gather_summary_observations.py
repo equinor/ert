ea9f05f7cf016faf3a93d8bb8e5b2214eaa56774
eq-sandkav <skav@equinor.com>
2026-06-05 15:43:35 +0200

    Sort observations by time before natsort
    This sorts the observations in two steps, first by time then by
    natsorting the summary key. It looks arguably more readable doing
    it in two operations than in a onelines.
    
    Along with this change has the happy path UI test been extended with
    asserting that the old observation config results in the same
    observations as the new one, apart from the defaulted observation
    names.

--- tests/ert/ui_tests/cli/test_gather_summary_observations.py
+++ tests/ert/ui_tests/cli/test_gather_summary_observations.py
@@ -1,3 +1,4 @@
+import shutil
 import subprocess
 from argparse import Namespace
 from pathlib import Path
@@ -5,6 +6,7 @@ from subprocess import CalledProcessError
 
 import pytest
 
+from ert.config import ErtConfig
 from ert.gather_summary_observations import main
 
 
@@ -33,9 +35,16 @@ def test_that_cli_command_without_feature_flag_raises_called_process_error() -> 
 )
 @pytest.mark.skip_mac_ci  # Ert api is too slow to start for mac tests
 def test_that_happy_path_on_snake_oil_produces_csv_and_stdout(capsys):
+    """This tests that the produced stdout and csv file from the
+    gather_summary_observations command is what we expect.
+    Finally, the test also creates a ErtConfig from the initial observation config
+    and compares it to the new one when replacing the summary observations with the
+    stdout and moving the csv file into the observations folder.
+    """
     config_path = "test_data/snake_oil.ert"
     storage_path = "test_data/storage/"
     experiment_path = "snake_oil/ensemble/experiments/"
+    observation_path = "test_data/observations/observations.txt"
     experiment = next(iter(Path(storage_path + experiment_path).iterdir()))
 
     args = Namespace(
@@ -58,4 +67,45 @@ def test_that_happy_path_on_snake_oil_produces_csv_and_stdout(capsys):
     assert all(line in csv_content for line in expected_csv_content)
 
     expected_stdout = "SUMMARY {\n  VALUES = summary_observations.csv;\n};\n"
-    assert expected_stdout in capsys.readouterr().out
+    stdout = capsys.readouterr().out
+    assert expected_stdout in stdout
+
+    old_obs_config = Path(observation_path).read_text(encoding="utf-8")
+    old_obs_config_lines = old_obs_config.split("\n")
+    # Assumes General Observation at bottom of file
+    summary_stop_line = next(
+        i
+        for i, line in enumerate(old_obs_config_lines)
+        if "GENERAL_OBSERVATION" in line
+    )
+    non_bulk_observations = old_obs_config_lines[summary_stop_line:]
+
+    stdout_lines = stdout.split("\n")
+    bulk_start_line = next(
+        i for i, line in enumerate(stdout_lines) if "SUMMARY {" in line
+    )
+    extracted_bulk_lines = stdout_lines[bulk_start_line:]
+
+    new_obs_content = "\n".join([*extracted_bulk_lines, *non_bulk_observations])
+
+    old_ert_config = ErtConfig.from_file("test_data/snake_oil.ert")
+
+    Path(observation_path).write_text(new_obs_content, encoding="utf-8")
+    shutil.move(
+        "summary_observations.csv", "test_data/observations/summary_observations.csv"
+    )
+    new_ert_config = ErtConfig.from_file("test_data/snake_oil.ert")
+
+    assert len(new_ert_config.observation_declarations) == len(
+        old_ert_config.observation_declarations
+    )
+    # Loop through observations and assert that they are the same except name,
+    # which also indirectly asserts the ordering is the same.
+    for i in range(len(new_ert_config.observation_declarations)):
+        old_obs = old_ert_config.observation_declarations[i].__dict__
+        new_obs = new_ert_config.observation_declarations[i].__dict__
+        # Bulk summary config must utilize default naming for observations,
+        # so these will differ between the observation declarations.
+        old_obs.pop("name")
+        new_obs.pop("name")
+        assert old_obs == new_obs
