560fc92d49d1afa8106818e3cb0b329963a265f5
Eilert Skram <eskra@equinor.com>
2026-06-09 14:39:16 +0200

    Add None-checks to set_pinned_control

--- tests/ert/unit_tests/gui/ertwidgets/test_everest_control_selection_widget.py
+++ tests/ert/unit_tests/gui/ertwidgets/test_everest_control_selection_widget.py
@@ -47,7 +47,7 @@ def test_that_setting_pinned_to_already_pinned_control_does_not_emit_signal(
         widget.set_pinned_control("control_b")
 
 
-def test_that_setting_pinned_to_none_allows_unselecting_all_controls(
+def test_that_setting_pinned_to_none_does_not_emit_signal_and_clears_selection(
     qtbot, controls_list
 ):
     widget = EverestControlSelectionWidget(controls=controls_list)
@@ -55,27 +55,53 @@ def test_that_setting_pinned_to_none_allows_unselecting_all_controls(
 
     widget.set_pinned_control("control_b")
 
-    control_a = widget._controls_list.item(0)
-    control_b = widget._controls_list.item(1)
-    control_c = widget._controls_list.item(2)
-    assert control_a is not None
-    assert control_b is not None
-    assert control_c is not None
+    for i in range(widget._controls_list.count()):
+        item = widget._controls_list.item(i)
+        assert item is not None
+        item.setSelected(True)
 
-    control_a.setSelected(True)
-    control_b.setSelected(True)
-    control_c.setSelected(True)
+    assert widget.get_selected_controls() == controls_list
+    with qtbot.assertNotEmitted(widget.controlSelectionChanged):
+        widget.set_pinned_control(None)
+        assert widget.get_selected_controls() == []
+
+
+def test_that_setting_pinned_control_to_non_existent_control_does_not_emit_signal_and_clears_selection(  # noqa: E501
+    qtbot, controls_list
+):
+    widget = EverestControlSelectionWidget(controls=controls_list)
+    qtbot.addWidget(widget)
+
+    widget.set_pinned_control("control_b")
+
+    for i in range(widget._controls_list.count()):
+        item = widget._controls_list.item(i)
+        assert item is not None
+        item.setSelected(True)
+
+    assert widget.get_selected_controls() == controls_list
+    with qtbot.assertNotEmitted(widget.controlSelectionChanged):
+        widget.set_pinned_control("non_existent_control")
+        assert widget.get_selected_controls() == []
+
+
+def test_that_setting_pinned_control_clears_selection_of_other_controls(
+    qtbot, controls_list
+):
+    widget = EverestControlSelectionWidget(controls=controls_list)
+    qtbot.addWidget(widget)
+
+    for i in range(widget._controls_list.count()):
+        item = widget._controls_list.item(i)
+        assert item is not None
+        item.setSelected(True)
 
     assert widget.get_selected_controls() == controls_list
 
-    control_a.setSelected(False)
-    control_b.setSelected(False)
-    control_c.setSelected(False)
+    widget.set_pinned_control("control_b")
 
-    assert widget.get_selected_controls() == ["control_b"]
-    widget.set_pinned_control(None)
-    control_b.setSelected(False)
-    assert widget.get_selected_controls() == []
+    selected = widget.get_selected_controls()
+    assert selected == ["control_b"]
 
 
 def test_that_setting_pinned_control_selects_it_and_makes_it_unselectable(
