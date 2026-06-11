4c3b58e9a477516e51ff388e2e7da3dba79b4267
Eilert Skram <eskra@equinor.com>
2026-06-11 14:09:20 +0200

    Delete runpath_argument.py
    Unused since 1b29b3a

--- src/ert/validation/__init__.py
+++ src/ert/validation/__init__.py
@@ -7,7 +7,6 @@ from .proper_name_argument import ExperimentValidation, ProperNameArgument
 from .proper_name_format_argument import ProperNameFormatArgument
 from .range_string_argument import RangeStringArgument, RangeSubsetStringArgument
 from .rangestring import mask_to_rangestring, rangestring_to_list, rangestring_to_mask
-from .runpath_argument import RunPathArgument
 from .string_definition import StringDefinition
 from .validation_status import ValidationStatus
 
@@ -22,7 +21,6 @@ __all__ = [
     "ProperNameFormatArgument",
     "RangeStringArgument",
     "RangeSubsetStringArgument",
-    "RunPathArgument",
     "StringDefinition",
     "ValidationStatus",
     "mask_to_rangestring",
