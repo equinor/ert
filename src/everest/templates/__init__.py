bb323c8b10a18cdd8eb04980f149fdf27bbe2dd8
Håvard Berland <havb@equinor.com>
2026-06-12 15:17:13 +0200

    Remove unused list

--- src/everest/templates/__init__.py
+++ src/everest/templates/__init__.py
@@ -1,14 +1,6 @@
 from importlib import util
 from pathlib import Path
 
-template_names = (
-    "schmerge.tmpl",
-    "slot_delay.tmpl",
-    "well_delay.tmpl",
-    "well_drill.tmpl",
-    "well_order.tmpl",
-)
-
 
 def fetch_template(template_name: str) -> str:
     module_spec = util.find_spec("everest.templates")
