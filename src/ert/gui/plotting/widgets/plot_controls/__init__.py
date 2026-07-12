90054b50faab2fe203b6e996cddf39630e6d5756
Magnus Elias Sletten <magnus_sletten@outlook.com>
2026-07-12 13:10:56 +0200

    Add general plot controls

--- src/ert/gui/plotting/widgets/plot_controls/__init__.py
+++ src/ert/gui/plotting/widgets/plot_controls/__init__.py
@@ -1,7 +1,9 @@
 from .everest_controls_plot_options import EverestControlsPlotOptions
+from .general_options import GeneralOptions
 from .misfits_options import MisfitsOptions
 
 __all__ = [
     "EverestControlsPlotOptions",
+    "GeneralOptions",
     "MisfitsOptions",
 ]
