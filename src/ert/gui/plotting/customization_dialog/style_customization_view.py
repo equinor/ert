57267a86012525c15c51549eb5b61d9926a22d8e
Eilert Skram <eskra@equinor.com>
2026-07-21 10:02:38 +0200

    Remove color selection from CustomizationDialog
    No longer in use and was being overwritten by the color palette selection

--- src/ert/gui/plotting/customization_dialog/style_customization_view.py
+++ src/ert/gui/plotting/customization_dialog/style_customization_view.py
@@ -2,7 +2,6 @@ from typing import TYPE_CHECKING, cast, override
 
 from PyQt6.QtWidgets import QHBoxLayout
 
-from .color_chooser import ColorBox
 from .customization_view import CustomizationView, WidgetProperty
 from .style_chooser import STYLESET_TOGGLE, StyleChooser
 
@@ -29,8 +28,6 @@ class StyleCustomizationView(CustomizationView):
     default_style = WidgetProperty()
     history_style = WidgetProperty()
     observations_style = WidgetProperty()
-    color_cycle = WidgetProperty()
-    observations_color = WidgetProperty()
 
     def __init__(self) -> None:
         CustomizationView.__init__(self)
@@ -77,56 +74,6 @@ class StyleCustomizationView(CustomizationView):
 
         self.add_spacing(10)
 
-        color_layout = QHBoxLayout()
-
-        self._color_boxes = []
-        for name in ["#1", "#2", "#3", "#4", "#5"]:
-            color_box = self.create_color_box(name)
-            self._color_boxes.append(color_box)
-            color_layout.addWidget(color_box)
-
-        self.add_row("Color cycle", color_layout)  # type: ignore
-        self.update_property(
-            "color_cycle",
-            StyleCustomizationView.get_color_cycle,
-            StyleCustomizationView.set_color_cycle,
-        )
-
-        self._observations_color_box = self.create_color_box("observations_color")
-        self.add_row("Observations color", self._observations_color_box)
-        self.update_property(
-            "observations_color",
-            StyleCustomizationView.get_observations_color,
-            StyleCustomizationView.set_observations_color,
-        )
-
-    def get_observations_color(self) -> tuple[str, float]:
-        return (
-            self._observations_color_box.color.name(),
-            self._observations_color_box.color.alphaF(),
-        )
-
-    def set_observations_color(self, color_tuple: tuple[str, float]) -> None:
-        self._observations_color_box.color = color_tuple
-
-    @staticmethod
-    def create_color_box(name: str) -> ColorBox:
-        color_box = ColorBox(size=20)
-        color_box.setToolTip(name)
-        return color_box
-
-    def get_color_cycle(self) -> list[tuple[str, float]]:
-        return [
-            (color_box.color.name(), color_box.color.alphaF())
-            for color_box in self._color_boxes
-        ]
-
-    def set_color_cycle(self, color_cycle: list[tuple[str, float]]) -> None:
-        for index, color_tuple in enumerate(color_cycle):
-            if 0 <= index < len(self._color_boxes):
-                color_box = self._color_boxes[index]
-                color_box.color = color_tuple
-
     @override
     def apply_customization(self, plot_config: "PlotConfig") -> None:
         plot_config.set_title(self.title)
@@ -135,8 +82,6 @@ class StyleCustomizationView(CustomizationView):
         plot_config.set_default_style(self.default_style)
         plot_config.set_history_style(self.history_style)
         plot_config.set_observations_style(self.observations_style)
-        plot_config.set_observations_color(self.observations_color)
-        plot_config.set_line_color_cycle(self.color_cycle)
 
     @override
     def revert_customization(self, plot_config: "PlotConfig") -> None:
@@ -149,5 +94,3 @@ class StyleCustomizationView(CustomizationView):
         self.default_style = plot_config.default_style()
         self.history_style = plot_config.history_style()
         self.observations_style = plot_config.observations_style()
-        self.observations_color = plot_config.observations_color()
-        self.color_cycle = plot_config.line_color_cycle()
