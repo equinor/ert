2394cb81a94fae6b6c09d81551cc8a80419b189f
Kaamya Shinde <kaams@equinor.com>
2026-07-31 10:19:34 +0200

    Rename theming to styling for better readability
    Renamed from theming to styling and grouped together the logic files in theme_manager to prepare for adding themes and color tokens

---
+++ src/ert/gui/styling/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from ert.gui.styling.theme_manager.manager import ColorSchemeManager
+from ert.gui.styling.theme_manager.theme import ColorScheme
+
+__all__ = [
+    "ColorScheme",
+    "ColorSchemeManager",
+]
