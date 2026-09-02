160dbbcfc9c01092b25cfcc1b044dc171d7ed018
Eilert Skram <eskra@equinor.com>
2026-09-02 15:40:47 +0200

    Move plot_histogram to generic shared plot
    Can most likely be reused and improves readability

--- src/ert/gui/plotting/ert_plots/distribution.py
+++ src/ert/gui/plotting/ert_plots/distribution.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import math
-from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -10,6 +9,7 @@ from matplotlib.lines import Line2D
 from scipy.stats import gaussian_kde
 
 from ert.gui.plotting.plot_api import EnsembleObject, PlotApiKeyDefinition
+from ert.gui.plotting.shared_plots.generic_histogram import plot_numeric_histogram
 from ert.gui.plotting.utils.plot_context import PlotType
 from ert.gui.plotting.utils.plot_tools import ConditionalAxisFormatter, PlotTools
 from ert.gui.utils import truncate_experiment_name
@@ -114,7 +114,7 @@ class DistributionPlot:
                 )
 
             if self._histogram:
-                self._plot_histogram(data[0], plot_context, histogram_axes)
+                plot_numeric_histogram(data[0], plot_context, histogram_axes)
 
             self._add_ensemble_legend(config, ensemble)
 
@@ -222,37 +222,6 @@ class DistributionPlot:
         if log_scale:
             axes.set_xscale("log")
         axes.plot(indexes, evaluated, color=config.current_color())
-
-    def _plot_histogram(
-        self,
-        data: pd.Series,
-        plot_context: PlotContext,
-        histogram_axes: Axes,
-    ) -> None:
-        if PlotTools.array_is_empty_or_non_numeric(data):
-            return
-
-        config = plot_context.plotConfig()
-        bins: str | Sequence[float]
-        if plot_context.log_scale:
-            log_edges = np.histogram_bin_edges(np.log10(data), bins="sqrt")
-            edges = 10**log_edges
-            # 10 ** log10(x) may not round-trip, which would drop the extreme values
-            edges[0] = min(edges[0], data.min())
-            edges[-1] = max(edges[-1], data.max())
-            bins = edges.tolist()
-            histogram_axes.set_xscale("log")
-        else:
-            bins = "sqrt"
-            histogram_axes.set_xscale("linear")
-            histogram_axes.xaxis.set_major_formatter(ConditionalAxisFormatter())
-
-        histogram_axes.hist(
-            data,
-            bins=bins,
-            alpha=0.3,
-            color=config.current_color(),
-        )
 
     def _plot_rug(
         self,
