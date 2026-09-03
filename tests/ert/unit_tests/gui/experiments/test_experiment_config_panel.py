c0ced2509411c2a25cf213336b42a6a2c6ccaaba
Torbjørn Wiik <22935970+tbwiik@users.noreply.github.com>
2026-09-03 10:23:08 +0200

    Test correct disable functionality of esmda
    If there are no updatable parameters for the current config,
    it should not be possible to run updatable experiments.
    
    If there are no updatable parameters for the current config,
    but the prior ensemble is selected,
    it should be possible to run this prior ensemble.

--- tests/ert/unit_tests/gui/experiments/test_experiment_config_panel.py
+++ tests/ert/unit_tests/gui/experiments/test_experiment_config_panel.py
@@ -1,22 +1,21 @@
 from unittest.mock import Mock
 
+import pytest
+
 from ert.config.parameter_config import LocalizationType, ParameterConfig
 from ert.gui.experiments.experiment_config_panel import has_updatable_parameters
 
 
-def test_that_has_updatable_parameters_returns_false_when_absent():
-
-    param_mock: ParameterConfig = Mock(spec=ParameterConfig, update_strategy=None)
-    parameter_configuration = [param_mock]
-
-    assert not has_updatable_parameters(parameter_configuration)
-
-
-def test_that_has_updatable_parameters_returns_true_when_present():
-
+@pytest.mark.parametrize(
+    ("update_strategy", "expected"),
+    [
+        (None, False),
+        (LocalizationType.GLOBAL, True),
+    ],
+)
+def test_has_updatable_parameters(update_strategy, expected):
     param_mock: ParameterConfig = Mock(
-        spec=ParameterConfig, update_strategy=LocalizationType.GLOBAL
+        spec=ParameterConfig, update_strategy=update_strategy
     )
     parameter_configuration = [param_mock]
-
-    assert has_updatable_parameters(parameter_configuration)
+    assert has_updatable_parameters(parameter_configuration) is expected
