81e2ec15e01c2c6534ebfaf887cda21a88791edd
Eilert Skram <eskra@equinor.com>
2026-09-04 14:06:56 +0200

    Revert "Rename theming to styling for better readability"
    This reverts commit ea68499a42d4b2c0a6748199e5ae81adc6a4fdd9.

--- src/ert/gui/styling/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from ert.gui.styling.theme_manager.manager import ColorSchemeManager
-from ert.gui.styling.theme_manager.theme import ColorScheme
-
-__all__ = [
-    "ColorScheme",
-    "ColorSchemeManager",
-]
---
+++ src/ert/gui/theming/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from .manager import ColorSchemeManager
+from .theme import ColorScheme
+
+__all__ = [
+    "ColorScheme",
+    "ColorSchemeManager",
+]
