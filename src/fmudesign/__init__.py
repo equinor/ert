80dbe2737ca88358ada30b285f54d30278ffb294
eq-sandkav <skav@equinor.com>
2026-09-17 09:34:28 +0200

    Fixup, add nullhandler to init logger

--- src/fmudesign/__init__.py
+++ src/fmudesign/__init__.py
@@ -4,10 +4,13 @@ setting up design matrix to run single sensitivities with ERT.
 Output of this module can be used in custom standalone applications.
 """
 
+import logging
+
 from ._designsummary import summarize_design
 from ._excel_to_dict import excel_to_dict, inputdict_to_yaml
 from .create_design import DesignMatrix
 
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 __all__ = [
     "DesignMatrix",
     "excel_to_dict",
