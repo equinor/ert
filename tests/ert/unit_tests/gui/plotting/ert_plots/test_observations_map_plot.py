68f87eead225ee77ccc5bd0c80a3acae5cea0420
akust <211008703+akustn@users.noreply.github.com>
2026-09-21 10:33:12 +0200

    fixup! Add observations map plot tabs

--- tests/ert/unit_tests/gui/plotting/ert_plots/test_observations_map_plot.py
+++ tests/ert/unit_tests/gui/plotting/ert_plots/test_observations_map_plot.py
@@ -1,46 +1,8 @@
 import pandas as pd
-import pytest
 from matplotlib.figure import Figure
 
 from ert.gui.plotting.ert_plots.observations_map import ObservationsMapPlot
-from ert.gui.plotting.plot_api import EnsembleObject, PlotApiKeyDefinition
 from ert.gui.plotting.utils.plot_config import PlotConfig
-from ert.gui.plotting.utils.plot_context import PlotContext
-
-
-@pytest.fixture
-def make_plot_context():
-    def _make(
-        ensembles: list[EnsembleObject],
-        *,
-        key: str = "FOPR",
-        plot_config: PlotConfig | None = None,
-    ) -> PlotContext:
-        return PlotContext(
-            plot_config if plot_config is not None else PlotConfig(),
-            ensembles=ensembles,
-            ensembles_color_indexes=list(range(len(ensembles))),
-            key=key,
-            layer=None,
-        )
-
-    return _make
-
-
-@pytest.fixture
-def make_key_def():
-    def _make(
-        *, key: str = "FOPR", data_origin: str = "summary"
-    ) -> PlotApiKeyDefinition:
-        return PlotApiKeyDefinition(
-            key,
-            index_type=None,
-            metadata={"data_origin": data_origin},
-            observations=True,
-            dimensionality=2,
-        )
-
-    return _make
 
 
 def test_that_observations_map_shows_no_data_message_when_observation_data_is_empty(
@@ -48,7 +10,7 @@ def test_that_observations_map_shows_no_data_message_when_observation_data_is_em
 ):
     figure = Figure()
     plot_context = make_plot_context([])
-    key_def = make_key_def()
+    key_def = make_key_def(data_origin="summary")
     ObservationsMapPlot().plot(
         figure=figure,
         plot_context=plot_context,
@@ -70,7 +32,7 @@ def test_that_observations_map_renders_tripcolor_with_default_axis_labels_and_ti
     plot_context = make_plot_context(
         [], plot_config=PlotConfig(title="Observation map")
     )
-    key_def = make_key_def()
+    key_def = make_key_def(data_origin="summary")
     observation_data = pd.DataFrame(
         data={
             0: [100.0, 200.0, 10.0],
