0ca6df3e549ff4eb975be22997f8a4f14e5427ad
Alena Chaikouskaya <alec@equinor.com>
2026-09-24 11:15:26 +0200

    Revert "Switch to polars for retrieving ropt results"
    This reverts commit a716db1d79a3c964d936cb816abfd1d332e4a2c2.

--- src/everest/util/ropt_unpacker.py
+++ src/everest/util/ropt_unpacker.py
@@ -105,7 +105,9 @@ def _ropt_to_df(
     values: list[str],
     select: list[str],
 ) -> pl.DataFrame:
-    df = results.to_polars(values).select(select + values)
+    df = pl.from_pandas(
+        results.to_pandas(values).reset_index(),
+    ).select(select + values)
     df = _rename_ropt_df_columns(df)
     return _enforce_dtypes(df)
 
