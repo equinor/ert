a709c05ea132bc7e0f055fc4cd3d03370e4fb3a4
Peter Verveer <pieter.verveer@tno.nl>
2026-09-24 18:13:38 +0200

    Add offsets to objective scaling

--- src/ert/storage/local_storage.py
+++ src/ert/storage/local_storage.py
@@ -33,7 +33,7 @@ from .realization_storage_state import RealizationStorageState
 logger = logging.getLogger(__name__)
 
 
-_LOCAL_STORAGE_VERSION = 40
+_LOCAL_STORAGE_VERSION = 41
 
 
 def open_storage(
@@ -647,6 +647,7 @@ class LocalStorage(BaseMode):
             to38,
             to39,
             to40,
+            to41,
         )
 
         try:  # ruff: ignore[too-many-statements-in-try-clause]
@@ -716,6 +717,7 @@ class LocalStorage(BaseMode):
                     37: to38,
                     38: to39,
                     39: to40,
+                    40: to41,
                 }
                 for from_version in range(version, _LOCAL_STORAGE_VERSION):
                     migrations[from_version].migrate(self.path)
